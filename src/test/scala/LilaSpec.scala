eccdefe394c279d6124899e82b9ee1eab8f60a2c
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-02-23 21:53:29 +0100

    Add lila specific test matcher

--- src/test/scala/LilaSpec.scala
+++ src/test/scala/LilaSpec.scala
@@ -2,13 +2,15 @@ package lila
 
 import ornicar.scalalib.test.OrnicarValidationMatchers
 import org.specs2.mutable.Specification
+import org.specs2.matcher.{ Matcher }
+
+import model._
 
 trait LilaSpec
     extends Specification
     with OrnicarValidationMatchers {
 
-  implicit def anyMatchers[A](a: A) = {
-    def equals(b: A) = a mustEqual b
+  def bePoss(poss: Pos*): Matcher[Valid[Set[Pos]]] = beSuccess.like {
+    case p ⇒ p.toSet must_== poss
   }
-
 }
