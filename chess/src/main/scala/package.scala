a322c77131dcff2ca81749ce4c4a4a1a1636f6f8
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-03-04 16:55:46 +0100

    Add .pp implicit function to chess package

--- chess/src/main/scala/package.scala
+++ chess/src/main/scala/package.scala
@@ -10,4 +10,8 @@ package object chess extends OrnicarValidation with OrnicarCommon {
   object implicitFailures {
     implicit def stringToFailures(str: String): Failures = str wrapNel
   }
+
+  implicit def addPP[A](a: A) = new {
+    def pp[A] = a ~ println
+  }
 }
