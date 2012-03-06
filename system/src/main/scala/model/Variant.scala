01f8e82d1e79a4db1ed1daf7d928ed619cbaf951
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-03-06 23:18:41 +0100

    Simplify variants using a sealed abstract class

--- system/src/main/scala/model/Variant.scala
+++ system/src/main/scala/model/Variant.scala
@@ -1,28 +1,11 @@
 package lila.system
 package model
 
-sealed trait Variant {
+sealed abstract class Variant(val id: Int)
 
-  val name: String
+case object Standard extends Variant(1)
 
-  val id: Int
-
-  override def toString = name
-}
-
-case object Standard extends Variant {
-
-  val name = "standard"
-
-  val id = 1
-}
-
-case object Chess960 extends Variant {
-
-  val name = "chess960"
-
-  val id = 2
-}
+case object Chess960 extends Variant(2)
 
 object Variant {
 
