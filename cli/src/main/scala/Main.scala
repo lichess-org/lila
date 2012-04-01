feced73d6d6486a7b5f46cc103862366ce8b0dc9
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-04-01 14:41:50 +0200

    Fix import-rooms command call

--- cli/src/main/scala/Main.scala
+++ cli/src/main/scala/Main.scala
@@ -13,7 +13,7 @@ object Main {
       //case "compact" :: Nil ⇒ CompactDb(env.mongodb)
       case "info" :: Nil         ⇒ Info(env)
       case "index" :: Nil        ⇒ IndexDb(env.gameRepo)
-      case "import-rooms" :: Nil ⇒ MigrateRooms(env.mongodb)
+      case "import-rooms" :: Nil ⇒ ImportRooms(env.mongodb)
       case "import-games" :: Nil ⇒ ImportGames(env.mongodb, env.gameRepo)
       case _ ⇒ new Command {
         def apply() = putStrLn("Usage: run command args")
