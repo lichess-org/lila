ef4dfd011d4016d5fa56375e32ec5600dd212e92
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-04-01 23:01:41 +0200

    Test castle breakage by capture

--- chess/src/test/scala/GameTest.scala
+++ chess/src/test/scala/GameTest.scala
@@ -21,12 +21,30 @@ class GameTest extends ChessTest {
       val game = Game("""
 bq
 R""").playMoves(
-  A1 -> A2,
-  B2 -> A2)
+        A1 -> A2,
+        B2 -> A2)
       game must beSuccess.like {
         case g ⇒ g.deads must haveTheSameElementsAs(List(
           A2 -> Black.bishop,
           A2 -> White.rook))
+      }
+    }
+  }
+  "prevent castle by capturing a rook" should {
+    val game = Game("""
+ b
+R   K""", Black)
+    "can castle queenside" in {
+      game.board.history canCastle White on QueenSide must_== true
+    }
+    "can still castle queenside" in {
+      game.playMoves(B2 -> A3) must beSuccess.like {
+        case g ⇒ g.board.history canCastle White on QueenSide must_== true
+      }
+    }
+    "can not castle queenside anymore" in {
+      game.playMoves(B2 -> A1) must beSuccess.like {
+        case g ⇒ g.board.history canCastle White on QueenSide must_== false
       }
     }
   }
