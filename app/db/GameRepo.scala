848e3322b25c1a7537bd0a922748122099e9e4f0
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-04-18 12:44:56 +0200

    Move DbGame diffing in a distinct class

--- app/db/GameRepo.scala
+++ app/db/GameRepo.scala
@@ -48,41 +48,13 @@ class GameRepo(collection: MongoCollection)
   }
 
   def save(progress: Progress): IO[Unit] =
-    diff(encode(progress.origin), encode(progress.game)) |> { diffs ⇒
+    new GameDiff(encode(progress.origin), encode(progress.game))() |> { diffs ⇒
       if (diffs.nonEmpty) {
         val fullDiffs = ("updatedAt" -> new Date()) :: diffs
         io { update(DBObject("_id" -> progress.origin.id), $set(fullDiffs: _*)) }
       }
       else io()
     }
-
-  def diff(a: RawDbGame, b: RawDbGame): List[(String, Any)] = {
-    val builder = scala.collection.mutable.ListBuffer[(String, Any)]()
-    def d[A](name: String, f: RawDbGame ⇒ A) {
-      if (f(a) != f(b)) builder += name -> f(b)
-    }
-    d("pgn", _.pgn)
-    d("status", _.status)
-    d("turns", _.turns)
-    d("lastMove", _.lastMove)
-    d("check", _.check)
-    d("positionHashes", _.positionHashes)
-    d("castles", _.castles)
-    for (i ← 0 to 1) {
-      val name = "players." + i + "."
-      d(name + "ps", _.players(i).ps)
-      d(name + "w", _.players(i).w)
-      d(name + "lastDrawOffer", _.players(i).lastDrawOffer)
-      d(name + "isOfferingDraw", _.players(i).isOfferingDraw)
-    }
-    a.clock foreach { c ⇒
-      d("clock.c", _.clock.get.c)
-      d("clock.w", _.clock.get.w)
-      d("clock.b", _.clock.get.b)
-      d("clock.timer", _.clock.get.timer)
-    }
-    builder.toList
-  }
 
   def insert(game: DbGame): IO[Option[String]] = io {
     insert(encode(game))
