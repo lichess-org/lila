4e6445a7bace90bde8db2a1f59591f3a510d148c
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-05-07 00:17:19 +0200

    Handle malformed pgn fen tags

--- chess/src/main/scala/format/PgnReader.scala
+++ chess/src/main/scala/format/PgnReader.scala
@@ -3,26 +3,28 @@ package format
 
 object PgnReader {
 
-  def apply(pgn: String): Valid[Replay] =
-    PgnParser(pgn) flatMap { parsed ⇒
-      parsed.sans.foldLeft(Replay(makeGame(parsed.tags)).success: Valid[Replay]) {
-        case (replayValid, san) ⇒ for {
-          replay ← replayValid
-          move ← san(replay.game)
-        } yield new Replay(
-          game = replay.game(move),
-          moves = move :: replay.moves)
-      }
+  def apply(pgn: String): Valid[Replay] = for {
+    parsed ← PgnParser(pgn)
+    game ← makeGame(parsed.tags)
+    replay ← parsed.sans.foldLeft(Replay(game).success: Valid[Replay]) {
+      case (replayValid, san) ⇒ for {
+        replay ← replayValid
+        move ← san(replay.game)
+      } yield new Replay(
+        game = replay.game(move),
+        moves = move :: replay.moves)
     }
+  } yield replay
 
-  def makeGame(tags: List[Tag]): Game =
+  def makeGame(tags: List[Tag]): Valid[Game] =
     tags collect {
       case Fen(fen) ⇒ fen
     } match {
-      case Nil ⇒ Game()
-      case fen :: _ ⇒ (Forsyth << fen).fold(
-        situation ⇒ Game(board = situation.board, player = situation.color),
-        Game()
+      case Nil ⇒ Game().success
+      case fen :: Nil ⇒ (Forsyth << fen).fold(
+        s ⇒ Game(board = s.board, player = s.color).success,
+        "Invalid fen %s".format(fen).failNel
       )
+      case many ⇒ "Multiple fen tags".failNel
     }
 }
