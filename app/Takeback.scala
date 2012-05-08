b0dea687e27e0f0b112e6a0596c342d7ffa37a6e
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-05-08 22:17:55 +0200

    Add game info to takeback failure messages

--- app/Takeback.scala
+++ app/Takeback.scala
@@ -10,14 +10,19 @@ final class Takeback(
     messenger: Messenger) {
 
   def apply(game: DbGame): Valid[IO[List[Event]]] =
-    game.rewind map save
+    game.rewind map save mapFail failInfo(game)
 
-  def double(game: DbGame): Valid[IO[List[Event]]] = for {
-    p1 ← game.rewind
-    p2 ← p1.game.rewind map { p ⇒
-      p1 withGame p.game
-    }
-  } yield save(p2)
+  def double(game: DbGame): Valid[IO[List[Event]]] = {
+    for {
+      p1 ← game.rewind
+      p2 ← p1.game.rewind map { p ⇒
+        p1 withGame p.game
+      }
+    } yield save(p2)
+  } mapFail failInfo(game)
+
+  def failInfo(game: DbGame) =
+    (failures: Failures) ⇒ "Takeback %s".format(game.id) <:: failures
 
   private def save(p1: Progress): IO[List[Event]] = for {
     _ ← messenger.systemMessage(p1.game, "Takeback proposition accepted")
