cf6f751aba09a3da3e9c43617e4d934a73964f1d
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-05-09 20:43:54 +0200

    Use two pgn parsers instead of one

--- chess/src/main/scala/format/PgnParser.scala
+++ chess/src/main/scala/format/PgnParser.scala
@@ -3,85 +3,112 @@ package format
 
 import scala.util.parsing.combinator._
 
-object PgnParser extends RegexParsers {
+object PgnParser {
 
-  def apply(pgn: String): Valid[ParsedPgn] =
-    parseAll(all, pgn) match {
-      case f: Failure ⇒
-        "Cannot parse pgn: %s\n%s".format(f.toString, pgn).failNel
+  type TagType = (List[Tag], String)
+
+  def apply(pgn: String): Valid[ParsedPgn] = for {
+    parsed ← TagParser(pgn)
+    (tags, moveString) = parsed
+    sans ← MoveParser(moveString)
+  } yield ParsedPgn(tags, sans)
+
+  object TagParser extends RegexParsers {
+
+    def apply(pgn: String): Valid[TagType] = parseAll(all, pgn) match {
+      case f: Failure       ⇒ "Cannot parse tags: %s\n%s".format(f.toString, pgn).failNel
       case Success(sans, _) ⇒ scalaz.Scalaz.success(sans)
     }
 
-  def all: Parser[ParsedPgn] = tags ~ moves <~ (result?) ^^ {
-    case ts ~ ms ⇒ ParsedPgn(tags = ts, sans = ms)
-  }
-
-  def tags: Parser[List[Tag]] = rep(tag) <~ (CRLF?)
-
-  def tag: Parser[Tag] = tagName ~ tagValue <~ (CRLF?) ^^ {
-    case "FEN" ~ value ⇒ Fen(value)
-    case name ~ value  ⇒ Unknown(name, value)
-  }
-
-  val tagName: Parser[String] = "[" ~> """[a-zA-Z]+""".r
-
-  val tagValue: Parser[String] = "\"" ~> """[^"]+""".r <~ "\"]"
-
-  def moves: Parser[List[San]] = rep(move)
-
-  val result: Parser[String] = "*" | "1/2-1/2" | "0-1" | "1-0"
-
-  def move: Parser[San] = (number?) ~> (qCastle | kCastle | std) <~ (comment?)
-
-  val comment: Parser[String] = "{" ~> """[^\}]+""".r <~ "}"
-
-  val qCastle: Parser[San] = "O-O-O" ^^^ Castle(QueenSide)
-
-  val kCastle: Parser[San] = "O-O" ^^^ Castle(KingSide)
-
-  def std: Parser[Std] = (simple | disambiguated) ~ suffixes ^^ {
-    case std ~ suf ⇒ std withSuffixes suf
-  }
-
-  val number: Parser[String] = """\d+\.+ """.r
-
-  def simple: Parser[Std] = role ~ x ~ dest ^^ {
-    case ro ~ ca ~ de ⇒ Std(dest = de, role = ro, capture = ca)
-  }
-
-  def disambiguated: Parser[Std] = role ~ opt(file) ~ opt(rank) ~ x ~ dest ^^ {
-    case ro ~ fi ~ ra ~ ca ~ de ⇒ Std(
-      dest = de, role = ro, capture = ca, file = fi, rank = ra)
-  }
-
-  def suffixes: Parser[Suffixes] = opt(promotion) ~ check ~ checkmate ^^ {
-    case p ~ c ~ cm ⇒ Suffixes(c, cm, p)
-  }
-
-  val x = exists("x")
-
-  val check = exists("+")
-
-  val checkmate = exists("#")
-
-  val role = mapParser(Role.allByPgn, "role") | success(Pawn)
-
-  val file = mapParser(rangeToMap('a' to 'h'), "file")
-
-  val rank = mapParser(rangeToMap('1' to '8'), "rank")
-
-  val promotion = "=" ~> mapParser(Role.allPromotableByPgn, "promotion")
-
-  val dest = mapParser(Pos.allKeys, "dest")
-
-  val CRLF = "\r\n" | "\n"
-
-  def exists(c: String): Parser[Boolean] = c ^^^ true | success(false)
-
-  def rangeToMap(r: Iterable[Char]) = r.zipWithIndex.toMap mapValues (_ + 1)
-
-  def mapParser[A, B](map: Map[A, B], name: String): Parser[B] =
-    map.foldLeft(failure(name + " not found"): Parser[B]) {
-      case (acc, (a, b)) ⇒ a.toString ^^^ b | acc
+    def all: Parser[TagType] = tags ~ """(.|\n)+""".r ^^ {
+      case ts ~ ms ⇒ (ts, ms)
     }
+
+    def tags: Parser[List[Tag]] = rep(tag)
+
+    def tag: Parser[Tag] = tagName ~ tagValue ^^ {
+      case "FEN" ~ value ⇒ Fen(value)
+      case name ~ value  ⇒ Unknown(name, value)
+    }
+
+    val tagName: Parser[String] = "[" ~> """[a-zA-Z]+""".r
+
+    val tagValue: Parser[String] = "\"" ~> """[^"]+""".r <~ "\"]"
+  }
+
+  object MoveParser extends RegexParsers {
+
+    val CRLF = "\r\n" | "\n"
+    val space = """\s+""".r
+
+    override val whiteSpace = "".r
+
+    def apply(pgn: String): Valid[List[San]] = 
+      parseAll(moves, (pgn.lines mkString " ")) match {
+        case f: Failure       ⇒ "Cannot parse moves: %s\n%s".format(f.toString, pgn).failNel
+        case Success(sans, _) ⇒ scalaz.Scalaz.success(sans)
+      }
+
+    def moves: Parser[List[San]] = repsep(move, space) <~ (result?)
+
+    val result: Parser[String] = space ~> ("*" | "1/2-1/2" | "0-1" | "1-0")
+
+    def move: Parser[San] = 
+      (number?) ~> (qCastle | kCastle | std) <~ (comment?) 
+
+    val comment: Parser[String] = space ~> "{" ~> """[^\}]+""".r <~ "}"
+
+    val qCastle: Parser[San] = "O-O-O" ^^^ Castle(QueenSide)
+
+    val kCastle: Parser[San] = "O-O" ^^^ Castle(KingSide)
+
+    def std: Parser[Std] = (complete | simple | disambiguated) ~ suffixes ^^ {
+      case std ~ suf ⇒ std withSuffixes suf
+    }
+
+    val number: Parser[String] = """\d+\.+""".r <~ space
+
+    def simple: Parser[Std] = role ~ x ~ dest ^^ {
+      case ro ~ ca ~ de ⇒ Std(dest = de, role = ro, capture = ca)
+    }
+
+    def disambiguated: Parser[Std] = role ~ opt(file) ~ opt(rank) ~ x ~ dest ^^ {
+      case ro ~ fi ~ ra ~ ca ~ de ⇒ Std(
+        dest = de, role = ro, capture = ca, file = fi, rank = ra)
+    }
+
+    def complete: Parser[Std] = role ~ file ~ rank ~ x ~ dest ^^ {
+      case ro ~ fi ~ ra ~ ca ~ de ⇒ Std(
+        dest = de, role = ro, capture = ca, file = fi.some, rank = ra.some)
+    }
+
+    def suffixes: Parser[Suffixes] = opt(promotion) ~ check ~ checkmate ^^ {
+      case p ~ c ~ cm ⇒ Suffixes(c, cm, p)
+    }
+
+    val x = exists("x")
+
+    val check = exists("+")
+
+    val checkmate = exists("#")
+
+    val role = mapParser(Role.allByPgn, "role") | success(Pawn)
+
+    val file = mapParser(rangeToMap('a' to 'h'), "file")
+
+    val rank = mapParser(rangeToMap('1' to '8'), "rank")
+
+    val promotion = "=" ~> mapParser(Role.allPromotableByPgn, "promotion")
+
+    val dest = mapParser(Pos.allKeys, "dest")
+
+    def exists(c: String): Parser[Boolean] = c ^^^ true | success(false)
+
+    def rangeToMap(r: Iterable[Char]) = r.zipWithIndex.toMap mapValues (_ + 1)
+
+    def mapParser[A, B](map: Map[A, B], name: String): Parser[B] =
+      map.foldLeft(failure(name + " not found"): Parser[B]) {
+        case (acc, (a, b)) ⇒ a.toString ^^^ b | acc
+      }
+  }
 }
--- chess/src/test/scala/format/PgnReaderTest.scala
+++ chess/src/test/scala/format/PgnReaderTest.scala
@@ -10,24 +10,10 @@ class PgnReaderTest extends ChessTest {
     "c4 Nc6 e3 Nf6 h3 Ne4 d3 Nc5 a3 Ne5 d4 d6 dxe5 dxe5 b4 Qxd1+ Kxd1 Ne4 f3 Nf2+ Ke2 Nxh1 Nd2 Ng3+ Ke1 Bf5 Bd3 Bxd3 Rb1 Bxb1 Nxb1 Rd8 Bd2 e6 h4 Be7 Nh3 Bxh4 Nf2 Ke7 Bc3 f6 Nd2 h5 c5 g5 Nc4 Rhg8 Na5 Nh1 Ke2 Nxf2 Be1 Nd3 Nxb7 Bxe1 Nxd8 Rxd8 c6 a5 bxa5 Bxa5 a4 f5 Kd1 Nf4+ Kc2 Rd2+ Kc1 Nxg2 Kb1 Nxe3 Kc1 h4 Kb1 h3 Kc1 h2 Kb1 h1=Q#",
     "d4 d5 c4 e6 a3 Nf6 Nf3 dxc4 Nc3 c5 Qa4+ Nc6 dxc5 Bxc5 b4 cxb3 Qxb3 O-O Bg5 Be7 e3 b6 Bb5 Bb7 O-O Nd5 Nxd5 Bxg5 Nxg5 Qxg5 Bxc6 Bxc6 Nb4 Qxg2#",
     "d4 Nf6 c4 Nc6 Nc3 e5 Nd5 Nxd5 cxd5 Nxd4 e3 Nf5 e4 Nd4 h4 Qf6 Bg5 Qb6 b3 h6 Bc4 hxg5 h5 Bc5 Ne2 Qa5+ Kf1 d6 Nxd4 Bxd4 Rc1 Qxa2 Rc2 Qa5 Qc1 g4 h6 g3 f3 gxh6 Rxh6 Rxh6 Qxh6 Bf2 Qh8+ Kd7 Qf8 Qe1#",
-    "e4 Nc6 d4 Nf6 d5 Ne5 Nf3 Nxf3+ gxf3 e6 Nd2 exd5 e5 Qe7 c4 Qxe5+ Be2 d4 Rg1 d3 b4 Qxa1 Bb2 Qxb2 Qc2 dxc2 Nb1 Qc1+ Bd1 d1=Q#",
-    "e4 d5 exd5 Nf6 Nc3 Nxd5 Nxd5 Qxd5 Nf3 Bg4 c4 Bxf3 Qxf3 Qe6+ Be2 Nc6 O-O Nd4 Qxb7 Nxe2+ Kh1 Rd8 Re1 Qg4 Qc6+ Rd7 Qa8+ Rd8 Qc6+ Qd7 Qxd7+ Rxd7 Rxe2 e6 b3 Bb4 Bb2 f5 Bc3 Bxc3 dxc3 Rd3 Rxe6+ Kf7 Re3 Rd2 a4 Rhd8 b4 Rd1+ Re1 Rxe1+ Rxe1 Rd3 Re3 Rd1+ Re1 Rxe1#",
-    "d4 d5 c4 b6 cxd5 Na6 e4 Nf6 Bb5+ Bd7 Bxa6 Ba4 Qxa4+ c6 Qxc6+ Nd7 e5 Rc8 Bxc8 Qxc8 Qxc8#",
     "Nc3 c6 Nf3 Na6 b4 Nxb4 Rb1 c5 a3 Nxc2+ Qxc2 b6 Nb5 Ba6 Qa4 Bxb5 Rxb5 Nf6 Bb2 Nd5 Qg4 Nc7 Bxg7 Bxg7 Qxg7 Rf8 Rb3 Ne6 Qxh7 Qb8 Re3 f6 Qg6+ Rf7 g3 Nf8 Qg8 e5 d4 d6 dxc5 Qc7 cxd6 Qc1#",
     "d4 Nf6 Nf3 Nc6 Nbd2 e6 e4 d6 c4 Qe7 Bd3 e5 d5 Nd4 Nxd4 exd4 O-O Bg4 f3 Bd7 Nb3 Qe5 Be2 c5 dxc6 bxc6 Qxd4 Qxd4+ Nxd4 Rb8 b3 Be7 Be3 Bd8 Rfd1 Bb6 Kf2 Ke7 Rd2 Ba5 Rd3 Bb6 Rad1 Rhd8 g4 h6 Bf4 g5 Bg3 h5 h3 h4 Bh2 Rb7 e5 dxe5 Bxe5 Ne8 Kg2 Bc7 Bxc7 Rxc7 Nf5+ Kf8 f4 gxf4 Nxh4 Ng7 Bf3 Ne6 Nf5 Nc5 Rd4 Ne6 Rd6 c5 h4 Ng7 Nxg7 Kxg7 g5 a5 Kf2 Kf8 Bc6 Ke7 Ba4 Bxa4 Rxd8 Bc6 h5 Ke6 h6 Be4 Rh8 Re7 Re1 Kf5 h7 Kg6 Rc8 Kxh7 Rxc5 a4 b4 Kg6 b5 f6 gxf6 Kxf6 b6 a3 Rc7 Rxc7 bxc7 Bb7 Re8 Kf5 c5 Ba6 Ra8 Bb7 Rf8+ Ke5 c6 Ba6 Ra8 Kd6 Rxa6 Kxc7 Kf3 Kb8 Kxf4 Kc8 Ke5 Kc7 Ke6 Kd8 Kd6 Ke8 Ra7 Kf8 c7 Kf7 c8=Q+ Kg6 Qg4+ Kf6 Ra8 Kf7 Qf5+ Kg7 Ra7+ Kg8 Qc8#",
     "e3 Nc6 Nf3 Nf6 Nc3 e6 a3 Bd6 d4 Ng4 h3 Nf6 Bb5 a6 Bxc6 dxc6 e4 Nd7 O-O h5 h4 c5 Bg5 f6 Be3 cxd4 Bxd4 c5 Be3 Qe7 g3 Ne5 Nxe5 Bxe5 Qe2 Bxc3 bxc3 Kf7 Rad1 b5 c4 Rb8 Rd2 Bb7 Rfd1 Bc6 Kg2 Bxe4+ Kf1 Bc6 Bf4 e5 Be3 Qe6 Bxc5 Qh3+ Ke1 Qh1+ Qf1 Qe4+ Re2 Qxc4 Bd6 Rbd8 Bb4 Bf3 Rxd8 Rxd8 Re3 Rd1#",
-    "e4 e5 Bc4 Nf6 d3 d5 Bg5 Be6 exd5 Bxd5 Bxd5 Qxd5 f3 e4 Bxf6 gxf6 fxe4 Qa5+ c3 Nd7 b4 Qb6 a4 Ne5 Nf3 O-O-O a5 Qe6 Nd4 Qd7 Qh5 Bxb4 cxb4 Qxd4 Qf5+ Kb8 Kd2 Nc4+ Ke2 Qxd3+ Kf2 Qe3+ Kf1 Rd1#",
-    "h4 d5 d4 Qd6 f4 Qb4+ c3 Qb6 b4 Qc6 Nf3 Bf5 Nfd2 Qg6 Rg1 Qg3#",
-    "d4 d5 e3 e6 f3 Bd6 c3 c6 b4 b5 g4 g5 h3 Bg3+ Ke2 f6 Kd2 Qd6 Qe2 a5 a4 axb4 cxb4 Qxb4+ Kd1 bxa4 Qg2 Qe1+ Kc2 Bf2 Bd2 Bxe3 Qe2 Qh4 Qxe3 Nd7 Be1 Qh6 Bd3 a3 Nxa3 Nb6 Rb1 Nd7 Rb3 Ba6 Kb2 Ke7 Bb4+ Kf7 Be1 Qf8 h4 gxh4 Rxh4 Bb7 Rxb7 Qxa3+ Kc2 Qa2+ Kc3 Ra3+ Kb4 Qb2#",
-    "d4 Nc6 Nc3 Nf6 e4 d6 Bb5 Bg4 f3 Be6 d5 Nxe4 dxc6 Nxc3 cxb7+ Nxb5 a8=Q Qxa8 Qd3 c6 Bd2 Bd5 c4 Nc7 cxd5 Nxd5 Rc1 e5 Ne2 Be7 O-O O-O h3 Rb8 b3 Bh4 Kh2 Kh8 g3 Bf6 h4 a5 Kh3 a4 Rc4 axb3 axb3 Qa6 Rb1 Ra8 b4 Qb5 Nc3 Nxc3 Qxc3 e4 Qc2 Qf5+ g4 Qxf3+ Kh2 Qf2+ Kh1 Qxh4+ Kg2 Qxg4+ Kf1 Qf3+ Ke1 Bh4#",
-    "e4 Nc6 Nc3 Nf6 Bc4 e6 Nf3 Bc5 d4 Bd6 Qe2 Ng4 h3 Nf6 e5 Bb4 exf6 Qxf6 Be3 Qf5 Bd3 Qd5 a3 Qa5 O-O Bxc3 bxc3 Qxc3 Rac1 Qxa3 d5 exd5 Bc5+ Kd8 Bxa3 Re8 Qd2 h6 Bb2 f6 Bg6 Re4 Rce1 Rxe1 Rxe1 d6 Qxd5 a5 Qg8+ Kd7 Qe8#",
-    "d3 Nc6 e3 Nf6 Be2 d6 Bf3 Be6 Bxc6+ bxc6 e4 Rb8 d4 Nxe4 f3 Nf6 Nh3 Bc4 Ng5 Nd5 a3 e6 f4 Be7 f5 Bxg5 h4 Bxh4+ Rxh4 Qxh4+ g3 Qxg3+ Kd2 Qe3#",
-    "c3 Nc6 Qc2 Nf6 e3 d6 Bd3 Bg4 h3 Be6 Nf3 Qd7 g4 h5 g5 Nd5 g6 Bxh3 gxf7+ Kd8 Na3 Qg4 Be4 Qg2 Rg1 Nxe3 dxe3 Nb4 cxb4 Qxg1+ Nxg1 Bc8 Nb5 c6 Bxc6 bxc6 Qxc6 Rb8 Qc7#",
-    "e3 d5 Qe2 e5 Nc3 Bc5 Nf3 Bg4 h3 Bxf3 gxf3 Nf6 f4 e4 Bg2 O-O d3 exd3 cxd3 c6 e4 d4 Na4 Bb4+ Bd2 Bxd2+ Qxd2 b6 e5 Nd5 Rc1 Qd7 b4 Na6 Rb1 b5 Bxd5 cxd5 Nb2 Rae8 Rg1 Qxh3 Nd1 f6 a4 Nc7 axb5 Nxb5 Ra1 fxe5 Nb2 exf4+ Kd1 Nc3+ Kc2 f3 Rge1 Rc8 Rxa7 Ne4+ Kb3 Nxd2+ Ka4 Qh6 Re5 Qb6 Rae7 Ra8+ Ra7 Rxa7#",
     "e4 e5 Nf3 Nc6 Nc3 Bb4 Nd5 Nf6 Nxb4 Nxb4 c3 Nc6 Nxe5 Nxe5 d4 Ng6 Bg5 h6 Bxf6 Qxf6 e5 Qe6 Bd3 d6 Qe2 Nf4 Qe4 dxe5 Bb5+ c6 d5 Nxd5 Bc4 O-O O-O b5 Bb3 Bb7 Bc2 Nf4 Qh7#")
-
-  val raw = "d4 Nf6 Nf3 Nc6 Nbd2 e6 e4 d6 c4 Qe7 Bd3 e5 d5 Nd4 Nxd4 exd4 O-O Bg4 f3 Bd7 Nb3 Qe5 Be2 c5 dxc6 bxc6 Qxd4 Qxd4+ Nxd4 Rb8 b3 Be7 Be3 Bd8 Rfd1 Bb6 Kf2 Ke7 Rd2 Ba5 Rd3 Bb6 Rad1 Rhd8 g4 h6 Bf4 g5 Bg3 h5 h3 h4 Bh2 Rb7 e5 dxe5 Bxe5 Ne8 Kg2 Bc7 Bxc7 Rxc7 Nf5+ Kf8 f4 gxf4 Nxh4 Ng7 Bf3 Ne6 Nf5 Nc5 Rd4 Ne6 Rd6 c5 h4 Ng7 Nxg7 Kxg7 g5 a5 Kf2 Kf8 Bc6 Ke7 Ba4 Bxa4 Rxd8 Bc6 h5 Ke6 h6 Be4 Rh8 Re7 Re1 Kf5 h7 Kg6 Rc8 Kxh7 Rxc5 a4 b4 Kg6 b5 f6 gxf6 Kxf6 b6 a3 Rc7 Rxc7 bxc7 Bb7 Re8 Kf5 c5 Ba6 Ra8 Bb7 Rf8+ Ke5 c6 Ba6 Ra8 Kd6 Rxa6 Kxc7 Kf3 Kb8 Kxf4 Kc8 Ke5 Kc7 Ke6 Kd8 Kd6 Ke8 Ra7 Kf8 c7 Kf7 c8=Q+ Kg6 Qg4+ Kf6 Ra8 Kf7 Qf5+ Kg7 Ra7+ Kg8 Qc8#"
-  //val raw = "e4 e5 Bc4 Nf6 d3 d5 Bg5 Be6 exd5 Bxd5 Bxd5 Qxd5 f3 e4 Bxf6 gxf6 fxe4 Qa5+ c3 Nd7 b4 Qb6 a4 Ne5 Nf3 O-O-O a5 Qe6 Nd4 Qd7 Qh5 Bxb4 cxb4 Qxd4 Qf5+ Kb8 Kd2 Nc4+ Ke2 Qxd3+ Kf2 Qe3+ Kf1 Rd1#"
 
   val complete960 = """[Event "Casual game"]
 [Site "http://en.lichess.org/analyse/---qxr00"]
@@ -43,7 +29,7 @@ class PgnReaderTest extends ChessTest {
 
 1. e3 Nf6 2. Ng3 Ne6 3. Nf3 d5 4. Nd4 Nxd4 5. exd4 e6 6. Re1 Ng4 7. Re2 f6 8. c4 dxc4 9. Be4 Rxd4 10. Bf3 Ne5 11. Ne4 Nxf3 12. gxf3 Bf7 13. Nxf6 gxf6 14. Kd1 e5 15. h4 Bg6 16. Bh2 Bf5 17. Rxe5 fxe5 18. Bxe5 Qxe5 19. Qf1 Qf4 20. d3 Rxd3+ 21. Ke1 Qd2# 0-1"""
 
-  val fromWikipedia = """ [Event "F/S Return Match"]
+  val fromWikipedia = """[Event "F/S Return Match"]
 [Site "Belgrade, Serbia Yugoslavia|JUG"]
 [Date "1992.11.04"]
 [Round "29"]
@@ -60,18 +46,43 @@ hxg5 29. b3 Ke6 30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33. f3 Bc8 34. Kf2 Bf5
 35. Ra7 g6 36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5 40. Rd6 Kc5 41. Ra6
 Nf2 42. g4 Bd3 43. Re6 1/2-1/2"""
 
+val fromChessgames = """[Event "The Match - Braingames World Chess Cham"]
+[Site "London"]
+[Date "2000.01.04"]
+[EventDate "2000.10.12"]
+[Round "3"]
+[Result "1/2-1/2"]
+[White "Garry Kasparov"]
+[Black "Vladimir Kramnik"]
+[ECO "C67"]
+[WhiteElo "2849"]
+[BlackElo "2770"]
+[PlyCount "106"]
+
+1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 4. O-O Nxe4 5. d4 Nd6 6. Bxc6
+dxc6 7. dxe5 Nf5 8. Qxd8+ Kxd8 9. Nc3 Bd7 10. b3 h6 11. Bb2
+Kc8 12. Rad1 b6 13. Ne2 c5 14. c4 Bc6 15. Nf4 Kb7 16. Nd5 Ne7
+17. Rfe1 Rg8 18. Nf4 g5 19. Nh5 Rg6 20. Nf6 Bg7 21. Rd3 Bxf3
+22. Rxf3 Bxf6 23. exf6 Nc6 24. Rd3 Rf8 25. Re4 Kc8 26. f4 gxf4
+27. Rxf4 Re8 28. Bc3 Re2 29. Rf2 Re4 30. Rh3 a5 31. Rh5 a4
+32. bxa4 Rxc4 33. Bd2 Rxa4 34. Rxh6 Rg8 35. Rh7 Rxa2 36. Rxf7
+Ne5 37. Rg7 Rf8 38. h3 c4 39. Re7 Nd3 40. f7 Nxf2 41. Re8+ Kd7
+42. Rxf8 Ke7 43. Rc8 Kxf7 44. Rxc7+ Ke6 45. Be3 Nd1 46. Bxb6
+c3 47. h4 Ra6 48. Bd4 Ra4 49. Bxc3 Nxc3 50. Rxc3 Rxh4 51. Rf3
+Rh5 52. Kf2 Rg5 53. Rf8 Ke5 1/2-1/2"""
+
+val fromProd = """d4 Nf6 c4 e6 Nc3 Bb4 Bd2 O-O Nf3 c6 e4 Qa5 Be2 Kh8 O-O h5 Ne5 h4 h3 Be7 Nd5 Qd8 Nxe7 d6 Ne7g6+"""
+
   "only raw moves" should {
-    "one complete game" in {
-      PgnReader(raw) must beSuccess.like {
-        case replay ⇒ replay.moves must have size (raw.split(' ').size)
-      }
-    }
-    "all games" in {
+    "many games" in {
       forall(raws) { (c: String) ⇒
         PgnReader(c) must beSuccess.like {
           case replay ⇒ replay.moves must have size (c.split(' ').size)
         }
       }
+    }
+    "example from prod" in {
+      PgnReader(fromProd) must beSuccess
     }
   }
   "tags and moves" should {
@@ -80,6 +91,12 @@ Nf2 42. g4 Bd3 43. Re6 1/2-1/2"""
     }
     "example from wikipedia" in {
       PgnReader(fromWikipedia) must beSuccess
+    }
+    "example from chessgames.com" in {
+      PgnReader(fromChessgames) must beSuccess
+    }
+    "example from prod" in {
+      PgnReader(fromProd) must beSuccess
     }
   }
 }
