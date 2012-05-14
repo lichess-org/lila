69134c18b58ffd8567af9008b62991d7f464f634
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-05-14 21:00:48 +0200

    Restore single project

---
+++ app/controllers/Wiki.scala
@@ -0,0 +1,9 @@
+package controllers
+
+import lila._
+import views._
+
+object Wiki extends LilaController {
+
+  val home = TODO
+}
--- app/wiki/Controller.scala
+++
@@ -1,7 +0,0 @@
-package lila
-package wiki
-
-object Controller extends controllers.LilaController {
-
-  val home = TODO
-}
