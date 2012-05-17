e1b18f83fde9dbc629a7a717f69afd39a9896c17
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-05-17 18:33:51 +0200

    Implement date helper

---
+++ app/templating/DateHelper.scala
@@ -0,0 +1,25 @@
+package lila
+package templating
+
+import http.Context
+import i18n.I18nHelper
+
+import org.joda.time.DateTime
+import org.joda.time.format.{ DateTimeFormat, DateTimeFormatter }
+import java.util.Locale
+import scala.collection.mutable
+
+trait DateHelper { self: I18nHelper ⇒
+
+  private val style = "MS"
+
+  private val formatters = mutable.Map[String, DateTimeFormatter]()
+
+  private def formatter(ctx: Context): DateTimeFormatter =
+    formatters.getOrElseUpdate(
+      lang(ctx).language,
+      DateTimeFormat forStyle style withLocale new Locale(lang(ctx).language))
+
+  def showDate(date: DateTime)(implicit ctx: Context): String =
+    formatter(ctx) print date
+}
