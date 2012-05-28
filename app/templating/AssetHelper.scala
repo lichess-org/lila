edb417bac3981d3ad4d953ed7f1f759967b07197
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-05-28 17:42:20 +0200

    Implement asset versions

--- app/templating/AssetHelper.scala
+++ app/templating/AssetHelper.scala
@@ -7,13 +7,15 @@ import play.api.templates.Html
 
 trait AssetHelper {
 
+  val assetVersion = 1
+
   def cssTag(name: String) = css("stylesheets/" + name)
 
   def cssVendorTag(name: String) = css("vendor/" + name)
 
   private def css(path: String) = Html {
-    """<link href="%s" type="text/css" rel="stylesheet"/>"""
-    .format(routes.Assets.at(path))
+    """<link href="%s?v=%d" type="text/css" rel="stylesheet"/>"""
+    .format(routes.Assets.at(path), assetVersion)
   }
 
   def jsTag(name: String) = js("javascripts/" + name)
@@ -21,7 +23,7 @@ trait AssetHelper {
   def jsVendorTag(name: String) = js("vendor/" + name)
 
   def js(path: String) = Html {
-    """<script src="%s"></script>"""
-    .format(routes.Assets.at(path))
+    """<script src="%s?v=%d"></script>"""
+    .format(routes.Assets.at(path), assetVersion)
   }
 }
