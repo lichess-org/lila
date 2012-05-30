5582bffebd4d30c5481c2270c90ee09760fce111
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-05-31 00:08:29 +0200

    simplify the generic actor cache

--- app/memo/ActorMemo.scala
+++ app/memo/ActorMemo.scala
@@ -2,13 +2,10 @@ package lila
 package memo
 
 import akka.actor._
-import akka.pattern.ask
-import akka.util.duration._
-import akka.util.Timeout
 import akka.dispatch.{ Future, Await }
 import akka.pattern.ask
-import akka.util.duration._
 import akka.util.Duration
+import akka.util.duration._
 import akka.util.Timeout
 
 import play.api.Play.current
@@ -19,7 +16,6 @@ import com.google.common.cache.LoadingCache
 final class ActorMemo[K, V] private (
     cache: LoadingCache[K, V],
     atMost: Duration,
-    keyDef: ActorMemo.KeyDef[K],
     valMan: Manifest[V]) {
 
   def apply(key: K): V = Await.result(async(key), atMost)
@@ -31,7 +27,7 @@ final class ActorMemo[K, V] private (
 
   private val actor = Akka.system.actorOf(Props(new Actor {
     def receive = {
-      case keyDef(key) ⇒ sender ! (cache get key)
+      case key ⇒ sender ! (cache get key.asInstanceOf[K])
     }
   }))
 }
@@ -39,21 +35,8 @@ final class ActorMemo[K, V] private (
 object ActorMemo {
 
   def apply[K, V](load: K ⇒ V, ttl: Int, atMost: Duration)(
-      implicit keyMan: Manifest[K], valMan: Manifest[V]) = new ActorMemo(
+      implicit valMan: Manifest[V]) = new ActorMemo(
     cache = Builder.cache[K, V](ttl, load),
     atMost = atMost,
-    keyDef = new KeyDef[K],
     valMan = valMan)
-
-  final class KeyDef[C](implicit desired: Manifest[C]) {
-
-    def unapply[X](c: X)(implicit m: Manifest[X]): Option[C] = {
-
-      def sameArgs = desired.typeArguments.zip(m.typeArguments).forall {
-        case (desired, actual) ⇒ desired >:> actual
-      }
-
-      (desired >:> m && sameArgs) option c.asInstanceOf[C]
-    }
-  }
 }
