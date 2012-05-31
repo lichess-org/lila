cb291537ec9f02299e72d8e9ccf91fc1b0c529bc
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-06-01 01:44:34 +0200

    fix lichess links in chats

--- app/templating/AssetHelper.scala
+++ app/templating/AssetHelper.scala
@@ -7,7 +7,7 @@ import play.api.templates.Html
 
 trait AssetHelper {
 
-  val assetVersion = 6
+  val assetVersion = 7
 
   def cssTag(name: String) = css("stylesheets/" + name)
 
--- public/javascripts/ctrl.js
+++ public/javascripts/ctrl.js
@@ -249,6 +249,6 @@ if (lichess.onProduction) {
 }
 
 function urlToLink(text) {
-  var exp = /\bhttp:\/\/[a-z]{0,3}\.(lichess\.org[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+  var exp = /\bhttp:\/\/(?:[a-z]{0,3}\.)?(lichess\.org[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
   return text.replace(exp,"<a href='http://$1'>$1</a>");
 }
