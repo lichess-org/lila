aa8044f857f2c64e92ffc983e7c3f60cba4187d2
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-06-13 00:10:32 +0200

    limit pagination depth of user pages to avoid costly db queries

--- app/controllers/User.scala
+++ app/controllers/User.scala
@@ -25,18 +25,21 @@ object User extends LilaController {
   def show(username: String) = showFilter(username, "all", 1)
 
   def showFilter(username: String, filterName: String, page: Int) = Open { implicit ctx ⇒
-    IOptionIOk(userRepo byId username) { u ⇒
-      u.enabled.fold({
-        val userSpy = isGranted(_.UserSpy) option securityStore.userSpy _
-        env.user.userInfo(u, bookmarkApi, userSpy, ctx) map { info ⇒
-          val filters = user.GameFilterMenu(info, ctx.me, filterName)
-          val paginator = filters.query.fold(
-            query ⇒ gamePaginator.recentlyCreated(query)(page),
-            bookmarkApi.gamePaginatorByUser(u, page))
-          html.user.show(u, info, paginator, filters)
-        }
-      }, io(html.user.disabled(u)))
-    }
+    (page < 50).fold(
+      IOptionIOk(userRepo byId username) { u ⇒
+        u.enabled.fold({
+          val userSpy = isGranted(_.UserSpy) option securityStore.userSpy _
+          env.user.userInfo(u, bookmarkApi, userSpy, ctx) map { info ⇒
+            val filters = user.GameFilterMenu(info, ctx.me, filterName)
+            val paginator = filters.query.fold(
+              query ⇒ gamePaginator.recentlyCreated(query)(page),
+              bookmarkApi.gamePaginatorByUser(u, page))
+            html.user.show(u, info, paginator, filters)
+          }
+        }, io(html.user.disabled(u)))
+      },
+      BadRequest("too old")
+    )
   }
 
   def list(page: Int) = Open { implicit ctx ⇒
