ffc7d59ed56c5b89626c9e8a8f4d8bbe2ec1fb53
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-06-14 00:57:12 +0200

    dramatically improve load time of pages containing google charts

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,13 +1,82 @@
-if (typeof google.visualization == "undefined" ) {
-  google.load("visualization", "1", {packages:["corechart"]});
-}
+function drawCharts() {
 
-google.elemToData = function(elem) {
-  var data = new google.visualization.DataTable();
-  $.each($(elem).data('columns'), function() {
-    data.addColumn(this[0], this[1]);
+  function elemToData(elem) {
+    var data = new google.visualization.DataTable();
+    $.each($(elem).data('columns'), function() {
+      data.addColumn(this[0], this[1]);
+    });
+    data.addRows($(elem).data('rows'));
+
+    return data;
+  }
+
+  $('div.elo_history').each(function() {
+    var data = elemToData(this);
+    var chart = new google.visualization.ComboChart(this);
+    chart.draw(data, {
+      series: [
+    { color: "blue", type: "area", lineWidth: 2 },
+      { color: "red", type: "line", lineWidth: 2 }
+    ],
+      width: 460,
+      height: 340,
+      axisTitlePosition: 'none',
+      chartArea:{left:"10%",top:"2%",width:"90%",height:"96%"},
+      titlePosition: 'none',
+      hAxis: {textPosition: "none"}
+    });
   });
-  data.addRows($(elem).data('rows'));
 
-  return data;
+  $('div.win_stats').each(function() {
+    var data = elemToData(this);
+    var chart = new google.visualization.PieChart(this);
+    chart.draw(data, {
+      width: 312,
+      height: 200,
+      titlePosition: 'none',
+      chartArea:{left:"0%",width:"100%",height:"100%"},
+      is3D: true,
+    });
+  });
+
+  $('div.elo_distribution').each(function() {
+    var data = elemToData(this);
+    var chart = new google.visualization.ScatterChart(this);
+    chart.draw(data, {
+      width: 747,
+      height: 500,
+      //axisTitlePosition: 'none',
+      chartArea:{left:"5%",top:"3%",width:"78%",height:"92%"},
+      title: $(this).attr('title'),
+      titlePosition: 'in',
+      pointSize: 3,
+    });
+  });
+
+  $('div.end_distribution').each(function() {
+    var data = elemToData(this);
+    var chart = new google.visualization.PieChart(this);
+    chart.draw(data, {
+      width: 747,
+      height: 500,
+      chartArea:{left:"0%",top:"5%",width:"100%",height:"95%"},
+      is3D: true,
+      title: $(this).attr('title'),
+    });
+  });
+
+  $('div.move-time').each(function() {
+    var data = elemToData(this);
+    var chart = new google.visualization.AreaChart(this);
+    chart.draw(data, {
+      width: 747,
+      height: 400,
+      title: $(this).data('title'),
+      chartArea:{left:"5%",top:"5%",width:"78%",height:"90%"}
+    });
+  });
 }
+
+$(function() {
+  $.getScript('https://www.google.com/jsapi?autoload={"modules":[{"name":"visualization","version":"1","packages":["corechart"],"callback":"drawCharts"}]}');
+});
