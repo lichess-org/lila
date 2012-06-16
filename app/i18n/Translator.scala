775db740ef3828839c685128bcaed512f651c586
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-06-17 00:31:17 +0200

    refactor translator for greater speed on default language

--- app/i18n/Translator.scala
+++ app/i18n/Translator.scala
@@ -1,8 +1,6 @@
 package lila
 package i18n
 
-import core.Global.env // OMG
-
 import play.api.i18n.{ MessagesApi, Lang }
 import play.api.mvc.RequestHeader
 import play.api.templates.Html
@@ -10,6 +8,7 @@ import play.api.templates.Html
 final class Translator(api: MessagesApi, pool: I18nPool) {
 
   private val messages = api.messages
+  private val defaultMessages = messages.get("default") err "No default messages"
 
   def html(key: String, args: List[Any])(implicit req: RequestHeader): Html =
     Html(str(key, args)(req))
@@ -21,19 +20,27 @@ final class Translator(api: MessagesApi, pool: I18nPool) {
     translate(key, args)(lang) getOrElse key
 
   def rawTranslation(lang: Lang)(key: String): Option[String] =
-    messages.get(lang.code).flatMap(_.get(key))
+    messages get lang.code flatMap (_ get key)
 
-  private def translate(key: String, args: List[Any])(lang: Lang): Option[String] = {
-    messages.get(lang.code).flatMap(_.get(key)).orElse(messages.get("default").flatMap(_.get(key))).map { pattern ⇒
-      try {
-        if (args.isEmpty) pattern else pattern.format(args: _*)
-      }
-      catch {
-        case e ⇒ {
-          println("Failed to translate " + key + " to " + lang.language + " - " + e.getMessage)
-          key
-        }
-      }
+  def defaultTranslation(key: String, args: List[Any]): Option[String] =
+    defaultMessages get key flatMap { pattern ⇒
+      formatTranslation(key, pattern, args) 
+    }
+
+  private def translate(key: String, args: List[Any])(lang: Lang): Option[String] =
+    if (lang == pool.default) defaultTranslation(key, args)
+    else messages get lang.code flatMap (_ get key) flatMap { pattern ⇒
+      formatTranslation(key, pattern, args) 
+    } orElse defaultTranslation(key, args)
+
+  def formatTranslation(key: String, pattern: String, args: List[Any]) = try {
+    Some(if (args.isEmpty) pattern else pattern.format(args: _*))
+  }
+  catch {
+    case e ⇒ {
+      println("Failed to translate %s -> %s (%s) - %s".format(
+        key, pattern, args, e.getMessage))
+      None
     }
   }
 }
