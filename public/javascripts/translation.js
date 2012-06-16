2dc0a9e1d355d9442a2e83dc05716ef7cb21aec4
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-06-16 19:10:28 +0200

    tweak translation UI

--- public/javascripts/translation.js
+++ public/javascripts/translation.js
@@ -1,19 +1,21 @@
 $(function() {
-	$('div.messages div.message').each(function() {
-		if (!$(this).find('input').val()) {
-			$(this).addClass('missing');
-		}
-	});
-	$('div.locale_menu a').click(function() {
-		$(this).parent().find('a').removeClass('active');
-		$(this).addClass('active');
-		$('div.messages div.message').show();
-		if ($(this).hasClass('missing')) {
-			$('div.messages div.message').not('.missing').hide();
-		}
-	});
-  if ($('div.messages div.missing').length > 0) {
-    $('div.locale_menu a.missing').click();
+  if ($('div.locale_menu').length > 0) {
+    $('div.messages div.message').each(function() {
+      if (!$(this).find('input').val()) {
+        $(this).addClass('missing');
+      }
+    });
+    $('div.locale_menu a').click(function() {
+      $(this).parent().find('a').removeClass('active');
+      $(this).addClass('active');
+      $('div.messages div.message').show();
+      if ($(this).hasClass('missing')) {
+        $('div.messages div.message').not('.missing').hide();
+      }
+    });
+    if ($('div.messages div.missing').length > 0) {
+      $('div.locale_menu a.missing').click();
+    }
   }
 });
 
