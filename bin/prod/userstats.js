886d6709855e137a0b648bf3056b25620f955bd3
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-08-05 22:08:14 +0200

    move userstats.js to bin/prod and improve it a bit

--- bin/prod/userstats.js
+++ bin/prod/userstats.js
@@ -5,6 +5,5 @@ db.user2.find({}).forEach(function(user) {
     nbLosses: db.game2.count({"userIds": uid, "status": { "$in": [ 30, 31, 35, 33 ] }, "winId": {"$ne": uid}, "players.aiLevel":{$exists:false}}),
     nbDraws: db.game2.count({"userIds": uid, "status": { "$in": [34, 32] }, "players.aiLevel":{$exists:false}})
   };
-  print(uid + " " + data.nbWins + " " + data.nbLosses + " " + data.nbDraws);
   db.user2.update({"_id": uid}, {"$set":data});
 });
