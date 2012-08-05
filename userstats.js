602f0b9ef893d35f5f53c4ea6ca63e3ab71af0b5
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-08-05 22:03:56 +0200

    Revert "remove userstats mongodb denormalization script"
    This reverts commit 27df1b90508e00a37b19d46caec87436a3e6ce50.

---
+++ userstats.js
@@ -0,0 +1,10 @@
+db.user2.find({}).forEach(function(user) {
+  var uid = user['_id'];
+  var data = {
+    nbWins: db.game2.count({"winId": uid, "players.aiLevel":{$exists:false}}),
+    nbLosses: db.game2.count({"userIds": uid, "status": { "$in": [ 30, 31, 35, 33 ] }, "winId": {"$ne": uid}, "players.aiLevel":{$exists:false}}),
+    nbDraws: db.game2.count({"userIds": uid, "status": { "$in": [34, 32] }, "players.aiLevel":{$exists:false}})
+  };
+  print(uid + " " + data.nbWins + " " + data.nbLosses + " " + data.nbDraws);
+  db.user2.update({"_id": uid}, {"$set":data});
+});
