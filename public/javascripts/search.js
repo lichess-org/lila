709ed890808a7e6e8b69dec0bf71e85544c5ac4b
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-09-07 14:36:43 +0200

    realtime search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,25 +1,37 @@
 $(function() {
 
-  $('.search_infinitescroll:has(.pager a)').each(function() {
-    var $next = $(this).find(".pager a:last")
-    $next.attr("href", $next.attr("href") + "&" + $("form.search").serialize());
-    $(this).infinitescroll({
-      navSelector: ".pager",
-      nextSelector: $next,
-      itemSelector: ".search_infinitescroll .paginated_element",
-      loading: {
-        msgText: "",
-      img: "/assets/images/hloader3.gif",
-      finishedMsg: "---"
-      }
-    }, function() {
-      $("#infscr-loading").remove();
-      $('body').trigger('lichess.content_loaded');
-    });
-  });
-
   var $form = $("form.search");
+  var $result = $(".search_result");
+
   $form.find(".opponent select").change(function() {
     $form.find(".aiLevel").toggle($(this).val() == 1);
   }).trigger("change");
+
+  function realtimeResults() {
+    $result.load(
+      $form.attr("action") + "?" + $form.serialize() + " .search_result",
+      function() {
+        $('body').trigger('lichess.content_loaded');
+        $result.find('.search_infinitescroll:has(.pager a)').each(function() {
+          var $next = $(this).find(".pager a:last")
+          $next.attr("href", $next.attr("href") + "&" + $form.serialize());
+          $(this).infinitescroll({
+            navSelector: ".pager",
+            nextSelector: $next,
+            itemSelector: ".search_infinitescroll .paginated_element",
+            loading: {
+              msgText: "",
+            img: "/assets/images/hloader3.gif",
+            finishedMsg: "---"
+            }
+          }, function() {
+            $("#infscr-loading").remove();
+            $('body').trigger('lichess.content_loaded');
+          });
+        });
+      });
+  }
+
+  $form.find("select").change(realtimeResults);
+  $form.find("input").change(realtimeResults);
 });
