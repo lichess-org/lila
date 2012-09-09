8f24bd1453d477f8fec73a7d045cc70c3d162349
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-09-09 23:35:47 +0200

    tournament wip

--- app/tournament/TournamentHelper.scala
+++ app/tournament/TournamentHelper.scala
@@ -2,19 +2,25 @@ package lila
 package tournament
 
 import http.Context
+import user.User
 
 import com.codahale.jerkson.Json
 import scala.math.{ min, max, round }
 
 trait TournamentHelper { 
 
-  def tournamentJsData(tour: Tournament, version: Int) = Json generate {
+  def tournamentJsData(
+    tour: Tournament, 
+    version: Int,
+    user: Option[User]) = Json generate {
 
     Map(
       "tournament" -> Map(
         "id" -> tour.id
       ),
       "version" -> version
-    )
+    ).combine(user) { (map, u) => 
+      map + ("username" -> u.username)
+    }
   }
 }
