d3c72cab99664fe16634a6141f3a3120f674dcd9
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-10-12 01:39:30 +0200

    rewrite a few queries

--- app/game/GameRepo.scala
+++ app/game/GameRepo.scala
@@ -102,7 +102,7 @@ final class GameRepo(collection: MongoCollection)
   }
 
   def findRandomStandardCheckmate(distribution: Int): IO[Option[DbGame]] = io {
-    find(Query.mate + ("v" -> Variant.Standard.id))
+    find(Query.mate ++ ("v" -> Variant.Standard.id))
       .sort(Query.sortCreated)
       .limit(1)
       .skip(Random nextInt distribution)
