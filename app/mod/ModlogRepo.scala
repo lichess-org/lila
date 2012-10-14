b663dbf336a2da9113daa87f819197a85c9d5fcb
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-10-14 15:03:49 +0200

    remove some unused imports

--- app/mod/ModlogRepo.scala
+++ app/mod/ModlogRepo.scala
@@ -5,7 +5,7 @@ import user.User
 
 import com.novus.salat._
 import com.novus.salat.dao._
-import com.mongodb.casbah.{ MongoCollection, WriteConcern }
+import com.mongodb.casbah.MongoCollection
 import com.mongodb.casbah.Imports._
 import scalaz.effects._
 
