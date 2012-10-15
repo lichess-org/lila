80b5dbc89eae7c264ecacce0bc901704317ab667
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-10-15 13:53:01 +0200

    fix analytics

--- public/javascripts/big.js
+++ public/javascripts/big.js
@@ -488,21 +488,6 @@ $.displayBoardMarks = function($board, isWhite) {
   $board.append(marks);
 };
 
-if (lichess.onProduction) {
-  //analytics
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-7935029-3']);
-  _gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'http://www.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-  })();
-}
-
 function urlToLink(text) {
   var exp = /\bhttp:\/\/(?:[a-z]{0,3}\.)?(lichess\.org[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
   return text.replace(exp,"<a href='http://$1'>$1</a>");
@@ -1840,3 +1825,18 @@ $(function() {
 });
 
 })();
+
+if (/.+\.lichess\.org/.test(document.domain)) {
+  //analytics
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-7935029-3']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'http://www.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+  })();
+}
