79bf9de382b6e8f52451906e949e89ca89787776
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-10-20 16:02:17 +0200

    super admin is never a thieve

--- app/controllers/TheftPrevention.scala
+++ app/controllers/TheftPrevention.scala
@@ -4,6 +4,7 @@ import lila._
 import views._
 import http.Context
 import game.{ DbGame, Pov }
+import security.{ Permission, Granter }
 
 import play.api.mvc._
 import play.api.mvc.Results.Redirect
@@ -14,5 +15,5 @@ trait TheftPrevention {
     isTheft(pov).fold(Redirect(routes.Round.watcher(pov.gameId, pov.color.name)), ok)
 
   protected def isTheft(pov: Pov)(implicit ctx: Context) =
-    pov.player.userId != ctx.userId 
+    pov.player.userId != ctx.userId && !Granter.option(Permission.SuperAdmin)(ctx.me)
 }
