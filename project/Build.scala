5bd8762b9f2e1470264c18f20f581f11466772c0
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-11-28 18:59:19 +0100

    start to upgrade to play 2.1-RC1

--- project/Build.scala
+++ project/Build.scala
@@ -1,9 +1,9 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 trait Resolvers {
-  val codahale = "repo.codahale.com" at "http://repo.codahale.com/"
+  // val codahale = "repo.codahale.com" at "http://repo.codahale.com/"
   val typesafe = "typesafe.com" at "http://repo.typesafe.com/typesafe/releases/"
   val iliaz = "iliaz.com" at "http://scala.iliaz.com/"
   val sonatype = "sonatype" at "http://oss.sonatype.org/content/repositories/releases"
@@ -15,17 +15,17 @@ trait Resolvers {
 }
 
 trait Dependencies {
-  val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.4"
-  val salat = "com.novus" %% "salat-core" % "1.9.1"
-  val scalalib = "com.github.ornicar" %% "scalalib" % "2.5"
-  val config = "com.typesafe" % "config" % "0.4.1"
+  val scalaz = "org.scalaz" % "scalaz-core_2.9.2" % "6.0.4"
+  val salat = "com.novus" % "salat-core_2.9.2" % "1.9.1"
+  val scalalib = "com.github.ornicar" % "scalalib_2.9.1" % "2.5"
+  val config = "com.typesafe" % "config" % "1.0.0"
   val guava = "com.google.guava" % "guava" % "13.0"
   val apache = "org.apache.commons" % "commons-lang3" % "3.1"
-  val scalaTime = "org.scala-tools.time" %% "time" % "0.5"
+  val scalaTime = "org.scala-tools.time" % "time_2.9.1" % "0.5"
   val slf4jNop = "org.slf4j" % "slf4j-nop" % "1.6.4"
-  val paginator = "com.github.ornicar" %% "paginator-core" % "1.6"
-  val paginatorSalat = "com.github.ornicar" %% "paginator-salat-adapter" % "1.5"
-  val csv = "com.github.tototoshi" %% "scala-csv" % "0.3"
+  val paginator = "com.github.ornicar" % "paginator-core_2.9.1" % "1.6"
+  val paginatorSalat = "com.github.ornicar" % "paginator-salat-adapter_2.9.1" % "1.5"
+  val csv = "com.github.tototoshi" % "scala-csv_2.9.1" % "0.3"
   val hasher = "com.roundeights" % "hasher" % "0.3" from "http://cloud.github.com/downloads/Nycto/Hasher/hasher_2.9.1-0.3.jar"
   val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % "1.3.0.201202151440-r"
   val actuarius = "eu.henkelmann" % "actuarius_2.9.2" % "0.2.4"
@@ -36,31 +36,31 @@ trait Dependencies {
 
 object ApplicationBuild extends Build with Resolvers with Dependencies {
 
-  private val buildSettings = Project.defaultSettings ++ Seq(
-    organization := "com.github.ornicar",
-    version := "1.2",
-    scalaVersion := "2.9.1",
-    resolvers := Seq(iliaz, codahale, sonatype, sonatypeS, typesafe, t2v, guice, jgitMaven, christophs),
-    libraryDependencies := Seq(scalaz, scalalib, hasher),
+  // private val buildSettings = Project.defaultSettings ++ Seq(
+  private val buildSettings = Seq(
+    // scalaVersion := "2.10.0-RC1",
+    // resolvers := Seq(iliaz, sonatype, sonatypeS, typesafe, t2v, guice, jgitMaven, christophs),
     shellPrompt := {
       (state: State) ⇒ "%s> ".format(Project.extract(state).currentProject.id)
     },
     scalacOptions := Seq("-deprecation", "-unchecked")
   )
 
-  lazy val lila = PlayProject("lila", mainLang = SCALA, settings = buildSettings).settings(
-    libraryDependencies ++= Seq(
-      config,
-      salat,
-      guava,
-      apache,
-      scalaTime,
-      paginator,
-      paginatorSalat,
-      csv,
-      jgit,
-      actuarius,
-      scalastic),
+  lazy val lila = play.Project("lila", "3", Seq(
+    scalaz, 
+    scalalib, 
+    hasher,
+    config,
+    salat,
+    guava,
+    apache,
+    scalaTime,
+    paginator,
+    paginatorSalat,
+    csv,
+    jgit,
+    actuarius,
+    scalastic)).settings(
     templatesImport ++= Seq(
       "lila.game.{ DbGame, DbPlayer, Pov }",
       "lila.user.User",
@@ -68,15 +68,16 @@ object ApplicationBuild extends Build with Resolvers with Dependencies {
       "lila.templating.Environment._",
       "lila.ui",
       "lila.http.Context",
-      "com.github.ornicar.paginator.Paginator")
-  ) dependsOn scalachess aggregate scalachess
+      "com.github.ornicar.paginator.Paginator"),
+    resolvers ++= Seq(iliaz, sonatype, sonatypeS, typesafe, t2v, guice, jgitMaven, christophs)
+  ) //dependsOn scalachess aggregate scalachess
 
-  lazy val scalachess = Project("scalachess", file("scalachess")).settings(
-    resolvers := Seq(iliaz),
-    libraryDependencies := Seq(scalaz, scalalib, hasher, jodaTime, jodaConvert)
-  )
+  // lazy val scalachess = Project("scalachess", file("scalachess")).settings(
+  //   resolvers := Seq(iliaz),
+  //   libraryDependencies := Seq(scalaz, scalalib, hasher, jodaTime, jodaConvert)
+  // )
 
-  lazy val cli = Project("cli", file("cli"), settings = buildSettings).settings(
-    libraryDependencies ++= Seq(scalastic)
-  ) dependsOn lila
+  // lazy val cli = Project("cli", file("cli"), settings = buildSettings).settings(
+  //   libraryDependencies ++= Seq(scalastic)
+  // ) dependsOn lila
 }
