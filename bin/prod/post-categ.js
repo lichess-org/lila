05aabfd2c13ed150350020334e61455501d7cf6b
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-12-13 02:14:38 +0100

    add mongodb script to denormalize forum post categ id

---
+++ bin/prod/post-categ.js
@@ -0,0 +1,16 @@
+print("Migrating forum posts");
+
+db.f_post.dropIndex({categId: 1});
+
+db.f_categ.find().forEach(function(categ) {
+  db.f_topic.find({categId: categ._id}).forEach(function(topic) {
+    db.f_post.find({topicId: topic._id}).forEach(function(post) {
+      db.f_post.update({_id: post._id},{'$set':{categId: categ._id}}, false, false);
+    });
+  });
+});
+
+print("Building indexes");
+db.f_post.ensureIndex({categId: 1});
+
+print("Done!");
