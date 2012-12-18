ee6af18a8a984cc6185e838fbe71a1d142e1bdc0
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-12-18 17:02:08 +0100

    don't send team join request messages

--- app/team/TeamMessenger.scala
+++ app/team/TeamMessenger.scala
@@ -10,18 +10,6 @@ final class TeamMessenger(
     send: LichessThread ⇒ IO[Unit],
     netBaseUrl: String) {
 
-  def joinRequest(team: Team, request: RequestWithUser) = send(LichessThread(
-    to = team.createdBy,
-    subject = """%s wants to join your team %s""".format(request.user.username, team.name),
-    message = """%s said:
----
-%s
----
-
-To accept or decline this join request, go to your team page %s
-""".format(request.user.username, request.message, teamUrl(team))
-  ))
-
   def acceptRequest(team: Team, request: Request) = send(LichessThread(
     to = request.user,
     subject = """You have joined the team %s""".format(team.name),
@@ -29,8 +17,6 @@ To accept or decline this join request, go to your team page %s
 
 Here is the team page: %s""".format(teamUrl(team))
   ))
-
-  def declineRequest(team: Team, request: Request) = io()
 
   private def teamUrl(team: Team) = netBaseUrl + routes.Team.show(team.id)
 }
