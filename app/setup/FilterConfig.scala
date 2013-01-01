e0e4b33414dbdd0ab8c7c0801d0e18490759e94e
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-01-01 23:18:22 +0100

    Add elo diff to lobby filters WIP

--- app/setup/FilterConfig.scala
+++ app/setup/FilterConfig.scala
@@ -6,26 +6,30 @@ import chess.{ Variant, Mode, Speed }
 case class FilterConfig(
     variant: Option[Variant],
     mode: Option[Mode],
-    speed: Option[Speed]) {
+    speed: Option[Speed],
+    eloDiff: Int) {
 
   def withModeCasual = copy(mode = Mode.Casual.some)
 
   def encode = RawFilterConfig(
     v = ~variant.map(_.id),
     m = mode.map(_.id) | -1,
-    s = ~speed.map(_.id)
+    s = ~speed.map(_.id),
+    e = eloDiff
   )
 
   def >> = (
     variant map (_.id),
     mode map (_.id),
-    speed map (_.id)
+    speed map (_.id),
+    eloDiff
   ).some
 
   def render = Map(
     "variant" -> variant.map(_.toString),
     "mode" -> mode.map(_.toString),
-    "speed" -> speed.map(_.id)
+    "speed" -> speed.map(_.id),
+    "eloDiff" -> eloDiff
   )
 }
 
@@ -34,16 +38,19 @@ object FilterConfig {
   val default = FilterConfig(
     variant = none,
     mode = none,
-    speed = none)
+    speed = none,
+    eloDiff = 0)
 
   val variants = 0 :: Config.variants
   val modes = -1 :: Mode.all.map(_.id)
   val speeds = 0 :: Config.speeds
+  val eloDiffs = 0 :: 100 :: 200 :: 300 :: 500 :: Nil
 
-  def <<(v: Option[Int], m: Option[Int], s: Option[Int]) = new FilterConfig(
+  def <<(v: Option[Int], m: Option[Int], s: Option[Int], e: Int) = new FilterConfig(
     variant = v flatMap Variant.apply,
     mode = m flatMap Mode.apply,
-    speed = s flatMap Speed.apply
+    speed = s flatMap Speed.apply,
+    eloDiff = e
   )
 
   import com.mongodb.casbah.Imports._
@@ -53,15 +60,17 @@ object FilterConfig {
     variant ← filter.getAs[Int]("v")
     mode ← filter.getAs[Int]("m")
     speed ← filter.getAs[Int]("s")
-    config ← RawFilterConfig(variant, mode, speed).decode
+    eloDiff = ~filter.getAs[Int]("e")
+    config ← RawFilterConfig(variant, mode, speed, eloDiff).decode
   } yield config
 }
 
-private[setup] case class RawFilterConfig(v: Int, m: Int, s: Int) {
+private[setup] case class RawFilterConfig(v: Int, m: Int, s: Int, e: Int) {
 
   def decode = FilterConfig(
     variant = Variant(v),
     mode = Mode(m),
-    speed = Speed(s)
+    speed = Speed(s),
+    eloDiff = e
   ).some
 }
