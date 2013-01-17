52330d1ffd2b6f15e47b064a48f0f118ac151507
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-01-17 21:38:00 +0100

    Synchronize elasticsearch operations using actors

--- app/team/SearchMapping.scala
+++ app/team/SearchMapping.scala
@@ -10,30 +10,23 @@ private[team] object SearchMapping {
     val description = "de"
     val location = "lo"
     val nbMembers = "nbm"
-    val date = "da"
   }
   import fields._
+  import Mapping.field
 
-  def mapping = {
-    import Mapping._
-    Map(
-      "properties" -> List(
-        field(name, "string"),
-        field(description, "string"),
-        field(location, "string"),
-        field(nbMembers, "short"),
-        field(date, "date", attrs = Map("format" -> Date.format))
-      ).toMap
-    )
-  }
+  def mapping = Map(
+    "properties" -> List(
+      field(name, "string"),
+      field(description, "string"),
+      field(location, "string"),
+      field(nbMembers, "short")
+    ).toMap
+  )
 
-  def apply(team: Team): Pair[String, Map[String, Any]] = team.id -> (List(
-    name -> team.name.some,
-    description -> team.description.some,
-    location -> team.location,
-    nbMembers -> team.nbMembers.some,
-    date -> (Date.formatter print team.createdAt).some
-  ) collect {
-      case (x, Some(y)) ⇒ x -> y
-    }).toMap
+  def apply(team: Team): Pair[String, Map[String, Any]] = team.id -> Map(
+    name -> team.name,
+    description -> team.description,
+    location -> ~team.location,
+    nbMembers -> team.nbMembers
+  )
 }
