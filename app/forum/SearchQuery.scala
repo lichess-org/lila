c89dd71fd6aaa549f9b6e6b42227c2f57f8855b5
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-01-22 19:24:19 +0100

    use multimatch query for forum search

--- app/forum/SearchQuery.scala
+++ app/forum/SearchQuery.scala
@@ -16,12 +16,14 @@ private[forum] final class SearchQuery private(text: String, staff: Boolean) {
 
   def countRequest = ElasticSearch.Request.Count(makeQuery)
 
-  private def makeQuery = {
+  private def makeQueryFuzzy = {
     boolQuery
     .should(fuzzyQuery(fields.body, text))
     .should(fuzzyQuery(fields.topic, text))
     .should(fuzzyQuery(fields.author, text))
   }
+
+  private def makeQuery = multiMatchQuery(text, fields.body, fields.topic, fields.author)
 
   private def makeFilters = !staff option termFilter(fields.staff, false)
 
