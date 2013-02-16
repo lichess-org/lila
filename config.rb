05e0b1c4585fbd3454136af8b358e9e8912f1c6f
Sergey Monin <moninsergei@gmail.com>
2013-02-17 00:34:16 +0400

    todo updated, fonts moved to stylesheets/fonts folder, common.scss and analyse.scss updated, sprite s16.png sliced into icons

--- config.rb
+++ config.rb
@@ -5,13 +5,14 @@ http_path = "/"
 css_dir = "public/stylesheets"
 sass_dir = "public/sass"
 images_dir = "public/images"
+fonts_dir = css_dir + "/fonts"
 javascripts_dir = "public/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+#relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
