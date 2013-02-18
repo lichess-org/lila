12f2c632d91f038abf0866460171378a863e674f
Sergey Monin <moninsergei@gmail.com>
2013-02-18 05:29:21 +0400

    update of user-show.scss, todo updated

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "public/stylesheets"
-sass_dir = "public/sass"
-images_dir = "public/images"
+css_dir = "assets/stylesheets"
+sass_dir = "assets/sass"
+images_dir = "assets/images"
 fonts_dir = css_dir + "/fonts"
-javascripts_dir = "public/javascripts"
+javascripts_dir = "assets/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
