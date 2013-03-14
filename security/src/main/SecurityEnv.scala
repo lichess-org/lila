482c66ca8a5f43fb50bb635ad324e39e8a719af1
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-03-15 00:51:39 +0100

    new security settings and environment

--- security/src/main/SecurityEnv.scala
+++ security/src/main/SecurityEnv.scala
@@ -15,17 +15,21 @@ final class SecurityEnv(
   val settings = new Settings(config)
   import settings._
 
-  // lazy val store = new security.Store(
-  //   collection = mongodb(SecurityCollectionSecurity))
+  lazy val api = new Api(
+    store = store,
+    userRepo = userRepo)
 
-  // lazy val firewall = new security.Firewall(
-  //   collection = mongodb(FirewallCollectionFirewall),
-  //   cookieName = FirewallCookieName.some filter (_ ⇒ FirewallCookieEnabled),
-  //   enabled = FirewallEnabled)
+  private lazy val store = new Store(
+    coll = db(CollectionSecurity))
 
-  // lazy val flood = new security.Flood
+  lazy val firewall = new Firewall(
+    coll = db(FirewallCollectionFirewall),
+    cookieName = FirewallCookieName.some filter (_ ⇒ FirewallCookieEnabled),
+    enabled = FirewallEnabled)
 
-  // lazy val wiretap = new security.Wiretap(SecurityWiretapIps.toSet)
+  lazy val flood = new Flood
+
+  lazy val wiretap = new Wiretap(WiretapIps)
 
   // lazy val forms = new DataForm(
   //   userRepo = userRepo,
