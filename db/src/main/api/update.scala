5d8bdd48e703e8244783afa56a622481dc63308d
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-03-28 12:39:27 -0300

    typeclass based dependency injection

--- db/src/main/api/update.scala
+++ db/src/main/api/update.scala
@@ -10,19 +10,22 @@ import play.modules.reactivemongo.Implicits._
 object update extends update
 trait update {
 
-  def apply[ID: Writes, A <: Identified[ID]](doc: A)(implicit coll: Coll, tube: Tube[A]): Funit =
-    (tube toMongo doc).fold(fuck(_), js ⇒ apply(select(doc.id), js))
+  def apply[ID: Writes, A <: Identified[ID] : TubeInColl](doc: A): Funit =
+    (implicitly[Tube[A]] toMongo doc).fold(
+      fuck(_), 
+      js ⇒ apply(select(doc.id), js)
+    )
 
-  def apply[A](selector: JsObject, update: JsObject, upsert: Boolean = false, multi: Boolean = false)(implicit coll: Coll): Funit = for {
-    lastErr ← coll.update(selector, update, upsert = upsert, multi = multi)
+  def apply(selector: JsObject, update: JsObject, upsert: Boolean = false, multi: Boolean = false)(implicit inColl: InColl[_]): Funit = for {
+    lastErr ← inColl.coll.update(selector, update, upsert = upsert, multi = multi)
     result ← lastErr.ok.fold(funit, fuck(lastErr.message))
   } yield result
 
-  def doc[ID: Writes, A <: Identified[ID]: Tube](id: ID)(op: A ⇒ JsObject)(implicit coll: Coll): Funit =
+  def doc[ID: Writes, A <: Identified[ID]: TubeInColl](id: ID)(op: A ⇒ JsObject): Funit =
     find byId id flatMap { docOption ⇒
       docOption zmap (doc ⇒ update(select(id), op(doc)))
     }
 
-  def field[ID: Writes, A: Writes](id: ID, field: String, value: A)(implicit coll: Coll): Funit =
+  def field[ID: Writes, A: Writes](id: ID, field: String, value: A)(implicit inColl: InColl[_]): Funit =
     update(select(id), $set(field -> value))
 }
