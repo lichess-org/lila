2cc897d2d2ee4082027dc07c3ee90a045dcbd9cf
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-03-28 18:30:52 -0300

    apply db api changes to gameSearch repo

--- gameSearch/src/main/Env.scala
+++ gameSearch/src/main/Env.scala
@@ -2,6 +2,7 @@ package lila.gameSearch
 
 import lila.search.TypeIndexer
 import lila.game.{ GameRepo, PgnRepo, Game ⇒ GameModel, Query ⇒ DbQuery }
+import lila.db.TubeInColl
 
 import com.typesafe.config.Config
 import akka.actor._
@@ -13,12 +14,11 @@ final class Env(
   config: Config,
   system: ActorSystem,
   esIndexer: EsIndexer,
-  gameRepo: GameRepo,
-  pgnRepo: PgnRepo) {
+  implicit val gameTube: TubeInColl[GameModel]) {
 
-  val IndexName = config getString "index"
-  val TypeName = config getString "type"
-  val PaginatorMaxPerPage = config getInt "paginator.max_per_page"
+  private val IndexName = config getString "index"
+  private val TypeName = config getString "type"
+  private val PaginatorMaxPerPage = config getInt "paginator.max_per_page"
 
   val indexer: ActorRef = system.actorOf(Props(new TypeIndexer(
     es = esIndexer,
@@ -36,7 +36,7 @@ final class Env(
   lazy val forms = new DataForm
 
   private def responseToGames(response: SearchResponse): Fu[List[GameModel]] = 
-    gameRepo.find byIds {
+    lila.db.api.$find.byOrderedIds[String, GameModel] {
       response.hits.hits.toList map (_.id)
     }
 
@@ -44,12 +44,11 @@ final class Env(
     import play.api.libs.json._
     import play.api.libs.concurrent.Execution.Implicits._
     import play.api.libs.iteratee._
-    import lila.db.api.Full.{ query, count }
+    import lila.db.api._
     import lila.db.Implicits.LilaPimpedQueryBuilder
-    import gameRepo.{ coll, bsonReader }
     val selector = DbQuery.frozen ++ sel
-    val cursor = query(selector).sort(DbQuery.sortCreated).cursor //limit 3000
-    val size = count(selector).await
+    val cursor = $query(selector).sort(DbQuery.sortCreated).cursor //limit 3000
+    val size = $count(selector).await
     var nb = 0
     cursor.enumerateBulks(5000) run {
       Iteratee foreach { (gameOptions: Iterator[Option[GameModel]]) ⇒
@@ -57,7 +56,7 @@ final class Env(
         nb = nb + games.size
         if (size > 1000) println(s"Index $nb of $size games")
         // #TODO fetch all pgns in one request
-        val pgns = games.map(g ⇒ (pgnRepo get g.id).await)
+        val pgns = games.map(g ⇒ (PgnRepo get g.id).await)
         esIndexer bulk {
           games zip pgns map {
             case (game, pgn) ⇒ esIndexer.index_prepare(
@@ -79,6 +78,5 @@ object Env {
     config = lila.common.PlayApp loadConfig "gameSearch",
     system = play.api.libs.concurrent.Akka.system(play.api.Play.current),
     esIndexer = lila.search.Env.current.esIndexer,
-    gameRepo = lila.game.Env.current.gameRepo,
-    pgnRepo = lila.game.Env.current.pgnRepo)
+    gameTube = lila.game.gameTube)
 }
