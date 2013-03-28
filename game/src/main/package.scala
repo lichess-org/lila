475ac7f42a532cefd120fe0bf05cc6bafbb334c9
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-03-28 18:30:52 -0300

    apply db api changes to gameSearch repo

--- game/src/main/package.scala
+++ game/src/main/package.scala
@@ -6,5 +6,6 @@ package object game extends PackageObject with WithPlay {
 
   private[game] lazy val pgnInColl = InColl(Env.current.pgnColl)
 
-  private[game] lazy val gameTube = Game.tube inColl Env.current.gameColl
+  // expose game tube
+  lazy val gameTube = Game.tube inColl Env.current.gameColl
 }
