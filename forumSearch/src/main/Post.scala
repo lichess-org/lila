d030385e73a33f643ca9be915c0888236a3f7ac2
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-04-01 10:11:00 -0300

    implement forumSearch module

---
+++ forumSearch/src/main/Post.scala
@@ -0,0 +1,39 @@
+package lila.forumSearch
+
+import lila.forum.PostLiteView
+import lila.search.ElasticSearch
+import play.api.libs.json._
+
+private[forumSearch] object Post {
+
+  object fields {
+    val body = "bo"
+    val topic = "to"
+    val topicId = "ti"
+    val author = "au"
+    val staff = "st"
+  }
+  import fields._
+  import ElasticSearch.Mapping._
+
+  def jsonMapping = Json.obj(
+    "properties" -> Json.toJson(List(
+      boost(body, "string", 2),
+      boost(topic, "string", 4),
+      boost(author, "string"),
+      field(topicId, "string"),
+      field(staff, "boolean")
+    ).toMap),
+    "analyzer" -> Json.toJson("snowball")
+  )
+
+  def from(view: PostLiteView): JsObject = Json.obj(
+    view.post.id -> Json.obj(
+      body -> view.post.text,
+      topic -> view.topic.name,
+      author -> ~(view.post.userId orElse view.post.author),
+      topicId -> view.topic.id,
+      staff -> view.post.isStaff
+    )
+  )
+}
---
+++ forumSearch/src/main/Query.scala
@@ -0,0 +1,34 @@
+package lila.forumSearch
+
+import lila.search.ElasticSearch
+import Post.fields
+
+import org.elasticsearch.index.query._, QueryBuilders._, FilterBuilders._
+
+private[forumSearch] final class Query private (
+    terms: List[String], staff: Boolean) extends lila.search.Query {
+
+  def searchRequest(from: Int = 0, size: Int = 10) = ElasticSearch.Request.Search(
+    query = makeQuery,
+    filter = makeFilters,
+    from = from,
+    size = size)
+
+  def countRequest = ElasticSearch.Request.Count(makeQuery)
+
+  private def makeQuery = terms.foldLeft(boolQuery()) {
+    case (query, term) ⇒ query must {
+      multiMatchQuery(term, fields.body, fields.topic, fields.author)
+    }
+  }
+
+  private def makeFilters = !staff option termFilter(fields.staff, false)
+
+}
+
+object Query {
+
+  def apply(text: String, staff: Boolean): Query = new Query(
+    ElasticSearch.Request decomposeTextQuery text, staff
+  )
+}
