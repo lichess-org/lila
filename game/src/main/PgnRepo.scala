296256ec0ba77539d13e30dbb760d8fe5d07a336
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-04-01 22:49:26 -0300

    fetch all pgns in one query while indexing games

--- game/src/main/PgnRepo.scala
+++ game/src/main/PgnRepo.scala
@@ -10,15 +10,24 @@ import play.modules.reactivemongo.Implicits._
 
 object PgnRepo {
 
+  type ID = String
+
   private implicit def inColl = pgnInColl
 
-  def get(id: String): Fu[String] = getOption(id) map (~_)
+  def get(id: ID): Fu[String] = getOption(id) map (~_)
 
-  def getOption(id: String): Fu[Option[String]] =
+  def getOption(id: ID): Fu[Option[String]] =
     $query($select(id)).one map { _ flatMap (_ str "p") }
 
-  def save(id: String, pgn: String): Funit =
+  def associate(ids: Seq[ID]): Fu[Map[String, String]] =
+    $query($select byIds ids).cursor.toList map2 { (obj: JsObject) ⇒
+      obj str "p" flatMap { pgn =>
+        obj str "_id" map (_ -> pgn)
+      }
+    } map (_.flatten.toMap)
+
+  def save(id: ID, pgn: String): Funit =
     $update($select(id), $set("p" -> pgn), upsert = true)
 
-  def removeIds(ids: List[String]): Funit = $remove($select byIds ids)
+  def removeIds(ids: List[ID]): Funit = $remove($select byIds ids)
 }
