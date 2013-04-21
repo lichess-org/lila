608f445791e48339c0c5acbd8233589eb84ab581
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-04-21 11:27:55 -0300

    optimize search indices regularly

--- modules/search/src/main/Env.scala
+++ modules/search/src/main/Env.scala
@@ -3,12 +3,15 @@ package lila.search
 import com.typesafe.config.Config
 import scalastic.elasticsearch
 import scala.concurrent.Future
+import akka.actor.ActorSystem
+import scala.collection.JavaConversions._
 
-final class Env(config: Config) {
+final class Env(config: Config, system: ActorSystem) {
 
   private val ESHost = config getString "es.host"
   private val ESPort = config getInt "es.port"
   private val ESCluster = config getString "es.cluster"
+  private val IndexesToOptimize = config getStringList "indexes_to_optimize"
 
   val esIndexer = elasticsearch.Indexer.transport(
     settings = Map("cluster.name" -> ESCluster),
@@ -21,10 +24,20 @@ final class Env(config: Config) {
     esIndexer.start
     loginfo("[search] ElasticSearch is running")
   }
+
+  {
+    val scheduler = new lila.common.Scheduler(system)
+    import scala.concurrent.duration._
+
+    scheduler.effect(2 hours, "search: optimize index") {
+      esIndexer.optimize(IndexesToOptimize)
+    }
+  }
 }
 
 object Env {
 
   lazy val current = "[boot] search" describes new Env(
-    config = lila.common.PlayApp loadConfig "search")
+    config = lila.common.PlayApp loadConfig "search",
+    system = lila.common.PlayApp.system)
 }
