56705c2f8d53f1249cfafd52f4c2ffeb5a35d6cd
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-06 18:15:48 -0300

    migrate user.settings to objects

---
+++ bin/mongodb/play21.js
@@ -0,0 +1,4 @@
+// user.settings should not be an empty array, but an empty object
+db.user2.find({settings:[]}).forEach(function(user) {
+  db.user2.update({'_id': user['_id']}, {'$set':{settings:{}}});
+});
