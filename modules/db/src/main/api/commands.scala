1a55cc1cba12294011f048f2dc017ad0a2b96ab0
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-06 21:05:19 -0300

    basic mongodb mapReduce implementation

---
+++ modules/db/src/main/api/commands.scala
@@ -0,0 +1,40 @@
+package lila.db
+package api
+
+import play.api.libs.json._
+import reactivemongo.bson._
+import reactivemongo.core.commands._
+
+/**
+ * MapReduce Command.
+ *
+ * Actually only produces inline output
+ */
+case class MapReduce(
+    collectionName: String,
+    mapFunction: JSFunction,
+    reduceFunction: JSFunction,
+    query: Option[BSONDocument] = None,
+    sort: Option[BSONDocument] = None,
+    limit: Option[Int] = None,
+    finalizeFunction: Option[JSFunction] = None,
+    scope: Option[String] = None,
+    verbose: Boolean = false) extends Command[BSONDocument] {
+
+  override def makeDocuments = BSONDocument(
+    "mapReduce" -> BSONString(collectionName),
+    "map" -> BSONString(mapFunction),
+    "reduce" -> BSONString(reduceFunction),
+    "out" -> BSONDocument("inline" -> true),
+    "query" -> query,
+    "sort" -> query,
+    "limit" -> limit.map(x ⇒ BSONInteger(x)),
+    "finalize" -> finalizeFunction.map(x ⇒ BSONString(x)),
+    "scope" -> scope.map(x ⇒ BSONString(x)),
+    "verbose" -> BSONBoolean(verbose)
+  )
+
+  val ResultMaker = new BSONCommandResultMaker[BSONDocument] {
+    def apply(document: BSONDocument) = Right(document)
+  }
+}
--- modules/db/src/main/api/package.scala
+++ modules/db/src/main/api/package.scala
@@ -4,6 +4,8 @@ import reactivemongo.core.commands.LastError
 
 package object api extends api.$operator {
 
+  type JSFunction = String
+
   private[api] def successful(result: Fu[LastError]): Funit = 
     result flatMap { lastErr ⇒
       lastErr.ok.fold(funit, fuck(lastErr.stringify))
