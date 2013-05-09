1bc5f84be50937e6eaf79f11e6238213bdbd6fff
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-08 23:16:53 -0300

    migrate lobby_room collection

--- bin/mongodb/play21.js
+++ bin/mongodb/play21.js
@@ -2,3 +2,5 @@
 db.user2.find({settings:[]}).forEach(function(user) {
   db.user2.update({'_id': user['_id']}, {'$set':{settings:{}}});
 });
+db.lobby_room.drop();
+db.createCollection("lobby_room",{capped:true,size:50000})
