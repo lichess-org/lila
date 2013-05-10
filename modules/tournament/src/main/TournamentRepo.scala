675931507164b6fab3b142da12854c55a929834b
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-10 14:42:41 -0300

    tournament repository

--- modules/tournament/src/main/TournamentRepo.scala
+++ modules/tournament/src/main/TournamentRepo.scala
@@ -8,70 +8,49 @@ import play.api.libs.json._
 import org.joda.time.DateTime
 import org.scala_tools.time.Imports._
 
-// object TournamentRepo {
+object TournamentRepo {
 
-//   def createdById(id: String): Fu[Option[Created]] = byIdAs(id, _.created)
+  private def asCreated(tour: Tournament): Option[Created] = tour.some collect {
+    case t: Created ⇒ t
+  }
+  private def asStarted(tour: Tournament): Option[Started] = tour.some collect {
+    case t: Started ⇒ t
+  }
+  private def asFinished(tour: Tournament): Option[Finished] = tour.some collect {
+    case t: Finished ⇒ t
+  }
 
-//   def startedById(id: String): Fu[Option[Started]] = byIdAs(id, _.started)
+  def createdById(id: String): Fu[Option[Created]] = byIdAs(id, asCreated)
 
-//   def createdByIdAndCreator(id: String, userId: String): Fu[Option[Created]] =
-//     createdById(id) map (_ filter (_ isCreator userId))
+  def startedById(id: String): Fu[Option[Started]] = byIdAs(id, asStarted)
 
-//   private def byIdAs[A](id: String, as: Tournament ⇒ Option[A]): Fu[Option[A]] = io {
-//     $find byId id map (_ flatMap as)
-//   }
+  def createdByIdAndCreator(id: String, userId: String): Fu[Option[Created]] =
+    createdById(id) map (_ filter (_ isCreator userId))
 
-//   def created: Fu[List[Created]] = 
-//     $find($query(
-//       Json.obj("status" -> Status.Created.id)) sort $sort.createdDesc
-//     )).toList map2 { (tour: Tournament) => tour.created }
+  private def byIdAs[A <: Tournament](id: String, as: Tournament ⇒ Option[A]): Fu[Option[A]] =
+    $find byId id map (_ flatMap as)
 
-//   def started: Fu[List[Started]] = io {
-//     find(DBObject("status" -> Status.Started.id))
-//       .sort(DBObject("createdAt" -> -1))
-//       .toList.map(_.started).flatten
-//   }
+  def created: Fu[List[Created]] = $find(
+    $query(Json.obj("status" -> Status.Created.id)) sort $sort.createdDesc
+  ) map { _.map(asCreated).flatten }
 
-//   def finished(limit: Int): Fu[List[Finished]] = io {
-//     find(DBObject("status" -> Status.Finished.id))
-//       .sort(DBObject("createdAt" -> -1))
-//       .limit(limit)
-//       .toList.map(_.finished).flatten
-//   }
+  def started: Fu[List[Started]] = $find(
+    $query(Json.obj("status" -> Status.Started.id)) sort $sort.createdDesc
+  ) map { _.map(asStarted).flatten }
 
-//   def setUsers(tourId: String, userIds: List[String]): Fu[Unit] = io {
-//     update(idSelector(tourId), $set(Seq("data.users" -> userIds.distinct)))
-//   }
+  def finished(limit: Int): Fu[List[Finished]] = $find(
+    $query(Json.obj("status" -> Status.Finished.id)) sort $sort.createdDesc,
+    limit
+  ) map { _.map(asFinished).flatten }
 
-//   def userHasRunningTournament(username: String): Fu[Boolean] = io {
-//     collection.findOne(
-//       ("status" $ne Status.Finished.id) ++ ("data.users" -> username)
-//     ).isDefined
-//   }
-
-//   val inProgressIds: Fu[List[String]] = io {
-//     primitiveProjections(DBObject("status" -> Status.Started.id), "_id")
-//   }
-
-//   def saveFu(tournament: Tournament): Fu[Unit] = io {
-//     save(tournament.encode)
-//   }
-
-//   def removeFu(tournament: Tournament): Fu[Unit] = io {
-//     remove(idSelector(tournament))
-//   }
-
-//   def withdraw(userId: String): Fu[List[String]] = for {
-//     createds ← created
-//     createdIds ← (createds map (_ withdraw userId) collect {
-//       case Success(tour) ⇒ saveFu(tour) map (_ ⇒ tour.id)
-//     }).sequence
-//     starteds ← started
-//     startedIds ← (starteds map (_ withdraw userId) collect {
-//       case Success(tour) ⇒ saveFu(tour) map (_ ⇒ tour.id)
-//     }).sequence
-//   } yield createdIds ::: startedIds
-
-//   private def idSelector(id: String): DBObject = DBObject("_id" -> id)
-//   private def idSelector(tournament: Tournament): DBObject = idSelector(tournament.id)
-// }
+  def withdraw(userId: String): Fu[List[String]] = for {
+    createds ← created
+    createdIds ← (createds map (_ withdraw userId) collect {
+      case scalaz.Success(tour) ⇒ $update(tour: Tournament) inject tour.id
+    }).sequence
+    starteds ← started
+    startedIds ← (starteds map (_ withdraw userId) collect {
+      case scalaz.Success(tour) ⇒ $update(tour: Tournament) inject tour.id
+    }).sequence
+  } yield createdIds ::: startedIds
+}
