35fb564b4e563f5fe3fbe19cd75d7d9c47152012
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-10 06:56:34 -0300

    mod app

--- app/controllers/Mod.scala
+++ app/controllers/Mod.scala
@@ -12,27 +12,23 @@ object Mod extends LilaController {
   private def modApi = Env.mod.api
   private def modLogApi = Env.mod.logApi
 
-  def engine(username: String) = TODO
-  // Secure(Permission.MarkEngine) { _ ⇒
-  //   me ⇒ AsyncRedirect(modApi.adjust(me, username))(routes.User show username)
-  // }
+  def engine(username: String) = Secure(_.MarkEngine) { _ ⇒
+    me ⇒ modApi.adjust(me.id, username) inject Redirect(routes.User show username)
+  }
 
-  def mute(username: String) = TODO
-  // Secure(Permission.MutePlayer) { _ ⇒
-  //   me ⇒ AsyncRedirect(modApi.mute(me, username))(routes.User show username)
-  // }
+  def mute(username: String) = Secure(_.MutePlayer) { _ ⇒
+    me ⇒ modApi.mute(me.id, username) inject Redirect(routes.User show username)
+  }
 
-  def ban(username: String) = TODO
-  // Secure(Permission.IpBan) { implicit ctx ⇒
-  //   me ⇒ AsyncRedirect(modApi.ban(me, username))(routes.User show username)
-  // }
+  def ban(username: String) = Secure(_.IpBan) { implicit ctx ⇒
+    me ⇒ modApi.ban(me.id, username) inject Redirect(routes.User show username)
+  }
 
-  def ipban(ip: String) = Secure(Permission.IpBan) { implicit ctx ⇒
+  def ipban(ip: String) = Secure(_.IpBan) { implicit ctx ⇒
     me ⇒ modApi.ipban(me.id, ip)
   }
 
-  def log = TODO
-  // Auth { implicit ctx ⇒
-  //   me ⇒ AsyncOk(modLogApi.recent) { html.mod.log(_) }
-  // }
+  def log = Auth { implicit ctx ⇒
+    me ⇒ modLogApi.recent map { html.mod.log(_) }
+  }
 }
