d07d67385e6441c39753547fca2f65d74899400f
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-13 08:51:40 -0300

    db typecheck that stops on error

--- modules/db/src/main/Typecheck.scala
+++ modules/db/src/main/Typecheck.scala
@@ -6,16 +6,20 @@ import play.api.libs.iteratee._
 
 object Typecheck {
 
-  private def iteratee[A]: Iteratee[A, Unit] = {
+  private def iteratee[A](stop: Boolean): Iteratee[A, Unit] = {
 
     def step(input: Input[A]): Iteratee[A, Unit] = input match {
       case Input.EOF                       ⇒ Done((), Input.EOF)
       case Input.Empty | Input.El(Some(_)) ⇒ Cont(i ⇒ step(i))
-      case Input.El(_)                     ⇒ Error("Type error", input)
+      case Input.El(_)                     ⇒ 
+      if (stop) Error("Type error", input)
+      else Cont(i => step(i))
     }
     Cont(i ⇒ step(i))
   }
 
-  def apply[A: TubeInColl]: Fu[String] =
-    $cursor[A]($select.all).enumerate run iteratee inject "Typecheck complete"
+  def apply[A: TubeInColl]: Fu[String] = apply(true)
+
+  def apply[A: TubeInColl](stop: Boolean): Fu[String] =
+    $cursor[A]($select.all).enumerate run iteratee(stop) inject "Typecheck complete"
 }
