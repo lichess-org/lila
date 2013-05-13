f3052e0cefe893a8157fd88abe8e13fea364e609
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-12 22:04:00 -0300

    Revert "attempt at using akka.ActorSelection, but does not support ask pattern"
    This reverts commit 9ccc8c253311c903597ad4809863a5e3b12dfc8a.

--- modules/hub/src/main/Env.scala
+++ modules/hub/src/main/Env.scala
@@ -10,39 +10,39 @@ final class Env(config: Config, system: ActorSystem) {
   private val SocketHubTimeout = config duration "socket.hub.timeout"
 
   object actor {
-    lazy val game = actorSelection("game.actor")
-    lazy val gameIndexer = actorSelection("game.indexer")
-    lazy val renderer = actorSelection("renderer")
-    lazy val captcher = actorSelection("captcher")
-    lazy val forum = actorSelection("forum.actor")
-    lazy val forumIndexer = actorSelection("forum.indexer")
-    lazy val messenger = actorSelection("messenger")
-    lazy val router = actorSelection("router")
-    lazy val teamIndexer = actorSelection("team.indexer")
-    lazy val ai = actorSelection("ai")
-    lazy val monitor = actorSelection("monitor")
-    lazy val tournamentOrganizer = actorSelection("tournament.organizer")
-    lazy val timeline = actorSelection("timeline")
-    lazy val bookmark = actorSelection("bookmark")
+    lazy val game = actorFor("game.actor")
+    lazy val gameIndexer = actorFor("game.indexer")
+    lazy val renderer = actorFor("renderer")
+    lazy val captcher = actorFor("captcher")
+    lazy val forum = actorFor("forum.actor")
+    lazy val forumIndexer = actorFor("forum.indexer")
+    lazy val messenger = actorFor("messenger")
+    lazy val router = actorFor("router")
+    lazy val teamIndexer = actorFor("team.indexer")
+    lazy val ai = actorFor("ai")
+    lazy val monitor = actorFor("monitor")
+    lazy val tournamentOrganizer = actorFor("tournament.organizer")
+    lazy val timeline = actorFor("timeline")
+    lazy val bookmark = actorFor("bookmark")
   }
 
   object socket {
-    lazy val lobby = socketSelection("lobby")
-    lazy val monitor = socketSelection("monitor")
-    lazy val site = socketSelection("site")
-    lazy val round = socketSelection("round")
-    lazy val tournament = socketSelection("tournament")
+    lazy val lobby = socketFor("lobby")
+    lazy val monitor = socketFor("monitor")
+    lazy val site = socketFor("site")
+    lazy val round = socketFor("round")
+    lazy val tournament = socketFor("tournament")
     // this one must load after its broadcasted actors
     lazy val hub = system.actorOf(Props(new Broadcast(List(
       lobby, site, round, tournament
     ))(makeTimeout(SocketHubTimeout))), name = SocketHubName)
   }
 
-  private def actorSelection(name: String) =
-    system.actorSelection("/user/" + config.getString("actor." + name))
+  private def actorFor(name: String) =
+    system.actorFor("/user/" + config.getString("actor." + name))
 
-  private def socketSelection(name: String) =
-    system.actorSelection("/user/" + config.getString("socket." + name))
+  private def socketFor(name: String) =
+    system.actorFor("/user/" + config.getString("socket." + name))
 }
 
 object Env {
