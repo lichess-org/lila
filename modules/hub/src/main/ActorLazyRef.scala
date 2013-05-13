c0f79017f61dbf49b0edc802e6e1e946eb2d6156
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-12 22:36:42 -0300

    use strings for actor paths

--- modules/hub/src/main/ActorLazyRef.scala
+++ modules/hub/src/main/ActorLazyRef.scala
@@ -6,9 +6,9 @@ import akka.util.Timeout
 
 // like ActorRef, but can be created before the referenced actor exists
 // like ActorSelection, but supports ask pattern
-final class ActorLazyRef(system: ActorSystem, path: ActorPath) {
+final class ActorLazyRef private (system: ActorSystem, path: String) {
 
-  def ref = system.actorFor("/user/" + path)
+  def ref = system actorFor path
 
   def !(msg: Any)(implicit sender: ActorRef = Actor.noSender) { ref ! msg }
 
@@ -18,5 +18,5 @@ final class ActorLazyRef(system: ActorSystem, path: ActorPath) {
 object ActorLazyRef {
 
   def apply(system: ActorSystem)(path: String): ActorLazyRef = 
-    new ActorLazyRef(system, ActorPath fromString path)
+    new ActorLazyRef(system, "/user/" + path)
 }
