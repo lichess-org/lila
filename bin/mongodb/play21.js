23ced6c32e802ebaa6e50d79078403ef097fd9bf
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-16 23:04:10 -0300

    migrate forum data

--- bin/mongodb/play21.js
+++ bin/mongodb/play21.js
@@ -11,6 +11,19 @@ db.user2.find({roles:{'$in':[{},[]]}}).forEach(function(user) {
 print("rename user.isChatBan -> user.troll");
 db.user2.update({},{$rename:{isChatBan:'troll'}}, {multi:true});
 
+print("add troll fields to the forum topics")
+db.f_topic.find().forEach(function(topic) {
+  db.f_topic.update({'_id': topic['_id']}, { $set: {
+    troll: false,
+    updatedAtTroll: topic['updatedAt'],
+    nbPostsTroll: topic['nbPosts'],
+    lastPostIdTroll: topic['lastPostId']
+  }});
+});
+
+print("mark all forum posts as not troll");
+db.f_post.update({},{$set:{troll:false}}, {multi:true});
+
 print("user.settings.{chat,sound} should be a string");
 ['settings.chat', 'settings.sound'].forEach(function(name) {
   [true, false].forEach(function(value) {
@@ -26,10 +39,10 @@ print("user.settings.{chat,sound} should be a string");
   });
 });
 
-print("Reset lobby_room");
-db.lobby_room.drop();
-db.createCollection("lobby_room",{capped:true,size:50000})
+// print("Reset lobby_room");
+// db.lobby_room.drop();
+// db.createCollection("lobby_room",{capped:true,size:50000})
 
-print("Reset lobby_entry");
-db.lobby_entry.drop();
-db.createCollection("lobby_entry",{capped:true,size:10000})
+// print("Reset lobby_entry");
+// db.lobby_entry.drop();
+// db.createCollection("lobby_entry",{capped:true,size:10000})
