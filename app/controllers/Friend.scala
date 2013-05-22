db20da3fb668fbc80a41fce2a28d4438332f715b
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-23 00:00:58 +0200

    propose to signup when anon tries to friend someone

--- app/controllers/Friend.scala
+++ app/controllers/Friend.scala
@@ -11,12 +11,13 @@ object Friend extends LilaController {
 
   private def env = Env.friend
 
-  def yes(friendId: String) = Auth { implicit ctx ⇒
-    me ⇒
-      env.api.yes(me.id, friendId).nevermind inject html.friend.button(friendId)
+  def yes(friendId: String) = Open { implicit ctx ⇒
+    ctx.userId.fold(Ok(html.friend.button(friendId)).fuccess) { userId ⇒
+      env.api.yes(userId, userId).nevermind inject html.friend.button(friendId)
+    }
   }
 
-  def no(friendId: String) = Auth { implicit ctx =>
+  def no(friendId: String) = Auth { implicit ctx ⇒
     me ⇒
       env.api.no(me.id, friendId).nevermind inject html.friend.button(friendId)
   }
