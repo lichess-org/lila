bc7c02203e01d9cc829a8fc069e1061637ba72a3
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-23 20:03:43 +0200

    parallelize user infos creation

---
+++ modules/game/src/main/BestOpponents.scala
@@ -0,0 +1,17 @@
+package lila.game
+
+import lila.user.{ User, UserRepo }
+
+object BestOpponents {
+
+  def apply(userId: String, limit: Int): Fu[List[(User, Int)]] =
+    GameRepo.bestOpponents(userId, limit) flatMap { opponents ⇒
+      UserRepo enabledByIds opponents.map(_._1) map { users ⇒
+        (users map { user ⇒
+          opponents find (_._1 == user.id) map { opponent ⇒
+            user -> opponent._2
+          }
+        }).flatten sortBy (-_._2)
+      }
+    }
+}
