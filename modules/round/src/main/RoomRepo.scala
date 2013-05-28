785b4839379271c9c630b17976752148d73e7d31
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-28 13:48:45 +0200

    give up with chat system message duplication skip

--- modules/round/src/main/RoomRepo.scala
+++ modules/round/src/main/RoomRepo.scala
@@ -7,19 +7,14 @@ object RoomRepo {
 
   def room(id: String): Fu[Room] = $find byId id map (_ | Room(id, Nil))
 
-  def addMessage(id: String, author: String, text: String): Funit =
+  def addMessage(id: String, author: String, text: String): Funit = 
     $update(
       $select(id),
       $push("messages", Room.encode(author, text)),
       upsert = true)
 
-  // false if skipped because previous message is similar
-  def addSystemMessage(id: String, text: String): Fu[Boolean] =
-    room(id) flatMap {
-      _.decodedMessages.lastOption ?? { _ == ("system", text) } ?? {
-        addMessage(id, "system", text) inject true
-      }
-    }
+  def addSystemMessage(id: String, text: String) =
+    addMessage(id, "system", text)
 
   def addSystemMessages(id: String, texts: Seq[String]): Funit =
     (texts map { addSystemMessage(id, _) }).sequenceFu.void
