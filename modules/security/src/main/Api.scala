8e534c7c9a22afc7ba034bfc362568a46ceda01d
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-29 12:25:56 +0200

    fix user login

--- modules/security/src/main/Api.scala
+++ modules/security/src/main/Api.scala
@@ -25,7 +25,7 @@ private[security] final class Api(firewall: Firewall) {
 
   // blocking function, required by Play2 form
   def authenticateUser(username: String, password: String): Option[User] =
-    UserRepo.authenticate(username, password).await
+    UserRepo.authenticate(username.toLowerCase, password).await
 
   def restoreUser(req: RequestHeader): Fu[Option[User]] =
     firewall accepts req flatMap { 
