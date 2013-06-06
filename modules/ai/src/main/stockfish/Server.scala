42abc09f581d46a77cec9065e4b22af151ab5c6e
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-06 15:15:58 +0200

    satisfying AI move and analysis

--- modules/ai/src/main/stockfish/Server.scala
+++ modules/ai/src/main/stockfish/Server.scala
@@ -5,7 +5,6 @@ import scala.concurrent.duration._
 
 import akka.actor.{ Props, Actor, ActorRef, Kill }
 import akka.pattern.{ ask, AskTimeoutException }
-import model.{ GetQueueSize, QueueSize }
 import play.api.libs.concurrent.Akka.system
 import play.api.Play.current
 
@@ -18,18 +17,18 @@ private[ai] final class Server(queue: ActorRef, config: Config) {
 
   def play(pgn: String, initialFen: Option[String], level: Int): Fu[String] = {
     implicit val timeout = makeTimeout(config.playTimeout)
-    UciDump(pgn, initialFen) fold (
-      err ⇒ fufail(err),
-      moves ⇒ queue ? PlayReq(moves, initialFen map chess960Fen, level) mapTo manifest[String]
-    )
+    UciDump(pgn, initialFen).future flatMap { moves ⇒
+      queue ? PlayReq(moves, initialFen map chess960Fen, level) mapTo
+        manifest[Valid[String]] flatMap (_.future)
+    }
   }
 
   def analyse(pgn: String, initialFen: Option[String]): Fu[AnalysisMaker] = {
     implicit val timeout = makeTimeout(config.analyseTimeout)
-    UciDump(pgn, initialFen).fold(
-      err ⇒ fufail(err),
-      moves ⇒ queue ? FullAnalReq(moves, initialFen map chess960Fen) mapTo manifest[AnalysisMaker]
-    )
+    UciDump(pgn, initialFen).future flatMap { moves ⇒
+      queue ? FullAnalReq(moves, initialFen map chess960Fen) mapTo
+        manifest[Valid[AnalysisMaker]] flatMap (_.future)
+    }
   }
 
   private def chess960Fen(fen: String) = (Forsyth << fen).fold(fen) { situation ⇒
