2f11fba7071fcad4af4f8ee40b7a57e9cf77eff7
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-12 14:03:51 +0200

    serve assets from a static domain

--- app/templating/AssetHelper.scala
+++ app/templating/AssetHelper.scala
@@ -9,13 +9,17 @@ trait AssetHelper {
 
   val assetVersion = 66
 
+  private val domain = lila.api.Env.current.Net.AssetDomain
+
+  private def url(path: String) = "http://%s/%s".format(domain, path)
+
   def cssTag(name: String) = css("stylesheets/" + name)
 
   def cssVendorTag(name: String) = css("vendor/" + name)
 
   private def css(path: String) = Html {
     """<link href="%s?v=%d" type="text/css" rel="stylesheet"/>"""
-      .format(routes.Assets.at(path), assetVersion)
+      .format(url(routes.Assets.at(path).toString), assetVersion)
   }
 
   def jsTag(name: String) = js("javascripts/" + name)
@@ -27,7 +31,7 @@ trait AssetHelper {
   private def js(path: String) = jsAt(routes.Assets.at(path).toString)
 
   def jsAt(path: String) = Html {
-    """<script src="%s?v=%d"></script>""".format(path, assetVersion)
+    """<script src="%s?v=%d"></script>""".format(url(path), assetVersion)
   }
 
   def embedJs(js: String): Html = Html("""<script type="text/javascript">
