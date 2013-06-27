6d739ca69bede188ae9b556ec466ca54195dc62a
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-27 19:33:21 +0200

    use custom version of play-reactivemongo

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,7 +44,7 @@ object Dependencies {
   val jodaConvert = "org.joda" % "joda-convert" % "1.3.1"
   val scalastic = "scalastic" %% "scalastic" % "0.90.0-thib"
   val reactivemongo = "org.reactivemongo" %% "reactivemongo" % "0.10-THIB"
-  val playReactivemongo = "org.reactivemongo" %% "play2-reactivemongo" % "0.9"
+  val playReactivemongo = "org.reactivemongo" %% "play2-reactivemongo" % "0.10-THIB"
   object play {
     val version = "2.1.2-RC1"
     val api = "play" %% "play" % version
