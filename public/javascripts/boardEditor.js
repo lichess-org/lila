1dbddaa75958dc7a0a37919f547441acc72f4f35
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-07-21 16:08:20 +0200

    board editor wip

--- public/javascripts/boardEditor.js
+++ public/javascripts/boardEditor.js
@@ -1,8 +1,30 @@
 $(function() {
-  var board = new ChessBoard('board', {
-    draggable: true,
-    dropOffBoard: 'trash',
-    position: 'start',
-    sparePieces: true
+  $('#board_editor').each(function() {
+    var $wrap = $(this);
+    var fen = "";
+
+    function setFen(f) {
+      fen = f;
+      $wrap.find('.fen_string').text(fen);
+      $wrap.find('a.fen_link').each(function() {
+        $(this).attr('href', $(this).attr('href').replace(/fen=[^#]*#/, "fen=" + fen + '#'));
+      });
+    }
+
+    var board = new ChessBoard('chessboard', {
+      draggable: true,
+      dropOffBoard: 'trash',
+      position: 'start',
+      sparePieces: true,
+      pieceTheme: '/assets/vendor/chessboard/img/chesspieces/wikipedia/{piece}.png',
+      onChange: function(oldPos, newPos) {
+        setFen(ChessBoard.objToFen(newPos));
+      }
+    });
+    setFen(board.fen());
+
+    $wrap.find('a.start').on('click', board.start);
+    $wrap.find('a.clear').on('click', board.clear);
+    $wrap.find('a.flip').on('click', board.flip);
   });
 });
