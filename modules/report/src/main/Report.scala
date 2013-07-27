6d9cfffca544a3d210a36405aa8c634fc3907c50
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-07-27 15:03:14 +0200

    implement report form

--- modules/report/src/main/Report.scala
+++ modules/report/src/main/Report.scala
@@ -3,9 +3,12 @@ package lila.report
 import org.joda.time.DateTime
 import ornicar.scalalib.Random
 
+import lila.user.User
+
 case class Report(
     id: String, // also the url slug
     text: String,
+    user: String, // the reportee
     processedBy: Option[String],
     createdAt: DateTime,
     createdBy: String) {
@@ -13,18 +16,22 @@ case class Report(
   def slug = id
 
   def isCreator(user: String) = user == createdBy
+
+  def process(by: User) = copy(processedBy = by.id.some)
 }
 
 object Report {
 
   def make(
+    user: User,
     text: String,
-    createdBy: String): Report = new Report(
+    createdBy: User): Report = new Report(
     id = Random nextString 8,
+    user = user.id,
     text = text,
     processedBy = none,
     createdAt = DateTime.now,
-    createdBy = createdBy)
+    createdBy = createdBy.id)
 
   import lila.db.Tube, Tube.Helpers._
   import play.api.libs.json._
