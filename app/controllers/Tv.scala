fd3a6e9b71ffe80d9b0bb9c1165c1d17211bb2e2
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-08-02 11:27:28 +0200

    dissociate TV from round watcher

--- app/controllers/Tv.scala
+++ app/controllers/Tv.scala
@@ -6,12 +6,25 @@ import views._
 
 import lila.app._
 import lila.game.Pov
+import lila.round.WatcherRoomRepo
+import lila.tournament.TournamentRepo
 
-object Tv extends LilaController with Watcher {
+object Tv extends LilaController {
 
   def index = Open { implicit ctx ⇒
     OptionFuResult(Env.game.featured.one) { game ⇒
-      watch(Pov creator game, true)
+      Env.round.version(game.id) zip
+        (WatcherRoomRepo room game.id map { room ⇒
+          html.round.watcherRoomInner(room.decodedMessages)
+        }) zip
+        (game.tournamentId ?? TournamentRepo.byId) map {
+          case ((v, roomHtml), tour) ⇒
+            Ok(html.tv.index(
+              Pov creator game,
+              v,
+              roomHtml,
+              tour))
+        }
     }
   }
 }
