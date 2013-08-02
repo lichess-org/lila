d7dea484f9be445c1fa84579557d6e20d5047cba
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-08-02 15:41:34 +0200

    also slice tournament chat messages

--- modules/tournament/src/main/RoomRepo.scala
+++ modules/tournament/src/main/RoomRepo.scala
@@ -7,11 +7,13 @@ import tube.roomTube
 
 private[tournament] object RoomRepo {
 
+  val maxMessages = 50
+
   def room(id: String): Fu[Room] = ($find byId id) map (_ | Room(id, Nil))
 
   def addMessage(id: String, msg: Message): Funit = $update(
     $select(id),
-    $push("messages", Room encode msg),
+    $pushSlice("messages", Room encode msg, -maxMessages),
     upsert = true,
     multi = false)
 }
