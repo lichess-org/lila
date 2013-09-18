ec6ac026b00f7221c973bddae379577b2103609b
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-09-18 17:57:15 +0200

    upgrade db project to scalaz 7

--- modules/db/src/main/api/enumerate.scala
+++ modules/db/src/main/api/enumerate.scala
@@ -1,13 +1,14 @@
 package lila.db
 package api
 
-import lila.db.Implicits._
 import play.api.libs.iteratee._
 import play.api.libs.json._
 import play.modules.reactivemongo.json.ImplicitBSONHandlers._
 import reactivemongo.api.Cursor
 import reactivemongo.bson._
-import scalaz.Zero
+import scalaz.Monoid
+
+import lila.db.Implicits._
 
 object $enumerate {
 
@@ -24,8 +25,8 @@ object $enumerate {
     }
 
   def fold[A: BSONDocumentReader, B](query: QueryBuilder)(zero: B)(f: (B, A) ⇒ B): Fu[B] =
-    query.cursor[A].enumerate |>>> Iteratee.fold(zero)(f) 
+    query.cursor[A].enumerate |>>> Iteratee.fold(zero)(f)
 
-  def foldZero[A: BSONDocumentReader, B: Zero](query: QueryBuilder)(f: (B, A) ⇒ B): Fu[B] =
-    fold(query)(∅[B])(f)
+  def foldMonoid[A: BSONDocumentReader, B: Monoid](query: QueryBuilder)(f: (B, A) ⇒ B): Fu[B] =
+    fold(query)(Monoid[B].zero)(f)
 }
