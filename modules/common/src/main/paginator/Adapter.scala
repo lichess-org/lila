125e550919a968b9dced9e2a79e1fa0bfd6b6353
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-09-18 17:30:54 +0200

    upgrade common project to scalaz 7

--- modules/common/src/main/paginator/Adapter.scala
+++ modules/common/src/main/paginator/Adapter.scala
@@ -24,7 +24,7 @@ trait AdapterLike[A] {
     def nbResults = AdapterLike.this.nbResults
 
     def slice(offset: Int, length: Int) =
-      AdapterLike.this.slice(offset, length) map2 f
+      AdapterLike.this.slice(offset, length) map (_.toList) map2 f
   }
 
   def mapFuture[B](f: A ⇒ Fu[B]): AdapterLike[B] = new AdapterLike[B] {
