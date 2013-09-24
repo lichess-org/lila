c5d4e7d26e0c09918563490e78ccb507c2ee0030
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-09-24 23:29:10 +0200

    improve AI failure logging

--- modules/ai/src/main/stockfish/Stockfish.scala
+++ modules/ai/src/main/stockfish/Stockfish.scala
@@ -7,7 +7,7 @@ import chess.format.UciMove
 object Stockfish {
 
   def applyMove(game: Game, pgn: String, move: String): Fu[(Game, chess.Move)] = (for {
-    bestMove ← UciMove(~(move.some filter (_.nonEmpty))) toValid "[stockfish] Wrong bestmove: " + move
+    bestMove ← UciMove(~(move.some filter (_.nonEmpty))) toValid "Wrong bestmove: " + move
     result ← (game withPgnMoves pgn)(bestMove.orig, bestMove.dest)
   } yield result).future
 }
