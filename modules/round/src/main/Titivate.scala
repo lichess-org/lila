f26046b613fc013f0600b8cb63268f8359df1753
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-09-26 14:35:05 +0200

    implement scheduler throttling

--- modules/round/src/main/Titivate.scala
+++ modules/round/src/main/Titivate.scala
@@ -2,11 +2,11 @@ package lila.round
 
 import scala.concurrent.duration._
 
-import akka.actor.{ ActorRef, Scheduler }
 import play.api.libs.iteratee._
 import play.api.libs.json._
 import play.modules.reactivemongo.json.ImplicitBSONHandlers._
 
+import lila.common.Scheduler
 import lila.common.PimpedJson._
 import lila.db.api._
 import lila.game.tube.gameTube
@@ -15,25 +15,19 @@ import lila.hub.actorApi.map.Tell
 import lila.round.actorApi.round.{ Outoftime, Abandon }
 
 private[round] final class Titivate(
-    roundMap: ActorRef,
+    roundMap: akka.actor.ActorRef,
     meddler: Meddler,
     scheduler: Scheduler) {
 
   def finishByClock: Funit =
     $primitive(Query.finishByClock, "_id", max = 5000.some)(_.asOpt[String]) addEffect { ids ⇒
       println("[titivate] Finish %d games by clock" format ids.size)
-      delayBatch(ids, 100.millis) { id ⇒ roundMap ! Tell(id, Outoftime) }
+      scheduler.throttle(100.millis)(ids) { id ⇒ roundMap ! Tell(id, Outoftime) }
     } void
 
   def finishAbandoned: Funit =
     $primitive(Query.abandoned, "_id", max = 5000.some)(_.asOpt[String]) addEffect { ids ⇒
       println("[titivate] Finish %d abandoned games" format ids.size)
-      delayBatch(ids, 100.millis) { id ⇒ roundMap ! Tell(id, Abandon) }
+      scheduler.throttle(100.millis)(ids) { id ⇒ roundMap ! Tell(id, Abandon) }
     } void
-
-  private def delayBatch[A](batch: Seq[A], duration: FiniteDuration)(op: A ⇒ Unit) {
-    batch.zipWithIndex foreach {
-      case (a, i) ⇒ scheduler.scheduleOnce((1 + i) * duration) { op(a) }
-    }
-  }
 }
