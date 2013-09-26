60730c4fa81a9025ab6515770277136253e551cb
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-09-26 18:55:25 +0200

    finer maintenance task

--- modules/game/src/main/Maintenance.scala
+++ modules/game/src/main/Maintenance.scala
@@ -24,8 +24,8 @@ private[game] final class Maintenance(
   }
 
   def cleanupUnplayed: Funit =
-    $primitive(Query.unplayed, "_id", max = 10000.some)(_.asOpt[String]) addEffect { ids ⇒
+    $primitive(Query.unplayed, "_id", max = 5000.some)(_.asOpt[String]) addEffect { ids ⇒
       println("[titivate] Remove %d unplayed games" format ids.size)
-      scheduler.throttle(1.second)(ids grouped 20 toSeq)(remove)
+      scheduler.throttle(1.second)(ids grouped 10 toSeq)(remove)
     } void
 }
