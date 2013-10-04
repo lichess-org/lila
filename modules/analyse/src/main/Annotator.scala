0f12a0d3e522b9c304b4194468613d4b86c33a0b
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-10-04 16:52:37 +0200

    compute PGN variations from engine UCI line - wip

--- modules/analyse/src/main/Annotator.scala
+++ modules/analyse/src/main/Annotator.scala
@@ -1,32 +1,46 @@
 package lila.analyse
 
-import chess.format.pgn
+import chess.format.pgn.{ Pgn, Tag, Turn, Move }
 
 private[analyse] final class Annotator(netDomain: String) {
 
-  def apply(p: pgn.Pgn, analysis: Analysis): pgn.Pgn =
+  def apply(p: Pgn, analysis: Analysis): Pgn =
     annotateTurns(p, analysis.advices).copy(
-      tags = p.tags :+ pgn.Tag("Annotator", netDomain)
+      tags = p.tags :+ Tag("Annotator", netDomain)
     )
 
-  private def annotateTurns(p: pgn.Pgn, advices: List[Advice]): pgn.Pgn =
+  private def annotateTurns(p: Pgn, advices: List[Advice]): Pgn =
     advices.foldLeft(p) {
-      case (acc, advice) ⇒ acc.updateTurn(advice.turn, turn ⇒
+      case (pgn, advice) ⇒ pgn.updateTurn(advice.turn, turn ⇒
         turn.update(advice.color, move ⇒
           move.copy(
             nag = advice.nag.code.some,
-            comment = makeComment(advice).some
+            comment = makeComment(advice).some,
+            variation = makeVariation(advice)
           )
         )
       )
     }
+
+  private def makeVariation(advice: Advice): List[Turn] = {
+    advice.info.line.isDefined ?? List(Turn(
+      number = advice.turn,
+      white = advice.info.color.white option Move("d4"),
+      black = Some(Move("e5"))
+    ))
+  }
+  // advice.info.line.??(_.list).zipWithIndex map { 
+  //   case (uciMove, i) => 
+  //     val 
+  //   }
+  // }
 
   private def makeComment(advice: Advice): String = (advice match {
     case CpAdvice(sev, _, _)   ⇒ sev.nag.toString
     case MateAdvice(sev, _, _) ⇒ sev.desc
   }) ++ makeBestComment(advice).fold(".")(". " + _)
 
-  private def makeBestComment(advice: Advice): Option[String] = 
+  private def makeBestComment(advice: Advice): Option[String] =
     (advice.info.move != advice.info.best) option {
       "Best was %s." format advice.info.best.uci
     }
