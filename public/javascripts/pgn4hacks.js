b0ebc0afa9455fd10368985922927f6a083d2a19
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-10-09 22:28:35 +0200

    un-table pgn moves in analysis view

--- public/javascripts/pgn4hacks.js
+++ public/javascripts/pgn4hacks.js
@@ -1,17 +1,6 @@
 // these functions must remain on root namespace 
 
 function customFunctionOnPgnGameLoad() {
-  var $text = $('#ShowPgnText');
-  var html = '<table><tbody><tr>';
-  $text.find('span.move, span.variation').remove();
-  $text.find('>a').each(function(it) {
-    if (0 == it % 2) {
-      html += '</tr><tr><th>' + (it / 2 + 1) + '</th>';
-    }
-    html += '<td>' + this.outerHTML + '</td>';
-  });
-  html += '</tr></tbody></table>';
-  $text.html(html).find('tr:empty').remove();
 
   $('div.lichess_goodies a.rotate_board').click(function() {
     $('#GameBoard').toggleClass('flip');
