b98a7b2cc393420e22e5c850dafd01d651be7050
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-10-20 20:31:08 +0200

    user prefs wip

--- app/controllers/Pref.scala
+++ app/controllers/Pref.scala
@@ -13,6 +13,25 @@ object Pref extends LilaController {
   private def api = Env.pref.api
   private def forms = Env.pref.forms
 
+  def form = Auth { implicit ctx ⇒
+    me ⇒
+      forms prefOf me map { form ⇒
+        Ok(html.account.pref(me, form))
+      }
+  }
+
+  def formApply = AuthBody { implicit ctx ⇒
+    me ⇒
+      implicit val req = ctx.body
+      FormFuResult(forms.pref) { err ⇒
+        fuccess(html.account.pref(me, err))
+      } { data ⇒
+        api getPref me flatMap { pref ⇒
+          api.setPref(data(pref))
+        } inject Redirect(routes.Pref.form)
+      }
+  }
+
   def set(name: String) = OpenBody { implicit ctx ⇒
     implicit val req = ctx.body
     (setters get name) ?? {
