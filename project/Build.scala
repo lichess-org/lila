d3856dcb571a574c332ca9cff6d8ad66414fc397
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-11-17 11:42:23 +0100

    upgrade dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -12,8 +12,8 @@ object ApplicationBuild extends Build {
     offline := true,
     libraryDependencies ++= Seq(
       scalaz, scalalib, hasher, config, apache, scalaTime,
-      csv, jgit, actuarius, scalastic, findbugs, reactivemongo,
-      playReactivemongo, spray.caching),
+      csv, jgit, actuarius, scalastic, findbugs, RM,
+      PRM, spray.caching),
       scalacOptions := compilerOptions,
       sources in doc in Compile := List(),
       templatesImport ++= Seq(
@@ -38,16 +38,16 @@ object ApplicationBuild extends Build {
     .settings(
       libraryDependencies := provided(
         play.api, hasher, config, apache, csv, jgit,
-        actuarius, scalastic, findbugs, reactivemongo)
+        actuarius, scalastic, findbugs, RM)
     ) aggregate (moduleRefs: _*)
 
   lazy val simulation = project("simulation", Seq(
     common, hub, socket, game, round, setup)).settings(
-    libraryDependencies ++= provided(play.api, reactivemongo)
+    libraryDependencies ++= provided(play.api, RM)
   )
 
   lazy val common = project("common").settings(
-    libraryDependencies ++= provided(play.api, play.test, reactivemongo, csv)
+    libraryDependencies ++= provided(play.api, play.test, RM, csv)
   )
 
   lazy val memo = project("memo", Seq(common)).settings(
@@ -55,7 +55,7 @@ object ApplicationBuild extends Build {
   )
 
   lazy val db = project("db", Seq(common)).settings(
-    libraryDependencies ++= provided(play.test, play.api, reactivemongo, playReactivemongo)
+    libraryDependencies ++= provided(play.test, play.api, RM, PRM)
   )
 
   lazy val search = project("search", Seq(common, hub)).settings(
@@ -64,117 +64,117 @@ object ApplicationBuild extends Build {
 
   lazy val timeline = project("timeline", Seq(common, db, game, user, hub, security, relation)).settings(
     libraryDependencies ++= provided(
-      play.api, play.test, reactivemongo, playReactivemongo)
+      play.api, play.test, RM, PRM)
   )
 
   lazy val mod = project("mod", Seq(common, db, user, hub, security)).settings(
     libraryDependencies ++= provided(
-      play.api, play.test, reactivemongo, playReactivemongo)
+      play.api, play.test, RM, PRM)
   )
 
   lazy val user = project("user", Seq(common, memo, db, hub, chess)).settings(
     libraryDependencies ++= provided(
-      play.api, play.test, reactivemongo, playReactivemongo, hasher)
+      play.api, play.test, RM, PRM, hasher)
   )
 
   lazy val game = project("game", Seq(common, memo, db, hub, user, chess)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo)
+      play.api, RM, PRM)
   )
 
   lazy val gameSearch = project("gameSearch", Seq(common, hub, chess, search, game, analyse)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo, scalastic)
+      play.api, RM, PRM, scalastic)
   )
 
   lazy val analyse = project("analyse", Seq(common, hub, chess, game, user)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo, spray.caching)
+      play.api, RM, PRM, spray.caching)
   )
 
   lazy val round = project("round", Seq(
     common, db, memo, hub, socket, chess, game, user, security, i18n, ai, pref)).settings(
-    libraryDependencies ++= provided(play.api, reactivemongo, playReactivemongo)
+    libraryDependencies ++= provided(play.api, RM, PRM)
   )
 
   lazy val lobby = project("lobby", Seq(
     common, db, memo, hub, socket, chess, game, user, round, timeline, relation)).settings(
-    libraryDependencies ++= provided(play.api, reactivemongo, playReactivemongo)
+    libraryDependencies ++= provided(play.api, RM, PRM)
   )
 
   lazy val setup = project("setup", Seq(
     common, db, memo, hub, socket, chess, game, user, lobby, round)).settings(
-    libraryDependencies ++= provided(play.api, reactivemongo, playReactivemongo)
+    libraryDependencies ++= provided(play.api, RM, PRM)
   )
 
   lazy val importer = project("importer", Seq(common, chess, game, round)).settings(
-    libraryDependencies ++= provided(play.api, reactivemongo, playReactivemongo)
+    libraryDependencies ++= provided(play.api, RM, PRM)
   )
 
   lazy val tournament = project("tournament", Seq(common, hub, socket, chess, game, round, setup, security)).settings(
-    libraryDependencies ++= provided(play.api, reactivemongo, playReactivemongo)
+    libraryDependencies ++= provided(play.api, RM, PRM)
   )
 
   lazy val ai = project("ai", Seq(common, hub, chess, game, analyse)).settings(
-    libraryDependencies ++= provided(play.api, reactivemongo, playReactivemongo)
+    libraryDependencies ++= provided(play.api, RM, PRM)
   )
 
   lazy val security = project("security", Seq(common, hub, db, user)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo, spray.caching)
+      play.api, RM, PRM, spray.caching)
   )
 
   lazy val relation = project("relation", Seq(common, db, memo, hub, user, game)).settings(
-    libraryDependencies ++= provided(play.api, reactivemongo, playReactivemongo)
+    libraryDependencies ++= provided(play.api, RM, PRM)
   )
 
   lazy val pref = project("pref", Seq(common, db, user)).settings(
-    libraryDependencies ++= provided(play.api, reactivemongo, playReactivemongo)
+    libraryDependencies ++= provided(play.api, RM, PRM)
   )
 
   lazy val message = project("message", Seq(common, db, user, hub, relation, security)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo, spray.caching)
+      play.api, RM, PRM, spray.caching)
   )
 
   lazy val forum = project("forum", Seq(common, db, user, security, hub, mod)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo, spray.caching)
+      play.api, RM, PRM, spray.caching)
   )
 
   lazy val forumSearch = project("forumSearch", Seq(common, hub, forum, search)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo, scalastic)
+      play.api, RM, PRM, scalastic)
   )
 
   lazy val team = project("team", Seq(common, memo, db, user, forum, security, hub)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo)
+      play.api, RM, PRM)
   )
 
   lazy val teamSearch = project("teamSearch", Seq(common, hub, team, search)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo, scalastic)
+      play.api, RM, PRM, scalastic)
   )
 
   lazy val i18n = project("i18n", Seq(common, db, user, hub)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo, jgit)
+      play.api, RM, PRM, jgit)
   )
 
   lazy val bookmark = project("bookmark", Seq(common, memo, db, hub, user, game)).settings(
     libraryDependencies ++= provided(
-      play.api, play.test, reactivemongo, playReactivemongo)
+      play.api, play.test, RM, PRM)
   )
 
   lazy val wiki = project("wiki", Seq(common, db)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo, jgit, actuarius, guava)
+      play.api, RM, PRM, jgit, actuarius, guava)
   )
 
   lazy val report = project("report", Seq(common, db, user)).settings(
     libraryDependencies ++= provided(
-      play.api, reactivemongo, playReactivemongo)
+      play.api, RM, PRM)
   )
 
   lazy val notification = project("notification", Seq(common, user, hub)).settings(
@@ -182,7 +182,7 @@ object ApplicationBuild extends Build {
   )
 
   lazy val monitor = project("monitor", Seq(common, hub, socket, db)).settings(
-    libraryDependencies ++= provided(play.api, reactivemongo, playReactivemongo)
+    libraryDependencies ++= provided(play.api, RM, PRM)
   )
 
   lazy val site = project("site", Seq(common, socket)).settings(
