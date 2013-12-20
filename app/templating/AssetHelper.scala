467ae7a66033bef5d2e471ba656ec26c407daa7a
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-12-20 21:55:39 +0100

    serve more assets through static url

--- app/templating/AssetHelper.scala
+++ app/templating/AssetHelper.scala
@@ -12,45 +12,43 @@ trait AssetHelper {
 
   private val domain = lila.api.Env.current.Net.AssetDomain
 
-  private def url(path: String) = "http://" + domain + path
+  def staticUrl(path: String) = s"http://$domain${routes.Assets.at(path)}"
 
   def cssTag(name: String) = css("stylesheets/" + name)
 
   def cssVendorTag(name: String) = css("vendor/" + name)
 
   private def css(path: String) = Html {
-    s"""<link href="${url(routes.Assets.at(path).toString)}?v=$assetVersion" type="text/css" rel="stylesheet"/>"""
+    s"""<link href="${staticUrl(path)}?v=$assetVersion" type="text/css" rel="stylesheet"/>"""
   }
 
-  def jsTag(name: String) = js("javascripts/" + name)
+  def jsTag(name: String) = jsAt("javascripts/" + name)
 
-  def jsTagCompiled(name: String) = isProd ? js("compiled/" + name) | jsTag(name)
+  def jsTagCompiled(name: String) = isProd ? jsTag("compiled/" + name) | jsTag(name)
 
-  def jsVendorTag(name: String) = js("vendor/" + name)
+  def jsVendorTag(name: String) = jsTag("vendor/" + name)
 
   lazy val highchartsTag = cdnOrLocal(
     cdn = "http://code.highcharts.com/3.0/highcharts.js",
     test = "window.Highcharts",
-    local = routes.Assets.at("vendor/highcharts/highcharts.js").toString)
+    local = staticUrl("vendor/highcharts/highcharts.js"))
 
   lazy val highchartsMoreTag = cdnOrLocal(
     cdn = "http://code.highcharts.com/3.0/highcharts-more.js",
     test = "window.Highcharts",
-    local = routes.Assets.at("vendor/highcharts/highcharts-more.js").toString)
+    local = staticUrl("vendor/highcharts/highcharts-more.js"))
 
   lazy val highstockTag = cdnOrLocal(
     cdn = "http://code.highcharts.com/stock/3.0/highstock.js",
     test = "window.Highcharts",
-    local = routes.Assets.at("vendor/highstock/highstock.js").toString)
+    local = staticUrl("vendor/highstock/highstock.js"))
 
   private def cdnOrLocal(cdn: String, test: String, local: String) = Html {
     s"""<script src="$cdn"></script><script>$test || document.write('<script src="$local?v=$assetVersion">\\x3C/script>')</script>"""
   }
 
-  private def js(path: String) = jsAt(routes.Assets.at(path).toString)
-
   def jsAt(path: String, static: Boolean = true) = Html {
-    s"""<script src="${static.fold(url(path), path)}?v=$assetVersion"></script>"""
+    s"""<script src="${static.fold(staticUrl(path), path)}?v=$assetVersion"></script>"""
   }
 
   def embedJs(js: String): Html = Html(s"""<script type="text/javascript">/* <![CDATA[ */ $js /* ]]> */</script>""")
