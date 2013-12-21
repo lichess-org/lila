16768e4d8c6e4f6bb236f3c4cf9e42bbbfbbaa64
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-12-21 20:24:41 +0100

    fix queenside castling premove

--- public/javascripts/big.js
+++ public/javascripts/big.js
@@ -1192,7 +1192,7 @@ var storage = {
         case 'king':
           return (Math.abs(t.x - f.x) <= 1 && Math.abs(t.y - f.y) <= 1) ||
             (f.y == t.y && (f.y == (color == 'white' ? 1 : 8)) && (
-          (f.x == 5 && (t.x == 2 || t.x == 7)) ||
+          (f.x == 5 && (t.x == 3 || t.x == 7)) ||
             $('#' + to + '>.rook.' + color).length == 1));
         case 'queen':
           return Math.abs(t.x - f.x) == Math.abs(t.y - f.y) || t.x == f.x || t.y == f.y;
