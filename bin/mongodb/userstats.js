d80fd89677445680786f9644ddf765832c7fb079
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-12-21 13:11:32 +0100

    update user stats denormalization mongodb script

--- bin/mongodb/userstats.js
+++ bin/mongodb/userstats.js
@@ -1,5 +1,5 @@
-var games = db.game4
-var users = db.user2
+var games = db.game5;
+var users = db.user3;
 
 var batchSize = 1000;
 var i, t, timeStrings, times, it=0;
@@ -10,23 +10,23 @@ function hintWid(query) {
   return games.find(query).hint({wid:1}).length();
 }
 
-print("Denormalize counts")
+print("Denormalize counts");
 users.find().sort({seenAt: -1}).forEach(function(user) {
-  var uid = user['_id'];
+  var uid = user._id;
   var count = {
-    game: games.count({'uids': uid}),
+    game: games.count({'us': uid}),
     win: games.count({"wid": uid}),
-    loss: games.count({"uids": uid, "s": { "$in": [ 30, 31, 35, 33 ] }, "wid": {"$ne": uid}}),
-    draw: games.count({"uids": uid, "s": { "$in": [34, 32] }}),
+    loss: games.count({"us": uid, "s": { "$in": [ 30, 31, 35, 33 ] }, "wid": {"$ne": uid}}),
+    draw: games.count({"us": uid, "s": { "$in": [34, 32] }}),
     winH: hintWid({"wid": uid, "p.ai":{$exists:false}}),
-    lossH: games.count({"uids": uid, "s": { "$in": [ 30, 31, 35, 33 ] }, "wid": {"$ne": uid}, "p.ai":{$exists:false}}),
-    drawH: games.count({"uids": uid, "s": { "$in": [34, 32] }, "p.ai":{$exists:false}}),
-    ai: games.count({"uids": uid, "p.ai":{$exists:true}}),
-    rated: games.count({"uids": uid, "ra":true}),
+    lossH: games.count({"us": uid, "s": { "$in": [ 30, 31, 35, 33 ] }, "wid": {"$ne": uid}, "p.ai":{$exists:false}}),
+    drawH: games.count({"us": uid, "s": { "$in": [34, 32] }, "p.ai":{$exists:false}}),
+    ai: games.count({"us": uid, "p.ai":{$exists:true}}),
+    rated: games.count({"us": uid, "ra":true}),
   };
   users.update({"_id": uid}, {"$set":{count: count}});
   ++it;
-  if (it % batchSize == 0) {
+  if (it % batchSize === 0) {
     var percent = Math.round((it / max) * 100);
     var dat2 = new Date().getTime() / 1000;
     var perSec = Math.round(batchSize / (dat2 - dat));
