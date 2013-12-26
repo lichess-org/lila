8cc4c46a64af10c46eabef6f68da06e10a033729
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-12-27 00:31:09 +0100

    improve autopair and lobby

--- modules/lobby/src/main/Lobby.scala
+++ modules/lobby/src/main/Lobby.scala
@@ -2,15 +2,17 @@ package lila.lobby
 
 import scala.concurrent.duration._
 
-import actorApi._
 import akka.actor._
 import akka.pattern.{ ask, pipe }
-import makeTimeout.short
 
+import actorApi._
 import lila.db.api._
+import lila.game.GameRepo
 import lila.hub.actorApi.GetUids
 import lila.memo.ExpireSetMemo
 import lila.socket.actorApi.Broom
+import org.joda.time.DateTime
+import makeTimeout.short
 
 private[lobby] final class Lobby(
     biter: Biter,
@@ -20,16 +22,18 @@ private[lobby] final class Lobby(
 
     case GetOpen ⇒ sender ! HookRepo.allOpen
 
-    case msg @ AddHook(hook) ⇒ {
+    case msg@AddHook(hook) ⇒ {
       HookRepo byUid hook.uid foreach remove
       hook.sid ?? { sid ⇒ HookRepo bySid sid foreach remove }
-      HookRepo findCompatible hook find { biter.canJoin(_, hook.user) } match {
+      findCompatible(hook) foreach {
         case Some(h) ⇒ self ! BiteHook(h.id, hook.uid, hook.user map (_.id))
-        case None ⇒ {
-          HookRepo save hook
-          socket ! msg
-        }
+        case None    ⇒ self ! SaveHook(msg)
       }
+    }
+
+    case SaveHook(msg) ⇒ {
+      HookRepo save msg.hook
+      socket ! msg
     }
 
     case CancelHook(uid) ⇒ {
@@ -38,12 +42,12 @@ private[lobby] final class Lobby(
 
     case BiteHook(hookId, uid, userId) ⇒ HookRepo byId hookId foreach { hook ⇒
       HookRepo byUid uid foreach remove
-      biter(hook, userId) map { f ⇒
-        socket ! f(uid)
-        remove(hook)
-      } recover {
-        case e: lila.common.LilaException ⇒ logwarn(e.getMessage)
-      } await 2.seconds
+      biter(hook, userId, uid) pipeTo self
+    }
+
+    case msg@JoinHook(_, hook, _, _) ⇒ {
+      socket ! msg
+      remove(hook)
     }
 
     case Broom ⇒ socket ? GetUids mapTo manifest[Iterable[String]] foreach { uids ⇒
@@ -56,6 +60,24 @@ private[lobby] final class Lobby(
     case RemoveHooks(hooks) ⇒ hooks foreach remove
 
     case Resync             ⇒ socket ! HookIds(HookRepo.list map (_.id))
+  }
+
+  private def findCompatible(hook: Hook): Fu[Option[Hook]] =
+    findCompatibleIn(hook, HookRepo findCompatible hook)
+
+  private def findCompatibleIn(hook: Hook, in: List[Hook]): Fu[Option[Hook]] = in match {
+    case Nil ⇒ fuccess(none)
+    case h :: rest ⇒ biter.canJoin(h, hook.user) >>& !{
+      (h.user |@| hook.user).tupled ?? {
+        case (u1, u2) ⇒
+          GameRepo.lastGameBetween(u1.id, u2.id, DateTime.now minusHours 1) map {
+            _ ?? (_.aborted)
+          }
+      }
+    } flatMap {
+      case true  ⇒ fuccess(h.some)
+      case false ⇒ findCompatibleIn(hook, rest)
+    }
   }
 
   private def remove(hook: Hook) = {
