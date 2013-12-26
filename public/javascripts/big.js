0bc5186732d72366151b4b2b3d6b1f5fa0fa36c2
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-12-27 00:53:51 +0100

    fix lobby filter reset

--- public/javascripts/big.js
+++ public/javascripts/big.js
@@ -2212,7 +2212,7 @@ var storage = {
                   values: values,
                   step: 50,
                   slide: change
-                }).change(change);
+                });
               });
             }
           });
