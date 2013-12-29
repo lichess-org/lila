10b20458efa022e359473afe071aa5568995282f
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-12-29 22:28:23 +0100

    remove bot members

--- modules/chat/src/main/ChatMember.scala
+++ modules/chat/src/main/ChatMember.scala
@@ -5,16 +5,23 @@ import play.api.libs.json.JsValue
 
 import lila.socket.JsChannel
 
-private[chat] sealed trait ChatMember[Message] {
-
-  val uid: String
-  val userId: String
-
-  def wants(line: Line): Boolean
-  def tell(msg: Message): Unit
+private[chat] final class ChatMember(
+    val uid: String,
+    val userId: String,
+    val troll: Boolean,
+    val channel: JsChannel) {
 
   private[chat] var head: ChatHead = ChatHead(Nil, none, Set.empty, none)
   private[chat] var blocks: Set[String] = Set.empty
+
+  def wants(line: Line) =
+    (troll || !line.troll) &&
+      (head.activeChanKeys contains line.chan.key) &&
+      !blocks(line.userId)
+
+  def tell(msg: JsValue) {
+    channel push msg
+  }
 
   def setHead(h: ChatHead) {
     head = h
@@ -45,33 +52,4 @@ private[chat] sealed trait ChatMember[Message] {
   def unBlock(u: String) {
     blocks = blocks - u
   }
-}
-
-private[chat] final class JsChatMember(
-    val uid: String,
-    val userId: String,
-    val troll: Boolean,
-    val channel: JsChannel) extends ChatMember[JsValue] {
-
-  def wants(line: Line) =
-    (troll || !line.troll) &&
-      (head.activeChanKeys contains line.chan.key) &&
-      !blocks(line.userId) &&
-      line.to.fold(true)(_ == userId)
-
-  def tell(msg: JsValue) {
-    channel push msg
-  }
-}
-
-private[chat] final class BotChatMember(
-    val uid: String,
-    actor: ActorRef) extends ChatMember[Line] {
-
-  val userId = uid
-  val troll = false
-
-  def wants(line: Line) = line.to == Some(userId)
-
-  def tell(msg: Line) { actor ! msg }
 }
