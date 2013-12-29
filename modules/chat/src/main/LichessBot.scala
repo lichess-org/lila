7bf86a135d587d0ceaa61aae8dc974954897938e
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-12-29 17:47:55 +0100

    chat help

--- modules/chat/src/main/LichessBot.scala
+++ modules/chat/src/main/LichessBot.scala
@@ -13,17 +13,40 @@ private[chat] final class LichessBot extends Actor {
 
   val chat = context.parent
 
+  val tutorialLines = """
+----------------------- lichess chat -----------------------
+The text input at the bottom can be used to enter commands.
+Commands start with a forward slash (/).
+For instance, try and send the message /help to see available commands.
+""".lines.toList filter (_.nonEmpty)
+
+  val helpLines = """
+--------------------------- help ---------------------------
+/help                   display this message
+/join <room>            join a room. Ex: /join en
+/tell <user> <message>  send a private message. Ex: /tell thibault lichess rocks!
+""".lines.toList filter (_.nonEmpty)
+
   def receive = {
     case line: Line ⇒ line.text match {
 
-      case text if text.startsWith("help") ⇒ chat ! Line.system(
-        chan = line.chan,
-        text = "You want help?",
-        to = line.userId.some)
+      case "help tutorial" ⇒ tutorialLines foreach { t ⇒
+        chat ! Line.system(
+          chan = line.chan,
+          text = t,
+          to = line.userId.some)
+      }
+
+      case "help" ⇒ helpLines foreach { t ⇒
+        chat ! Line.system(
+          chan = line.chan,
+          text = t,
+          to = line.userId.some)
+      }
 
       case text ⇒ chat ! Line.system(
         chan = line.chan,
-        text = s"Hi @${line.username}, I did not understand that.",
+        text = s"Command not found. Type /help for the list of available commands.",
         to = line.userId.some)
     }
   }
