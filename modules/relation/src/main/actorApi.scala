b4ed4949cbcc8edf029c1a566d263a7a4ef98f15
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-01-02 17:02:14 +0100

    remove blocking in relation actor

--- modules/relation/src/main/actorApi.scala
+++ modules/relation/src/main/actorApi.scala
@@ -1,5 +1,8 @@
 package lila.relation
 package actorApi
 
-private[relation] case object NotifyMovement
 private[relation] case object ReloadAllOnlineFriends
+private[relation] case class AllOnlineFriends(onlines: Map[ID, Username])
+
+private[relation] case object NotifyMovement
+private[relation] case class Movement(leaves: List[User], enters: List[User])
