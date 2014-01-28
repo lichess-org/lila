6b52b42131f3491f65097920fead36a8d59d154c
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-01-29 00:15:00 +0100

    complete chat backend

--- modules/chat/src/main/Api.scala
+++ modules/chat/src/main/Api.scala
@@ -1,42 +1,87 @@
 package lila.chat
 
-private[chat] final class Api {
+import chess.Color
+import reactivemongo.bson.BSONDocument
+
+import lila.db.Types.Coll
+import lila.user.{ User, UserRepo }
+
+private[chat] final class Api(
+    coll: Coll,
+    flood: lila.security.Flood,
+    maxLinesPerChat: Int,
+    netDomain: String) {
+
+  import Chat.userChatBSONHandler
 
   object userChat {
 
-    def write(chatId: String, username: String, text: String): Fu[Option[UserLine]] =
-      makeLine(chanName, userId, text) flatMap {
-        case None ⇒ {
-          logger.info(s"$userId @ $chanName : $text")
-          fuccess(none)
-        }
-        case Some(line) if flood.allowMessage(line.userId, line.text) ⇒ (line.chan match {
-          case UserChan(u1, u2) ⇒ relationApi.areFriends(u1, u2)
-          case _                ⇒ fuccess(true)
-        }) flatMap {
-          _ ?? (LineRepo insert line inject line.some)
-        }
-        case Some(line) ⇒ {
-          logger.info(s"Flood: $userId @ $chanName : $text")
-          fuccess(none)
-        }
+    def find(chatId: ChatId): Fu[Option[UserChat]] =
+      coll.find(BSONDocument("_id" -> chatId)).one[UserChat]
+
+    def write(chatId: ChatId, userId: String, text: String): Fu[Option[UserLine]] =
+      makeLine(userId, text) flatMap {
+        _ ?? { line ⇒ pushLine(chatId, line) inject line.some }
       }
 
-    def makeLine(chanKey: String, userId: String, t1: String): Fu[Option[Line]] =
-      getUser(userId) flatMap { user ⇒
-        val chanOption = Chan parse chanKey
-        (chanOption match {
-          case Some(TeamChan(teamId)) ⇒ getTeamIds(user.id) map (_ contains teamId)
-          case _                      ⇒ fuccess(true)
-        }) map {
-          _ ?? {
-            for {
-              chan ← chanOption
-              t2 ← Some(t1.trim take 200) filter (_.nonEmpty)
-              if !user.disabled
-            } yield Line.make(chan, user, Writer preprocessUserInput t2)
-          }
+    private[Api] def makeLine(userId: String, t1: String): Fu[Option[UserLine]] = UserRepo byId userId map {
+      _ flatMap { user ⇒
+        Writer cut t1 ifFalse user.disabled flatMap { t2 ⇒
+          flood.allowMessage(user.id, t2) option
+            UserLine(user.username, Writer preprocessUserInput t2, user.troll)
         }
       }
+    }
+  }
+
+  object mixedChat {
+
+    def find(chatId: ChatId): Fu[Option[MixedChat]] =
+      coll.find(BSONDocument("_id" -> chatId)).one[MixedChat]
+
+    def write(chatId: ChatId, user: Either[String, Color], text: String): Fu[Option[Line]] =
+      makeLine(chatId, user, text) flatMap {
+        _ ?? { line ⇒ pushLine(chatId, line) inject line.some }
+      }
+
+    private def makeLine(chatId: ChatId, user: Either[String, Color], t1: String): Fu[Option[Line]] = user match {
+      case Left(userId) ⇒ userChat.makeLine(userId, t1)
+      case Right(color) ⇒ fuccess {
+        Writer cut t1 flatMap { t2 ⇒
+          flood.allowMessage(s"$chatId/${color.letter}", t2) option
+            PlayerLine(color, Writer preprocessUserInput t2)
+        }
+      }
+    }
+  }
+
+  private def pushLine(chatId: ChatId, line: Line) = coll.update(
+    BSONDocument("_id" -> chatId),
+    BSONDocument("$push" -> BSONDocument(
+      "messages" -> BSONDocument(
+        "$each" -> line,
+        "$slice" -> maxLinesPerChat)
+    )),
+    upsert = true)
+
+  private object Writer {
+
+    import java.util.regex.Matcher.quoteReplacement
+    import org.apache.commons.lang3.StringEscapeUtils.escapeXml
+
+    def preprocessUserInput(in: String) = addLinks(delocalize(noPrivateUrl(escapeXml(in))))
+
+    def cut(text: String) = Some(text.trim take 200) filter (_.nonEmpty)
+    def addLinks(text: String) = urlRegex.replaceAllIn(text, m ⇒ {
+      val url = delocalize(quoteReplacement(m group 1))
+      "<a target='_blank' href='%s'>%s</a>".format(prependHttp(url), url)
+    })
+    def prependHttp(url: String): String = url startsWith "http" fold (url, "http://" + url)
+    val delocalize = new lila.common.String.Delocalizer(netDomain)
+    val domainRegex = netDomain.replace(".", """\.""")
+    val urlRegex = """(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""".r
+    val gameUrlRegex = (domainRegex + """/([\w-]{8})[\w-]{4}""").r
+    def noPrivateUrl(str: String): String =
+      gameUrlRegex.replaceAllIn(str, m ⇒ quoteReplacement(netDomain + "/" + (m group 1)))
   }
 }
