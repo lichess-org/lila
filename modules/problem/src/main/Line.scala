5f9a30b9a426f62d5dbb55509080cf9c6f3c2ce8
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-03 00:59:32 +0100

    improve puzzle integration

---
+++ modules/problem/src/main/Line.scala
@@ -0,0 +1,27 @@
+package lila.problem
+
+import play.api.libs.json._
+
+sealed trait Line
+
+case class Node(move: String, lines: Lines) extends Line
+
+case class End(move: String) extends Line
+
+object Line {
+
+  def toString(lines: Lines, level: Int = 0): String = {
+    val indent = "  " * level
+    lines map {
+      case End(move) => s"$indent$move end"
+      case Node(move, more) => s"$indent$move\n${toString(more, level + 1)}"
+    } mkString "\n"
+  }
+
+  def toJson(lines: Lines): JsObject = JsObject(lines map {
+    case End(move)        ⇒ move -> JsString("end")
+    case Node(move, more) ⇒ move -> toJson(more)
+  })
+
+  def toJsonString(lines: Lines) = Json stringify toJson(lines)
+}
