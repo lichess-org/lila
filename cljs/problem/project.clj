148c6b0f499ba032fcf1dbc334b6d2b037ada36e
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-03 00:18:16 +0100

    clojurescript problem solver POC

--- cljs/problem/project.clj
+++ cljs/problem/project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2156" :exclusions [org.apache.ant/ant]]
                  [prismatic/dommy "0.1.2"]
-                 ]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
   :plugins [[lein-cljsbuild "1.0.2"]]
   :cljsbuild {
               :builds {
@@ -14,7 +14,7 @@
                                         :optimizations :whitespace
                                         :pretty-print true}}
                        :prod {
-                             :source-paths ["src"]
-                             :compiler {:output-to "target/compiled.js"
-                                        :optimizations :advanced
-                                        :pretty-print false}}}})
+                              :source-paths ["src"]
+                              :compiler {:output-to "target/compiled.js"
+                                         :optimizations :advanced
+                                         :pretty-print false}}}})
--- cljs/problem/src/problem.cljs
+++ cljs/problem/src/problem.cljs
@@ -1,9 +1,50 @@
-(ns pcg
-  (:require [dommy.core :as dommy])
-  (:use-macros
-    [dommy.macros :only [node sel sel1]]))
+(ns lichess.problem
+  (:require [dommy.core :as dommy]
+            [cljs.core.async :refer [put! chan <!]])
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:use-macros [dommy.macros :only [node sel sel1]]))
 
 (defn log! [& args] (.log js/console (apply pr-str args)))
+(defn log-obj! [obj] (.log js/console obj))
 
-; Run the application!
-(dommy/listen! (sel1 'document) :click log!)
+(def static-domain (str "http://" (clojure.string/replace (.-domain js/document) #"^\w+" "static")))
+(def chessboard-elem (sel1 "#chessboard"))
+(def initial-fen (dommy/attr chessboard-elem "data-fen"))
+(def lines (js->clj (js/JSON.parse (dommy/attr chessboard-elem "data-lines"))))
+(def drop-chan (chan))
+(def chessboard (new js/ChessBoard "chessboard"
+                     (clj->js {:position initial-fen
+                               :orientation (dommy/attr chessboard-elem "data-color")
+                               :draggable true
+                               :dropOffBoard "snapback"
+                               :sparePieces false
+                               :pieceTheme (str static-domain "/assets/images/chessboard/{piece}.png")
+                               :moveSpeed 500
+                               :onDrop #(put! drop-chan %&)})))
+
+(defn split-move [move] (clojure.string/replace move #"^(\w{2})(\w{2})" "$1-$2"))
+
+(defn set-position! [fen] (.position chessboard fen true))
+
+(defn try-move [progress move]
+  (log! progress move)
+  (let [new-progress (conj progress move)
+        new-lines (get-in lines new-progress)]
+    (if new-lines [new-progress new-lines] false)))
+
+(defn ai-play! [branch]
+  (let [move (first (first branch))]
+    (.move chessboard (split-move move))
+    move))
+
+(defn start! []
+  (go (loop [progress [] fen initial-fen]
+        (let [[orig, target] (<! drop-chan) move (str orig target)]
+          (if-let [[new-progress new-lines] (try-move progress move)]
+            (do
+              (let [ai-move (ai-play! new-lines)]
+                (recur (conj new-progress ai-move) (.position chessboard "fen"))))
+            (do
+              (set-position! fen)
+              (recur progress fen)))))))
+(start!)
