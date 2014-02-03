c721fee9ee539cdbd9bb33553681c8b0bd14d932
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-03 01:14:10 +0100

    update user count denormalizer

--- bin/mongodb/userstats.js
+++ bin/mongodb/userstats.js
@@ -21,7 +21,7 @@ users.find().forEach(function(user) {
     winH: hintWid({"wid": uid, "p.ai":{$exists:false}}),
     lossH: games.count({"us": uid, "s": { "$in": [ 30, 31, 35, 33 ] }, "wid": {"$ne": uid}, "p.ai":{$exists:false}}),
     drawH: games.count({"us": uid, "s": { "$in": [34, 32] }, "p.ai":{$exists:false}}),
-    ai: games.count({"us": uid, "p.ai":{$exists:true}}),
+    ai: games.count({"us": uid, $or:[{'p0.ai':{$exists:true}},{'p1.ai':{$exists:true}}]}),
     rated: games.count({"us": uid, "ra":true}),
   };
   users.update({"_id": uid}, {"$set":{count: count}});
