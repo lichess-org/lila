5e6cb49a1d24f71e87a782b0446fadaa04ebd754
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-06 00:42:46 +0100

    produce a flat line from the tree

--- modules/puzzle/src/main/Line.scala
+++ modules/puzzle/src/main/Line.scala
@@ -13,16 +13,24 @@ object Line {
   def minDepth(lines: Lines): Int = {
     def walk(subs: Vector[(Lines, Int)]): Option[Int] = subs match {
       case Vector() ⇒ none
-      case (lines, depth) +: rest ⇒
-        lines match {
-          case Nil                  ⇒ walk(rest)
-          case Win(_) :: _          ⇒ depth.some
-          case Retry(_) :: siblings ⇒ walk(rest :+ (siblings -> depth))
-          case Node(_, children) :: siblings ⇒
-            walk(rest :+ (siblings -> depth) :+ (children -> (depth + 1)))
-        }
+      case (lines, depth) +: rest ⇒ lines match {
+        case Nil                  ⇒ walk(rest)
+        case Win(_) :: _          ⇒ depth.some
+        case Retry(_) :: siblings ⇒ walk(rest :+ (siblings -> depth))
+        case Node(_, children) :: siblings ⇒
+          walk(rest :+ (siblings -> depth) :+ (children -> (depth + 1)))
+      }
     }
     (1 + ~(walk(Vector(lines -> 1)))) / 2
+  }
+
+  def flatLine(line: Line): List[String] = line match {
+    case Win(move) ⇒ List(move)
+    case Retry(_)  ⇒ Nil
+    case Node(move, lines) ⇒ move :: ~(lines.find {
+      case Retry(_)            ⇒ false
+      case Win(_) | Node(_, _) ⇒ true
+    }).map(flatLine)
   }
 
   def toString(lines: Lines, level: Int = 0): String = {
