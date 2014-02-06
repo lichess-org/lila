291b6f327b9dcf1080c08186cc0c6055580e1907
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-07 00:49:16 +0100

    split clojurescript program

---
+++ cljs/puzzle/src/core.cljs
@@ -0,0 +1,32 @@
+(ns lichess.puzzle.core
+  (:require [jayq.core :as jq :refer [$]]
+            [cljs.core.async :as async :refer [chan <! >! alts! put! close! timeout]]))
+
+(defn log! [& args] (.log js/console (apply pr-str args)))
+(defn log-obj! [obj] (.log js/console obj))
+
+(def $puzzle ($ :#puzzle))
+(def $board ($ :#chessboard))
+(def lines (js->clj (js/JSON.parse (jq/attr $board :data-lines))))
+(def animation-delay 300)
+(def chess (new js/Chess))
+
+(defn await-in [ch value duration] (js/setTimeout #(put! ch value) duration) ch)
+
+(defn apply-move
+  ([orig, dest] (.move chess (clj->js {:from orig :to dest :promotion "q"})))
+  ([move] (let [[a, b, c, d] (seq move)] (apply-move (str a b) (str c d)))))
+
+(defn color-move! [move]
+  (let [[a b c d] (seq move) [orig dest] [(str a b) (str c d)]]
+    (jq/remove-class ($ [$board :.last]) :last)
+    (let [squares (clojure.string/join ", " (map #(str ".square-" %) [orig dest]))]
+      (jq/add-class ($ squares) :last))))
+
+(defn make-chessboard [config]
+  (let [static-domain (str "http://" (clojure.string/replace (.-domain js/document) #"^\w+" "static"))]
+    (new js/ChessBoard "chessboard"
+         (clj->js (merge {:orientation (jq/attr $board :data-color)
+                          :sparePieces false
+                          :pieceTheme (str static-domain "/assets/images/chessboard/{piece}.png")
+                          :moveSpeed animation-delay} config)))))
