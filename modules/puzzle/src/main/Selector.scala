d6daa3a35acf99c659a4c5c41debfbc8e91b0f57
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-08 12:43:41 +0100

    smarter selector for anon

--- modules/puzzle/src/main/Selector.scala
+++ modules/puzzle/src/main/Selector.scala
@@ -1,26 +1,33 @@
 package lila.puzzle
 
+import scala.concurrent.duration._
 import scala.util.Random
 
 import reactivemongo.api.QueryOpts
 import reactivemongo.bson.{ BSONDocument, BSONInteger }
+import reactivemongo.core.commands.Count
 
 import lila.db.Types.Coll
 import lila.user.User
 
 private[puzzle] final class Selector(puzzleColl: Coll) {
 
-  private val skipMax = 3000
+  private val count = lila.memo.AsyncCache.single[Int](
+    f = puzzleColl.db command Count(puzzleColl.name, none),
+    timeToLive = 1 hour)
+
   private val ratingToleranceStep = 100
   private val ratingToleranceMax = 1000
 
   def apply(me: Option[User]): Fu[Puzzle] = me match {
-    case None ⇒ puzzleColl.find(BSONDocument())
-      .projection(Puzzle.withoutUsers)
-      .options(QueryOpts(skipN = Random.nextInt(skipMax)))
-      .cursor[Puzzle]
-      .collect[List](1)
-      .map(_.headOption) flatten "Can't find a puzzle for anon player!"
+    case None ⇒ count(true) map (_ - 1) flatMap { skipMax ⇒
+      puzzleColl.find(BSONDocument())
+        .projection(Puzzle.withoutUsers)
+        .options(QueryOpts(skipN = Random.nextInt(skipMax)))
+        .cursor[Puzzle]
+        .collect[List](1)
+        .map(_.headOption) flatten "Can't find a puzzle for anon player!"
+    }
     case Some(user) ⇒ tryRange(user, 100)
   }
 
@@ -37,10 +44,10 @@ private[puzzle] final class Selector(puzzleColl: Coll) {
     .cursor[Puzzle]
     .collect[List](1)
     .map(_.headOption) flatMap {
-      case Some(puzzle) => fuccess(puzzle)
-      case None => if ((tolerance + ratingToleranceStep) <= ratingToleranceMax)
+      case Some(puzzle) ⇒ fuccess(puzzle)
+      case None ⇒ if ((tolerance + ratingToleranceStep) <= ratingToleranceMax)
         tryRange(user, tolerance + ratingToleranceStep)
-        else fufail(s"Can't find a puzzle for user $user!")
+      else fufail(s"Can't find a puzzle for user $user!")
     }
 
 }
