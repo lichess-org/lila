3afebd8bca7fedd60be74e1c0c4a5e04ea2c0063
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-18 09:56:32 +0100

    style tweaks

--- app/templating/StringHelper.scala
+++ app/templating/StringHelper.scala
@@ -15,7 +15,7 @@ trait StringHelper { self: NumberHelper =>
 
   val slugify = lila.common.String.slugify _
 
-  def shorten(text: String, length: Int, sep: String = " [...]"): String = {
+  def shorten(text: String, length: Int, sep: String = " [&#8230;]"): String = {
     val t = text.replace("\n", " ")
     if (t.size > (length + sep.size)) (t take length) ++ sep
     else t
