66867763dd719607828b70b4ee690d289a7b43b8
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-18 23:30:58 +0100

    fix cheat detection

--- modules/round/src/main/CheatDetector.scala
+++ modules/round/src/main/CheatDetector.scala
@@ -10,16 +10,14 @@ private[round] final class CheatDetector(reporter: ActorSelection) {
   def apply(game: Game): Fu[Option[Color]] = interesting(game) ?? {
     GameRepo findMirror game map {
       _ ?? { mirror =>
-        mirror.players find (p => p.userId ?? game.userIds.contains) match {
-          case Some(player) =>
-            play.api.Logger("cheat detector").info(s"${player.color} @ ${game.id} uses ${mirror.id}")
-            player.userId foreach { userId =>
-              reporter ! lila.hub.actorApi.report.Cheater(userId,
-                s"Cheat detected on ${gameUrl(game.id)}, using lichess AI: ${gameUrl(mirror.id)}")
-            }
-            Some(player.color)
-          case None => None
-        }
+        mirror.players map (p => p -> p.userId) collectFirst {
+          case (player, Some(userId)) => game.players find (_.userId == player.userId) map { cheater =>
+            play.api.Logger("cheat detector").info(s"${cheater.color} ($userId) @ ${game.id} uses ${mirror.id}")
+            reporter ! lila.hub.actorApi.report.Cheater(userId,
+              s"Cheat detected on ${gameUrl(game.id)}, using lichess AI: ${gameUrl(mirror.id)}")
+            cheater.color
+          }
+        } flatten
       }
     }
   }
