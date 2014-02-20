82292d01684f3f68d8a08ded4ebdcfb95aced79a
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-20 22:59:38 +0100

    improve profile page

--- modules/game/src/main/Namer.scala
+++ modules/game/src/main/Namer.scala
@@ -3,22 +3,26 @@ package lila.game
 import chess.Clock
 
 import lila.user.User
+import play.api.templates.Html
 
 object Namer {
 
-  def players(game: Game, withRatings: Boolean = true)(implicit getUsername: String => Fu[String]): Fu[(String, String)] =
+  def players(game: Game, withRatings: Boolean = true)(implicit getUsername: String => Fu[String]): Fu[(Html, Html)] =
     player(game.firstPlayer, withRatings) zip player(game.secondPlayer, withRatings)
 
-  def player(player: Player, withRating: Boolean = true)(implicit getUsername: String => Fu[String]): Fu[String] =
+  def player(player: Player, withRating: Boolean = true)(implicit getUsername: String => Fu[String]): Fu[Html] =
     player.aiLevel.fold(
       player.userId.fold(fuccess(User.anonymous)) { id =>
         getUsername(id) map { username =>
           withRating.fold(
-            s"$username (${player.rating getOrElse "?"})",
+            s"$username&nbsp;(${player.rating getOrElse "?"})",
             username)
         }
-      }) { level => fuccess("A.I. level " + level) }
+      }) { level =>
+        fuccess("A.I.&nbsp;level&nbsp;" + level)
+      } map Html.apply
 
-  def shortClock(clock: Clock): String = "%d + %d ".format(
-    clock.limitInMinutes, clock.increment)
+  def shortClock(clock: Clock): Html = Html {
+    s"${clock.limitInMinutes}&nbsp;+&nbsp;${clock.increment}"
+  }
 }
