d912a2e117809c5bca62ebb65a45ee1ada8d3f93
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-23 14:03:48 +0100

    promote TV embed

--- app/controllers/Tv.scala
+++ app/controllers/Tv.scala
@@ -25,13 +25,22 @@ object Tv extends LilaController {
     }
   }
 
-  def embed = Open { implicit ctx =>
+  def embed = Action { req =>
+    Ok {
+      val bg = get("bg", req) | "light"
+      val theme = get("theme", req) | "brown"
+      val url = s"""${req.domain + routes.Tv.frame}?bg=$bg&theme=$theme"""
+      s"""document.write("<iframe src='http://$url&embed=" + document.domain + "' class='lichess-tv-iframe' allowtransparency='true' frameBorder='0' style='width: 226px; height: 266px;' title='Lichess free online chess'></iframe>");"""
+    } as JAVASCRIPT withHeaders (CACHE_CONTROL -> "max-age=86400")
+  }
+
+  def frame = Action.async { req =>
     Env.game.featured.one map {
       case None => NotFound
       case Some(game) => Ok(views.html.tv.embed(
         game,
-        get("bg") | "light",
-        lila.pref.Theme(~get("theme")).cssClass
+        get("bg", req) | "light",
+        lila.pref.Theme(~get("theme", req)).cssClass
       ))
     }
   }
