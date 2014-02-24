c9e2a87f2d6b9051b186b0ea313c3c8877404030
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-24 20:33:53 +0100

    move gg analytics to common.js

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -77,3 +77,18 @@ function parseFen($elem) {
     html = pclasses = increment = pregex = fen = $this = 0;
   });
 }
+
+if (/.+\.lichess\.org/.test(document.domain)) {
+  //analytics
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-7935029-3']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'http://www.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+  })();
+}
