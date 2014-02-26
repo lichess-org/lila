789cda1df75fe0401101ed93b8736e080ead515f
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-26 22:19:46 +0100

    implement language detection

---
+++ modules/common/src/main/DetectLanguage.scala
@@ -0,0 +1,37 @@
+package lila.common
+
+import lila.common.PimpedJson._
+import lila.common.ws.WS
+import play.api.i18n.Lang
+import play.api.libs.json._
+
+// http://detectlanguage.com
+final class DetectLanguage(url: String, key: String) {
+
+  private case class Detection(
+    language: String,
+    confidence: Float,
+    isReliable: Boolean)
+
+  private implicit val DetectionReads = Json.reads[Detection]
+
+  def apply(message: String): Fu[Option[Lang]] =
+    WS.url(url).post(Map(
+      "key" -> Seq(key),
+      "q" -> Seq(message)
+    )) map { response =>
+      (response.json \ "data" \ "detections").as[List[Detection]]
+        .filter(_.isReliable)
+        .sortBy(-_.confidence)
+        .headOption map (_.language) flatMap Lang.get
+    }
+
+}
+
+object DetectLanguage {
+
+  import com.typesafe.config.Config
+  def apply(config: Config): DetectLanguage = new DetectLanguage(
+    url = config getString "api.url",
+    key = config getString "api.key")
+}
