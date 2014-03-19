20db252245e09fd017219925875724b3eff09457
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-03-20 00:41:03 +0100

    log offender name in hold alerts

--- modules/round/src/main/Round.scala
+++ modules/round/src/main/Round.scala
@@ -105,7 +105,9 @@ private[round] final class Round(
 
     case HoldAlert(playerId, mean, sd) => handle(playerId) { pov =>
       !pov.player.hasHoldAlert ?? {
-        play.api.Logger("hold").info(s"http://lichess.org/${pov.gameId}/${pov.color.name}#${pov.game.turns} mean: $mean SD: $sd")
+        play.api.Logger("hold").info(
+          s"http://lichess.org/${pov.gameId}/${pov.color.name}#${pov.game.turns} ${pov.player.userId | "anon"} mean: $mean SD: $sd"
+        )
         GameRepo.setHoldAlert(pov, mean, sd) inject List[Event]()
       }
     }
