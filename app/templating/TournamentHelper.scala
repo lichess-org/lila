e0f1b174563804e0354351189f4f6cb7780a4a3c
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-04-12 19:32:28 +0200

    change scheduled tournament icon color

--- app/templating/TournamentHelper.scala
+++ app/templating/TournamentHelper.scala
@@ -1,17 +1,17 @@
 package lila.app
 package templating
 
-import lila.user.User
+import controllers.routes
+import lila.api.Context
 import lila.tournament.Tournament
+import lila.user.User
 
 import play.api.libs.json.Json
+import play.api.templates.Html
 
-trait TournamentHelper {
+trait TournamentHelper { self: I18nHelper =>
 
-  def tournamentJsData(
-    tour: Tournament,
-    version: Int,
-    user: Option[User]) = {
+  def tournamentJsData(tour: Tournament, version: Int, user: Option[User]) = {
 
     val data = Json.obj(
       "tournament" -> Json.obj("id" -> tour.id),
@@ -20,5 +20,12 @@ trait TournamentHelper {
     Json stringify {
       user.fold(data) { u => data ++ Json.obj("username" -> u.username) }
     }
+  }
+
+  def tournamentLink(tour: Tournament)(implicit ctx: Context) = Html {
+    val cssClass = if (tour.scheduled) "is-gold" else ""
+    val url = routes.Tournament.show(tour.id)
+    val name = if (tour.scheduled) tour.name else trans.xTournament(tour.name)
+    s"""<a data-icon="g" class="$cssClass" href="$url">&nbsp;$name</a>"""
   }
 }
