333ea811fcedb0e5019a71b6139e7b4d70037147
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-04-20 18:54:55 +0200

    simplify move broadcaster

--- modules/round/src/main/MoveBroadcast.scala
+++ modules/round/src/main/MoveBroadcast.scala
@@ -13,20 +13,13 @@ private final class MoveBroadcast extends Actor {
     context.system.lilaBus.unsubscribe(self)
   }
 
-  private val format = Enumeratee.map[MoveEvent] { move =>
-    s"${move.gameId} ${move.ip}"
-  }
-
-  private val (enumerator, channel) =
-    play.api.libs.iteratee.Concurrent.broadcast[MoveEvent]
-
-  private val formattedEnumerator = enumerator &> format
+  private val (enumerator, channel) = Concurrent.broadcast[String]
 
   def receive = {
 
-    case MoveBroadcast.GetEnumerator => sender ! formattedEnumerator
+    case MoveBroadcast.GetEnumerator => sender ! enumerator
 
-    case move: MoveEvent             => channel push move
+    case move: MoveEvent             => channel push s"${move.gameId} ${move.ip}"
   }
 }
 
