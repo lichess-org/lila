714f536d0350a11149a96d1dbb5614ee9b4212ca
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-04-21 18:15:09 +0200

    upgrade clojurescript

--- cljs/puzzle/project.clj
+++ cljs/puzzle/project.clj
@@ -1,11 +1,11 @@
 (defproject pcg "1.0"
   :description "lichess.org puzzle solver"
   :source-paths ["src-clj"]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156" :exclusions [org.apache.ant/ant]]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2202" :exclusions [org.apache.ant/ant]]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [jayq "2.5.0"]]
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
   :cljsbuild {
               :builds {
                        :dev {
