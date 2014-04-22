be375e48ddcb6ebc068d618c4b037074dcf45df3
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-04-22 23:48:37 +0200

    simplify friend lookup

--- modules/relation/src/main/Cached.scala
+++ modules/relation/src/main/Cached.scala
@@ -8,15 +8,7 @@ private[relation] final class Cached {
   private[relation] val following = AsyncCache(RelationRepo.following, maxCapacity = 8000)
   private[relation] val blockers = AsyncCache(RelationRepo.blockers, maxCapacity = 8000)
   private[relation] val blocking = AsyncCache(RelationRepo.blocking, maxCapacity = 8000)
-  private[relation] val friends = AsyncCache(findFriends, maxCapacity = 80000)
   private[relation] val relation = AsyncCache(findRelation, maxCapacity = 80000)
-
-  private def findFriends(userId: String): Fu[Set[ID]] =
-    following(userId) flatMap { ids =>
-      (ids.toList map { id => following(id) map (_ contains userId) }).sequenceFu map { ids zip _ } map {
-        _.filter(_._2).map(_._1)
-      }
-    }
 
   private def findRelation(pair: (String, String)): Fu[Option[Relation]] = pair match {
     case (u1, u2) => following(u1) flatMap { f =>
@@ -27,7 +19,7 @@ private[relation] final class Cached {
   }
 
   private[relation] def invalidate(u1: ID, u2: ID): Funit =
-    (List(followers, following, blockers, blocking, friends) flatMap { cache =>
+    (List(followers, following, blockers, blocking) flatMap { cache =>
       List(u1, u2) map cache.remove
     }).sequenceFu.void >> relation.remove(u1, u2)
 }
