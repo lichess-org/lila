d07a82a7cb73b888e871bab50b73c8bcf0a88fda
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-04-27 23:10:52 +0200

    let players rest after the monthly bullet tournament

--- modules/tournament/src/main/Scheduler.scala
+++ modules/tournament/src/main/Scheduler.scala
@@ -11,10 +11,6 @@ private[tournament] final class Scheduler(api: TournamentApi) extends Actor {
   import Schedule.Freq._
   import Schedule.Speed._
 
-  private val bulletHour = 17
-  private val blitzHour = 18
-  private val slowHour = 20
-
   def receive = {
 
     case ScheduleNow => TournamentRepo.scheduled.map(_.map(_.schedule).flatten) foreach { dbScheds =>
@@ -28,15 +24,15 @@ private[tournament] final class Scheduler(api: TournamentApi) extends Actor {
       val nextHour = nextHourDate.getHourOfDay
 
       val scheds = List(
-        Schedule(Monthly, Bullet, at(lastFridayOfCurrentMonth, bulletHour)),
-        Schedule(Monthly, Blitz, at(lastFridayOfCurrentMonth, blitzHour)),
-        Schedule(Monthly, Slow, at(lastFridayOfCurrentMonth, slowHour)),
-        Schedule(Weekly, Bullet, at(nextFriday, bulletHour)),
-        Schedule(Weekly, Blitz, at(nextFriday, blitzHour)),
-        Schedule(Weekly, Slow, at(nextFriday, slowHour)),
-        Schedule(Daily, Bullet, at(today, bulletHour)),
-        Schedule(Daily, Blitz, at(today, blitzHour)),
-        Schedule(Daily, Slow, at(today, slowHour)),
+        Schedule(Monthly, Bullet, at(lastFridayOfCurrentMonth, 17)),
+        Schedule(Monthly, Blitz, at(lastFridayOfCurrentMonth, 18, 20)),
+        Schedule(Monthly, Slow, at(lastFridayOfCurrentMonth, 20, 30)),
+        Schedule(Weekly, Bullet, at(nextFriday, 17)),
+        Schedule(Weekly, Blitz, at(nextFriday, 18)),
+        Schedule(Weekly, Slow, at(nextFriday, 20)),
+        Schedule(Daily, Bullet, at(today, 17)),
+        Schedule(Daily, Blitz, at(today, 18)),
+        Schedule(Daily, Slow, at(today, 20)),
         Schedule(Hourly, Bullet, at(nextHourDate, nextHour)),
         Schedule(Hourly, Blitz, at(nextHourDate, nextHour, 30))
       ).foldLeft(List[Schedule]()) {
