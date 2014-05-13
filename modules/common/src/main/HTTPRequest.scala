aa0cd8f8bda66c08490245a2b07203a199f24836
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-05-13 10:39:09 +0200

    don't redirect ajax requests

--- modules/common/src/main/HTTPRequest.scala
+++ modules/common/src/main/HTTPRequest.scala
@@ -1,22 +1,22 @@
 package lila.common
 
-import play.api.mvc.RequestHeader
 import play.api.http.HeaderNames
+import play.api.mvc.RequestHeader
 
 object HTTPRequest {
 
-  def isXhr(req: RequestHeader): Boolean = 
+  def isXhr(req: RequestHeader): Boolean =
     (req.headers get "X-Requested-With") == Some("XMLHttpRequest")
 
-  def isSocket(req: RequestHeader): Boolean = 
+  def isSocket(req: RequestHeader): Boolean =
     (req.headers get HeaderNames.UPGRADE) == Some("websocket")
 
   def isSynchronousHttp(req: RequestHeader) = !isXhr(req) && !isSocket(req)
 
-  def fullUrl(req: RequestHeader): String = 
+  def fullUrl(req: RequestHeader): String =
     "http://" + req.host + req.uri
 
-  def userAgent(req: RequestHeader): Option[String] = 
+  def userAgent(req: RequestHeader): Option[String] =
     req.headers get HeaderNames.USER_AGENT
 
   def sid(req: RequestHeader): Option[String] =
--- modules/i18n/src/main/I18nRequestHandler.scala
+++ modules/i18n/src/main/I18nRequestHandler.scala
@@ -4,16 +4,14 @@ import play.api.i18n.Lang
 import play.api.mvc.Results.Redirect
 import play.api.mvc.{ Action, RequestHeader, Handler }
 
+import lila.common.HTTPRequest
+
 final class I18nRequestHandler(pool: I18nPool, protocol: String) {
 
   def apply(req: RequestHeader): Option[Handler] =
-    if (lila.common.HTTPRequest isSocket req) None
-    else pool.domainLang(req).isDefined.fold(
-      None,
-      Action {
-        Redirect(redirectUrl(req))
-      } some
-    )
+    (HTTPRequest.isSynchronousHttp(req) && !pool.domainLang(req).isDefined) option Action {
+      Redirect(redirectUrl(req))
+    }
 
   private def redirectUrl(req: RequestHeader) =
     protocol +
