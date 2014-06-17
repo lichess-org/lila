91f76f091111c4bd30f5a53715fa2fc84eef2131
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-06-17 23:34:12 +0200

    don't pair people right away if there are many playing

--- modules/pool/src/main/AutoPairing.scala
+++ modules/pool/src/main/AutoPairing.scala
@@ -13,26 +13,32 @@ private[pool] object AutoPairing {
     def inPoolRoom(p: Player) = userIds contains p.user.id
     def isPlaying(p: Player) = playingUserIds contains p.user.id
     val availablePlayers = pool.players filter inPoolRoom filterNot isPlaying
+    val nbAvailablePlayers = availablePlayers.size
+    val nbPlaying = pool.players count isPlaying
 
-    def basedOnRating = availablePlayers.sortBy(-_.rating) grouped 2
+    if (nbAvailablePlayers < (nbPlaying / 2)) Nil
+    else {
 
-    def basedOnWins = availablePlayers.map { player =>
-      player -> pool.pairings.foldLeft(Sheet(0, 0)) {
-        case (sheet, _) if sheet.games > 5                   => sheet
-        case (sheet, pairing) if (pairing wonBy player.id)   => sheet add 1
-        case (sheet, pairing) if (pairing drawnBy player.id) => sheet add 0
-        case (sheet, pairing) if (pairing drawnBy player.id) => sheet add -1
-        case (sheet, _)                                      => sheet
-      }
-    }.sortBy {
-      case (player, sheet) => (-sheet.average, -player.rating)
-    }.map(_._1) grouped 2
+      def basedOnRating = availablePlayers.sortBy(-_.rating) grouped 2
 
-    val pairs = basedOnWins
+      def basedOnWins = availablePlayers.map { player =>
+        player -> pool.pairings.foldLeft(Sheet(0, 0)) {
+          case (sheet, _) if sheet.games > 5                   => sheet
+          case (sheet, pairing) if (pairing wonBy player.id)   => sheet add 1
+          case (sheet, pairing) if (pairing drawnBy player.id) => sheet add 0
+          case (sheet, pairing) if (pairing drawnBy player.id) => sheet add -1
+          case (sheet, _)                                      => sheet
+        }
+      }.sortBy {
+        case (player, sheet) => (-sheet.average, -player.rating)
+      }.map(_._1) grouped 2
 
-    pairs.map { scala.util.Random.shuffle(_) }.map {
-      case List(p1, p2) => Pairing(p1.user.id, p2.user.id).some
-      case _            => none
-    }.toList.flatten
+      val pairs = basedOnWins
+
+      pairs.map { scala.util.Random.shuffle(_) }.map {
+        case List(p1, p2) => Pairing(p1.user.id, p2.user.id).some
+        case _            => none
+      }.toList.flatten
+    }
   }
 }
