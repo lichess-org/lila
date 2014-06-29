b2efad10b8210936a67b0ce77da714ef138f0299
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-06-29 21:13:15 +0200

    ridiculous micro-optimizations

--- app/actor/Renderer.scala
+++ app/actor/Renderer.scala
@@ -19,26 +19,33 @@ private[app] final class Renderer extends Actor {
       sender ! V.notification.view(id, from)(Html(body))
 
     case lila.tournament.actorApi.RemindTournament(tournament) =>
-      sender ! V.tournament.reminder(tournament)
+      sender ! spaceless(V.tournament.reminder(tournament))
 
     case lila.pool.actorApi.RemindPool(pool) =>
-      sender ! V.pool.reminder(pool)
+      sender ! spaceless(V.pool.reminder(pool))
 
     case lila.hub.actorApi.setup.RemindChallenge(gameId, from, _) =>
       val replyTo = sender
       (GameRepo game gameId) zip (UserRepo named from) onSuccess {
-        case (Some(game), Some(user)) => replyTo ! V.setup.challengeNotification(game, user)
+        case (Some(game), Some(user)) => replyTo ! spaceless(V.setup.challengeNotification(game, user))
       }
 
-    case lila.hub.actorApi.RemindDeployPre  => sender ! V.notification.deploy("pre")
-    case lila.hub.actorApi.RemindDeployPost => sender ! V.notification.deploy("post")
+    case lila.hub.actorApi.RemindDeployPre =>
+      sender ! spaceless(V.notification.deploy("pre"))
+    case lila.hub.actorApi.RemindDeployPost =>
+      sender ! spaceless(V.notification.deploy("post"))
 
     case lila.tournament.actorApi.TournamentTable(tours) =>
-      sender ! V.tournament.enterable(tours)
+      sender ! spaceless(V.tournament.enterable(tours))
 
     case lila.puzzle.RenderDaily(puzzle, fen, lastMove) =>
-      sender ! V.puzzle.daily(puzzle, fen, lastMove)
+      sender ! spaceless(V.puzzle.daily(puzzle, fen, lastMove))
 
     case lila.tv.StreamsOnAir(streams) => sender ! V.tv.streamsOnAir(streams)
+  }
+
+  private val spaceRegex = """\s{2,}""".r
+  private def spaceless(html: Html) = Html {
+    spaceRegex.replaceAllIn(html.body.replace("\\n", " "), " ")
   }
 }
