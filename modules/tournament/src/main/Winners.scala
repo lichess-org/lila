1a842a8e67d9744e399458138984b47f1daefdf4
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-08-02 23:54:49 +0200

    kick cheaters out of tournament leaderboard

--- modules/tournament/src/main/Winners.scala
+++ modules/tournament/src/main/Winners.scala
@@ -12,15 +12,21 @@ final class Winners(ttl: FiniteDuration) {
   import Schedule.Freq
   private def fetchScheduled(nb: Int): Fu[List[Winner]] =
     List(Freq.Monthly, Freq.Weekly, Freq.Daily).map { freq =>
-      TournamentRepo.lastFinishedScheduledByFreq(freq, 3) map toursToWinners
+      TournamentRepo.lastFinishedScheduledByFreq(freq, 3) flatMap toursToWinners
     }.sequenceFu map (_.flatten) flatMap { winners =>
       TournamentRepo.lastFinishedScheduledByFreq(
         Freq.Hourly, math.max(0, nb - winners.size)
-      ) map toursToWinners map (winners ::: _)
+      ) flatMap toursToWinners map (winners ::: _)
     }
 
-  private def toursToWinners(tours: List[Finished]) =
-    tours.flatMap { tour => tour.winner map { w => Winner(tour.id, tour.name, w.id) } }
+  private def toursToWinners(tours: List[Finished]): Fu[List[Winner]] =
+    tours.flatMap { tour =>
+      tour.winner map { w =>
+        Winner(tour.id, tour.name, w.id)
+      }
+  }.map { winner =>
+    UserRepo isEngine winner.userId map (!_ option winner)
+  }.sequenceFu map (_.flatten)
 
   def scheduled(nb: Int) = scheduledCache apply nb
 }
