1e01b7ca3d8f8c6d01408dcf4e82742a1b2a92af
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-09-20 18:14:43 +0200

    Revert "Revert "puzzle selector mongodb query strategy""
    This reverts commit ddf1e8cfb0db0b48a4dffeed29bea3a28fcabd07.

--- modules/puzzle/src/main/Selector.scala
+++ modules/puzzle/src/main/Selector.scala
@@ -4,13 +4,13 @@ import scala.concurrent.duration._
 import scala.util.Random
 
 import reactivemongo.api.QueryOpts
-import reactivemongo.bson.{ BSONDocument, BSONInteger }
+import reactivemongo.bson.{ BSONDocument, BSONInteger, BSONArray }
 import reactivemongo.core.commands.Count
 
 import lila.db.Types.Coll
 import lila.user.User
 
-private[puzzle] final class Selector(puzzleColl: Coll) {
+private[puzzle] final class Selector(puzzleColl: Coll, api: PuzzleApi) {
 
   private val anonMinRating = 30
   private val ratingToleranceStep = 160
@@ -33,28 +33,26 @@ private[puzzle] final class Selector(puzzleColl: Coll) {
   def apply(me: Option[User], difficulty: Int): Fu[Puzzle] = me match {
     case None => popularCount(true) map (_ - 1) flatMap { skipMax =>
       puzzleColl.find(popularSelector)
-        .projection(Puzzle.withoutUsers)
         .options(QueryOpts(skipN = Random nextInt skipMax))
         .one[Puzzle] flatten "Can't find a puzzle for anon player!"
     }
-    case Some(user) => tryRange(user, ratingToleranceStep, difficultyDecay(difficulty))
+    case Some(user) => api.attempt.playedIds(user) flatMap { ids =>
+      tryRange(user, ratingToleranceStep, difficultyDecay(difficulty), ids)
+    }
   }
 
-  private def tryRange(user: User, tolerance: Int, decay: Int): Fu[Puzzle] = puzzleColl.find(BSONDocument(
-    Puzzle.BSONFields.users -> BSONDocument("$ne" -> user.id),
-    Puzzle.BSONFields.rating -> BSONDocument(
-      "$gt" -> BSONInteger(user.perfs.puzzle.intRating - tolerance + decay),
-      "$lt" -> BSONInteger(user.perfs.puzzle.intRating + tolerance + decay)
-    )
-  ), BSONDocument(
-    Puzzle.BSONFields.users -> false
-  )).projection(Puzzle.withoutUsers)
-    .sort(BSONDocument(Puzzle.BSONFields.voteSum -> -1))
-    .hint(BSONDocument(Puzzle.BSONFields.voteSum -> -1, Puzzle.BSONFields.rating -> 1))
-    .one[Puzzle] flatMap {
-      case Some(puzzle) => fuccess(puzzle)
-      case None => if ((tolerance + ratingToleranceStep) <= ratingToleranceMax)
-        tryRange(user, tolerance + ratingToleranceStep, decay)
-      else fufail(s"Can't find a puzzle for user $user!")
-    }
+  private def tryRange(user: User, tolerance: Int, decay: Int, ids: BSONArray): Fu[Puzzle] =
+    puzzleColl.find(BSONDocument(
+      Puzzle.BSONFields.id -> BSONDocument("$nin" -> ids),
+      Puzzle.BSONFields.rating -> BSONDocument(
+        "$gt" -> BSONInteger(user.perfs.puzzle.intRating - tolerance + decay),
+        "$lt" -> BSONInteger(user.perfs.puzzle.intRating + tolerance + decay)
+      )
+    )).sort(BSONDocument(Puzzle.BSONFields.voteSum -> -1))
+      .one[Puzzle] flatMap {
+        case Some(puzzle) => fuccess(puzzle)
+        case None => if ((tolerance + ratingToleranceStep) <= ratingToleranceMax)
+          tryRange(user, tolerance + ratingToleranceStep, decay, ids)
+        else fufail(s"Can't find a puzzle for user $user!")
+      }
 }
