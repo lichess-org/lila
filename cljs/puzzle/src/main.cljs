294ebc89911dbe8c370492f2bd27c0d2b6e5309e
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-09-28 17:42:53 +0200

    remove cljs/ :'(

--- cljs/puzzle/src/main.cljs
+++
@@ -1,9 +0,0 @@
-(ns lichess.puzzle.main
-  (:require [lichess.puzzle.core :as core]
-            [lichess.puzzle.play :as play]
-            [lichess.puzzle.view :as view]
-            [jayq.core :as jq :refer [$]]))
-
-(defn playing? [] (jq/has-class ($ :#puzzle) "playing"))
-
-(if (playing?) (play/run!) (view/run! []))
