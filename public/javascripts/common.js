8675449db2a930b1b5605752cc6c47e9d7c77cec
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-10-09 22:56:32 +0200

    reuse chessground instances for mini boards, allowing animations

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -6,11 +6,14 @@ function parseFen($elem) {
     var $this = $(this).removeClass('parse_fen');
     var lm = $this.data('lastmove');
     var lastMove = lm ? [lm[0] + lm[1], lm[2] + lm[3]] : [];
-
-    Chessground($this[0], {
+    var color = $this.data('color');
+    var ground = $this.data('chessground');
+    var config = {
       fen: $this.data('fen'),
       lastMove: lm ? [lm[0] + lm[1], lm[2] + lm[3]] : [],
-      orientation: $this.data('color') || "white"
-    });
+    };
+    if (color) config.orientation = color;
+    if (ground) ground.set(config);
+    else $this.data('chessground', Chessground($this[0], config));
   });
 }
