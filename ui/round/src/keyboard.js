4c9c8d9c0d1fb5f7473c1e5a7432792d4046a5aa
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-10-23 02:17:03 +0200

    fix keyboard controls

--- ui/round/src/keyboard.js
+++ ui/round/src/keyboard.js
@@ -1,13 +1,17 @@
 var k = require('mousetrap');
 
+function replayPly(ctrl) {
+  return ctrl.replay.active ? ctrl.replay.ply : ctrl.data.game.moves.length;
+}
+
 module.exports = {
   init: function(ctrl) {
     k.bind(['left', 'h'], function() {
-      ctrl.replay.jump(ctrl.replay.ply - 1);
+      ctrl.replay.jump(replayPly(ctrl) - 1);
       m.redraw();
     });
     k.bind(['right', 'l'], function() {
-      ctrl.replay.jump(ctrl.replay.ply + 1);
+      ctrl.replay.jump(replayPly(ctrl) + 1);
       m.redraw();
     });
     k.bind(['up', 'j'], function() {
