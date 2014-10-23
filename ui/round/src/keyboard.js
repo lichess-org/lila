3d557d89b9d20471ef1b44c991ccd1966a1ea638
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-10-23 02:43:18 +0200

    prevent default in replay keyboard shortcuts

--- ui/round/src/keyboard.js
+++ ui/round/src/keyboard.js
@@ -4,23 +4,35 @@ function replayPly(ctrl) {
   return ctrl.replay.active ? ctrl.replay.ply : ctrl.data.game.moves.length;
 }
 
+function preventing(f) {
+  return function(e) {
+    if (e.preventDefault) {
+      e.preventDefault();
+    } else {
+      // internet explorer
+      e.returnValue = false;
+    }
+    f();
+  };
+}
+
 module.exports = {
   init: function(ctrl) {
-    k.bind(['left', 'h'], function() {
+    k.bind(['left', 'h'], preventing(function() {
       ctrl.replay.jump(replayPly(ctrl) - 1);
       m.redraw();
-    });
-    k.bind(['right', 'l'], function() {
+    }));
+    k.bind(['right', 'l'], preventing(function() {
       ctrl.replay.jump(replayPly(ctrl) + 1);
       m.redraw();
-    });
-    k.bind(['up', 'j'], function() {
+    }));
+    k.bind(['up', 'j'], preventing(function() {
       ctrl.replay.jump(1);
       m.redraw();
-    });
-    k.bind(['down', 'k'], function() {
+    }));
+    k.bind(['down', 'k'], preventing(function() {
       ctrl.replay.jump(ctrl.data.game.moves.length);
       m.redraw();
-    });
+    }));
   }
 };
