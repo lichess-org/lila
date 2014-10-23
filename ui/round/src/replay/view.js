1c9e8e94f5cdf48ecb966c94578092706f8ca95a
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-10-23 17:12:44 +0200

    show digit result in replay box

--- ui/round/src/replay/view.js
+++ ui/round/src/replay/view.js
@@ -1,11 +1,12 @@
 var partial = require('chessground').util.partial;
 var classSet = require('chessground').util.classSet;
+var status = require('../status');
 
-function renderTd(move, klass, ply, curPly) {
+function renderTd(move, ply, curPly) {
   return move ? {
     tag: 'td',
     attrs: {
-      class: klass + (ply === curPly ? ' active' : ''),
+      class: 'move' + (ply === curPly ? ' active' : ''),
       'data-ply': ply
     },
     children: move
@@ -16,6 +17,29 @@ function renderTable(ctrl, curPly) {
   var moves = ctrl.root.data.game.moves;
   var pairs = [];
   for (var i = 0; i < moves.length; i += 2) pairs.push([moves[i], moves[i + 1]]);
+  var result;
+  if (status.finished(ctrl.root.data)) switch (ctrl.root.data.game.winner) {
+    case 'white':
+      result = '1-0';
+      break;
+    case 'black':
+      result = '0-1';
+      break;
+    default:
+      result = '½-½';
+  }
+  var trs = pairs.map(function(pair, i) {
+    return m('tr', [
+      m('td', i + 1),
+      renderTd(pair[0], 2 * i + 1, curPly),
+      renderTd(pair[1], 2 * i + 2, curPly)
+    ]);
+  });
+  if (result) {
+    result = m('td.result', result);
+    if (moves.length % 2 === 0) trs.push(m('tr', [m('td'), result]));
+    else trs[trs.length - 1].children[2] = result;
+  }
   return m('table',
     m('tbody', {
         onclick: function(e) {
@@ -23,12 +47,7 @@ function renderTable(ctrl, curPly) {
           if (ply) ctrl.jump(parseInt(ply));
         }
       },
-      pairs.map(function(pair, i) {
-        return m('tr',
-          m('td', i + 1),
-          renderTd(pair[0], 'move', 2 * i + 1, curPly),
-          renderTd(pair[1], 'move', 2 * i + 2, curPly));
-      })));
+      trs));
 }
 
 function renderButtons(ctrl, curPly) {
