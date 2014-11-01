28f3764e9bf5eafc416971b9c7a386d27ac4ab32
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-11-01 13:13:17 +0100

    log exports

--- modules/game/src/main/PngExport.scala
+++ modules/game/src/main/PngExport.scala
@@ -6,7 +6,7 @@ import scala.sys.process._
 
 object PngExport {
 
-  private val logger = ProcessLogger(_ => (), _ => ())
+  private val logger = ProcessLogger(_ => (), println)
 
   def apply(execPath: String)(game: Game)(out: OutputStream) {
     val fen = (Forsyth >> game.toChess).split(' ').head
