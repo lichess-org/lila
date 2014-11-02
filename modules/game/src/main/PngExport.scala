c38c3162d0d0c402f0bebe85c216b9321232e609
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-11-02 13:12:26 +0100

    better png export logging

--- modules/game/src/main/PngExport.scala
+++ modules/game/src/main/PngExport.scala
@@ -6,17 +6,17 @@ import scala.sys.process._
 
 object PngExport {
 
-  private def log(n: String)(msg: String) {
-    println(s"$n: $msg")
-  }
-
-  private val logger = ProcessLogger(log("png out"), log("png err"))
+  private val logger = ProcessLogger(
+    x => play.api.Logger("png").info(x),
+    x => play.api.Logger("png").error(x))
 
   def apply(execPath: String)(game: Game)(out: OutputStream) {
     val fen = (Forsyth >> game.toChess).split(' ').head
     val color = game.firstColor.letter.toString
     val lastMove = ~game.castleLastMoveTime.lastMoveString
-    val exec = Process(Seq("php", "board-creator.php", fen, color, lastMove), new File(execPath))
+    val parts = Seq("php", "board-creator.php", fen, color, lastMove)
+    val exec = Process(parts, new File(execPath))
+    play.api.Logger("png").warn(parts mkString " ")
     exec #> out ! logger
   }
 }
