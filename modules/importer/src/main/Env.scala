cc45d2cb0f3967a8d8e08b8347f01fa7ee226efe
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-11-18 21:44:14 +0100

    non-blocking import delay

--- modules/importer/src/main/Env.scala
+++ modules/importer/src/main/Env.scala
@@ -6,13 +6,14 @@ import lila.common.PimpedConfig._
 
 final class Env(
     config: Config,
+    scheduler: akka.actor.Scheduler,
     roundMap: akka.actor.ActorRef) {
 
   private val Delay = config duration "delay"
 
   lazy val forms = new DataForm
 
-  lazy val importer = new Importer(roundMap, Delay)
+  lazy val importer = new Importer(roundMap, Delay, scheduler)
 
   lazy val live = new Live(roundMap)
 }
@@ -21,5 +22,6 @@ object Env {
 
   lazy val current = "[boot] importer" describes new Env(
     config = lila.common.PlayApp loadConfig "importer",
+    scheduler = lila.common.PlayApp.system.scheduler,
     roundMap = lila.round.Env.current.roundMap)
 }
