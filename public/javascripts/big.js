7b99f658d378ab90c4a0eac1040df3803b231255
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-11-25 23:20:46 +0100

    Revert "Restyle messages drop-down."
    This reverts commit 3a8a2f132551f96cea9dd86efae314057bb84b54.

--- public/javascripts/big.js
+++ public/javascripts/big.js
@@ -319,22 +319,26 @@ var storage = {
     setTimeout(userPowertips, 600);
     $('body').on('lichess.content_loaded', userPowertips);
 
-    $('#message_notifications_tag').on('click', function() {
-      $.ajax({
-        url: $(this).data('href'),
-        success: function(html) {
-          console.log(html);
-          $('#message_notifications_display').html(html).addClass('messages').find('a.mark_as_read').click(function() {
-            $.ajax({
-              url: $(this).attr('href'),
-              method: 'post'
+    $('#top a.message').powerTip({
+      placement: 'sw',
+      mouseOnToPopup: true,
+      closeDelay: 200
+    }).on({
+      powerTipPreRender: function() {
+        $.ajax({
+          url: $(this).data('href'),
+          success: function(html) {
+            $('#powerTip').html(html).addClass('messages').find('a.mark_as_read').click(function() {
+              $.ajax({
+                url: $(this).attr('href'),
+                method: 'post'
+              });
+              return false;
             });
-            $('#message_notifications_parent').toggleClass("shown", false);
-            return false;
-          });
-        }
-      });
-    });
+          }
+        });
+      }
+    }).data('powertip', ' ');
 
     function setMoment() {
       $("time.moment").removeClass('moment').each(function() {
