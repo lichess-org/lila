871eae0cc6d491555bb6749bd213f9fd43c33cbe
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-11-30 16:47:06 +0100

    log maintenance events

--- modules/round/src/main/Titivate.scala
+++ modules/round/src/main/Titivate.scala
@@ -26,32 +26,47 @@ private[round] final class Titivate(
 
   def delay(f: => Unit): Funit = akka.pattern.after(50 millis, scheduler)(Future(f))
 
+  def log(msg: String) {
+    play.api.Logger.info(s"[titivate] $msg")
+  }
+
   def receive = {
 
-    case Schedule => scheduler.scheduleOnce(10 minutes)(self, Run)
+    case Schedule =>
+      scheduler.scheduleOnce(10 seconds, self, Run)
 
-    case Run => $enumerate.over[Game]($query(Query.checkable), 5000) { game =>
-      if (game.finished || game.isPgnImport) GameRepo unsetCheckAt game
-      else if (game.outoftimePlayer.isDefined) delay {
-        roundMap ! Tell(game.id, Outoftime)
+    case Run =>
+      log("run")
+      $enumerate.over[Game]($query(Query.checkable), 5000) { game =>
+        if (game.finished || game.isPgnImport) {
+          log(s"${game.id} unset")
+          GameRepo unsetCheckAt game
+        }
+        else if (game.outoftimePlayer.isDefined) delay {
+          log(s"${game.id} outoftime")
+          roundMap ! Tell(game.id, Outoftime)
+        }
+        else if (game.abandoned) delay {
+          log(s"${game.id} abandon")
+          roundMap ! Tell(game.id, Abandon)
+        }
+        else if (game.unplayed) {
+          log(s"${game.id} unplayed")
+          bookmark ! lila.hub.actorApi.bookmark.Remove(game.id)
+          GameRepo remove game.id
+        }
+        else if (game.hasClock) {
+          val hours = game.started.fold(1, 6)
+          log(s"${game.id} reschedule clock in $hours hours")
+          GameRepo.setCheckAt(game, DateTime.now plusHours hours)
+        }
+        else {
+          val days = game.daysPerTurn | 3
+          log(s"${game.id} reschedule slow in $days days")
+          GameRepo.setCheckAt(game, DateTime.now plusDays days)
+        }
+      }.void andThenAnyway {
+        self ! Schedule
       }
-      else if (game.abandoned) delay {
-        roundMap ! Tell(game.id, Abandon)
-      }
-      else if (game.unplayed) {
-        bookmark ! lila.hub.actorApi.bookmark.Remove(game.id)
-        GameRepo remove game.id
-      }
-      else if (game.hasClock) {
-        val hours = game.started.fold(1, 6)
-        GameRepo.setCheckAt(game, DateTime.now plusHours hours)
-      }
-      else {
-        val days = game.daysPerTurn | 3
-        GameRepo.setCheckAt(game, DateTime.now plusDays days)
-      }
-    }.void andThenAnyway {
-      self ! Schedule
-    }
   }
 }
