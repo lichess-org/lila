1c615a9c4ae0acba0153bd71714f9e7ed2e7de17
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-12-03 02:28:41 +0100

    remove all game maintenance logging

--- modules/round/src/main/Titivate.scala
+++ modules/round/src/main/Titivate.scala
@@ -28,51 +28,37 @@ private[round] final class Titivate(
   def delayF(f: => Funit): Funit = akka.pattern.after(delayDuration, scheduler)(f)
   def delay(f: => Unit): Funit = akka.pattern.after(delayDuration, scheduler)(Future(f))
 
-  def log(msg: String) {
-    // loginfo(s"[titivate] $msg")
-  }
-
   def receive = {
 
     case Schedule =>
       scheduler.scheduleOnce(30 seconds, self, Run)
 
     case Run =>
-      log("run")
-      var nb = 0
       $enumerate.over[Game]($query(Query.checkable), 5000) { game =>
-        nb = nb + 1
-        if (game.finished || game.isPgnImport) {
-          log(s"$nb ${game.id} unset")
+        if (game.finished || game.isPgnImport) delayF {
           GameRepo unsetCheckAt game
         }
         else if (game.outoftimePlayer.isDefined) delay {
-          log(s"$nb ${game.id} outoftime")
           roundMap ! Tell(game.id, Outoftime)
         }
         else if (game.abandoned) delay {
-          log(s"$nb ${game.id} abandon")
           roundMap ! Tell(game.id, Abandon)
         }
         else if (game.unplayed) delayF {
-          log(s"$nb ${game.id} unplayed")
           bookmark ! lila.hub.actorApi.bookmark.Remove(game.id)
           GameRepo remove game.id
         }
         else game.clock match {
           case Some(clock) if clock.isRunning => delayF {
             val minutes = (clock.estimateTotalTime / 60).toInt
-            log(s"$nb ${game.id} reschedule clock in $minutes minutes")
             GameRepo.setCheckAt(game, DateTime.now plusMinutes minutes)
           }
           case Some(clock) => delayF {
             val hours = Game.unplayedHours
-            log(s"$nb ${game.id} reschedule clock in $hours hours")
             GameRepo.setCheckAt(game, DateTime.now plusHours hours)
           }
           case None => delayF {
             val days = game.daysPerTurn | Game.abandonedDays
-            log(s"$nb ${game.id} reschedule slow in $days days")
             GameRepo.setCheckAt(game, DateTime.now plusDays days)
           }
         }
