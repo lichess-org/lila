5bdbc377d96c80c4f709190f1ca8eb7f37af32d6
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-12-25 14:18:48 +0100

    tweak antichess JS warning

--- public/javascripts/big.js
+++ public/javascripts/big.js
@@ -1746,7 +1746,7 @@ var storage = {
       '960': "This is a Chess960 game!\n\nThe starting position of the pieces on the players' home ranks is randomized.\nRead more: http://wikipedia.org/wiki/Chess960\n\nDo you want to play Chess960?",
       'KotH': "This is a King of the Hill game!\n\nThe game can be won by bringing the king to the center.\nRead more: http://lichess.org/king-of-the-hill",
       '3+': "This is a Three-check game!\n\nThe game can be won by checking the opponent 3 times.\nRead more: http://en.wikipedia.org/wiki/Three-check_chess",
-      "antichess": "This is a antichess chess game!\n\n If can take a piece, you must. The game can be won by losing all your pieces."
+      "antichess": "This is an antichess chess game!\n\n If you can take a piece, you must. The game can be won by losing all your pieces."
     };
 
     function confirmVariant(variant) {
