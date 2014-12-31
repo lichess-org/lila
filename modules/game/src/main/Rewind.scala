ee69cdf7c0895dd53de075fc4bbcb4cd1e45304a
Gordon Martin <gordonhughmartin@gmail.com>
2014-12-31 16:05:46 +0000

    Fix for case where doing a takeback in an antichess game results in a checkable king because the game is rewound to be standard.

--- modules/game/src/main/Rewind.scala
+++ modules/game/src/main/Rewind.scala
@@ -4,13 +4,17 @@ import chess.format.{ pgn => chessPgn }
 
 object Rewind {
 
+  private def createTags(fen: Option[String], game: Game) = {
+    val variantTag = Some(chessPgn.Tag(_.Variant, game.variant.name))
+    val fenTag = fen map (fenString => chessPgn.Tag(_.FEN, fenString))
+
+    List(variantTag, fenTag).flatten
+  }
+
   def apply(game: Game, initialFen: Option[String]): Valid[Progress] = chessPgn.Reader.movesWithSans(
     moveStrs = game.pgnMoves,
     op = sans => sans.isEmpty.fold(sans, sans.init),
-    tags = initialFen.??(fen => List(
-      chessPgn.Tag(_.FEN, fen),
-      chessPgn.Tag(_.Variant, game.variant.name)
-    ))) map { replay =>
+    tags = createTags(initialFen, game)) map { replay =>
       val rewindedGame = replay.state
       val rewindedHistory = rewindedGame.board.history
       val rewindedSituation = rewindedGame.situation
