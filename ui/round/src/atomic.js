a1d7c391cb6f9c1856923b037ae86bce313cfe8c
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-01-02 17:02:43 +0100

    fix atomic UI explosions

--- ui/round/src/atomic.js
+++ ui/round/src/atomic.js
@@ -1,15 +1,20 @@
 var util = require('chessground').util;
 
 function capture(ctrl, key) {
-  var ps = [];
-  var pos = util.key2pos(key);
+  var exploding = [];
+  var diff = {};
+  var orig = util.key2pos(key);
   for (var x = -1; x < 2; x++) {
     for (var y = -1; y < 2; y++) {
-      var p = util.pos2key([pos[0] + x, pos[1] + y]);
-      if (p) ps.push(p);
+      var key = util.pos2key([orig[0] + x, orig[1] + y]);
+      if (key) {
+        exploding.push(key);
+        if (ctrl.chessground.data.pieces[key] && ctrl.chessground.data.pieces[key].role !== 'pawn') diff[key] = null;
+      }
     }
   }
-  ctrl.chessground.explode(ps);
+  ctrl.chessground.setPieces(diff);
+  ctrl.chessground.explode(exploding);
 };
 
 module.exports = {
