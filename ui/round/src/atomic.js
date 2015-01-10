7312304f055add8c1d3f266488314b56ba1a70de
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-01-10 16:08:13 +0100

    atomic ground zero always blow up

--- ui/round/src/atomic.js
+++ ui/round/src/atomic.js
@@ -6,10 +6,12 @@ function capture(ctrl, key) {
   var orig = util.key2pos(key);
   for (var x = -1; x < 2; x++) {
     for (var y = -1; y < 2; y++) {
-      var key = util.pos2key([orig[0] + x, orig[1] + y]);
-      if (key) {
-        exploding.push(key);
-        if (ctrl.chessground.data.pieces[key] && ctrl.chessground.data.pieces[key].role !== 'pawn') diff[key] = null;
+      var k = util.pos2key([orig[0] + x, orig[1] + y]);
+      if (k) {
+        exploding.push(k);
+        var explodes = ctrl.chessground.data.pieces[k] && (
+          k === key || ctrl.chessground.data.pieces[k].role !== 'pawn')
+        if (explodes) diff[k] = null;
       }
     }
   }
