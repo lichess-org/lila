649cde92ef13ebc3fe29118458e92e5a785d0d61
clarkerubber <jamesjosephclarke@gmail.com>
2015-01-26 22:20:12 +1100

    Use BS SfAccuracies and lower tolerance
    Essential increase the amount of similarity that two games must achieve
    to be considered similar. Lower false positive rate.

--- modules/evaluation/src/main/Group.scala
+++ modules/evaluation/src/main/Group.scala
@@ -66,14 +66,18 @@ case class GameGroup(analysed: Analysed, color: Color, assessment: Option[Int] =
     val thisMt: List[Int] = skip(this.analysed.game.moveTimes.toList, {if (this.color == Color.White) 0 else 1})
     val thatMt: List[Int] = skip(that.analysed.game.moveTimes.toList, {if (that.color == Color.White) 0 else 1})
 
-    listToListSimilarity(thisMt, thatMt, 0.3)
+    listToListSimilarity(thisMt, thatMt, 0.8)
   }
 
-  def compareSfAccuracies (that: GameGroup): Similarity = listToListSimilarity(
+  def compareSfAccuracies (that: GameGroup): (Similarity, Similarity) = (listToListSimilarity(
       Accuracy.diffsList(Pov(this.analysed.game, this.color), this.analysed.analysis),
       Accuracy.diffsList(Pov(that.analysed.game, that.color), that.analysed.analysis),
-      0.7
-    )
+      0.8
+    ), listToListSimilarity(
+      Accuracy.diffsList(Pov(this.analysed.game, !this.color), this.analysed.analysis),
+      Accuracy.diffsList(Pov(that.analysed.game, !that.color), that.analysed.analysis),
+      0.8
+    ))
 
   def compareBlurRates (that: GameGroup): Similarity = pointToPointSimilarity(
     (200 * this.analysed.game.player(this.color).blurs / this.analysed.game.turns).toInt,
@@ -88,9 +92,12 @@ case class GameGroup(analysed: Analysed, color: Color, assessment: Option[Int] =
 
   def similarityTo (that: GameGroup): MatchAndSig = {
     // Calls compare functions to determine how similar `this` and `that` are to each other
+    val sfComparison = compareSfAccuracies(that)
+
     val similarities = NonEmptyList(
       compareMoveTimes(that),
-      compareSfAccuracies(that),
+      sfComparison._1,
+      sfComparison._2,
       compareBlurRates(that),
       compareHoldAlerts(that)
     )
