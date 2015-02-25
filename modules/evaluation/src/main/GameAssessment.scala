145f2a9dd59f2cd4508591b835509214bb6e70f0
clarkerubber <jamesjosephclarke@gmail.com>
2015-02-25 19:44:19 +1100

    Improve info display for mod zone

--- modules/evaluation/src/main/GameAssessment.scala
+++ modules/evaluation/src/main/GameAssessment.scala
@@ -179,16 +179,18 @@ object Display {
 
   def stockfishSig(pa: PlayerAssessment): Int =
     (pa.flags.suspiciousErrorRate, pa.flags.alwaysHasAdvantage) match {
-      case (true, _) => 5
-      case (_, true) => 4
-      case _         => 1
+      case (true, true)  => 5
+      case (true, false) => 4
+      case (false, true) => 3
+      case _             => 1
     }
 
   def moveTimeSig(pa: PlayerAssessment): Int =
     (pa.flags.consistentMoveTimes, pa.flags.noFastMoves) match {
-      case (true, _) => 5
-      case (_, true) => 4
-      case _         => 1
+      case (true, true)  => 5
+      case (true, false) => 4
+      case (false, true) => 3
+      case _             => 1
     }
 
   def blurSig(pa: PlayerAssessment): Int =
