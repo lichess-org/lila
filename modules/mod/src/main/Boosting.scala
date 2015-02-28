43055d7d77f3fb8b030368aad0a32ba0fa125d46
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-02-28 11:02:02 +0100

    check boosting for more variants, fix CS

--- modules/mod/src/main/Boosting.scala
+++ modules/mod/src/main/Boosting.scala
@@ -1,23 +1,28 @@
 package lila.mod
 
+import chess.Color
+import chess.variant
 import lila.db.Types.Coll
 import lila.game.Game
 import lila.user.User
-import chess.variant.Variant
-import chess.Color
 
 import reactivemongo.bson._
 import scala.concurrent._
 
-
 final class BoostingApi(
-  modApi: ModApi, 
-  collBoosting: Coll, 
-  nbGamesToMark: Int,
-  ratioGamesToMark: Double) {
+    modApi: ModApi,
+    collBoosting: Coll,
+    nbGamesToMark: Int,
+    ratioGamesToMark: Double) {
   import BoostingApi._
 
   private implicit val boostingRecordBSONHandler = Macros.handler[BoostingRecord]
+
+  private val variants = Set(
+    variant.Standard,
+    variant.Chess960,
+    variant.KingOfTheHill,
+    variant.ThreeCheck)
 
   def getBoostingRecord(id: String): Fu[Option[BoostingRecord]] =
     collBoosting.find(BSONDocument("_id" -> id))
@@ -30,7 +35,8 @@ final class BoostingApi(
     if (record.games >= nbGamesToMark &&
       record.games >= (winner.count.rated * ratioGamesToMark)) {
       modApi.autoBooster(winner.username)
-    } else {
+    }
+    else {
       funit
     }
   }
@@ -38,39 +44,40 @@ final class BoostingApi(
   def boostingId(winner: User, loser: User): String = winner.id + "/" + loser.id
 
   def check(game: Game, whiteUser: User, blackUser: User): Funit = {
-    if (game.rated 
-      && game.accountable 
-      && game.playedTurns <= 10 
-      && !game.isTournament 
+    if (game.rated
+      && game.accountable
+      && game.playedTurns <= 10
+      && !game.isTournament
       && game.winnerColor.isDefined
-      && game.variant == Variant.default
+      && variants(game.variant)
       && !game.isCorrespondence
-      && game.clock.fold(false){_.limitInMinutes >= 1}) {
+      && game.clock.fold(false) { _.limitInMinutes >= 1 }) {
       game.winnerColor match {
         case Some(a) => {
-          val result: GameResult = a match { 
+          val result: GameResult = a match {
             case Color.White => GameResult(winner = whiteUser, loser = blackUser)
             case Color.Black => GameResult(winner = blackUser, loser = whiteUser)
           }
           val id = boostingId(result.winner, result.loser)
-          getBoostingRecord(id).flatMap{
+          getBoostingRecord(id).flatMap {
             case Some(record) =>
               val newRecord = BoostingRecord(
                 _id = id,
                 player = result.winner.id,
                 games = record.games + 1
-                )
+              )
               createBoostRecord(newRecord) >> determineBoosting(newRecord, result.winner)
             case none => createBoostRecord(BoostingRecord(
-                _id = id,
-                player = result.winner.id,
-                games = 1
-              ))
+              _id = id,
+              player = result.winner.id,
+              games = 1
+            ))
           }
         }
         case none => funit
       }
-    } else {
+    }
+    else {
       funit
     }
   }
@@ -80,9 +87,9 @@ final class BoostingApi(
 object BoostingApi {
 
   case class BoostingRecord(
-    _id: String,
-    player: String,
-    games: Int) {
+      _id: String,
+      player: String,
+      games: Int) {
     def id = _id
   }
 
