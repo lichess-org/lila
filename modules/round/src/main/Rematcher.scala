3ea752fb3b66bcf0d8d72c1ba1b90049cbafb26f
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-03-01 16:19:58 +0100

    make rematch casual if a player was marked as engine or booster

--- modules/round/src/main/Rematcher.scala
+++ modules/round/src/main/Rematcher.scala
@@ -11,7 +11,7 @@ import lila.db.api._
 import lila.game.tube.gameTube
 import lila.game.{ GameRepo, Game, Event, Progress, Pov, Source, AnonCookie, PerfPicker }
 import lila.memo.ExpireSetMemo
-import lila.user.UserRepo
+import lila.user.{ User, UserRepo }
 import makeTimeout.short
 
 private[round] final class Rematcher(
@@ -74,31 +74,28 @@ private[round] final class Rematcher(
       case FromPosition => situation.fold(Standard.pieces)(_.situation.board.pieces)
       case variant      => variant.pieces
     }
-    whitePlayer ← returnPlayer(pov.game, White)
-    blackPlayer ← returnPlayer(pov.game, Black)
+    users <- UserRepo byIds pov.game.userIds
   } yield Game.make(
     game = ChessGame(
       board = Board(pieces, variant = pov.game.variant),
       clock = pov.game.clock map (_.reset),
       turns = situation ?? (_.turns),
       startedAtTurn = situation ?? (_.turns)),
-    whitePlayer = whitePlayer,
-    blackPlayer = blackPlayer,
-    mode = pov.game.mode,
+    whitePlayer = returnPlayer(pov.game, White, users),
+    blackPlayer = returnPlayer(pov.game, Black, users),
+    mode = if (users.exists(_.lame)) chess.Mode.Casual else pov.game.mode,
     variant = pov.game.variant,
     source = pov.game.source | Source.Lobby,
     daysPerTurn = pov.game.daysPerTurn,
     castles = situation.fold(Castles.init)(_.situation.board.history.castles),
     pgnImport = None)
 
-  private def returnPlayer(game: Game, color: ChessColor): Fu[lila.game.Player] = {
+  private def returnPlayer(game: Game, color: ChessColor, users: List[User]): lila.game.Player = {
     val player = lila.game.Player.make(color = color, aiLevel = game.opponent(color).aiLevel)
-    game.player(!color).userId.fold(fuccess(player)) { userId =>
-      UserRepo byId userId map {
-        _.fold(player) { u =>
-          player.withUser(u.id, PerfPicker.mainOrDefault(game)(u.perfs))
-        }
-      }
+    game.player(!color).userId.flatMap { id =>
+      users.find(_.id == id)
+    }.fold(player) { user =>
+      player.withUser(user.id, PerfPicker.mainOrDefault(game)(user.perfs))
     }
   }
 
