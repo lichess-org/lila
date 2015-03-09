46da0ed0f7ee69857f1f9ea705c31405fe1c0ecd
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-03-09 16:33:16 +0100

    show horde initial position in PGN

--- modules/game/src/main/PgnDump.scala
+++ modules/game/src/main/PgnDump.scala
@@ -45,7 +45,8 @@ final class PgnDump(
   private def player(p: Player, u: Option[LightUser]) =
     p.aiLevel.fold(u.fold(p.name | lila.user.User.anonymous)(_.name))("lichess AI level " + _)
 
-  private val customStartPosition: Set[chess.variant.Variant] = Set(chess.variant.Chess960, chess.variant.FromPosition)
+  private val customStartPosition: Set[chess.variant.Variant] =
+    Set(chess.variant.Chess960, chess.variant.FromPosition, chess.variant.Horde)
 
   private def tags(game: Game, initialFen: Option[String]): List[Tag] = gameLightUsers(game) match {
     case (wu, bu) => List(
