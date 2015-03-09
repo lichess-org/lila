0982dd9bf9c46ed8d6028282b6b2f2c59d21d34b
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-03-09 15:40:30 +0100

    material difference makes no sense in horde chess

--- ui/round/src/data.js
+++ ui/round/src/data.js
@@ -14,5 +14,8 @@ module.exports = function(old, cfg) {
   if (cfg.game.moves) data.game.moves = data.game.moves.split(' ');
   else data.game.moves = [];
 
+  if (data.game.variant.key === 'horde')
+    data.pref.showCaptured = false;
+
   return data;
 };
