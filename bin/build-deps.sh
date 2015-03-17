15e6e60b3d099e6df24ac5974eb2f176eca7cd40
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-03-18 00:22:11 +0100

    tweak build-deps.sh

--- bin/build-deps.sh
+++ bin/build-deps.sh
@@ -4,11 +4,13 @@ dir=`mktemp -d`
 echo "Building in $dir"
 cd $dir
 
+rm -rf scalalib
 git clone https://github.com/ornicar/scalalib
 cd scalalib
 sbt publish-local
 cd ..
 
+rm -rf maxmind-geoip2-scala
 git clone https://github.com/ornicar/maxmind-geoip2-scala --branch customBuild
 cd maxmind-geoip2-scala
 sbt publish-local
