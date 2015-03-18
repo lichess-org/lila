ebd2c7bf26ecd10c9a5b9de7abff78da7865764d
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-03-18 14:49:00 +0100

    fix training puzzles PGN export

--- modules/puzzle/src/main/PngExport.scala
+++ modules/puzzle/src/main/PngExport.scala
@@ -6,13 +6,12 @@ import scala.sys.process._
 
 object PngExport {
 
-  // private val logger = ProcessLogger(_ => (), _ => ())
-  private val logger = ProcessLogger(println,println)
+  private val logger = ProcessLogger(_ => (), _ => ())
 
   def apply(execPath: String)(puzzle: Puzzle)(out: OutputStream) {
     val color = puzzle.color.letter.toString
     val lastMove = puzzle.initialMove
-    val fen = puzzle.fenAfterInitialMove | puzzle.fen
+    val fen = (puzzle.fenAfterInitialMove | puzzle.fen).takeWhile(' ' !=)
     val exec = Process(Seq("php", "board-creator.php", fen, color, lastMove), new File(execPath))
     exec #> out ! logger
   }
