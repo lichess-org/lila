bf6921972c75244eea7c81029cd0e8ed15a704e4
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-03-20 16:21:34 +0100

    remove Horde from top bar perfs

--- app/templating/UserHelper.scala
+++ app/templating/UserHelper.scala
@@ -31,8 +31,7 @@ trait UserHelper { self: I18nHelper with StringHelper =>
     PerfType.ThreeCheck,
     PerfType.Correspondence,
     PerfType.Antichess,
-    PerfType.Atomic,
-    PerfType.Horde)
+    PerfType.Atomic)
 
   private def best3Of(u: User, perfTypes: List[PerfType]) =
     perfTypes.sortBy { pt => -u.perfs(pt).nb } take 3
