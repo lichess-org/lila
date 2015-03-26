50a7528a74bbdd92f6b6c62a2c89b5e8f389b9b8
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-03-26 22:07:28 +0100

    display game results even when PGN moves are hidden by user pref - closes #371

--- ui/round/src/replay/view.js
+++ ui/round/src/replay/view.js
@@ -16,10 +16,7 @@ function renderTd(move, ply, curPly) {
   } : null;
 }
 
-function renderTable(ctrl, curPly) {
-  var moves = ctrl.root.data.game.moves;
-  var pairs = [];
-  for (var i = 0; i < moves.length; i += 2) pairs.push([moves[i], moves[i + 1]]);
+function renderResult(ctrl) {
   var result;
   if (status.finished(ctrl.root.data)) switch (ctrl.root.data.game.winner) {
     case 'white':
@@ -31,21 +28,29 @@ function renderTable(ctrl, curPly) {
     default:
       result = '½-½';
   }
+  if (result || status.aborted(ctrl.root.data)) {
+    var winner = game.getPlayer(ctrl.root.data, ctrl.root.data.game.winner);
+    return [
+      m('tr', m('td.result[colspan=3]', result)),
+      m('tr.status', m('td[colspan=3]', [
+        renderStatus(ctrl.root),
+        winner ? ', ' + ctrl.root.trans(winner.color == 'white' ? 'whiteIsVictorious' : 'blackIsVictorious') : null
+      ]))
+    ];
+  }
+}
+
+function renderTable(ctrl, curPly) {
+  var moves = ctrl.root.data.game.moves;
+  var pairs = [];
+  for (var i = 0; i < moves.length; i += 2) pairs.push([moves[i], moves[i + 1]]);
   var trs = pairs.map(function(pair, i) {
     return m('tr', [
       m('td.index', i + 1),
       renderTd(pair[0], 2 * i + 1, curPly),
       renderTd(pair[1], 2 * i + 2, curPly)
     ]);
-  });
-  if (result || status.aborted(ctrl.root.data)) {
-    trs.push(m('tr', m('td.result[colspan=3]', result)));
-    var winner = game.getPlayer(ctrl.root.data, ctrl.root.data.game.winner);
-    trs.push(m('tr.status', m('td[colspan=3]', [
-      renderStatus(ctrl.root),
-      winner ? ', ' + ctrl.root.trans(winner.color == 'white' ? 'whiteIsVictorious' : 'blackIsVictorious') : null
-    ])));
-  }
+  }).concat(renderResult(ctrl));
   return m('table',
     m('tbody', {
         onclick: function(e) {
@@ -67,8 +72,7 @@ function renderButtons(ctrl, curPly) {
   else if (root.data.player.spectator) flipAttrs.href = root.router.Round.watcher(root.data.game.id, root.data.opponent.color).url;
   else flipAttrs.onclick = root.flip;
   return m('div.buttons', [
-    m('a', flipAttrs, m('span[data-icon=B]')),
-    [
+    m('a', flipAttrs, m('span[data-icon=B]')), [
       ['first', 'W', 1],
       ['prev', 'Y', curPly - 1],
       ['next', 'X', curPly + 1],
@@ -83,8 +87,7 @@ function renderButtons(ctrl, curPly) {
         'data-icon': b[1],
         onclick: enabled ? partial(ctrl.jump, b[2]) : null
       });
-    }),
-    (game.userAnalysable(root.data) ? m('a', {
+    }), (game.userAnalysable(root.data) ? m('a', {
       class: 'button hint--top analysis',
       'data-hint': root.trans('analysis'),
       href: root.router.UserAnalysis.game(root.data.game.id, root.data.player.color).url,
@@ -111,6 +114,6 @@ module.exports = function(ctrl) {
         autoScroll(el);
         if (!isUpdate) setTimeout(partial(autoScroll, el), 100);
       },
-    }, renderTable(ctrl, curPly)) : null
+    }, renderTable(ctrl, curPly)) : renderResult(ctrl)
   ]);
 }
