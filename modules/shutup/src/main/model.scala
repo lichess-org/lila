4fbc98422cb3b7dc4ea3bb673bf0e75c27c681a4
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-04-25 20:26:30 +0200

    tweak insult tolerance

--- modules/shutup/src/main/model.scala
+++ modules/shutup/src/main/model.scala
@@ -47,7 +47,7 @@ case class TextReport(textType: TextType, ratios: List[Double]) {
 
   def minRatios = textType.rotation / 10
   def nbBad = ratios.count(_ > TextReport.unacceptableRatio)
-  def tolerableNb = ratios.size / 10
+  def tolerableNb = (ratios.size / 10) max 3
 
   def unacceptable = (ratios.size >= minRatios) && (nbBad > tolerableNb)
 }
