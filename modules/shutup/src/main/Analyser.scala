20738898495f5d07fd0264b475f0d11276e17991
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-04-26 14:45:10 +0200

    improve shutup analyser

--- modules/shutup/src/main/Analyser.scala
+++ modules/shutup/src/main/Analyser.scala
@@ -7,41 +7,9 @@ object Analyser {
     bigRegex.findAllMatchIn(text).map(_.toString).toList
   )
 
-  // based on https://github.com/snipe/banbuilder/blob/master/src/CensorWords.php#L97
-  private val leetReplace = Map(
-    'a' -> """(a|a\.|a\-|4|@|Á|á|À|Â|à|Â|â|Ä|ä|Ã|ã|Å|å|α|Δ|Λ|λ)""",
-    'b' -> """(b|b\.|b\-|8|\|3|ß|Β|β)""",
-    'c' -> """(c|c\.|c\-|Ç|ç|¢|€|<|\(|\{|©)""",
-    'd' -> """(d|d\.|d\-|&part;|\|\)|Þ|þ|Ð|ð)""",
-    'e' -> """(e|e\.|e\-|3|€|È|è|É|é|Ê|ê|∑)""",
-    'f' -> """(f|f\.|f\-|ƒ)""",
-    'g' -> """(g|g\.|g\-|6|9)""",
-    'h' -> """(h|h\.|h\-|Η)""",
-    'i' -> """(i|i\.|i\-|!|\||\]\[|]|1|∫|Ì|Í|Î|Ï|ì|í|î|ï)""",
-    'j' -> """(j|j\.|j\-)""",
-    'k' -> """(k|k\.|k\-|Κ|κ)""",
-    'l' -> """(l|1\.|l\-|!|\||\]\[|]|£|∫|Ì|Í|Î|Ï)""",
-    'm' -> """(m|m\.|m\-)""",
-    'n' -> """(n|n\.|n\-|η|Ν|Π)""",
-    'o' -> """(o|o\.|o\-|0|Ο|ο|Φ|¤|°|ø)""",
-    'p' -> """(p|p\.|p\-|ρ|Ρ|¶|þ)""",
-    'q' -> """(q|q\.|q\-)""",
-    'r' -> """(r|r\.|r\-|®)""",
-    's' -> """(s|s\.|s\-|5|\$|§)""",
-    't' -> """(t|t\.|t\-|Τ|τ)""",
-    'u' -> """(u|u\.|u\-|υ|µ)""",
-    'v' -> """(v|v\.|v\-|υ|ν)""",
-    'w' -> """(w|w\.|w\-|ω|ψ|Ψ)""",
-    'x' -> """(x|x\.|x\-|Χ|χ)""",
-    'y' -> """(y|y\.|y\-|¥|γ|ÿ|ý|Ÿ|Ý)""",
-    'z' -> """(z|z\.|z\-|Ζ)""")
-
   private def wordsRegexes: List[String] = Dictionary.en.map { word =>
-    val regex = word.map { char =>
-      leetReplace.getOrElse(char, char.toString)
-    }.mkString
-    if (word endsWith "s") regex
-    else regex + leetReplace.getOrElse('s', "s") + "?"
+    if (word endsWith "s") word
+    else word + "s?"
   }
 
   private val bigRegex = {
--- modules/shutup/src/test/AnalyserTest.scala
+++ modules/shutup/src/test/AnalyserTest.scala
@@ -15,8 +15,8 @@ class DetectTest extends Specification {
     }
     "find many bad words" in {
       find("fuck that shit") must_== List("fuck", "shit")
-      find("Beat them cunting nigger faggots with a bitchin' fuckstick") must_==
-        List("cunting", "nigger", "faggots", "fuckstick")
+      find("Beat them cunting nigger faggots with a communist dick") must_==
+        List("cunting", "nigger", "faggots", "dick")
     }
     "find no bad words" in {
       find("") must_== Nil
@@ -24,9 +24,24 @@ class DetectTest extends Specification {
       find("A sonnet is a poetic form which originated in Italy; Giacomo Da Lentini is credited with its invention.") must_== Nil
       find("computer analysis") must_== Nil
     }
+    "find badly spelled words" in {
+      find("fuk") must_== List("fuk")
+      find("well fuk me") must_== List("fuk")
+      find("foo ashole bar fukd") must_== List("ashole", "fukd")
+    }
+    "find variants" in {
+      find("cunt kunt cunting kawa kunting") must_== List("cunt", "kunt", "cunting", "kunting")
+      find("ass as ashole") must_== List("ass", "ashole")
+    }
+    "find plurals" in {
+      find("cunts kunts cuntings kawas kuntings") must_== List("cunts", "kunts", "cuntings", "kuntings")
+    }
+    "fucks" in {
+      find("fuck fffuuk fektard feak fak phuk") must_== List("fuck", "fffuuk", "fektard", "fak", "phuk")
+    }
     "compute ratio" in {
       ratio("fuck that shit") must_== 2d/3
-      ratio("Beat them cunting nigger faggots with a bitchin' fuckstick") must_== 4d/9
+      ratio("Beat them cunting nigger faggots with a communist dick") must_== 4d/9
       ratio("hello there") must_== 0
       ratio("") must_== 0
     }
