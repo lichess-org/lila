67ed12af71ab4dd197894c51e3d45685c3509d80
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-04-26 13:51:39 +0200

    make dictionary more readable

--- modules/shutup/src/main/Dictionary.scala
+++ modules/shutup/src/main/Dictionary.scala
@@ -1,22 +1,239 @@
 package lila.shutup
 
-// from https://github.com/snipe/banbuilder/tree/master/src/dict
+/**
+ * - words are automatically pluralized. "tit" will also match "tits"
+ * - words are automatically leetified. "tit" will also match "t1t", "t-i-t", and more.
+ * - words do not partial match. "anal" will NOT match "analysis".
+ */
 object Dictionary {
 
   val en: List[String] = (enBase ++ enUk ++ enUs).distinct
 
-  private def enBase = List("anal", "anus", "ass", "asshole", "bastard", "bitch", "boob", "cock", "coward", "cum", "cunnilingu", "cunt", "cunting", "dick",
-    "dildo", "dyke", "fag", "faggot", "fuck", "fuk", "fack", "fuckstick", "fucktard", "fucking", "handjob", "homo", "jerk", "jizz", "kike",
-    "kunt", "muff", "nigger", "niger", "pederast", "penis", "piss", "poop", "pussy", "queer", "rape", "scum", "scumbag", "semen", "sex", "shit",
-    "shite", "shitty", "shity", "shitbag", "slut", "titties", "twat", "vagin", "vagina", "vulva", "wank")
-  private def enUk = List("analplug", "analsex", "arse", "arsehole", "balls", "bimbo", "blowjob", "bollocks",
-    "boner", "boobies", "boobs", "bugger", "bukkake", "bullshit", "chink", "clit", "clitoris", "cocksucker", "condom", "coon", "crap",
-    "cumshot", "damm", "dammit", "damn", "dickhead", "doggystyle", "f0ck", "fags", "fanny", "fck", "fcker", "fckr", "fcku", "fcuk",
-    "fucked", "fucker", "fuckface", "fuckr", "fuct", "genital", "genitalia", "genitals", "glory hole", "gloryhole", "gobshite", "godammet",
-    "godammit", "goddammet", "goddammit", "goddamn", "gypo", "hitler", "hooker", "hore", "horny", "jesussucks", "jizzum", "kaffir", "kill",
-    "killer", "killin", "killing", "lesbo", "masturbate", "milf", "molest", "moron", "motherfuck", "mthrfckr", "murder", "murderer", "nazi",
-    "negro", "niga", "nigah", "nigga", "niggah", "nonce", "paedo", "paedophile", "paki", "pecker", "pedo", "pedofile", "pedophile", "phuk",
-    "pig", "pimp", "poof", "porn", "prick", "pron", "prostitute", "raped", "rapes", "rapist", "schlong", "screw", "scrotum", "shag",
-    "shemale", "shite", "shiz", "slag", "spastic", "spaz", "sperm", "spunk", "stripper", "stupid", "tart", "terrorist", "tit", "tittyfuck", "tosser", "turd", "vaginal", "vibrator", "wanker", "weed", "wetback", "whor", "whore", "wog", "wtf", "xxx")
-  private def enUs = List("abortion", "anus", "bewb", "blow", "blumpkin", "cawk", "choad", "cooter", "cornhole", "dong", "douche", "fart", "foreskin", "gangbang", "gook", "hell", "honkey", "humping", "jiz", "labia", "nutsack", "pen1s", "poon", "punani", "queef", "quim", "rectal", "rectum", "rimjob", "spick", "spoo", "spooge", "taint", "titty", "vag", "whore")
+  private def enBase = dict("""
+anal
+anus
+ass
+asshole
+bastard
+bitch
+boob
+cock
+coward
+cum
+cunnilingu
+cunt
+cunting
+dick
+dildo
+dyke
+fag
+faggot
+fuck
+fuk
+fack
+fuckstick
+fucktard
+fucking
+handjob
+homo
+jerk
+jizz
+kike
+kunt
+muff
+nigger
+niger
+pederast
+penis
+piss
+poop
+pussy
+queer
+rape
+scum
+scumbag
+semen
+sex
+shit
+shite
+shitty
+shity
+shitbag
+slut
+titties
+twat
+vagin
+vagina
+vulva
+wank
+""").pp
+  private def enUk = dict("""
+analplug
+analsex
+arse
+arsehole
+balls
+bimbo
+blowjob
+bollocks
+boner
+boobies
+boobs
+bugger
+bukkake
+bullshit
+chink
+clit
+clitoris
+cocksucker
+condom
+coon
+crap
+cumshot
+damm
+dammit
+damn
+dickhead
+doggystyle
+f0ck
+fags
+fanny
+fck
+fcker
+fckr
+fcku
+fcuk
+fucked
+fucker
+fuckface
+fuckr
+fuct
+genital
+genitalia
+genitals
+glory hole
+gloryhole
+gobshite
+godammet
+godammit
+goddammet
+goddammit
+goddamn
+gypo
+hitler
+hooker
+hore
+horny
+jesussucks
+jizzum
+kaffir
+kill
+killer
+killin
+killing
+lesbo
+masturbate
+milf
+molest
+moron
+motherfuck
+mthrfckr
+murder
+murderer
+nazi
+negro
+niga
+nigah
+nigga
+niggah
+nonce
+paedo
+paedophile
+paki
+pecker
+pedo
+pedofile
+pedophile
+phuk
+pig
+pimp
+poof
+porn
+prick
+pron
+prostitute
+raped
+rapes
+rapist
+schlong
+screw
+scrotum
+shag
+shemale
+shite
+shiz
+slag
+spastic
+spaz
+sperm
+spunk
+stripper
+stupid
+tart
+terrorist
+tit
+tittyfuck
+tosser
+turd
+vaginal
+vibrator
+wanker
+weed
+wetback
+whor
+whore
+wog
+wtf
+xxx
+""")
+  private def enUs = dict("""
+abortion
+anus
+bewb
+blow
+blumpkin
+cawk
+choad
+cooter
+cornhole
+dong
+douche
+fart
+foreskin
+gangbang
+gook
+hell
+honkey
+humping
+jiz
+labia
+nutsack
+pen1s
+poon
+punani
+queef
+quim
+rectal
+rectum
+rimjob
+spick
+spoo
+spooge
+taint
+titty
+vag
+whore
+""")
+
+  private def dict(words: String) = words.lines.filter(_.nonEmpty).toList
 }
