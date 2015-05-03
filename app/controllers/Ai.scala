0ed8b5c9757c2a66047af30ff5e7108916df9994
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-05-03 18:29:32 +0200

    remove AI debug

--- app/controllers/Ai.scala
+++ app/controllers/Ai.scala
@@ -32,7 +32,6 @@ object Ai extends LilaController {
 
   def analyse = Action.async { req =>
     get("replyUrl", req) foreach { replyToUrl =>
-      println(s"analyse gameId ${get("gameId", req)}")
       Env.ai.server.analyse(
         uciMoves = get("uciMoves", req) ?? (_.split(' ').toList),
         initialFen = get("initialFen", req),
