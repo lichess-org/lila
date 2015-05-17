956f30e86df30f022786cc4c4dd9335115b61b0d
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-05-17 10:40:30 +0200

    ignore standard initial FEN when importing - fixes #488

--- modules/importer/src/main/DataForm.scala
+++ modules/importer/src/main/DataForm.scala
@@ -35,9 +35,10 @@ private[importer] case class ImportData(pgn: String) {
           tags find (_.name == which(Tag)) map (_.value)
 
         val initBoard = tag(_.FEN) flatMap Forsyth.<< map (_.board)
+        val fromPosition = initBoard.nonEmpty && tag(_.FEN) != Forsyth.initial.some
         val variant = {
           tag(_.Variant).map(Chess960.fixVariantName).flatMap(chess.variant.Variant.byName) | {
-            initBoard.nonEmpty.fold(chess.variant.FromPosition, chess.variant.Standard)
+            fromPosition.fold(chess.variant.FromPosition, chess.variant.Standard)
           }
         } match {
           case chess.variant.Chess960 if !Chess960.isStartPosition(setup.board) => chess.variant.FromPosition
