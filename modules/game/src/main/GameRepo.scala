c906e8ef9cf26abbd64c16d572d0cf9960eb9f04
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-05-26 15:33:14 +0200

    fix analysis when chess960 position is standard

--- modules/game/src/main/GameRepo.scala
+++ modules/game/src/main/GameRepo.scala
@@ -223,7 +223,7 @@ object GameRepo {
     val userIds = g2.userIds.distinct
     val fen = (!g2.variant.standardInitialPosition)
       .option(Forsyth >> g2.toChess)
-      .filterNot(Forsyth.initial ==)
+      .filter(Forsyth.initial !=)
     val bson = (gameTube.handler write g2) ++ BSONDocument(
       F.initialFen -> fen,
       F.checkAt -> (!g2.isPgnImport).option(DateTime.now.plusHours(g2.hasClock.fold(1, 24))),
