731d4391bfca174ca671685742bf33201b6d4aea
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-06-09 12:33:35 +0200

    fix arena win count

--- ui/tournament/src/view/arena.js
+++ ui/tournament/src/view/arena.js
@@ -68,7 +68,7 @@ function podiumStats(p, data) {
   else if (p.perf < 0) perf = m('span.negative[data-icon=M]', -p.perf);
   var nbGames = p.sheet.scores.length;
   var winP = Math.round(p.sheet.scores.filter(function(s) {
-    return s[1] === 'double' ? s[0] >= 4 : s[0] >= 2;
+    return s[1] === 3 ? s[0] >= 4 : s[0] >= 2;
   }).length * 100 / nbGames);
   var berserkP = Math.round(p.sheet.scores.filter(function(s) {
     return s === 3 || s[0] === 3 || s[0] === 5;
