29683a328478d5eb6fa101d389d5442157c4f6d5
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-06-14 23:51:34 +0200

    remove extra ui/game file

--- ui/game/src/main.js
+++ ui/game/src/main.js
@@ -1,10 +1,23 @@
 module.exports = {
   game: require('./game'),
   status: require('./status'),
-  perf: require('./perf'),
   view: {
     status: require('./view/status'),
     user: require('./view/user'),
     mod: require('./view/mod')
+  },
+  perf: {
+    icons: {
+      bullet: "T",
+      blitz: ")",
+      classical: "+",
+      correspondence: ";",
+      chess960: "'",
+      kingOfTheHill: "(",
+      threeCheck: ".",
+      antichess: "@",
+      atomic: ">",
+      horde: "_"
+    }
   }
 };
