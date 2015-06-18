1eb71098fe1d5f37951afa9c66b8884a378dd4b1
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-06-19 01:10:17 +0200

    try arena pairings every 3 seconds

--- modules/tournament/src/main/Env.scala
+++ modules/tournament/src/main/Env.scala
@@ -123,7 +123,7 @@ final class Env(
       organizer -> actorApi.AllCreatedTournaments
     }
 
-    scheduler.message(4 seconds) {
+    scheduler.message(3 seconds) {
       organizer -> actorApi.StartedTournaments
     }
 
