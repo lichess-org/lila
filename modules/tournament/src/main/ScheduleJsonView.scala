1a01a5db41f2b7ad507e747315efeac48a83ff73
Jimmie Elvenmark <flugsio@gmail.com>
2015-06-18 12:36:42 +0200

    first version of new tournament schedule

--- modules/tournament/src/main/ScheduleJsonView.scala
+++ modules/tournament/src/main/ScheduleJsonView.scala
@@ -5,6 +5,7 @@ import play.api.libs.json._
 import lila.common.LightUser
 import lila.common.PimpedJson._
 import lila.game.{ Game, GameRepo }
+import lila.rating.PerfType
 import lila.user.User
 
 final class ScheduleJsonView(
@@ -26,11 +27,17 @@ final class ScheduleJsonView(
     "fullName" -> tour.fullName,
     "nbPlayers" -> tour.nbPlayers,
     "private" -> tour.`private`,
-    "variant" -> tour.variant.key,
+    "variant" -> Json.obj(
+      "key" -> tour.variant.key,
+      "short" -> tour.variant.shortName,
+      "name" -> tour.variant.name),
     "secondsToStart" -> tour.secondsToStart,
-    "startsAt" -> org.joda.time.format.ISODateTimeFormat.dateTime.print(tour.startsAt),
+    "startsAt" -> tour.startsAt,
+    "startsAtString" -> org.joda.time.format.ISODateTimeFormat.dateTime.print(tour.startsAt),
+    "finishesAt" -> tour.finishesAt,
     "schedule" -> tour.schedule.map(scheduleJson),
-    "winner" -> tour.winnerId.flatMap(getLightUser).map(userJson))
+    "winner" -> tour.winnerId.flatMap(getLightUser).map(userJson),
+    "perf" -> tour.perfType.map(perfJson))
 
   private def scheduleJson(s: Schedule) = Json.obj(
     "freq" -> s.freq.name,
@@ -49,4 +56,10 @@ final class ScheduleJsonView(
     "id" -> u.id,
     "name" -> u.name,
     "title" -> u.title)
+
+  private def perfJson(p: PerfType) = Json.obj(
+    "icon" -> p.iconChar.toString,
+    "name" -> p.name,
+    "index" -> PerfType.all.indexOf(p))
+
 }
--- ui/tournamentSchedule/src/ctrl.js
+++ ui/tournamentSchedule/src/ctrl.js
@@ -1,10 +1,17 @@
 var m = require('mithril');
+var socket = require('./socket');
 
 module.exports = function(env) {
 
   this.data = env.data;
 
   this.userId = env.userId;
+
+  this.socket = new socket(env.socketSend, this);
+
+  this.reload = function(data) {
+    this.data = data;
+  }.bind(this);
 
   this.trans = function(key) {
     var str = env.i18n[key] || key;
