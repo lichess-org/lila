177d32f7e1bf120c8d218df0ae64e46f3cf8c716
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-06-19 12:51:50 +0200

    export puzzles in mobile app format

--- modules/puzzle/src/main/Export.scala
+++ modules/puzzle/src/main/Export.scala
@@ -5,18 +5,18 @@ import play.api.libs.json._
 object Export {
 
   def apply(puzzles: List[Puzzle]) = Json stringify {
-    JsArray(puzzles map { puzzle =>
-      Json.obj(
-        "id" -> puzzle.id,
-        "rating" -> puzzle.perf.intRating,
-        "attempts" -> puzzle.attempts,
-        "fen" -> puzzle.fen,
-        "color" -> puzzle.color.name,
-        "initialMove" -> puzzle.initialMove,
-        "initialPly" -> puzzle.initialPly,
-        "gameId" -> puzzle.gameId,
-        "lines" -> lila.puzzle.Line.toJson(puzzle.lines),
-        "vote" -> puzzle.vote.sum)
-    })
+    JsArray(
+      puzzles map { puzzle =>
+        JsString(Json stringify {
+          Json.obj(
+            "id" -> puzzle.id,
+            "fen" -> puzzle.fen,
+            "color" -> puzzle.color.name,
+            "move" -> puzzle.initialMove,
+            "ply" -> puzzle.initialPly,
+            "game" -> puzzle.gameId,
+            "lines" -> lila.puzzle.Line.toJson(puzzle.lines))
+        })
+      })
   }
 }
