ec9acf3fdfe42950c037e3d6ebdc548e259bcd55
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-06-24 14:27:30 +0200

    fix relay status display

--- ui/relay/src/view/main.js
+++ ui/relay/src/view/main.js
@@ -2,15 +2,18 @@ var m = require('mithril');
 var util = require('chessground').util;
 var games = require('./games');
 
+function statusName(ctrl, s) {
+  if (s == 30) return ctrl.trans('finished');
+  else return ctrl.trans('started');
+}
+
 module.exports = function(ctrl) {
 
   return m('div', {
     class: 'content_box no_padding relay_box relay_show'
   }, [
-    m('h1.text[data-icon=n]', [
-      ctrl.data.name,
-      m('span.status', ctrl.data.status)
-    ]),
+    m('div.title_tag', statusName(ctrl, ctrl.data.status)),
+    m('h1.text[data-icon=n]', ctrl.data.name),
     games(ctrl.data.games)
   ]);
 };
