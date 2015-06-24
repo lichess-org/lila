e217efe4ae12ef255b3165b7bf82958f90164d2b
T. Alexander Lystad <alexander.lystad@gmail.com>
2015-06-24 14:03:32 +0200

    adding tooltips for player connection status - closes #635

--- ui/game/src/view/user.js
+++ ui/game/src/view/user.js
@@ -11,6 +11,11 @@ function ratingDiff(player) {
 module.exports = function(ctrl, player, klass) {
   var perf = player.user ? player.user.perfs[ctrl.data.game.perf] : null;
   var rating = player.rating ? player.rating : (perf ? perf.rating : null);
+  var playerOnGameIcon = m('span.status.hint--top', {
+	  'data-hint': 'Player' + (player.onGame ? ' is connected' : ' has disconnected')
+	}, m('span', {
+	  'data-icon': (player.onGame ? '3' : '0')
+	}))
   return player.user ? [
     m('a', {
       class: 'text ulpt user_link ' + (player.user.online ? 'online is-green' : 'offline') + (klass ? ' ' + klass : ''),
@@ -26,9 +31,9 @@ module.exports = function(ctrl, player, klass) {
         title: ctrl.trans('thisPlayerUsesChessComputerAssistance')
       }) : null
     ]),
-    m('span.status')
+    playerOnGameIcon
   ] : m('span.user_link', [
     'Anonymous',
-    m('span.status')
+	playerOnGameIcon
   ]);
 }
