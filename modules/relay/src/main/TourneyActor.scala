c90d8598a0dd1b9392197b50e5bac23a1d5cc309
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-06-24 10:36:38 +0200

    new relay actor architecture

---
+++ modules/relay/src/main/TourneyActor.scala
@@ -0,0 +1,82 @@
+package lila.relay
+
+import akka.actor._
+import akka.pattern.{ ask, pipe }
+import scala.concurrent.duration._
+
+import lila.hub.actorApi.map.Tell
+import lila.hub.SequentialActor
+
+private[relay] final class TourneyActor(
+    id: String,
+    ficsProps: Props,
+    repo: RelayRepo,
+    importer: Importer) extends SequentialActor {
+
+  import TourneyActor._
+
+  context setReceiveTimeout 3.hours
+
+  val fics = context.actorOf(ficsProps, name = "fics")
+
+  val gameMap = context.actorOf(Props(new lila.hub.ActorMap {
+    def mkActor(ficsIdStr: String) = {
+      val ficsId = parseIntOption(ficsIdStr) err s"Invalid relay FICS id $ficsIdStr"
+      new GameActor(
+        fics = fics,
+        ficsId = ficsId,
+        getGameId = () => repo gameIdByFicsId ficsId,
+        importer = importer)
+    }
+    def receive = actorMapReceive
+  }), name = "games")
+
+  override def preStart() {
+    context.system.lilaBus.subscribe(self, 'relayMove)
+    self ! Recover
+  }
+
+  override def postStop() {
+    context.system.lilaBus unsubscribe self
+  }
+
+  def process = {
+
+    case Recover => withRelay { relay =>
+      implicit val t = makeTimeout seconds 30
+      fics ? command.ListGames(relay.ficsId) mapTo
+        manifest[command.ListGames.Result] flatMap { games =>
+          val rgs = games.map { g =>
+            relay gameByFicsId g.id match {
+              case None     => Relay.Game make g.id
+              case Some(rg) => rg
+            }
+          }
+          val nr = relay.copy(games = rgs)
+          println(s"[relay] ${nr.name}: ${nr.activeGames.size}/${nr.games.size} games")
+          repo.setGames(nr) >>-
+            nr.activeGames.foreach { rg =>
+              gameMap ! Tell(rg.ficsId.toString, GameActor.Recover)
+            }
+        }
+    }
+
+    case ReceiveTimeout => fuccess {
+      self ! SequentialActor.Terminate
+    }
+  }
+
+  def withRelay[A](f: Relay => Fu[A]): Funit = repo byId id flatMap {
+    case None        => fufail(s"No relay found for ID $id")
+    case Some(relay) => f(relay).void
+  }
+
+  override def onFailure(e: Exception) {
+    println(s"[$id] ERR ${e.getMessage}")
+  }
+}
+
+object TourneyActor {
+
+  case object Recover
+}
