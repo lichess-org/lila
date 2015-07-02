19fa5375252354c9468822a103ede70bcb97424d
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-07-03 00:33:15 +0200

    use syzygy tablebases from ai.syzygy_path

--- modules/ai/src/main/Env.scala
+++ modules/ai/src/main/Env.scala
@@ -28,6 +28,7 @@ final class Env(
     hashSize = c getInt "hash_size",
     nbThreads = c getInt "threads",
     nbInstances = c getInt "instances",
+    syzygyPath = if (c hasPath "syzygy_path") c getString "syzygy_path" else "<empty>",
     playMaxMoveTime = c duration "play.movetime",
     analyseMoveTime = c duration "analyse.movetime",
     playTimeout = c duration "play.timeout",
