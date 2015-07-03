fcf2e4b7eb1853f0e8c9fc05769fd688f3ed5bec
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-07-03 12:20:32 +0200

    remove game indexer limit

--- modules/gameSearch/src/main/Indexer.scala
+++ modules/gameSearch/src/main/Indexer.scala
@@ -64,7 +64,7 @@ private[gameSearch] final class Indexer(
         var nbSkipped = 0
         var started = nowMillis
         Await.result(
-          $enumerate.bulk[Option[lila.game.Game]]($query.all, batchSize, limit = 800 * 1000) { gameOptions =>
+          $enumerate.bulk[Option[lila.game.Game]]($query.all, batchSize) { gameOptions =>
             val games = gameOptions.flatten filter storable
             val nbGames = games.size
             (GameRepo filterAnalysed games.map(_.id).toSeq flatMap { analysedIds =>
