eb2618a2129023dde94168bd83b17fee30133a82
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-08-13 18:48:27 +0200

    tweak regulator

--- modules/rating/src/main/Regulator.scala
+++ modules/rating/src/main/Regulator.scala
@@ -6,7 +6,7 @@ object Regulator {
     if (before.nb >= after.nb) after
     else {
       val diff = (after.glicko.rating - before.glicko.rating).abs
-      val extra = diff / 100
+      val extra = diff / 50
       after.copy(
         glicko = after.glicko.copy(
           rating = after.glicko.rating + extra))
