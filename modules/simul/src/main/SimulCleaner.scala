8f9856378ded30df8d881b2246eede3479af0908
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-08-15 00:23:29 +0200

    make simuls a bit less volatile

--- modules/simul/src/main/SimulCleaner.scala
+++ modules/simul/src/main/SimulCleaner.scala
@@ -19,7 +19,7 @@ private[simul] final class SimulCleaner(
           manifest[Iterable[String]] map
           (_.toSet contains simul.hostId) map {
             case true => repo setHostSeenNow simul
-            case false if simul.hostSeenAt.??(_ isBefore DateTime.now.minusMinutes(2)) => api abort simul.id
+            case false if simul.hostSeenAt.??(_ isBefore DateTime.now.minusMinutes(3)) => api abort simul.id
             case false =>
           }
       }
