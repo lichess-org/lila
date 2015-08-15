21f81c5543e6930b890ab673e41e2c2bc467b031
Unihedron <unihedron@unihedro.com>
2015-08-15 21:32:28 +0800

    actually check the tenth digit instead

--- app/templating/NumberHelper.scala
+++ app/templating/NumberHelper.scala
@@ -22,7 +22,7 @@ trait NumberHelper { self: I18nHelper =>
     def localize(implicit ctx: UserContext): String = formatter format number
   }
 
-  def nth(number: Int) = if ((11 to 13).contains(number))
+  def nth(number: Int) = if ((11 to 13).contains(number % 100))
     "th"
   else number % 10 match {
     case 1 => "st"
