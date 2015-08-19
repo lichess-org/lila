8d8194e878bdd775e425ddd387eccd58b54572ca
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-08-19 12:50:38 +0200

    reformat assessment code

--- modules/evaluation/src/main/Assessible.scala
+++ modules/evaluation/src/main/Assessible.scala
@@ -1,10 +1,10 @@
 package lila.evaluation
 
 import chess.Color
-import org.joda.time.DateTime
-import lila.game.{ Game, Pov }
 import lila.analyse.{ Accuracy, Analysis }
+import lila.game.{ Game, Pov }
 import Math.signum
+import org.joda.time.DateTime
 
 case class Analysed(game: Game, analysis: Analysis)
 
@@ -16,9 +16,10 @@ case class Assessible(analysed: Analysed) {
     listAverage(Accuracy.diffsList(Pov(game, color), analysis)) < 15
 
   def alwaysHasAdvantage(color: Color): Boolean =
-    !analysis.infos.exists{ info =>
-      info.score.fold(info.mate.fold(false){ a => (signum(a).toInt == color.fold(-1, 1)) }){ cp =>
-        color.fold(cp.centipawns < -100, cp.centipawns > 100)}
+    !analysis.infos.exists { info =>
+      info.score.fold(info.mate.fold(false) { a => (signum(a).toInt == color.fold(-1, 1)) }) { cp =>
+        color.fold(cp.centipawns < -100, cp.centipawns > 100)
+      }
     }
 
   def highBlurRate(color: Color): Boolean =
@@ -40,26 +41,30 @@ case class Assessible(analysed: Analysed) {
     suspiciousHoldAlert(color)
   )
 
+  private val T = true
+  private val F = false
+
   def rankCheating(color: Color): GameAssessment = {
     import GameAssessment._
     val flags = mkFlags(color)
+    if (game.id == "tSaUCfxp") println(flags)
     val assessment = flags match {
-                   //  SF1    SF2    BLR1   BLR2   MTs1   MTs2   Holds
-      case PlayerFlags(true,  true,  true,  true,  true,  true,  true) => Cheating // all true, obvious cheat
-      case PlayerFlags(_   ,  _   ,  _   ,  _   ,  _   ,  _   ,  true) => Cheating // Holds are bad, hmk?
-      case PlayerFlags(true,  _,     true,  _,     _,     true,  _)    => Cheating // high accuracy, high blurs, no fast moves
+      //               SF1 SF2 BLR1 BLR2 MTs1 MTs2 Holds
+      case PlayerFlags(T, T, T, T, T, T, T) => Cheating // all T, obvious cheat
+      case PlayerFlags(_, _, _, _, _, _, T) => Cheating // Holds are bad, hmk?
+      case PlayerFlags(T, _, T, _, _, T, _) => Cheating // high accuracy, high blurs, no fast moves
 
-      case PlayerFlags(true,  _,     _,     _,     true,  true,  _)    => LikelyCheating // high accuracy, consistent move times, no fast moves
-      case PlayerFlags(true,  _,     _,     true,  _,     true,  _)    => LikelyCheating // high accuracy, moderate blurs, no fast moves
-      case PlayerFlags(_,     true,  _,     true,  true,  _,     _)    => LikelyCheating // always has advantage, moderate blurs, highly consistent move times
-      case PlayerFlags(_,     true,  true,  _,     _,     _,     _)    => LikelyCheating // always has advantage, high blurs
+      case PlayerFlags(T, _, _, _, T, T, _) => LikelyCheating // high accuracy, consistent move times, no fast moves
+      case PlayerFlags(T, _, _, T, _, T, _) => LikelyCheating // high accuracy, moderate blurs, no fast moves
+      case PlayerFlags(_, T, _, T, T, _, _) => LikelyCheating // always has advantage, moderate blurs, highly consistent move times
+      case PlayerFlags(_, T, T, _, _, _, _) => LikelyCheating // always has advantage, high blurs
 
-      case PlayerFlags(true,  _,     _,     false, false, true,  _)    => Unclear // high accuracy, no fast moves, but doesn't blur or flat line
+      case PlayerFlags(T, _, _, F, F, T, _) => Unclear // high accuracy, no fast moves, but doesn't blur or flat line
 
-      case PlayerFlags(true,  _,     _,     _,     _,     false, _)    => UnlikelyCheating // high accuracy, but has fast moves
+      case PlayerFlags(T, _, _, _, _, F, _) => UnlikelyCheating // high accuracy, but has fast moves
 
-      case PlayerFlags(false, false, _,     _,     _,    _,      _)    => NotCheating // low accuracy, doesn't hold advantage
-      case _                                                           => NotCheating
+      case PlayerFlags(F, F, _, _, _, _, _) => NotCheating // low accuracy, doesn't hold advantage
+      case _                                => NotCheating
     }
 
     if (flags.suspiciousHoldAlert) assessment
@@ -77,20 +82,20 @@ case class Assessible(analysed: Analysed) {
 
   def playerAssessment(color: Color): PlayerAssessment =
     PlayerAssessment(
-    _id = game.id + "/" + color.name,
-    gameId = game.id,
-    userId = ~game.player(color).userId,
-    white = (color == Color.White),
-    assessment = rankCheating(color),
-    date = DateTime.now,
-    // meta
-    flags = mkFlags(color),
-    sfAvg = sfAvg(color),
-    sfSd = sfSd(color),
-    mtAvg = mtAvg(color),
-    mtSd = mtSd(color),
-    blurs = blurs(color),
-    hold = hold(color)
+      _id = game.id + "/" + color.name,
+      gameId = game.id,
+      userId = ~game.player(color).userId,
+      white = (color == Color.White),
+      assessment = rankCheating(color),
+      date = DateTime.now,
+      // meta
+      flags = mkFlags(color),
+      sfAvg = sfAvg(color),
+      sfSd = sfSd(color),
+      mtAvg = mtAvg(color),
+      mtSd = mtSd(color),
+      blurs = blurs(color),
+      hold = hold(color)
     )
 
   val assessments: PlayerAssessments = PlayerAssessments(
