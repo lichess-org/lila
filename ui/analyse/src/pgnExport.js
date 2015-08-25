1c79ed6275679dff3c54b35eb525d55ead2afdfc
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-08-25 15:42:53 +0200

    fixes

--- ui/analyse/src/pgnExport.js
+++ ui/analyse/src/pgnExport.js
@@ -3,10 +3,9 @@ module.exports = function(ctrl) {
   ctrl.analyse.tree.forEach(function(node, i) {
     if (i === 0) return;
     if (i > ctrl.vm.path[0].ply) return;
-    console.log(node);
     if (i % 2 === 1) s += ((i + 1) / 2) + '. '
     else s += '';
     s += node.san + ' ';
   });
-  return s;
+  return s.trim();
 };
