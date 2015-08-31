1d7314db755b0b0ab33168aff6ff87e2d524e6f0
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-08-31 03:26:07 +0200

    fork prismic scala-kit to avoid netty 4.0 dependency

--- bin/build-deps.sh
+++ bin/build-deps.sh
@@ -10,6 +10,13 @@ cd scalalib
 sbt publish-local
 cd ..
 
+rm -rf scala-kit
+git clone https://github.com/ornicar/scala-kit
+cd scala-kit
+git checkout 1210thib
+sbt -Dversion=1.2.10-THIB publish-local
+cd ..
+
 rm -rf maxmind-geoip2-scala
 git clone https://github.com/ornicar/maxmind-geoip2-scala --branch customBuild
 cd maxmind-geoip2-scala
