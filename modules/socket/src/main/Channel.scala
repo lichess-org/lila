454b2c1ad89e7cf4e84263fc9c3b0a92d6f32783
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-09-08 08:50:08 +0200

    generic socket channel

---
+++ modules/socket/src/main/Channel.scala
@@ -0,0 +1,36 @@
+package lila.socket
+
+import actorApi.SocketLeave
+import akka.actor._
+import play.api.libs.json.JsValue
+
+final class Channel extends Actor {
+
+  context.system.lilaBus.subscribe(self, 'socketDoor)
+
+  import Channel._
+
+  val members = scala.collection.mutable.Set.empty[SocketMember]
+
+  def receive = {
+
+    case Sub(member)            => members += member
+
+    case UnSub(member)          => members -= member
+
+    case SocketLeave(_, member) => members -= member
+
+    case Publish(msg) =>
+      println(members.size)
+      members.foreach(_ push msg)
+
+  }
+}
+
+object Channel {
+
+  case class Sub(member: SocketMember)
+  case class UnSub(member: SocketMember)
+
+  case class Publish(msg: JsValue)
+}
