80dd162d5f224138333366fd35c04b77cae02cd6
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-09-13 09:08:29 +0200

    remove unused IP wiretaping

--- modules/security/src/main/Env.scala
+++ modules/security/src/main/Env.scala
@@ -20,7 +20,6 @@ final class Env(
 
   private val settings = new {
     val CollectionSecurity = config getString "collection.security"
-    val WiretapIps = config.getStringList("wiretap.ips").toSet
     val FirewallEnabled = config getBoolean "firewall.enabled"
     val FirewallCookieName = config getString "firewall.cookie.name"
     val FirewallCookieEnabled = config getBoolean "firewall.cookie.enabled"
@@ -56,8 +55,6 @@ final class Env(
     cachedIpsTtl = FirewallCachedIpsTtl)
 
   lazy val flood = new Flood(FloodDuration)
-
-  lazy val wiretap = new Wiretap(WiretapIps)
 
   lazy val recaptcha = new Recaptcha(
     privateKey = RecaptchaPrivateKey,
