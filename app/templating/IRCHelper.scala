59759b1a44468ea4e7c4e960f6b8d26c3dc4aceb
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-09-20 14:58:48 +0200

    remove unused web IRC related code

--- app/templating/IRCHelper.scala
+++ app/templating/IRCHelper.scala
@@ -12,21 +12,12 @@ trait IRCHelper { self: TeamHelper with SecurityHelper with I18nHelper =>
   def myIrcUrl(teams: List[Team])(implicit ctx: Context) =
     """http://webchat.freenode.net?nick=%s&channels=%s&prompt=%s&uio=%s""".format(
       ctx.username | "Anon-.",
-      myIrcChannels(teams) mkString ",",
+      teamChans(teams) mkString ",",
       prompt,
       uio)
 
-  def myIrcChannels(teams: List[Team])(implicit ctx: Context): List[String] =
-    teamChans(teams) ::: staffChans ::: langChans
-
-  private def teamChans(teams: List[Team]) = teams.map(teamIrcChan).flatten
+  def teamChans(teams: List[Team]) = teams flatMap teamIrcChan
 
   def teamIrcChan(team: Team) = team.irc option "lichess-team-" + team.id
-
-  private def staffChans(implicit ctx: Context) =
-    isGranted(_.StaffForum) ?? List("lichess-staff")
-
-  private def langChans(implicit ctx: Context) =
-    List("lichess", lang.language + ".lichess") filterNot ("en.lichess" ==)
 }
 
