0deb10992b2f2709dc4d09370156521deee39632
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-09-24 17:15:05 +0200

    regulate depending on perf type

--- modules/rating/src/main/Regulator.scala
+++ modules/rating/src/main/Regulator.scala
@@ -2,13 +2,19 @@ package lila.rating
 
 object Regulator {
 
-  def apply(before: Perf, after: Perf) =
+  def apply(perfType: PerfType, before: Perf, after: Perf) =
     if (before.nb >= after.nb) after
     else {
       val diff = (after.glicko.rating - before.glicko.rating).abs
-      val extra = diff / 100
+      val extra = diff / regulationDivider(perfType)
       after.copy(
         glicko = after.glicko.copy(
           rating = after.glicko.rating + extra))
     }
+
+  private def regulationDivider(perfType: PerfType): Int = perfType match {
+    case PerfType.Bullet => 80
+    case PerfType.Blitz  => 70
+    case _               => 50
+  }
 }
