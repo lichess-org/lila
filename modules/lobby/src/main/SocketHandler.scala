9b7e68db30d5042bea131c60f4ebba4638af96f3
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-11-05 14:18:13 +0700

    now this works. the former PartialFunction compiled but failed

--- modules/lobby/src/main/SocketHandler.scala
+++ modules/lobby/src/main/SocketHandler.scala
@@ -24,21 +24,28 @@ private[lobby] final class SocketHandler(
     socket: ActorRef,
     uid: String,
     member: Member): Handler.Controller = {
-    case ("p", o) => o int "v" foreach { v => socket ! PingVersion(uid, v) }
-    case ("join", o) =>
-      o str "d" foreach { id =>
-        lobby ! BiteHook(id, uid, member.user)
+
+    lazy val RateLimit = new lila.memo.RateLimitGlobal(2 seconds)
+
+    {
+      case ("p", o) => o int "v" foreach { v => socket ! PingVersion(uid, v) }
+      case ("join", o) => RateLimit {
+        o str "d" foreach { id =>
+          lobby ! BiteHook(id, uid, member.user)
+        }
       }
-    case ("cancel", o) => lobby ! CancelHook(uid)
-    case ("joinSeek", o) =>
-      for {
+      case ("cancel", o) => lobby ! CancelHook(uid)
+      case ("joinSeek", o) => RateLimit {
+        for {
+          id <- o str "d"
+          user <- member.user
+        } lobby ! BiteSeek(id, user)
+      }
+      case ("cancelSeek", o) => for {
         id <- o str "d"
         user <- member.user
-      } lobby ! BiteSeek(id, user)
-    case ("cancelSeek", o) => for {
-      id <- o str "d"
-      user <- member.user
-    } lobby ! CancelSeek(id, user)
+      } lobby ! CancelSeek(id, user)
+    }: Handler.Controller
   }
 
   def apply(uid: String, user: Option[User]): Fu[JsSocketHandler] =
