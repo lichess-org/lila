3a4e7e57d5c5cee8aa54d9af30fd760312e1ca33
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-11-05 09:41:12 +0700

    fix lobby rate limiter

--- modules/lobby/src/main/SocketHandler.scala
+++ modules/lobby/src/main/SocketHandler.scala
@@ -20,33 +20,34 @@ private[lobby] final class SocketHandler(
     socket: ActorRef,
     blocking: String => Fu[Set[String]]) {
 
-  private lazy val RateLimit = new lila.security.RateLimitGlobal(2 seconds)
-
   private def controller(
     socket: ActorRef,
     uid: String,
     member: Member): Handler.Controller = {
-    case ("p", o) => o int "v" foreach { v => socket ! PingVersion(uid, v) }
-    case ("join", o) => RateLimit {
-      o str "d" foreach { id =>
-        lobby ! BiteHook(id, uid, member.user)
+    lazy val RateLimit = new lila.memo.RateLimitGlobal(2 seconds)
+    PartialFunction {
+      case ("p", o) => o int "v" foreach { v => socket ! PingVersion(uid, v) }
+      case ("join", o) => RateLimit {
+        o str "d" foreach { id =>
+          lobby ! BiteHook(id, uid, member.user)
+        }
       }
-    }
-    case ("cancel", o) => RateLimit {
-      lobby ! CancelHook(uid)
-    }
-    case ("joinSeek", o) => RateLimit {
-      for {
-        id <- o str "d"
-        user <- member.user
-      } lobby ! BiteSeek(id, user)
-    }
+      case ("cancel", o) => RateLimit {
+        lobby ! CancelHook(uid)
+      }
+      case ("joinSeek", o) => RateLimit {
+        for {
+          id <- o str "d"
+          user <- member.user
+        } lobby ! BiteSeek(id, user)
+      }
 
-    case ("cancelSeek", o) => RateLimit {
-      for {
-        id <- o str "d"
-        user <- member.user
-      } lobby ! CancelSeek(id, user)
+      case ("cancelSeek", o) => RateLimit {
+        for {
+          id <- o str "d"
+          user <- member.user
+        } lobby ! CancelSeek(id, user)
+      }
     }
   }
 
