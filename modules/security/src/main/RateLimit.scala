8885265ba684674d1e60e27fd34128dbaa411cad
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-11-05 09:09:18 +0700

    rate limit hook form and post requests to prevent playban flood

--- modules/security/src/main/RateLimit.scala
+++ modules/security/src/main/RateLimit.scala
@@ -1,20 +1,40 @@
 package lila.security
 
-import scala.concurrent.duration.Duration
 import lila.memo.ExpireSetMemo
+import scala.concurrent.duration.Duration
+import ornicar.scalalib.Zero
 
-/** very simple side effect throttler
- that allows one call per duration,
- and discards the rest */
+/**
+ * very simple side effect throttler
+ * that allows one call per duration,
+ * and discards the rest
+ */
 final class RateLimitGlobal(duration: Duration) {
 
   private val durationMillis = duration.toMillis
 
   private var lastHit: Long = nowMillis - durationMillis - 10
 
-  def apply(op: => Unit) {
-    if (nowMillis > lastHit + durationMillis) {
+  def apply[A: Zero](op: => A): A = {
+    (nowMillis > lastHit + durationMillis) ?? {
       lastHit = nowMillis
+      op
+    }
+  }
+}
+
+/**
+ * very simple side effect throttler
+ * that allows one call per duration and per key,
+ * and discards the rest
+ */
+final class RateLimitByKey(duration: Duration) {
+
+  private val storage = new ExpireSetMemo(ttl = duration)
+
+  def apply[A: Zero](key: String)(op: => A): A = {
+    (!storage.get(key)) ?? {
+      storage put key
       op
     }
   }
