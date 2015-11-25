9e2e9a9284049b7a6ef4ee112d6652e6164d3451
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-11-25 13:35:20 +0700

    parameterize Dimension type with the possible values type

--- modules/coach/src/main/Dimension.scala
+++ modules/coach/src/main/Dimension.scala
@@ -1,14 +1,17 @@
 package lila.coach
 
-sealed abstract class Dimension(val key: String, val name: String)
+import lila.rating.PerfType
+import chess.{Color,Role}
+
+sealed abstract class Dimension[A](val key: String, val name: String)
 
 object Dimension {
 
-  case object Perf extends Dimension("perf", "Variant")
-  case object Phase extends Dimension("phase", "Game phase")
-  case object Result extends Dimension("result", "Result")
-  case object Color extends Dimension("color", "Color")
-  case object Opening extends Dimension("opening", "Opening")
-  case object OpponentStrength extends Dimension("opponentStrength", "Opponent strength")
-  case object PieceRole extends Dimension("pieceRole", "Piece moved")
+  case object Perf extends Dimension[PerfType]("perf", "Variant")
+  case object Phase extends Dimension[Phase]("phase", "Game phase")
+  case object Result extends Dimension[Result]("result", "Result")
+  case object Color extends Dimension[Color]("color", "Color")
+  case object Opening extends Dimension[Ecopening]("opening", "Opening")
+  case object OpponentStrength extends Dimension[RelativeStrength]("opponentStrength", "Opponent strength")
+  case object PieceRole extends Dimension[Role]("pieceRole", "Piece moved")
 }
