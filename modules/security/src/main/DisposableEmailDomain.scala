b2f683a1d28e336befeedbb2ffaf08e857b92927
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-12-08 13:51:03 +0700

    reduce disposable email logging

--- modules/security/src/main/DisposableEmailDomain.scala
+++ modules/security/src/main/DisposableEmailDomain.scala
@@ -10,8 +10,10 @@ final class DisposableEmailDomain(providerUrl: String) {
   private[security] def refresh {
     WS.url(providerUrl).get() map { res =>
       try {
+        val prevSize = domains.size
         domains = res.json.as[List[String]]
-        loginfo(s"[disposable email] registered ${domains.size} domains")
+        if (domains.size != prevSize)
+          loginfo(s"[disposable email] registered ${domains.size} domains")
       }
       catch {
         case e: Exception => logerr(s"Can't update disposable emails: $e")
