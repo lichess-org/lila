a10f8fda92f898380de0d48881fe25580548a4d6
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-12-10 21:43:11 +0700

    don't use alternate websocket ports on development

--- public/javascripts/big.js
+++ public/javascripts/big.js
@@ -57,9 +57,9 @@ lichess.StrongSocket.defaults = {
     lagTag: false, // jQuery object showing ping lag
     ignoreUnknownMessages: true,
     baseUrls: ['socket.' + document.domain].concat(
-      [9021, 9022, 9023, 9024, 9025, 9026, 9027, 9028, 9029].map(function(port) {
+      /lichess\.org/.test(document.domain) ? [9021, 9022, 9023, 9024, 9025, 9026, 9027, 9028, 9029].map(function(port) {
         return 'socket.' + document.domain + ':' + port;
-      })),
+      }) : []),
     onFirstConnect: $.noop,
     baseUrlKey: 'surl3'
   }
