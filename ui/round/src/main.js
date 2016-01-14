5efdc354677eaa3ff8e47c9b7ad11861210cf76b
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-01-14 10:35:42 +0700

    make people read the rules of racing kings

--- ui/round/src/main.js
+++ ui/round/src/main.js
@@ -7,9 +7,32 @@ module.exports = function(opts) {
   var controller = new ctrl(opts);
 
   m.module(opts.element, {
-    controller: function () { return controller; },
+    controller: function() {
+      return controller;
+    },
     view: view
   });
+
+  setTimeout(function() {
+    if (opts.data.game.variant.key === 'racingKings' &&
+      lichess.once('round.racing-kings')) lichess.hopscotch(function() {
+      hopscotch.configure({
+        i18n: {
+          doneBtn: 'OK, got it'
+        }
+      }).startTour({
+        id: "round-racing-kings",
+        showPrevButton: true,
+        steps: [{
+          title: "Racing Kings",
+          content: "This is a game of racing kings. " +
+            'Would you like to check out <a target="_blank" href="http://www.chessvariants.com/diffobjective.dir/racing.html">the rules</a>?',
+          target: "div.game_infos .variant-link",
+          placement: "bottom"
+        }]
+      });
+    });
+  }, 2000);
 
   return {
     socketReceive: controller.socket.receive,
