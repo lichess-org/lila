0b95fe0f1f3e82cb61ffcce2e6382911c501d321
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-01-15 11:51:51 +0700

    better tooltip updates in lobby

--- ui/lobby/src/ctrl.js
+++ ui/lobby/src/ctrl.js
@@ -35,11 +35,15 @@ module.exports = function(env) {
     this.vm.stepHooks = this.data.hooks.slice(0);
     if (this.vm.tab === 'real_time') {
       m.redraw();
-      setTimeout(function() {
-        lichess.userPowertip($('.ulpt', env.element), 'w');
-      }, 200);
+      updateUserPowertipsSoon();
     }
   }.bind(this);
+
+  var updateUserPowertipsSoon = function() {
+    setTimeout(function() {
+      lichess.userPowertip($('.ulpt', env.element), 'w');
+    }, 200);
+  };
 
   this.flushHooks = function(now) {
     clearTimeout(flushHooksTimeout);
@@ -62,6 +66,7 @@ module.exports = function(env) {
     if (tab === 'seeks' && tab !== this.vm.tab) xhr.seeks().then(this.setSeeks);
     this.vm.tab = store.tab.set(tab);
     this.vm.filterOpen = false;
+    updateUserPowertipsSoon();
   }.bind(this);
 
   this.setMode = function(mode) {
