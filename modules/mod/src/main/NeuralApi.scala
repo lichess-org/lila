90f3b7845d04690f23c46c832371ee792fc37f0c
clarkerubber <jamesjosephclarke@gmail.com>
2016-01-27 03:38:09 +1100

    Some changes to TensorFlow API

--- modules/mod/src/main/NeuralApi.scala
+++ modules/mod/src/main/NeuralApi.scala
@@ -1,8 +1,11 @@
 package lila.mod
 
 import lila.common.Chronometer
-import lila.evaluation.PlayerAssessment
 import lila.user.User
+import lila.game.{Game, GameRepo, Pov}
+import lila.analyse.{Analysis, AnalysisRepo, Accuracy}
+import chess.Color
+
 import play.api.libs.json._
 import play.api.libs.ws.WS
 import play.api.Play.current
@@ -12,6 +15,33 @@ private final class NeuralApi(
     assessApi: AssessApi) {
 
   import NeuralApi._
+
+  def apply(user: User): Fu[Option[Result]] = {
+    val games: Fu[List[Game]] = GameRepo.gamesForAssessment(user.id, 100)
+    val gameIds: Fu[List[String]] = games flatMap { gs => gs flatMap (g => g.id)}
+    val analysis: List[Fu[Option[Analysis]]] = games flatMap {
+      gs => gs map {
+        g => {
+          val i: Fu[Option[Analysis]] = AnalysisRepo.doneById(g.id)
+          i
+        }
+      }
+    }
+    ???
+  }
+  /*
+    GameRepo.gamesForAssessment(user.id, 100) flatMap { gs =>
+      (gs map { g => 
+        AnalysisRepo.doneById(g.id) flatMap {
+          case Some(a) => Chronometer.result {
+            callEndpoint(toJson(g, a, user))
+          }
+          case _       => fuccess(none)
+        }
+      })
+      ???
+    }
+    
 
   def apply(user: User): Fu[Option[Result]] =
     assessApi.getPlayerAssessmentsByUserId(user.id, 200) flatMap {
@@ -24,23 +54,28 @@ private final class NeuralApi(
         play.api.Logger("neural").warn(e.toString)
         none
     }
+  */
 
-  private def toJson(pas: List[PlayerAssessment]) = JsArray {
-    def bool2int(b: Boolean) = b.fold(1, 0)
-    pas map { pa =>
-      Json.obj(
-        "ser" -> pa.flags.suspiciousErrorRate.|>(bool2int),
-        "aha" -> pa.flags.alwaysHasAdvantage.|>(bool2int),
-        "cmt" -> pa.flags.consistentMoveTimes.|>(bool2int),
-        "nfm" -> pa.flags.noFastMoves.|>(bool2int),
-        "sha" -> pa.flags.suspiciousHoldAlert.|>(bool2int),
-        "sfAvg" -> pa.sfAvg,
-        "sfSd" -> pa.sfSd,
-        "mtAvg" -> pa.mtAvg,
-        "mtSd" -> pa.mtSd,
-        "blurs" -> pa.blurs
-      )
-    }
+  private def toJson(game: Game, analysis: Analysis, user: User) = JsArray {
+    val color = game.player(user).fold(Color.white)(_.color)
+    val player = game.player(color)
+    val pov = Pov(game, color)
+    Json.obj(
+      // Main Image
+      "moveTimes" -> game.moveTimes(color),
+      "evals" -> Accuracy.evalsList(pov, analysis),
+      "diffs" -> Accuracy.diffsList(pov, analysis),
+      // Modifiers
+      //"cpAvg" -> Accuracy.mean(pov, analysis),
+      "blurs" -> game.player(color).blurs.toDouble / game.playerMoves(color),
+      "holdAlert" -> player.holdAlert map {
+        case Some(_) => true
+        case _       => false
+      },
+      "isWinner" -> player.isWinner,
+      "rating" -> player.rating,
+      "isProvisional" -> player.provisional
+    )
   }
 
   private implicit val answerReads = Json.reads[Answer]
@@ -58,6 +93,6 @@ private final class NeuralApi(
 
 object NeuralApi {
 
-  case class Answer(decision: String, cheatPercent: Int, nonCheatPercent: Int)
+  case class Answer(decision: String, cheatPercent: Int)
   case class Result(answer: Answer, millis: Int)
 }
