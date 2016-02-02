bd7694f105897d9cbc579699d4e3f93ea3f8a1d3
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-02-02 21:11:45 +0100

    Fetch on board changes

--- ui/explorer/src/ctrl.js
+++ ui/explorer/src/ctrl.js
@@ -1,12 +1,25 @@
 var chessground = require('chessground');
 var m = require('mithril');
 
+function fetchApi(ctrl) {
+  m.request({
+    method: 'GET',
+    url: 'http://130.211.90.176/bullet?fen=' + ctrl.chessground.getFen() + ' w KQkq - 0 1'
+  }).then(ctrl.api);
+}
+
 module.exports = function(cfg) {
+  var self = this;
+
+  this.api = m.prop();
+
   this.chessground = new chessground.controller({
+    events: {
+      change: function() {
+        fetchApi(self);
+      }
+    }
   });
 
-  this.api = m.request({
-    method: 'GET',
-    url: 'http://localhost:9000/bullet?fen=rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-  });
+  fetchApi(self);
 };
