1545e71d9301c065c82c42342d314776715c1ce2
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-02-02 14:08:31 +0700

    support disposable email regex

--- modules/security/src/main/DisposableEmailDomain.scala
+++ modules/security/src/main/DisposableEmailDomain.scala
@@ -1,27 +1,38 @@
 package lila.security
 
+import play.api.libs.json._
 import play.api.libs.ws.WS
 import play.api.Play.current
 
 final class DisposableEmailDomain(providerUrl: String) {
 
-  private var domains = List.empty[String]
+  private type Matcher = String => Boolean
+
+  private var matchers = List.empty[Matcher]
 
   private[security] def refresh {
     WS.url(providerUrl).get() map { res =>
-      try {
-        val prevSize = domains.size
-        domains = res.json.as[List[String]]
-        if (domains.size != prevSize)
-          loginfo(s"[disposable email] registered ${domains.size} domains")
-      }
-      catch {
-        case e: Exception => logerr(s"Can't update disposable emails: $e")
-      }
+      setDomains(res.json)
     }
   }
 
-  def apply(domain: String) = domains.exists { d =>
-    domain == d || domain.endsWith(s".$d")
+  private[security] def setDomains(json: JsValue): Unit = try {
+    val ds = json.as[List[String]]
+    if (ds.size != matchers.size)
+      loginfo(s"[disposable email] registered ${matchers.size} -> ${ds.size} domains")
+    matchers = ds.map { d =>
+      val regex = s"""(.+\\.|)${d.replace(".", "\\.")}"""
+      makeMatcher(regex)
+    }
   }
+  catch {
+    case e: Exception => logerr(s"Can't update disposable emails: $e")
+  }
+
+  private def makeMatcher(regex: String): Matcher = {
+    val matcher = regex.r.pattern matcher _
+    (s: String) => matcher(s).matches
+  }
+
+  def apply(domain: String) = matchers exists { _(domain) }
 }
