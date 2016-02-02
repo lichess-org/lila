6f5587197f89ab572d25bd642ff1c7427ce8329f
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-02-02 14:34:53 +0700

    regexify the disposable email domain database

--- modules/security/src/test/DisposableEmailDomainTest.scala
+++ modules/security/src/test/DisposableEmailDomainTest.scala
@@ -11,6 +11,8 @@ class DisposableEmailDomainTest extends Specification {
   "disposable email domain" should {
     "simple" in {
       d("gmail.com") must beFalse
+      d("hotmail.com") must beFalse
+      d("live.com") must beFalse
       d("docmail.cz") must beTrue
     }
     "suffix" in {
@@ -25,6 +27,18 @@ class DisposableEmailDomainTest extends Specification {
       d("domforfb29.tk") must beTrue
       d("foo.domforfb11.tk") must beTrue
       d("foo2.domforfb11.tk") must beTrue
+      d("10minutemail.com") must beTrue
+      d("12hourmail.de") must beTrue
+      d("30minutesmail.co") must beTrue
+      d("centermail.net") must beTrue
+      d("center-mail.info") must beTrue
+      d("discard.com") must beTrue
+      d("discardmail.com") must beTrue
+      d("discartmail.com") must beTrue
+      d("guerillamail.com") must beTrue
+      d("guerrillamail.com") must beTrue
+      d("jetable.fr.nf") must beTrue
+      d("notjetable.fr") must beFalse
     }
   }
 }
