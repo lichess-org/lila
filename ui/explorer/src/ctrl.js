d624e4fc0ff9221da3640fc6c91abe9d2e048d6c
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-02-02 19:00:07 +0100

    Display results in a table

--- ui/explorer/src/ctrl.js
+++ ui/explorer/src/ctrl.js
@@ -1,6 +1,12 @@
 var chessground = require('chessground');
+var m = require('mithril');
 
 module.exports = function(cfg) {
   this.chessground = new chessground.controller({
+  });
+
+  this.api = m.request({
+    method: 'GET',
+    url: 'http://localhost:9000/bullet?fen=rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
   });
 };
