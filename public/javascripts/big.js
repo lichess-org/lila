7538b704dec1e662ea93c4166529b11990350564
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-02-06 12:34:38 +0700

    backport material spinner from challenge branch

--- public/javascripts/big.js
+++ public/javascripts/big.js
@@ -332,6 +332,7 @@ lichess.trans = function(i18n) {
     return str;
   };
 };
+lichess.spinnerHtml = '<div class="spinner"><svg viewBox="0 0 40 40"><circle cx=20 cy=20 r=18 fill="none"></circle></svg></div>';
 lichess.hopscotch = function(f) {
   var baseUrl = $('body').data('asset-url');
   $('head').append($('<link rel="stylesheet" type="text/css" />')
