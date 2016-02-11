54fc4075a59c52c19dfff7be026cbec832843433
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-02-11 08:08:22 +0700

    simplify round user JSON

--- modules/user/src/main/JsonView.scala
+++ modules/user/src/main/JsonView.scala
@@ -1,7 +1,7 @@
 package lila.user
 
 import lila.common.PimpedJson._
-import lila.rating.{ Perf, Glicko }
+import lila.rating.{ Perf, Glicko, PerfType }
 import play.api.libs.json._
 import User.PlayTime
 
@@ -14,27 +14,23 @@ final class JsonView(isOnline: String => Boolean) {
       "rd" -> o.glicko.deviation.toInt,
       "prog" -> o.progress)
   }
-  private implicit val perfsWrites: Writes[Perfs] = Writes { o =>
-    JsObject(o.perfsMap.toList map {
-      case (name, perf) => name -> perfWrites.writes(perf)
-    })
-  }
   private implicit val profileWrites = Json.writes[Profile]
   private implicit val playTimeWrites = Json.writes[PlayTime]
 
-  def apply(u: User, extended: Boolean) = Json.obj(
+  def apply(u: User, onlyPerf: Option[PerfType] = None) = Json.obj(
     "id" -> u.id,
-    "username" -> u.username
-  ) ++ extended.??(Json.obj(
-      "title" -> u.title,
-      "online" -> isOnline(u.id),
-      "engine" -> u.engine,
-      "booster" -> u.booster,
-      "language" -> u.lang,
-      "profile" -> u.profile.??(profileWrites.writes).noNull,
-      "perfs" -> u.perfs,
-      "createdAt" -> u.createdAt,
-      "seenAt" -> u.seenAt,
-      "playTime" -> u.playTime
-    )).noNull
+    "username" -> u.username,
+    "title" -> u.title,
+    "online" -> isOnline(u.id),
+    "engine" -> u.engine,
+    "booster" -> u.booster,
+    "language" -> u.lang,
+    "profile" -> u.profile.??(profileWrites.writes).noNull,
+    "perfs" -> JsObject(u.perfs.perfsMap collect {
+      case (key, perf) if onlyPerf.fold(true)(_.key == key) => key -> perfWrites.writes(perf)
+    }),
+    "createdAt" -> u.createdAt,
+    "seenAt" -> u.seenAt,
+    "playTime" -> u.playTime
+  ).noNull
 }
