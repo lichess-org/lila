094902c651e28400971250781293cb9fe3be9ab2
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-02-15 16:38:21 +0700

    no longer open analysis in mobile app - closes #1600

--- public/javascripts/big.js
+++ public/javascripts/big.js
@@ -2313,7 +2313,6 @@ lichess.numberFormat = (function() {
 
   function startAnalyse(element, cfg) {
     var data = cfg.data;
-    if (lichess.openInMobileApp(data.game.id)) return;
     if (data.chat) $('#chat').chat({
       messages: data.chat,
       initialNote: data.note,
