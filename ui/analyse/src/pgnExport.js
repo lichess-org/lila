b045a6d87f1955d992d430ae4ee547d0b7096a67
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-02-19 16:35:19 +0700

    newlines in analysis PGN export

--- ui/analyse/src/pgnExport.js
+++ ui/analyse/src/pgnExport.js
@@ -5,11 +5,11 @@ function renderStepsTxt(steps) {
   if (!steps[0].san) steps = steps.slice(1);
   if (!steps[0]) return '';
   var s = steps[0].ply % 2 === 1 ? '' : Math.floor((steps[0].ply + 1) / 2) + '... ';
-  steps.forEach(function(step) {
+  steps.forEach(function(step, i) {
     if (step.ply === 0) return;
     if (step.ply % 2 === 1) s += ((step.ply + 1) / 2) + '. '
     else s += '';
-    s += step.san + ' ';
+    s += step.san + ((i + 11) % 10 === 0 ? '\n' : ' ');
   });
   return s.trim();
 }
