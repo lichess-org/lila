0a3e1f6144e8a3b041f726f373129d7fd8f3dbce
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-02-24 10:56:30 +0700

    wrap analysis PGN every 8 plies

--- ui/analyse/src/pgnExport.js
+++ ui/analyse/src/pgnExport.js
@@ -9,7 +9,7 @@ function renderStepsTxt(steps) {
     if (step.ply === 0) return;
     if (step.ply % 2 === 1) s += ((step.ply + 1) / 2) + '. '
     else s += '';
-    s += step.san + ((i + 11) % 10 === 0 ? '\n' : ' ');
+    s += step.san + ((i + 9) % 8 === 0 ? '\n' : ' ');
   });
   return s.trim();
 }
