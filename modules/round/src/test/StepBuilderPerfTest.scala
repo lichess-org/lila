f14b94e783cb7ebce499c2175761a5e8b22f122b
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-02-26 09:04:37 +0700

    step builder with opening perfs test

--- modules/round/src/test/StepBuilderPerfTest.scala
+++ modules/round/src/test/StepBuilderPerfTest.scala
@@ -7,27 +7,28 @@ import Pos._
 
 class StepBuilderPerfTest extends Specification {
 
-  val nb = 500
+  sequential
+
+  val nb = 200
   val gameMoves = Fixtures.prod500standard.take(nb).map {
     _.split(' ').toList
   }
-  val iterations = 5
+  val iterations = 10
   // val nb = 1
   // val iterations = 1
 
-  def runOne(moves: List[String]) =
-    StepBuilder("abcd1234", moves, chess.variant.Standard, None, format.Forsyth.initial)
-  def run { gameMoves foreach runOne }
+  def runOne(withOpening: Boolean)(moves: List[String]) =
+    StepBuilder("abcd1234", moves, chess.variant.Standard, None, format.Forsyth.initial, withOpening = withOpening)
+  def run(withOpening: Boolean) { gameMoves foreach runOne(withOpening) }
 
-  "playing a game" should {
-    "many times" in {
-      runOne(gameMoves.head)
+  def runTests(withOpening: Boolean) = {
+      runOne(withOpening)(gameMoves.head)
       println("warming up")
-      run
+      run(withOpening)
       println("running tests")
       val durations = for (i ← 1 to iterations) yield {
         val start = System.currentTimeMillis
-        run
+        run(withOpening)
         val duration = System.currentTimeMillis - start
         println(s"$nb games in $duration ms")
         duration
@@ -37,6 +38,14 @@ class StepBuilderPerfTest extends Specification {
       println(s"Average = $moveMicros microseconds per game")
       println(s"          ${1000000 / moveMicros} games per second")
       true === true
+  }
+
+  "playing a game" should {
+    "without opening" in {
+      runTests(false)
+    }
+    "with opening" in {
+      runTests(true)
     }
   }
 }
