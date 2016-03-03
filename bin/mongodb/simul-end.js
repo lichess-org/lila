6c85f7de0bc0c3c7554e404387e4a8afe8de96f3
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-03-04 00:05:11 +0700

    mongodb script to terminate simuls

---
+++ bin/mongodb/simul-end.js
@@ -0,0 +1,26 @@
+var ids = [];
+
+db.simul.find({
+  status: 20
+}).forEach(function(s) {
+  var finished = true;
+  printjson(s);
+  s.pairings.forEach(function(p) {
+    if (p.status < 25 && p.status !== 10) finished = false;
+  });
+  if (finished) ids.push(s._id);
+});
+
+db.simul.update({
+  _id: {
+    $in: ids
+  }
+}, {
+  $set: {
+    status: NumberInt(30),
+    finishedAt: new Date(),
+    hostGameId: null
+  }
+}, {
+  multi: 1
+});
