c33ad5ae1c2b9673e28eb3de643a3ea6ebde1192
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-03-09 10:05:30 +0700

    build RM-play-json locally as well

--- bin/build-deps.sh
+++ bin/build-deps.sh
@@ -5,38 +5,37 @@ dir=$(mktemp -d)
 echo "Building in $dir"
 cd "$dir"
 
-rm -rf reactivemongo
 git clone https://github.com/ReactiveMongo/ReactiveMongo
 cd ReactiveMongo
 git checkout 466f214da17ad79f2055f274ef674eeb636ffd64
 sbt publish-local
 cd ..
 
-rm -rf play-reactivemongo
 git clone https://github.com/ReactiveMongo/Play-ReactiveMongo
 cd Play-ReactiveMongo
 sbt publish-local
 cd ..
 
-rm -rf scalalib
+git clone https://github.com/ReactiveMongo/Reactivemongo-Play-Json
+cd Reactivemongo-Play-Json
+sbt publish-local
+cd ..
+
 git clone https://github.com/ornicar/scalalib
 cd scalalib
 sbt publish-local
 cd ..
 
-rm -rf scala-kit
 git clone https://github.com/ornicar/scala-kit --branch lichess-fork
 cd scala-kit
 sbt -Dversion=1.2.11-THIB publish-local
 cd ..
 
-rm -rf maxmind-geoip2-scala
 git clone https://github.com/ornicar/maxmind-geoip2-scala --branch customBuild
 cd maxmind-geoip2-scala
 sbt publish-local
 cd ..
 
-rm -rf hasher
 git clone https://github.com/Nycto/Hasher
 cd Hasher
 sbt publish-local
