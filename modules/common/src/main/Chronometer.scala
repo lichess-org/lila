3bfbcef6253a4d212951727c58b55fe370d8f886
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-03-11 09:52:45 +0700

    remove Chronometer.kamon

--- modules/common/src/main/Chronometer.scala
+++ modules/common/src/main/Chronometer.scala
@@ -1,18 +1,11 @@
 package lila.common
 
-import kamon.Kamon
-
 object Chronometer {
 
   case class Lap[A](result: A, millis: Int) {
 
     def logIfSlow(threshold: Int, logger: String)(msg: A => String) = {
       if (millis >= threshold) play.api.Logger(logger).debug(s"<${millis}ms> ${msg(result)}")
-      this
-    }
-
-    def kamon(histogram: String) = {
-      Kamon.metrics histogram histogram record millis
       this
     }
   }
@@ -28,11 +21,6 @@ object Chronometer {
       lap foreach { l =>
         lila.mon.recPath(path)(l.millis)
       }
-      this
-    }
-
-    def kamon(histogram: String) = {
-      lap.foreach(_ kamon histogram)
       this
     }
 
