d9ea222ab1a12d7f855319eed36e168ec3f4943d
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-03-18 11:35:12 +0700

    don't build pushy-scala

--- bin/build-deps.sh
+++ bin/build-deps.sh
@@ -5,11 +5,6 @@ dir=$(mktemp -d)
 echo "Building in $dir"
 cd "$dir"
 
-git clone https://github.com/msimav/pushy-scala
-cd pushy-scala
-sbt publish-local
-cd ..
-
 git clone https://github.com/ornicar/scalalib
 cd scalalib
 sbt publish-local
