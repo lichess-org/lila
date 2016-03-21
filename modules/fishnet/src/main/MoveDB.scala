e5e8e7cabcd2cd980306e49c5f8c02619bd323dc
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-03-21 12:00:12 +0700

    fix fishnet move DB

--- modules/fishnet/src/main/MoveDB.scala
+++ modules/fishnet/src/main/MoveDB.scala
@@ -27,11 +27,13 @@ private final class MoveDB {
   def size = coll.size
 
   def oldestNonAcquired = coll.foldLeft(none[Move]) {
-    case (acc, (_, m)) if m.nonAcquired => Some {
-      acc.fold(m) { a =>
-        if (m.createdAt isBefore a.createdAt) m else a
+    case (acc, (_, m)) =>
+      if (m.nonAcquired) Some {
+        acc.fold(m) { a =>
+          if (m.createdAt isBefore a.createdAt) m else a
+        }
       }
-    }
+      else acc
   }
 
   def updateOrGiveUp(move: Move) =
