c683a0ec186e20117ffee65cb5ada68b77509660
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-03-22 17:58:43 +0700

    use move trace in the monitor subscriber

--- modules/round/src/main/MoveMonitor.scala
+++ modules/round/src/main/MoveMonitor.scala
@@ -8,30 +8,15 @@ private final class MoveMonitor(
     system: ActorSystem,
     channel: ActorRef) {
 
-  Kamon.metrics.subscribe("trace", "**", system.actorOf(Props(new Actor {
+  Kamon.metrics.subscribe("trace", "round.move.trace", system.actorOf(Props(new Actor {
     def receive = {
-      case tick: TickMetricSnapshot => tick.metrics foreach {
-        case (entity, snapshot) =>
-          println("-------- " + entity)
-          println(snapshot)
-          println(snapshot.histogram("histogram"))
-          println(snapshot.histogram("histogram").foreach { h =>
-            println(h.sum)
-            println(h.numberOfMeasurements)
-          })
+      case tick: TickMetricSnapshot => tick.metrics.collectFirst {
+        case (entity, snapshot) if entity.category == "trace" => snapshot
+      } flatMap (_ histogram "elapsed-time") foreach { h =>
+        if (!h.isEmpty) channel ! lila.socket.Channel.Publish(
+          lila.socket.Socket.makeMessage("mlat", (h.sum / h.numberOfMeasurements / 1000000).toInt)
+        )
       }
     }
   })))
-
-  // Kamon.metrics.subscribe("histogram", "round.move.trace.elapsed-time", system.actorOf(Props(new Actor {
-  //   def receive = {
-  //     case tick: TickMetricSnapshot => tick.pp.metrics.pp.collectFirst {
-  //       case (entity, snapshot) if entity.category == "histogram" => snapshot
-  //     } flatMap (_ histogram "histogram") foreach { h =>
-  //       if (!h.pp.isEmpty.pp) channel ! lila.socket.Channel.Publish(
-  //         lila.socket.Socket.makeMessage("mlat", (h.sum / h.numberOfMeasurements / 1000000).toInt)
-  //       )
-  //     }
-  //   }
-  // })))
 }
