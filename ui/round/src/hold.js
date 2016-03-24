04e9c0c1ee20a700f912e0e5396689477f289d28
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-03-24 23:30:29 +0700

    tweak round js

--- ui/round/src/hold.js
+++ ui/round/src/hold.js
@@ -47,9 +47,7 @@ function find(el, d) {
 
 module.exports = {
   applies: function(data) {
-    return data.game.variant.key === 'standard' && !(
-      data.player.user && data.player.user.title
-    );
+    return data.game.variant.key === 'standard';
   },
   register: register,
   find: find
