91ffacb5f1eaadba930e9189a47b8d422e263a20
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-04-03 23:37:17 +0700

    fix build-deps

--- bin/build-deps.sh
+++ bin/build-deps.sh
@@ -5,20 +5,20 @@ dir=$(mktemp -d)
 echo "Building in $dir"
 cd "$dir"
 
-# git clone https://github.com/gilt/gfc-semver
-# cd gfc-semver
-# sbt publish-local
-# cd ..
+git clone https://github.com/gilt/gfc-semver
+cd gfc-semver
+sbt publish-local
+cd ..
 
-# git clone https://github.com/ornicar/ReactiveMongo --branch lichess
-# cd ReactiveMongo
-# sbt publish-local
-# cd ..
+git clone https://github.com/ornicar/ReactiveMongo --branch lichess
+cd ReactiveMongo
+sbt publish-local
+cd ..
 
-# git clone https://github.com/ornicar/scalalib
-# cd scalalib
-# sbt publish-local
-# cd ..
+git clone https://github.com/ornicar/scalalib
+cd scalalib
+sbt publish-local
+cd ..
 
 git clone https://github.com/ornicar/scala-kit --branch lichess-fork
 cd scala-kit
@@ -26,14 +26,14 @@ git checkout 75fb61c8306a7a3db4c5dae6f110972130e7c6bc
 sbt -Dversion=1.2.11-THIB2 publish-local
 cd ..
 
-# git clone https://github.com/ornicar/maxmind-geoip2-scala --branch customBuild
-# cd maxmind-geoip2-scala
-# sbt publish-local
-# cd ..
+git clone https://github.com/ornicar/maxmind-geoip2-scala --branch customBuild
+cd maxmind-geoip2-scala
+sbt publish-local
+cd ..
 
-# git clone https://github.com/Nycto/Hasher
-# cd Hasher
-# sbt publish-local
-# cd ..
+git clone https://github.com/Nycto/Hasher
+cd Hasher
+sbt publish-local
+cd ..
 
 rm -rf "$dir"
