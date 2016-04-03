784f083082ab0cb14456706f97a43c08b8fb754e
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-04-03 11:59:27 +0700

    fix tournament manager

--- modules/tournament/src/main/crud/CrudApi.scala
+++ modules/tournament/src/main/crud/CrudApi.scala
@@ -38,7 +38,7 @@ final class CrudApi {
     TournamentRepo insert tour inject tour
   }
 
-  private val empty = Tournament.make(
+  private def empty = Tournament.make(
     createdByUserId = "lichess",
     clock = TournamentClock(0, 0),
     minutes = 0,
