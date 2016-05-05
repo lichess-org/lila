3f27f950d0a4a8b3390190a13573d1cd00b23da3
Jimmie Elvenmark <flugsio@gmail.com>
2016-05-05 17:24:20 +0200

    fix #1847 crazyhouse: static piece positions in hand

--- ui/round/src/crazy/crazyDrag.js
+++ ui/round/src/crazy/crazyDrag.js
@@ -6,8 +6,9 @@ module.exports = function(ctrl, e) {
   if (e.button !== undefined && e.button !== 0) return; // only touch or left click
   if (ctrl.replaying() || !game.isPlayerPlaying(ctrl.data)) return;
   var role = e.target.getAttribute('data-role'),
-    color = e.target.getAttribute('data-color');
-  if (!role || !color) return;
+    color = e.target.getAttribute('data-color'),
+    number = e.target.getAttribute('data-nb');
+  if (!role || !color || number === '0') return;
   e.stopPropagation();
   e.preventDefault();
   var key;
