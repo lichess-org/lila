d34b2a5a56709234762fc452f7d2fe7ca88995d4
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-05-22 12:11:59 +0200

    fix study chapter tour race condition

--- ui/analyse/src/study/chapterNewForm.js
+++ ui/analyse/src/study/chapterNewForm.js
@@ -38,7 +38,8 @@ module.exports = {
       vm.open = true;
       loadVariants();
       vm.initial(false);
-      if (lichess.once('insight-tour-chapter')) tours.chapter(vm.tab);
+      if (lichess.once('insight-tour-chapter'))
+        setTimeout(partial(tours.chapter, vm.tab), 1000);
     };
     var close = function() {
       vm.open = false;
