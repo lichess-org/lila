86491065c8368e2cf9f5195aea7a8587b968a12b
Unihedron <unihedron@unihedro.com>
2016-05-24 20:56:30 +0800

    Add and fix capturing group

--- app/templating/StringHelper.scala
+++ app/templating/StringHelper.scala
@@ -52,7 +52,7 @@ trait StringHelper { self: NumberHelper =>
   // https://github.com/ornicar/lila/blob/master/modules/security/src/main/DataForm.scala#L34-L44
   // Example: everyone says @ornicar is a pretty cool guy
   // False example: Write to lichess.contact@gmail.com, @1
-  private val atUsernameRegex = """\B@(?>[a-zA-Z_-][\w-]{1,19})(?U)(?![\w-])""".r
+  private val atUsernameRegex = """\B@(?>([a-zA-Z_-][\w-]{1,19}))(?U)(?![\w-])""".r
 
   private val urlRegex = """(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s<>]+|\(([^\s<>]+|(\([^\s<>]+\)))*\))+(?:\(([^\s<>]+|(\([^\s<>]+\)))*\)|[^\s`!\[\]{};:'".,<>?«»“”‘’]))""".r
 
@@ -62,7 +62,7 @@ trait StringHelper { self: NumberHelper =>
     * @return The text as a HTML hyperlink
     */
   def addUserProfileLinks(text: String) = atUsernameRegex.replaceAllIn(text, m => {
-    var user = m group 2
+    var user = m group 1
     var url = s"$netDomain/@/$user"
 
     s"""<a href="${prependHttp(url)}">@$user</a>"""
