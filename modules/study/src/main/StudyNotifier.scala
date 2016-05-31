261cb181a320ce51dc8ae803ef209f9ac6785fa8
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-05-31 14:38:28 +0200

    simplify study notifier

--- modules/study/src/main/StudyNotifier.scala
+++ modules/study/src/main/StudyNotifier.scala
@@ -4,9 +4,8 @@ import akka.actor._
 import akka.pattern.ask
 
 import lila.hub.actorApi.HasUserId
-import lila.hub.actorApi.message.LichessThread
 import lila.notify.InvitedToStudy.InvitedBy
-import lila.notify.{InvitedToStudy, NotifyApi, Notification}
+import lila.notify.{ InvitedToStudy, NotifyApi, Notification }
 import lila.relation.RelationApi
 import makeTimeout.short
 import org.joda.time.DateTime
@@ -16,16 +15,17 @@ private final class StudyNotifier(
     relationApi: RelationApi) {
 
   def apply(study: Study, invited: lila.user.User, socket: ActorRef) =
-  relationApi.fetchBlocks(invited.id, study.ownerId).flatMap {
-    blocked =>
-      socket ? HasUserId(invited.id) mapTo manifest[Boolean] map { isPresent =>
-        study.owner.ifFalse(isPresent) foreach { owner =>
-          if (!blocked && !isPresent) {
-            val notificationContent = InvitedToStudy(InvitedToStudy.InvitedBy(owner.id), InvitedToStudy.StudyName(study.name), InvitedToStudy.StudyId(study.id))
-            val notification = Notification(Notification.Notifies(invited.id), notificationContent, Notification.NotificationRead(false), DateTime.now())
-            notifyApi.addNotification(notification)
+    relationApi.fetchBlocks(invited.id, study.ownerId).flatMap {
+      case true => funit
+      case false =>
+        socket ? HasUserId(invited.id) mapTo manifest[Boolean] map { isPresent =>
+          study.owner.ifFalse(isPresent) foreach { owner =>
+            if (!isPresent) {
+              val notificationContent = InvitedToStudy(InvitedToStudy.InvitedBy(owner.id), InvitedToStudy.StudyName(study.name), InvitedToStudy.StudyId(study.id))
+              val notification = Notification(Notification.Notifies(invited.id), notificationContent, Notification.NotificationRead(false), DateTime.now())
+              notifyApi.addNotification(notification)
+            }
           }
         }
-      }
-  }
+    }
 }
