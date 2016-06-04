911d0a40e2d7780a25cdd5c2b82169b2b80dbc06
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-06-04 10:20:59 +0200

    cache analysis ACPL section when complete

--- ui/analyse/src/acpl.js
+++ ui/analyse/src/acpl.js
@@ -14,9 +14,19 @@ var advices = [
   ['blunder', 'Blunders', '??']
 ];
 
+var cached = false;
+
 module.exports = function(ctrl) {
   var d = ctrl.data;
   if (!d.analysis) return;
+
+  var first = ctrl.vm.mainline[0].eval;
+  if (first.cp || first.mate) {
+    if (cached) return {
+      subtree: 'retain'
+    };
+    else cached = true;
+  }
 
   return m('div.advice_summary', {
     config: function(el, isUpdate) {
