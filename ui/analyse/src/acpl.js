5428bca962bc950241c0adc859dabd964ae3c902
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-06-05 11:58:23 +0200

    user badge on ACPL display

--- ui/analyse/src/acpl.js
+++ ui/analyse/src/acpl.js
@@ -5,7 +5,7 @@ function renderPlayer(data, color) {
   var p = getPlayer(data, color);
   if (p.name) return p.name;
   if (p.ai) return 'Stockfish level ' + p.ai;
-  if (p.user) return m('a.user_link', {
+  if (p.user) return m('a.user_link.ulpt', {
     href: '/@/' + p.user.username
   }, p.user.username);
   return 'Anonymous';
