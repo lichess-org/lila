c6ebd7ca2cde0f60d415ab6e809db4b31b996d48
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-06-18 17:01:01 +0200

    improve spam detection

--- modules/security/src/main/Spam.scala
+++ modules/security/src/main/Spam.scala
@@ -7,22 +7,36 @@ object Spam {
     blacklist exists text.contains
   }
 
+  val in = "moc.satimulocni".reverse
+  val cb = "tob-ssehc".reverse
+
   private val blacklist = List(
     /* While links to other chess websites are welcome,
      * refer links grant the referrer money,
      * effectively inducing spam */
     "velocitychess.com/ref/",
     "chess24.com?ref=",
-    "chess.com/register?refId="
+    "chess.com/register?refId=",
+    /* links to cheats */
+    cb,
+    s"${in}/pages/lichess-bot"
   )
 
   def replace(text: String) = replacements.foldLeft(text) {
     case (t, (regex, rep)) => regex.replaceAllIn(t, rep)
   }
 
+  val tosUrl = "lichess.org/terms-of-service"
+
+  val protocol = """(https?://)?"""
+
   private val replacements = List(
-    """velocitychess.com/ref/\w+""".r -> "velocitychess.com",
-    """chess24.com?ref=\w+""".r -> "chess24.com",
-    """chess.com/register?refId=\w+""".r -> "chess.com/register"
-  )
+    s"""${protocol}velocitychess.com/ref/\\w+""" -> "velocitychess.com",
+    s"""${protocol}chess24.com?ref=\\w+""" -> "chess24.com",
+    s"""${protocol}chess.com/register?refId=\\w+""" -> "chess.com",
+    s"""${protocol}${cb}(\\.com)?[^\\s]*""" -> tosUrl,
+    s"""${protocol}${in}[^\\s]+""" -> tosUrl
+  ).map {
+    case (regex, replacement) => regex.r -> replacement
+  }
 }
