d3af031a502a765cfd9dcd4657a6746e3157329a
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-06-25 13:41:35 +0200

    rewrite queen lesson

--- ui/learn/src/lesson/queen.js
+++ ui/learn/src/lesson/queen.js
@@ -6,71 +6,31 @@ module.exports = {
   subtitle: 'Queen = rook + bishop.',
   image: util.assetUrl + 'images/learn/pieces/Q.svg',
   stages: [{
-    goal: 'Use the queen like a rook!',
-    fen: '8/8/8/8/8/8/8/3Q4 w - - 0 1',
-    items: {
-      d8: 'apple',
-      h8: 'flower'
-    },
-    nbMoves: 1,
-    shapes: [arrow('d1d8'), arrow('d8h8')]
+    goal: 'Grab all the stars!',
+    fen: '8/8/8/8/8/8/4Q3/8 w - - 0 1',
+    apples: 'e5 b8',
+    nbMoves: 2,
+    shapes: [arrow('e2e5'), arrow('e5b8')]
   }, {
-    goal: 'Use the queen like a bishop!',
-    fen: '8/8/8/8/8/8/8/3Q4 w - - 0 1',
-    items: {
-      h5: 'apple',
-      e8: 'flower'
-    },
-    nbMoves: 1,
-    shapes: [arrow('d1h5'), arrow('h5e8')]
+    goal: 'Grab all the stars!',
+    fen: '8/8/8/8/3Q4/8/8/8 w - - 0 1',
+    apples: 'a3 f2 f8 h3',
+    nbMoves: 4
   }, {
-    goal: 'Grab the stars,<br>then go to the castle!',
-    fen: '8/8/8/8/8/2Q5/8/8 w - - 0 1',
-    items: {
-      b2: 'apple',
-      b7: 'apple',
-      e7: 'apple',
-      b7: 'apple',
-      g5: 'apple',
-      g8: 'apple',
-      h8: 'apple',
-      g2: 'flower'
-    },
+    goal: 'Grab all the stars!',
+    fen: '8/8/8/8/2Q5/8/8/8 w - - 0 1',
+    apples: 'a3 d6 f1 f8 g3 h6',
+    nbMoves: 6
+  }, {
+    goal: 'Grab all the stars!',
+    fen: '8/6q1/8/8/8/8/8/8 b - - 0 1',
+    apples: 'a2 b5 d3 g1 g8 h2 h5',
     nbMoves: 7
   }, {
     goal: 'The queen is the strong!',
-    fen: '8/8/8/8/8/8/8/7Q w - - 0 1',
-    items: {
-      a3: 'apple',
-      b2: 'apple',
-      b3: 'apple',
-      b7: 'apple',
-      c3: 'apple',
-      c6: 'apple',
-      c7: 'apple',
-      d7: 'apple',
-      e5: 'apple',
-      f4: 'apple',
-      f5: 'apple',
-      g5: 'apple',
-      f8: 'flower'
-    },
-    nbMoves: 13
-  }, {
-    goal: 'Grab the stars,<br>then go to the castle!',
-    fen: '8/8/8/8/8/8/8/Q7 w - - 0 1',
-    items: {
-      a5: 'apple',
-      b3: 'apple',
-      c1: 'apple',
-      d7: 'apple',
-      e2: 'apple',
-      f8: 'apple',
-      g6: 'apple',
-      h4: 'apple',
-      h8: 'flower'
-    },
-    nbMoves: 16
+    fen: '8/8/8/8/8/8/8/4q3 b - - 0 1',
+    apples: 'a6 d1 f2 f6 g6 g8 h1 h4',
+    nbMoves: 9
   }].map(util.toStage),
   complete: 'Congratulations! Queens have no secrets for you.'
 };
