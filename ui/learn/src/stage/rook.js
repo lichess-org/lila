f4636d2a718bbc41c232e2572b7f036003db80f9
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-07-03 21:10:33 +0200

    simplify rook stage

--- ui/learn/src/stage/rook.js
+++ ui/learn/src/stage/rook.js
@@ -21,18 +21,18 @@ module.exports = {
     goal: 'Grab all the stars!',
     fen: '8/2R5/8/8/8/8/8/8 w - - 0 1',
     apples: 'c5 g5',
-    nbMoves: 3,
+    nbMoves: 2,
     shapes: [arrow('c7c5'), arrow('c5g5')]
   }, {
     goal: 'The fewer moves you make,<br>the more points you win!',
     fen: '8/8/8/8/3R4/8/8/8 w - - 0 1',
-    apples: 'a4 b3 b8 d8 g3 g4',
-    nbMoves: 7
+    apples: 'a4 g3 g4',
+    nbMoves: 3
   }, {
     goal: 'Grab all the stars!',
     fen: '7R/8/8/8/8/8/8/8 w - - 0 1',
-    apples: 'a2 f2 f8 g1 g7 g8 h7',
-    nbMoves: 7
+    apples: 'f8 g1 g7 g8 h7',
+    nbMoves: 5
   }, {
     goal: 'Use two rooks<br>to speed things up!',
     fen: '8/1R6/8/8/3R4/8/8/8 w - - 0 1',
@@ -41,8 +41,8 @@ module.exports = {
   }, {
     goal: 'Use two rooks<br>to speed things up!',
     fen: '8/8/8/8/8/5R2/8/R7 w - - 0 1',
-    apples: 'a8 b7 d5 f2 f7 g4 g7 h5 h8',
-    nbMoves: 11
+    apples: 'b7 d1 d5 f2 f7 g4 g7',
+    nbMoves: 7
   }].map(util.toLevel),
   complete: 'Congratulations! You have successfully mastered the rook.'
 };
