2fe2e6c4bbeee26e34e5c737954a3f02b9e2f687
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-07-13 13:55:59 +0200

    fix stripe checkout

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -1,12 +1,12 @@
 lichess.checkout = function(publicKey) {
 
   var $checkout = $('div.plan_checkout');
+  var $stripeForm = $checkout.find('form.stripe_checkout');
 
   $checkout.find('button.stripe').on('click', function() {
     var $input = $checkout.find('group.radio input:checked');
     var amount = parseInt($input.data('amount'));
-    var $form = $checkout.find('form.stripe_checkout');
-    $form.find('.amount').val(amount);
+    $stripeForm.find('.amount').val(amount);
 
     stripeHandler.open({
       description: $input.data('description'),
@@ -22,6 +22,7 @@ lichess.checkout = function(publicKey) {
     var $form = $checkout.find('form.paypal_checkout');
     $form.find('input.usd').val(usd);
     $form.submit();
+    $checkout.find('.service').html(lichess.spinnerHtml);
   });
 
   var stripeHandler = StripeCheckout.configure({
@@ -35,8 +36,8 @@ lichess.checkout = function(publicKey) {
     currency: 'usd',
     token: function(token) {
       $checkout.find('.service').html(lichess.spinnerHtml);
-      $form.find('.token').val(token.id);
-      $form.submit();
+      $stripeForm.find('.token').val(token.id);
+      $stripeForm.submit();
     }
   });
   // Close Checkout on page navigation:
