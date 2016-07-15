197923f7a88726080d1ec32b1465c88f1fb6983f
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-07-15 20:26:45 +0200

    api version helper function

--- app/controllers/Round.scala
+++ app/controllers/Round.scala
@@ -81,7 +81,7 @@ object Round extends LilaController with TheftPrevention {
         else Env.api.roundApi.player(pov, apiVersion) zip
           getPlayerChat(pov.game) map {
             case (data, chat) => Ok(chat.fold(data) { c =>
-              data + ("chat" -> lila.chat.JsonView(c, mobileEscape = apiVersion == 1))
+              data + ("chat" -> lila.chat.JsonView(c, mobileEscape = apiVersion.v1))
             })
           }
       }.mon(_.http.response.player.mobile)
--- modules/common/src/main/model.scala
+++ modules/common/src/main/model.scala
@@ -1,3 +1,5 @@
 package lila.common
 
-case class ApiVersion(value: Int) extends AnyVal with IntValue
+case class ApiVersion(value: Int) extends AnyVal with IntValue {
+  def v1 = value == 1
+}
