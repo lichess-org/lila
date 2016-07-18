e94024268ae9aed90d4efce3e7221ce812bc7de4
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-07-18 18:39:29 +0200

    patron since

--- modules/user/src/main/Plan.scala
+++ modules/user/src/main/Plan.scala
@@ -2,27 +2,34 @@ package lila.user
 
 import org.joda.time.DateTime
 
-case class Plan(months: Int, active: Boolean) {
+case class Plan(
+    months: Int,
+    active: Boolean,
+    since: Option[DateTime]) {
 
   def incMonths = copy(
     months = months + 1,
-    active = true)
+    active = true,
+    since = since orElse DateTime.now.some)
 
   def disable = copy(active = false)
 
   def enable = copy(
     active = true,
-    months = months max 1)
+    months = months max 1,
+    since = since orElse DateTime.now.some)
 
   def isEmpty = months == 0
 
   def nonEmpty = !isEmpty option this
+
+  def sinceDate = since | DateTime.now
 }
 
 object Plan {
 
-  val empty = Plan(0, false)
-  val start = Plan(1, true)
+  val empty = Plan(0, false, none)
+  def start = Plan(1, true, DateTime.now.some)
 
   import lila.db.dsl._
   private[user] val planBSONHandler = reactivemongo.bson.Macros.handler[Plan]
