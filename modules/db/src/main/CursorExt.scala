4aea685eb117ab50e5797c34c45d721a2d887040
cchantep <chantepie@altern.org>
2016-07-21 14:41:34 +0200

    Upgrade to ReactiveMongo 0.12 (Release Candidate 1)

--- modules/db/src/main/CursorExt.scala
+++ modules/db/src/main/CursorExt.scala
@@ -7,11 +7,11 @@ import reactivemongo.bson._
 
 trait CursorExt { self: dsl =>
 
+  // Can be refactor as CursorProducer
   final implicit class ExtendCursor[A: BSONDocumentReader](val c: Cursor[A]) {
 
     // like collect, but with stopOnError defaulting to false
-    def gather[M[_]](upTo: Int = Int.MaxValue)(implicit cbf: CanBuildFrom[M[_], A, M[A]]): Fu[M[A]] =
-      c.collect[M](upTo, stopOnError = false)
+    def gather[M[_]](upTo: Int = Int.MaxValue)(implicit cbf: CanBuildFrom[M[_], A, M[A]]): Fu[M[A]] = c.collect[M](upTo, Cursor.ContOnError[M[A]]())
 
     def list(limit: Option[Int]): Fu[List[A]] = gather[List](limit | Int.MaxValue)
 
@@ -20,7 +20,7 @@ trait CursorExt { self: dsl =>
     def list(): Fu[List[A]] = list(none)
 
     // like headOption, but with stopOnError defaulting to false
-    def uno: Fu[Option[A]] =
-      c.collect[Iterable](1, stopOnError = false).map(_.headOption)
+    def uno: Fu[Option[A]] = c.collect[Iterable](
+      1, Cursor.ContOnError[Iterable[A]]()).map(_.headOption)
   }
 }
