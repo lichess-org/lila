e954131433227d3af926d73fe28618c0b71802e5
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-07-21 18:23:56 +0200

    analysis tree view v2 WIP

--- ui/analyse/src/tree/treeView2.js
+++ ui/analyse/src/tree/treeView2.js
@@ -15,30 +15,53 @@ function renderIndex(ply) {
   };
 }
 
-function renderChildrenOf(node) {
+function renderVariations(ctx, nodes, parentPath) {
+  return {
+    tag: 'wrap',
+    children: nodes.map(function(n) {
+      return [
+        renderMoveOf(ctx, n, parentPath),
+        renderChildrenOf(ctx, n, parentPath)
+      ];
+    })
+  };
+}
+
+function renderChildrenOf(ctx, node, parentPath) {
   var main = node.children[0];
   if (!main) return;
+  var path = parentPath + main.id;
   var variations = node.children.slice(1);
   return [
-    renderMoveOf(main),
-    renderChildrenOf(main)
+    renderMoveOf(ctx, main, path),
+    variations[0] ? renderVariations(ctx, variations, path) : null
   ];
 }
 
-function renderMoveOf(node) {
+function renderMoveOf(ctx, node, path) {
+  var attrs = {
+    p: path
+  };
+  var classes = path === ctx.ctrl.vm.path ? ['active'] : [];
+  if (path === ctx.ctrl.vm.contextMenuPath) classes.push('context_menu');
+  if (path === ctx.ctrl.vm.initialPath && game.playable(ctx.ctrl.data)) classes.push('current');
+  if (ctx.conceal) classes.push(ctx.conceal);
+  // if (!isMainline && (node.comments || node.shapes)) classes.push('annotated');
+  if (classes.length) attrs.class = classes.join(' ');
   return [
     renderIndex(node.ply),
-    moveTag([
+    moveTag(attrs, [
       util.fixCrazySan(node.san),
       node.glyphs ? renderGlyphs(node.glyphs) : null
     ])
   ];
 }
 
-function moveTag(children) {
+function moveTag(attrs, content) {
   return {
     tag: 'move',
-    children: children
+    attrs: attrs,
+    children: content
   };
 }
 
@@ -56,5 +79,15 @@ function renderGlyphs(glyphs) {
 
 module.exports = function(ctrl, conceal) {
   var root = ctrl.tree.root;
-  return m('div.tview2', renderChildrenOf(root));
+  var ctx = {
+    ctrl: ctrl,
+    conceal: conceal
+  };
+  return m('div.tview2', {
+    onmousedown: function(e) {
+      if (e.button !== undefined && e.button !== 0) return; // only touch or left click
+      var path = e.target.getAttribute('p');
+      if (path) ctrl.userJump(path);
+    },
+  }, renderChildrenOf(ctx, root, ''));
 };
