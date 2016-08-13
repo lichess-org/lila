6aee0135741f4788109cd29bd8117288aa79c96b
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-08-13 16:25:09 +0200

    globally rate limit exports

--- app/controllers/Export.scala
+++ app/controllers/Export.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import play.api.mvc.Action
+import scala.concurrent.duration._
 
 import lila.app._
 import lila.common.HTTPRequest
@@ -35,35 +36,51 @@ object Export extends LilaController {
     }
   }
 
+  private val PdfRateLimitGlobal = new lila.memo.RateLimit(
+    credits = 20,
+    duration = 1 minute,
+    name = "export PDF global")
+
   def pdf(id: String) = Open { implicit ctx =>
     OnlyHumans {
-      lila.mon.export.pdf()
-      OptionResult(GameRepo game id) { game =>
-        Ok.chunked(Enumerator.outputStream(env.pdfExport(game.id))).withHeaders(
-          CONTENT_TYPE -> "application/pdf",
-          CACHE_CONTROL -> "max-age=7200")
+      PdfRateLimitGlobal("-", msg = HTTPRequest lastRemoteAddress ctx.req) {
+        lila.mon.export.pdf()
+        OptionResult(GameRepo game id) { game =>
+          Ok.chunked(Enumerator.outputStream(env.pdfExport(game.id))).withHeaders(
+            CONTENT_TYPE -> "application/pdf",
+            CACHE_CONTROL -> "max-age=7200")
+        }
       }
     }
   }
 
+  private val PngRateLimitGlobal = new lila.memo.RateLimit(
+    credits = 60,
+    duration = 1 minute,
+    name = "export PDF global")
+
   def png(id: String) = Open { implicit ctx =>
     OnlyHumansAndFacebook {
-      lila.mon.export.png.game()
-      OptionResult(GameRepo game id) { game =>
-        Ok.chunked(Enumerator.outputStream(env.pngExport(game))).withHeaders(
-          CONTENT_TYPE -> "image/png",
-          CACHE_CONTROL -> "max-age=7200")
+      PngRateLimitGlobal("-", msg = HTTPRequest lastRemoteAddress ctx.req) {
+        lila.mon.export.png.game()
+        OptionResult(GameRepo game id) { game =>
+          Ok.chunked(Enumerator.outputStream(env.pngExport(game))).withHeaders(
+            CONTENT_TYPE -> "image/png",
+            CACHE_CONTROL -> "max-age=7200")
+        }
       }
     }
   }
 
   def puzzlePng(id: Int) = Open { implicit ctx =>
     OnlyHumansAndFacebook {
-      lila.mon.export.png.puzzle()
-      OptionResult(Env.puzzle.api.puzzle find id) { puzzle =>
-        Ok.chunked(Enumerator.outputStream(Env.puzzle.pngExport(puzzle))).withHeaders(
-          CONTENT_TYPE -> "image/png",
-          CACHE_CONTROL -> "max-age=7200")
+      PngRateLimitGlobal("-", msg = HTTPRequest lastRemoteAddress ctx.req) {
+        lila.mon.export.png.puzzle()
+        OptionResult(Env.puzzle.api.puzzle find id) { puzzle =>
+          Ok.chunked(Enumerator.outputStream(Env.puzzle.pngExport(puzzle))).withHeaders(
+            CONTENT_TYPE -> "image/png",
+            CACHE_CONTROL -> "max-age=7200")
+        }
       }
     }
   }
