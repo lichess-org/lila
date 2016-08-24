c3577ed1e61e96e098c2462fca13810417ae19f9
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-08-24 16:14:40 +0200

    fix adv. search and user search players inputs

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -4,11 +4,6 @@ $(function() {
   var $usernames = $form.find(".usernames input");
   var $userRows = $form.find(".user_row");
   var $result = $(".search_result");
-  var playersRegexp = /^\??(?:.*&?players.winner=([\w-\+]+))?(?:&?players.white=([\w-\+]+))?(?:&?players.black=([\w-\+]+))?.*$/g;
-  var match = playersRegexp.exec(window.location.search);
-  var $playersWinner = isPlayerChosen(match[1]);
-  var $playersWhite = isPlayerChosen(match[2]);
-  var $playersBlack = isPlayerChosen(match[3]);
 
   var serialize = function(all) {
     var sel = $form.find(":input");
@@ -17,7 +12,6 @@ $(function() {
     }).serialize()
   };
 
-  lichess.pubsub.emit('content_loaded')();
   var serialized = serialize();
   $result.find("a.permalink").each(function() {
     var s = $(this).hasClass('download') ? serialize(true) : serialized;
@@ -25,7 +19,8 @@ $(function() {
   });
   $result.find('.search_infinitescroll:has(.pager a)').each(function() {
     var $next = $(this).find(".pager a:last");
-    $next.attr("href", $next.attr("href") + "&" + serialized);
+    var newHref = $next.attr("href") + "&" + serialized;
+    $next.attr("href", newHref);
     $(this).infinitescroll({
       navSelector: ".pager",
       nextSelector: $next,
@@ -40,37 +35,42 @@ $(function() {
     });
   });
 
+  function getUsernames() {
+    var us = [];
+    $usernames.each(function() {
+      var u = $.trim($(this).val());
+      if (u) us.push(u);
+    });
+    return us;
+  }
+
   function userChoices(row) {
     var options = ["<option value=''></option>"];
-    $usernames.each(function() {
-      var user = $.trim($(this).val());
-      if (user.length) {
-        var option = [];
-        option.push("<option value='" + user + "'");
-        option.push(isSelected(row, "winner", user, $playersWinner));
-        option.push(isSelected(row, "whiteUser", user, $playersWhite));
-        option.push(isSelected(row, "blackUser", user, $playersBlack));
-        option.push(">" + user + "</option>");
-        options.push(option.join(""));
-      }
+    getUsernames().forEach(function(user) {
+      var option = [];
+      option.push("<option value='" + user + "'");
+      option.push(isSelected(row, "winner", user, 'req-winner'));
+      option.push(isSelected(row, "whiteUser", user, 'req-white'));
+      option.push(isSelected(row, "blackUser", user, 'req-black'));
+      option.push(">" + user + "</option>");
+      options.push(option.join(""));
     });
     $(row).find('select').html(options.join(""));
     $(row).toggle(options.length > 1);
   }
 
-  function isPlayerChosen(match) {
-    return typeof match !== "undefined" ? match.replace(/\+/g, " ") : "";
-  }
-
-  function isSelected(row, rowClassName, user, player) {
+  function isSelected(row, rowClassName, user, dataKey) {
+    var player = $form.data(dataKey);
     return (row.classList.contains(rowClassName) && player.length && user == player) ? "selected" : ""
   }
 
-  $usernames.bind("keyup", function() {
+  function reloadUserChoices() {
     $userRows.each(function() {
       userChoices(this);
     });
-  }).trigger("keyup");
+  }
+  reloadUserChoices();
+  $usernames.bind("input paste", reloadUserChoices);
 
   var toggleAiLevel = function() {
     $form.find(".opponent select").each(function() {
@@ -90,6 +90,6 @@ $(function() {
       $form.submit();
     };
     $form.find("select, input[type=checkbox]").change(submit);
-    $usernames.bind("keyup", $.fp.debounce(submit, 2000));
+    $usernames.bind("keyup", $.fp.debounce(submit, 1500));
   }
 });
