5b19f6cd76aa89c1bd8062ed74dddeb937576be2
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-08-27 10:18:26 +0200

    reformat Antma pairing code

--- modules/tournament/src/main/arena/AntmaPairing.scala
+++ modules/tournament/src/main/arena/AntmaPairing.scala
@@ -7,22 +7,28 @@ private object AntmaPairing {
 
   def apply(data: Data, players: RankedPlayers): List[Pairing.Prep] = players.nonEmpty ?? {
     import data._
+
     val a: Array[RankedPlayer] = players.toArray
     val n: Int = a.length
-    def pairScore(i: Int, j: Int): Int = {
-      def playedTogether(u1: String, u2: String) =
-        if (lastOpponents.hash.get(u1).contains(u2)) 1 else 0
-      def f(x: Int): Int = (11500000 - 3500000 * x) * x
+
+    def playedTogether(u1: String, u2: String) =
+      if (lastOpponents.hash.get(u1).contains(u2)) 1 else 0
+
+    def f(x: Int): Int = (11500000 - 3500000 * x) * x
+
+    def pairScore(i: Int, j: Int): Int =
       Math.abs(a(i).rank - a(j).rank) * 1000 +
         Math.abs(a(i).player.rating - a(j).player.rating) +
         f {
           playedTogether(a(i).player.userId, a(j).player.userId) +
             playedTogether(a(j).player.userId, a(i).player.userId)
         }
-    }
+
     try {
       val mate = WMMatching.minWeightMatching(WMMatching.fullGraph(n, pairScore))
-      WMMatching.mateToEdges(mate).map(x => Pairing.prep(tour, a(x._1).player, a(x._2).player))
+      WMMatching.mateToEdges(mate).map { x =>
+        Pairing.prep(tour, a(x._1).player, a(x._2).player)
+      }
     }
     catch {
       case e: Exception =>
