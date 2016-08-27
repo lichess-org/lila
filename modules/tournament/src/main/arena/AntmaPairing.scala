ae728dcc55e6d97c3c2e618be5fd9643444c3400
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-08-27 10:15:45 +0200

    cache antma pairing exceptions (just in case)

--- modules/tournament/src/main/arena/AntmaPairing.scala
+++ modules/tournament/src/main/arena/AntmaPairing.scala
@@ -1,7 +1,7 @@
 package lila.tournament
 package arena
 
-import PairingSystem.{Data,url}
+import PairingSystem.{ Data, url }
 
 private object AntmaPairing {
 
@@ -10,13 +10,24 @@ private object AntmaPairing {
     val a: Array[RankedPlayer] = players.toArray
     val n: Int = a.length
     def pairScore(i: Int, j: Int): Int = {
-      def playedTogether(u1:String, u2:String) = if (lastOpponents.hash.get(u1).contains(u2)) 1 else 0
+      def playedTogether(u1: String, u2: String) =
+        if (lastOpponents.hash.get(u1).contains(u2)) 1 else 0
       def f(x: Int): Int = (11500000 - 3500000 * x) * x
       Math.abs(a(i).rank - a(j).rank) * 1000 +
-      Math.abs(a(i).player.rating - a(j).player.rating) +
-      f (playedTogether(a(i).player.userId, a(j).player.userId) + playedTogether(a(j).player.userId, a(i).player.userId))
+        Math.abs(a(i).player.rating - a(j).player.rating) +
+        f {
+          playedTogether(a(i).player.userId, a(j).player.userId) +
+            playedTogether(a(j).player.userId, a(i).player.userId)
+        }
     }
-    val mate = WMMatching.minWeightMatching(WMMatching.fullGraph(n, pairScore))
-    WMMatching.mateToEdges(mate).map (x => Pairing.prep(tour, a(x._1).player, a(x._2).player))
+    try {
+      val mate = WMMatching.minWeightMatching(WMMatching.fullGraph(n, pairScore))
+      WMMatching.mateToEdges(mate).map(x => Pairing.prep(tour, a(x._1).player, a(x._2).player))
+    }
+    catch {
+      case e: Exception =>
+        logger.error("AntmaPairing", e)
+        Nil
+    }
   }
 }
