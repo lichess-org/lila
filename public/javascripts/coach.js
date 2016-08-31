5e183cd0ee3bb0b5e5cb82a7e98e395722342455
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-08-31 16:03:03 +0200

    coach user review WIP

--- public/javascripts/coach.js
+++ public/javascripts/coach.js
@@ -2,4 +2,8 @@ $(function() {
   $(".bar-rating").barrating({
     theme: 'fontawesome-stars'
   });
+  $('.review-form .toggle').click(function() {
+    $(this).remove();
+    $('.review-form form').slideDown(500);
+  });
 });
