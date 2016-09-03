29c3e058a647da1b1cab66723d29a398c968a97f
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-09-03 10:28:11 +0200

    basic coach profile form validation

--- public/javascripts/coach.form.js
+++ public/javascripts/coach.form.js
@@ -1,5 +1,54 @@
 $(function() {
+
   var $editor = $('.coach_edit');
+
+  var todo = (function() {
+
+    var $overview = $editor.find('.overview');
+    var $el = $overview.find('.todo');
+    var $option = $editor.find('select[name=listed] option[value=true]');
+
+    var must = [{
+      name: 'Complete your lichess profile',
+      check: function() {
+        return $el.data('profile');
+      }
+    }, {
+      name: 'Upload a profile picture',
+      check: function() {
+        return $editor.find('img.picture').length;
+      }
+    }, {
+      name: 'Fill in basic informations',
+      check: function() {
+        ['profile.headline', 'profile.languages'].forEach(function(name) {
+          if (!$editor.find('[name="' + name + '"]').val()) return false;
+        });
+        return true;
+      }
+    }, {
+      name: 'Fill at least 3 description texts',
+      check: function() {
+        return $editor.find('.panel.texts textarea').filter(function() {
+          return !!$(this).val();
+        }).length >= 3;
+      }
+    }];
+
+    return function() {
+      var points = [];
+      must.forEach(function(o) {
+        if (!o.check()) points.push($('<li>').text(o.name));
+      });
+      $el.find('ul').html(points);
+      var fail = !!points.length;
+      $overview.toggleClass('with_todo', fail);
+      if (fail) $option.parent().val('false');
+      $option.attr('disabled', fail);
+    };
+  })();
+  todo();
+
   $editor.find('.tabs > div').click(function() {
     $editor.find('.tabs > div').removeClass('active');
     $(this).addClass('active');
@@ -28,7 +77,10 @@ $(function() {
     var $review = $(this).parents('.review');
     $.ajax({
       method: 'post',
-      url: $review.data('action') + '?v=' + $(this).data('value')
+      url: $review.data('action') + '?v=' + $(this).data('value'),
+      complete: function() {
+        todo();
+      }
     });
     $review.slideUp(300);
     $editor.find('.tabs div[data-tab=reviews]').attr('data-count', $reviews.find('.review').length - 1);
