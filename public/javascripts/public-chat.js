82a6248d6d5aabb210fd6608d0d3a11f1aead6b6
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-09-05 10:30:34 +0200

    reformat JS

--- public/javascripts/public-chat.js
+++ public/javascripts/public-chat.js
@@ -1,57 +1,56 @@
 $(function() {
 
-    var onPageReload = function () {
+  var onPageReload = function() {
 
-        var reloadTimer = null;
+    var reloadTimer = null;
 
-        var autoRefreshEnabled = true;
+    var autoRefreshEnabled = true;
 
-        var startAutoRefresh = function() {
-            reloadTimer = setTimeout(function() {
-                    // Reload only the chat grid portions of the page
-                    $("#lichess").load("/mod/public-chat #communication", function() {
-                        onPageReload();
-                    });
+    var startAutoRefresh = function() {
+      reloadTimer = setTimeout(function() {
+        // Reload only the chat grid portions of the page
+        $("#lichess").load("/mod/public-chat #communication", function() {
+          onPageReload();
+        });
 
-            }, 3000);
-        };
-
-        var stopAutoRefresh = function () {
-            if (reloadTimer) {
-                clearTimeout(reloadTimer);
-            }
-        };
-
-        var addAutoRefreshLink = function () {
-            var enableAutoRefreshText = "Enable auto refresh";
-            var disableAutoRefreshText = "Disable auto refresh";
-
-            var a = document.createElement('a');
-            a.id="auto_refresh";
-            var linkText = document.createTextNode(disableAutoRefreshText);
-            a.appendChild(linkText);
-            a.classList.add('button');
-
-            a.onclick = function() {
-
-                if (autoRefreshEnabled) {
-                    stopAutoRefresh();
-                    $("#auto_refresh").text(enableAutoRefreshText);
-                }
-                else {
-                    startAutoRefresh();
-                    $("#auto_refresh").text(disableAutoRefreshText);
-                }
-
-                autoRefreshEnabled = !autoRefreshEnabled;
-            };
-
-            startAutoRefresh();
-            $("#communication").append(a);
-        };
-
-        addAutoRefreshLink();
+      }, 3000);
     };
 
-    onPageReload();
+    var stopAutoRefresh = function() {
+      if (reloadTimer) {
+        clearTimeout(reloadTimer);
+      }
+    };
+
+    var addAutoRefreshLink = function() {
+      var enableAutoRefreshText = "Enable auto refresh";
+      var disableAutoRefreshText = "Disable auto refresh";
+
+      var a = document.createElement('a');
+      a.id = "auto_refresh";
+      var linkText = document.createTextNode(disableAutoRefreshText);
+      a.appendChild(linkText);
+      a.classList.add('button');
+
+      a.onclick = function() {
+
+        if (autoRefreshEnabled) {
+          stopAutoRefresh();
+          $("#auto_refresh").text(enableAutoRefreshText);
+        } else {
+          startAutoRefresh();
+          $("#auto_refresh").text(disableAutoRefreshText);
+        }
+
+        autoRefreshEnabled = !autoRefreshEnabled;
+      };
+
+      startAutoRefresh();
+      $("#communication").append(a);
+    };
+
+    addAutoRefreshLink();
+  };
+
+  onPageReload();
 });
