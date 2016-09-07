8aeb00ff2b739b8513692b655f553d74a3c7b345
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-09-07 11:41:57 +0200

    FIDE, USCF, and ECF ratings

--- modules/user/src/main/Profile.scala
+++ modules/user/src/main/Profile.scala
@@ -6,7 +6,9 @@ case class Profile(
     bio: Option[String] = None,
     firstName: Option[String] = None,
     lastName: Option[String] = None,
-    fideRating: Option[Int] = None) {
+    fideRating: Option[Int] = None,
+    uscfRating: Option[Int] = None,
+    ecfRating: Option[Int] = None) {
 
   def nonEmptyRealName = List(ne(firstName), ne(lastName)).flatten match {
     case Nil   => none
@@ -28,10 +30,19 @@ case class Profile(
     100 * c.count(identity) / c.size
   }
 
+  import Profile.OfficialRating
+
+  def officialRating: Option[OfficialRating] =
+    fideRating.map { OfficialRating("fide", _) } orElse
+      uscfRating.map { OfficialRating("uscf", _) } orElse
+      ecfRating.map { OfficialRating("ecf", _) }
+
   private def ne(str: Option[String]) = str.filter(_.nonEmpty)
 }
 
 object Profile {
+
+  case class OfficialRating(name: String, rating: Int)
 
   val default = Profile()
 
