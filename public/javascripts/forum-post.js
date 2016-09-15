dfb81db04a044d5a85398540f6978b3fd3bc6adc
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-09-15 21:27:30 +0100

    reveal submit and cancel buttons if edit is clicked and remove edit, and vice versa.

--- public/javascripts/forum-post.js
+++ public/javascripts/forum-post.js
@@ -1,17 +1,30 @@
 
+var oldContents = null;
 
 var editForumPost = function(postNumber) {
     var postSelector = $("#" + postNumber);
 
     // We grab the text element of the post and turn it into a textarea to make it editable
-    var textSelector = postSelector.find(".message")
+    oldContents = postSelector.find(".message")
 
-    var postContents = textSelector.text();
+    var postContents = oldContents.text();
 
-    var editableArea = $("<textarea style='width: 100%; height:300px'>");
+    var editableArea = $("<textarea id='post-edit-area' style='width: 100%; height:300px'>");
 
     editableArea.text(postContents);
 
-    textSelector.replaceWith(editableArea);
+    oldContents.replaceWith(editableArea);
 
+    $("#edit-button").hide();
+    $("#edit-submit-button").show();
+    $("#edit-cancel-button").show();
+
+}
+
+var cancelEdit = function(postNumber) {
+    $('#post-edit-area').replaceWith(oldContents);
+
+    $("#edit-button").show();
+    $("#edit-submit-button").hide();
+    $("#edit-cancel-button").hide();
 }
