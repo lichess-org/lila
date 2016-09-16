684daa5fcc92e8347374678fe008faf3a4744763
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-09-17 00:46:24 +0100

    Fix issue when edit is clicked on more than one post and then cancelled on both.

--- public/javascripts/forum-post.js
+++ public/javascripts/forum-post.js
@@ -1,13 +1,17 @@
 
-var oldContents = null;
+// Map of old post contents by post number in case the user clicks 'edit' on multiple posts
+// on the same page (although rare.)
+var oldContents = {};
 
 var editForumPost = function(postId, postNumber) {
     var postSelector = $("#" + postNumber);
 
     // We grab the text element of the post and turn it into a textarea to make it editable
-    oldContents = postSelector.find(".message")
+    oldContents[postNumber] = postSelector.find(".message");
 
-    var postContents = oldContents.text();
+    var old = oldContents[postNumber];
+
+    var postContents = old.text();
 
     var editForm = $('<form>', {
                                 id: 'post-edit-form-' + postNumber,
@@ -25,13 +29,13 @@ var editForumPost = function(postId, postNumber) {
     editForm.append(formSubmitButton);
     editForm.append(formCancelButton);
 
-    oldContents.replaceWith(editForm);
+    old.replaceWith(editForm);
 
     $("#edit-button-" + postNumber).hide();
 };
 
 var cancelEdit = function(postNumber) {
-    $('#post-edit-form-' + postNumber).replaceWith(oldContents);
+    $('#post-edit-form-' + postNumber).replaceWith(oldContents[postNumber]);
 
     $("#edit-button-" + postNumber).show();
 }
