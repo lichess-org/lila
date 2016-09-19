a6dd85564e25a8ae441fcf4b896bcff0087a750c
clarkerubber <jamesjosephclarke@gmail.com>
2016-09-19 23:56:01 +1000

    increase endgame piece count limit

--- bin/mongodb/puzzle-disable-endgames.js
+++ bin/mongodb/puzzle-disable-endgames.js
@@ -2,7 +2,7 @@ var puzzles = db.puzzle;
 puzzles.find().forEach(function(p) {
   var parts = p.fen.split(/\s/);
   var pieceCount = parts[0].split(/[nbrqkp]/i).length - 1;
-  if (pieceCount < 8) {
+  if (pieceCount < 9) {
     puzzles.update({_id: p._id},{$set:{vote: {sum:-9000}}});
   }
 });
