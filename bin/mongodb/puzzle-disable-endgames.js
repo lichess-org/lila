2207afc982794871070827b12cb518b53b328c1b
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-09-20 12:59:49 +0200

    fix puzzle-disable-endgames.js and backup previous vote

--- bin/mongodb/puzzle-disable-endgames.js
+++ bin/mongodb/puzzle-disable-endgames.js
@@ -1,8 +1,22 @@
 var puzzles = db.puzzle;
+var count = 0;
 puzzles.find().forEach(function(p) {
   var parts = p.fen.split(/\s/);
   var pieceCount = parts[0].split(/[nbrqkp]/i).length - 1;
   if (pieceCount < 9) {
-    puzzles.update({_id: p._id},{$set:{vote: {sum:-9000}}});
+    count++;
+    puzzles.update({
+      _id: p._id
+    }, {
+      $set: {
+        vote_backup: p.vote,
+        vote: {
+          up: NumberInt(0),
+          down: NumberInt(0),
+          sum: NumberInt(-9000)
+        }
+      }
+    });
   }
 });
+print("Disabled " + count + " puzzles");
