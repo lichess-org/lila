c11972e1c723f6f208d7679ccde6f45c92440dad
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-09-23 12:52:04 +0200

    remove websocket proxy code

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -242,9 +242,6 @@ lichess.StrongSocket = function(url, version, settings) {
       options.onNextConnect();
       delete options.onNextConnect;
     }
-    if (lichess.proxy) lichess.proxy.getLatency(options.protocol + "//" + baseUrl(), function(ms) {
-      console.log(ms, 'latency');
-    });
   };
 
   var baseUrl = function() {
