a1ea92ce16e9ab27579c36049529b0aace78c637
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-09-27 22:46:26 +0100

    Fix indentation.

--- public/javascripts/forum-post.js
+++ public/javascripts/forum-post.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
   $('.edit.button').add('.edit-post-cancel').click(function(e) {
     e.preventDefault();
 
@@ -12,66 +12,62 @@ $(function () {
 
   lichess.loadScript('/assets/vendor/jquery-textcomplete/dist/jquery.textcomplete.js').then(function() {
 
-    var getTopicId = function () {
-        return $('.post-text-area').attr('data-topic');
+    var getTopicId = function() {
+      return $('.post-text-area').attr('data-topic');
     };
 
     var getThreadParticipants = function() {
-        var topicId = getTopicId();
+      var topicId = getTopicId();
 
-        if (!topicId) {
-            return jQuery.Deferred().resolve([]);
-        } else {
-            return $.ajax({url: "/forum/participants/" + topicId});
-        }
+      if (!topicId) {
+        return jQuery.Deferred().resolve([]);
+      } else {
+        return $.ajax({
+          url: "/forum/participants/" + topicId
+        });
+      }
 
     };
 
     var searchCandidates = function(term, candidateUsers) {
-        // We use jQuery's map because returning 'null' removes it from the list.
-        // This follows jquery-autocomplete's semantics.
-        return $.map(candidateUsers, function (user) {
-                return user.indexOf(term) === 0 ? user : null;
-            });
+      // We use jQuery's map because returning 'null' removes it from the list.
+      // This follows jquery-autocomplete's semantics.
+      return $.map(candidateUsers, function(user) {
+        return user.indexOf(term) === 0 ? user : null;
+      });
     };
 
     var participants = getThreadParticipants();
 
-    $('.post-text-area').textcomplete([
-         {
-             match: /\B@(\w*)$/,
-             search: function (term, callback) {
+    $('.post-text-area').textcomplete([{
+        match: /\B@(\w*)$/,
+        search: function(term, callback) {
 
-                if (term.length < 3) {
-                    // Initially we only autocomplete by participants in the thread. As the user types more,
-                    // we can autocomplete against all users on the site.
+          if (term.length < 3) {
+            // Initially we only autocomplete by participants in the thread. As the user types more,
+            // we can autocomplete against all users on the site.
 
-                    participants.done(function(participants) {
-                        callback(searchCandidates(term, participants));
-                    });
-                } else {
-                    $.ajax(
-                            {
-                                url: "/player/autocomplete?term=" + term,
-                                success: function(candidateUsers) {
-                                    callback(searchCandidates(term, candidateUsers));
-                                }
-                            });
-                }
-             },
-             index: 1,
-             replace: function (mention) {
-                 return '@' + mention + ' ';
-             }
-         }
-        ],
-        {
-            'placement' : 'top'
+            participants.done(function(participants) {
+              callback(searchCandidates(term, participants));
+            });
+          } else {
+            $.ajax({
+              url: "/player/autocomplete?term=" + term,
+              success: function(candidateUsers) {
+                callback(searchCandidates(term, candidateUsers));
+              }
+            });
+          }
+        },
+        index: 1,
+        replace: function(mention) {
+          return '@' + mention + ' ';
         }
+      }], {
+        'placement': 'top'
+      }
 
-
-        );
+    );
   });
-
 
 });
