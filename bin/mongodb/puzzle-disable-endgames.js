5ebd8a4549960bfac2b17cb0f6ea9425f4a418b2
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-09-29 11:03:49 +0200

    tweak puzzle disabler

--- bin/mongodb/puzzle-disable-endgames.js
+++ bin/mongodb/puzzle-disable-endgames.js
@@ -3,13 +3,12 @@ var count = 0;
 puzzles.find().forEach(function(p) {
   var parts = p.fen.split(/\s/);
   var pieceCount = parts[0].split(/[nbrqkp]/i).length - 1;
-  if (pieceCount < 9) {
+  if (pieceCount < 9 && p.vote.sum < 50) {
     count++;
     puzzles.update({
       _id: p._id
     }, {
       $set: {
-        vote_backup: p.vote,
         vote: {
           up: NumberInt(0),
           down: NumberInt(0),
