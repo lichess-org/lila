aa562019fe3d34e9b0148891fb1cf1cfca04fcc1
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-09-30 14:49:02 +0200

    aggregate analysis requesters to fight abuse

--- modules/analyse/src/main/Analyser.scala
+++ modules/analyse/src/main/Analyser.scala
@@ -3,12 +3,14 @@ package lila.analyse
 import akka.actor.ActorSelection
 
 import chess.format.FEN
+import lila.db.dsl._
 import lila.game.actorApi.InsertGame
 import lila.game.{ Game, GameRepo }
 import lila.hub.actorApi.map.Tell
 
 final class Analyser(
     indexer: ActorSelection,
+    requesterColl: Coll,
     roundSocket: ActorSelection,
     bus: lila.common.Bus) {
 
@@ -21,11 +23,24 @@ final class Analyser(
         sendAnalysisProgress(analysis) >>- {
           bus.publish(actorApi.AnalysisReady(game, analysis), 'analysisReady)
           indexer ! InsertGame(game)
+          logRequester(analysis)
         }
     }
   }
 
   def progress(analysis: Analysis): Funit = sendAnalysisProgress(analysis)
+
+  private object logRequester {
+    import org.joda.time._
+    private val formatter = format.DateTimeFormat.forPattern("YYYY-MM-dd")
+    def apply(analysis: Analysis) =
+      requesterColl.update(
+        $id(analysis.uid | "anonymous"),
+        $inc("total" -> 1) ++
+          $inc(formatter.print(DateTime.now) -> 1) ++
+          $set("last" -> analysis.id),
+        upsert = true)
+  }
 
   private def sendAnalysisProgress(analysis: Analysis): Funit =
     GameRepo gameWithInitialFen analysis.id map {
