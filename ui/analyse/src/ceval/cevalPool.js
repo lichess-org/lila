74ad8c1c8347716361035aac97740225c2c46d4c
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-10-01 18:03:33 +0200

    add pnacl support for ceval

--- ui/analyse/src/ceval/cevalPool.js
+++ ui/analyse/src/ceval/cevalPool.js
@@ -1,7 +1,65 @@
 var m = require('mithril');
 
-module.exports = function(opts, makeWorker, nb) {
+function makeHelper(makeWorker, terminateWorker, poolOpts, makeProtocol, protocolOpts) {
+  var worker, protocol, api;
 
+  var boot = function () {
+    worker = makeWorker(poolOpts);
+    protocol = makeProtocol(api, protocolOpts);
+    worker.addEventListener('message', function(e) {
+      protocol.received(e.data);
+    }, true);
+  };
+
+  var stop = function() {
+    var stopped = m.deferred(false);
+    setTimeout(function () {
+      stopped.reject();
+    }, 1000);
+    return protocol.stop(stopped);
+  };
+
+  api = {
+    send: function(text) {
+      worker.postMessage(text);
+    },
+    start: function (work) {
+      stop().then(function () {
+        protocol.start(work);
+      }, function () {
+        terminateWorker(worker);
+        boot();
+      });
+    },
+    stop: stop
+  };
+
+  boot();
+
+  return api;
+}
+
+function makeWebWorker(makeProtocol, poolOpts, protocolOpts) {
+  return makeHelper(function () {
+    return new Worker(poolOpts.asmjs);
+  }, function (worker) {
+    worker.terminate();
+  }, poolOpts, makeProtocol, protocolOpts);
+}
+
+function makePNaClModule(makeProtocol, poolOpts, protocolOpts) {
+  return makeHelper(function () {
+    var module = document.createElement('embed');
+    module.setAttribute('src', poolOpts.pnacl);
+    module.setAttribute('type', 'application/x-pnacl');
+    module.setAttribute('width', '0');
+    module.setAttribute('height', '0');
+    document.body.appendChild(module);
+    return module;
+  }, function () {}, poolOpts, makeProtocol, protocolOpts);
+}
+
+module.exports = function(makeProtocol, poolOpts, protocolOpts) {
   var workers = [];
   var token = -1;
 
@@ -12,9 +70,13 @@ module.exports = function(opts, makeWorker, nb) {
   };
 
   var initWorkers = function() {
-    if (!workers.length)
-      for (var i = 1; i <= nb; i++)
-        workers.push(makeWorker(opts, 'W' + i));
+    if (workers.length) return;
+
+    if (poolOpts.pnacl && navigator.mimeTypes['application/x-pnacl'])
+      workers.push(makePNaClModule(makeProtocol, poolOpts, protocolOpts));
+    else
+      for (var i = 1; i <= 4; i++)
+        workers.push(makeWebWorker(makeProtocol, poolOpts, protocolOpts));
   }
 
   var stopAll = function() {
