ad3b497c4bf0f8ce83e6107538d774ec582d56a2
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-10-05 01:26:13 +0200

    maybe more familiar wording

--- ui/analyse/src/actionMenu.js
+++ ui/analyse/src/actionMenu.js
@@ -191,7 +191,7 @@ module.exports = {
             return m('div.setting', [
               m('label', {
                 'for': id
-              }, 'Threads'),
+              }, 'CPUs'),
               m('input', {
                 id: id,
                 type: 'range',
