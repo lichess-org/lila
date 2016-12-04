849f2ae3cdcae5faed1fe7f8e0c2f6bd1c806ff5
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-04 14:17:35 +0100

    remove dead code

--- modules/socket/src/main/SocketMember.scala
+++ modules/socket/src/main/SocketMember.scala
@@ -14,14 +14,3 @@ trait SocketMember {
 
   def end = channel.end
 }
-
-object SocketMember {
-
-  def apply(c: JsChannel): SocketMember = apply(c, none, false)
-
-  def apply(c: JsChannel, uid: Option[String], tr: Boolean): SocketMember = new SocketMember {
-    val channel = c
-    val userId = uid
-    val troll = tr
-  }
-}
