cc9b6645e6f284accbeaa7cbdd607de8738612d3
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-05 21:08:59 +0100

    make popular pools faster

--- modules/pool/src/main/PoolList.scala
+++ modules/pool/src/main/PoolList.scala
@@ -7,13 +7,13 @@ object PoolList {
   import PoolConfig._
 
   val all: List[PoolConfig] = List(
-    PoolConfig(1 ++ 0, Wave(15 seconds, 20 players)),
+    PoolConfig(1 ++ 0, Wave(13 seconds, 20 players)),
     PoolConfig(2 ++ 1, Wave(18 seconds, 20 players)),
-    PoolConfig(3 ++ 0, Wave(20 seconds, 30 players)),
+    PoolConfig(3 ++ 0, Wave(15 seconds, 30 players)),
     PoolConfig(3 ++ 2, Wave(22 seconds, 20 players)),
-    PoolConfig(5 ++ 0, Wave(20 seconds, 30 players)),
+    PoolConfig(5 ++ 0, Wave(15 seconds, 30 players)),
     PoolConfig(5 ++ 3, Wave(25 seconds, 20 players)),
-    PoolConfig(10 ++ 0, Wave(30 seconds, 20 players)),
+    PoolConfig(10 ++ 0, Wave(20 seconds, 20 players)),
     PoolConfig(15 ++ 15, Wave(60 seconds, 16 players))
   )
 
