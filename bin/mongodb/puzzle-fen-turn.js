f942359b3468891d68047f401d50b40e201331fa
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-08 00:21:32 +0100

    idempotent migration script

--- bin/mongodb/puzzle-fen-turn.js
+++ bin/mongodb/puzzle-fen-turn.js
@@ -1,23 +1,22 @@
 var puzzles = db.puzzle;
 
 function fullMoveNumber(p) {
-  return Math.floor(1 + p.history.split(' ').length / 2);
+  return Math.floor(1 + (p.history.split(' ').length - 1) / 2);
 }
 
-function changeFenMoveNumber(fen) {
+function changeFenMoveNumber(fen, n) {
   parts = fen.split(' ');
-  if (parts[1] === 'b') return fen;
-  parts[5] = parseInt(parts[5]) + 1;
+  parts[parts.length-1] = n;
   return parts.join(' ');
 }
 
 puzzles.find({
-  // _id: 10107
   "_id": {
     "$lt": 60121
   }
 }).forEach(function(p) {
-  var newFen = changeFenMoveNumber(p.fen);
+  var newMoveNumber = fullMoveNumber(p);
+  var newFen = changeFenMoveNumber(p.fen, newMoveNumber);
   puzzles.update({
     _id: p._id
   }, {
