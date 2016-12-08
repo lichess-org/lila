4e6f01a0d440fec0e05cf5927caa3fee759698ad
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-08 11:34:03 +0100

    ratelimit lobby socket actions

--- modules/lobby/src/main/SocketHandler.scala
+++ modules/lobby/src/main/SocketHandler.scala
@@ -23,47 +23,65 @@ private[lobby] final class SocketHandler(
     poolApi: PoolApi,
     blocking: String => Fu[Set[String]]) {
 
+  private val HookPoolLimitPerMember = new lila.memo.RateLimit(
+    credits = 25,
+    duration = 1 minute,
+    name = "lobby hook/pool per member",
+    key = "lobby.hook_pool.member")
+
   private def controller(socket: ActorRef, member: Member): Handler.Controller = {
-    case ("join", o) =>
+    case ("join", o) => HookPoolLimitPerMember(member.uid, cost = 5) {
       o str "d" foreach { id =>
         lobby ! BiteHook(id, member.uid, member.user)
       }
-    case ("cancel", _) =>
+    }
+    case ("cancel", _) => HookPoolLimitPerMember(member.uid, cost = 1) {
       lobby ! CancelHook(member.uid)
-    case ("joinSeek", o) => for {
-      id <- o str "d"
-      user <- member.user
-    } lobby ! BiteSeek(id, user)
-    case ("cancelSeek", o) => for {
-      id <- o str "d"
-      user <- member.user
-    } lobby ! CancelSeek(id, user)
+    }
+    case ("joinSeek", o) => HookPoolLimitPerMember(member.uid, cost = 5) {
+      for {
+        id <- o str "d"
+        user <- member.user
+      } lobby ! BiteSeek(id, user)
+    }
+    case ("cancelSeek", o) => HookPoolLimitPerMember(member.uid, cost = 1) {
+      for {
+        id <- o str "d"
+        user <- member.user
+      } lobby ! CancelSeek(id, user)
+    }
     case ("idle", o) => socket ! SetIdle(member.uid, ~(o boolean "d"))
     // entering a pool
-    case ("poolIn", o) => for {
-      user <- member.user
-      d <- o obj "d"
-      id <- d str "id"
-      ratingRange = d str "range" flatMap RatingRange.apply
-    } {
-      lobby ! CancelHook(member.uid) // in case there's one...
-      poolApi.join(
-        PoolConfig.Id(id),
-        PoolApi.Joiner(
-          userId = user.id,
-          socketId = lila.socket.Socket.Uid(member.uid),
-          ratingMap = user.ratingMap,
-          ratingRange = ratingRange,
-          engine = user.engine,
-          blocking = user.blocking))
+    case ("poolIn", o) => HookPoolLimitPerMember(member.uid, cost = 1) {
+      for {
+        user <- member.user
+        d <- o obj "d"
+        id <- d str "id"
+        ratingRange = d str "range" flatMap RatingRange.apply
+      } {
+        lobby ! CancelHook(member.uid) // in case there's one...
+        poolApi.join(
+          PoolConfig.Id(id),
+          PoolApi.Joiner(
+            userId = user.id,
+            socketId = lila.socket.Socket.Uid(member.uid),
+            ratingMap = user.ratingMap,
+            ratingRange = ratingRange,
+            engine = user.engine,
+            blocking = user.blocking))
+      }
     }
     // leaving a pool
-    case ("poolOut", o) => for {
-      id <- o str "d"
-      user <- member.user
-    } poolApi.leave(PoolConfig.Id(id), user.id)
+    case ("poolOut", o) => HookPoolLimitPerMember(member.uid, cost = 1) {
+      for {
+        id <- o str "d"
+        user <- member.user
+      } poolApi.leave(PoolConfig.Id(id), user.id)
+    }
     // entering the hooks view
-    case ("hookIn", _)  => lobby ! HookSub(member, true)
+    case ("hookIn", _)  => HookPoolLimitPerMember(member.uid, cost = 2) {
+      lobby ! HookSub(member, true)
+    }
     // leaving the hooks view
     case ("hookOut", _) => socket ! HookSub(member, false)
   }
