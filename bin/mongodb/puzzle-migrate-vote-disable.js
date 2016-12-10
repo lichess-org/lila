085f1f7051f198797274516e17f98a2b62ef066d
James Clarke <jamesjosephclarke@gmail.com>
2016-12-10 23:00:37 +1100

    more fixing of puzzle vote migration

--- bin/mongodb/puzzle-migrate-vote-disable.js
+++ bin/mongodb/puzzle-migrate-vote-disable.js
@@ -1,13 +1,20 @@
 var puzzles = db.puzzle;
 
-puzzles.find({}).forEach(function(p) {
+modified = 0;
+
+puzzles.find().forEach(function(p) {
   puzzles.update({
     _id: p._id
   }, {
     $set: {
       vote: {
-        enabled: (p.vote.up * 3 > p.vote.down) || (p.vote.up + p.vote.down < 50)
+        enabled: (p.vote.up * 3 > p.vote.down) || (p.vote.up + p.vote.down < 50),
+        up: p.vote.up,
+        down: p.vote.down
       }
     }
   });
+  modified += 1;
 });
+
+print(modified);
