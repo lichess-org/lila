9051d52d7242bd1db3a4c73dd0b940dedfe51250
James Clarke <jamesjosephclarke@gmail.com>
2016-12-12 06:32:32 +1100

    NumberInt all the things

--- bin/mongodb/puzzle-migrate-vote-disable.js
+++ bin/mongodb/puzzle-migrate-vote-disable.js
@@ -7,8 +7,8 @@ puzzles.find().forEach(function(p) {
     _id: p._id
   }, {
     $set: {
-      "vote.ratio": Math.floor(100*(p.vote.up - p.vote.down)/(p.vote.up + p.vote.down)),
-      "vote.nb": (p.vote.up + p.vote.down)
+      "vote.ratio": NumberInt(100*(p.vote.up - p.vote.down)/(p.vote.up + p.vote.down)),
+      "vote.nb": NumberInt(p.vote.up + p.vote.down)
     }
   });
   modified += 1;
