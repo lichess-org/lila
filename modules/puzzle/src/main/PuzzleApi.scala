38436ba8ae3803d754065c84cc5396d153c35421
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-11 21:18:16 +0100

    remove unnecessary check
    since now all dups from previous puzzles are ignored

--- modules/puzzle/src/main/PuzzleApi.scala
+++ modules/puzzle/src/main/PuzzleApi.scala
@@ -53,7 +53,6 @@ private[puzzle] final class PuzzleApi(
         puzzleColl.exists($doc(
           F.id -> $gte(puzzleIdMin),
           F.fen.$regex(fenStart.replace("/", "\\/"), ""),
-          F.voteRatio -> $gte(AggregateVote.minRatio)
         )) flatMap {
           case false => puzzleColl insert p inject id
           case _     => fufail(s"Duplicate puzzle $fenStart")
