bebe108b46c9212e97174d630d7ee47780182499
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-11 22:22:57 +0100

    unset puzzle vote.sum

--- bin/mongodb/puzzle-migrate-vote-disable.js
+++ bin/mongodb/puzzle-migrate-vote-disable.js
@@ -3,7 +3,7 @@ var puzzles = db.puzzle;
 modified = 0;
 
 puzzles.find({
-  'vote.ratio': {'$exists': false}
+  'vote.ratio': {$exists: false}
 }).forEach(function(p) {
   puzzles.update({
     _id: p._id
@@ -11,6 +11,9 @@ puzzles.find({
     $set: {
       "vote.ratio": NumberInt(100*(p.vote.up - p.vote.down)/(p.vote.up + p.vote.down)),
       "vote.nb": NumberInt(p.vote.up + p.vote.down)
+    },
+    $unset: {
+      'vote.sum': true
     }
   });
   modified += 1;
