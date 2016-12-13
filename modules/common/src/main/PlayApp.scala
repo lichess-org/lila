01c84eca6eac4065f4403f2eb77aa85478888176
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-13 14:16:21 +0100

    memoize the scheduler

--- modules/common/src/main/PlayApp.scala
+++ modules/common/src/main/PlayApp.scala
@@ -33,7 +33,7 @@ object PlayApp {
 
   private def enableScheduler = !(loadConfig getBoolean "app.scheduler.disabled")
 
-  def scheduler = new Scheduler(system.scheduler,
+  lazy val scheduler = new Scheduler(system.scheduler,
     enabled = enableScheduler && isServer,
     debug = loadConfig getBoolean "app.scheduler.debug")
 
