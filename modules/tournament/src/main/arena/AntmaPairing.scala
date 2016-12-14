6ddfff187eb47fec6e5938ec973ce197d29cf9d4
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-14 13:31:08 +0100

    disallow immediate tournament rematches in Antma pairing

--- modules/tournament/src/main/arena/AntmaPairing.scala
+++ modules/tournament/src/main/arena/AntmaPairing.scala
@@ -9,19 +9,15 @@ private object AntmaPairing {
   def apply(data: Data, players: RankedPlayers): List[Pairing.Prep] = players.nonEmpty ?? {
     import data._
 
-    def playedTogether(u1: String, u2: String) =
-      if (lastOpponents.hash.get(u1).contains(u2)) 1 else 0
+    def justPlayedTogether(u1: String, u2: String) =
+      lastOpponents.hash.get(u1).contains(u2) ||
+        lastOpponents.hash.get(u2).contains(u1)
 
-    def f(x: Int): Int = (11500000 - 3500000 * x) * x
-
-    def pairScore(a: RankedPlayer, b: RankedPlayer): Option[Int] = Some {
-      Math.abs(a.rank - b.rank) * 1000 +
-        Math.abs(a.player.rating - b.player.rating) +
-        f {
-          playedTogether(a.player.userId, b.player.userId) +
-            playedTogether(b.player.userId, a.player.userId)
-        }
-    }
+    def pairScore(a: RankedPlayer, b: RankedPlayer): Option[Int] =
+      !justPlayedTogether(a.player.userId, b.player.userId) option {
+        Math.abs(a.rank - b.rank) * 1000 +
+          Math.abs(a.player.rating - b.player.rating)
+      }
 
     WMMatching(players.toArray, pairScore).fold(
       err => {
