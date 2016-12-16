4fb0824ff0d6f79f816150f61824128ce57fb5d8
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-16 16:16:01 +0100

    no longer make reports for games terminated as cheated

--- modules/round/src/main/CheatDetector.scala
+++ modules/round/src/main/CheatDetector.scala
@@ -5,9 +5,7 @@ import chess.Color
 
 import lila.game.{ Game, GameRepo, Pov }
 
-private[round] final class CheatDetector(reporter: ActorSelection) {
-
-  private val createReport = false
+private[round] final class CheatDetector {
 
   def apply(game: Game): Fu[Option[Color]] = interesting(game) ?? {
     GameRepo findMirror game map {
@@ -15,8 +13,6 @@ private[round] final class CheatDetector(reporter: ActorSelection) {
         mirror.players map (p => p -> p.userId) collectFirst {
           case (player, Some(userId)) => game.players find (_.userId == player.userId) map { cheater =>
             lila.log("cheat").info(s"${cheater.color} ($userId) @ ${game.id} uses ${mirror.id}")
-            if (createReport) reporter ! lila.hub.actorApi.report.Cheater(userId,
-              s"Cheat detected on ${gameUrl(game.id)}, using lichess AI: ${gameUrl(mirror.id)}")
             cheater.color
           }
         } flatten
