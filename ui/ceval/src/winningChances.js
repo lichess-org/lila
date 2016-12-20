9b02635f086f612f3fd79d4eb0ab6f43111cedf2
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-20 13:02:41 +0100

    {master} make the winning chances equation more noob friendly

--- ui/ceval/src/winningChances.js
+++ ui/ceval/src/winningChances.js
@@ -4,9 +4,12 @@ function toPov(color, diff) {
   return color === 'white' ? diff : -diff;
 }
 
+/**
+ * https://graphsketch.com/?eqn1_color=1&eqn1_eqn=100+*+%282+%2F+%281+%2B+exp%28-0.005+*+x%29%29+-+1%29&eqn2_color=2&eqn2_eqn=100+*+%282+%2F+%281+%2B+exp%28-0.004+*+x%29%29+-+1%29&eqn3_color=3&eqn3_eqn=&eqn4_color=4&eqn4_eqn=&eqn5_color=5&eqn5_eqn=&eqn6_color=6&eqn6_eqn=&x_min=-1000&x_max=1000&y_min=-100&y_max=100&x_tick=100&y_tick=10&x_label_freq=2&y_label_freq=2&do_grid=0&do_grid=1&bold_labeled_lines=0&bold_labeled_lines=1&line_width=4&image_w=850&image_h=525
+ */
 function cpWinningChances(cp) {
   var normalized = Math.min(Math.max(-1000, cp), 1000);
-  return 2 / (1 + Math.exp(-0.005 * normalized)) - 1;
+  return 2 / (1 + Math.exp(-0.004 * normalized)) - 1;
 }
 
 function mateWinningChances(mate) {
