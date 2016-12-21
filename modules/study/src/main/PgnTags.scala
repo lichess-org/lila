3620197ef201525b1db7760d17a3a4792df363a4
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-21 15:11:26 +0100

    drop study chapter variant tag

--- modules/study/src/main/PgnTags.scala
+++ modules/study/src/main/PgnTags.scala
@@ -9,8 +9,7 @@ private object PgnTags {
   private def isRelevant(tag: Tag) = tag match {
     case Tag(t, _) if !relevantTags(t) => false
     case Tag(_, v) if unknownValues(v) => false
-    case Tag(Tag.Variant, v) if v.toLowerCase == "standard" => false
-    case _ => true
+    case _                             => true
   }
 
   private val unknownValues = Set("", "?", "unknown")
@@ -19,15 +18,14 @@ private object PgnTags {
     import Tag._
     Set(Event, Site, Date, Round, White, Black, TimeControl,
       WhiteElo, BlackElo, WhiteTitle, BlackTitle,
-      Tag.Result, Tag.FEN, Variant, Termination, Annotator)
+      Tag.Result, Tag.FEN, Termination, Annotator)
   }
 
   private def sort(tags: List[Tag]) = tags.sortBy {
     case Tag(Tag.White, _) => 1
     case Tag(Tag.Black, _) => 2
-    case Tag(Tag.Variant, _) => 3
-    case Tag(Tag.Date, _) => 4
-    case Tag(Tag.Termination, _) => 5
+    case Tag(Tag.Date, _) => 3
+    case Tag(Tag.Termination, _) => 4
     case Tag(Tag.Site | Tag.Event | Tag.Round | Tag.Annotator, _) => 10
     case _ => 9
   }
