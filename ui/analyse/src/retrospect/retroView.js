3db717019ee149494acb748064f609cd16a4aa9c
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-12-21 01:18:42 +0100

    tweak retrospective view

--- ui/analyse/src/retrospect/retroView.js
+++ ui/analyse/src/retrospect/retroView.js
@@ -154,14 +154,9 @@ function renderFeedback(root, fb) {
   var current = ctrl.current();
   if (ctrl.isSolving() && current && root.vm.path !== current.prev.path)
     return feedback.offTrack(ctrl, current);
-  if (fb === 'find') {
-    if (current) return feedback.find(ctrl, current);
-    return feedback.end(ctrl, root.flip, root.hasFullComputerAnalysis);
-  }
-  if (fb === 'win') return feedback.win(ctrl);
-  if (fb === 'fail') return feedback.fail(ctrl);
-  if (fb === 'eval') return feedback.eval(ctrl);
-  if (fb === 'view') return feedback.view(ctrl);
+  if (fb === 'find') return current ? feedback.find(ctrl, current) :
+    feedback.end(ctrl, root.flip, root.hasFullComputerAnalysis);
+  return feedback[fb](ctrl);
 }
 
 function renderTitle(ctrl) {
