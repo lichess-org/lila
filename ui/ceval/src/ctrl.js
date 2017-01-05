850d5df0f7da21ac39400e5dc131aaa123105e23
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-01-05 17:10:58 +0100

    update ceval sf to 050117 (has pv selection patch)

--- ui/ceval/src/ctrl.js
+++ ui/ceval/src/ctrl.js
@@ -25,8 +25,8 @@ module.exports = function(opts) {
   var hoveringUci = m.prop(null);
 
   var pool = makePool(stockfishProtocol, {
-    asmjs: '/assets/vendor/stockfish.js/stockfish.js?v=8',
-    pnacl: pnaclSupported && '/assets/vendor/stockfish.pexe/nacl/stockfish.nmf?v=8',
+    asmjs: '/assets/vendor/stockfish.js/stockfish.js?v=9',
+    pnacl: pnaclSupported && '/assets/vendor/stockfish.pexe/nacl/stockfish.nmf?v=9',
     onCrash: opts.onCrash
   }, {
     minDepth: minDepth,
