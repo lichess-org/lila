8fb2906ffbca7016503096221d4dc3b013a2ec31
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-01-15 14:19:20 +0100

    remove unused config

--- modules/hub/src/main/Env.scala
+++ modules/hub/src/main/Env.scala
@@ -25,7 +25,6 @@ final class Env(config: Config, system: ActorSystem) {
     val shutup = select("actor.shutup")
     val mod = select("actor.mod")
     val chat = select("actor.chat")
-    val analyser = select("actor.analyser")
     val moveBroadcast = select("actor.move_broadcast")
     val userRegister = select("actor.user_register")
     val notification = select("actor.notify")
