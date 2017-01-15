2be2b7becaacd2c345415f4027aa3fcbb1a88447
magicgoose <magicgoose@users.noreply.github.com>
2017-01-15 18:54:41 +0200

    Added 2 missing mainstream email domains

--- modules/security/src/main/DisposableEmailDomain.scala
+++ modules/security/src/main/DisposableEmailDomain.scala
@@ -75,7 +75,7 @@ object DisposableEmailDomain {
     "email.com", "games.com" /* AOL */ , "gmx.net", "hush.com", "hushmail.com", "icloud.com", "inbox.com",
     "lavabit.com", "love.com" /* AOL */ , "outlook.com", "pobox.com", "rocketmail.com" /* Yahoo */ ,
     "safe-mail.net", "wow.com" /* AOL */ , "ygm.com" /* AOL */ , "ymail.com" /* Yahoo */ , "zoho.com", "fastmail.fm",
-    "yandex.com",
+    "yandex.com", "cock.li", "airmail.cc"
 
     /* United States ISP domains */
     "bellsouth.net", "charter.net", "comcast.net", "cox.net", "earthlink.net", "juno.com",
