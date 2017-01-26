4f3621ac1bd814dc6edc18ba40ae1b37298f08e5
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-01-26 21:01:04 +0100

    rewrite ExpireSetMemo using Scaffeine

--- modules/memo/src/main/ExpireSetMemo.scala
+++ modules/memo/src/main/ExpireSetMemo.scala
@@ -1,25 +1,27 @@
 package lila.memo
 
-import scala.collection.JavaConversions._
+import com.github.blemale.scaffeine.{ Cache, Scaffeine }
 import scala.concurrent.duration.Duration
 
 final class ExpireSetMemo(ttl: Duration) {
 
-  protected val cache = Builder.expiry[String, Boolean](ttl)
+  private val cache: Cache[String, Boolean] = Scaffeine()
+    .expireAfterWrite(ttl)
+    .build[String, Boolean]
 
-  def get(key: String): Boolean = Option(cache getIfPresent key) getOrElse false
+  def get(key: String): Boolean = cache getIfPresent key getOrElse false
 
-  def put(key: String) { cache.put(key, true) }
+  def put(key: String) = cache.put(key, true)
 
   def putAll(keys: Iterable[String]) {
     keys.toList.distinct foreach { cache.put(_, true) }
   }
 
-  def remove(key: String) { cache invalidate key }
+  def remove(key: String) = cache invalidate key
 
   def keys: Iterable[String] = cache.asMap.keys
 
   def keySet: Set[String] = keys.toSet
 
-  def count = keys.toList.size
+  def count = cache.asMap.size
 }
