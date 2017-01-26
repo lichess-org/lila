134d5386fb18ce90ba3ef0063056b396d093934d
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-01-26 20:51:36 +0100

    rewrite UciMemo using Scaffeine

--- modules/game/src/main/UciMemo.scala
+++ modules/game/src/main/UciMemo.scala
@@ -1,41 +1,44 @@
 package lila.game
 
+import com.github.blemale.scaffeine.{ Cache, Scaffeine }
 import scala.concurrent.duration._
 
 import chess.format.UciDump
 
 final class UciMemo(ttl: Duration) {
 
-  private val memo = lila.memo.Builder.expiry[String, Vector[String]](ttl)
+  type UciVector = Vector[String]
+
+  private val cache: Cache[Game.ID, UciVector] = Scaffeine()
+    .expireAfterAccess(ttl)
+    .build[Game.ID, UciVector]
 
   private val hardLimit = 300
 
-  def add(game: Game, uciMove: String) {
-    val current = Option(memo getIfPresent game.id) | Vector.empty
-    memo.put(game.id, current :+ uciMove)
+  def add(game: Game, uciMove: String): Unit = {
+    val current = ~cache.getIfPresent(game.id)
+    cache.put(game.id, current :+ uciMove)
   }
-  def add(game: Game, move: chess.MoveOrDrop) {
+  def add(game: Game, move: chess.MoveOrDrop): Unit =
     add(game, UciDump.move(game.variant)(move))
-  }
 
-  def set(game: Game, uciMoves: Seq[String]) {
-    memo.put(game.id, uciMoves.toVector)
-  }
+  def set(game: Game, uciMoves: Seq[String]) =
+    cache.put(game.id, uciMoves.toVector)
 
-  def get(game: Game, max: Int = hardLimit): Fu[Vector[String]] =
-    Option(memo getIfPresent game.id) filter { moves =>
+  def get(game: Game, max: Int = hardLimit): Fu[UciVector] =
+    cache getIfPresent game.id filter { moves =>
         moves.size.min(max) == game.pgnMoves.size.min(max)
       } match {
         case Some(moves) => fuccess(moves)
         case _           => compute(game, max) addEffect { set(game, _) }
       }
 
-  def drop(game: Game, nb: Int) {
-    val current = Option(memo getIfPresent game.id) | Vector.empty
-    memo.put(game.id, current.take(current.size - nb))
+  def drop(game: Game, nb: Int) = {
+    val current = ~cache.getIfPresent(game.id)
+    cache.put(game.id, current.take(current.size - nb))
   }
 
-  private def compute(game: Game, max: Int): Fu[Vector[String]] = for {
+  private def compute(game: Game, max: Int): Fu[UciVector] = for {
     fen ← GameRepo initialFen game
     uciMoves ← UciDump(game.pgnMoves.take(max), fen, game.variant).future
   } yield uciMoves.toVector
