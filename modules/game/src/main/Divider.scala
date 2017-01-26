47048af3eaea10e79e76d11e0fcf32a9cd153385
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-01-26 20:41:49 +0100

    rewrite Divider cache with Scaffeine

--- modules/game/src/main/Divider.scala
+++ modules/game/src/main/Divider.scala
@@ -1,22 +1,26 @@
 package lila.game
 
-import lila.memo.Builder
-import chess.variant.Variant
+import com.github.blemale.scaffeine.{ Cache, Scaffeine }
+import scala.concurrent.duration._
+
 import chess.Division
+import chess.variant.Variant
 
 final class Divider {
 
-  private val cache = Builder.size[String, Division](5000)
+  private val cache: Cache[Game.ID, Division] =
+    Scaffeine()
+      .expireAfterAccess(20 minutes)
+      .build[Game.ID, Division]
 
   def apply(game: Game, initialFen: Option[String]): Division =
-    if (!Variant.divisionSensibleVariants.contains(game.variant)) Division.empty
-    else Option(cache getIfPresent game.id) | {
+    if (!Variant.divisionSensibleVariants(game.variant)) Division.empty
+    else cache.get(game.id, _ => {
       val div = chess.Replay.boards(
         moveStrs = game.pgnMoves,
         initialFen = initialFen map chess.format.FEN,
         variant = game.variant
       ).toOption.fold(Division.empty)(chess.Divider.apply)
-      cache.put(game.id, div)
       div
-    }
+    })
 }
