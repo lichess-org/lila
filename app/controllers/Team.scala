96de98acc3206bd600a8529014d01b585d0cd266
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-01-26 12:05:21 +0100

    fix code formatting

--- app/controllers/Team.scala
+++ app/controllers/Team.scala
@@ -51,37 +51,41 @@ object Team extends LilaController {
       case (info, pag) => html.team.show(team, pag, info)
     }
 
-  def edit(id: String) = Auth { implicit ctx => me => OptionFuResult(api team id) { team =>
-    Owner(team) { fuccess(html.team.edit(team, forms edit team)) }
-  }
-  }
-
-  def update(id: String) = AuthBody { implicit ctx => implicit me => OptionFuResult(api team id) { team =>
-    Owner(team) {
-      implicit val req = ctx.body
-      forms.edit(team).bindFromRequest.fold(
-        err => BadRequest(html.team.edit(team, err)).fuccess,
-        data => api.update(team, data, me) inject Redirect(routes.Team.show(team.id))
-      )
+  def edit(id: String) = Auth { implicit ctx => me =>
+    OptionFuResult(api team id) { team =>
+      Owner(team) { fuccess(html.team.edit(team, forms edit team)) }
     }
   }
-  }
 
-  def kickForm(id: String) = Auth { implicit ctx => me => OptionFuResult(api team id) { team =>
-    Owner(team) {
-      MemberRepo userIdsByTeam team.id map { userIds =>
-        html.team.kick(team, userIds.filterNot(me.id ==).toList.sorted)
+  def update(id: String) = AuthBody { implicit ctx => implicit me =>
+    OptionFuResult(api team id) { team =>
+      Owner(team) {
+        implicit val req = ctx.body
+        forms.edit(team).bindFromRequest.fold(
+          err => BadRequest(html.team.edit(team, err)).fuccess,
+          data => api.update(team, data, me) inject Redirect(routes.Team.show(team.id))
+        )
       }
     }
   }
-  }
 
-  def kick(id: String) = AuthBody { implicit ctx => implicit me => OptionFuResult(api team id) { team =>
-    Owner(team) {
-      implicit val req = ctx.body
-      forms.kick.bindFromRequest.value ?? { api.kick(team, _) } inject Redirect(routes.Team.show(team.id))
+  def kickForm(id: String) = Auth { implicit ctx => me =>
+    OptionFuResult(api team id) { team =>
+      Owner(team) {
+        MemberRepo userIdsByTeam team.id map { userIds =>
+          html.team.kick(team, userIds.filterNot(me.id ==).toList.sorted)
+        }
+      }
     }
   }
+
+  def kick(id: String) = AuthBody { implicit ctx => me =>
+    OptionFuResult(api team id) { team =>
+      Owner(team) {
+        implicit val req = ctx.body
+        forms.kick.bindFromRequest.value ?? { api.kick(team, _) } inject Redirect(routes.Team.show(team.id))
+      }
+    }
   }
 
   def close(id: String) = Secure(_.CloseTeam) { implicit ctx => me =>
@@ -102,20 +106,22 @@ object Team extends LilaController {
     }
   }
 
-  def create = AuthBody { implicit ctx => implicit me => OnePerWeek(me) {
-    implicit val req = ctx.body
-    forms.create.bindFromRequest.fold(
-      err => forms.anyCaptcha map { captcha =>
-        BadRequest(html.team.form(err, captcha))
-      },
-      data => api.create(data, me) ?? {
-        _ map { team => Redirect(routes.Team.show(team.id)): Result }
-      }
-    )
-  }
+  def create = AuthBody { implicit ctx => implicit me =>
+    OnePerWeek(me) {
+      implicit val req = ctx.body
+      forms.create.bindFromRequest.fold(
+        err => forms.anyCaptcha map { captcha =>
+          BadRequest(html.team.form(err, captcha))
+        },
+        data => api.create(data, me) ?? {
+          _ map { team => Redirect(routes.Team.show(team.id)): Result }
+        }
+      )
+    }
   }
 
-  def mine = Auth { implicit ctx => me => api mine me map { html.team.mine(_) }
+  def mine = Auth { implicit ctx => me =>
+    api mine me map { html.team.mine(_) }
   }
 
   def joinPage(id: String) = Auth { implicit ctx => me =>
@@ -129,51 +135,57 @@ object Team extends LilaController {
     }
   }
 
-  def join(id: String) = Auth { implicit ctx => implicit me => api join id flatMap {
-    case Some(Joined(team))   => Redirect(routes.Team.show(team.id)).fuccess
-    case Some(Motivate(team)) => Redirect(routes.Team.requestForm(team.id)).fuccess
-    case _                    => notFound
-  }
-  }
-
-  def requests = Auth { implicit ctx => me => api requestsWithUsers me map { html.team.allRequests(_) }
+  def join(id: String) = Auth { implicit ctx => implicit me =>
+    api join id flatMap {
+      case Some(Joined(team))   => Redirect(routes.Team.show(team.id)).fuccess
+      case Some(Motivate(team)) => Redirect(routes.Team.requestForm(team.id)).fuccess
+      case _                    => notFound
+    }
   }
 
-  def requestForm(id: String) = Auth { implicit ctx => me => OptionFuOk(api.requestable(id, me)) { team =>
-    forms.anyCaptcha map { html.team.requestForm(team, forms.request, _) }
-  }
-  }
-
-  def requestCreate(id: String) = AuthBody { implicit ctx => me => OptionFuResult(api.requestable(id, me)) { team =>
-    implicit val req = ctx.body
-    forms.request.bindFromRequest.fold(
-      err => forms.anyCaptcha map { captcha =>
-        BadRequest(html.team.requestForm(team, err, captcha))
-      },
-      setup => api.createRequest(team, setup, me) inject Redirect(routes.Team.show(team.id))
-    )
-  }
+  def requests = Auth { implicit ctx => me =>
+    api requestsWithUsers me map { html.team.allRequests(_) }
   }
 
-  def requestProcess(requestId: String) = AuthBody { implicit ctx => me => OptionFuRedirectUrl(for {
-    requestOption ← api request requestId
-    teamOption ← requestOption.??(req => TeamRepo.owned(req.team, me.id))
-  } yield (teamOption |@| requestOption).tupled) {
-    case (team, request) => {
+  def requestForm(id: String) = Auth { implicit ctx => me =>
+    OptionFuOk(api.requestable(id, me)) { team =>
+      forms.anyCaptcha map { html.team.requestForm(team, forms.request, _) }
+    }
+  }
+
+  def requestCreate(id: String) = AuthBody { implicit ctx => me =>
+    OptionFuResult(api.requestable(id, me)) { team =>
       implicit val req = ctx.body
-      forms.processRequest.bindFromRequest.fold(
-        _ => fuccess(routes.Team.show(team.id).toString), {
-          case (decision, url) =>
-            api.processRequest(team, request, (decision === "accept")) inject url
-        }
+      forms.request.bindFromRequest.fold(
+        err => forms.anyCaptcha map { captcha =>
+          BadRequest(html.team.requestForm(team, err, captcha))
+        },
+        setup => api.createRequest(team, setup, me) inject Redirect(routes.Team.show(team.id))
       )
     }
   }
+
+  def requestProcess(requestId: String) = AuthBody { implicit ctx => me =>
+    OptionFuRedirectUrl(for {
+      requestOption ← api request requestId
+      teamOption ← requestOption.??(req => TeamRepo.owned(req.team, me.id))
+    } yield (teamOption |@| requestOption).tupled) {
+      case (team, request) => {
+        implicit val req = ctx.body
+        forms.processRequest.bindFromRequest.fold(
+          _ => fuccess(routes.Team.show(team.id).toString), {
+            case (decision, url) =>
+              api.processRequest(team, request, (decision === "accept")) inject url
+          }
+        )
+      }
+    }
   }
 
-  def quit(id: String) = Auth { implicit ctx => implicit me => OptionResult(api quit id) { team =>
-    Redirect(routes.Team.show(team.id))
-  }
+  def quit(id: String) = Auth { implicit ctx => implicit me =>
+    OptionResult(api quit id) { team =>
+      Redirect(routes.Team.show(team.id))
+    }
   }
 
   private def OnePerWeek[A <: Result](me: UserModel)(a: => Fu[A])(implicit ctx: Context): Fu[Result] =
