2400f08f68d0c70e5c75a365fc8b664c143268aa
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-01-26 18:06:14 +0100

    implement RateLimit with Scaffeine

--- project/Build.scala
+++ project/Build.scala
@@ -36,7 +36,7 @@ object ApplicationBuild extends Build {
         jgit, findbugs, reactivemongo.driver, reactivemongo.iteratees, akka.actor, akka.slf4j,
         spray.caching, maxmind, prismic,
         kamon.core, kamon.statsd, kamon.influxdb,
-        java8compat, semver, scrimage, configs, guava, scalaffeine),
+        java8compat, semver, scrimage, configs, guava, scaffeine),
       TwirlKeys.templateImports ++= Seq(
         "lila.game.{ Game, Player, Pov }",
         "lila.tournament.Tournament",
@@ -133,7 +133,7 @@ object ApplicationBuild extends Build {
   )
 
   lazy val memo = project("memo", Seq(common, db)).settings(
-    libraryDependencies ++= Seq(findbugs, spray.caching, configs) ++ provided(play.api, reactivemongo.driver)
+    libraryDependencies ++= Seq(findbugs, spray.caching, scaffeine, configs) ++ provided(play.api, reactivemongo.driver)
   )
 
   lazy val search = project("search", Seq(common, hub)).settings(
