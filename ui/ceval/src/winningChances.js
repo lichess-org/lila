0c3e871015676341752df9e3afaedf67556f1fb4
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-01-27 11:13:03 +0100

    make sure the gauge is impressed by mate perspectives - closes #2573

--- ui/ceval/src/winningChances.js
+++ ui/ceval/src/winningChances.js
@@ -7,21 +7,22 @@ function toPov(color, diff) {
 /**
  * https://graphsketch.com/?eqn1_color=1&eqn1_eqn=100+*+%282+%2F+%281+%2B+exp%28-0.005+*+x%29%29+-+1%29&eqn2_color=2&eqn2_eqn=100+*+%282+%2F+%281+%2B+exp%28-0.004+*+x%29%29+-+1%29&eqn3_color=3&eqn3_eqn=&eqn4_color=4&eqn4_eqn=&eqn5_color=5&eqn5_eqn=&eqn6_color=6&eqn6_eqn=&x_min=-1000&x_max=1000&y_min=-100&y_max=100&x_tick=100&y_tick=10&x_label_freq=2&y_label_freq=2&do_grid=0&do_grid=1&bold_labeled_lines=0&bold_labeled_lines=1&line_width=4&image_w=850&image_h=525
  */
+function rawWinningChances(cp) {
+  return 2 / (1 + Math.exp(-0.004 * cp)) - 1;
+}
+
 function cpWinningChances(cp) {
-  var normalized = Math.min(Math.max(-1000, cp), 1000);
-  return 2 / (1 + Math.exp(-0.004 * normalized)) - 1;
+  return rawWinningChances(Math.min(Math.max(-1000, cp), 1000));
 }
 
 function mateWinningChances(mate) {
   var cp = (21 - Math.min(10, Math.abs(mate))) * 100;
   var signed = cp * (mate > 0 ? 1 : -1);
-  return cpWinningChances(signed);
+  return rawWinningChances(signed);
 }
 
 function evalWinningChances(eval) {
-  return defined(eval.cp) ?
-    cpWinningChances(eval.cp) :
-    mateWinningChances(eval.mate);
+  return defined(eval.cp) ? cpWinningChances(eval.cp) : mateWinningChances(eval.mate);
 }
 
 function povChances(color, eval) {
