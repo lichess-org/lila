20dfbda9d94755d42fc84e909515d0cc572b9dd7
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-02-01 12:16:39 +0100

    optimise eval cache storage

--- modules/evalCache/src/main/BSONHandlers.scala
+++ modules/evalCache/src/main/BSONHandlers.scala
@@ -17,31 +17,24 @@ object BSONHandlers {
     v => SmallFen raw FEN(v))
   private implicit val TrustBSONHandler = doubleAnyValHandler[Trust](_.value, Trust.apply)
 
-  implicit val UciHandler = new BSONHandler[BSONString, Uci] {
-    def read(bs: BSONString): Uci = Uci(bs.value) err s"Bad UCI: ${bs.value}"
-    def write(x: Uci) = BSONString(x.uci)
-  }
-  implicit val MovesHandler = new BSONHandler[BSONString, Moves] {
-    private val separator = " "
-    def read(bs: BSONString): Moves = Moves {
-      bs.value.split(separator).toList.flatMap { Uci(_) }.toNel err s"Invalid Pv ${bs.value}"
+  implicit val PvHandler = new BSONHandler[BSONString, Pv] {
+    private def scoreWrite(s: Score): String = s.value.fold(_.value.toString, m => s"#${m.value}")
+    private def scoreRead(str: String): Option[Score] =
+      if (str startsWith "#") parseIntOption(str drop 1) map { m => Score mate Mate(m) }
+      else parseIntOption(str) map { c => Score cp Cp(c) }
+    private def movesWrite(moves: Moves): String = Uci writeListPiotr moves.value.list
+    private def movesRead(str: String): Option[Moves] =
+      Uci readListPiotr str flatMap (_.toNel) map Moves.apply
+    private val separator = '|'
+    def read(bs: BSONString): Pv = bs.value.split(separator) match {
+      case Array(score, moves) => Pv(
+        scoreRead(score) err s"Invalid score $score",
+        movesRead(moves) err s"Invalid moves $moves")
+      case _ => sys error s"Invalid PV ${bs.value}"
     }
-    def write(x: Moves) = BSONString(x.value.list map (_.uci) mkString separator)
-  }
-  implicit val ScoreHandler = new BSONHandler[BSONInteger, Score] {
-    private val sillyThreshold = math.pow(10, 6).toInt
-    def read(bs: BSONInteger): Score =
-      if (bs.value > sillyThreshold) Score.mate(Mate(bs.value - sillyThreshold))
-      else if (bs.value < -sillyThreshold) Score.mate(Mate(bs.value + sillyThreshold))
-      else Score cp Cp(bs.value)
-    def write(x: Score) = BSONInteger(x.value match {
-      case Left(cp)                => cp.value
-      case Right(mate) if mate > 0 => mate.value + sillyThreshold
-      case Right(mate) if mate < 0 => mate.value - sillyThreshold
-    })
+    def write(x: Pv) = BSONString(s"${scoreWrite(x.score)}$separator${movesWrite(x.moves)}")
   }
 
-  implicit val pvHandler = Macros.handler[Pv]
   implicit val evalHandler = Macros.handler[Eval]
   implicit val trustedEvalHandler = Macros.handler[TrustedEval]
   implicit val entryHandler = Macros.handler[EvalCacheEntry]
