28103c670bc2efbfcaf09683dc407631e6dddbff
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-02-01 12:55:02 +0100

    no longer preload study destinations, as to leverage eval cache

--- app/controllers/Practice.scala
+++ app/controllers/Practice.scala
@@ -58,7 +58,7 @@ object Practice extends LilaController {
           case (baseData, studyJson) =>
             val analysis = baseData ++ Json.obj(
               "treeParts" -> partitionTreeJsonWriter.writes {
-                lila.study.TreeBuilder(chapter.root, chapter.setup.variant)
+                lila.study.TreeBuilder(chapter.root)
               },
               "practiceGoal" -> lila.practice.PracticeGoal(chapter))
             (analysis, studyJson)
