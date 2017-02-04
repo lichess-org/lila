29a9812b538966a73b5a7a5dadc65c1a1b65233e
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-02-04 10:49:56 +0100

    don't wait for higher depth than possible

--- ui/analyse/src/practice/practiceCtrl.js
+++ ui/analyse/src/practice/practiceCtrl.js
@@ -26,7 +26,10 @@ module.exports = function(root, playableDepth) {
   };
   var playable = function(node) {
     var ceval = node.ceval;
-    return ceval && (ceval.depth >= playableDepth() || (ceval.depth >= 15 && ceval.millis > 5000));
+    return ceval && (
+      ceval.depth >= Math.min(ceval.maxDepth, playableDepth()) ||
+      (ceval.depth >= 15 && ceval.millis > 5000)
+    );
   };
 
   var altCastles = {
