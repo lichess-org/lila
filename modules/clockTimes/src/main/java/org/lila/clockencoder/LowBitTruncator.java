4128539b5121c36dc318a9b922942aa0ead2ce23
Isaac Levy <isaac.r.levy@gmail.com>
2017-02-19 16:10:01 -0500

    Fix bugs in LowBitTruncator

--- modules/clockTimes/src/main/java/org/lila/clockencoder/LowBitTruncator.java
+++ modules/clockTimes/src/main/java/org/lila/clockencoder/LowBitTruncator.java
@@ -4,7 +4,6 @@ public class LowBitTruncator {
     // Truncate 3 bits from centisecs, but preserve precision for low values.
     // CENTI_CUTOFF must be a multiple of 8 (the truncation divisor)
     private static final int CENTI_CUTOFF = 1000;
-    private static final int TRUNC_CUTOFF = CENTI_CUTOFF >>> 3;
 
     public static TruncPair lossyEncode(int[] centis) {
         IntArrayList truncDigits = new IntArrayList();
@@ -13,7 +12,7 @@ public class LowBitTruncator {
 
         for (int i = 0; i < moves; i++) {
             int cs = centis[i];
-            trunced[i] = cs >>> 3;
+            trunced[i] = cs >> 3;
             if (cs < CENTI_CUTOFF) truncDigits.add(cs & 0x07);
         }
         return new TruncPair(trunced, truncDigits);
@@ -32,7 +31,7 @@ public class LowBitTruncator {
         int[] centis = new int[moves];
         for (int i = 0; i < moves; i++) {
             int rounded = trunced[i] << 3;
-            if (rounded < TRUNC_CUTOFF) {
+            if (rounded < CENTI_CUTOFF) {
                 centis[i] = rounded | reader.readBits(3);
             } else {
                 // Truncation cuts off 3.5 on average.
@@ -43,8 +42,8 @@ public class LowBitTruncator {
     }
 
     public static class TruncPair {
-        final int[] trunced;
-        final IntArrayList lowBits;
+        public final int[] trunced;
+        public final IntArrayList lowBits;
         TruncPair(int[] trunced, IntArrayList lowBits) {
             this.trunced = trunced;
             this.lowBits = lowBits;
