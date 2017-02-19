fe0907adda966d1d6d5c2a48a2f6b6a7b9e78888
Isaac Levy <isaac.r.levy@gmail.com>
2017-02-19 02:22:43 -0500

    Simplify LinearEstimator
    Remove divides and unneeded logic

--- modules/clockTimes/src/main/java/clockencoder/LinearEstimator.java
+++ modules/clockTimes/src/main/java/clockencoder/LinearEstimator.java
@@ -18,26 +18,23 @@ public class LinearEstimator {
         dest[size - 1] = src[size - 1];
 
         int[] realValues = isEncoding ? src : dest;
-        encodeHelper(src, dest, realValues, 0, size - 1);
-
-        return dest;
+        return encodeHelper(src, dest, realValues, 0, size - 1);
     }
 
-    private static void encodeHelper(int[] src, int[] dest, int[] realValues,
+    private static int[] encodeHelper(int[] src, int[] dest, int[] realValues,
                                      int startIdx, int endIdx) {
         int l = endIdx - startIdx;
-        if (l == 0) return;
-
-        int midIdx = startIdx + l / 2;
+        if (l == 0) return dest;
 
         // It's important to save estimate in fixed precision to ensure
         // the encode and decode math behaves identically.
-        int estimate = (realValues[startIdx] * (endIdx - midIdx) +
-                        realValues[endIdx]   * (midIdx - startIdx)) / l;
+        int estimate = (realValues[startIdx] + realValues[endIdx]) >>> 1;
+
+        int midIdx = startIdx + (l >>> 1);
 
         dest[midIdx] = estimate - src[midIdx];
 
         encodeHelper(src, dest, realValues, startIdx, midIdx);
-        encodeHelper(src, dest, realValues, midIdx, endIdx);
+        return encodeHelper(src, dest, realValues, midIdx, endIdx);
     }
 }
