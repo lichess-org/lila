50c827c7ce741d7e7098b0260672377d43871cb7
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-02-22 10:07:08 +0100

    simplify move time encoding failure tests

--- modules/game/src/test/BinaryMoveTimeTest.scala
+++ modules/game/src/test/BinaryMoveTimeTest.scala
@@ -32,7 +32,7 @@ class BinaryMoveTimeTest extends Specification {
         Vector(1, 10, 100, 5, 600, 1)
       }
     }
-    "buckets" in {
+    "buckets - long game" in {
       val times = Vector(
         0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63,
         66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99, 102, 105, 108, 199, 333, 567, 666, 2000
@@ -46,16 +46,18 @@ class BinaryMoveTimeTest extends Specification {
       val again = BinaryFormat.moveTime.read(BinaryFormat.moveTime.write(rounded))
       again must_== rounded
     }
-    "buckets 2" in {
-      val times = Vector(
-        0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63,
-        66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99, 102, 105, 108, 199, 333, 567, 666
-      ).map(_ * 100 millis)
+    "buckets - short game" in {
+      val times = Vector(0, 3, 6, 9).map(_ * 100 millis)
       val rounded = BinaryFormat.moveTime.read(BinaryFormat.moveTime.write(times))
-      val expected = Vector(
-        1, 1, 5, 10, 10, 15, 20, 20, 20, 30, 30, 30, 40, 40, 40, 40, 50, 50, 50, 60, 60, 60,
-        60, 60, 80, 80, 80, 80, 80, 80, 80, 100, 100, 100, 100, 100, 100, 200, 300, 600, 600
-      ).map(_ * 100 millis)
+      val expected = Vector(1, 1, 5, 10).map(_ * 100 millis)
+      rounded must_== expected
+      val again = BinaryFormat.moveTime.read(BinaryFormat.moveTime.write(rounded))
+      again must_== rounded
+    }
+    "buckets - short game - odd number of moves" in {
+      val times = Vector(0, 3, 6).map(_ * 100 millis)
+      val rounded = BinaryFormat.moveTime.read(BinaryFormat.moveTime.write(times))
+      val expected = Vector(1, 1, 5).map(_ * 100 millis)
       rounded must_== expected
       val again = BinaryFormat.moveTime.read(BinaryFormat.moveTime.write(rounded))
       again must_== rounded
