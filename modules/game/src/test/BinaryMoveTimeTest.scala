ef048326c9f956c25c27ab908a71510981745144
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-02-22 10:05:37 +0100

    failing test for move time encoding

--- modules/game/src/test/BinaryMoveTimeTest.scala
+++ modules/game/src/test/BinaryMoveTimeTest.scala
@@ -43,6 +43,22 @@ class BinaryMoveTimeTest extends Specification {
         60, 60, 80, 80, 80, 80, 80, 80, 80, 100, 100, 100, 100, 100, 100, 200, 300, 600, 600, 600
       ).map(_ * 100 millis)
       rounded must_== expected
+      val again = BinaryFormat.moveTime.read(BinaryFormat.moveTime.write(rounded))
+      again must_== rounded
+    }
+    "buckets 2" in {
+      val times = Vector(
+        0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63,
+        66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99, 102, 105, 108, 199, 333, 567, 666
+      ).map(_ * 100 millis)
+      val rounded = BinaryFormat.moveTime.read(BinaryFormat.moveTime.write(times))
+      val expected = Vector(
+        1, 1, 5, 10, 10, 15, 20, 20, 20, 30, 30, 30, 40, 40, 40, 40, 50, 50, 50, 60, 60, 60,
+        60, 60, 80, 80, 80, 80, 80, 80, 80, 100, 100, 100, 100, 100, 100, 200, 300, 600, 600
+      ).map(_ * 100 millis)
+      rounded must_== expected
+      val again = BinaryFormat.moveTime.read(BinaryFormat.moveTime.write(rounded))
+      again must_== rounded
     }
   }
 }
