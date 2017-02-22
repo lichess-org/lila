e743a7b3d86397328b5257df8782e02a9214967e
Isaac Levy <isaac.r.levy@gmail.com>
2017-02-22 10:35:16 -0500

    Update EndTimeEstimator.java
    use linear estimate

--- modules/clockTimes/src/main/java/org/lila/clockencoder/EndTimeEstimator.java
+++ modules/clockTimes/src/main/java/org/lila/clockencoder/EndTimeEstimator.java
@@ -2,14 +2,20 @@ package org.lila.clockencoder;
 
 public class EndTimeEstimator {
     public static void encode(int[] vals, int startTime) {
-        int moves = vals.length;
+        int maxIdx = vals.length - 1;
+        // Linear estimate with move 0 == startTime and move 32 == 0.
         // TODO: Test this.
-        vals[moves - 1] -= (startTime >> (moves >> 4));
+        if (maxIdx < 32) {
+            vals[maxIdx] -= startTime - ((startTime * maxIdx) >>> 5);
+        }
     }
 
     public static void decode(int[] vals, int startTime) {
-        int moves = vals.length;
+        int maxIdx = vals.length - 1;
+        // Linear estimate with move 0 == startTime and move 32 == 0.
         // TODO: Test this.
-        vals[moves - 1] += (startTime >> (moves >> 4));
+        if (maxIdx < 32) {
+            vals[maxIdx] += startTime - ((startTime * maxIdx) >>> 5);
+        }
     }
 }
