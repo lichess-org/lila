0ee7545c4948eaa274e463d23095dabac980d3e8
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-03-01 00:47:11 +0100

    fix analysis crazyhouse support using new chessground API

--- ui/analyse/src/crazy/crazyDrag.js
+++ ui/analyse/src/crazy/crazyDrag.js
@@ -9,30 +9,5 @@ module.exports = function(ctrl, color, e) {
   if (!role || !color || number === '0') return;
   e.stopPropagation();
   e.preventDefault();
-  var key = 'a0';
-  var coords = util.key2pos(ctrl.chessground.state.orientation === 'white' ? key : util.invertKey(key));
-  var piece = {
-    role: role,
-    color: color
-  };
-  var obj = {};
-  obj[key] = piece;
-  ctrl.chessground.setPieces(obj);
-  var bounds = ctrl.chessground.state.dom.bounds();
-  var squareBounds = ctrl.vm.element.querySelector('.cg-board piece').getBoundingClientRect();
-  var rel = [
-    (coords[0] - 1) * squareBounds.width + bounds.left, (8 - coords[1]) * squareBounds.height + bounds.top
-  ];
-  ctrl.chessground.state.draggable.current = {
-    orig: key,
-    piece: piece.color + piece.role,
-    rel: rel,
-    epos: [e.clientX, e.clientY],
-    pos: [e.clientX - rel[0], e.clientY - rel[1]],
-    dec: [-squareBounds.width / 2, -squareBounds.height / 2],
-    bounds: bounds,
-    started: true,
-    newPiece: true
-  };
-  drag.processDrag(ctrl.chessground.state);
+  drag.dragNewPiece(ctrl.chessground.state, { color: color, role: role }, e);
 }
