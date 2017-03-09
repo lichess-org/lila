ac0f719e21f9a34cafb807ec866022a9dcfb51e3
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-03-09 12:16:51 +0100

    extract analytics code out of HTML

--- public/javascripts/ga.js
+++ public/javascripts/ga.js
@@ -1,7 +1,5 @@
-@()
-<script>
 lichess.requestIdleCallback(function() {
-if (/.+\.lichess\.org/.test(document.domain)) {
+if (/.+\.lichess\.org/.test(document.domain) && !window.dataLayer) {
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -10,4 +8,3 @@ ga('create', 'UA-7935029-3', 'auto');
 ga('send', 'pageview');
 }
 });
-</script>
