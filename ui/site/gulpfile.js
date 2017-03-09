91169102c18e0a738e8c7b170ec937d863f130fa
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-03-09 15:08:20 +0100

    improve ui/site build

--- ui/site/gulpfile.js
+++ ui/site/gulpfile.js
@@ -1,7 +1,6 @@
 var source = require('vinyl-source-stream');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var watchify = require('watchify');
 var browserify = require('browserify');
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
@@ -13,6 +12,12 @@ var onError = function(error) {
   gutil.log(gutil.colors.red(error.message));
 };
 var standalone = 'Lichess';
+
+gulp.task('jquery-fill', function() {
+  return gulp.src('src/jquery.fill.js')
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('prod-source', function() {
   return browserify('./src/index.js', {
@@ -33,32 +38,12 @@ gulp.task('dev-source', function() {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('watch', function() {
-  var opts = watchify.args;
-  opts.debug = true;
-  opts.standalone = standalone;
-
-  var bundleStream = watchify(browserify(sources, opts))
-    .on('update', rebundle)
-    .on('log', gutil.log);
-
-  function rebundle() {
-    return bundleStream.bundle()
-      .on('error', onError)
-      .pipe(source('lichess.site.js'))
-      .pipe(gulp.dest(destination))
-      .pipe(addDepsTo('lichess.site.js'));
-  }
-
-  return rebundle();
-});
-
-function addDepsTo(filename) {
+function makeBundle(filename) {
   return function() {
     return gulp.src([
-      './dep/jquery.min.js',
-      './src/jquery.fill.js',
-      './dep/moment.min.js',
+      '../../public/javascripts/vendor/jquery.min.js',
+      './dist/jquery.fill.js',
+      '../../public/vendor/moment/min/moment.min.js',
       './dep/misc.min.js',
       './dist/' + filename
     ])
@@ -67,9 +52,13 @@ function addDepsTo(filename) {
   };
 }
 
-gulp.task('dev-bundle', addDepsTo('lichess.site.source.js'));
-gulp.task('prod-bundle', addDepsTo('lichess.site.source.min.js'));
+gulp.task('dev-bundle', makeBundle('lichess.site.source.js'));
+gulp.task('prod-bundle', makeBundle('lichess.site.source.min.js'));
 
-gulp.task('default', ['watch']);
-gulp.task('dev', ['dev-source', 'dev-bundle']);
-gulp.task('prod', ['prod-source', 'prod-bundle']);
+gulp.task('dev', ['jquery-fill', 'dev-source', 'dev-bundle']);
+gulp.task('prod', ['jquery-fill', 'prod-source', 'prod-bundle']);
+
+gulp.task('dev-watch', function() {
+  return gulp.watch('src/*.js', ['dev']);
+});
+gulp.task('default', ['dev', 'dev-watch']);
