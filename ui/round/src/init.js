bac74b307fa8194ebde7c6f6bf44d3c6e2cd64c9
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-03-09 17:07:33 +0100

    allow page reload when playing an AI

--- ui/round/src/init.js
+++ ui/round/src/init.js
@@ -26,7 +26,7 @@ module.exports = {
 
     if (game.isPlayerPlaying(d)) {
       window.addEventListener('beforeunload', function(e) {
-        if (!lichess.hasToReload && !ctrl.data.blind && game.playable(ctrl.data) && ctrl.data.clock) {
+        if (!lichess.hasToReload && !ctrl.data.blind && game.playable(ctrl.data) && ctrl.data.clock && !ctrl.data.opponent.ai) {
           document.body.classList.remove('fpmenu');
           ctrl.socket.send('bye2');
           var msg = 'There is a game in progress!';
