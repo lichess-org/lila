25970a3a36a6d62d72d41b6aaa5ed524583d684a
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-03-10 10:39:56 +0100

    fix analytics script

--- public/javascripts/ga.js
+++ public/javascripts/ga.js
@@ -1,4 +1,4 @@
-(window.lichess ? window.lichess.requestIdleCallback : setTimeout)(function() {
+(window.lichess ? lichess.requestIdleCallback : setTimeout)(function() {
 if (/.+\.lichess\.org/.test(document.domain) && !window.dataLayer) {
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -7,4 +7,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-7935029-3', 'auto');
 ga('send', 'pageview');
 }
-}, 200);
+});
