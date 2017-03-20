6d88556fd94d4ddf6bbee663c6a7faa998a12f4c
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-03-20 12:27:47 +0100

    introduce case class Centis - for #2701 & #2718

--- modules/common/src/main/model.scala
+++ modules/common/src/main/model.scala
@@ -1,5 +1,7 @@
 package lila.common
 
+import scala.concurrent.duration._
+
 case class ApiVersion(value: Int) extends AnyVal with IntValue {
   def v1 = value == 1
   def v2 = value == 2
@@ -19,4 +21,31 @@ object IpAddress {
 
   def isv4(a: IpAddress) = ipv4Regex matches a.value
   def isv6(a: IpAddress) = ipv6Regex matches a.value
+}
+
+// maximum value = Int.MaxValue / 100 / 60 / 60 / 24 = 248 days
+case class Centis(cs: Int) extends AnyVal {
+
+  def toDuration = FiniteDuration(cs * 10, MILLISECONDS)
+
+  def +(other: Centis) = Centis(cs + other.cs)
+  def -(other: Centis) = Centis(cs - other.cs)
+  def *(scalar: Int) = Centis(scalar * cs)
+  def unary_- = Centis(-cs)
+}
+
+object Centis {
+
+  def apply(centis: Long): Centis = Centis {
+    if (centis > Int.MaxValue) {
+      lila.log("common").error(s"Truncating Centis! $centis")
+      Int.MaxValue
+    }
+    else centis.toInt
+  }
+
+  def apply(d: FiniteDuration): Centis = Centis {
+    if (d.unit eq MILLISECONDS) d.length / 10
+    else d.toMillis / 10
+  }
 }
