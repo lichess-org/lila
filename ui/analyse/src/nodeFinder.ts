e3eea384007406f70e2adf2004e9ecdfb37f7d82
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-04-06 01:07:02 +0200

    convert nodeFinder.js

---
+++ ui/analyse/src/nodeFinder.ts
@@ -0,0 +1,35 @@
+import { winningChances } from 'ceval';
+
+function hasCompChild(node: Tree.Node): boolean {
+  return !!node.children.find(function(c) {
+    return !!c.comp;
+  });
+}
+
+export function nextGlyphSymbol(color: Color, symbol: string, mainline: Tree.Node[], fromPly: number): Tree.Node | undefined {
+  const len = mainline.length;
+  if (!len) return;
+  const fromIndex = fromPly - mainline[0].ply;
+  for (var i = 1; i < len; i++) {
+    const node = mainline[(fromIndex + i) % len];
+    const found = (node.ply % 2 === (color === 'white' ? 1 : 0)) && node.glyphs && node.glyphs.find(function(g) {
+      return g.symbol === symbol;
+    });
+    if (found) return node;
+  }
+  return;
+}
+
+export function evalSwings(mainline: Tree.Node[], nodeFilter: (node: Tree.Node) => boolean): Tree.Node[] {
+  const found: Tree.Node[] = [];
+  const threshold = 0.075;
+  for (var i = 1; i < mainline.length; i++) {
+    var node = mainline[i];
+    var prev = mainline[i - 1];
+    if (nodeFilter(node) && node.eval && prev.eval) {
+      var diff = Math.abs(winningChances.povDiff('white', prev.eval, node.eval));
+      if (diff > threshold && hasCompChild(prev)) found.push(node);
+    }
+  }
+  return found;
+}
