1757e27afa47005ae7efec3c3b81db90e0b081bf
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-04-18 12:58:45 +0200

    no game count requirement for daily ultrabullet tournaments

--- modules/tournament/src/main/Schedule.scala
+++ modules/tournament/src/main/Schedule.scala
@@ -207,6 +207,9 @@ object Schedule {
       import Freq._, Speed._
 
       val nbRatedGame = (s.freq, s.speed) match {
+
+        case (_, UltraBullet) => 0
+
         case (Hourly, UltraBullet | HyperBullet | Bullet) => 20
         case (Hourly, SuperBlitz | Blitz) => 15
         case (Hourly, Classical) => 10
