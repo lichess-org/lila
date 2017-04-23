ba981d2e033595c1fcc8fbfbe454048ddc8bc6d7
Isaac Levy <isaac.r.levy@gmail.com>
2017-04-23 16:33:31 -0400

    Simplify durationSeconds calc

--- modules/game/src/main/Game.scala
+++ modules/game/src/main/Game.scala
@@ -105,7 +105,8 @@ case class Game(
   def durationSeconds =
     (updatedAtOrCreatedAt.getSeconds - createdAt.getSeconds).toInt atMost {
       clock.fold(Int.MaxValue) { c =>
-        (((c.elapsedTime(White) + c.elapsedTime(Black) + c.increment * turns) toSeconds) * 1.1).toInt
+        // centis.toSeconds * 1.1 == centis * (1.1 / 100) ~= centis / 91
+        (c.elapsedTime(White) + c.elapsedTime(Black) + c.increment * turns).value / 91
       }
     }
 
