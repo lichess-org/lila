7e11fe2295122206ce48fc335235b451c51f6650
Isaac Levy <isaac.r.levy@gmail.com>
2017-04-27 23:56:40 -0400

    Modify submodule checkout script
    attempt 2

--- bin/submodule-setup.sh
+++ bin/submodule-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -ex
 
-git submodule init
-git submodule foreach --quiet --recursive \
-  "git config --local remote.origin.url | grep -e ornicar -e lichess \
-   && git fetch origin +refs/pull/*/head:refs/remotes/origin/pr/* || :"
-git submodule update --recursive --init
+if ! git submodule update --init --recursive; then
+  git submodule foreach --quiet --recursive \
+     "git config --local remote.origin.url | grep -e ornicar -e lichess \
+      && git fetch origin +refs/pull/*/head:refs/remotes/origin/pr/* || :"
+  git submodule update --recursive --init
+fi
