caa6dae560528ff8b42b7003b481f0d84574e025
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-05-02 14:41:02 +0200

    compress irwin move storage

--- modules/irwin/src/main/BSONHandlers.scala
+++ modules/irwin/src/main/BSONHandlers.scala
@@ -1,12 +1,38 @@
 package lila.irwin
 
 import lila.db.dsl._
+import lila.db.BSON
 import reactivemongo.bson._
 
 object BSONHandlers {
 
   import IrwinReport._
-  private implicit val MoveReportBSONHandler = Macros.handler[MoveReport]
+
+  private implicit val MoveReportBSONHandler = new BSON[MoveReport] {
+
+    private val activation = "a"
+    private val rank = "r"
+    private val ambiguity = "m"
+    private val odds = "o"
+    private val loss = "l"
+
+    def reads(r: BSON.Reader) = MoveReport(
+      activation = r intD activation,
+      rank = r intO rank,
+      ambiguity = r intD ambiguity,
+      odds = r intD odds,
+      loss = r intD loss
+    )
+
+    def writes(w: BSON.Writer, o: MoveReport) = BSONDocument(
+      activation -> w.intO(o.activation),
+      rank -> o.rank.flatMap(w.intO),
+      ambiguity -> w.intO(o.ambiguity),
+      odds -> w.intO(o.odds),
+      loss -> w.intO(o.loss)
+    )
+  }
+
   private implicit val GameReportBSONHandler = Macros.handler[GameReport]
   implicit val ReportBSONHandler = Macros.handler[IrwinReport]
 
