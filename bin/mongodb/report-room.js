1b9f2fca110708046085939ce23caf5d4b875e8b
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-05-11 02:29:33 +0200

    tweak report migration script

--- bin/mongodb/report-room.js
+++ bin/mongodb/report-room.js
@@ -3,7 +3,7 @@ var lastWeek = new Date(Date.now() - 1000 * 60 * 60 * 24 * 7);
 db.report.find().forEach(r => {
 
   var room = 'others';
-  // if (!r.processedBy && r.createdAt < lastWeek) room = 'xfiles';
+  if (!r.processedBy && r.createdAt < lastWeek) room = 'xfiles';
   if (r.reason === 'cheat') room = 'cheat';
   else if (r.reason === 'cheatprint') room = 'print';
   else if (r.reason === 'troll') room = 'coms';
