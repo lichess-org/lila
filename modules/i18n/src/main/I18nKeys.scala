9578b8481274ce23d3b098178387bf1eca3188dd
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-05-28 19:30:22 +0200

    more translatable text

--- modules/i18n/src/main/I18nKeys.scala
+++ modules/i18n/src/main/I18nKeys.scala
@@ -1,4 +1,4 @@
-// Generated with bin/trans-dump.js Sun May 28 2017 18:57:12 GMT+0200 (CEST)
+// Generated with bin/trans-dump.js Sun May 28 2017 19:27:56 GMT+0200 (CEST)
 package lila.i18n
 
 // format: OFF
@@ -505,6 +505,7 @@ val `boardGeometry` = new Translated("boardGeometry")
 val `boardTheme` = new Translated("boardTheme")
 val `boardSize` = new Translated("boardSize")
 val `pieceSet` = new Translated("pieceSet")
+val `embedInYourWebsite` = new Translated("embedInYourWebsite")
 val `nbPlayers` = new Translated("nbPlayers")
 val `nbGames` = new Translated("nbGames")
 val `nbBookmarks` = new Translated("nbBookmarks")
