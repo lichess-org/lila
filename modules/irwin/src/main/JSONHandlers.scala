4f3eaa476818064188b4e36f6d470e21f37db8ed
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-06-04 14:19:46 +0200

    compress irwin move report JSON

--- modules/irwin/src/main/JSONHandlers.scala
+++ modules/irwin/src/main/JSONHandlers.scala
@@ -7,13 +7,21 @@ import play.api.libs.json._
 object JSONHandlers {
 
   import IrwinReport._
-  private implicit val moveReportReader = Json.reads[MoveReport]
-  private implicit val gameReportReader = Json.reads[GameReport]
 
   private implicit val pvReader = Reads[Option[Int]] {
     case JsNumber(n) if n.isValidInt => JsSuccess(n.toInt.some)
     case _ => JsSuccess(None)
   }
+
+  implicit val moveReader: Reads[MoveReport] = (
+    (__ \ "a").read[Int] and // activation
+    (__ \ "r").readNullable[Int] and // rank
+    (__ \ "m").read[Int] and // ambiguity
+    (__ \ "o").read[Int] and // odds
+    (__ \ "l").read[Int] // loss
+  )(MoveReport.apply _)
+
+  private implicit val gameReportReader = Json.reads[GameReport]
 
   implicit val reportReader: Reads[IrwinReport] = (
     (__ \ "userId").read[String] and
