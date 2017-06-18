08fb3922d7212323fd0916a451b4b4d70ae12d1c
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-06-18 14:10:12 +0200

    preload chessground last move highlights

--- app/templating/ChessgroundHelper.scala
+++ app/templating/ChessgroundHelper.scala
@@ -1,27 +1,39 @@
 package lila.app
 package templating
 
-import chess.{ Color, Board }
-import play.twirl.api.Html
+import chess.format.Uci
+import chess.{ Color, Board, Pos }
 import lila.api.Context
+import play.twirl.api.Html
 
 import lila.game.Pov
 
 trait ChessgroundHelper {
 
-  def chessground(board: Board, orient: Color)(implicit ctx: Context): Html = wrap {
+  def chessground(board: Board, orient: Color, lastMove: List[Pos] = Nil)(implicit ctx: Context): Html = wrap {
     if (ctx.pref.is3d) ""
-    else board.pieces.map {
-      case (pos, piece) =>
-        val klass = s"${piece.color.name} ${piece.role.name}"
-        val top = orient.fold(8 - pos.y, pos.y - 1) * 12.5
-        val left = orient.fold(pos.x - 1, 8 - pos.x) * 12.5
-        s"""<piece class="$klass" style="top:$top%;left:$left%"></piece>"""
-    } mkString ""
+    else {
+      def top(p: Pos) = orient.fold(8 - p.y, p.y - 1) * 12.5
+      def left(p: Pos) = orient.fold(p.x - 1, 8 - p.x) * 12.5
+      val highlights = lastMove.distinct.map { pos =>
+        s"""<square class="last-move" style="top:${top(pos)}%;left:${left(pos)}%"></square>"""
+      } mkString ""
+      val pieces = board.pieces.map {
+        case (pos, piece) =>
+          val klass = s"${piece.color.name} ${piece.role.name}"
+          s"""<piece class="$klass" style="top:${top(pos)}%;left:${left(pos)}%"></piece>"""
+      } mkString ""
+      s"$highlights$pieces"
+    }
   }
 
-  def chessground(pov: Pov)(implicit ctx: Context): Html =
-    chessground(pov.game.toChess.board, pov.color)
+  def chessground(pov: Pov)(implicit ctx: Context): Html = chessground(
+    board = pov.game.toChess.board,
+    orient = pov.color,
+    lastMove = pov.game.castleLastMoveTime.lastMove.?? {
+      case (orig, dest) => List(orig, dest)
+    }
+  )
 
   private def wrap(content: String) = Html {
     s"""<div class="cg-board-wrap"><div class="cg-board">$content</div></div>"""
