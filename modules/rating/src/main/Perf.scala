5d608e72bdc5689186598e43fa9a8a862f1fc84e
Daniel Dugovic <dandydand@gmail.com>
2017-07-02 08:33:26 -0500

    Implement Glicko-2 rating deviation aging during calculation

--- modules/rating/src/main/Perf.scala
+++ modules/rating/src/main/Perf.scala
@@ -21,7 +21,7 @@ case class Perf(
   }
 
   def add(g: Glicko, date: DateTime): Perf = copy(
-    glicko = g.cap,
+    glicko = g.age(latest, date).cap,
     nb = nb + 1,
     recent = updateRecentWith(g),
     latest = date.some
