7bb74da2d9f9d63a37394bc743b9ceead52c4e65
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-07-04 01:12:55 +0200

    user ping UI

--- app/templating/Environment.scala
+++ app/templating/Environment.scala
@@ -82,4 +82,17 @@ object Environment
 
   def NotForKids[Html](f: => Html)(implicit ctx: lila.api.Context) =
     if (ctx.kid) Html("") else f
+
+  def signalBars(v: Int) = Html {
+    val bars = (1 to 4).map { b =>
+      s"""<i${if (v < b) " class=\"off\"" else ""}></i>"""
+    } mkString ""
+    val title = v match {
+      case 1 => "Poor connection"
+      case 2 => "Decent connection"
+      case 3 => "Good connection"
+      case _ => "Excellent connection"
+    }
+    s"""<signal data-hint="$title" class="q$v hint--top">$bars</signal>"""
+  }
 }
