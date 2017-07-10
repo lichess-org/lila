03b3f9d67b9f0cd77f0d9fc1767afc7405c0f90a
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-07-10 13:51:04 +0200

    fix study chapter creation

--- ui/analyse/src/study/chapterNewForm.ts
+++ ui/analyse/src/study/chapterNewForm.ts
@@ -151,15 +151,17 @@ export function view(ctrl): VNode {
         })
       ]),
       h('form.chapter_form.material.form', {
-        hook: bindSubmit(e => ctrl.submit({
+        hook: bindSubmit(e => {
+          ctrl.submit({
             name: fieldValue(e, 'name'),
             game: fieldValue(e, 'game'),
             variant: fieldValue(e, 'variant'),
-            fen: fieldValue(e, 'fen') || (activeTab === 'edit' ? ctrl.vm.editorFen() : null),
+            fen: fieldValue(e, 'fen') || (ctrl.vm.tab() === 'edit' ? ctrl.vm.editorFen() : null),
             pgn: fieldValue(e, 'pgn'),
             orientation: fieldValue(e, 'orientation'),
             mode: fieldValue(e, 'mode')
-          }), ctrl.redraw)
+          });
+        }, ctrl.redraw)
       }, [
         h('div.form-group', [
           h('input#chapter-name', {
