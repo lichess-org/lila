4406b069a97312b66a3f10c89884f624a72cfccb
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-07-16 09:48:52 +0200

    translate combat

--- ui/learn/src/stage/combat.js
+++ ui/learn/src/stage/combat.js
@@ -7,37 +7,37 @@ var imgUrl = util.assetUrl + 'images/learn/battle-gear.svg';
 module.exports = {
   key: 'combat',
   title: 'Combat',
-  subtitle: 'Capture and defend pieces',
+  subtitle: 'captureAndDefendPieces',
   image: imgUrl,
-  intro: "A good warrior knows both attack and defense!",
+  intro: 'combatIntro',
   illustration: util.roundSvg(imgUrl),
   levels: [{ // rook
-    goal: 'Take the black pieces!<br>And don\'t lose yours.',
+    goal: 'takeTheBlackPiecesAndDontLoseYours',
     fen: '8/8/8/8/P2r4/6B1/8/8 w - -',
     nbMoves: 3,
     captures: 1,
     shapes: [arrow('a4a5'), arrow('g3f2'), arrow('f2d4'), arrow('d4a4', 'yellow')],
     success: assert.extinct('black')
   }, {
-    goal: 'Take the black pieces!<br>And don\'t lose yours.',
+    goal: 'takeTheBlackPiecesAndDontLoseYours',
     fen: '2r5/8/3b4/2P5/8/1P6/2B5/8 w - -',
     nbMoves: 4,
     captures: 2,
     success: assert.extinct('black')
   }, {
-    goal: 'Take the black pieces!<br>And don\'t lose yours.',
+    goal: 'takeTheBlackPiecesAndDontLoseYours',
     fen: '1r6/8/5n2/3P4/4P1P1/1Q6/8/8 w - -',
     nbMoves: 4,
     captures: 2,
     success: assert.extinct('black')
   }, {
-    goal: 'Take the black pieces!<br>And don\'t lose yours.',
+    goal: 'takeTheBlackPiecesAndDontLoseYours',
     fen: '2r5/8/3N4/5b2/8/8/PPP5/8 w - -',
     nbMoves: 4,
     captures: 2,
     success: assert.extinct('black')
   }, {
-    goal: 'Take the black pieces!<br>And don\'t lose yours.',
+    goal: 'takeTheBlackPiecesAndDontLoseYours',
     fen: '8/6q1/8/4P1P1/8/4B3/r2P2N1/8 w - -',
     nbMoves: 8,
     captures: 2,
@@ -46,5 +46,5 @@ module.exports = {
     l.pointsForCapture = true;
     return util.toLevel(l, i);
   }),
-  complete: 'Congratulations! You know how to fight with chess pieces!'
+  complete: 'combatComplete'
 };
