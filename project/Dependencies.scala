83a61436f0cc3f09f4b1d618d8b21fc6647a56e3
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-08-01 02:05:46 +0200

    upgrade cache libraries

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
   val semver = "com.gilt" %% "gfc-semver" % "0.0.5"
   val scrimage = "com.sksamuel.scrimage" %% "scrimage-core" % "2.1.8"
   val configs = "com.github.kxbmap" %% "configs" % "0.4.4"
-  val scaffeine = "com.github.blemale" %% "scaffeine" % "2.0.0" % "compile"
+  val scaffeine = "com.github.blemale" %% "scaffeine" % "2.2.0" % "compile"
   val netty = "io.netty" % "netty" % "3.10.6.Final"
   val guava = "com.google.guava" % "guava" % "21.0"
   val specs2 = "org.specs2" %% "specs2-core" % "3.9.0" % "test"
