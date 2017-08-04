0df7a7b33c902d6c701ff8a4f9dfbe1ae5559fe5
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-08-04 11:23:30 +0200

    improve date localized formatting with support for safari 9

--- ui/site/src/timeago.js
+++ ui/site/src/timeago.js
@@ -1,8 +1,8 @@
 /**
-* based on https://github.com/hustcc/timeago.js
-* Copyright (c) 2016 hustcc
-* License: MIT
-**/
+ * based on https://github.com/hustcc/timeago.js
+ * Copyright (c) 2016 hustcc
+ * License: MIT
+ **/
 lichess.timeago = (function() {
 
   // second, minute, hour, day, week, month, year(365 days)
@@ -42,36 +42,43 @@ lichess.timeago = (function() {
     return (Date.now() - toDate(date)) / 1000;
   }
 
-  /**
-  * timeago: the function to get `timeago` instance.
-  *
-  * How to use it?
-  * var timeagoFactory = require('timeago.js');
-  * var timeago = timeagoFactory(); // all use default.
-  * var timeago = timeagoFactory('2016-09-10'); // the relative date is 2016-09-10, so the 2016-09-11 will be 1 day ago.
-  **/
-  return {
-    render: function(nodes) {
-      if (nodes.length === undefined) nodes = [nodes];
-      for (var i = 0, len = nodes.length; i < len; i++) {
-        var node = nodes[i],
-          abs = node.classList.contains('abs'),
-          set = node.classList.contains('set');
-        if (set && abs) continue;
-        var date = toDate(node.getAttribute('datetime'));
-        if (!set) {
-          var str = date.toLocaleString();
-          if (abs) node.innerHTML = str;
-          else node.setAttribute('title', str);
-          node.classList.add('set');
+  var formatter = (window.Intl && Intl.DateTimeFormat) ?
+    new Intl.DateTimeFormat(document.documentElement.lang, {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric'
+    }).format : function(d) { return d.toLocaleString(); }
+
+    /**
+    * timeago: the function to get `timeago` instance.
+    *
+    * How to use it?
+    * var timeagoFactory = require('timeago.js');
+    * var timeago = timeagoFactory(); // all use default.
+    * var timeago = timeagoFactory('2016-09-10'); // the relative date is 2016-09-10, so the 2016-09-11 will be 1 day ago.
+    **/
+    return {
+      render: function(nodes) {
+        if (nodes.length === undefined) nodes = [nodes];
+        for (var i = 0, len = nodes.length; i < len; i++) {
+          var node = nodes[i],
+            abs = node.classList.contains('abs'),
+            set = node.classList.contains('set');
+          if (set && abs) continue;
+          var date = toDate(node.getAttribute('datetime'));
+          if (!set) {
+            var str = formatter(date);
+            if (abs) node.innerHTML = str;
+            else node.setAttribute('title', str);
+            node.classList.add('set');
+          }
+          if (!abs) node.innerHTML = formatDiff(diffSec(date));
         }
-        if (!abs) {
-          node.innerHTML = formatDiff(diffSec(date));
-        }
+      },
+      format: function(date) {
+        return formatDiff(diffSec(date));
       }
-    },
-    format: function(date) {
-      return formatDiff(diffSec(date));
-    }
-  };
+    };
 })();
