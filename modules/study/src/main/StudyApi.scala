2659c52908c1a32b5cc71ab2aac7c30e02619631
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-08-15 11:14:12 -0500

    remove debug

--- modules/study/src/main/StudyApi.scala
+++ modules/study/src/main/StudyApi.scala
@@ -397,7 +397,7 @@ final class StudyApi(
 
   def setGamebook(userId: User.ID, studyId: Study.Id, position: Position.Ref, gamebook: Gamebook, uid: Uid) = sequenceStudyWithChapter(studyId, position.chapterId) {
     case Study.WithChapter(study, chapter) => Contribute(userId, study) {
-      chapter.setGamebook(gamebook.pp, position.path).pp match {
+      chapter.setGamebook(gamebook, position.path) match {
         case Some(newChapter) =>
           studyRepo.updateNow(study)
           chapterRepo.setGamebook(newChapter, position.path, gamebook) >>- {
