e17e616d2cf630d9c55f497fa669f7db9e950370
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-08-18 14:22:33 -0500

    toggle clock class

--- ui/round/src/clock/clockView.ts
+++ ui/round/src/clock/clockView.ts
@@ -85,10 +85,7 @@ function showBar(ctrl: ClockController, els: ClockElements, millis: Millis, bers
 export function updateElements(clock: ClockController, els: ClockElements, millis: Millis) {
   if (els.time) els.time.innerHTML = formatClockTime(clock.showTenths, millis, true);
   if (els.bar) els.bar.style.width = clock.timePercent(millis) + '%';
-  if (els.clock) {
-    if (millis < clock.emergMs) els.clock.classList.add("emerg");
-    else els.clock.classList.remove("emerg");
-  }
+  if (els.clock) els.clock.classList.toggle("emerg", millis < clock.emergMs);
 }
 
 function showBerserk(ctrl: RoundController, color: Color): boolean {
