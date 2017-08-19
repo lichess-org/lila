9039133a510629260413160200c3e283eae4479b
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-08-18 23:06:50 -0500

    JSON encoding as HTML with type safety

--- app/templating/JsonHelper.scala
+++ app/templating/JsonHelper.scala
@@ -8,23 +8,21 @@ import lila.api.Context
 
 trait JsonHelper {
 
-  def toJson[A: Writes](map: Map[Int, A]): String = Json stringify {
-    Json toJson {
-      map mapKeys (_.toString)
-    }
+  def toJson[A: Writes](map: Map[Int, A]): Html = toJson {
+    map mapKeys (_.toString)
   }
 
-  def toJson[A: Writes](a: A): String = Json stringify {
-    Json toJson a
+  def toJson[A: Writes](a: A): Html = Html {
+    Json stringify {
+      Json toJson a
+    }
   }
 
   def J = Json
 
   def htmlOrNull[A, B](a: Option[A])(f: A => Html) = a.fold(Html("null"))(f)
 
-  def jsOrNull[A: Writes](a: Option[A]) = Html {
-    a.fold("null")(x => toJson(x))
-  }
+  def jsOrNull[A: Writes](a: Option[A]) = a.fold(Html("null"))(x => toJson(x))
 
   def jsUserId(implicit ctx: Context) = Html {
     ctx.userId.fold("null")(id => s""""$id"""")
