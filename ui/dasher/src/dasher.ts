12d2875afe326c05a147a1db063b7a5f5ab9e262
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-08-22 07:28:57 -0500

    extract zen CSS

--- ui/dasher/src/dasher.ts
+++ ui/dasher/src/dasher.ts
@@ -77,7 +77,8 @@ export function makeCtrl(opts: DasherOpts, data: DasherData, redraw: Redraw): Da
 
   function toggleZen() {
     data.zen = data.zen ? 0 : 1;
-    $('body').toggleClass('zen', data.zen)
+    if (data.zen) window.lichess.loadCss('/assets/stylesheets/zen.css');
+    $('body').toggleClass('zen', data.zen);
     $.post('/pref/zen', { zen: data.zen });
     redraw();
   }
