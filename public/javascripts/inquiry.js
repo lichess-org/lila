60391d729d70119b4512203541a49daac48618b2
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-09-12 17:06:42 -0500

    temporarily, locally hide the inquiry bar

--- public/javascripts/inquiry.js
+++ public/javascripts/inquiry.js
@@ -2,4 +2,8 @@ $(function() {
   $('#inquiry .notes').on('mouseenter', function() {
     $(this).find('textarea')[0].focus();
   });
+  $('#inquiry .costello').click(function() {
+    $('#inquiry').toggleClass('hidden');
+    $('body').toggleClass('no-inquiry');
+  });
 });
