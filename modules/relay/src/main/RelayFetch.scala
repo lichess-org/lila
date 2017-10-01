a31a4c655ee33c0d6e7ea42fd0c4c977a300ac2e
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-10-01 17:29:59 -0500

    cache relay responses

--- modules/relay/src/main/RelayFetch.scala
+++ modules/relay/src/main/RelayFetch.scala
@@ -2,7 +2,7 @@ package lila.relay
 
 import akka.actor._
 import org.joda.time.DateTime
-import play.api.libs.ws.WS
+import play.api.libs.ws.{ WS, WSResponse }
 import play.api.Play.current
 import scala.concurrent.duration._
 
@@ -35,10 +35,10 @@ private final class RelayFetch(
       throw new RuntimeException(msg)
 
     case Tick =>
-      val startAt = nowMillis
-      getSyncable().map(_ filter RelayFetch.shouldFetchNow).flatMap {
+      val fetcher = new RelayFetch.CachedFetcher
+      getSyncable().map(_ filter fetcher.shouldFetchNow).flatMap {
         _.map { relay =>
-          RelayFetch(relay.sync.upstream) flatMap {
+          fetcher(relay.sync.upstream) flatMap {
             sync(relay, _)
           } flatMap { res =>
             addLog(relay.id, SyncLog.event(none)) inject res
@@ -54,59 +54,69 @@ private final class RelayFetch(
 
 private object RelayFetch {
 
-  def shouldFetchNow(r: Relay) = !r.sync.log.alwaysFails || {
-    r.sync.log.updatedAt ?? { DateTime.now.minusSeconds(30).isAfter }
-  }
+  final class CachedFetcher {
 
-  import Relay.Sync.Upstream
-  import RelaySync.MultiPgn
+    private type Url = String
 
-  import play.api.libs.json._
+    private val cache = scala.collection.mutable.Map[Url, Fu[WSResponse]]()
 
-  private case class RoundJsonPairing(live: Boolean)
-  private case class RoundJson(pairings: List[RoundJsonPairing])
-  private implicit val roundPairingReads = Json.reads[RoundJsonPairing]
-  private implicit val roundReads = Json.reads[RoundJson]
-
-  def apply(upstream: Upstream): Fu[MultiPgn] = upstream match {
-    case Upstream.DgtOneFile(file) => dgtOneFile(file)
-    case Upstream.DgtManyFiles(dir) => dgtManyFiles(dir)
-  }
-
-  private val httpTimeout = 4.seconds
-
-  private def get(url: String) =
-    WS.url(url).withRequestTimeout(httpTimeout.toMillis).get()
-
-  private def dgtOneFile(file: String): Fu[MultiPgn] =
-    get(file).flatMap {
-      case res if res.status == 200 => fuccess(splitPgn(res.body))
-      case res => fufail(s"Cannot fetch $file (error ${res.status})")
+    def shouldFetchNow(r: Relay) = !r.sync.log.alwaysFails || {
+      r.sync.log.updatedAt ?? { DateTime.now.minusSeconds(30).isAfter }
     }
 
-  private def dgtManyFiles(dir: String): Fu[MultiPgn] = {
-    val roundUrl = s"$dir/round.json"
-    get(roundUrl) flatMap {
-      case res if res.status == 200 => roundReads reads res.json match {
-        case JsError(err) => fufail(err.toString)
-        case JsSuccess(round, _) => (1 to round.pairings.size).map { number =>
-          val gameUrl = s"$dir/game-$number.pgn"
-          get(gameUrl).flatMap {
-            case res if res.status == 200 => fuccess(number -> res.body)
-            case res => fufail(s"Cannot fetch $gameUrl (error ${res.status})")
-          }
-        }.sequenceFu map { results =>
-          MultiPgn(results.sortBy(_._1).map(_._2).toList)
-        }
+    import Relay.Sync.Upstream
+    import RelaySync.MultiPgn
+
+    import play.api.libs.json._
+
+    private case class RoundJsonPairing(live: Boolean)
+    private case class RoundJson(pairings: List[RoundJsonPairing])
+    private implicit val roundPairingReads = Json.reads[RoundJsonPairing]
+    private implicit val roundReads = Json.reads[RoundJson]
+
+    def apply(upstream: Upstream): Fu[MultiPgn] = upstream match {
+      case Upstream.DgtOneFile(file) => dgtOneFile(file)
+      case Upstream.DgtManyFiles(dir) => dgtManyFiles(dir)
+    }
+
+    private val httpTimeout = 4.seconds
+
+    private def getCached(url: String): Fu[WSResponse] =
+      cache.getOrElseUpdate(
+        url,
+        WS.url(url).withRequestTimeout(httpTimeout.toMillis).get()
+      )
+
+    private def dgtOneFile(file: String): Fu[MultiPgn] =
+      getCached(file).flatMap {
+        case res if res.status == 200 => fuccess(splitPgn(res.body))
+        case res => fufail(s"Cannot fetch $file (error ${res.status})")
       }
-      case res => fufail(s"Cannot fetch $roundUrl (error ${res.status})")
-    }
-  }
 
-  private def splitPgn(str: String) = MultiPgn {
-    """\n\n\[""".r.split(str.replace("\r\n", "\n")).toList match {
-      case first :: rest => first :: rest.map(t => s"[$t")
-      case Nil => Nil
+    private def dgtManyFiles(dir: String): Fu[MultiPgn] = {
+      val roundUrl = s"$dir/round.json"
+      getCached(roundUrl) flatMap {
+        case res if res.status == 200 => roundReads reads res.json match {
+          case JsError(err) => fufail(err.toString)
+          case JsSuccess(round, _) => (1 to round.pairings.size).map { number =>
+            val gameUrl = s"$dir/game-$number.pgn"
+            getCached(gameUrl).flatMap {
+              case res if res.status == 200 => fuccess(number -> res.body)
+              case res => fufail(s"Cannot fetch $gameUrl (error ${res.status})")
+            }
+          }.sequenceFu map { results =>
+            MultiPgn(results.sortBy(_._1).map(_._2).toList)
+          }
+        }
+        case res => fufail(s"Cannot fetch $roundUrl (error ${res.status})")
+      }
+    }
+
+    private def splitPgn(str: String) = MultiPgn {
+      """\n\n\[""".r.split(str.replace("\r\n", "\n")).toList match {
+        case first :: rest => first :: rest.map(t => s"[$t")
+        case Nil => Nil
+      }
     }
   }
 }
