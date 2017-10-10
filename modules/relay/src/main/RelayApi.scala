ad6e31bbb66d24cf26a3b8aeed298f5934696359
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-10-10 10:51:15 -0500

    delete relay along study

--- modules/relay/src/main/RelayApi.scala
+++ modules/relay/src/main/RelayApi.scala
@@ -103,6 +103,9 @@ final class RelayApi(
       sendToContributors(id, "relayLog", JsonView.syncLogEventWrites writes event) >>-
       event.error.foreach { err => logger.info(s"$id $err") }
 
+  private[relay] def onStudyRemove(studyId: Study.Id) =
+    coll.remove($id(studyId)).void
+
   private[relay] def publishRelay(relay: Relay): Funit =
     sendToContributors(relay.id, "relayData", JsonView.relayWrites writes relay)
 
