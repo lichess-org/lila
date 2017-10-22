beb8079d79e7b1a6ae56c4a7841c532ec0816b16
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-10-21 19:42:16 -0500

    user autocomplete in cli input

--- ui/site/src/cli.js
+++ ui/site/src/cli.js
@@ -3,21 +3,27 @@ if (!$wrap.length) return;
 
 function toggle() {
   $wrap.toggleClass('shown');
-  if (!$wrap.hasClass('init')) $wrap
-    .addClass('init')
-    .find('input')
-    .on('blur', toggle)
-    .on('keypress', function(e) {
-      if (e.which == 10 || e.which == 13) {
-        execute(e.target.value);
-        e.target.value = '';
-      }
-    });
-  $wrap.find('input').focus()
+  var $input = $wrap.find('input');
+  if (!$wrap.hasClass('init')) {
+    $wrap.addClass('init');
+    $input
+      .on('blur', toggle)
+      .on('keypress', function(e) {
+        if (e.which == 10 || e.which == 13) {
+          execute(e.target.value);
+          e.target.value = '';
+        }
+      });
+      lichess.userAutocomplete($input, {
+        focus: 1,
+        friend: true
+      });
+  } else $input.focus();
 }
 
 function execute(cmd) {
-  console.log(cmd);
+  if (cmd[0] === '/') console.log(cmd);
+  else location.href = '/@/' + cmd;
 }
 
 Mousetrap.bind('s', function() {
