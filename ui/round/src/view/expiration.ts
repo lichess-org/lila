f203b0df37dfd856bd081190297005610e641ce5
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-10-22 18:05:14 -0500

    game expiration sound

--- ui/round/src/view/expiration.ts
+++ ui/round/src/view/expiration.ts
@@ -3,12 +3,18 @@ import { VNode } from 'snabbdom/vnode'
 import RoundController from '../ctrl';
 import { game } from 'game';
 
+let rang = false;
+
 export default function(ctrl: RoundController): [VNode, boolean] | undefined {
   const d = ctrl.data.expiration;
   if (!d) return;
   const timeLeft = Math.max(0, d.movedAt - Date.now() + d.millisToMove),
   myTurn = game.isPlayerTurn(ctrl.data),
   emerg = myTurn && timeLeft < 8000;
+  if (!rang && emerg) {
+    window.lichess.sound.lowtime();
+    rang = true;
+  }
   return [
     h('div.expiration.suggestion', {
       class: { emerg }
