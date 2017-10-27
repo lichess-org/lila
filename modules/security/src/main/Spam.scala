a582400e7b801b93a96345faf150b07036c74f71
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-10-27 09:05:46 -0500

    fix detection of chess24 induced spam - fuck your referral programs
    > Spam around and make money from your friends!

--- modules/security/src/main/Spam.scala
+++ modules/security/src/main/Spam.scala
@@ -7,33 +7,35 @@ object Spam {
     fullBlacklist exists text.contains
   }
 
-  val cb = "tob-ssehc".reverse
+  private[security] lazy val cb = "tob-ssehc".reverse
 
-  private val referBlacklist = List(
+  private def referBlacklist = List(
     /* While links to other chess websites are welcome,
      * refer links grant the referrer money,
      * effectively inducing spam */
     "velocitychess.com/ref/",
     "chess24.com?ref=",
-    "chess.com/register?refId=",
-    /* links to cheats */
+    "chess.com/register?refId="
+  )
+
+  private def tosBlacklist = List(
     cb
   )
 
-  private val fullBlacklist = referBlacklist
+  private lazy val fullBlacklist = referBlacklist ::: tosBlacklist
 
   def replace(text: String) = replacements.foldLeft(text) {
     case (t, (regex, rep)) => regex.replaceAllIn(t, rep)
   }
 
-  val tosUrl = "lichess.org/terms-of-service"
+  private[security] val tosUrl = "lichess.org/terms-of-service"
 
-  val protocol = """(https?://)?"""
+  private val protocol = """(https?://)?"""
 
   private val replacements = List(
-    s"""${protocol}velocitychess.com/ref/\\w+""" -> "velocitychess.com",
-    s"""${protocol}chess24.com?ref=\\w+""" -> "chess24.com",
-    s"""${protocol}chess.com/register?refId=\\w+""" -> "chess.com",
+    s"""velocitychess.com/ref/\\w+""" -> "velocitychess.com",
+    s"""chess24.com\\?ref=\\w+""" -> "chess24.com",
+    s"""chess.com/register\\?refId=\\w+""" -> "chess.com",
     s"""${protocol}${cb}(\\.com)?[^\\s]*""" -> tosUrl
   ).map {
       case (regex, replacement) => regex.r -> replacement
