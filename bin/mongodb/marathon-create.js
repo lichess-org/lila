f26254e527c03203a2de0ee4585bdd16f4a14f1f
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-10-29 15:39:56 -0500

    fix marathon script

--- bin/mongodb/marathon-create.js
+++ bin/mongodb/marathon-create.js
@@ -12,6 +12,10 @@ t.clock = {
 };
 t.schedule.speed = 'blitz';
 t.startsAt = ISODate('2017-12-28');
+t.spotlight.description = 
+  "Let's make this the biggest chess tournament in history. " + 
+  "24h of " + (t.clock.limit / 60) + "+" + t.increment + " " + t.schedule.speed + " chess: " +
+  "top 100 players get a unique trophy!";
 
 // initialize values
 t.status = NumberInt(10);
