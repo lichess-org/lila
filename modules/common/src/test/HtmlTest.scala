2198220a774a82f474106ba670d89fc520f83a8f
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-11-01 09:48:46 -0500

    fix tests

--- modules/common/src/test/HtmlTest.scala
+++ modules/common/src/test/HtmlTest.scala
@@ -10,13 +10,13 @@ class HtmlTest extends Specification {
   "add links" should {
     "detect link" in {
       val url = "http://zombo.com"
-      addLinks(s"""link to $url here""") must_== Html {
+      addLinks(Html(s"""link to $url here""")) must_== Html {
         s"""link to <a rel="nofollow" href="$url" target="_blank">$url</a> here"""
       }
     }
     "detect image" in {
       val url = "http://zombo.com/pic.jpg"
-      addLinks(s"""img to $url here""") must_== Html {
+      addLinks(Html(s"""img to $url here""")) must_== Html {
         val img = s"""<img class="embed" src="$url"/>"""
         s"""img to <a rel="nofollow" href="$url" target="_blank">$img</a> here"""
       }
@@ -24,20 +24,20 @@ class HtmlTest extends Specification {
     "detect imgur image URL" in {
       val url = "https://imgur.com/NXy19Im"
       val picUrl = "https://i.imgur.com/NXy19Im.jpg"
-      addLinks(s"""img to $url here""") must_== Html {
+      addLinks(Html(s"""img to $url here""")) must_== Html {
         val img = s"""<img class="embed" src="$picUrl"/>"""
         s"""img to <a rel="nofollow" href="$url" target="_blank">$img</a> here"""
       }
     }
     "ignore imgur gallery URL" in {
       val url = "https://imgur.com/gallery/pMtTE"
-      addLinks(s"""link to $url here""") must_== Html {
+      addLinks(Html(s"""link to $url here""")) must_== Html {
         s"""link to <a rel="nofollow" href="$url" target="_blank">$url</a> here"""
       }
     }
     // "skip markdown images" in {
     //   val url = "http://zombo.com"
-    //   addLinks(s"""img of ![some alt]($url) here""") must_== Html {
+    //   addLinks(Html(s"""img of ![some alt]($url) here""")) must_== Html {
     //     s"""img of ![some alt]($url) here"""
     //   }
     // }
