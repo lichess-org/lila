3c42b7494b4588161919b0c54b9a73ca1479e4f3
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-11-01 22:40:49 -0500

    tweak autoreport link

--- modules/evaluation/src/main/PlayerAggregateAssessment.scala
+++ modules/evaluation/src/main/PlayerAggregateAssessment.scala
@@ -123,7 +123,7 @@ case class PlayerAggregateAssessment(
 
   def reportText(reason: String, maxGames: Int = 10): String = {
     val gameLinks: String = (playerAssessments.sortBy(-_.assessment.id).take(maxGames).map { a =>
-      a.assessment.emoticon + " //lichess.org/" + a.gameId + "/" + a.color.name
+      a.assessment.emoticon + " lichess.org/" + a.gameId + "/" + a.color.name
     }).mkString("\n")
 
     s"""[AUTOREPORT] $reason
