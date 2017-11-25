3134d81c2ca51c4f6b7d20115862b2a85d3d6c40
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-11-25 07:10:40 -0500

    store game ceval up to ply 12

--- ui/analyse/src/ctrl.ts
+++ ui/analyse/src/ctrl.ts
@@ -755,7 +755,7 @@ export default class AnalyseCtrl {
       canPut: (node: Tree.Node) => {
         return this.data.evalPut && this.canEvalGet(node) && (
           // if not in study, only put decent opening moves
-          this.opts.study || (node.ply < 10 && !node.ceval!.mate && Math.abs(node.ceval!.cp!) < 99)
+          this.opts.study || (node.ply < 12 && !node.ceval!.mate && Math.abs(node.ceval!.cp!) < 99)
         );
       },
       getNode: () => this.node,
