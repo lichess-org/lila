42acbaff3f2f6b1fa1cca9221e6e13687184eda7
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-11-28 18:29:06 -0500

    allow more shields than we have perfs

--- modules/tournament/src/main/TournamentScheduler.scala
+++ modules/tournament/src/main/TournamentScheduler.scala
@@ -158,10 +158,11 @@ Thank you all, you rock!"""
 
             List( // shield tournaments!
               month.firstWeek.withDayOfWeek(MONDAY) -> Bullet,
-              month.firstWeek.withDayOfWeek(TUESDAY) -> Blitz,
-              month.firstWeek.withDayOfWeek(WEDNESDAY) -> Classical,
-              month.firstWeek.withDayOfWeek(THURSDAY) -> HyperBullet,
-              month.firstWeek.withDayOfWeek(FRIDAY) -> UltraBullet
+              month.firstWeek.withDayOfWeek(TUESDAY) -> SuperBlitz,
+              month.firstWeek.withDayOfWeek(WEDNESDAY) -> Blitz,
+              month.firstWeek.withDayOfWeek(THURSDAY) -> Classical,
+              month.firstWeek.withDayOfWeek(FRIDAY) -> HyperBullet,
+              month.firstWeek.withDayOfWeek(SATURDAY) -> UltraBullet
             ).flatMap {
                 case (day, speed) => at(day, 16) map { date =>
                   Schedule(Shield, speed, Standard, std, date) plan {
--- modules/tournament/src/main/TournamentShield.scala
+++ modules/tournament/src/main/TournamentShield.scala
@@ -19,8 +19,8 @@ final class TournamentShieldApi(
     _.filter(_.userId == u.id)
   }
 
-  def apply(pt: PerfType): Fu[Option[Owner]] = cache.get map {
-    _.find(_.perfType == pt)
+  def apply(c: Category): Fu[Option[Owner]] = cache.get map {
+    _.find(_.categ == c)
   }
 
   def apply: Fu[List[Owner]] = cache.get
@@ -39,24 +39,120 @@ final class TournamentShieldApi(
       "status" -> statusBSONHandler.write(Status.Finished),
       "startsAt" $gt DateTime.now.minusMonths(1).minusDays(1)
     )).sort($sort desc "startsAt").list[Tournament]() map { tours =>
-      PerfType.leaderboardable.flatMap { perfType =>
-        tours.flatMap { tour =>
-          for {
-            tourPerfType <- tour.perfType
-            if tourPerfType == perfType
-            winner <- tour.winnerId
-          } yield Owner(
-            perfType = perfType,
-            userId = winner,
-            since = tour.finishesAt,
-            tourId = tour.id
-          )
-        }.headOption
+      Category.all.flatMap { categ =>
+        for {
+          tour <- tours find categ.matches
+          winner <- tour.winnerId
+        } yield Owner(
+          categ = categ,
+          userId = winner,
+          since = tour.finishesAt,
+          tourId = tour.id
+        )
       }
     }
 }
 
 object TournamentShield {
+
+  case class Owner(
+      categ: Category,
+      userId: User.ID,
+      since: DateTime,
+      tourId: Tournament.ID
+  )
+
+  sealed abstract class Category(
+      val of: Either[Schedule.Speed, chess.variant.Variant],
+      val iconChar: Char
+  ) {
+    def key = of.fold(_.name, _.key)
+    def name = of.fold(_.toString, _.name)
+    def matches(tour: Tournament) = of.fold(
+      speed => tour.schedule.exists(_.speed == speed),
+      variant => tour.variant == variant
+    )
+  }
+
+  object Category {
+
+    case object UltraBullet extends Category(
+      of = Left(Schedule.Speed.UltraBullet),
+      iconChar = '{'
+    )
+
+    case object HyperBullet extends Category(
+      of = Left(Schedule.Speed.HyperBullet),
+      iconChar = 'T'
+    )
+
+    case object Bullet extends Category(
+      of = Left(Schedule.Speed.Bullet),
+      iconChar = 'T'
+    )
+
+    case object SuperBlitz extends Category(
+      of = Left(Schedule.Speed.SuperBlitz),
+      iconChar = ')'
+    )
+
+    case object Blitz extends Category(
+      of = Left(Schedule.Speed.Blitz),
+      iconChar = ')'
+    )
+
+    case object Classical extends Category(
+      of = Left(Schedule.Speed.Classical),
+      iconChar = '+'
+    )
+
+    case object Correspondence extends Category(
+      of = Left(Schedule.Speed.Classical),
+      iconChar = ';'
+    )
+
+    case object Chess960 extends Category(
+      of = Right(chess.variant.Chess960),
+      iconChar = '''
+    )
+
+    case object KingOfTheHill extends Category(
+      of = Right(chess.variant.KingOfTheHill),
+      iconChar = '('
+    )
+
+    case object Antichess extends Category(
+      of = Right(chess.variant.Antichess),
+      iconChar = '@'
+    )
+
+    case object Atomic extends Category(
+      of = Right(chess.variant.Atomic),
+      iconChar = '>'
+    )
+
+    case object ThreeCheck extends Category(
+      of = Right(chess.variant.ThreeCheck),
+      iconChar = '.'
+    )
+
+    case object Horde extends Category(
+      of = Right(chess.variant.Horde),
+      iconChar = '_'
+    )
+
+    case object RacingKings extends Category(
+      of = Right(chess.variant.RacingKings),
+      iconChar = ''
+    )
+
+    case object Crazyhouse extends Category(
+      of = Right(chess.variant.Crazyhouse),
+      iconChar = ''
+    )
+
+    val all: List[Category] = List(UltraBullet, HyperBullet, Bullet, SuperBlitz, Blitz, Classical, Crazyhouse, Chess960, KingOfTheHill, ThreeCheck, Antichess, Atomic, Horde, RacingKings)
+  }
 
   def spotlight(name: String) = Spotlight(
     iconFont = "5".some,
@@ -65,12 +161,5 @@ object TournamentShield {
 The winner keeps it for one month,
 then must defend it during the next $name Shield tournament!""",
     homepageHours = 6.some
-  )
-
-  case class Owner(
-      perfType: PerfType,
-      userId: User.ID,
-      since: DateTime,
-      tourId: Tournament.ID
   )
 }
