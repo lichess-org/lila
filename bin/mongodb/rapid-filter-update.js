b02c6c67ff31a508d6d7ea9ee48c93ba79cd1d2d
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-11-29 23:35:32 -0500

    rapid filter update script

---
+++ bin/mongodb/rapid-filter-update.js
@@ -0,0 +1,4 @@
+var speedId = 5;
+[db.config, db.config_anon].forEach(coll => {
+  coll.update({ 'filter.s.2': { $exists: true } }, { $push: { 'filter.s': NumberInt(speedId) } }, { multi: true });
+});
