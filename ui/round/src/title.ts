cd547df5a883508511e5d34d3a75015657980650
Isaac Levy <isaac.r.levy@gmail.com>
2017-11-29 20:42:12 -0500

    Improve favicon behavior during game
    Use white/grey during player turn, flip icon for
    opponent turn.

--- ui/round/src/title.ts
+++ ui/round/src/title.ts
@@ -3,35 +3,37 @@ import RoundController from './ctrl';
 
 const initialTitle = document.title;
 
-var curFaviconIdx = 0;
-const F = [
-  '/assets/images/favicon-32-white.png',
-  '/assets/images/favicon-32-black.png'
-].map(function(path, i) {
-  return function() {
-    if (curFaviconIdx !== i) {
-      const favicon = document.getElementById('favicon') as HTMLAnchorElement;
-      favicon.href = path;
-      curFaviconIdx = i;
+let curFavicon: string;
+const faviconElem = document.getElementById('favicon') as HTMLAnchorElement;
+class Favicon {
+  constructor(readonly path: string) {}
+
+  isSet = () => curFavicon === this.path;
+
+  set() {
+    if (curFavicon !== this.path) {
+        faviconElem.href = this.path;
+        curFavicon = this.path;
     }
-  };
-});
+  }
+}
+
+const favWhite = new Favicon('/assets/images/favicon-32-white.png'),
+      favBlack = new Favicon('/assets/images/favicon-32-black.png'),
+      favBlackFlip = new Favicon('/assets/images/favicon-32-black-flip.png'),
+      favGrey = new Favicon('/assets/images/favicon-32-grey.png');
 
 let tickerTimer: number | undefined;
 function resetTicker() {
-  if (tickerTimer) clearTimeout(tickerTimer);
-  tickerTimer = undefined;
-  F[0]();
+  if (tickerTimer !== undefined) {
+    clearTimeout(tickerTimer);
+    tickerTimer = undefined;
+  }
 }
 
-function startTicker() {
-  function tick() {
-    if (!document.hasFocus()) {
-      F[1 - curFaviconIdx]();
-      tickerTimer = setTimeout(tick, 1000);
-    }
-  }
-  if (!tickerTimer) tickerTimer = setTimeout(tick, 200);
+function ticker() {
+  (favGrey.isSet() ? favWhite : favGrey).set();
+  tickerTimer = setTimeout(ticker, 1000);
 }
 
 export function init() {
@@ -43,12 +45,17 @@ export function set(ctrl: RoundController, text?: string) {
   if (!text) {
     if (status.finished(ctrl.data)) {
       text = ctrl.trans('gameOver');
+      resetTicker();
+      favBlack.set();
     } else if (game.isPlayerTurn(ctrl.data)) {
       text = ctrl.trans('yourTurn');
-      if (!document.hasFocus()) startTicker();
+      favWhite.set();
+      if (!document.hasFocus() && !tickerTimer)
+        tickerTimer = setTimeout(ticker, 500);
     } else {
       text = ctrl.trans('waitingForOpponent');
       resetTicker();
+      favBlackFlip.set();
     }
   }
   document.title = text + " - " + initialTitle;
