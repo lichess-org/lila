2003d647926bf1c7a093481a511d669ee0830e5b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-12-30 23:44:02 +0100

    update autoLink tests

--- modules/common/src/test/HtmlTest.scala
+++ modules/common/src/test/HtmlTest.scala
@@ -7,22 +7,22 @@ class HtmlTest extends Specification {
 
   import String.html._
 
-  "add links" should {
+  "rich text" should {
     "detect link" in {
       val url = "http://zombo.com"
-      addLinks(Html(s"""link to $url here""")) must_== Html {
+      richText(s"""link to $url here""") must_== Html {
         s"""link to <a rel="nofollow" href="$url" target="_blank">$url</a> here"""
       }
     }
     "skip buggy url like http://foo-@bar" in {
       val url = "http://foo-@bar"
-      addLinks(Html(s"""link to $url here""")) must_== Html {
+      richText(s"""link to $url here""") must_== Html {
         s"""link to http://foo-@bar here"""
       }
     }
     "detect image" in {
       val url = "http://zombo.com/pic.jpg"
-      addLinks(Html(s"""img to $url here""")) must_== Html {
+      richText(s"""img to $url here""") must_== Html {
         val img = s"""<img class="embed" src="$url"/>"""
         s"""img to <a rel="nofollow" href="$url" target="_blank">$img</a> here"""
       }
@@ -30,23 +30,17 @@ class HtmlTest extends Specification {
     "detect imgur image URL" in {
       val url = "https://imgur.com/NXy19Im"
       val picUrl = "https://i.imgur.com/NXy19Im.jpg"
-      addLinks(Html(s"""img to $url here""")) must_== Html {
+      richText(s"""img to $url here""") must_== Html {
         val img = s"""<img class="embed" src="$picUrl"/>"""
         s"""img to <a rel="nofollow" href="$url" target="_blank">$img</a> here"""
       }
     }
     "ignore imgur gallery URL" in {
       val url = "https://imgur.com/gallery/pMtTE"
-      addLinks(Html(s"""link to $url here""")) must_== Html {
+      richText(s"""link to $url here""") must_== Html {
         s"""link to <a rel="nofollow" href="$url" target="_blank">$url</a> here"""
       }
     }
-    // "skip markdown images" in {
-    //   val url = "http://zombo.com"
-    //   addLinks(Html(s"""img of ![some alt]($url) here""")) must_== Html {
-    //     s"""img of ![some alt]($url) here"""
-    //   }
-    // }
   }
 
   "markdown links" should {
