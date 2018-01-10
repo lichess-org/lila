2afff55f6173b32e3a70e946330a5ad1139b57af
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-01-09 22:44:46 -0500

    Revert "hackfix streamer nested form"
    This reverts commit 0bccd0a2a4ca2cf8226a1857e856cdbc787a87c4.

--- public/javascripts/streamer.form.js
+++ public/javascripts/streamer.form.js
@@ -3,13 +3,4 @@ $(function() {
     $('.picture_wrap').html(lichess.spinnerHtml);
     $(this).parents('form').submit();
   });
-  // hackfix nested forms
-  var $form = $('form.material');
-  var $status = $form.find('div.status');
-  if ($status.find('button').length) {
-    $form.siblings().not('.top').appendTo($('form.material'));
-  }
-  $form.before($status.html(
-    $('<form method="post" action="/streamer/approval/request">').html($status.html())
-  ));
 });
