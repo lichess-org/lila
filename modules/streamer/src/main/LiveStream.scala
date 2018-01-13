e5cb154459ff570732058f6a6b5f35e333f9f8f6
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-01-13 11:13:28 -0500

    fix live stream title display

--- modules/streamer/src/main/LiveStream.scala
+++ modules/streamer/src/main/LiveStream.scala
@@ -19,14 +19,14 @@ case class LiveStreams(streams: List[Stream]) {
 
   def withTitles(lightUser: lila.user.LightUserApi) = LiveStreams.WithTitles(
     this,
-    streams.map(_.streamer.userId).map { userId =>
-      userId -> lightUser.sync(userId).flatMap(_.title)
+    streams.map(_.streamer.userId).flatMap { userId =>
+      lightUser.sync(userId).flatMap(_.title) map (userId ->)
     }.toMap
   )
 }
 
 object LiveStreams {
-  case class WithTitles(live: LiveStreams, titles: Map[User.ID, Option[String]]) {
+  case class WithTitles(live: LiveStreams, titles: Map[User.ID, String]) {
     def titleName(s: Stream) = s"${titles.get(s.streamer.userId).fold("")(_ + " ")}${s.streamer.name}"
   }
 }
