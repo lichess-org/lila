8b22ec54e88ae54318a211aa477ef8d912486457
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-01-18 07:38:52 +0100

    tweak formatting

--- modules/game/src/test/BinaryPgnTest.scala
+++ modules/game/src/test/BinaryPgnTest.scala
@@ -7,6 +7,7 @@ import org.specs2.mutable._
 class BinaryPgnTest extends Specification {
 
   "game compression" should {
+
     "compress and decompress" in {
       val pgnMoves = "Nf3 Nf6 d4 Ng8 b3 e6 Bb2 g6 c4 h5 Na3 c6 Bc1 Qf6 c5 Qe5 g3 Qf4 h3 Bd6 Ng5 Qxf2+ Kd2 Kf8 Qe1 b6 b4 Qf5 Ke3 Ke8 Rh2 Rh6 h4 Rh8 Kd2 Qf3 Rh3 Qxf1 d5 Qxe2+ Kxe2".split(" ")
       val encoded = GameEncoder.encode(pgnMoves)
@@ -19,7 +20,6 @@ class BinaryPgnTest extends Specification {
       val encoded = GameEncoder.encode(pgnMoves)
       val decoded = GameEncoder.decode(encoded)
       pgnMoves must_== decoded.toArray
-
     }
 
     "en passant and promotion" in {
