7e053acc1412d019936cbd7edea6063c211d017f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-01-18 07:03:26 +0100

    test a game with checkmate

--- modules/game/src/test/BinaryPgnTest.scala
+++ modules/game/src/test/BinaryPgnTest.scala
@@ -13,6 +13,14 @@ class BinaryPgnTest extends Specification {
       val decoded = GameEncoder.decode(encoded)
       pgnMoves must_== decoded.toArray
     }
+
+    "with checkmate" in {
+      val pgnMoves = "d4 g6 c4 Bg7 Nc3 Nf6 e4 O-O Nf3 Ne8 Be2 d6 O-O c6 Ne1 Nd7 Be3 e5 d5 c5 f3 b6 a3 a6 b4 f5 bxc5 Nxc5 Bxc5 bxc5 Rb1 fxe4 fxe4 Rxf1+ Bxf1 Nf6 Bd3 Bg4 Nf3 h5 Qa4 Bd7 Qb3 Ng4 Qb6 Qxb6 Rxb6 Ne3 Rxd6 Bg4 Kf2 Bxf3 gxf3 Nxc4 Bxc4 Kh7 Rd7 Rb8 Ke3 Rb2 Kd3 Rxh2 d6 Rh3 Ke2 Rh2+ Ke3 Rh3 Kf2 g5 Re7 g4 d7 g3+ Kg2 h4 Kxh3 g2 Kxg2 h3+ Kxh3 Kg6 d8=Q Bf6 Qd7 Bxe7 Qxe7 a5 Qe6+ Kg7 Bd5 Kf8 Bc6 Kg7 Bd5 Kf8 Qf7#".split(" ")
+      val encoded = GameEncoder.encode(pgnMoves)
+      val decoded = GameEncoder.decode(encoded)
+      pgnMoves must_== decoded.toArray
+
+    }
   }
 
 }
