18b4007f758b0da804bb7e4d756f32fd1940f54b
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-02-05 09:33:38 -0500

    reduce irwin API output

--- modules/mod/src/main/JsonView.scala
+++ modules/mod/src/main/JsonView.scala
@@ -59,7 +59,12 @@ final class JsonView(
     JsNumber(a.id)
   }
   private implicit val playerAssWrites = Json.writes[PlayerAssessment]
-  private implicit val playerAggAssWrites = Json.writes[PlayerAggregateAssessment]
+  private implicit val playerAggAssWrites = OWrites[PlayerAggregateAssessment] { a =>
+    Json.obj(
+      "user" -> a.user,
+      "relatedCheaters" -> a.relatedCheaters
+    )
+  }
 
   private implicit val gameWithFenWrites = OWrites[(Game, Option[FEN], Option[Analysis])] {
     case (g, fen, analysis) => Json.obj(
