378601c3dbbfae87ba17a85ec7e458d6be2543c0
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-03-10 11:00:55 -0500

    tweak playban feedback messages

--- modules/playban/src/main/PlaybanFeedback.scala
+++ modules/playban/src/main/PlaybanFeedback.scala
@@ -10,15 +10,15 @@ private final class PlaybanFeedback(
 
   private val tempBan = "will result in a temporary ban."
 
-  def abort(pov: Pov): Unit = tell(pov, s"Warning, {user}: aborting too many games $tempBan")
+  def abort(pov: Pov): Unit = tell(pov, s"Warning, {user}. Aborting too many games $tempBan")
 
-  def noStart(pov: Pov): Unit = tell(pov, s"Warning, {user}: failing to start games $tempBan")
+  def noStart(pov: Pov): Unit = tell(pov, s"Warning, {user}. Failing to start games $tempBan")
 
-  def rageQuit(pov: Pov): Unit = tell(pov, s"Warning, {user}: leaving games without resigning $tempBan")
+  def rageQuit(pov: Pov): Unit = tell(pov, s"Warning, {user}. Leaving games without resigning $tempBan")
 
-  def sitting(pov: Pov): Unit = tell(pov, s"Warning, {user}: letting time run out instead of resigning $tempBan")
+  def sitting(pov: Pov): Unit = tell(pov, s"Warning, {user}. Letting time run out instead of resigning $tempBan")
 
-  def sandbag(pov: Pov): Unit = tell(pov, s"Warning, {user}: losing games on purpose will result in a ban.")
+  def sandbag(pov: Pov): Unit = tell(pov, s"Warning, {user}. Losing games on purpose will result in a ban.")
 
   private def tell(pov: Pov, template: String): Unit =
     pov.player.userId foreach { userId =>
