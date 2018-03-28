944fc5ce8ed81f6ff6eb945fa0dada1e2cf7a852
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-03-28 21:10:46 +0200

    educate stream watchers about malware blocking

---
+++ public/javascripts/ads.js
@@ -0,0 +1,6 @@
+// Internet is a dangerous place.
+// You should really install a malware blocker to get rid of ads, trackers and malware sites.
+// Lichess recommends uBlock Origin. It's free and open source; fast and safe.
+// Get it for Firefox: https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/
+// Get it for Chrome:  https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?hl=en
+lichess.needBlocker = true;
---
+++ public/javascripts/streamer.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('button.follow').click(function() {
+    var klass = 'active';
+    $(this).toggleClass(klass);
+    $.ajax({
+      url: '/rel/' + ($(this).hasClass('active') ? 'follow/' : 'unfollow/') + $(this).data('user'),
+      method:'post'
+    });
+  });
+  setTimeout(function() {
+    var url = 'https://github.com/gorhill/uBlock#installation';
+    if (lichess.isChrome) url = 'https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm';
+    else if (navigator.userAgent.indexOf(' Firefox/') > -1) url = 'https://addons.mozilla.org/addon/ublock-origin/';
+    else if (navigator.userAgent.indexOf(' Edge/') > -1) url = 'https://www.microsoft.com/store/p/app/9nblggh444l4';
+    var html = '<a class="blocker button" href="'+url+'">' +
+      '<img src="https://raw.githubusercontent.com/gorhill/uBlock/master/doc/img/icon38@2x.png" width=76 height=76 />' +
+      '<strong>Install a malware blocker!</strong>' +
+      'Be safe from ads and trackers.<br />' +
+      'We recommend uBlock Origin.' +
+      '</a>';
+    if (lichess.needBlocker) $('#site_header').append(html);
+  });
+});
