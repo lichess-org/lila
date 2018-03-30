1b4b0435281f30fb5c7471c180791c5e47099de6
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-03-30 17:25:35 +0200

    double production of boardimage PNGs

--- app/controllers/Export.scala
+++ app/controllers/Export.scala
@@ -41,7 +41,7 @@ object Export extends LilaController {
   }
 
   private val PngRateLimitGlobal = new lila.memo.RateLimit[String](
-    credits = 120,
+    credits = 240,
     duration = 1 minute,
     name = "export PNG global",
     key = "export.png.global"
