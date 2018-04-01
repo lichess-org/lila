30ccf0e8d66df68ea1e45971f16be7a750a48858
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-02 01:40:12 +0200

    publish server lag every 2 seconds

--- modules/round/src/main/MoveMonitor.scala
+++ modules/round/src/main/MoveMonitor.scala
@@ -1,5 +1,6 @@
 package lila.round
 
+import scala.concurrent.duration._
 import akka.actor._
 import kamon._
 import metric.SubscriptionsDispatcher.TickMetricSnapshot
@@ -9,13 +10,15 @@ private object MoveMonitor {
   def start(system: ActorSystem, channel: ActorRef) =
 
     Kamon.metrics.subscribe("trace", "round.move.trace", system.actorOf(Props(new Actor {
+      var current: Int = 0
+      context.system.scheduler.schedule(5 second, 2 second) {
+        channel ! lila.socket.Channel.Publish(lila.socket.Socket.makeMessage("mlat", current))
+      }
       def receive = {
         case tick: TickMetricSnapshot => tick.metrics.collectFirst {
           case (entity, snapshot) if entity.category == "trace" => snapshot
         } flatMap (_ histogram "elapsed-time") foreach { h =>
-          if (!h.isEmpty) channel ! lila.socket.Channel.Publish(
-            lila.socket.Socket.makeMessage("mlat", (h.sum / h.numberOfMeasurements / 1000000).toInt)
-          )
+          if (!h.isEmpty) current = (h.sum / h.numberOfMeasurements / 1000000).toInt
         }
       }
     })))
