2698a1f38932605dcf51ad32a51afe238bfd6fd3
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-02 06:04:41 +0200

    add /api

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -41,6 +41,10 @@ object Api extends LilaController {
     Ok(apiStatusJson.add("mustUpgrade", mustUpgrade)) as JSON
   }
 
+  def index = Action {
+    Ok(views.html.base.api())
+  }
+
   def user(name: String) = ApiRequest { implicit ctx =>
     userApi.one(name, ctx.me) map toApiResult
   }
