92ef83f5bd600e24935932cceb9a12e481f9b0f8
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-03 01:25:31 +0200

    tweak relay logging

--- modules/relay/src/main/RelayFetch.scala
+++ modules/relay/src/main/RelayFetch.scala
@@ -134,7 +134,7 @@ private object RelayFetch {
   private def dgtOneFile(file: String, max: Int): Fu[MultiPgn] =
     httpGet(file).flatMap {
       case res if res.status == 200 => fuccess(splitPgn(res.body, max))
-      case res => fufail(res.status.toString)
+      case res => fufail(s"[${res.status}]")
     }
 
   import play.api.libs.json._
