970bb8dcb70651b7025f8c907796ea1d87a80216
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-05 02:09:36 +0200

    implement Pause.remainingDelay(player: Player, tour: Tournament)

--- modules/tournament/src/main/Pause.scala
+++ modules/tournament/src/main/Pause.scala
@@ -7,7 +7,7 @@ import scala.concurrent.duration._
 /*
  * Computes the delay before a player can rejoin a tournament after pausing.
  * The first pause results in a delay of gameTotalTime / 5 (e.g. 60 seconds for 5+0)
- * with a minimum of 30 seconds and a maximum of 180 seconds.
+ * with a minimum of 30 seconds and a maximum of 120 seconds.
  * Following pauses accumulate delays (2x delay, 3x delay, etc...).
  * After 20 minutes without a pause, the delay is reinitialized.
  */
@@ -15,31 +15,38 @@ private final class Pauser {
 
   import Pauser._
 
-  private val cache: Cache[Player.ID, Pauses] = Scaffeine()
+  private val cache: Cache[Player.ID, Record] = Scaffeine()
     .expireAfterWrite(20 minutes)
-    .build[Player.ID, Pauses]
-
-  private def getPauses(player: Player) = cache.get(player.id, _ => Pauses(0))
+    .build[Player.ID, Record]
 
   private def initialDelayOf(tour: Tournament) = Delay {
     (tour.clock.estimateTotalSeconds / 5) atLeast 30 atMost 120
   }
 
   def add(player: Player, tour: Tournament): Delay = {
-    val pauses = getPauses(player).add
-    initialDelayOf(tour) * pauses
+    val record = cache.getIfPresent(player.id).fold(Record(1, DateTime.now))(_.add)
+    initialDelayOf(tour) * record
   }
+
+  def remainingDelay(player: Player, tour: Tournament): Option[Delay] =
+    cache.getIfPresent(player.id) flatMap { record =>
+      val seconds = record.pausedAt.getSeconds - nowSeconds + (initialDelayOf(tour) * record).seconds
+      seconds > 1 option Delay(seconds.toInt)
+    }
 }
 
 object Pauser {
 
-  // pause counter of a player
-  case class Pauses(count: Int) extends AnyVal {
-    def add = copy(count = count + 1)
+  case class Record(count: Int, pausedAt: DateTime) {
+    def add = copy(
+      count = count + 1,
+      pausedAt = DateTime.now
+    )
   }
 
   // pause counter of a player
   case class Delay(seconds: Int) extends AnyVal {
-    def *(pauses: Pauses) = copy(seconds = seconds * pauses.count)
+    def *(record: Record) = copy(seconds = seconds * record.count)
+    def isEmpty = seconds < 1
   }
 }
