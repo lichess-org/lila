4c91d7b26b840d87ddeb8be90532d7af79088efc
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-16 21:18:37 +0200

    send current game on bot stream start

--- modules/bot/src/main/BotEventStream.scala
+++ modules/bot/src/main/BotEventStream.scala
@@ -5,6 +5,7 @@ import play.api.libs.iteratee._
 import play.api.libs.json._
 
 import lila.game.actorApi.UserStartGame
+import lila.game.Game
 import lila.user.User
 
 final class BotEventStream(
@@ -14,7 +15,7 @@ final class BotEventStream(
 
   import BotStream._
 
-  def apply(me: User): Enumerator[String] = {
+  def apply(me: User, gamesInProgress: List[Game]): Enumerator[String] = {
 
     var stream: Option[ActorRef] = None
 
@@ -22,12 +23,16 @@ final class BotEventStream(
       onStart = channel => {
         val actor = system.actorOf(Props(new Actor {
 
+          gamesInProgress foreach pushGameStart
+
           def receive = {
-            case UserStartGame(userId, game) if userId == me.id => channel push Json.obj(
-              "type" -> "gameStart",
-              "game" -> Json.obj("id" -> game.id)
-            )
+            case UserStartGame(userId, game) if userId == me.id => pushGameStart(game)
           }
+
+          def pushGameStart(game: Game) = channel push Json.obj(
+            "type" -> "gameStart",
+            "game" -> Json.obj("id" -> game.id)
+          )
         }))
         system.lilaBus.subscribe(actor, Symbol(s"userStartGame:${me.id}"))
         stream = actor.some
