714180d758758570e451acb2e2fd2fefd58743ca
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-16 15:18:43 +0200

    fix bot flag in /account/me

--- app/controllers/Account.scala
+++ app/controllers/Account.scala
@@ -36,9 +36,8 @@ object Account extends LidraughtsController {
         relationEnv.api.countFollowing(me.id) zip
         Env.pref.api.getPref(me) zip
         lidraughts.game.GameRepo.urgentGames(me) zip
-        lidraughts.user.UserRepo.isBot(me) zip
         Env.challenge.api.countInFor.get(me.id) map {
-          case nbFollowers ~ nbFollowing ~ prefs ~ povs ~ isBot ~ nbChallenges =>
+          case nbFollowers ~ nbFollowing ~ prefs ~ povs ~ nbChallenges =>
             Env.current.system.lidraughtsBus.publish(lidraughts.user.User.Active(me), 'userActive)
             Ok {
               import lidraughts.pref.JsonView._
@@ -49,7 +48,6 @@ object Account extends LidraughtsController {
                 "nbFollowers" -> nbFollowers,
                 "nbChallenges" -> nbChallenges
               ).add("kid" -> me.kid)
-                .add("bot" -> isBot)
                 .add("troll" -> me.troll)
             }
         }
