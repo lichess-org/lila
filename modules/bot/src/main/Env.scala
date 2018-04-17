4d4675799858f10066acf1ff68a257c5486d5182
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-17 18:03:07 +0200

    send empty frames for ping; keep bot player alive in the game socket

--- modules/bot/src/main/Env.scala
+++ modules/bot/src/main/Env.scala
@@ -5,12 +5,17 @@ import akka.actor._
 final class Env(
     system: ActorSystem,
     hub: lila.hub.Env,
+    onlineUserIds: lila.memo.ExpireSetMemo,
     lightUserApi: lila.user.LightUserApi
 ) {
 
   lazy val jsonView = new BotJsonView(lightUserApi)
 
-  lazy val gameStateStream = new GameStateStream(system, jsonView)
+  lazy val gameStateStream = new GameStateStream(
+    system,
+    jsonView,
+    hub.socket.round
+  )
 
   lazy val player = new BotPlayer(
     roundMap = hub.actor.roundMap
@@ -22,6 +27,7 @@ object Env {
   lazy val current: Env = "bot" boot new Env(
     system = lila.common.PlayApp.system,
     hub = lila.hub.Env.current,
+    onlineUserIds = lila.user.Env.current.onlineUserIdMemo,
     lightUserApi = lila.user.Env.current.lightUserApi
   )
 }
--- modules/socket/src/main/actorApi.scala
+++ modules/socket/src/main/actorApi.scala
@@ -11,6 +11,7 @@ case class Connected[M <: SocketMember](
 )
 case class Sync(uid: String, friends: List[String])
 case class Ping(uid: String, version: Option[Int], lagCentis: Option[Centis])
+case class BotPing(color: chess.Color)
 
 object Ping {
   def apply(uid: Socket.Uid, o: JsObject): Ping =
