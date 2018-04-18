bbac1b6094a29baf16727cd5db7184229e210165
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-18 04:30:53 +0200

    code tweak

--- app/controllers/Streamer.scala
+++ app/controllers/Streamer.scala
@@ -21,15 +21,11 @@ object Streamer extends LidraughtsController {
   }
 
   def live = Api.ApiRequest { implicit ctx =>
-    val ids = Env.streamer.liveStreamApi.userIds
-    Env.user.lightUserApi asyncMany ids.toList dmap (_.flatten) map { users =>
-      val actualIds = users.map(_.id)
-      val onlineIds = Env.user.onlineUserIdMemo intersect actualIds
-      val playingIds = Env.relation.online.playing intersect actualIds
+    Env.user.lightUserApi asyncMany Env.streamer.liveStreamApi.userIds.toList dmap (_.flatten) map { users =>
+      val playingIds = Env.relation.online.playing intersect users.map(_.id)
       Api.toApiResult {
         users.map { u =>
-          lidraughts.common.LightUser.lightUserWrites.writes(u)
-            .add("playing" -> playingIds(u.id))
+          lidraughts.common.LightUser.lightUserWrites.writes(u).add("playing" -> playingIds(u.id))
         }
       }
     }
