0204d82b9c32df4ed60c6b2bf4c5eec65ea67b3f
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-22 07:00:43 +0200

    cut down bot lag estimate

--- modules/round/src/main/Player.scala
+++ modules/round/src/main/Player.scala
@@ -121,7 +121,7 @@ private[round] final class Player(
   }
 
   private val draughtsnetLag = MoveMetrics(clientLag = Centis(5).some)
-  private val botLag = MoveMetrics(clientLag = Centis(100).some)
+  private val botLag = MoveMetrics(clientLag = Centis(50).some)
 
   private def applyUci(game: Game, uci: Uci, blur: Boolean, metrics: MoveMetrics, finalSquare: Boolean = false): Valid[MoveResult] =
     (uci match {
