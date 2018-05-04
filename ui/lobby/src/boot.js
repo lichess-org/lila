1d96fa596f08ced6e8e7ee22bcfa3304629ef302
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-05-04 15:42:11 +0200

    add lichess language to browser languages

--- ui/lobby/src/boot.js
+++ ui/lobby/src/boot.js
@@ -30,6 +30,7 @@ module.exports = function(cfg, element) {
     langs = langs.map(function(l) {
       return l.slice(0, 2).toLowerCase();
     });
+    langs.push($('html').attr('lang'));
     $('#streams_on_air').find('a').each(function() {
       var match = $(this).text().match(/\[(\w{2})\]/mi);
       if (match && langs.indexOf(match[1].toLowerCase()) === -1) $(this).hide();
