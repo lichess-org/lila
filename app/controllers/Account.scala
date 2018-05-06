a44fb9bd428ba6f73f5bf07d1e81a2e7f2f1b575
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-05-06 17:56:50 +0200

    XHR login form

--- app/controllers/Account.scala
+++ app/controllers/Account.scala
@@ -148,9 +148,9 @@ object Account extends LidraughtsController {
   def emailConfirm(token: String) = Open { implicit ctx =>
     Env.security.emailChange.confirm(token) flatMap {
       _ ?? { user =>
-        controllers.Auth.authenticateUser(user, result = Redirect {
-          s"${routes.Account.email}?ok=1"
-        }.fuccess.some)
+        controllers.Auth.authenticateUser(user, result = Some { _ =>
+          Redirect(s"${routes.Account.email}?ok=1")
+        })
       }
     }
   }
