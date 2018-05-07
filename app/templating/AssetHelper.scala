1c9724239f995768f532285ed16e608c8334d8db
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-05-07 20:49:49 +0200

    allow socket fallback ports in csp

--- app/templating/AssetHelper.scala
+++ app/templating/AssetHelper.scala
@@ -92,7 +92,7 @@ trait AssetHelper { self: I18nHelper =>
     val socket = (if (req.secure) "wss://" else "ws://") + socketDomain
     ContentSecurityPolicy(
       defaultSrc = List("'self'", assets),
-      connectSrc = List("'self'", assets, socket, lila.api.Env.current.ExplorerEndpoint, lila.api.Env.current.TablebaseEndpoint),
+      connectSrc = List("'self'", assets, socket, socket + ":*", lila.api.Env.current.ExplorerEndpoint, lila.api.Env.current.TablebaseEndpoint),
       styleSrc = List("'self'", "'unsafe-inline'", assets, "https://fonts.googleapis.com"),
       fontSrc = List("'self'", assetDomain, "https://fonts.gstatic.com"),
       childSrc = List("'self'", "https://www.youtube.com"),
