44ed126a940f9014096064a7f13e58f2a0203244
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-05-09 04:17:28 +0200

    reconnect WS less aggressively

--- ui/site/src/socket.js
+++ ui/site/src/socket.js
@@ -291,7 +291,7 @@ lidraughts.StrongSocket.defaults = {
     idle: false,
     pingMaxLag: 8000, // time to wait for pong before reseting the connection
     pingDelay: 2000, // time between pong and ping
-    autoReconnectDelay: 2000,
+    autoReconnectDelay: 3000,
     protocol: location.protocol === 'https:' ? 'wss:' : 'ws:',
     baseUrls: (function(d) {
       return [d].concat((d === 'socket.lidraughts.org' ? [5, 6, 7, 8, 9] : []).map(function(port) {
