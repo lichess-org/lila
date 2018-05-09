788fda1bf4e775cbf836e88c79ace846eac3dc3f
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-05-09 05:38:42 +0200

    reduce bot lag compensation (100ms)

--- modules/round/src/main/Player.scala
+++ modules/round/src/main/Player.scala
@@ -110,7 +110,7 @@ private[round] final class Player(
   }
 
   private val fishnetLag = MoveMetrics(clientLag = Centis(5).some)
-  private val botLag = MoveMetrics(clientLag = Centis(50).some)
+  private val botLag = MoveMetrics(clientLag = Centis(10).some)
 
   private def applyUci(game: Game, uci: Uci, blur: Boolean, metrics: MoveMetrics): Valid[MoveResult] =
     (uci match {
