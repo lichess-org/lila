9440e357554488f46574f7ceb63edf225b95d3d7
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-07-14 12:56:43 +0200

    remove double sorting

--- app/controllers/Team.scala
+++ app/controllers/Team.scala
@@ -90,7 +90,7 @@ object Team extends LilaController {
     OptionFuResult(api team id) { team =>
       Owner(team) {
         MemberRepo userIdsByTeam team.id map { userIds =>
-          html.team.kick(team, userIds.filterNot(me.id ==).toList.sorted)
+          html.team.kick(team, userIds - me.id)
         }
       }
     }
@@ -109,7 +109,7 @@ object Team extends LilaController {
     OptionFuResult(api team id) { team =>
       Owner(team) {
         MemberRepo userIdsByTeam team.id map { userIds =>
-          html.team.changeOwner(team, userIds.filterNot(team.createdBy ==).toList.sorted)
+          html.team.changeOwner(team, userIds - team.createdBy)
         }
       }
     }
