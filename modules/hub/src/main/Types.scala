feb08705cdfe7037062bf02a46fedf1fdc1e7c81
ProgramFOX <programfox@hotmail.be>
2018-07-19 17:17:40 +0200

    Semicolons, who needs semicolons?

--- modules/hub/src/main/Types.scala
+++ modules/hub/src/main/Types.scala
@@ -1,8 +1,8 @@
 package lila.hub
 
 package object tournamentTeam {
-  type TeamId = String;
-  type TeamName = String;
-  type TeamIdList = List[TeamId];
+  type TeamId = String
+  type TeamName = String
+  type TeamIdList = List[TeamId]
   type TeamIdsWithNames = List[(TeamId, TeamName)]
 }
