df83b1e2ff18a1b8b9ec6cee6251014d00df03eb
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-07-29 15:09:21 +0200

    fix concurrent impersonate

--- modules/mod/src/main/Impersonate.scala
+++ modules/mod/src/main/Impersonate.scala
@@ -8,13 +8,14 @@ object Impersonate {
   private var modToUser = Map.empty[User.ID, User.ID]
   private var userToMod = Map.empty[User.ID, User.ID]
 
-  def start(mod: User, user: User) = Granter(_.Impersonate)(mod) ?? {
+  def start(mod: User, user: User): Unit = Granter(_.Impersonate)(mod) ?? {
+    stop(user)
     modToUser = modToUser + (mod.id -> user.id)
     userToMod = userToMod + (user.id -> mod.id)
     logger.info(s"${mod.username} starts impersonating ${user.username}")
   }
 
-  def stop(user: User) = userToMod.get(user.id) ?? { modId =>
+  def stop(user: User): Unit = userToMod.get(user.id) ?? { modId =>
     modToUser = modToUser - modId
     userToMod = userToMod - user.id
     logger.info(s"${modId} stops impersonating ${user.username}")
