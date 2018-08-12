586668b4ed82e3aea3e3885d3fa61caab60707a9
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-08-12 11:04:10 +0200

    prepend stream events

--- modules/api/src/main/EventStream.scala
+++ modules/api/src/main/EventStream.scala
@@ -22,12 +22,10 @@ final class EventStream(
 
     var stream: Option[ActorRef] = None
 
-    Concurrent.unicast[Option[JsObject]](
+    val enumerator = Concurrent.unicast[Option[JsObject]](
       onStart = channel => {
         val actor = system.actorOf(Props(new Actor {
 
-          gamesInProgress foreach pushGameStart
-          challenges foreach pushChallenge
           self ! SetOnline
 
           def receive = {
@@ -40,25 +38,17 @@ final class EventStream(
                 self ! SetOnline
               }
 
-            case UserStartGame(userId, game) if userId == me.id => pushGameStart(game)
+            case UserStartGame(userId, game) if userId == me.id => channel push toJson(game).some
 
-            case lila.challenge.Event.Create(c) if c.destUserId has me.id => pushChallenge(c)
+            case lila.challenge.Event.Create(c) if c.destUserId has me.id => channel push toJson(c).some
 
             // pretend like the rematch is a challenge
             case lila.hub.actorApi.round.RematchOffer(gameId) => ChallengeMaker.makeRematchFor(gameId, me) foreach {
-              _ foreach { c => pushChallenge(c.copy(_id = gameId)) }
+              _ foreach { c =>
+                channel push toJson(c.copy(_id = gameId)).some
+              }
             }
           }
-
-          def pushGameStart(game: Game) = channel push Json.obj(
-            "type" -> "gameStart",
-            "game" -> Json.obj("id" -> game.id)
-          ).some
-
-          def pushChallenge(c: lila.challenge.Challenge) = channel push Json.obj(
-            "type" -> "challenge",
-            "challenge" -> challengeJsonView(none)(c)
-          ).some
         }))
         system.lilaBus.subscribe(
           actor,
@@ -70,5 +60,19 @@ final class EventStream(
       },
       onComplete = onComplete(stream, system)
     )
+
+    lila.common.Iteratee.prepend(
+      ((gamesInProgress map toJson) ::: (challenges map toJson)) map some,
+      enumerator
+    )
   }
+
+  private def toJson(game: Game) = Json.obj(
+    "type" -> "gameStart",
+    "game" -> Json.obj("id" -> game.id)
+  )
+  private def toJson(c: Challenge) = Json.obj(
+    "type" -> "challenge",
+    "challenge" -> challengeJsonView(none)(c)
+  )
 }
