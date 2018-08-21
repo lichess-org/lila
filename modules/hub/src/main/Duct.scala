62c4fa8ef546495712e628ed01e7c43c335be35d
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-08-21 19:28:07 +0200

    hopefully thread safe Duct (?)

--- modules/hub/src/main/Duct.scala
+++ modules/hub/src/main/Duct.scala
@@ -1,5 +1,6 @@
 package lila.hub
 
+import scala.collection.immutable.Queue
 import scala.concurrent.duration._
 import scala.concurrent.stm._
 
@@ -16,19 +17,40 @@ trait Duct {
   // implement async behaviour here
   protected val process: ReceiveAsync
 
+  import Duct.State
+
   def !(msg: Any): Unit = atomic { implicit txn =>
-    current.transform(_ >> run(msg))
+    stateRef.transform { state =>
+      if (state.busy) state enqueue msg
+      else {
+        Txn.afterCommit { _ => run(msg) }
+        state.copy(busy = true)
+      }
+    }
   }
 
-  private[this] val current: Ref[Fu[Any]] = Ref(funit)
+  private[this] val stateRef = Ref(State(false, Queue.empty))
 
-  private[this] def run(msg: Any) =
-    process.applyOrElse(msg, Duct.fallback).recover {
-      case _ => ()
+  private[this] def run(msg: Any): Fu[Any] =
+    process.applyOrElse(msg, Duct.fallback) addEffectAnyway postRun
+
+  private[this] def postRun = atomic { implicit txn =>
+    stateRef.transform { state =>
+      state.queue.dequeueOption match {
+        case None => state.copy(busy = false)
+        case Some((msg, newQueue)) =>
+          Txn.afterCommit { _ => run(msg) }
+          state.copy(busy = true, queue = newQueue)
+      }
     }
+  }
 }
 
 object Duct {
+
+  case class State(busy: Boolean, queue: Queue[Any]) {
+    def enqueue(msg: Any) = copy(queue = queue enqueue msg)
+  }
 
   private val fallback = { msg: Any =>
     lila.log("Duct").warn(s"unhandled msg: $msg")
