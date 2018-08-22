52b5729f91867cd47dce78c4cc74844f75809093
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-08-22 17:22:34 +0200

    low level caffeine DuctMap

--- modules/hub/src/main/DuctMap.scala
+++ modules/hub/src/main/DuctMap.scala
@@ -1,31 +1,35 @@
 package lila.hub
 
-import scala.concurrent.duration.FiniteDuration
-import com.github.blemale.scaffeine.{ Cache, Scaffeine }
-import com.github.benmanes.caffeine.cache.RemovalCause
+import com.github.benmanes.caffeine.cache._
 
-import actorApi.map._
+import scala.compat.java8.FunctionConverters.asJavaFunction
+import scala.collection.JavaConverters._
+import scala.concurrent.duration.FiniteDuration
+import java.util.concurrent.TimeUnit
 
 final class DuctMap[D <: Duct](
     mkDuct: String => D,
     accessTimeout: FiniteDuration
 ) {
 
-  def getOrMake(id: String): D = ducts.get(id, mkDuct)
+  def getOrMake(id: String): D = ducts.get(id)
 
   def tell(id: String, msg: Any): Unit = getOrMake(id) ! msg
 
-  def tellAll(msg: Any) = ducts.asMap().foreach(_._2 ! msg)
+  def tellAll(msg: Any) = ducts.asMap().asScala.foreach(_._2 ! msg)
 
   def tellIds(ids: Seq[String], msg: Any): Unit = ids foreach { tell(_, msg) }
 
-  def exists(id: String): Boolean = ducts.getIfPresent(id).isDefined
+  def exists(id: String): Boolean = ducts.getIfPresent(id) != null
 
   def size: Int = ducts.estimatedSize().toInt
 
   def kill(id: String): Unit = ducts invalidate id
 
-  private[this] val ducts: Cache[String, D] = Scaffeine()
-    .expireAfterAccess(accessTimeout)
-    .build[String, D]()
+  private[this] val ducts: LoadingCache[String, D] =
+    Caffeine.newBuilder()
+      .expireAfterAccess(accessTimeout.toMillis, TimeUnit.MILLISECONDS)
+      .build[String, D](new CacheLoader[String, D] {
+        def load(id: String): D = mkDuct(id)
+      })
 }
