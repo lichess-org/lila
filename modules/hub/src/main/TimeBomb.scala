56c0575919bcbec550f2a6140714680633e08aec
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-08-26 06:33:33 +0200

    Revert "remove unused TimeBomb"
    This reverts commit dd4437f5734a203b2a2f86ebb92c7b8f0936d563.

---
+++ modules/hub/src/main/TimeBomb.scala
@@ -0,0 +1,16 @@
+package lila.hub
+
+import scala.concurrent.duration.Duration
+
+final class TimeBomb(delayDuration: Duration) {
+
+  private val delayMs = delayDuration.toMillis
+
+  private var delayedAt: Double = nowMillis
+
+  def delay: Unit = {
+    delayedAt = nowMillis
+  }
+
+  def boom = (delayedAt + delayMs) < nowMillis
+}
