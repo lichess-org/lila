cda3cb2fc2a028ebcad858cdbea59bb7cd1d6bd6
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-09-08 10:46:49 +0200

    syntax fix

--- app/controllers/User.scala
+++ app/controllers/User.scala
@@ -250,7 +250,7 @@ object User extends LilaController {
     else renderModZone(username, me).logTime(s"$username renderModZone")
 
   protected[controllers] def renderModZone(username: String, me: UserModel)(implicit ctx: Context): Fu[Result] =
-    OptionFuOk(UserRepo named username logTime s"$username UserRepo.named") { user =>
+    OptionFuOk(UserRepo named username) { user =>
       UserRepo.emails(user.id).logTimeIfGt(s"$username UserRepo.emails", 100 millis) zip
         UserRepo.isErased(user).logTimeIfGt(s"$username UserRepo.isErased", 100 millis) zip
         (Env.security userSpy user).logTimeIfGt(s"$username security.userSpy", 100 millis) zip
