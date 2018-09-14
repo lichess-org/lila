6b89f4112037ae31c13db299e9bb3e10fd4115ad
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-09-14 22:01:11 +0200

    prevent robots from crawling the websocket domain

--- app/controllers/Main.scala
+++ app/controllers/Main.scala
@@ -127,9 +127,9 @@ object Main extends LidraughtsController {
     }
   }
 
-  val robots = Action {
+  val robots = Action { req =>
     Ok {
-      if (Env.api.Net.Crawlable) """User-agent: *
+      if (Env.api.Net.Crawlable && req.domain == Env.api.Net.Domain) """User-agent: *
 Allow: /
 Disallow: /game/export
 Disallow: /games/export
