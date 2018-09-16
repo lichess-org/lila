168555797c1115793411456b1264b2a0b830077f
Stefan Schermann <stefan.schermann@gmail.com>
2018-09-16 22:58:22 +0200

    Update est. Ai ratings

--- modules/draughtsnet/src/main/AiPerf.scala
+++ modules/draughtsnet/src/main/AiPerf.scala
@@ -3,13 +3,13 @@ package lidraughts.draughtsnet
 final class AiPerfApi {
 
   def intRatings: Map[Int, Int] = Map(
-    1 -> 1350,
-    2 -> 1420,
-    3 -> 1500,
-    4 -> 1600,
-    5 -> 1700,
-    6 -> 1900,
-    7 -> 2200,
-    8 -> 2500
+    1 -> 1200,
+    2 -> 1400,
+    3 -> 1650,
+    4 -> 1900,
+    5 -> 2100,
+    6 -> 2200,
+    7 -> 2400,
+    8 -> 2700
   )
 }
