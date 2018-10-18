00191712654c1ada46d60317ecb524ad9ed7d6a9
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-10-18 11:20:13 +0200

    update SpamTest after bfcf80e1350e

---
+++ modules/security/src/test/SpamTest.scala
@@ -0,0 +1,21 @@
+package lidraughts.security
+
+import org.specs2.mutable.Specification
+
+class SpamTest extends Specification {
+
+  val spam = new Spam(() => Nil)
+  val foobar = """foo bar"""
+  val _c2 = """https://chess24.com?ref=masterpart"""
+
+  "spam" should {
+    "detect" in {
+      spam.detect(foobar) must beFalse
+      spam.detect(_c2) must beTrue
+    }
+    "replace" in {
+      spam.replace(foobar) must_== foobar
+      spam.replace(_c2) must_== """https://chess24.com"""
+    }
+  }
+}
