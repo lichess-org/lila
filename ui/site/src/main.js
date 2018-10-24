10e1e9416918f12167510405ea135de30e7d349a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-10-24 23:19:30 +0200

    revert commits from #4657 to fix team pages
    The "more" button breaks the two column layout on team pages like
    https://lichess.org/team/crazyhouse-world-championship.
    
    This reverts commits efbd9f3, e24b01a, cbb430a, 1b56c33, 143e98d.

--- ui/site/src/main.js
+++ ui/site/src/main.js
@@ -507,6 +507,36 @@ lichess.topMenuIntent = function() {
         });
       });
 
+      lichess.loadInfiniteScroll = function(el) {
+        $(el).each(function() {
+          if (!$('.pager a', this).length) return;
+          var $scroller = $(this).infinitescroll({
+            navSelector: ".pager",
+            nextSelector: ".pager a",
+            itemSelector: ".infinitescroll .paginated_element",
+            errorCallback: function() {
+              $("#infscr-loading").remove();
+            },
+            loading: {
+              msg: $('<div id="infscr-loading">').html(lichess.spinnerHtml)
+            }
+          }, function() {
+            $("#infscr-loading").remove();
+            lichess.pubsub.emit('content_loaded')();
+            var ids = [];
+            $(el).find('.paginated_element[data-dedup]').each(function() {
+              var id = $(this).data('dedup');
+              if (id) {
+                if (lichess.fp.contains(ids, id)) $(this).remove();
+                else ids.push(id);
+              }
+            });
+          }).find('div.pager').hide().end();
+          $scroller.parent().append($('<button class="inf-more">More</button>').on('click', function() {
+            $scroller.infinitescroll('retrieve');
+          }));
+        });
+      }
       lichess.loadInfiniteScroll('.infinitescroll');
 
       $('#top').on('click', 'a.toggle', function() {
