8390dddab08bf799f4707a6d7620d9c3857ab385
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-11-07 14:03:08 +0100

    refactor ui/simul gulpfile

--- ui/simul/gulpfile.js
+++ ui/simul/gulpfile.js
@@ -8,44 +8,38 @@ const browserify = require('browserify');
 const uglify = require('gulp-uglify');
 const size = require('gulp-size');
 
-const browserifyOpts = {
+const browserifyOpts = (debug) => ({
   entries: ['./src/main.js'],
-  standalone: 'LidraughtsSimul'
-};
-const destination = gulp.dest('../../public/compiled/');
+  standalone: 'LidraughtsSimul',
+  debug: debug
+});
+const destination = () => gulp.dest('../../public/compiled/');
 
-function prod() {
-  return browserify(browserifyOpts).bundle()
-    .pipe(source('lidraughts.simul.min.js'))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(size())
-    .pipe(destination);
-}
+const prod = () => browserify(browserifyOpts(false)).bundle()
+  .pipe(source('lidraughts.simul.min.js'))
+  .pipe(buffer())
+  .pipe(uglify())
+  .pipe(size())
+  .pipe(destination());
 
-function dev() {
-  return browserify(browserifyOpts).bundle()
-    .pipe(source('lidraughts.simul.js'))
-    .pipe(destination);
-}
+const dev = () => browserify(browserifyOpts(true)).bundle()
+  .pipe(source('lidraughts.simul.js'))
+  .pipe(destination());
 
-function watch() {
+const watch = () => {
 
   const bundle = () => bundler.bundle()
     .on('error', error => logger.error(colors.red(error.message)))
     .pipe(source('lidraughts.simul.js'))
-    .pipe(destination);
+    .pipe(destination());
 
   const bundler = watchify(
-    browserify(Object.assign({}, watchify.args, browserifyOpts, {
-      debug: true
-    }))
+    browserify(Object.assign({}, watchify.args, browserifyOpts(true)))
   ).on('update', bundle).on('log', logger.info);
 
   return bundle();
-}
+};
 
 gulp.task('prod', prod);
 gulp.task('dev', dev);
-gulp.task('watch', watch);
 gulp.task('default', watch);
