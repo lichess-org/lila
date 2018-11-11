5b6fce9cae37e3070a5ae72c6edca249407ec81e
Stefan Schermann <stefan.schermann@gmail.com>
2018-11-11 17:07:04 +0100

    Add increment to daily antidraughts

--- modules/tournament/src/main/Schedule.scala
+++ modules/tournament/src/main/Schedule.scala
@@ -209,6 +209,7 @@ object Schedule {
 
     (s.freq, s.variant, s.speed) match {
       // Special cases.
+      case (Daily, Antidraughts, SuperBlitz) => TC(3 * 60, 2)
       case (Weekly, Frisian, Blitz) => TC(5 * 60, 2)
       case (Hourly, Standard, Bullet) if bulletInc(s) => TC(60, 1)
       case (Hourly, Standard, SuperBlitz) if standardInc(s) => TC(3 * 60, 2)
