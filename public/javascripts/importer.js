093c7398bca0ffc487e641851f25ad2d13750101
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-12-02 14:33:38 +0700

    /paste file upload - for #4733

---
+++ public/javascripts/importer.js
@@ -0,0 +1,20 @@
+$(function() {
+  var $form = $("#import_game form");
+  $form.submit(function() {
+    setTimeout(function() { $form.html(lichess.spinnerHtml); }, 50);
+  });
+
+  if (window.FileReader) {
+    $form.find('input[type=file]').on('change', function() {
+      var file = this.files[0];
+      if (!file) return;
+      var $file = $(this);
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $form.find('textarea').val(e.target.result);
+        $file.val('');
+      };
+      reader.readAsText(file);
+    });
+  } else $form.find('.upload').remove();
+});
