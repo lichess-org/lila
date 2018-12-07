6b51f7651aa89bfa08b8ab1716332c0ed01daada
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-12-07 15:16:19 +0700

    refactor analyse socket member add

--- modules/analyse/src/main/AnalyseSocketHandler.scala
+++ modules/analyse/src/main/AnalyseSocketHandler.scala
@@ -3,28 +3,22 @@ package lidraughts.analyse
 import lidraughts.socket._
 import lidraughts.user.User
 
-import play.api.libs.iteratee._
-import play.api.libs.json.JsValue
-
 private[analyse] final class AnalyseSocketHandler(
-    socket: lidraughts.hub.Trouper,
+    socket: AnalyseSocket,
     hub: lidraughts.hub.Env,
     evalCacheHandler: lidraughts.evalCache.EvalCacheSocketHandler
 ) {
 
   import AnalyseSocket._
 
-  def join(uid: Socket.Uid, user: Option[User], apiVersion: Int): Fu[JsSocketHandler] = {
-    val (enumerator, channel) = Concurrent.broadcast[JsValue]
-    val member = Member(channel, user.map(_.id))
-    socket.ask[Unit](AddMember(uid, member, _)) map { _ =>
-      Handler.iteratee(
+  def join(uid: Socket.Uid, user: Option[User], apiVersion: Int): Fu[JsSocketHandler] =
+    socket.addMember(uid)(Member(_, user.map(_.id))) map {
+      case (member, enum) => Handler.iteratee(
         hub,
         evalCacheHandler(uid, member, user),
         member,
         socket,
         uid
-      ) -> enumerator
+      ) -> enum
     }
-  }
 }
