c4f8ee206ba17e173f7517db22c6ed3bb550f084
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-12-09 08:58:22 +0700

    use location.host
    https://stackoverflow.com/questions/6725890/location-host-vs-location-hostname-and-cross-browser-compatibility

--- public/javascripts/twofactor.form.js
+++ public/javascripts/twofactor.form.js
@@ -1,5 +1,5 @@
 $(function() {
-  var issuer = window.location.hostname; // lidraughts.org
+  var issuer = window.location.host; // lidraughts.org
   var user = $(document.body).data('user');
   var secret = $('input[name=secret]').val();
   new QRCode(document.getElementById('qrcode'), {
