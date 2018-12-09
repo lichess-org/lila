57c9bcd8626088420a7955c6586900bf89ab4e85
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-12-09 20:17:24 +0700

    monitor pushing on closed WS

--- modules/socket/src/main/SocketMember.scala
+++ modules/socket/src/main/SocketMember.scala
@@ -10,7 +10,11 @@ trait SocketMember {
 
   def isAuth = userId.isDefined
 
-  def push(msg: JsValue) = channel push msg
+  def push(msg: JsValue) = try {
+    channel push msg
+  } catch {
+    case _: java.nio.channels.ClosedChannelException => lila.mon.socket.pushChannelClosed()
+  }
 
   def end = channel.end
 }
