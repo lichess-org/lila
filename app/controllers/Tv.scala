c7f1fe01d13d36065587f4f7802cfa44612d0a2c
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-12-13 10:27:29 +0800

    fix embed protocol

--- app/controllers/Tv.scala
+++ app/controllers/Tv.scala
@@ -94,7 +94,7 @@ object Tv extends LidraughtsController {
       val bg = get("bg", req) | "light"
       val theme = get("theme", req) | "maple"
       val url = s"""${req.domain + routes.Tv.frame}?bg=$bg&theme=$theme"""
-      s"""document.write("<iframe src='//$url&embed=" + document.domain + "' class='lidraughts-tv-iframe' allowtransparency='true' frameBorder='0' style='width: 224px; height: 264px;' title='Lidraughts free online draughts'></iframe>");"""
+      s"""document.write("<iframe src='https://$url&embed=" + document.domain + "' class='lidraughts-tv-iframe' allowtransparency='true' frameBorder='0' style='width: 224px; height: 264px;' title='Lidraughts free online draughts'></iframe>");"""
     } as JAVASCRIPT withHeaders (CACHE_CONTROL -> "max-age=86400")
   }
 
