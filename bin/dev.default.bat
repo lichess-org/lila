14655a0a31470a89187f59cdf9a1acc0287763be
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-12-13 11:55:15 +0100

    update bin/dev.default.bat

--- bin/dev.default.bat
+++ bin/dev.default.bat
@@ -1,9 +1,16 @@
 @echo off
 
 REM Starts a dev console to compile and run lichess.
-REM To edit this file, copy it to bin\dev.bat: it's not indexed by Git.
+REM
+REM To edit this file, copy it to bin\dev.bat. It will not be tracked by Git.
+REM
+REM Usage:
+REM bin\dev.bat [compile] [run]
 
-REM Yes it needs tons of memory. Go for 2048M if you have them.
-set JAVA_OPTS=-Xms1536M -Xmx1536M -XX:ReservedCodeCacheSize=64m -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:+ExitOnOutOfMemoryError -Dkamon.auto-start=true
+REM Yes it needs tons of memory. Go for 4G if you have them.
+set JAVA_OPTS=-Xms2048M -Xmx2560M -XX:ReservedCodeCacheSize=128m -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:+ExitOnOutOfMemoryError -Dkamon.auto-start=true
+
+REM For development without nginx.
+set SERVE_ASSETS=1
 
 sbt %*
