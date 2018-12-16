9759b2bc5814c8937969552982f6372e23ba0850
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-12-16 22:39:09 +0800

    monitor timeline API

--- app/controllers/Timeline.scala
+++ app/controllers/Timeline.scala
@@ -4,27 +4,22 @@ import play.api.libs.json._
 
 import lidraughts.app._
 import lidraughts.common.HTTPRequest
+import lidraughts.timeline.Entry.entryWrites
 import views._
 
 object Timeline extends LidraughtsController {
 
-  def home = Auth { implicit ctx =>
-    import lidraughts.timeline.Entry.entryWrites
-    val nb = getInt("nb").fold(100)(_ min 100)
-    me =>
-      negotiate(
-        html = {
-          if (HTTPRequest.isXhr(ctx.req))
-            Env.timeline.entryApi.userEntries(me.id) map { html.timeline.entries(_) }
-          else {
-            Env.timeline.entryApi.moreUserEntries(me.id, nb) map { html.timeline.more(_) }
-          }
-        },
-        _ => {
-          val entries = Env.timeline.entryApi.moreUserEntries(me.id, nb)
-          entries map { es => Ok(Json.obj("entries" -> es)) }
-        }
-      )
+  def home = Auth { implicit ctx => me =>
+    def nb = getInt("nb").fold(15)(_ min 75)
+    lidraughts.mon.http.response.timeline.count()
+    negotiate(
+      html =
+        if (HTTPRequest.isXhr(ctx.req))
+          Env.timeline.entryApi.userEntries(me.id) map { html.timeline.entries(_) }
+        else
+          Env.timeline.entryApi.moreUserEntries(me.id, nb) map { html.timeline.more(_) },
+      _ => Env.timeline.entryApi.moreUserEntries(me.id, nb) map { es => Ok(Json.obj("entries" -> es)) }
+    ).mon(_.http.response.accountInfo.time)
   }
 
   def unsub(channel: String) = Auth { implicit ctx => me =>
