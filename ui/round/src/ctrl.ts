6b63b707ed472020383e2784d57a3bbfa11986fb
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-12-17 00:19:10 +0800

    wait longer before reloading TV

--- ui/round/src/ctrl.ts
+++ ui/round/src/ctrl.ts
@@ -515,7 +515,7 @@ export default class RoundController {
         this.redraw();
         this.autoScroll();
         this.onChange();
-        if (d.tv) setTimeout(li.reload, 8000);
+        if (d.tv) setTimeout(li.reload, 10000);
     };
 
     challengeRematch = (): void => {
