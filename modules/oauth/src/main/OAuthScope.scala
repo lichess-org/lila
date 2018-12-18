cc60775ba5c1fd60b4366e080af2f640a4b25049
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-12-18 21:55:32 +0800

    tweak wording

--- modules/oauth/src/main/OAuthScope.scala
+++ modules/oauth/src/main/OAuthScope.scala
@@ -23,7 +23,7 @@ object OAuthScope {
   }
 
   object Bot {
-    case object Play extends OAuthScope("bot:play", "Play bot moves")
+    case object Play extends OAuthScope("bot:play", "Play as a bot")
   }
 
   case class Scoped(user: lila.user.User, scopes: List[OAuthScope])
