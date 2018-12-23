764b0c0d06514c640994226bd4a9b97ec5c42b90
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-12-23 13:26:07 +0800

    upgrade scalachess

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
   val findbugs = "com.google.code.findbugs" % "jsr305" % "3.0.1"
   val hasher = "com.roundeights" %% "hasher" % "1.2.0"
   val jodaTime = "joda-time" % "joda-time" % "2.9.9"
-  val chess = "org.lichess" %% "scalachess" % "8.6.19"
+  val chess = "org.lichess" %% "scalachess" % "8.6.20"
   val compression = "org.lichess" %% "compression" % "1.4"
   val maxmind = "com.sanoma.cda" %% "maxmind-geoip2-scala" % "1.2.3-THIB"
   val prismic = "io.prismic" %% "scala-kit" % "1.2.13-THIB211"
