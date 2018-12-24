eb33545409e68e3a9cace27216a770bd3e39c03e
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-12-24 15:05:54 +0800

    {master} email confirmation help dedicated page

--- modules/user/src/main/DataForm.scala
+++ modules/user/src/main/DataForm.scala
@@ -57,6 +57,5 @@ object DataForm {
     Constraints maxLength 30,
     Constraints.pattern(regex = User.historicalUsernameRegex)
   )
-
   lazy val historicalUsernameField = text.verifying(historicalUsernameConstraints: _*)
 }
