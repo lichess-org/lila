6a1a9039bc88526a5e5a64ca0cc67c5f45d93eab
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-01-11 13:27:57 +0800

    rewrite round textual representation

---
+++ app/views/game/textualRepresentation.scala
@@ -0,0 +1,48 @@
+package views.html
+package game
+
+import lila.api.Context
+import lila.app.templating.Environment._
+import lila.app.ui.ScalatagsTemplate._
+
+import controllers.routes
+
+object textualRepresentation {
+
+  def apply(pov: lila.game.Pov, playing: Boolean)(implicit ctx: Context) = frag(
+
+    h1("Textual representation"),
+    dl(
+      dt("Turn"),
+      dd(pov.game.turns),
+      dt("PGN"),
+      dd(raw(pov.game.pgnMoves.mkString("<br>"))),
+      dt("FEN"),
+      dd(chess.format.Forsyth.>>(pov.game.chess)),
+      if (playing) frag(
+        dt("Your color"),
+        dd(pov.color.name),
+        dt("Opponent"),
+        dd(playerText(pov.opponent))
+      )
+      else frag(
+        dt("White player"),
+        dd(playerText(pov.game.whitePlayer)),
+        dt("Black player"),
+        dd(playerText(pov.game.blackPlayer))
+      ),
+      dt("Game status"),
+      dd(
+        if (pov.game.finishedOrAborted) gameEndStatus(pov.game)
+        else frag(pov.game.turnColor.name, " plays")
+      ),
+      (playing && pov.game.playable && pov.game.turnOf(pov.player)) option form(
+        label(
+          "Your move",
+          input(name := "move", cls := "move", `type` := "text", cls := "", autocomplete := "off")
+        ),
+        button(`type` := "submit")("Send move")
+      )
+    )
+  )
+}
