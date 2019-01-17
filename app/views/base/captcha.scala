01baa1ec47119ebce9f8be325cf7df44cb0e16f6
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-01-17 14:03:08 +0800

    rewrite captcha template

---
+++ app/views/base/captcha.scala
@@ -0,0 +1,58 @@
+package views.html.base
+
+import lila.common.String.html.safeJsonValue
+import play.api.libs.json.Json
+
+import lila.api.Context
+import lila.app.templating.Environment._
+import lila.app.ui.ScalatagsTemplate._
+
+import controllers.routes
+
+object captcha {
+
+  private val dataCheckUrl = attr("data-check-url")
+  private val dataPlayable = attr("data-playable")
+  private val dataX = attr("data-x")
+  private val dataY = attr("data-y")
+  private val dataZ = attr("data-z")
+
+  def apply(form: lila.common.Form.FormLike, captcha: lila.common.Captcha)(implicit ctx: Context) = frag(
+    form3.hidden(form("gameId"), captcha.gameId.some),
+    if (ctx.blindMode) form3.hidden(form("move"), captcha.solutions.head.some)
+    else {
+      val url = netBaseUrl + routes.Round.watcher(captcha.gameId, if (captcha.white) "white" else "black")
+      div(
+        cls := List(
+          "captcha form-group" -> true,
+          "is-invalid" -> lila.common.Captcha.isFailed(form)
+        ),
+        dataCheckUrl := routes.Main.captchaCheck(captcha.gameId)
+      )(
+          div(
+            cls := "mini_board parse_fen is2d",
+            dataPlayable := "1",
+            dataX := encodeFen(safeJsonValue(Json.toJson(captcha.moves))),
+            dataY := encodeFen(if (captcha.white) { "white" } else { "black" }),
+            dataZ := encodeFen(captcha.fen)
+          )(miniBoardContent),
+          div(cls := "captcha-explanation")(
+            label(cls := "form-label")(trans.colorPlaysCheckmateInOne.frag(
+              (if (captcha.white) trans.white else trans.black).frag()
+            )),
+            br, br,
+            trans.thisIsAChessCaptcha.frag(),
+            br,
+            trans.clickOnTheBoardToMakeYourMove.frag(),
+            br, br,
+            trans.help.frag(),
+            " ",
+            a(cls := "hint--bottom", dataHint := trans.viewTheSolution.txt(), target := "_blank", href := url)(url),
+            div(cls := "result success text", dataIcon := "E")(trans.checkmate.frag()),
+            div(cls := "result failure text", dataIcon := "k")(trans.notACheckmate.frag()),
+            form3.hidden(form("move"))
+          )
+        )
+    }
+  )
+}
