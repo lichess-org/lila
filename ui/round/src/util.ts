90acee156b9726fcd433d63515f947ab066d6fc4
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-01-23 10:11:54 +0800

    nvui rematch negotiation

--- ui/round/src/util.ts
+++ ui/round/src/util.ts
@@ -72,7 +72,9 @@ export function getScore(pieces: cg.Pieces): number {
 }
 
 export function spinner() {
-  return h('div.spinner', [
+  return h('div.spinner', {
+    'aria-label': 'loading'
+  }, [
     h('svg', { attrs: { viewBox: '0 0 40 40' } }, [
       h('circle', {
         attrs: { cx: 20, cy: 20, r: 18, fill: 'none' }
