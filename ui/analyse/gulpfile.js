12df322f8df9e910266405c7fc217bbd3dcdacfc
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-01-25 14:06:54 +0800

    refactor gulp TS plugins, bootstrap analyse nvui plugin

--- ui/analyse/gulpfile.js
+++ ui/analyse/gulpfile.js
@@ -1,3 +1,13 @@
 const lilaGulp = require('../gulp/tsProject.js');
+const lilaGulpPlugins = require('../gulp/tsPlugins.js');
 
 lilaGulp('LichessAnalyse', 'lichess.analyse', __dirname);
+
+// adds commands: NVUI
+lilaGulpPlugins([
+  {
+    standalone: 'NVUI',
+    entries: ['src/plugins/nvui.ts'],
+    target: 'lichess.round.nvui.min.js'
+  }
+]);
