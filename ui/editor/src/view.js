e65562cae9f27d76a172f2da8c4f404d7f17f9ce
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-02-14 12:15:49 +0700

    friend box WIP

--- ui/editor/src/view.js
+++ ui/editor/src/view.js
@@ -193,7 +193,7 @@ function sparePieces(ctrl, color, orientation, position) {
   var pieces = [[color, 'king'], [color, 'man'], ['', ''], ['', ''], [opposite, 'man'], [opposite, 'king']];
 
   return m('div', {
-    class: ['spare', 'spare-' + position, orientation, 'spare-' + color].join(' ')
+    class: ['spare', 'spare-' + position, 'orientation-' + orientation, 'spare-' + color].join(' ')
   }, ['pointer'].concat(pieces).concat('trash').map(function(s) {
 
     var className = selectedToClass(s);
