e8e425b9c6c0d743565546d7ef1a61374f4e1871
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-02-17 07:49:52 +0700

    fix nasty bugs

--- ui/site/src/standalones/util.js
+++ ui/site/src/standalones/util.js
@@ -2,7 +2,7 @@ var lidraughts = window.lidraughts = window.lidraughts || {};
 
 lidraughts.engineName = 'Scan 3.1';
 
-lidraughts.raf = window.requestAnimationFrame;
+lidraughts.raf = window.requestAnimationFrame.bind(window);
 lidraughts.requestIdleCallback = (window.requestIdleCallback || window.setTimeout).bind(window);
 lidraughts.dispatchEvent = function(el, eventName) {
   // compability for ie 11 instead of el.dispatchEvent(new Event(eventName)))
