dc8e9216271324ec4791c735a491db70290b52b9
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-02-25 14:36:10 +0700

    browser upgrade prompt

--- ui/site/src/main.js
+++ ui/site/src/main.js
@@ -604,6 +604,9 @@ lidraughts.topMenuIntent = function() {
         return false;
       });
 
+      if (!getComputedStyle(document.body).getPropertyValue('--gride'))
+        $.get(lichess.assetUrl('oops/browser.html'), html => $('body').prepend($(html)))
+
       if (window.Fingerprint2) setTimeout(function() {
         var t = Date.now()
         new Fingerprint2({
