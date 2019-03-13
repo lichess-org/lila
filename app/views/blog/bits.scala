b8eff7a3b75f91255d304c9cc7fc7462790cac9e
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-03-13 08:48:08 +0700

    blog UI WIP

---
+++ app/views/blog/bits.scala
@@ -0,0 +1,36 @@
+package views.html.blog
+
+import lidraughts.api.Context
+import lidraughts.app.templating.Environment._
+import lidraughts.app.ui.ScalatagsTemplate._
+import lidraughts.common.String.html.richText
+
+import controllers.routes
+
+object bits {
+
+  def layout(title: String, openGraph: Option[lidraughts.app.ui.OpenGraph] = None, moreJs: Frag = emptyFrag)(body: Modifier*)(implicit ctx: Context) =
+    views.html.base.layout(
+      title = title,
+      moreCss = responsiveCssTag("blog"),
+      moreJs = frag(prismicJs, moreJs),
+      responsive = true,
+      openGraph = openGraph,
+      csp = defaultCsp.withPrismic(isGranted(_.Prismic)).some
+    ) {
+        main(cls := "blog page-small box box-pad")(body)
+      }
+
+  def metas(doc: io.prismic.Document)(implicit ctx: Context, prismic: lidraughts.blog.BlogApi.Context) =
+    div(cls := "meta")(
+      doc.getDate("blog.date").map { date =>
+        span(cls := "text", dataIcon := "p")(semanticDate(date.value.toDateTimeAtStartOfDay))
+      },
+      doc.getText("blog.author").map { author =>
+        span(cls := "text", dataIcon := "r")(richText(author))
+      },
+      doc.getText("blog.category").map { categ =>
+        span(cls := "text", dataIcon := "t")(categ)
+      }
+    )
+}
