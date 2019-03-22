69ee6689402befc2391cdc7d3f8740a533221642
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-03-22 15:56:45 +0700

    prismic pages UI

---
+++ app/views/site/page.scala
@@ -0,0 +1,22 @@
+package views.html.site
+
+import lila.api.Context
+import lila.app.templating.Environment._
+import lila.app.ui.ScalatagsTemplate._
+
+object page {
+
+  def apply(doc: io.prismic.Document, resolver: io.prismic.DocumentLinkResolver)(implicit ctx: Context) =
+    views.html.base.layout(
+      moreCss = responsiveCssTag("page"),
+      responsive = true,
+      title = s"${~doc.getText("doc.title")}"
+    ) {
+        main(cls := "page-small box box-pad page")(
+          h1(doc.getText("doc.title")),
+          div(cls := "body")(
+            raw(~doc.getHtml("doc.content", resolver))
+          )
+        )
+      }
+}
