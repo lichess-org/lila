a6d6987c5d62c310e2f586c77605421fd79e4452
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-03-26 17:17:39 +0700

    /variant UI

--- app/views/site/variant.scala
+++ app/views/site/variant.scala
@@ -13,49 +13,60 @@ object variant {
     resolver: io.prismic.DocumentLinkResolver,
     variant: draughts.variant.Variant,
     perfType: lidraughts.rating.PerfType
-  )(implicit ctx: Context) = layout(active = perfType.some, title = s"${variant.name} • ${variant.title}") {
-    div(cls := "content_box small_box doc_box")(
-      h1(cls := "lidraughts_title text", dataIcon := perfType.iconChar)(variant.name),
-      h2(cls := "headline")(variant.title),
-      div(cls := "body")(raw(~doc.getHtml("variant.content", resolver)))
-    )
-  }
+  )(implicit ctx: Context) = layout(
+    active = perfType.some,
+    title = s"${variant.name} • ${variant.title}",
+    klass = "box-pad page variant"
+  )(
+    h1(cls := "text", dataIcon := perfType.iconChar)(variant.name),
+    h2(cls := "headline")(variant.title),
+    div(cls := "body")(raw(~doc.getHtml("variant.content", resolver)))
+  )
 
-  def home()(implicit ctx: Context) = layout(title = trans.rulesAndVariants.txt()) {
-    div(cls := "content_box small_box doc_box no_padding")(
-      h1(cls := "lidraughts_title text", dataIcon := "")(trans.rulesAndVariants()),
-      div(cls := "body content_box_content")(
+  def home()(implicit ctx: Context) = layout(
+    title = trans.rulesAndVariants.txt(),
+    klass = "variants"
+  )(
+      h1(trans.rulesAndVariants()),
+      div(cls := "body box__pad")(
         p(trans.standardFmjdRegulationsWithDrawingRules()),
         p(trans.exploreDraughtsVariants())
       ),
       div(cls := "variants")(
         lidraughts.rating.PerfType.variantsPlus map { pt =>
           val variant = lidraughts.rating.PerfType variantOf pt
-          a(cls := "variant text", href := routes.Page.variant(pt.key), dataIcon := pt.iconChar)(
-            h2(variant.name),
-            h3(cls := "headline")(variant.title)
+          a(cls := "variant text box__pad", href := routes.Page.variant(pt.key), dataIcon := pt.iconChar)(
+            span(
+              h2(variant.name),
+              h3(cls := "headline")(variant.title)
+            )
           )
         }
       )
     )
-  }
 
   private def layout(
     title: String,
+    klass: String,
     active: Option[lidraughts.rating.PerfType] = None,
     openGraph: Option[lidraughts.app.ui.OpenGraph] = None
-  )(body: Frag)(implicit ctx: Context) = views.html.base.layout(
+  )(body: Modifier*)(implicit ctx: Context) = views.html.base.layout(
     title = title,
-    menu = Some(frag(
-      lidraughts.rating.PerfType.variantsPlus map { pt =>
-        a(
-          cls := List("text" -> true, "active" -> active.has(pt)),
-          href := routes.Page.variant(pt.key),
-          dataIcon := pt.iconChar
-        )(pt.name)
-      }
-    )),
-    moreCss = cssTags("page.css", "variant.css"),
+    moreCss = responsiveCssTag("variant"),
+    responsive = true,
     openGraph = openGraph
-  )(body)
+  )(
+      main(cls := "page-menu")(
+        st.aside(cls := "page-menu__menu subnav")(
+          lidraughts.rating.PerfType.variantsPlus map { pt =>
+            a(
+              cls := List("text" -> true, "active" -> active.has(pt)),
+              href := routes.Page.variant(pt.key),
+              dataIcon := pt.iconChar
+            )(pt.name)
+          }
+        ),
+        div(cls := s"page-menu__content box $klass")(body)
+      )
+    )
 }
