ab9878b2a179a6fc49c383e7f98f075cdccc05be
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-03-31 09:23:37 +0200

    fix /developers

--- app/views/site/help.scala
+++ app/views/site/help.scala
@@ -17,7 +17,7 @@ object help {
       title = title,
       active = active,
       moreCss = cssTag("page.css")
-    )(div(cls := "content_box small_box doc_box")(
+    )(div(cls := "page-small box box-pad page")(
         h1(cls := "lidraughts_title")(title),
         div(cls := "body")(raw(~doc.getHtml("doc.content", resolver)))
       ))
@@ -35,7 +35,7 @@ object help {
       title = "Webmasters",
       active = "webmasters"
     )(frag(
-      div(cls := "content_box small_box developers")(
+      div(cls := "box box-pad developers")(
         h1(id := "embed-tv", cls := "lidraughts_title")("Embed Lidraughts TV in your site"),
         raw(s"""<script src="$netBaseUrl/tv/embed?theme=wood&bg=light"></script>"""),
         p("Just add the following HTML to your site:"),
@@ -43,7 +43,7 @@ object help {
         parameters
       ),
       br,
-      div(cls := "content_box small_box developers")(
+      div(cls := "box box-pad developers")(
         h1(id := "embed-puzzle", cls := "lidraughts_title")("Embed the daily puzzle in your site"),
         raw("""<script src="/training/embed?theme=auto&bg=auto"></script>"""),
         p("Just add the following HTML to your site:"),
@@ -52,7 +52,7 @@ object help {
         p("The text is automatically translated to your visitor's language.")
       ),
       br,
-      div(cls := "content_box small_box developers")(
+      div(cls := "box box-pad developers")(
         h1("Embed a draughts analysis in your site"),
         raw(s"""<iframe width=530 height=353 src="$netBaseUrl/study/embed/xGDc4tlJ/AqJhrQbk?bg=auto&theme=auto" frameborder=0 style="margin-bottom: 1em"></iframe>"""),
         p("Create ", a(href := routes.Study.allDefault(1), cls := "blue")("a study"), ", then click the share button to get the HTML code for the current chapter."),
@@ -63,7 +63,7 @@ src="$netBaseUrl/study/embed/xGDc4tlJ/AqJhrQbk?bg=auto&theme=auto"
         p("The text is automatically translated to your visitor's language.")
       ),
       br,
-      div(cls := "content_box small_box developers")(
+      div(cls := "box box-pad developers")(
         h1("Embed an interactive lesson in your site"),
         raw(s"""<iframe width=530 height=353 src="$netBaseUrl/study/embed/vxL8cJ67/fh6Ycb8X?next=true&bg=auto&theme=auto" frameborder=0 style="margin-bottom: 1em"></iframe>"""),
         p("Create ", a(href := routes.Study.allDefault(1), cls := "blue")("a study"), " with a chapter of type \"Interactive lesson\", then click the share button to get the HTML code for that chapter."),
@@ -74,7 +74,7 @@ src="$netBaseUrl/study/embed/vxL8cJ67/fh6Ycb8X?next=true&bg=auto&theme=auto"
         p("The text is automatically translated to your visitor's language.")
       ),
       br,
-      div(cls := "content_box small_box developers")(
+      div(cls := "box box-pad developers")(
         h1("Embed a draughts game in your site"),
         raw(s"""<iframe width=530 height=353 src="$netBaseUrl/embed/JLuuVBv5?bg=auto&theme=auto" frameborder=0 style="margin-bottom: 1em"></iframe>"""),
         p(raw("""On a game analysis page, click the <em>"FEN &amp; PGN"</em> tab at the bottom, then """), "\"", em(trans.embedInYourWebsite(), "\".")),
@@ -85,7 +85,7 @@ src="$netBaseUrl/embed/JLuuVBv5?theme=auto&bg=auto"
         p("The text is automatically translated to your visitor's language.")
       )
     /*br,
-      div(cls := "content_box small_box developers")(
+      div(cls := "box box-pad developers")(
         h1("HTTP API"),
         p(raw("""Lidraughts exposes a RESTish HTTP/JSON API that you are welcome to use. Read the <a href="/api" class="blue">HTTP API documentation</a>."""))
       )*/
