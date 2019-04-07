ecbd5754e97a2827ac1892b5e08b6cc649cd86c7
thomas <thomas.kluiters@gmail.com>
2019-04-07 23:01:14 +0200

    Implement username changes in front-end

---
+++ app/views/account/username.scala
@@ -0,0 +1,26 @@
+package views.html
+package account
+
+import lidraughts.api.Context
+import lidraughts.app.templating.Environment._
+import lidraughts.app.ui.ScalatagsTemplate._
+
+import controllers.routes
+
+object username {
+
+  def apply(u: lidraughts.user.User, form: play.api.data.Form[_])(implicit ctx: Context) = account.layout(
+    title = s"${u.username} - ${trans.editProfile.txt()}",
+    active = "editUsername",
+    evenMoreCss = cssTag("form3.css")
+  ) {
+      div(cls := "content_box small_box")(
+        h1(cls := "lidraughts_title text", dataIcon := "*")(trans.editProfile()),
+        st.form(cls := "form3", action := routes.Account.usernameApply, method := "POST")(
+          form3.globalError(form),
+          form3.group(form("userName"), trans.username.frag(), half = true, help = trans.usernameDescription.frag().some)(form3.input(_)),
+          form3.actionHtml(form3.submit(trans.apply.frag()))
+        )
+      )
+    }
+}
