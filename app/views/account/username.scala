9e9e806f8e97a3cfc4d24ce6787a42c66df37c25
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-04-08 18:16:25 +0700

    merge username change UI

--- app/views/account/username.scala
+++ app/views/account/username.scala
@@ -9,18 +9,17 @@ import controllers.routes
 
 object username {
 
-  def apply(u: lidraughts.user.User, form: play.api.data.Form[_])(implicit ctx: Context) = account.layout(
-    title = s"${u.username} - ${trans.changeUsername.txt()}",
-    active = "editUsername",
-    evenMoreCss = cssTag("form3.css")
+  def apply(u: lila.user.User, form: play.api.data.Form[_])(implicit ctx: Context) = account.layout(
+    title = s"${u.username} - ${trans.editProfile.txt()}",
+    active = "username"
   ) {
-      div(cls := "content_box small_box")(
-        h1(cls := "lidraughts_title text")(trans.changeUsername()),
-        st.form(cls := "form3", action := routes.Account.usernameApply, method := "POST")(
-          form3.globalError(form),
-          form3.group(form("userName"), trans.username.frag(), half = true, help = trans.changeUsernameDescription.frag().some)(form3.input(_)),
-          form3.actionHtml(form3.submit(trans.apply.frag()))
-        )
+    div(cls := "content_box small_box")(
+      h1(cls := "lidraughts_title text", dataIcon := "*")(trans.editProfile()),
+      st.form(cls := "form3", action := routes.Account.usernameApply, method := "POST")(
+        form3.globalError(form),
+        form3.group(form("username"), trans.username.frag(), half = true, help = trans.changeUsernameDescription.frag().some)(form3.input(_)),
+        form3.actionHtml(form3.submit(trans.apply.frag()))
       )
-    }
+    )
+  }
 }
