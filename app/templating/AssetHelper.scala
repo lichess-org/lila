128e11ca02453ae4cde5b2d827e5011e66f3aaf7
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-04-13 15:39:07 +0700

    fix captcha JS tag

--- app/templating/AssetHelper.scala
+++ app/templating/AssetHelper.scala
@@ -72,7 +72,7 @@ trait AssetHelper { self: I18nHelper with SecurityHelper =>
   def analyseNvuiTag(implicit ctx: Context) = ctx.blind option
     jsAt(s"compiled/lichess.analyse.nvui.min.js")
 
-  def captchaTag = jsAt(s"compiled/captcha${isProd ?? (".min")}.js")
+  def captchaTag = jsAt(s"compiled/captcha.js")
 
   val highchartsLatestTag = raw {
     s"""<script src="${staticUrl("vendor/highcharts-4.2.5/highcharts.js")}"></script>"""
