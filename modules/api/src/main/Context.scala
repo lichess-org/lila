49a0a8689c03b36705af74ad323e641ef18d039b
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-04-16 14:00:20 +0700

    allow to zoom out even more

--- modules/api/src/main/Context.scala
+++ modules/api/src/main/Context.scala
@@ -74,7 +74,7 @@ sealed trait Context extends lidraughts.user.UserContextWrapper {
 
   def zoom: Option[Int] = req.session get "zoom" flatMap parseIntOption filter (100<=)
 
-  def respZoom = zoom.fold(80)(_ - 100)
+  def respZoom = zoom.fold(85)(_ - 100)
 }
 
 sealed abstract class BaseContext(
