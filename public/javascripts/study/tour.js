bdbf851be5369d44236d74df8f629911e2a07c08
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-04-18 13:07:58 +0700

    fix study tour for member (not owner)

--- public/javascripts/study/tour.js
+++ public/javascripts/study/tour.js
@@ -44,7 +44,7 @@ lichess.studyTour = function(study) {
       attachTo: ".study__members right",
       when: onTab('members')
     },
-      study.isContrib ? {
+      study.isOwner ? {
         title: "Invite members",
         text: "By clicking the <i data-icon='O'></i> button.<br>" +
         "Then decide who can contribute or not.",
