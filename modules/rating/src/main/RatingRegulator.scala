09d63704d925a0835a467915e590a1d02a51280e
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-04-20 14:14:46 +0700

    monitor regulator extra points (as micropoints)

--- modules/rating/src/main/RatingRegulator.scala
+++ modules/rating/src/main/RatingRegulator.scala
@@ -4,16 +4,17 @@ case object RatingRegulator {
 
   def apply(factors: RatingFactors)(perfType: PerfType, before: Perf, after: Perf): Perf =
     factors.get(perfType).filter(1!=).fold(after) {
-      apply(_, before, after)
+      apply(_, perfType, before, after)
     }
 
-  def apply(factor: RatingFactor, before: Perf, after: Perf): Perf =
+  def apply(factor: RatingFactor, perfType: PerfType, before: Perf, after: Perf): Perf =
     if ({
       (after.nb == before.nb + 1) && // after playing one game
         (after.glicko.rating > before.glicko.rating) // and gaining rating
     }) {
       val diff = (after.glicko.rating - before.glicko.rating).abs
       val extra = diff * factor.value
+      lila.mon.rating.regulator.micropoints(perfType.key)((extra * 1000 * 1000).toLong)
       after.copy(
         glicko = after.glicko.copy(
           rating = after.glicko.rating + extra
