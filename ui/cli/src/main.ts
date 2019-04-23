1e44ee671ff7374f9b7f6f93390d5c1f428d7683
Andrew-9 <andrew@yatzkan.com>
2019-04-22 22:36:48 -0500

    added /commands

--- ui/cli/src/main.ts
+++ ui/cli/src/main.ts
@@ -22,5 +22,6 @@ function command(q: string) {
   var parts = q.split(' '), exec = parts[0];
   if (exec === 'tv' || exec === 'follow') location.href = '/@/' + parts[1] + '/tv';
   else if (exec === 'play' || exec === 'challenge' || exec === 'match') location.href = '/?user=' + parts[1] + '#friend';
+  else if (exec === 'commands') alert('/tv <username>\n/follow <username>\n/play <username>\n/challenge <username>\n/match <username>\n/light\n/dark\n/transp');
   else alert('Unknown command: ' + q);
 }
