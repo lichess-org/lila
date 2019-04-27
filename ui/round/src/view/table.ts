d3b75ef1af83c7f0ccb02470902eed62880c1384
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-04-27 13:43:23 +0700

    finally fix mobile round move list overflow
    Put clocks and players in the same grid cell.
    This allows having a single cell column, instead of
    columns: auto min-content;
    This is required to display the overflowable horizontal move list,
    so that it can be contain within the grid parent.

--- ui/round/src/view/table.ts
+++ ui/round/src/view/table.ts
@@ -103,8 +103,6 @@ function anyClock(ctrl: RoundController, position: Position) {
 
 export function renderTable(ctrl: RoundController): MaybeVNodes {
   return [
-    anyClock(ctrl, 'top'),
-    anyClock(ctrl, 'bottom'),
     h('div.round__app__table'),
     renderExpiration(ctrl),
     renderPlayer(ctrl, 'top'),
@@ -112,5 +110,11 @@ export function renderTable(ctrl: RoundController): MaybeVNodes {
       game.playable(ctrl.data) ? renderTablePlay(ctrl) : renderTableEnd(ctrl)
     )),
     renderPlayer(ctrl, 'bottom'),
+    /* render clocks after players so they display on top of them in col1,
+     * since they occupy the same grid cell. This is required to avoid
+     * having two columns with min-content, which causes the horizontal moves
+     * to overflow: it couldn't be contained in the parent anymore */
+    anyClock(ctrl, 'top'),
+    anyClock(ctrl, 'bottom'),
   ];
 };
