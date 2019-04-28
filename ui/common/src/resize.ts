3aa8ca84ab32beadd3ca25e77f0b4f641a9d74a0
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-04-28 09:00:16 +0700

    use real distance for board resize

--- ui/common/src/resize.ts
+++ ui/common/src/resize.ts
@@ -1,6 +1,9 @@
-export default function resizeHandle(el: HTMLElement) {
-  el.addEventListener('mousedown', start => {
+export type MouchEvent = MouseEvent & TouchEvent;
 
+export default function resizeHandle(el: HTMLElement) {
+  el.addEventListener(window.lichess.mousedownEvent, (start: MouchEvent) => {
+
+    const startPos = eventPosition(start)!;
     const initialZoom = parseInt(getComputedStyle(document.body).getPropertyValue('--zoom'));
     let zoom = initialZoom;
 
@@ -8,11 +11,12 @@ export default function resizeHandle(el: HTMLElement) {
       $.ajax({ method: 'post', url: '/pref/zoom?v=' + (100 + zoom) });
     }, 1000);
 
-    const resize = (move: MouseEvent) => {
+    const resize = (move: MouchEvent) => {
 
-      const delta = move.clientX - start.clientX + move.clientY - start.clientY;
+      const pos = eventPosition(move)!;
+      const distance = Math.pow(pos[0] - startPos[0], 2) + Math.pow(pos[1] - startPos[1], 2);
 
-      zoom = Math.round(Math.min(100, Math.max(0, initialZoom + delta / 10)));
+      zoom = Math.round(Math.min(100, Math.max(0, initialZoom + distance / 8)));
 
       document.body.setAttribute('style', '--zoom:' + zoom);
       window.lichess.dispatchEvent(window, 'resize');
@@ -29,4 +33,10 @@ export default function resizeHandle(el: HTMLElement) {
       document.body.classList.remove('resizing');
     }, { once: true });
   });
+}
+
+function eventPosition(e: MouchEvent): [number, number] | undefined {
+  if (e.clientX || e.clientX === 0) return [e.clientX, e.clientY];
+  if (e.touches && e.targetTouches[0]) return [e.targetTouches[0].clientX, e.targetTouches[0].clientY];
+  return undefined;
 }
