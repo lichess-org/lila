96d81313d956bb364faa40866e8d78635f9683b0
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-04-29 09:07:58 +0200

    fix typo in faq

--- app/views/site/faq.scala
+++ app/views/site/faq.scala
@@ -164,7 +164,7 @@ object faq {
           "high-ratings",
           "Why are ratings higher compared to other sites and organisations such as FIDE, USCF and the ICC?",
           p("It is best not to think of ratings as absolute numbers, or compare them against other organisations. Different organisations have different levels of players, different rating systems (Elo, Glicko, Glicko-2, or a modified version of the aforementioned). These factors can drastically affect the absolute numbers (ratings)."),
-          p("""It's best to think of ratings as "relative" figures (as opposed to "absolute" figures): Within a pool of players, their relative differences in ratings will help you estimate who will win/draw/loss, and how often. Saying "I have X rating" means nothing unless there are other players to compare that rating to.""")
+          p("""It's best to think of ratings as "relative" figures (as opposed to "absolute" figures): Within a pool of players, their relative differences in ratings will help you estimate who will win/draw/lose, and how often. Saying "I have X rating" means nothing unless there are other players to compare that rating to.""")
         ),
         question(
           "hide-ratings",
