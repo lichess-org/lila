008cad3c98256848370d12a10fde5b73d37e9ed0
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-04-29 08:42:43 +0700

    reset dasher on toggle

--- ui/dasher/src/dasher.ts
+++ ui/dasher/src/dasher.ts
@@ -75,6 +75,8 @@ export function makeCtrl(opts: DasherOpts, data: DasherData, redraw: Redraw): Da
     piece: pieceCtrl(data.piece, trans, () => data.board.is3d ? 'd3' : 'd2', redraw, setMode)
   };
 
+  window.lidraughts.pubsub.on('top.toggle.user_tag', () => setMode(defaultMode));
+
   return {
     mode,
     setMode,
