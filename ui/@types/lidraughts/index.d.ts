3551ef78d1e5eef82081fad13afff2766f9e7489
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-05-02 09:56:10 +0200

    fix storage types

--- ui/@types/lidraughts/index.d.ts
+++ ui/@types/lidraughts/index.d.ts
@@ -98,14 +98,14 @@ interface Pubsub {
 interface LidraughtsStorageHelper {
   make(k: string): LidraughtsStorage;
   makeBoolean(k: string): LidraughtsBooleanStorage;
-  get(k: string): string;
-  set(k: string, v: string): string;
+  get(k: string): string | null;
+  set(k: string, v: string): void;
   remove(k: string): void;
 }
 
 interface LidraughtsStorage {
-  get(): string;
-  set(v: string): string;
+  get(): string | null;
+  set(v: string): void;
   remove(): void;
   listen(f: (e: StorageEvent) => void): void;
 }
