ccf5fa3e796b4d8e5d5ee1c2060e084fee40ec5f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-05-02 09:15:42 +0200

    multiplex web push requests

--- modules/push/src/main/WebPush.scala
+++ modules/push/src/main/WebPush.scala
@@ -14,30 +14,28 @@ private final class WebPush(
 
   def apply(userId: User.ID)(data: => PushApi.Data): Funit =
     getSubscriptions(userId) flatMap { subscriptions: List[WebSubscription] =>
-      subscriptions.map(send(_, data)).sequenceFu.void
+      WS.url(url)
+        .withHeaders("ContentType" -> "application/json")
+        .post(Json.obj(
+          "subs" -> JsArray(subscriptions.map { sub =>
+            Json.obj(
+              "endpoint" -> sub.endpoint,
+              "keys" -> Json.obj(
+                "p256dh" -> sub.p256dh,
+                "auth" -> sub.auth
+              )
+            )
+          }),
+          "payload" -> Json.obj(
+            "title" -> data.title,
+            "body" -> data.body,
+            "stacking" -> data.stacking.key,
+            "payload" -> data.payload
+          ).toString,
+          "ttl" -> 43200
+        ))
+    } flatMap {
+      case res if res.status == 200 => funit
+      case res => fufail(s"[push] web: ${res.status} ${res.body}")
     }
-
-  private def send(sub: WebSubscription, data: PushApi.Data): Funit = {
-    WS.url(url)
-      .withHeaders("ContentType" -> "application/json")
-      .post(Json.obj(
-        "sub" -> Json.obj(
-          "endpoint" -> sub.endpoint,
-          "keys" -> Json.obj(
-            "p256dh" -> sub.p256dh,
-            "auth" -> sub.auth
-          )
-        ),
-        "payload" -> Json.obj(
-          "title" -> data.title,
-          "body" -> data.body,
-          "stacking" -> data.stacking.key,
-          "payload" -> data.payload
-        ).toString,
-        "ttl" -> 43200
-      )).flatMap {
-        case res if res.status == 204 => funit
-        case res => fufail(s"[push] web: ${res.status} ${res.body}")
-      }
-  }
 }
