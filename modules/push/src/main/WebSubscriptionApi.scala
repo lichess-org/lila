60868a2a746ccf215a3957c191be3907263d6594
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-05-02 10:29:00 +0200

    store and order by subscription timestamp

--- modules/push/src/main/WebSubscriptionApi.scala
+++ modules/push/src/main/WebSubscriptionApi.scala
@@ -1,5 +1,7 @@
 package lila.push
 
+import org.joda.time.DateTime
+
 import reactivemongo.bson._
 
 import lila.db.dsl._
@@ -7,11 +9,11 @@ import lila.user.User
 
 private final class WebSubscriptionApi(coll: Coll) {
 
-  def getSubscriptions(userId: User.ID): Fu[List[WebSubscription]] =
+  def getSubscriptions(max: Int)(userId: User.ID): Fu[List[WebSubscription]] =
     // TODO: This query needs an index.
     coll.find($doc(
       "userId" -> userId
-    )).list[Bdoc](3).map { docs =>
+    )).sort($doc("seenAt" -> -1)).list[Bdoc](max).map { docs =>
       docs.flatMap { doc =>
         for {
           endpoint <- doc.getAs[String]("_id")
@@ -26,7 +28,8 @@ private final class WebSubscriptionApi(coll: Coll) {
       "_id" -> subscription.endpoint,
       "userId" -> user.id,
       "auth" -> subscription.auth,
-      "p256dh" -> subscription.p256dh
+      "p256dh" -> subscription.p256dh,
+      "seenAt" -> DateTime.now
     ), upsert = true).void
   }
 
