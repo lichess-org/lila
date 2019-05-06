c57fd2acf858ecfbb1b71c1bd0436426c44acdcc
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-05-06 08:38:18 +0700

    Firefox 63- doesn't support @media hover

--- ui/site/src/standalones/util.js
+++ ui/site/src/standalones/util.js
@@ -210,7 +210,7 @@ lichess.widget = function(name, prototype) {
 };
 lichess.isHoverable = function () {
   if (typeof lichess.hoverable === 'undefined')
-    lichess.hoverable = !!getComputedStyle(document.body).getPropertyValue('--hoverable');
+    lichess.hoverable = !lichess.hasTouchEvents || !!getComputedStyle(document.body).getPropertyValue('--hoverable');
   return lichess.hoverable;
 };
 lichess.spinnerHtml = '<div class="spinner"><svg viewBox="0 0 40 40"><circle cx=20 cy=20 r=18 fill="none"></circle></svg></div>';
