2ae38f2eec4300c806877bd8e3191fad52a9e0c3
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-05-07 14:10:52 +0700

    fix mod inquiry bar auto-next

--- public/javascripts/inquiry.js
+++ public/javascripts/inquiry.js
@@ -1,26 +1,21 @@
-window.requestIdleCallback(function() {
-  $(function() {
-    $('#inquiry .notes').on('mouseenter', function() {
-      $(this).find('textarea')[0].focus();
-    });
-    $('#inquiry .costello').click(function() {
-      $('#inquiry').toggleClass('hidden');
-      $('body').toggleClass('no-inquiry');
-    });
+$(function() {
+  $('#inquiry .notes').on('mouseenter', function() {
+    $(this).find('textarea')[0].focus();
+  });
+  $('#inquiry .costello').click(function() {
+    $('#inquiry').toggleClass('hidden');
+    $('body').toggleClass('no-inquiry');
+  });
 
-    var nextStore = lichess.storage.makeBoolean('inquiry-auto-next');
-    var next = function() {
-      return nextStore.get();
-    };
+  var nextStore = lichess.storage.makeBoolean('inquiry-auto-next');
 
-    if (!next()) {
-      $('#inquiry .switcher input').attr('checked', false);
-      $('#inquiry input.auto-next').val('0');
-    }
+  if (!nextStore.get()) {
+    $('#inquiry .switcher input').attr('checked', false);
+    $('#inquiry input.auto-next').val('0');
+  }
 
-    $('#inquiry .switcher input').on('change', function() {
-      nextStore.set(next());
-      $('#inquiry input.auto-next').val(next() ? '1' : '0');
-    });
+  $('#inquiry .switcher input').on('change', function() {
+    nextStore.set(this.checked);
+    $('#inquiry input.auto-next').val(this.checked ? '1' : '0');
   });
 });
