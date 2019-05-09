4da6861a40aba0ba9d1f06f6b2f488676ed1ed4d
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-05-10 05:10:02 +0700

    fix mongo script

--- bin/mongodb/patron-since.js
+++ bin/mongodb/patron-since.js
@@ -1,4 +1,4 @@
-var u = 'neio';
+var u = 'neio'.toLowerCase();
 var since = new Date('2019-01-17');
 
 function monthDiff(dateFrom, dateTo) {
@@ -6,4 +6,4 @@ function monthDiff(dateFrom, dateTo) {
    (12 * (dateTo.getFullYear() - dateFrom.getFullYear()))
 }
 
-db.user4.update({_id: u}, {$set: { 'plan.months': monthDiff(since, new Date()) + 1, 'plan.since': since}});
+db.user4.update({_id: u}, {$set: { 'plan.months': NumberInt(monthDiff(since, new Date()) + 1), 'plan.since': since}});
