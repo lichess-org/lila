115679212928fd0933f4faa81db1e368c5c664d1
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-05-10 21:26:39 +0700

    remove unused CSS class

--- ui/editor/src/view.js
+++ ui/editor/src/view.js
@@ -210,7 +210,7 @@ function sparePieces(ctrl, color, orientation, position) {
   });
 
   return m('div', {
-    class: ['spare', 'spare-' + position, 'orientation-' + orientation, 'spare-' + color].join(' ')
+    class: ['spare', 'spare-' + position, 'spare-' + color].join(' ')
   }, ['pointer'].concat(pieces).concat('trash').map(function(s) {
 
     var className = selectedToClass(s);
