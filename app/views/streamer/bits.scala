e18b1ba188f38bf9a8814270b2d40c26ab1e5a3a
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-05-15 14:35:16 +0700

    better streamer menu for mods

--- app/views/streamer/bits.scala
+++ app/views/streamer/bits.scala
@@ -51,7 +51,8 @@ object bits {
           (ctx.is(st.user) || isGranted(_.Streamers)) option
             a(cls := active.active("edit"), href := s"${routes.Streamer.edit}?u=${st.streamer.id.value}")("Edit streamer page")
         )
-      } getOrElse { isGranted(_.Beta) option a(href := routes.Streamer.edit)("Your streamer page") }
+      } getOrElse { isGranted(_.Beta) option a(href := routes.Streamer.edit)("Your streamer page") },
+      isGranted(_.Streamers) option a(cls := active.active("requests"), href := s"${routes.Streamer.index()}?requests=1")("Approval requests")
     /*a(dataIcon := "", cls := "text", href := "/blog/Wk5z0R8AACMf6ZwN/join-the-lichess-streamer-community")("Streamer community"),
       a(href := "/about")("Download streamer kit")*/
     )
