7fa52a000e143a9dae29333194d686ea8760b188
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-05-18 16:36:35 +0200

    dont hit network without subscriptions

--- modules/push/src/main/WebPush.scala
+++ modules/push/src/main/WebPush.scala
@@ -1,5 +1,7 @@
 package lila.push
 
+import scalaz.NonEmptyList
+
 import play.api.libs.json._
 import play.api.libs.ws.WS
 import play.api.Play.current
@@ -13,29 +15,33 @@ private final class WebPush(
 ) {
 
   def apply(userId: User.ID)(data: => PushApi.Data): Funit =
-    getSubscriptions(userId) flatMap { subscriptions: List[WebSubscription] =>
-      WS.url(url)
-        .withHeaders("ContentType" -> "application/json")
-        .post(Json.obj(
-          "subs" -> JsArray(subscriptions.map { sub =>
-            Json.obj(
-              "endpoint" -> sub.endpoint,
-              "keys" -> Json.obj(
-                "p256dh" -> sub.p256dh,
-                "auth" -> sub.auth
-              )
-            )
-          }),
-          "payload" -> Json.obj(
-            "title" -> data.title,
-            "body" -> data.body,
-            "stacking" -> data.stacking.key,
-            "payload" -> data.payload
-          ).toString,
-          "ttl" -> 43200
-        ))
-    } flatMap {
-      case res if res.status == 200 => funit
-      case res => fufail(s"[push] web: ${res.status} ${res.body}")
+    getSubscriptions(userId) flatMap { subscriptions =>
+      subscriptions.toNel ?? send(data)
     }
+
+  private def send(data: => PushApi.Data)(subscriptions: NonEmptyList[WebSubscription]): Funit = {
+    WS.url(url)
+      .withHeaders("ContentType" -> "application/json")
+      .post(Json.obj(
+        "subs" -> JsArray(subscriptions.map { sub =>
+          Json.obj(
+            "endpoint" -> sub.endpoint,
+            "keys" -> Json.obj(
+              "p256dh" -> sub.p256dh,
+              "auth" -> sub.auth
+            )
+          )
+        }.toList),
+        "payload" -> Json.obj(
+          "title" -> data.title,
+          "body" -> data.body,
+          "stacking" -> data.stacking.key,
+          "payload" -> data.payload
+        ).toString,
+        "ttl" -> 43200
+      )) flatMap {
+        case res if res.status == 200 => funit
+        case res => fufail(s"[push] web: ${res.status} ${res.body}")
+      }
+  }
 }
