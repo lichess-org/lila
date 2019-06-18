baa4355c5ee196a34d1a4a3b3c976170c858aeb6
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-06-18 13:43:22 +0200

    ensure a member only quits once
    after a member is ejected, ignore its connection close event.
    
    Before patch, the following sequence of events was possible:
    - client connects to game
    - client loses Internet
    - client recovers Internet
    - client establishes new connection with same SRI
    - client notifies end of the initial connection (from before network split)
    - since both connections share SRI, the new one is unregistered
    
    this patch ensures that an ejected connection can't close a new one.

--- modules/socket/src/main/SocketMember.scala
+++ modules/socket/src/main/SocketMember.scala
@@ -7,9 +7,14 @@ trait SocketMember {
   protected val channel: JsChannel
   val userId: Option[String]
 
+  var ended = false
+
   def isAuth = userId.isDefined
 
   def push(msg: JsValue) = channel push msg
 
-  def end = channel.end
+  def end = {
+    ended = true
+    channel.end
+  }
 }
