025cb8a675b8f507273683a2aefdf76390c43216
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-06-28 11:37:04 -0400

    make user action buttons accessible

--- app/views/relation/actions.scala
+++ app/views/relation/actions.scala
@@ -22,13 +22,13 @@ object actions {
         (myId != userId) ?? frag(
           !blocked option frag(
             a(
-              title := trans.challengeToPlay.txt(),
+              ariaTitle(trans.challengeToPlay.txt()),
               href := s"${routes.Lobby.home()}?user=$userId#friend",
               cls := "btn-rack__btn",
               dataIcon := "U"
             ),
             a(
-              title := trans.composeMessage.txt(),
+              ariaTitle(trans.composeMessage.txt()),
               href := s"${routes.Message.form()}?user=$userId",
               cls := "btn-rack__btn",
               dataIcon := "c"
@@ -39,13 +39,13 @@ object actions {
               followable && !blocked option a(
                 cls := "btn-rack__btn relation-button",
                 href := routes.Relation.follow(userId),
-                title := trans.follow.txt(),
+                ariaTitle(trans.follow.txt()),
                 dataIcon := "h"
               ),
               a(
                 cls := "btn-rack__btn relation-button",
                 href := routes.Relation.block(userId),
-                title := trans.block.txt(),
+                ariaTitle(trans.block.txt()),
                 dataIcon := "k"
               )
             )
@@ -53,14 +53,14 @@ object actions {
               dataIcon := "h",
               cls := "btn-rack__btn relation-button text hover-text",
               href := routes.Relation.unfollow(userId),
-              st.title := trans.following.txt(),
+              ariaTitle(trans.following.txt()),
               dataHoverText := trans.unfollow.txt()
             )
             case Some(false) => a(
               dataIcon := "k",
               cls := "btn-rack__btn relation-button text hover-text",
               href := routes.Relation.unblock(userId),
-              st.title := trans.blocked.txt(),
+              ariaTitle(trans.blocked.txt()),
               dataHoverText := trans.unblock.txt()
             )
           }
