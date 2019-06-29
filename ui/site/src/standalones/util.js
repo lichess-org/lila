306d0524d33476a6d44bfa639024dee642f7305a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-06-29 14:28:07 +0200

    Revert "HACKFIX try to work around broken locales"
    This reverts commit 5dc1e949b366c6ea8593a850594e866ade85d4ca.

--- ui/site/src/standalones/util.js
+++ ui/site/src/standalones/util.js
@@ -1,11 +1,5 @@
 lichess = window.lichess || {};
 
-lichess.quantity = lichess.quantity || function(c) {
-  return (c == 1) ? 'one' : 'other';
-};
-
-lichess.timeagoLocale = lichess.timeagoLocale || function(s,n){return[["just now","right now"],["%s seconds ago","in %s seconds"],["1 minute ago","in 1 minute"],["%s minutes ago","in %s minutes"],["1 hour ago","in 1 hour"],["%s hours ago","in %s hours"],["1 day ago","in 1 day"],["%s days ago","in %s days"],["1 week ago","in 1 week"],["%s weeks ago","in %s weeks"],["1 month ago","in 1 month"],["%s months ago","in %s months"],["1 year ago","in 1 year"],["%s years ago","in %s years"]][n]};
-
 lichess.engineName = 'Stockfish 10+';
 
 lichess.raf = window.requestAnimationFrame.bind(window);
