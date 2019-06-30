b8246af124167c8be1a273f0b20946425182a2b1
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-06-30 17:42:40 -0400

    Add a runtime setting to toggle the new rating deviation logic

--- modules/rating/src/main/Env.scala
+++ modules/rating/src/main/Env.scala
@@ -9,6 +9,12 @@ final class Env(settingStore: lidraughts.memo.SettingStore.Builder) {
     default = Map.empty,
     text = "Rating gain factor per perf type".some
   )
+
+  val deviationIncreaseOverTimeSetting = settingStore[Boolean](
+    "deviationIncreaseOverTime",
+    default = true,
+    text = "Increase rating deviation over time instead of after every game".some
+  )
 }
 
 object Env {
--- modules/round/src/main/PerfsUpdater.scala
+++ modules/round/src/main/PerfsUpdater.scala
@@ -108,7 +108,7 @@ final class PerfsUpdater(
       case Glicko.Result.Loss => results.addResult(black, white)
     }
     try {
-      Glicko.system.updateRatings(results, true)
+      Glicko.system.updateRatings(results, lidraughts.rating.Env.current.deviationIncreaseOverTimeSetting.get)
     } catch {
       case e: Exception => logger.error("update ratings", e)
     }
