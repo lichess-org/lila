9f923c00d8e9d5460d6147014452ac3b0837893b
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-07-05 15:53:22 -0400

    space encoding for remote socket format

--- modules/socket/src/main/RemoteSocket.scala
+++ modules/socket/src/main/RemoteSocket.scala
@@ -23,15 +23,15 @@ private final class RemoteSocket(
     val Disconnect = "disconnect"
     val Watch = "watch"
     val Notified = "notified"
-    val Count = "count"
+    val Connections = "connections"
   }
   private object Out {
     val Move = "move"
     val TellUser = "tell/user"
     val TellUsers = "tell/users"
     val TellAll = "tell/all"
+    val TellFlag = "tell/flag"
     val Mlat = "mlat"
-    val Flag = "flag"
   }
 
   private val clientIn = makeRedis()
@@ -41,75 +41,55 @@ private final class RemoteSocket(
   private val watchedGameIds = collection.mutable.Set.empty[String]
 
   bus.subscribeFun('moveEvent, 'finishGameId, 'socketUsers, 'deploy, 'announce, 'mlat, 'sendToFlag) {
-    case MoveEvent(gameId, fen, move) if watchedGameIds(gameId) => send(Out.Move, Json.obj(
-      "gameId" -> gameId,
-      "fen" -> fen,
-      "move" -> move
-    ))
-    case FinishGameId(gameId) if watchedGameIds(gameId) => watchedGameIds -= gameId
+    case MoveEvent(gameId, fen, move) if watchedGameIds(gameId) =>
+      send(Out.Move, gameId, move, fen)
+    case FinishGameId(gameId) if watchedGameIds(gameId) =>
+      watchedGameIds -= gameId
     case SendTos(userIds, payload) =>
       val connectedUsers = userIds intersect connectedUserIds
-      if (connectedUsers.nonEmpty) send(Out.TellUsers, Json.obj(
-        "users" -> connectedUsers,
-        "payload" -> payload
-      ))
-    case SendTo(userId, payload) if connectedUserIds(userId) => send(Out.TellUser, Json.obj(
-      "user" -> userId,
-      "payload" -> payload
-    ))
-    case d: Deploy => send(Out.TellAll, Json.obj(
-      "payload" -> Json.obj("t" -> d.key)
-    ))
-    case Announce(msg) => send(Out.TellAll, Json.obj(
-      "payload" -> Json.obj(
-        "t" -> "announce",
-        "d" -> Json.obj("msg" -> msg)
-      )
-    ))
-    case Mlat(ms) => send(Out.Mlat, Json.obj("value" -> ms))
-    case WithUserIds(f) => f(connectedUserIds)
-    case actorApi.SendToFlag(flag, payload) => send(Out.Flag, Json.obj(
-      "flag" -> flag,
-      "payload" -> payload
-    ))
+      if (connectedUsers.nonEmpty) send(Out.TellUsers, connectedUsers mkString ",", Json stringify payload)
+    case SendTo(userId, payload) if connectedUserIds(userId) =>
+      send(Out.TellUser, userId, Json stringify payload)
+    case d: Deploy =>
+      send(Out.TellAll, Json stringify Json.obj("t" -> d.key))
+    case Announce(msg) =>
+      send(Out.TellAll, Json stringify Json.obj("t" -> "announce", "d" -> Json.obj("msg" -> msg)))
+    case Mlat(ms) =>
+      send(Out.Mlat, ms.toString)
+    case actorApi.SendToFlag(flag, payload) =>
+      send(Out.TellFlag, flag, Json stringify payload)
+    case WithUserIds(f) =>
+      f(connectedUserIds)
   }
 
-  private def onReceive(path: String, data: JsObject) = path match {
-    case In.Connect => data str "user" foreach { userId =>
+  private def onReceive(path: String, args: String) = path match {
+    case In.Connect =>
+      val userId = args
       connectedUserIds += userId
       bus.publish(lila.hub.actorApi.relation.ReloadOnlineFriends(userId), 'reloadOnlineFriends)
-    }
-    case In.Disconnect => data str "user" foreach { userId =>
-      connectedUserIds -= userId
-    }
-    case In.Watch => data str "game" foreach { gameId =>
+    case In.Disconnect =>
+      val userId = args
+      connectedUserIds -= args
+    case In.Watch =>
+      val gameId = args
       watchedGameIds += gameId
-    }
-    case In.Notified => data str "user" foreach { userId =>
+    case In.Notified =>
+      val userId = args
       notificationActor ! lila.hub.actorApi.notify.Notified(userId)
-    }
-    case In.Count => data int "value" foreach setNb
-    case path => logger.warn(s"Invalid path $path")
+    case In.Connections =>
+      parseIntOption(args) foreach setNb
+    case path =>
+      logger.warn(s"Invalid path $path")
   }
 
-  private def send(path: String, data: JsObject) = clientOut.publish(
-    chanOut,
-    Json stringify {
-      Json.obj("path" -> path) ++ data
-    }
-  )
+  private def send(path: String, args: String*) =
+    clientOut.publish(chanOut, s"$path ${args mkString " "}")
 
   Future {
     clientIn.subscribe(new JedisPubSub() {
       override def onMessage(channel: String, message: String): Unit = {
-        try {
-          Json.parse(message) match {
-            case o: JsObject => o str "path" foreach { onReceive(_, o) }
-            case _ => logger warn s"Invalid message $message"
-          }
-        } catch {
-          case _: Exception => logger.warn(s"Can't parse remote socket message $message")
-        }
+        val parts = message.split(" ", 2)
+        onReceive(parts(0), ~parts.lift(1))
       }
     }, chanIn)
   }
