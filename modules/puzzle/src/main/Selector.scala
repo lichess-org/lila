79da831b5c8ec184be886d9637f16a798e5b50f0
Stefan Schermann <stefan.schermann@gmail.com>
2019-07-06 13:35:02 +0200

    New puzzle when selected nonexistent

--- modules/puzzle/src/main/Selector.scala
+++ modules/puzzle/src/main/Selector.scala
@@ -18,20 +18,27 @@ private[puzzle] final class Selector(
   def apply(me: Option[User], variant: Variant): Fu[Puzzle] = {
     lidraughts.mon.puzzle.selector.count()
     me match {
-      case None =>
-        puzzleColl(variant) // this query precisely matches a mongodb partial index
-          .find($doc(F.voteNb $gte 1)) //original 50
-          .sort($sort desc F.voteRatio)
-          .skip(Random nextInt anonSkipMax(variant))
-          .uno[Puzzle]
-      case Some(user) =>
-        api.head.find(user, variant) flatMap {
-          case Some(PuzzleHead(_, Some(c), _)) => api.puzzle.find(c, variant)
-          case headOption =>
-            newPuzzleForUser(user, variant, headOption) flatMap { next =>
-              next.?? { p => api.head.addNew(user, p.id, variant) } inject next
-            }
-        }
+      case None => anonPuzzle(variant) flatMap {
+        case p @ Some(puzzle) => fuccess(p)
+        case _ =>
+          logger.warn(s"Retry ${variant.key} puzzle for anonymous")
+          anonPuzzle(variant)
+      }
+      case Some(user) => api.head.find(user, variant) flatMap {
+        case head @ Some(PuzzleHead(_, Some(c), _)) =>
+          api.puzzle.find(c, variant) flatMap {
+            case p @ Some(puzzle) => fuccess(p)
+            case _ =>
+              logger.warn(s"Retry ${variant.key} puzzle $c for ${user.username}")
+              newPuzzleForUser(user, variant, head) flatMap { next =>
+                next.?? { p => api.head.addNew(user, p.id, variant) } inject next
+              }
+          }
+        case headOption =>
+          newPuzzleForUser(user, variant, headOption) flatMap { next =>
+            next.?? { p => api.head.addNew(user, p.id, variant) } inject next
+          }
+      }
     }
   }.mon(_.puzzle.selector.time) flatten "No puzzles available" addEffect { puzzle =>
     if (puzzle.vote.sum < -1000)
@@ -39,6 +46,13 @@ private[puzzle] final class Selector(
     else
       lidraughts.mon.puzzle.selector.vote(puzzle.vote.sum)
   }
+
+  private def anonPuzzle(variant: Variant) =
+    puzzleColl(variant) // FIXME: this query precisely matches a mongodb partial index
+      .find($doc(F.voteNb $gte 1)) //original 50
+      .sort($sort desc F.voteRatio)
+      .skip(Random nextInt anonSkipMax(variant))
+      .uno[Puzzle]
 
   private def newPuzzleForUser(user: User, variant: Variant, headOption: Option[PuzzleHead]): Fu[Option[Puzzle]] = {
     val rating = user.perfs.puzzle(variant).intRating min 2300 max 900
