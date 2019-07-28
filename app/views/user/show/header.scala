b84c277f553ba233d4dd29b126f918b46069c7fc
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-07-28 15:01:49 +0200

    fix nofollow attribute

--- app/views/user/show/header.scala
+++ app/views/user/show/header.scala
@@ -194,7 +194,7 @@ It's useful against spambots. These marks are not visible to the public."""
                 },
                 div(cls := "social_links col2")(
                   profile.actualLinks.map { link =>
-                    a(href := link.url, target := "_blank", rel := "no-follow")(link.site.name)
+                    a(href := link.url, target := "_blank", rel := "nofollow")(link.site.name)
                   }
                 ),
                 div(cls := "teams col2")(
