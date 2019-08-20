5d442011062a00b69c6626db17314ebcd2bb6668
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-08-20 10:18:18 +0200

    get next game from round proxy

--- app/controllers/Round.scala
+++ app/controllers/Round.scala
@@ -125,7 +125,7 @@ object Round extends LilaController with TheftPrevention {
   }
 
   def next(gameId: String) = Auth { implicit ctx => me =>
-    OptionFuResult(GameRepo game gameId) { currentGame =>
+    OptionFuResult(env.proxy game gameId) { currentGame =>
       otherPovs(currentGame) map getNext(currentGame) map {
         _ orElse Pov(currentGame, me)
       } flatMap {
