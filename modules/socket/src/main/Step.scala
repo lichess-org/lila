fc7a3ac6c68b41958d0dcda94dd370ffb08d7c6c
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-08-21 07:47:55 +0200

    tweaks

--- modules/socket/src/main/Step.scala
+++ modules/socket/src/main/Step.scala
@@ -50,29 +50,21 @@ object Step {
 
   implicit val stepJsonWriter: Writes[Step] = Writes { step =>
     import step._
-    (
-      add("check", true, check) _ compose
-      add("dests", dests.map {
+    Json.obj(
+      "ply" -> ply,
+      "uci" -> move.map(_.uciString),
+      "san" -> move.map(_.san),
+      "fen" -> fen
+    )
+      .add("check", check)
+      .add("dests", dests.map {
         _.map {
           case (orig, dests) => s"${orig.piotr}${dests.map(_.piotr).mkString}"
         }.mkString(" ")
-      }) _ compose
-      add("drops", drops.map { drops =>
+      })
+      .add("drops", drops.map { drops =>
         JsString(drops.map(_.key).mkString)
-      }) _ compose
-      add("crazy", crazyData)
-    )(Json.obj(
-        "ply" -> ply,
-        "uci" -> move.map(_.uciString),
-        "san" -> move.map(_.san),
-        "fen" -> fen
-      ))
+      })
+      .add("crazy", crazyData)
   }
-
-  private def add[A](k: String, v: A, cond: Boolean)(o: JsObject)(implicit writes: Writes[A]): JsObject =
-    if (cond) o + (k -> writes.writes(v)) else o
-
-  private def add[A: Writes](k: String, v: Option[A]): JsObject => JsObject =
-    v.fold(identity[JsObject] _) { add(k, _, true) _ }
 }
-
