72862092e487d098c7239c8e711994271bf834c2
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-08-23 13:23:37 +0200

    commit app/views/study/jsI18n.scala (forgotten earlier)

---
+++ app/views/study/jsI18n.scala
@@ -0,0 +1,23 @@
+package views.html.study
+
+import lidraughts.app.templating.Environment._
+import lidraughts.common.Lang
+import lidraughts.i18n.{ I18nKeys => trans }
+
+object jsI18n {
+
+  def apply()(implicit lang: Lang) =
+    views.html.board.userAnalysisI18n(withAdvantageChart = true) ++
+      i18nFullDbJsObject(lidraughts.i18n.I18nDb.Study) ++
+      i18nJsObject(translations)
+
+  private val translations = Vector(
+    trans.name,
+    trans.white,
+    trans.black,
+    trans.variant,
+    trans.none,
+    trans.clearBoard,
+    trans.startPosition
+  )
+}
