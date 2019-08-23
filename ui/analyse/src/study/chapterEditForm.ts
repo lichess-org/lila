553efa6bbeb33c0c7cb1a7079729acdeefac2322
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-08-23 09:43:48 +0200

    add types for study chapterEditForm

--- ui/analyse/src/study/chapterEditForm.ts
+++ ui/analyse/src/study/chapterEditForm.ts
@@ -1,22 +1,34 @@
 import { h } from 'snabbdom'
 import { VNode } from 'snabbdom/vnode'
-import { prop } from 'common';
+import { defined, prop, Prop } from 'common';
+import { Redraw } from '../interfaces';
 import { bind, bindSubmit, spinner, option, onInsert, emptyRedButton } from '../util';
 import * as modal from '../modal';
 import * as chapterForm from './chapterNewForm';
-import { StudyChapterMeta } from './interfaces';
+import { StudyChapterConfig, StudyChapterMeta } from './interfaces';
 
-export function ctrl(send: SocketSend, chapterConfig, redraw: () => void) {
+interface StudyChapterEditFormCtrl {
+  current: Prop<StudyChapterMeta | StudyChapterConfig | null>;
+  open(data: StudyChapterMeta): void;
+  toggle(data: StudyChapterMeta): void;
+  submit(data: StudyChapterConfig): void;
+  delete(id: string): void;
+  clearAnnotations(id: string): void;
+  isEditing(id: string): boolean;
+  redraw: Redraw;
+}
 
-  const current = prop<StudyChapterMeta | null>(null);
+export function ctrl(send: SocketSend, chapterConfig: (string) => JQueryPromise<StudyChapterConfig>, redraw: Redraw): StudyChapterEditFormCtrl {
 
-  function open(data) {
+  const current = prop<StudyChapterMeta | StudyChapterConfig | null>(null);
+
+  function open(data: StudyChapterMeta) {
     current({
       id: data.id,
       name: data.name
     });
     chapterConfig(data.id).then(d => {
-      current(d);
+      current(d!);
       redraw();
     });
   };
@@ -55,15 +67,9 @@ export function ctrl(send: SocketSend, chapterConfig, redraw: () => void) {
   }
 }
 
-export function view(ctrl): VNode | undefined {
-
+export function view(ctrl: StudyChapterEditFormCtrl): VNode | undefined {
   const data = ctrl.current();
-  if (!data) return;
-
-  const isLoaded = !!data.orientation;
-  const mode = data.practice ? 'practice' : (!isNaN(data.conceal) ? 'conceal' : (data.gamebook ? 'gamebook' : 'normal'));
-
-  return modal.modal({
+  return data ? modal.modal({
     class: 'edit-' + data.id, // full redraw when changing chapter
     onClose() {
       ctrl.current(null);
@@ -97,32 +103,9 @@ export function view(ctrl): VNode | undefined {
               }
             })
           })
-        ])
-      ].concat(
-        isLoaded ? [
-          h('div.form-split', [
-            h('div.form-group.form-half', [
-              h('label.form-label', {
-                attrs: { for: 'chapter-orientation' }
-              }, 'Orientation'),
-              h('select#chapter-orientation.form-control', ['White', 'Black'].map(function(color) {
-                const v = color.toLowerCase();
-                return option(v, data.orientation, color);
-              }))
-            ]),
-            h('div.form-group.form-half', [
-              h('label.form-label', {
-                attrs: { for: 'chapter-mode' }
-              }, 'Analysis mode'),
-              h('select#chapter-mode.form-control', chapterForm.modeChoices.map(c => {
-                return option(c[0], mode, c[1]);
-              }))
-            ])
-          ]),
-          chapterForm.descriptionGroup(data.description),
-          modal.button('Save chapter')
-        ] : [spinner()]
-      )),
+        ]),
+        ...(isLoaded(data) ? viewLoaded(data) : [spinner()])
+      ]),
       h('div.destructive', [
         h(emptyRedButton, {
           hook: bind('click', _ => {
@@ -138,5 +121,36 @@ export function view(ctrl): VNode | undefined {
         }, 'Delete chapter')
       ])
     ]
-  });
+  }) : undefined;
+}
+
+function isLoaded(data: StudyChapterMeta | StudyChapterConfig): data is StudyChapterConfig {
+  return !!data['orientation'];
+}
+
+function viewLoaded(data: StudyChapterConfig): VNode[] {
+  const mode = data.practice ? 'practice' : (defined(data.conceal) ? 'conceal' : (data.gamebook ? 'gamebook' : 'normal'));
+  return [
+    h('div.form-split', [
+      h('div.form-group.form-half', [
+        h('label.form-label', {
+          attrs: { for: 'chapter-orientation' }
+        }, 'Orientation'),
+        h('select#chapter-orientation.form-control', ['White', 'Black'].map(function(color) {
+          const v = color.toLowerCase();
+          return option(v, data.orientation, color);
+        }))
+      ]),
+      h('div.form-group.form-half', [
+        h('label.form-label', {
+          attrs: { for: 'chapter-mode' }
+        }, 'Analysis mode'),
+        h('select#chapter-mode.form-control', chapterForm.modeChoices.map(c => {
+          return option(c[0], mode, c[1]);
+        }))
+      ])
+    ]),
+    chapterForm.descriptionGroup(data.description),
+    modal.button('Save chapter')
+  ];
 }
