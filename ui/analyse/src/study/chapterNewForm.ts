54bfe5d15c04fed603156c57d23cda93c637cd15
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-08-23 11:00:16 +0200

    i18n: study new chapter

--- ui/analyse/src/study/chapterNewForm.ts
+++ ui/analyse/src/study/chapterNewForm.ts
@@ -106,6 +106,7 @@ export function ctrl(send: SocketSend, chapters: Prop<StudyChapterMeta[]>, setTa
 
 export function view(ctrl: StudyChapterNewFormCtrl): VNode {
 
+  const trans = ctrl.root.trans;
   const activeTab = ctrl.vm.tab();
   const makeTab = function(key: string, name: string, title: string) {
     return h('span.' + key, {
@@ -126,7 +127,7 @@ export function view(ctrl: StudyChapterNewFormCtrl): VNode {
     },
     content: [
       activeTab === 'edit' ? null : h('h2', [
-        'New chapter',
+        trans.noarg('newChapter'),
         h('i.help', {
           attrs: { 'data-icon': '' },
           hook: bind('click', ctrl.startTour)
@@ -146,123 +147,122 @@ export function view(ctrl: StudyChapterNewFormCtrl): VNode {
         h('div.form-group', [
           h('label.form-label', {
             attrs: {for: 'chapter-name' }
-          }, 'Name'),
+          }, trans.noarg('name')),
           h('input#chapter-name.form-control', {
             attrs: {
               minlength: 2,
               maxlength: 80
             },
             hook: onInsert<HTMLInputElement>(el => {
-                if (!el.value) {
-                  el.value = 'Chapter ' + (ctrl.vm.initial() ? 1 : (ctrl.chapters().length + 1));
-                  el.select();
-                  el.focus();
-                }
-              })
-            })
-          ]),
-          h('div.tabs-horiz', [
-            makeTab('init', 'Empty', 'Start from initial position'),
-            makeTab('edit', 'Editor', 'Start from custom position'),
-            makeTab('game', 'URL', 'Load a game URL'),
-            makeTab('fen', 'FEN', 'Load a FEN position'),
-            makeTab('pdn', 'PDN', 'Load a PDN game')
-          ]),
-          activeTab === 'edit' ? h('div.board-editor-wrap.is2d', {
-            hook: {
-              insert: vnode => {
-                $.when(
-                  window.lidraughts.loadScript('compiled/lidraughts.editor.min.js'),
-                  $.get('/editor.json', {
-                    fen: ctrl.root.node.fen,
-                    variant: currentChapter.setup.variant.key
-                  })
-                ).then(function (_, b) {
-                  const data = b[0];
-                  data.embed = true;
-                  data.options = {
-                    inlineCastling: true,
-                    onChange: ctrl.vm.editorFen
-                  };
-                  ctrl.vm.editor = window['LidraughtsEditor'](vnode.elm as HTMLElement, data);
-                  ctrl.vm.editorFen(ctrl.vm.editor.getFen());
-                });
-              },
-              destroy: _ => {
-                ctrl.vm.editor = null;
-              }
-            }
-          }, [spinner()]) : null,
-          activeTab === 'game' ? h('div.form-group', [
-            h('label.form-label', {
-              attrs: { 'for': 'chapter-game' }
-            }, 'Load a game from lidraughts.org'),
-            h('input#chapter-game.form-control', {
-              attrs: { placeholder: 'URL of the game' }
-            })
-          ]) : null,
-          activeTab === 'fen' ? h('div.form-group', [
-            h('input#chapter-fen.form-control', {
-              attrs: {
-                value: ctrl.root.node.fen,
-                placeholder: 'Initial FEN position'
+              if (!el.value) {
+                el.value = trans('chapterX', (ctrl.vm.initial() ? 1 : (ctrl.chapters().length + 1)));
+                el.select();
+                el.focus();
               }
             })
-          ]) : null,
-          activeTab === 'pdn' ? h('div.form-group', [
-            h('textarea#chapter-pdn.form-control', {
-              attrs: { placeholder: 'Paste your PDN text, up to ' + ctrl.multiPdnMax + ' games, each separated by an empty line' }
-            }),
-            window.FileReader ? h('input#chapter-pdn-file.form-control', {
-              attrs: {
-                type: 'file',
-                accept: '.pdn'
-              },
-              hook: bind('change', e => {
-                const file = (e.target as HTMLInputElement).files![0];
-                if (!file) return;
-                const reader = new FileReader();
-                reader.onload = function() {
-                  (document.getElementById('chapter-pdn') as HTMLTextAreaElement).value = reader.result as string;
-                };
-                reader.readAsText(file);
-              })
-            }) : null
-          ]) : null,
-          h('div.form-split', [
-            h('div.form-group.form-half', [
-              h('label.form-label', {
-                attrs: { 'for': 'chapter-variant' }
-              }, 'Variant'),
-              h('select#chapter-variant.form-control', {
-                attrs: { disabled: gameOrPdn }
-              }, gameOrPdn ? [
-                h('option', 'Automatic')
-              ] :
-              ctrl.vm.variants.map(v => option(v.key, currentChapter.setup.variant.key, v.name)))
-            ]),
-            h('div.form-group.form-half', [
-              h('label.form-label', {
-                attrs: { 'for': 'chapter-orientation' }
-              }, 'Orientation'),
-              h('select#chapter-orientation.form-control', {
-                hook: bind('change', e => {
-                  ctrl.vm.editor && ctrl.vm.editor.setOrientation((e.target as HTMLInputElement).value);
+          })
+        ]),
+        h('div.tabs-horiz', [
+          makeTab('init', trans.noarg('empty'), trans.noarg('startFromInitialPosition')),
+          makeTab('edit', trans.noarg('editor'), trans.noarg('startFromCustomPosition')),
+          makeTab('game', 'URL', trans.noarg('loadAGameByUrl')),
+          makeTab('fen', 'FEN', trans.noarg('loadAPositionFromFen')),
+          makeTab('pdn', 'PDN', trans.noarg('loadAGameFromPdn'))
+        ]),
+        activeTab === 'edit' ? h('div.board-editor-wrap.is2d', {
+          hook: {
+            insert: vnode => {
+              $.when(
+                window.lidraughts.loadScript('compiled/lidraughts.editor.min.js'),
+                $.get('/editor.json', {
+                  fen: ctrl.root.node.fen,
+                  variant: currentChapter.setup.variant.key
                 })
-              }, ['White', 'Black'].map(function (color) {
-                const c = color.toLowerCase();
-                return option(c, currentChapter.setup.orientation, color);
-              }))
-            ])
-          ]),
-          h('div.form-group', [
+              ).then(function (_, b) {
+                const data = b[0];
+                data.embed = true;
+                data.options = {
+                  inlineCastling: true,
+                  onChange: ctrl.vm.editorFen
+                };
+                ctrl.vm.editor = window['LidraughtsEditor'](vnode.elm as HTMLElement, data);
+                ctrl.vm.editorFen(ctrl.vm.editor.getFen());
+              });
+            },
+            destroy: _ => {
+              ctrl.vm.editor = null;
+            }
+          }
+        }, [spinner()]) : null,
+        activeTab === 'game' ? h('div.form-group', [
+          h('label.form-label', {
+            attrs: { 'for': 'chapter-game' }
+          }, 'Load a game from lidraughts.org'),
+          h('input#chapter-game.form-control', {
+            attrs: { placeholder: trans.noarg('urlOfTheGame') }
+          })
+        ]) : null,
+        activeTab === 'fen' ? h('div.form-group', [
+          h('input#chapter-fen.form-control', {
+            attrs: {
+              value: ctrl.root.node.fen,
+              placeholder: trans.noarg('loadAPositionFromFen')
+            }
+          })
+        ]) : null,
+        activeTab === 'pdn' ? h('div.form-group', [
+          h('textarea#chapter-pdn.form-control', {
+            attrs: { placeholder: trans.plural('pasteYourPdnTextHereUpToNbGames', ctrl.multiPdnMax) }
+          }),
+          window.FileReader ? h('input#chapter-pdn-file.form-control', {
+            attrs: {
+              type: 'file',
+              accept: '.pdn'
+            },
+            hook: bind('change', e => {
+              const file = (e.target as HTMLInputElement).files![0];
+              if (!file) return;
+              const reader = new FileReader();
+              reader.onload = function() {
+                (document.getElementById('chapter-pdn') as HTMLTextAreaElement).value = reader.result as string;
+              };
+              reader.readAsText(file);
+            })
+          }) : null
+        ]) : null,
+        h('div.form-split', [
+          h('div.form-group.form-half', [
             h('label.form-label', {
-              attrs: { 'for': 'chapter-mode' }
-            }, 'Analysis mode'),
-            h('select#chapter-mode.form-control', modeChoices.map(c => option(c[0], mode, ctrl.root.trans.noarg(c[1]))))
+              attrs: { 'for': 'chapter-variant' }
+            }, trans.noarg('variant')),
+            h('select#chapter-variant.form-control', {
+              attrs: { disabled: gameOrPdn }
+            }, gameOrPdn ? [
+              h('option', trans.noarg('automatic'))
+            ] :
+            ctrl.vm.variants.map(v => option(v.key, currentChapter.setup.variant.key, v.name)))
           ]),
-          modal.button('Create chapter')
-        ])
+          h('div.form-group.form-half', [
+            h('label.form-label', {
+              attrs: { 'for': 'chapter-orientation' }
+            }, trans.noarg('orientation')),
+            h('select#chapter-orientation.form-control', {
+              hook: bind('change', e => {
+                ctrl.vm.editor && ctrl.vm.editor.setOrientation((e.target as HTMLInputElement).value);
+              })
+            }, ['white', 'black'].map(function(color) {
+              return option(color, currentChapter.setup.orientation, trans.noarg(color));
+            }))
+          ])
+        ]),
+        h('div.form-group', [
+          h('label.form-label', {
+            attrs: { 'for': 'chapter-mode' }
+          }, trans.noarg('analysisMode')),
+          h('select#chapter-mode.form-control', modeChoices.map(c => option(c[0], mode, trans.noarg(c[1]))))
+        ]),
+        modal.button(trans.noarg('createChapter'))
+      ])
     ]
   });
 }
