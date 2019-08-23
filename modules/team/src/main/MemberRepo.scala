8eb4c8554e3acba988c9acef32adcfbac12c0a1d
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-08-23 10:07:25 +0200

    remove (undocumented) /api/games/team endpoint

--- modules/team/src/main/MemberRepo.scala
+++ modules/team/src/main/MemberRepo.scala
@@ -3,6 +3,7 @@ package lidraughts.team
 import reactivemongo.bson._
 
 import lidraughts.db.dsl._
+import lidraughts.user.User
 
 object MemberRepo {
 
@@ -13,28 +14,29 @@ object MemberRepo {
 
   type ID = String
 
+  // expensive with thousands of members!
   def userIdsByTeam(teamId: ID): Fu[Set[ID]] =
     coll.distinct[String, Set]("user", $doc("team" -> teamId).some)
 
-  def teamIdsByUser(userId: ID): Fu[Set[ID]] =
-    coll.distinct[String, Set]("team", $doc("user" -> userId).some)
+  def teamIdsByUser(userId: User.ID): Fu[Set[ID]] =
+    coll.distinct[ID, Set]("team", $doc("user" -> userId).some)
 
   def removeByteam(teamId: ID): Funit =
     coll.remove(teamQuery(teamId)).void
 
-  def removeByUser(userId: ID): Funit =
+  def removeByUser(userId: User.ID): Funit =
     coll.remove(userQuery(userId)).void
 
-  def exists(teamId: ID, userId: ID): Fu[Boolean] =
+  def exists(teamId: ID, userId: User.ID): Fu[Boolean] =
     coll.exists(selectId(teamId, userId))
 
-  def add(teamId: String, userId: String): Funit =
+  def add(teamId: ID, userId: User.ID): Funit =
     coll.insert(Member.make(team = teamId, user = userId)).void
 
-  def remove(teamId: String, userId: String): Funit =
+  def remove(teamId: ID, userId: User.ID): Funit =
     coll.remove(selectId(teamId, userId)).void
 
-  def countByTeam(teamId: String): Fu[Int] =
+  def countByTeam(teamId: ID): Fu[Int] =
     coll.countSel(teamQuery(teamId))
 
   def selectId(teamId: ID, userId: ID) = $id(Member.makeId(teamId, userId))
