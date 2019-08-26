d2492268c1806c50a01317d8f88f757d4414aec0
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-08-26 13:02:34 +0200

    tweak game bson handler

--- modules/game/src/main/BSONHandlers.scala
+++ modules/game/src/main/BSONHandlers.scala
@@ -121,7 +121,7 @@ object BSONHandlers {
       )
 
       Game(
-        id = r str F.id,
+        id = light.id,
         whitePlayer = light.whitePlayer,
         blackPlayer = light.blackPlayer,
         chess = chessGame,
@@ -210,8 +210,7 @@ object BSONHandlers {
       val (whiteUid, blackUid) = (uids.headOption.filter(_.nonEmpty), uids.lift(1).filter(_.nonEmpty))
       def makePlayer(field: String, color: Color, id: Player.ID, uid: Player.UserId): Player = {
         val builder = r.getO[Player.Builder](field)(playerBSONHandler) | emptyPlayerBuilder
-        val win = winC map (_ == color)
-        builder(color)(id)(uid)(win)
+        builder(color)(id)(uid)(winC map (_ == color))
       }
       LightGame(
         id = r str F.id,
