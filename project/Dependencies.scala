711bfa6d30bea8802c0a9082553d6cccb8720f85
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-08-28 20:58:03 +0200

    upgrade lettuce (redis)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
   val specs2Scalaz = "org.specs2" %% "specs2-scalaz" % "4.0.2" % "test"
   val scalaUri = "io.lemonlabs" %% "scala-uri" % "1.2.0"
   val scalatags = "com.lihaoyi" %% "scalatags" % "0.6.7"
-  val lettuce = "io.lettuce" % "lettuce-core" % "5.1.7.RELEASE"
+  val lettuce = "io.lettuce" % "lettuce-core" % "5.1.8.RELEASE"
   val epoll = "io.netty" % "netty-transport-native-epoll" % "4.1.36.Final" classifier "linux-x86_64"
 
   object reactivemongo {
