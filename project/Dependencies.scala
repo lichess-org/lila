88b3d32ce0dbefc5bf3d1a79d4953a3d1a691818
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-09-01 20:15:48 +0200

    upgrade dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,11 +23,11 @@ object Dependencies {
   }
 
   val scalaz = "org.scalaz" %% "scalaz-core" % "7.2.16"
-  val scalalib = "com.github.ornicar" %% "scalalib" % "6.6"
+  val scalalib = "com.github.ornicar" %% "scalalib" % "6.7"
   val typesafeConfig = "com.typesafe" % "config" % "1.3.1"
   val findbugs = "com.google.code.findbugs" % "jsr305" % "3.0.1"
   val hasher = "com.roundeights" %% "hasher" % "1.2.0"
-  val jodaTime = "joda-time" % "joda-time" % "2.10.2"
+  val jodaTime = "joda-time" % "joda-time" % "2.10.3"
   val compression = "org.lichess" %% "compression" % "1.4"
   val maxmind = "com.sanoma.cda" %% "maxmind-geoip2-scala" % "1.2.3-THIB"
   val prismic = "io.prismic" %% "scala-kit" % "1.2.13-THIB211"
