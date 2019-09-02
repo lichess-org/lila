d211194513ae517829fe13f074d07158f193b678
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-09-02 08:27:22 +0200

    tweak logging order

--- modules/round/src/main/DeployPersistence.scala
+++ modules/round/src/main/DeployPersistence.scala
@@ -12,8 +12,8 @@ private final class DeployPersistence(system: ActorSystem) {
   def isEnabled() = ongoing.isDefined
 
   def enable(): Unit = {
-    logger.warn("Enabling round persistence")
     cancel()
+    logger.warn("Enabling round persistence")
     ongoing = system.scheduler.scheduleOnce(7.minutes) {
       logger.warn("Expiring round persistence")
       ongoing = none
