9316f426761a7b79328bf6ad5dec26279cd0b71c
Daniel Dugovic <dandydand@gmail.com>
2019-09-14 17:25:00 -0500

    Estimate performance rating using 400 multiplier
    https://en.wikipedia.org/wiki/Elo_rating_system#Performance_rating

--- modules/tournament/src/main/TournamentApi.scala
+++ modules/tournament/src/main/TournamentApi.scala
@@ -346,11 +346,12 @@ final class TournamentApi(
       }
     }
 
+  // Estimates performance rating (Elo)
   private def performanceOf(g: Game, userId: String): Option[Int] = for {
     opponent <- g.opponentByUserId(userId)
     opponentRating <- opponent.rating
     multiplier = g.winnerUserId.??(winner => if (winner == userId) 1 else -1)
-  } yield opponentRating + 500 * multiplier
+  } yield opponentRating + 400 * multiplier
 
   private def withdrawNonMover(game: Game): Unit = for {
     tourId <- game.tournamentId
