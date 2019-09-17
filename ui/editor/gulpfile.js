9f12cc026f7bd46168adf73e13cbacd78e499c30
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-09-17 21:24:36 +0200

    factor out jsProject

--- ui/editor/gulpfile.js
+++ ui/editor/gulpfile.js
@@ -1,3 +1,3 @@
-require('../gulp/jsProject.js')('LichessEditor', 'lichess.editor', __dirname);
+require('@build/jsProject')('LichessEditor', 'lichess.editor', __dirname);
 
 require('../gulp/cssProject.js')(__dirname);
