f2e6555af0dfd1cbc2ffcc6629f9a7563dc09835
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-09-20 23:22:53 +0200

    code golf, remove debug

--- app/controllers/Options.scala
+++ app/controllers/Options.scala
@@ -4,7 +4,7 @@ import play.api.mvc._
 import scala.concurrent.duration._
 
 import lila.app._
-import lila.common.HTTPRequest.{ isApi, isLocalApp }
+import lila.common.HTTPRequest.isApiOrLocalApp
 import lila.common.ResponseHeaders.allowMethods
 
 object Options extends LilaController {
@@ -12,14 +12,10 @@ object Options extends LilaController {
   val root = all("")
 
   def all(url: String) = Action { req =>
-    if (isLocalApp(req) || isApi(req).pp) {
-      NoContent.withHeaders({
-        List(
-          "Allow" -> allowMethods,
-          "Access-Control-Max-Age" -> "1728000"
-        )
-      }: _*)
-    } else
-      NotFound
+    if (isApiOrLocalApp(req)) NoContent.withHeaders(
+      "Allow" -> allowMethods,
+      "Access-Control-Max-Age" -> "1728000"
+    )
+    else NotFound
   }
 }
