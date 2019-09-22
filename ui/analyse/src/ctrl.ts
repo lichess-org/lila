7b264abfb36cd5ed2facb08cc97fb75e59b3f359
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-09-22 09:56:42 +0200

    last node has no eval if it's mate or stalemate - fixes #5162

--- ui/analyse/src/ctrl.ts
+++ ui/analyse/src/ctrl.ts
@@ -732,7 +732,7 @@ export default class AnalyseCtrl {
     this.tree.merge(data.tree);
     if (!this.showComputer()) this.tree.removeComputerVariations();
     this.data.analysis = data.analysis;
-    if (data.analysis) data.analysis.partial = !!treeOps.findInMainline(data.tree, n => !n.eval);
+    if (data.analysis) data.analysis.partial = !!treeOps.findInMainline(data.tree, n => !n.eval && !!n.children.length);
     if (data.division) this.data.game.division = data.division;
     if (this.retro) this.retro.onMergeAnalysisData();
     if (this.study) this.study.serverEval.onMergeAnalysisData();
