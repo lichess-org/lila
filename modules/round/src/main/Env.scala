b05b45cb5fd561b7896706d22e724135ee193f49
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-11-14 14:09:04 -0600

    direct 50% of games through lila-ws

--- modules/round/src/main/Env.scala
+++ modules/round/src/main/Env.scala
@@ -151,8 +151,8 @@ final class Env(
 
   import lila.memo.SettingStore.Regex._
   lazy val remoteSocketSetting = settingStore[scala.util.matching.Regex](
-    "roundRemoteSocket",
-    default = "[0-9].+".r,
+    "roundRemoteSocket2",
+    default = "[0-9a-u].+".r,
     text = "Remote socket game ID regex".some
   )
   def useRemoteSocket(gameId: Game.ID) = remoteSocketSetting.get().matches(gameId)
