cde7f8fd43459465870fb7f9ca24ffd54ba016b7
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-11-30 14:20:25 +0100

    use async/await in service worker

--- ui/serviceWorker/src/main.ts
+++ ui/serviceWorker/src/main.ts
@@ -6,38 +6,40 @@ self.addEventListener('push', event => {
     body: data.body,
     tag: data.tag,
     data: data.payload,
-    requireInteraction: true
+    requireInteraction: true,
   });
 });
 
-self.addEventListener('notificationclick', event => {
-  event.waitUntil(self.registration.getNotifications().then(notifications => {
-    notifications.forEach(notification => notification.close());
-    return self.clients.matchAll({
-      type: 'window',
-      includeUncontrolled: true
-    });
-  }).then((windowClients: WindowClient[]) => {
-    // determine url
-    const data = event.notification.data.userData;
-    let url = '/';
-    if (data.fullId) url = '/' + data.fullId;
-    else if (data.threadId) url = '/inbox/' + data.threadId + '#bottom';
-    else if (data.challengeId) url = '/' + data.challengeId;
+async function handleNotificationClick(event: NotificationEvent) {
+  const notifications = await self.registration.getNotifications();
+  notifications.forEach(notification => notification.close());
 
-    // focus open window with same url
-    for (const client of windowClients) {
-      const clientUrl = new URL(client.url, self.location.href);
-      if (clientUrl.pathname === url && 'focus' in client) return client.focus();
-    }
+  const windowClients = await self.clients.matchAll({
+    type: 'window',
+    includeUncontrolled: true,
+  }) as ReadonlyArray<WindowClient>;
 
-    // navigate from open homepage to url
-    for (const client of windowClients) {
-      const clientUrl = new URL(client.url, self.location.href);
-      if (clientUrl.pathname === '/') return client.navigate(url);
-    }
+  // determine url
+  const data = event.notification.data.userData;
+  let url = '/';
+  if (data.fullId) url = '/' + data.fullId;
+  else if (data.threadId) url = '/inbox/' + data.threadId + '#bottom';
+  else if (data.challengeId) url = '/' + data.challengeId;
 
-    // open new window
-    return self.clients.openWindow(url);
-  }));
-});
+  // focus open window with same url
+  for (const client of windowClients) {
+    const clientUrl = new URL(client.url, self.location.href);
+    if (clientUrl.pathname === url && 'focus' in client) return await client.focus();
+  }
+
+  // navigate from open homepage to url
+  for (const client of windowClients) {
+    const clientUrl = new URL(client.url, self.location.href);
+    if (clientUrl.pathname === '/') return await client.navigate(url);
+  }
+
+  // open new window
+  return await self.clients.openWindow(url);
+}
+
+self.addEventListener('notificationclick', e => e.waitUntil(handleNotificationClick(e)));
