dcd87e7d779aeedb4b1558c007151aef32cf7bba
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-12-09 13:35:10 -0600

    work queue: fail promise with future

--- modules/common/src/main/WorkQueue.scala
+++ modules/common/src/main/WorkQueue.scala
@@ -1,7 +1,5 @@
 package lila.common
 
-import lila.log.Logger
-
 import akka.stream.scaladsl._
 import akka.stream.{ Materializer, OverflowStrategy }
 import com.github.blemale.scaffeine.{ LoadingCache, Scaffeine }
@@ -12,7 +10,7 @@ import scala.concurrent.Promise
 // queue.run(() => task1); queue.run(() => task2)
 // runs task2 only after task1 completes, much like:
 // task1 flatMap { _ => task2 }
-final class WorkQueue(buffer: Int, logger: Logger)(implicit mat: Materializer) {
+final class WorkQueue(buffer: Int)(implicit mat: Materializer) {
 
   type Task = () => Funit
   private type TaskWithPromise = (Task, Promise[Unit])
@@ -27,23 +25,21 @@ final class WorkQueue(buffer: Int, logger: Logger)(implicit mat: Materializer) {
     .queue[TaskWithPromise](buffer, OverflowStrategy.dropHead)
     .mapAsync(1) {
       case (task, promise) =>
-        task()
-          .map(promise.success)
-          .recover {
-            case e: Exception => logger.error("WorkQueue recover", e)
-          }
+        val future = task()
+        promise completeWith future
+        future
     }
     .toMat(Sink.ignore)(Keep.left)
     .run
 }
 
 // Distributes tasks to many sequencers
-final class WorkQueues(buffer: Int, logger: Logger, expiration: FiniteDuration)(implicit mat: Materializer) {
+final class WorkQueues(buffer: Int, expiration: FiniteDuration)(implicit mat: Materializer) {
 
   def run(key: String)(task: => Funit): Funit =
     queues.get(key).run(() => task)
 
   private val queues: LoadingCache[String, WorkQueue] = Scaffeine()
     .expireAfterAccess(expiration)
-    .build((key: String) => new WorkQueue(buffer, logger branch key))
+    .build(_ => new WorkQueue(buffer))
 }
