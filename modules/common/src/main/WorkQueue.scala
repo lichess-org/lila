ef710243e84508279ee31ef7bc595f6da6c17713
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-12-09 13:25:11 -0600

    akka stream implementation of WorkQueue

--- modules/common/src/main/WorkQueue.scala
+++ modules/common/src/main/WorkQueue.scala
@@ -2,34 +2,48 @@ package lila.common
 
 import lila.log.Logger
 
+import akka.stream.scaladsl._
+import akka.stream.{ Materializer, OverflowStrategy }
 import com.github.blemale.scaffeine.{ LoadingCache, Scaffeine }
-import java.util.concurrent.atomic.AtomicReference
 import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.Promise
 
 // Sequences async tasks, so that:
 // queue.run(() => task1); queue.run(() => task2)
 // runs task2 only after task1 completes, much like:
 // task1 flatMap { _ => task2 }
-final class WorkQueue(logger: Logger) {
+final class WorkQueue(buffer: Int, logger: Logger)(implicit mat: Materializer) {
 
-  def run(task: () => Funit): Funit = queue.updateAndGet {
-    _ flatMap { _ =>
-      task().recover {
-        case e: Exception => logger.error("WorkQueue recover", e)
-      }
-    }
+  type Task = () => Funit
+  private type TaskWithPromise = (Task, Promise[Unit])
+
+  def run(task: Task): Funit = {
+    val promise = Promise[Unit]
+    queue.offer(task -> promise)
+    promise.future
   }
 
-  private val queue: AtomicReference[Funit] = new AtomicReference(funit)
+  private val queue = Source
+    .queue[TaskWithPromise](buffer, OverflowStrategy.dropHead)
+    .mapAsync(1) {
+      case (task, promise) =>
+        task()
+          .map(promise.success)
+          .recover {
+            case e: Exception => logger.error("WorkQueue recover", e)
+          }
+    }
+    .toMat(Sink.ignore)(Keep.left)
+    .run
 }
 
 // Distributes tasks to many sequencers
-final class WorkQueues(logger: Logger, expiration: FiniteDuration) {
+final class WorkQueues(buffer: Int, logger: Logger, expiration: FiniteDuration)(implicit mat: Materializer) {
 
   def run(key: String)(task: => Funit): Funit =
     queues.get(key).run(() => task)
 
   private val queues: LoadingCache[String, WorkQueue] = Scaffeine()
     .expireAfterAccess(expiration)
-    .build((key: String) => new WorkQueue(logger branch key))
+    .build((key: String) => new WorkQueue(buffer, logger branch key))
 }
