bfd2282decb15e00265cba69bfd9aecae0f9ba70
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-12-15 12:19:18 -0600

    load testing: don't check team battle user's team - REVERT ME

--- modules/tournament/src/main/Condition.scala
+++ modules/tournament/src/main/Condition.scala
@@ -177,7 +177,7 @@ object Condition {
     def canEnter(user: User, getUserTeamIds: User => Fu[List[TeamID]])(
         tour: Tournament
     )(implicit ec: scala.concurrent.ExecutionContext): Fu[Boolean] =
-      apply(tour.conditions, user, getUserTeamIds).dmap(_.accepted)
+      apply(tour.conditions, user, getUserTeamIds).dmap(_.accepted || true)
   }
 
   object BSONHandlers {
