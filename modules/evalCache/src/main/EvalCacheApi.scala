ec95aa03c6f11bdc1670bea7109f435987c06800
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-12-20 21:56:11 -0600

    fix eval cache monitoring

--- modules/evalCache/src/main/EvalCacheApi.scala
+++ modules/evalCache/src/main/EvalCacheApi.scala
@@ -27,7 +27,7 @@ final class EvalCacheApi(
       _.map { JsonHandlers.writeEval(_, fen) }
     } addEffect { res =>
       Forsyth getPly fen.value foreach { ply =>
-        lila.mon.evalCache.request(ply, res.isDefined)
+        lila.mon.evalCache.request(ply, res.isDefined).increment()
       }
     }
 
