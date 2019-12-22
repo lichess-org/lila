fe8b36ccb0ec2df6e2fd03c147450f98c7e077cc
Greg Finley <greg.finley@lambdaschool.com>
2019-12-21 21:26:10 -0800

    Add more time controls, fixes #5770

--- app/templating/SetupHelper.scala
+++ app/templating/SetupHelper.scala
@@ -45,9 +45,13 @@ trait SetupHelper { self: I18nHelper =>
     "40",
     "45",
     "60",
+    "75",
     "90",
+    "105",
     "120",
+    "135",
     "150",
+    "165",
     "180"
   ).map { v =>
     (v.toString, v.toString, none)
