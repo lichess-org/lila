1792fef2af8d49c467c99c30fdded7576bf22ae8
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-12-28 21:20:33 -0600

    show more timeline entries - closes #5833

--- app/controllers/Timeline.scala
+++ app/controllers/Timeline.scala
@@ -12,7 +12,6 @@ import views._
 final class Timeline(env: Env) extends LilaController(env) {
 
   def home = Auth { implicit ctx => me =>
-    def nb = Max(getInt("nb").fold(10)(_ atMost 30))
     negotiate(
       html =
         if (HTTPRequest.isXhr(ctx.req))
@@ -21,13 +20,11 @@ final class Timeline(env: Env) extends LilaController(env) {
             .logTimeIfGt(s"timeline site entries for ${me.id}", 10 seconds)
             .map { html.timeline.entries(_) } else
           env.timeline.entryApi
-            .moreUserEntries(me.id, nb)
-            .logTimeIfGt(s"timeline site more entries ($nb) for ${me.id}", 10 seconds)
+            .moreUserEntries(me.id, Max(30))
             .map { html.timeline.more(_) },
       _ =>
         env.timeline.entryApi
-          .moreUserEntries(me.id, nb atMost env.apiTimelineSetting.get())
-          .logTimeIfGt(s"timeline mobile $nb for ${me.id}", 10 seconds)
+          .moreUserEntries(me.id, Max(getInt("nb") | 10) atMost env.apiTimelineSetting.get())
           .map { es =>
             Ok(Json.obj("entries" -> es))
           }
