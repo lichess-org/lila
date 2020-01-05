15c4922c5fe913a5aa41150fb9a0e04e7b4d5531
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-01-04 19:41:06 -0600

    allow to claim victory in case of disconnections in classical games

--- modules/game/src/main/Game.scala
+++ modules/game/src/main/Game.scala
@@ -352,7 +352,7 @@ case class Game(
 
   def resignable      = playable && !abortable
   def drawable        = playable && !abortable
-  def forceResignable = resignable && nonAi && !fromFriend && speed < Speed.Classical
+  def forceResignable = resignable && nonAi && !fromFriend
 
   def finish(status: Status, winner: Option[Color]) = {
     val newClock = clock map { _.stop }
