c3e2037a5a191ced96436cef83a661d00718e445
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-01-17 08:48:12 -0600

    malware warning is misunderstood, disable

--- public/javascripts/ads.js
+++ public/javascripts/ads.js
@@ -1,5 +1,5 @@
 // If this script loads, it means the user is vulnerable to ads and trackers!
 // Recommend that they install uBlock Origin.
 // See https://lichess.org/ads
-document.querySelectorAll('.ads-vulnerable').forEach(e => e.classList.remove('none'));
+// document.querySelectorAll('.ads-vulnerable').forEach(e => e.classList.remove('none'));
 document.querySelectorAll('.ads-protected').forEach(e => e.classList.add('none'));
