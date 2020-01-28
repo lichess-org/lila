60ef939fb4f40043978af67f4e794321c49e10eb
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-01-27 18:15:47 -0600

    msg unread style

--- ui/msg/src/view/contact.ts
+++ ui/msg/src/view/contact.ts
@@ -5,13 +5,11 @@ import MsgCtrl from '../ctrl';
 import { userName, userIcon, bindMobileMousedown } from './util';
 
 export default function renderContact(ctrl: MsgCtrl, contact: Contact, active?: string): VNode {
-  const user = contact.user, msg = contact.lastMsg;
+  const user = contact.user, msg = contact.lastMsg,
+    isNew = !msg.read && msg.user != ctrl.data.me.id;
   return h('div.msg-app__side__contact', {
     key: user.id,
-    class: {
-      active: active == user.id,
-      new: !msg.read && msg.user != ctrl.data.me.id
-    },
+    class: { active: active == user.id, },
     hook: bindMobileMousedown(_ => ctrl.openConvo(user.id)),
   }, [
     userIcon(user, 'msg-app__side__contact__icon'),
@@ -20,7 +18,14 @@ export default function renderContact(ctrl: MsgCtrl, contact: Contact, active?: 
         h('div.msg-app__side__contact__name', userName(user)),
         h('div.msg-app__side__contact__date', renderDate(msg))
       ]),
-      h('div.msg-app__side__contact__msg', msg.text)
+      h('div.msg-app__side__contact__body', [
+        h('div.msg-app__side__contact__msg', {
+          class: { 'msg-app__side__contact__msg--new': isNew }
+        }, msg.text),
+        isNew ? h('i.msg-app__side__contact__new', {
+          attrs: { 'data-icon': '' }
+        }) : null
+      ])
     ])
   ]);
 }
