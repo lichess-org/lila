ada5aa68a5e00a7610ef4a879c2f6c36a118feb8
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-02-05 16:31:41 -0600

    upgrade play to 2.8.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -57,7 +57,7 @@ object Dependencies {
   }
 
   object play {
-    val version = "2.8.0"
+    val version = "2.8.1"
     val api     = "com.typesafe.play" %% "play" % version
     val json    = "com.typesafe.play" %% "play-json" % "2.8.1"
   }
