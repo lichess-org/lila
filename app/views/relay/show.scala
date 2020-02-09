35228a74800b76e7c3afeda2d0728051bd25ff3c
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-02-09 13:18:39 -0600

    code format

--- app/views/relay/show.scala
+++ app/views/relay/show.scala
@@ -31,8 +31,10 @@ object show {
             "relay" -> data.relay,
             "study" -> data.study,
             "data"  -> data.analysis,
-            "i18n" -> (board
-              .userAnalysisI18n(withAdvantageChart = true) ++ i18nFullDbJsObject(lila.i18n.I18nDb.Study)),
+            "i18n" -> {
+              board.userAnalysisI18n(withAdvantageChart = true) ++
+                i18nFullDbJsObject(lila.i18n.I18nDb.Study)
+            },
             "tagTypes" -> lila.study.PgnTags.typesToString,
             "userId"   -> ctx.userId,
             "chat" -> chatOption.map(c =>
