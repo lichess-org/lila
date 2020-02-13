e4c80f110ad9ab45ad9e4cfee31997e2b6b86612
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-02-13 17:59:39 +0100

    trans-lint: provide url to fix issues

--- bin/trans-lint.py
+++ bin/trans-lint.py
@@ -3,6 +3,8 @@
 import os
 import re
 import sys
+import pathlib
+import urllib
 
 # Do not load C implementation, so that we can override some parser methods.
 sys.modules["_elementtree"] = None
@@ -23,14 +25,22 @@ class Report:
         self.warnings = 0
 
 
+def short_lang(lang):
+    return lang.split("-")[0]
+
+
 class ReportContext:
-    def __init__(self, report, path, el):
+    def __init__(self, report, path, el, name, text):
         self.report = report
         self.path = path
         self.el = el
+        self.name = name
+        self.text = text
 
     def log(self, level, message):
-        print(f"::{level} file={self.path},line={self.el.line},col={self.el.col}::{message}")
+        lang = short_lang(self.path.stem)
+        url = f"https://crowdin.com/translate/lichess/all/en-{lang}#q={urllib.parse.quote(self.text)}"
+        print(f"::{level} file={self.path},line={self.el.line},col={self.el.col}::{message} ({self.name}): {url}")
 
     def error(self, message):
         self.log("error", message)
@@ -43,30 +53,28 @@ class ReportContext:
 
 
 def lint(report, path):
-    dirname = os.path.dirname(path)
-    db = os.path.basename(dirname) # site, study, ...
-
-    source = ET.parse(os.path.join(dirname, "..", "..", "source", f"{db}.xml"), parser=AnnotatingParser()).getroot()
+    db = path.parent.name # site, study, ...
+    source = ET.parse(path.parent.parent.parent / "source" / f"{db}.xml", parser=AnnotatingParser()).getroot()
 
     root = ET.parse(path, parser=AnnotatingParser()).getroot()
     for el in root:
-        ctx = ReportContext(report, path, el)
-
         name = el.attrib["name"]
+        ctx = ReportContext(report, path, el, name, el.text)
         if "'" in name or " " in name:
-            ctx.error(f"bad {el.tag} name: {name!r}")
+            ctx.error(f"bad {el.tag} name")
             continue
 
         source_el = source.find(f".//{el.tag}[@name='{name}']")
         if source_el is None:
-            ctx.error(f"did not find source element for {el.tag}: {name!r}")
+            ctx.error(f"did not find source element for {el.tag}")
         elif el.tag == "string":
             lint_string(ctx, name, el.text, source_el.text)
         elif el.tag == "plurals":
             for item in el:
                 quantity = item.attrib["quantity"]
                 allow_missing = 1 if quantity in ["zero", "one", "two"] else 0
-                lint_string(ReportContext(report, path, item), "{}:{}".format(name, quantity), item.text, source_el.find("./item[@quantity='other']").text, allow_missing)
+                name = f"{name}:{quantity}"
+                lint_string(ReportContext(report, path, item, name, item.text), name, item.text, source_el.find("./item[@quantity='other']").text, allow_missing)
         else:
             ctx.error(f"bad resources tag: {el.tag}")
 
@@ -74,53 +82,53 @@ def lint(report, path):
 def lint_string(ctx, name, dest, source, allow_missing=0):
     placeholders = source.count("%s")
     if placeholders > 1:
-        ctx.error(f"more than 1 %s in source: {name} {source}")
+        ctx.error(f"more than 1 %s in source")
 
     diff = placeholders - dest.count("%s")
     if diff > 0:
         allow_missing -= diff
         if allow_missing < 0:
-            ctx.log("error", f"missing %s: {name} {dest}")
+            ctx.log("error", "missing %s")
     elif diff < 0:
-        ctx.error(f"too many %s: {name} {dest}")
+        ctx.error("too many %s")
 
     for placeholder in re.findall(r"%\d+\$s", source):
         if placeholder == "%1$s" and placeholder not in dest and allow_missing > 0:
             allow_missing -= 1
         elif dest.count(placeholder) < 1:
-            ctx.error(f"missing {placeholder}: {name} {dest}")
+            ctx.error(f"missing {placeholder}")
 
     for placeholder in re.findall(r"%\d+\$s", dest):
         if source.count(placeholder) < 1:
-            ctx.error(f"unexpected {placeholder}: {name} {dest}")
+            ctx.error(f"unexpected {placeholder}")
 
     for pattern in ["O-O", "SAN", "FEN", "PGN", "K, Q, R, B, N"]:
         m_source = source if pattern.isupper() else source.lower()
         m_dest = dest if pattern.isupper() else dest.lower()
         if pattern in m_source and pattern not in m_dest:
-            ctx.notice(f"missing {pattern}: {name} {dest}")
+            ctx.notice(f"missing {pattern}")
         #elif pattern not in m_source and pattern in m_dest:
-        #    ctx.notice(f"unexpected {pattern}: {name} {dest}")
+        #    ctx.notice(f"unexpected {pattern}")
 
     if "PGN" in source and "PNG" in dest:
-        ctx.warning(f"PNG instead of PGN: {name} {dest}")
+        ctx.warning("PNG instead of PGN")
 
     if "\n" not in source and "\n" in dest:
-        ctx.notice(f"expected single line string: {name} {dest}")
+        ctx.notice("expected single line string")
 
     if re.match(r"\n", dest):
-        ctx.error(f"has leading newlines: {name} {dest}")
+        ctx.error("has leading newlines")
     elif re.match(r"\s+", dest):
-        ctx.warning(f"has leading spaces: {name} {dest}")
+        ctx.warning("has leading spaces")
 
     if re.search(r"\s+$", dest):
-        ctx.warning(f"has trailing spaces: {name} {dest}")
+        ctx.warning("has trailing spaces")
 
     if re.search(r"\t", dest):
-        ctx.warning(f"has tabs: {name} {dest}")
+        ctx.warning("has tabs")
 
     if re.search(r"\n{3,}", dest):
-        ctx.warning(f"has more than one successive empty line: {name} {dest}")
+        ctx.warning("has more than one successive empty line")
 
 
 if __name__ == "__main__":
@@ -128,7 +136,7 @@ if __name__ == "__main__":
     if args:
         report = Report()
         for arg in sys.argv[1:]:
-            lint(report, arg)
+            lint(report, pathlib.Path(arg))
         print(f"{report.errors} error(s), {report.warnings} warning(s)")
         if report.errors:
             sys.exit(1)
