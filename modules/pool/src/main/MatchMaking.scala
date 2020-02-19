c46033321daec27af5b98d2edb036541a59f0252
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-02-19 09:28:48 -0600

    make better but slower pool pairings

--- modules/pool/src/main/MatchMaking.scala
+++ modules/pool/src/main/MatchMaking.scala
@@ -24,12 +24,15 @@ object MatchMaking {
   private object wmMatching {
 
     // above that, no pairing is allowed
-    // 1800 ~> 300
-    // 2500 ~> 450
-    // 3000 ~> 650
+    // 1000 ~> 150
+    // 1200 ~> 150
+    // 1500 ~> 150
+    // 2000 ~> 200
+    // 2500 ~> 250
+    // 3000 ~> 300
     private def ratingToMaxScore(rating: Int) =
-      if (rating < 2000) 300
-      else rating / 3.5 - 250
+      if (rating < 1500) 150
+      else rating / 10
 
     // quality of a potential pairing. Lower is better.
     // None indicates a forbidden pairing
@@ -38,23 +41,30 @@ object MatchMaking {
         missBonus(a) atMost missBonus(b)
       } + {
         rangeMalus(a, b) + rangeMalus(b, a)
+      } - {
+        rangeBonus(a, b)
       } + {
         blockMalus(a, b) + blockMalus(b, a)
       } - {
         ragesitBonus(a, b)
       }
-      val maxScore = ratingToMaxScore(a.rating atLeast b.rating)
+      val maxScore = ratingToMaxScore(a.rating atMost b.rating)
       if (score <= maxScore) Some(score) else None
     }
 
     // score bonus based on how many waves the member missed
     // when the user's sit counter is lower than -3, the maximum bonus becomes lower
     private def missBonus(p: PoolMember) =
-      (p.misses * 15) atMost ((460 + (p.rageSitCounter atMost -3) * 20) atLeast 0)
+      (p.misses * 12) atMost ((460 + (p.rageSitCounter atMost -3) * 20) atLeast 0)
 
     // big malus if players have conflicting rating ranges
     private def rangeMalus(a: PoolMember, b: PoolMember) =
-      if (a.ratingRange.exists(!_.contains(b.rating))) 6000 else 0
+      if (a.ratingRange.exists(!_.contains(b.rating))) 9000 else 0
+
+    // bonus if both players have rating ranges, and they're compatible
+    private def rangeBonus(a: PoolMember, b: PoolMember) =
+      if (a.ratingRange.exists(_ contains b.rating) && b.ratingRange.exists(_ contains a.rating)) 200
+      else 0
 
     // huge malus if players block each other
     private def blockMalus(a: PoolMember, b: PoolMember) =
@@ -64,10 +74,10 @@ object MatchMaking {
     // bonus if the two players both have a bad sit counter
     // malus (so negative number as bonus) if neither of those are true, meaning that their sit counters are far away (e.g. 0 and -5)
     private def ragesitBonus(a: PoolMember, b: PoolMember) =
-      if (a.rageSitCounter >= -2 && b.rageSitCounter >= -2) 50         // good players
-      else if (a.rageSitCounter <= -10 && b.rageSitCounter <= -10) 100 // very bad players
-      else if (a.rageSitCounter <= -5 && b.rageSitCounter <= -5) 50    // bad players
-      else (abs(a.rageSitCounter - b.rageSitCounter) atMost 10) * -10  // match of good and bad player
+      if (a.rageSitCounter >= -2 && b.rageSitCounter >= -2) 30        // good players
+      else if (a.rageSitCounter <= -10 && b.rageSitCounter <= -10) 80 // very bad players
+      else if (a.rageSitCounter <= -5 && b.rageSitCounter <= -5) 30   // bad players
+      else (abs(a.rageSitCounter - b.rageSitCounter) atMost 10) * -30 // match of good and bad player
 
     def apply(members: Vector[PoolMember]): Option[Vector[Couple]] = {
       WMMatching(members.toArray, pairScore).fold(
