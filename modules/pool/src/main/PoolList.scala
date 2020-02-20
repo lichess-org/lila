0f9090796622eb30f575d64250303701d16c7177
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-02-20 14:06:32 -0600

    tweak 5+0 pool

--- modules/pool/src/main/PoolList.scala
+++ modules/pool/src/main/PoolList.scala
@@ -11,7 +11,7 @@ object PoolList {
     PoolConfig(2 ++ 1, Wave(18 seconds, 30 players)),
     PoolConfig(3 ++ 0, Wave(12 seconds, 40 players)),
     PoolConfig(3 ++ 2, Wave(22 seconds, 30 players)),
-    PoolConfig(5 ++ 0, Wave(10 seconds, 34 players)),
+    PoolConfig(5 ++ 0, Wave(14 seconds, 40 players)),
     PoolConfig(5 ++ 3, Wave(25 seconds, 26 players)),
     PoolConfig(10 ++ 0, Wave(13 seconds, 30 players)),
     PoolConfig(15 ++ 15, Wave(60 seconds, 20 players))
