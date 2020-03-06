8b5ba4bde0eec4fc5be788a6838afc9431417df2
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-03-06 12:05:33 +0100

    work around #5543 for gif export

--- modules/game/src/main/GifExport.scala
+++ modules/game/src/main/GifExport.scala
@@ -104,7 +104,7 @@ final class GifExport(
           case (g, Uci.WithSan(uci, _)) => (g, uci.some)
         })
         framesRec(steps.zip(game.moveTimes match {
-          case Some(moveTimes) => scaleMoveTimes(moveTimes).map(_.some) :+ None
+          case Some(moveTimes) => scaleMoveTimes(moveTimes).map(_.some) :+ None :+ None // one for last move, one for #5543
           case None            => LazyList.continually(None)
         }), Json.arr())
     }
