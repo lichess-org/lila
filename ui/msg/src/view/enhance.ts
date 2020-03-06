2c29de0ca84da2b4eb2cd95294cd06f491e1125b
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-03-06 09:45:36 -0600

    link all inbox images

--- ui/msg/src/view/enhance.ts
+++ ui/msg/src/view/enhance.ts
@@ -23,19 +23,21 @@ const expandUrl = (url: string) =>
 
 const imgurRegex = /https?:\/\/(?:i\.)?imgur\.com\/(\w+)(?:\.jpe?g|\.png|\.gif)?/;
 const expandImgur = (url: string) =>
-  imgurRegex.test(url) ? url.replace(imgurRegex, (_, id) => img(`https://i.imgur.com/${id}.jpg`)) : undefined;
+  imgurRegex.test(url) ? url.replace(imgurRegex, (_, id) => aImg(`https://i.imgur.com/${id}.jpg`)) : undefined;
 
 const giphyRegex = /https:\/\/(?:media\.giphy\.com\/media\/|giphy\.com\/gifs\/(?:\w+-)*)(\w+)(?:\/giphy\.gif)?/;
 const expandGiphy = (url: string) =>
-  giphyRegex.test(url) ? url.replace(giphyRegex, (_, id) => img(`https://media.giphy.com/media/${id}/giphy.gif`)) : undefined;
+  giphyRegex.test(url) ? url.replace(giphyRegex, (_, id) => aImg(`https://media.giphy.com/media/${id}/giphy.gif`)) : undefined;
 
-const expandImage = (url: string) => /\.(jpg|jpeg|png|gif)$/.test(url) ? a(url, img(url)) : undefined;
+const expandImage = (url: string) => /\.(jpg|jpeg|png|gif)$/.test(url) ? aImg(url) : undefined;
 
 const expandLink = (url: string) => a(url, url.replace(/^https?:\/\//, ''));
 
 const a = (href: string, body: string) => `<a target="_blank" href="${href}">${body}</a>`;
 
 const img = (src: string) => `<img src="${src}"/>`;
+
+const aImg = (src: string) => a(src, img(src))
 
 /* now with the iframe expansion */
 
