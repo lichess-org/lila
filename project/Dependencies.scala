936db4930206bc2c3604fecc8d5b07f5547b1815
Scala Steward <me@scala-steward.org>
2020-03-07 06:35:16 +0100

    Update kamon-core to 2.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -62,7 +62,7 @@ object Dependencies {
     val json    = "com.typesafe.play" %% "play-json" % "2.8.1"
   }
   object kamon {
-    val core     = "io.kamon" %% "kamon-core"           % "2.0.4"
+    val core     = "io.kamon" %% "kamon-core"           % "2.0.5"
     val influxdb = "io.kamon" %% "kamon-influxdb"       % "2.0.1-LILA"
     val metrics  = "io.kamon" %% "kamon-system-metrics" % "2.0.2"
   }
