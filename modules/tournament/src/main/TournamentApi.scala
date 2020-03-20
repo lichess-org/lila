909deea1bdef7aaf70f3454da4e5d7fc823c7c21
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-03-20 16:06:37 -0600

    show fewer user tournaments

--- modules/tournament/src/main/TournamentApi.scala
+++ modules/tournament/src/main/TournamentApi.scala
@@ -522,12 +522,24 @@ final class TournamentApi(
   }
 
   def fetchVisibleTournaments: Fu[VisibleTournaments] =
-    tournamentRepo.publicCreatedSorted(6 * 60) zip
-      tournamentRepo.publicStarted zip
-      tournamentRepo.finishedNotable(30) map {
+    tournamentRepo.publicCreatedSorted(6 * 60).flatMap(filterMajor) zip
+      tournamentRepo.publicStarted.flatMap(filterMajor) zip
+      tournamentRepo.finishedNotable(30).flatMap(filterMajor) map {
       case ((created, started), finished) =>
         VisibleTournaments(created, started, finished)
     }
+
+  private def filterMajor(tours: List[Tournament]): Fu[List[Tournament]] =
+    tours
+      .map { t =>
+        (fuccess(t.isScheduled) >>| lightUserApi
+          .async(t.createdBy)
+          .dmap(_.exists(_.title.isDefined))) dmap {
+          _ option t
+        }
+      }
+      .sequenceFu
+      .dmap(_.flatten)
 
   def playerInfo(tour: Tournament, userId: User.ID): Fu[Option[PlayerInfoExt]] =
     userRepo named userId flatMap {
