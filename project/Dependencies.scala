59738eab2b2064c778fbf21fa4410e5d6fd0847a
Scala Steward <me@scala-steward.org>
2020-03-22 00:24:17 +0100

    Update scaffeine to 4.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
   val maxmind     = "com.sanoma.cda"        %% "maxmind-geoip2-scala"           % "1.3.1-THIB"
   val prismic     = "io.prismic"            %% "scala-kit"                      % "1.2.17-THIB213"
   val scrimage    = "com.sksamuel.scrimage" %% "scrimage-core"                  % "2.1.8-SNAPSHOT"
-  val scaffeine   = "com.github.blemale"    %% "scaffeine"                      % "3.1.0" % "compile"
+  val scaffeine   = "com.github.blemale"    %% "scaffeine"                      % "4.0.0" % "compile"
   val googleOAuth = "com.google.auth"       % "google-auth-library-oauth2-http" % "0.20.0"
   val scalaUri    = "io.lemonlabs"          %% "scala-uri"                      % "2.2.0"
   val scalatags   = "com.lihaoyi"           %% "scalatags"                      % "0.8.5"
