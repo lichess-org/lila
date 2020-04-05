c8cefb8c6203dd4f39330d2c350c5cd4c813f242
Reinhard Bronner <reinhard_bronner@t-online.de>
2020-04-06 00:54:53 +0200

    copy link instead of show link text in pm All

--- app/views/team/admin.scala
+++ app/views/team/admin.scala
@@ -68,7 +68,12 @@ object admin {
 
     views.html.base.layout(
       title = title,
-      moreCss = cssTag("team")
+      moreCss = cssTag("team"),
+      moreJs = embedJsUnsafe(s"""
+           |$$('.copy-url-button').on('click', function(e) {
+           |$$('#form3-message').val(function(i, x) {return x + $$(e.target).data('copyurl') + '\\n'})
+           |})
+           |""".stripMargin),
     ) {
       main(cls := "page-menu page-small")(
         bits.menu(none),
@@ -91,8 +96,11 @@ object admin {
                     " ",
                     momentFromNow(t.startsAt),
                     " - ",
-                    netDomain,
-                    routes.Tournament.show(t.id).url
+                    a(
+                      dataIcon := "u",
+                      cls := "text copy-url-button",
+                      data.copyurl := s"${netDomain}${routes.Tournament.show(t.id).url}",
+                    )("Copy URL")
                   )
                 }
               )
