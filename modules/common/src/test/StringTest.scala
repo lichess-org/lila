28b4e6012397d51c668b78286306a0db0199a789
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-04-23 01:02:23 +0200

    update tests after xhtml -> html

--- modules/common/src/test/StringTest.scala
+++ modules/common/src/test/StringTest.scala
@@ -17,7 +17,7 @@ class StringTest extends Specification {
     "handle nl" in {
       val url = "http://imgur.com/gallery/pMtTE"
       String.html.richText(s"link to $url here\n") must_== raw {
-        s"""link to <a rel="nofollow noopener noreferrer" href="$url" target="_blank">$url</a> here<br />"""
+        s"""link to <a rel="nofollow noopener noreferrer" href="$url" target="_blank">$url</a> here<br>"""
       }
 
       String.html.richText(s"link\n", false) must_== raw("link\n")
