eaedc14c75242b9788a15c0203ab6c4bf5d503da
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-04-24 12:38:16 -0600

    tweak gm bootstrap script

--- bin/mongodb/gm-bootstrap.js
+++ bin/mongodb/gm-bootstrap.js
@@ -1,20 +1,23 @@
-var userIds = ['GeorgMeier'].map(u => u.toLowerCase());
-var perfType = 'bullet';
+var userIds = ['sasha'].map(u => u.toLowerCase());
+var perfTypes = ['bullet', 'blitz', 'rapid', 'classical'];
 var perf = {
   "gl": {
-    "r": 2500,
+    "r": 2700,
     "d": 150,
     "v": 0.06
   },
   "nb": NumberInt(0),
   "re": [ ]
 };
+var updateTournaments = false;
 
 userIds.forEach(id => {
   var user = db.user4.findOne({_id:id});
-  if (user && (!user.perfs[perfType] || !user.perfs[perfType].nb))
-    db.user4.update({_id:id},{$set:{['perfs.' + perfType]: perf}});
-  db.tournament_player.update(
+  perfTypes.forEach(pt => {
+    if (user && (!user.perfs[pt] || !user.perfs[pt].nb))
+      db.user4.update({_id:id},{$set:{['perfs.' + pt]: perf}});
+  });
+  if (updateTournaments) db.tournament_player.update(
     {uid:id,r:1500,m:1500},
     {$set:{r:NumberInt(perf.gl.r),m:NumberInt(perf.gl.r)}},
     {multi:1});
