8367183015a83210ff97b47c5960be976e093b4a
kraktus <56031107+kraktus@users.noreply.github.com>
2020-04-26 13:45:31 +0100

    Add colored coords for all themes

--- ui/common/src/coordsColour.ts
+++ ui/common/src/coordsColour.ts
@@ -8,8 +8,25 @@ export default function changeColourHandle(){
 		"blue3": ["#d9e0e6", "#315991"],
 		"canvas": ["#d7daeb", "#547388"],
 		"wood": ["#d8a45b", "#9b4d0f"],
+		"wood2": ["#a38b5d", "#6c5017"],
+		"wood3": ["#d0ceca", "#755839"],
+		"maple": ["#e8ceab", "#bc7944"],
+		"maple2": ["#E2C89F", "#996633"],
+		"leather": ["#d1d1c9", "#c28e16"],
+		"green": ["#FFFFDD", "#6d8753"],
 		"brown": ["#F0D9B5", "#946f51"],
-		"pink": ["#f1f1c9", "#f07272"]
+		"pink": ["#E8E9B7", "#ED7272"],
+		"marble": ["#93ab91", "#4f644e"],
+		"blue-marble": ["#EAE6DD", "#7C7F87"],
+		"green-plastic": ["#f2f9bb", "#59935d"],
+		"green-glass": ["#fafa97", "#e4a836"],
+		"grey": ["#b8b8b8", "#7d7d7d"],
+		"metal": ["#c9c9c9", "#727272"],
+		"olive": ["#b8b19f", "#6d6655"],
+		"newspaper": ["#fff", "#8d8d8d"],
+		"purple": ["#9f90b0", "#7d4a8d"],
+		"purple-diag": ["#E5DAF0", "#957AB0"],
+		"ic": ["#ececec", "#c1c18e"]
 	}
 	let color: string
 	for (color of list_class){
