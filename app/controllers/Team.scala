ba20da6b2e67e9c203f9df82524889fbad19fc07
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-05-01 12:58:06 -0600

    let class teachers create more teams - for #6524

--- app/controllers/Team.scala
+++ app/controllers/Team.scala
@@ -410,7 +410,8 @@ You received this message because you are part of the team lichess.org${routes.T
 
   private def OnePerWeek[A <: Result](me: UserModel)(a: => Fu[A])(implicit ctx: Context): Fu[Result] =
     api.hasCreatedRecently(me) flatMap { did =>
-      if (did && !Granter(_.ManageTeam)(me)) Forbidden(views.html.site.message.teamCreateLimit).fuccess
+      if (did && !Granter(_.Teacher)(me) && !Granter(_.ManageTeam)(me))
+        Forbidden(views.html.site.message.teamCreateLimit).fuccess
       else a
     }
 
