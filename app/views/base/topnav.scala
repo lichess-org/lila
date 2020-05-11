e9892b884da7dfdb12f5af6b8bb9c14e04872dd7
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-05-11 14:38:17 -0600

    show swiss tournaments in top menu

--- app/views/base/topnav.scala
+++ app/views/base/topnav.scala
@@ -28,7 +28,8 @@ object topnav {
           if (ctx.noBot) a(href := "/?any#hook")(trans.createAGame())
           else a(href := "/?any#friend")(trans.playWithAFriend()),
           ctx.noBot option frag(
-            a(href := routes.Tournament.home())(trans.tournaments()),
+            a(href := routes.Tournament.home())(trans.arena.arenaTournaments()),
+            a(href := routes.Swiss.home())(trans.swiss.swissTournaments()),
             a(href := routes.Simul.home)(trans.simultaneousExhibitions())
           )
         )
