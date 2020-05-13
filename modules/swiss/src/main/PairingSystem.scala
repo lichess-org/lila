983deb71d19ecb65fcf267b9a6b27514c95dfe3e
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-05-12 18:56:10 -0600

    stream trf to file

--- modules/swiss/src/main/PairingSystem.scala
+++ modules/swiss/src/main/PairingSystem.scala
@@ -1,6 +1,7 @@
 package lila.swiss
 
 import akka.stream.scaladsl._
+import akka.util.ByteString
 import java.io.{ File, PrintWriter }
 import scala.concurrent.blocking
 import scala.sys.process._
@@ -11,28 +12,22 @@ final private class PairingSystem(trf: SwissTrf, executable: String)(implicit
 ) {
 
   def apply(swiss: Swiss): Fu[List[SwissPairing.ByeOrPending]] =
-    trf(swiss)
-      .toMat(Sink.fold("") {
-        case (a, l) => s"$l\n$a"
-      })(Keep.right)
-      .run map invoke map reader
+    invoke(swiss, trf(swiss)) map reader
 
-  private def invoke(input: String): List[String] =
-    lila.mon.chronoSync(_.swiss.bbpairing) {
-      blocking {
-        withTempFile(input) { file =>
-          val command = s"$executable --dutch $file -p"
-          val stdout  = new collection.mutable.ListBuffer[String]
-          val stderr  = new StringBuilder
-          val status  = command ! ProcessLogger(stdout append _, stderr append _)
-          if (status != 0) {
-            val error = stderr.toString
-            if (error contains "No valid pairing exists") Nil
-            else throw new PairingSystem.BBPairingException(error, input)
-          } else stdout.toList
-        }
+  private def invoke(swiss: Swiss, input: Source[String, _]): Fu[List[String]] =
+    withTempFile(input) { file =>
+      val command = s"$executable --dutch $file -p"
+      val stdout  = new collection.mutable.ListBuffer[String]
+      val stderr  = new StringBuilder
+      val status = blocking {
+        command ! ProcessLogger(stdout append _, stderr append _)
       }
-    }
+      if (status != 0) {
+        val error = stderr.toString
+        if (error contains "No valid pairing exists") Nil
+        else throw new PairingSystem.BBPairingException(error, swiss)
+      } else stdout.toList
+    }.mon(_.swiss.bbpairing)
 
   private def reader(output: List[String]): List[SwissPairing.ByeOrPending] =
     output
@@ -54,21 +49,20 @@ final private class PairingSystem(trf: SwissTrf, executable: String)(implicit
   /** NOTE: This function uses the createTempFile function from the File class. The prefix and
     * suffix must be at least 3 characters long, otherwise this function throws an IllegalArgumentException.
     */
-  def withTempFile[A](contents: String)(f: File => A): A = {
+  def withTempFile[A](contents: Source[String, _])(f: File => A): Fu[A] = {
     val file = File.createTempFile("lila-", "-swiss")
-    val p    = new PrintWriter(file, "UTF-8")
-    try {
-      p.write(contents)
-      p.flush()
-      val res = f(file)
-      res
-    } finally {
-      p.close()
-      file.delete()
-    }
+    contents
+      .intersperse("\n")
+      .map(ByteString.apply)
+      .runWith(FileIO.toPath(file.toPath))
+      .map { _ =>
+        val res = f(file)
+        file.delete()
+        res
+      }
   }
 }
 
 private object PairingSystem {
-  case class BBPairingException(val message: String, val input: String) extends lila.base.LilaException
+  case class BBPairingException(val message: String, val swiss: Swiss) extends lila.base.LilaException
 }
