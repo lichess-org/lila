d1c1523cfedb2e99a9452c289c0b29b203a97da7
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-05-16 11:22:36 -0600

    net.http.log config

--- app/http/HttpFilter.scala
+++ app/http/HttpFilter.scala
@@ -11,7 +11,7 @@ final class HttpFilter(env: Env)(implicit val mat: Materializer) extends Filter 
   private val httpMon     = lila.mon.http
   private val net         = env.net
   private val logger      = lila.log("http")
-  private val logRequests = true
+  private val logRequests = env.config.get[Boolean]("net.http.log")
 
   def apply(nextFilter: RequestHeader => Fu[Result])(req: RequestHeader): Fu[Result] =
     if (HTTPRequest isAssets req) nextFilter(req) dmap { result =>
@@ -35,9 +35,9 @@ final class HttpFilter(env: Env)(implicit val mat: Materializer) extends Filter 
     val reqTime    = nowMillis - startTime
     val statusCode = result.header.status
     val client     = HTTPRequest clientName req
-    if (env.isDev && logRequests)
-      logger.info(s"$statusCode $client $req ${req.method} $actionName ${reqTime}ms")
-    else httpMon.time(actionName, client, req.method, statusCode).record(reqTime)
+    if (env.isDev) {
+      if (logRequests) logger.info(s"$statusCode $client $req ${req.method} $actionName ${reqTime}ms")
+    } else httpMon.time(actionName, client, req.method, statusCode).record(reqTime)
   }
 
   private def redirectWrongDomain(req: RequestHeader): Option[Result] =
