86b752dc591889b6318791ba63baed46f6d7bd22
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-05-16 21:39:34 -0600

    tweak team page

--- app/views/team/show.scala
+++ app/views/team/show.scala
@@ -63,7 +63,7 @@ object show {
         }
       )(
         div(cls := "box__top")(
-          h1(cls := "text", dataIcon := "f")(t.name, " ", em(trans.team.team.txt().toUpperCase)),
+          h1(cls := "text", dataIcon := "f")(t.name)),
           div(
             if (t.disabled) span(cls := "staff")("CLOSED")
             else nbMembers.plural(t.nbMembers, strong(t.nbMembers.localize))
