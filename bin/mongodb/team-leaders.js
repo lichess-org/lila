01e5268602a4f915e856a183f0d9f3d6b33f0168
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-05-22 11:21:24 -0600

    fix db team leaders

--- bin/mongodb/team-leaders.js
+++ bin/mongodb/team-leaders.js
@@ -1,4 +1,9 @@
-db.team.find({},{createdBy:1}).forEach(t => {
-  db.team.update({_id:t._id},{$set:{leaders:[t.createdBy]}});
+db.team.find().forEach(t => {
+  if (t.leaders.length == 1 && t.leaders[0] == t.createdBy) return;
+  const leaders = db.team_member.distinct('user',{team:t._id,user:{$in:t.leaders}});
+  if (leaders.length != t.leaders.length) {
+    if (!leaders.length) leaders.push(t.createdBy);
+    print(`${t._id} ${t.name} ${t.leaders.join(',')} -> ${leaders.join(',')}`);
+    db.team.update({_id:t._id},{$set:{leaders:leaders}});
+  }
 });
-db.team.ensureIndex({leaders:1},{background:1})
