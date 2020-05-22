ac78c66ba9e2ace85103c1bc22183ddeef0b94ed
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-05-22 17:43:25 -0600

    swiss cleanup scripts

--- bin/mongodb/swiss-fix.js
+++ bin/mongodb/swiss-fix.js
@@ -1,5 +1,5 @@
 db.swiss.find({featurable:true,startsAt:{$lt:new Date(Date.now() - 1000 * 3600 * 24)},'settings.i':{$lt:3600 * 24}}).forEach(s => {
-  print(`${s._id} ${s.name} round:${s.round}`);
+  print(`FINISH ${s._id} ${s.name} round:${s.round}`);
   db.swiss.update({_id:s._id},{
     $set:{
       finishedAt: new Date(),
@@ -11,4 +11,15 @@ db.swiss.find({featurable:true,startsAt:{$lt:new Date(Date.now() - 1000 * 3600 *
       featurable: 1
     }
   });
+});
+
+db.swiss.find({nbOngoing:{$ne:0}}).forEach(s => {
+  const count = db.swiss_pairing.count({s:s._id,t:true});
+  if (count != s.nbOngoing) {
+    print(`nbOngoing ${s._id} ${s.name} ${s.nbOngoing} -> ${count}`);
+    db.swiss.update({_id:s._id},{$set:{
+      nbOngoing:NumberInt(count),
+      nextRoundAt: new Date(Date.now() + 1000 * 30)
+    }});
+  }
 });
