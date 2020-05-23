09f56b7ec8fa5494832d9e1cbc1c29a4d322e883
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-05-23 08:48:23 -0600

    don't show the streamer badge before they actually stream

--- modules/db/src/main/package.scala
+++ modules/db/src/main/package.scala
@@ -7,6 +7,9 @@ package object db extends PackageObject {
   def recoverDuplicateKey[A](f: WriteResult => A): PartialFunction[Throwable, A] = {
     case wr: WriteResult if isDuplicateKey(wr) => f(wr)
   }
+  def ignoreDuplicateKey: PartialFunction[Throwable, Unit] = {
+    case wr: WriteResult if isDuplicateKey(wr) => ()
+  }
 
   def isDuplicateKey(wr: WriteResult) = wr.code.contains(11000)
 
