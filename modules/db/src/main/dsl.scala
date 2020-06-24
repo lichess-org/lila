0f3f60d333ccfaac8038b043b1ced8e6259bf82a
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-06-24 11:36:35 +0200

    db dsl tweaks

--- modules/db/src/main/dsl.scala
+++ modules/db/src/main/dsl.scala
@@ -133,7 +133,7 @@ trait dsl {
   /** Matches values that are greater than or equal to the value specified in the query. */
   def $gte[T: BSONWriter](value: T) = $doc("$gte" -> value)
 
-  /** Matches any of the values that exist in an array specified in the query.*/
+  /** Matches any of the values that exist in an array specified in the query. */
   def $in[T: BSONWriter](values: T*) = $doc("$in" -> values)
 
   /** Matches values that are less than the value specified in the query. */
@@ -170,9 +170,6 @@ trait dsl {
       $doc("$type" -> value)
     }
   }
-
-  def $currentDate(items: (String, CurrentDateValueProducer[_])*): Bdoc =
-    $doc("$currentDate" -> $doc(items.map(item => (item._1, item._2.produce))))
 
   // End of Top Level Field Update Operators
   //**********************************************************************************************//
@@ -245,7 +242,6 @@ trait dsl {
     *  "age" $gt 50 $lt 60
     *  "age" $gte 50 $lte 60
     * }}}
-    *
     */
   case class CompositeExpression(field: String, value: Bdoc)
       extends Expression[Bdoc]
@@ -272,7 +268,7 @@ trait dsl {
       CompositeExpression(field, append($doc("$gte" -> value)))
     }
 
-    /** Matches any of the values that exist in an array specified in the query.*/
+    /** Matches any of the values that exist in an array specified in the query. */
     def $in[T: BSONWriter](values: Iterable[T]): SimpleExpression[Bdoc] = {
       SimpleExpression(field, $doc("$in" -> values))
     }
