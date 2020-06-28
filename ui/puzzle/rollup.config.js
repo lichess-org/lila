360a5dac92b3044fa07fce35f38405c382fba220
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-06-28 21:40:51 +0200

    unify ui/build using package.json scripts

--- ui/puzzle/rollup.config.js
+++ ui/puzzle/rollup.config.js
@@ -3,33 +3,34 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: 'src/main.ts',
-  output: [
-    {
-      file: '../../public/compiled/lichess.puzzle.js',
-      format: 'iife',
-      name: 'LichessPuzzle',
-    },
-    {
-      file: '../../public/compiled/lichess.puzzle.min.js',
-      format: 'iife',
-      name: 'LichessPuzzle',
-      plugins: [
-        terser({
-          output: {
-            safari10: true,
-            comments: false,
-          },
-        }),
-      ],
-    },
-  ],
-  plugins: [
-    resolve(),
-    typescript(),
-    commonjs({
-      extensions: ['.js', '.ts'],
-    }),
-  ]
-};
+export default args => {
+  return {
+    input: 'src/main.ts',
+    output: [
+      args['config-prod'] ? {
+        file: '../../public/compiled/lichess.puzzle.min.js',
+        format: 'iife',
+        name: 'LichessPuzzle',
+        plugins: [
+          terser({
+            output: {
+              safari10: true,
+              comments: false,
+            },
+          }),
+        ],
+      } : {
+        file: '../../public/compiled/lichess.puzzle.js',
+        format: 'iife',
+        name: 'LichessPuzzle',
+      }
+    ],
+    plugins: [
+      resolve(),
+      typescript(),
+      commonjs({
+        extensions: ['.js', '.ts'],
+      }),
+    ]
+  };
+}
