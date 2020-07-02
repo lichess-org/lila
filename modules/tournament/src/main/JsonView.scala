b280646bf4664bedbdcbf7ca8c5ae3baef3ad679
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-07-02 12:51:27 +0200

    add /api/tournament/:id/teams API endpoint for team battles - for #6756

--- modules/tournament/src/main/JsonView.scala
+++ modules/tournament/src/main/JsonView.scala
@@ -367,7 +367,7 @@ final class JsonView(
       }
   }
 
-  private def getTeamStanding(tour: Tournament): Fu[Option[JsArray]] =
+  def getTeamStanding(tour: Tournament): Fu[Option[JsArray]] =
     tour.isTeamBattle ?? {
       teamStandingCache get tour.id dmap some
     }
