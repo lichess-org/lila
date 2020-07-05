921ffea924d1b13438d486c961ff38be37857e76
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-07-05 11:15:28 +0200

    fix team battle update

--- modules/tournament/src/main/TournamentApi.scala
+++ modules/tournament/src/main/TournamentApi.scala
@@ -145,7 +145,10 @@ final class TournamentApi(
       filterExistingTeamIds: Set[TeamID] => Fu[Set[TeamID]]
   ): Funit =
     filterExistingTeamIds(data.potentialTeamIds) flatMap { teamIds =>
-      tournamentRepo.setTeamBattle(tour.id, TeamBattle(teamIds, data.nbLeaders))
+      tournamentRepo.setTeamBattle(tour.id, TeamBattle(teamIds, data.nbLeaders)) >>
+        tour.teamBattle.?? { prev =>
+          tournamentRepo.setForTeams(tour.id, prev.teams intersect teamIds).void
+        }
     }
 
   private val hadPairings = new lila.memo.ExpireSetMemo(1 hour)
