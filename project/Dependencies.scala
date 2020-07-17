7fd68337c6590b704939c3ef49f2775457dddd4f
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-07-17 12:21:58 +0200

    upgrade scalachess - closes #6984

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val scalalib    = "com.github.ornicar"   %% "scalalib"                        % "6.8"
   val hasher      = "com.roundeights"      %% "hasher"                          % "1.2.1"
   val jodaTime    = "joda-time"             % "joda-time"                       % "2.10.6"
-  val chess       = "org.lichess"          %% "scalachess"                      % "9.3.1"
+  val chess       = "org.lichess"          %% "scalachess"                      % "9.3.2"
   val compression = "org.lichess"          %% "compression"                     % "1.5"
   val maxmind     = "com.sanoma.cda"       %% "maxmind-geoip2-scala"            % "1.3.1-THIB"
   val prismic     = "io.prismic"           %% "scala-kit"                       % "1.2.18-THIB213"
