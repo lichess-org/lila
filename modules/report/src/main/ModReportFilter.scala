c7462465ae5dfaad0b735873aac61b9e671eca51
Lakin Wecker <lakin@structuredabstraction.com>
2020-07-21 10:19:19 -0600

    Use builtin cache object, instead of homegrown solution.
    Much nicer code as a result.

--- modules/report/src/main/ModReportFilter.scala
+++ modules/report/src/main/ModReportFilter.scala
@@ -1,9 +1,10 @@
 package lila.report
 
 import lila.user.User
-import org.joda.time.DateTime
+import com.github.blemale.scaffeine.Cache
+import scala.concurrent.duration.FiniteDuration
 
-case class TempThreshold(score: Int, lastUsed: DateTime)
+case class TempThreshold(score: Int)
 
 final class ModReportFilter {
 
@@ -13,7 +14,9 @@ final class ModReportFilter {
   // Also, this value shouldn't be in the DB. If we ever move the other value to the
   // db we should move this to a cookie or something. I intend
   // for this to be a temporary cookie of some sort.
-  private var modTempThreshold = Map.empty[User.ID, Option[TempThreshold]]
+  private val modTempThreshold: Cache[User.ID, TempThreshold] = lila.memo.CacheApi.scaffeineNoScheduler
+    .expireAfterAccess(FiniteDuration(1, "hour"))
+    .build[User.ID, TempThreshold]()
 
   def get(mod: User): Option[Room] = modIdFilter.get(mod.id).flatten
 
@@ -22,17 +25,12 @@ final class ModReportFilter {
 
   def getThreshold(mod: User): Option[Int] =
     modTempThreshold
-      .get(mod.id)
-      .flatten
-      .filter(_.lastUsed isAfter DateTime.now.minusHours(1))
-      .map(t => {
-        modTempThreshold = modTempThreshold + (mod.id -> some(TempThreshold(t.score, DateTime.now)))
-        t.score
-      })
+      .getIfPresent(mod.id)
+      .map(_.score)
 
   def updateThreshold(mod: User, score: Option[Int]): Option[Int] =
     score.fold(getThreshold(mod))(score => {
-      modTempThreshold = modTempThreshold + (mod.id -> some(TempThreshold(score, DateTime.now)))
+      modTempThreshold.put(mod.id, TempThreshold(score))
       some(score)
     })
 }
