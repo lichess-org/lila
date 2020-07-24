3c13919ee7e397b679657b6bc019d79bb462aa49
Thibault Duplessis <t@lichess.org>
2020-07-24 19:39:50 +0200

    upgrade compression lib

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val hasher      = "com.roundeights"      %% "hasher"                          % "1.2.1"
   val jodaTime    = "joda-time"             % "joda-time"                       % "2.10.6"
   val chess       = "org.lichess"          %% "scalachess"                      % "9.3.2"
-  val compression = "org.lichess"          %% "compression"                     % "1.5"
+  val compression = "org.lichess"          %% "compression"                     % "1.6"
   val maxmind     = "com.sanoma.cda"       %% "maxmind-geoip2-scala"            % "1.3.1-THIB"
   val prismic     = "io.prismic"           %% "scala-kit"                       % "1.2.18-THIB213"
   val scrimage    = "com.sksamuel.scrimage" % "scrimage-core"                   % "4.0.5"
