6c5819192b45a1cffaa678467ad632e54b142a3e
Thibault Duplessis <t@lichess.org>
2020-08-02 09:46:29 +0200

    upgrade kamon

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
     val json    = "com.typesafe.play" %% "play-json" % "2.9.0"
   }
   object kamon {
-    val version    = "2.1.3"
+    val version    = "2.1.4"
     val core       = "io.kamon" %% "kamon-core"           % version
     val influxdb   = "io.kamon" %% "kamon-influxdb"       % version
     val metrics    = "io.kamon" %% "kamon-system-metrics" % version
