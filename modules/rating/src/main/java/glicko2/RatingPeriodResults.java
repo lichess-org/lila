742bb4d3592920e036c68bdde4ad58969e349f7e
Thibault Duplessis <t@lichess.org>
2020-08-16 12:19:29 +0200

    {master} automated code cleanup

--- modules/rating/src/main/java/glicko2/RatingPeriodResults.java
+++ modules/rating/src/main/java/glicko2/RatingPeriodResults.java
@@ -13,78 +13,78 @@ import java.util.Set;
 
 /**
  * This class holds the results accumulated over a rating period.
- * 
+ *
  * @author Jeremy Gooch
  */
 public class RatingPeriodResults {
-	private List<Result> results = new ArrayList<Result>();
+	private final List<Result> results = new ArrayList<Result>();
 	private Set<Rating> participants = new HashSet<Rating>();
 
-	
+
 	/**
 	 * Create an empty resultset.
 	 */
 	public RatingPeriodResults() {}
-	
+
 
 	/**
 	 * Constructor that allows you to initialise the list of participants.
-	 * 
+	 *
 	 * @param participants (Set of Rating objects)
 	 */
 	public RatingPeriodResults(Set<Rating> participants) {
 		this.participants = participants;
 	}
-	
-	
+
+
 	/**
 	 * Add a result to the set.
-	 * 
+	 *
 	 * @param winner
 	 * @param loser
 	 */
 	public void addResult(Rating winner, Rating loser) {
 		Result result = new Result(winner, loser);
-		
+
 		results.add(result);
 	}
-	
-	
+
+
 	/**
 	 * Record a draw between two players and add to the set.
-	 * 
+	 *
 	 * @param player1
 	 * @param player2
 	 */
 	public void addDraw(Rating player1, Rating player2) {
 		Result result = new Result(player1, player2, true);
-		
+
 		results.add(result);
 	}
-	
-	
+
+
 	/**
 	 * Get a list of the results for a given player.
-	 * 
+	 *
 	 * @param player
 	 * @return List of results
 	 */
 	public List<Result> getResults(Rating player) {
 		List<Result> filteredResults = new ArrayList<Result>();
-		
+
 		for ( Result result : results ) {
 			if ( result.participated(player) ) {
 				filteredResults.add(result);
 			}
 		}
-		
+
 		return filteredResults;
 	}
 
-	
+
 	/**
 	 * Get all the participants whose results are being tracked.
-	 * 
+	 *
 	 * @return set of all participants covered by the resultset.
 	 */
 	public Set<Rating> getParticipants() {
@@ -96,8 +96,8 @@ public class RatingPeriodResults {
 
 		return participants;
 	}
-	
-	
+
+
 	/**
 	 * Add a participant to the rating period, e.g. so that their rating will
 	 * still be calculated even if they don't actually compete.
@@ -107,8 +107,8 @@ public class RatingPeriodResults {
 	public void addParticipants(Rating rating) {
 		participants.add(rating);
 	}
-	
-	
+
+
 	/**
 	 * Clear the resultset.
 	 */
