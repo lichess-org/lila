b7b8efef2effdffa1bfde027340c04bc0d9edf19
Thibault Duplessis <t@lichess.org>
2020-08-16 12:19:29 +0200

    {master} automated code cleanup

--- modules/rating/src/main/java/glicko2/Result.java
+++ modules/rating/src/main/java/glicko2/Result.java
@@ -8,22 +8,22 @@ package org.goochjs.glicko2;
 
 /**
  * Represents the result of a match between two players.
- * 
+ *
  * @author Jeremy Gooch
  */
 public class Result {
 	private static final double POINTS_FOR_WIN = 1.0;
 	private static final double POINTS_FOR_LOSS = 0.0;
 	private static final double POINTS_FOR_DRAW = 0.5;
-	
+
 	private boolean isDraw = false;
-	private Rating winner;
-	private Rating loser;
-	
-	
+	private final Rating winner;
+	private final Rating loser;
+
+
 	/**
 	 * Record a new result from a match between two players.
-	 * 
+	 *
 	 * @param winner
 	 * @param loser
 	 */
@@ -35,11 +35,11 @@ public class Result {
 		this.winner = winner;
 		this.loser = loser;
 	}
-	
-	
+
+
 	/**
 	 * Record a draw between two players.
-	 * 
+	 *
 	 * @param player1
 	 * @param player2
 	 * @param isDraw (must be set to "true")
@@ -48,16 +48,16 @@ public class Result {
 		if (! isDraw || ! validPlayers(player1, player2) ) {
 			throw new IllegalArgumentException();
 		}
-		
+
 		this.winner = player1;
 		this.loser = player2;
 		this.isDraw = true;
 	}
 
-	
+
 	/**
 	 * Check that we're not doing anything silly like recording a match with only one player.
-	 * 
+	 *
 	 * @param player1
 	 * @param player2
 	 * @return
@@ -65,71 +65,71 @@ public class Result {
 	private boolean validPlayers(Rating player1, Rating player2) {
 		return !player1.equals(player2);
 	}
-	
-	
+
+
 	/**
 	 * Test whether a particular player participated in the match represented by this result.
-	 * 
+	 *
 	 * @param player
 	 * @return boolean (true if player participated in the match)
 	 */
 	public boolean participated(Rating player) {
 		return winner.equals(player) || loser.equals(player);
 	}
-	
-	
+
+
 	/**
 	 * Returns the "score" for a match.
-	 * 
+	 *
 	 * @param player
 	 * @return 1 for a win, 0.5 for a draw and 0 for a loss
 	 * @throws IllegalArgumentException
 	 */
 	public double getScore(Rating player) throws IllegalArgumentException {
 		double score;
-		
+
 		if ( winner.equals(player) ) {
 			score = POINTS_FOR_WIN;
 		} else if ( loser.equals(player) ) {
-			score = POINTS_FOR_LOSS;			
+			score = POINTS_FOR_LOSS;
 		} else {
 			throw new IllegalArgumentException("Player did not participate in match");
 		}
-		
+
 		if ( isDraw ) {
 			score = POINTS_FOR_DRAW;
 		}
-		
+
 		return score;
 	}
-	
-	
+
+
 	/**
 	 * Given a particular player, returns the opponent.
-	 * 
+	 *
 	 * @param player
 	 * @return opponent
 	 */
 	public Rating getOpponent(Rating player) {
 		Rating opponent;
-		
+
 		if ( winner.equals(player) ) {
 			opponent = loser;
 		} else if ( loser.equals(player) ) {
-			opponent = winner;			
+			opponent = winner;
 		} else {
 			throw new IllegalArgumentException("Player did not participate in match");
 		}
-		
+
 		return opponent;
 	}
-	
-	
+
+
 	public Rating getWinner() {
 		return this.winner;
 	}
 
-	
+
 	public Rating getLoser() {
 		return this.loser;
 	}
