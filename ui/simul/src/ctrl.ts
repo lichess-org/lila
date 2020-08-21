cbeda4a34501c106f5e12bc56366d6b14a2b21b7
Thibault Duplessis <t@lichess.org>
2020-08-21 11:38:02 +0200

    remove unused code

--- ui/simul/src/ctrl.ts
+++ ui/simul/src/ctrl.ts
@@ -1,10 +1,6 @@
 import { makeSocket, SimulSocket } from './socket';
 import xhr from './xhr';
-import * as status from 'game/status';
-import {
-  SimulData,
-  SimulOpts
-} from './interfaces';
+import { SimulData, SimulOpts } from './interfaces';
 
 export default class SimulCtrl {
 
@@ -39,17 +35,11 @@ export default class SimulCtrl {
   };
 
   teamBlock = () => this.data.team && !this.data.team.isIn;
-
   createdByMe = () => this.opts.userId === this.data.host.id;
   candidates = () => this.data.applicants.filter(a => !a.accepted);
   accepted = () => this.data.applicants.filter(a => a.accepted);
-  myCurrentPairing = () =>
-    this.opts.userId ? this.data.pairings.find(p =>
-      p.game.status < status.ids.mate && p.player.id === this.opts.userId
-    ) : null;
   acceptedContainsMe = () => this.accepted().some(a => a.player.id === this.opts.userId);
   applicantsContainsMe = () => this.candidates().some(a => a.player.id === this.opts.userId);
   containsMe = () => this.opts.userId && (this.applicantsContainsMe() || this.pairingsContainMe());
   pairingsContainMe = () => this.data.pairings.some(a => a.player.id === this.opts.userId);
-
 }
