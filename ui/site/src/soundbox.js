efc8ee691c56df130bf2d9b7c5d3336a74e6194c
Thibault Duplessis <t@lichess.org>
2020-08-28 11:58:39 +0200

    play with AudioContext

--- ui/site/src/soundbox.js
+++ ui/site/src/soundbox.js
@@ -3,6 +3,15 @@ class SoundBox {
 
   sounds = {}; // The loaded sounds and their instances
 
+  constructor() {
+    window.lichess.requestIdleCallback(this.ensureContext);
+  }
+
+  ensureContext = () => {
+    this.context = this.context || (window.AudioContext && new AudioContext());
+    if (this.context && this.context.state != 'running') this.context.resume();
+  }
+
   load = (name, path) => new Promise((resolve, reject) => {
     this.sounds[name] = new Audio(path);
     this.sounds[name].addEventListener("canplaythrough", resolve);
@@ -15,6 +24,7 @@ class SoundBox {
   play(name, volume = 1) {
     if (!this.sounds[name]) console.error(`Can't find sound ${name}`);
     else try {
+      this.ensureContext();
       const sound = this.sounds[name].cloneNode();
       sound.volume = volume;
       sound.play();
