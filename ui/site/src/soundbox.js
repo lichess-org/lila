71554343efe62f42b821919019773b9c524bc299
Thibault Duplessis <t@lichess.org>
2020-08-29 09:47:21 +0200

    include howler back, to fix iPad play issues

--- ui/site/src/soundbox.js
+++ ui/site/src/soundbox.js
@@ -1,36 +1,17 @@
-// loosely based on https://raw.githubusercontent.com/sbrl/soundbox/master/soundbox.js
 class SoundBox {
 
   sounds = {}; // The loaded sounds and their instances
 
-  constructor() {
-    window.lichess.requestIdleCallback(this.ensureContext);
+  loadOggOrMp3 = (name, path) => {
+    this.sounds[name] = new Howl({
+      src: ['ogg', 'mp3'].map(ext => `${path}.${ext}`)
+    });
   }
-
-  ensureContext = () => {
-    this.context = this.context || (window.AudioContext && new AudioContext());
-    if (this.context && this.context.state != 'running') this.context.resume();
-  }
-
-  load = (name, path) => new Promise((resolve, reject) => {
-    this.sounds[name] = new Audio(path);
-    this.sounds[name].addEventListener("canplaythrough", resolve);
-    this.sounds[name].addEventListener("error", reject);
-  });
-
-  loadOggOrMp3 = (name, path) =>
-    this.load(name, `${path}.ogg`).catch(() => this.load(name, `${path}.mp3`));
 
   play(name, volume = 1) {
-    if (!this.sounds[name]) console.error(`Can't find sound ${name}`);
-    else try {
-      this.ensureContext();
-      const sound = this.sounds[name].cloneNode();
-      sound.volume = volume;
-      sound.play();
-    } catch (e) {
-      console.log(e);
-    }
+    if (!this.sounds[name]) return console.error(`Can't find sound ${name}`);
+    this.sounds[name].volume(volume);
+    this.sounds[name].play();
   };
 }
 lichess.soundBox = new SoundBox
