5b49640f160521000bfb2969e7ba833ac1815084
Thibault Duplessis <t@lichess.org>
2020-08-31 18:00:40 +0200

    simplify round markup

--- ui/round/src/view/replay.ts
+++ ui/round/src/view/replay.ts
@@ -10,7 +10,7 @@ import * as util from '../util';
 import RoundController from '../ctrl';
 import { Step, MaybeVNodes, RoundData } from '../interfaces';
 
-const scrollMax = 99999, moveTag = 'u8t', indexTag = 'i5z', indexTagUC = indexTag.toUpperCase(), movesTag = 'l2j', rmovesTag = 'x2n', activeClass = 'a1t', mutedTag = 'iu9.active';
+const scrollMax = 99999, moveTag = 'u8t', indexTag = 'i5z', indexTagUC = indexTag.toUpperCase(), movesTag = 'x2n', rmovesTag = 'rm5', activeClass = 'a1t';
 
 const autoScroll = throttle(100, (movesEl: HTMLElement, ctrl: RoundController) =>
   window.requestAnimationFrame(() => {
@@ -204,7 +204,6 @@ export function render(ctrl: RoundController): VNode | undefined {
     }, renderMoves(ctrl));
   return ctrl.nvui ? undefined : h(rmovesTag, [
     renderButtons(ctrl),
-    h(mutedTag, '-'),
     initMessage(d, ctrl.trans.noarg) || (moves ? (
       col1 ? h('div.col1-moves', [
         col1Button(ctrl, -1, 'Y', ctrl.ply == round.firstPly(d)),
