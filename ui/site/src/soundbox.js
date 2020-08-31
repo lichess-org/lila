c486ebca719420c3ca7d382acfd7237aa91d7f3c
Thibault Duplessis <t@lichess.org>
2020-08-31 09:43:45 +0200

    resume audio context before playing a sound

--- ui/site/src/soundbox.js
+++ ui/site/src/soundbox.js
@@ -2,16 +2,18 @@ class SoundBox {
 
   sounds = {}; // The loaded sounds and their instances
 
-  loadOggOrMp3 = (name, path) => {
+  loadOggOrMp3 = (name, path) => 
     this.sounds[name] = new Howl({
       src: ['ogg', 'mp3'].map(ext => `${path}.${ext}`)
     });
-  }
 
   play(name, volume = 1) {
-    if (!this.sounds[name]) return console.error(`Can't find sound ${name}`);
-    this.sounds[name].volume(volume);
-    this.sounds[name].play();
+    const doPlay = () => {
+      this.sounds[name].volume(volume);
+      this.sounds[name].play();
+    };
+    if (Howler.ctx.state == "suspended") Howler.ctx.resume().then(doPlay);
+    else doPlay();
   };
 }
 lichess.soundBox = new SoundBox
