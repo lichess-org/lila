eb22f417943280144fe513bde641f6a075061c90
Thibault Duplessis <t@lichess.org>
2020-09-03 10:23:57 +0200

    fix courtesy conditions

--- ui/round/src/ctrl.ts
+++ ui/round/src/ctrl.ts
@@ -480,10 +480,10 @@ export default class RoundController {
       d.player.ratingDiff = o.ratingDiff[d.player.color];
       d.opponent.ratingDiff = o.ratingDiff[d.opponent.color];
     }
-    if (!d.player.spectator && d.game.turns > 6 && !d.tournament && !d.swiss) {
+    if (!d.player.spectator && d.game.turns > 1) {
       const key = o.winner ? (d.player.color === o.winner ? 'victory' : 'defeat') : 'draw';
       li.sound[key]();
-      if (key != 'victory' && li.storage.get('courtesy')) 
+      if (key != 'victory' && d.game.turns > 6 && !d.tournament && !d.swiss && li.storage.get('courtesy')) 
         this.opts.chat?.instance?.then(c => c.post('Good game, well played'));
     }
     if (d.crazyhouse) crazyEndHook();
