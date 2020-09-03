93de5a5db26c83da8b9dba1582aeea5ed0949ba0
Thibault Duplessis <t@lichess.org>
2020-09-03 10:36:27 +0200

    code golf

--- ui/site/src/component/jquery-ajax.ts
+++ ui/site/src/component/jquery-ajax.ts
@@ -1,7 +1,7 @@
 $.ajaxSetup({
   cache: false
 });
-$.ajaxTransport('script', function(s) {
+$.ajaxTransport('script', s => {
   // Monkeypatch jQuery to load scripts with nonce. Upstream patch:
   // - https://github.com/jquery/jquery/pull/3766
   // - https://github.com/jquery/jquery/pull/3782
@@ -9,24 +9,20 @@ $.ajaxTransport('script', function(s) {
   // https://github.com/jquery/jquery/blob/master/src/ajax/script.js
   let script: JQuery, callback;
   return {
-    send: function(_, complete) {
+    send(_, complete) {
       script = $("<script>").prop({
         nonce: document.body.getAttribute('data-nonce'), // Add the nonce!
         charset: s.scriptCharset,
         src: s.url
-      }).on("load error", callback = function(evt) {
+      }).on("load error", callback = evt => {
         script.remove();
         callback = null;
-        if (evt) {
-          complete(evt.type === "error" ? 404 : 200, evt.type);
-        }
+        if (evt) complete(evt.type === "error" ? 404 : 200, evt.type);
       });
       document.head.appendChild(script[0]);
     },
-    abort: function() {
-      if (callback) {
-        callback();
-      }
+    abort() {
+      if (callback) callback();
     }
   };
 });
