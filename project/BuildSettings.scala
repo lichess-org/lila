2cb5eba1019cac6421c21c202d11b3887be88f1c
Thibault Duplessis <t@lichess.org>
2020-09-05 15:27:23 +0200

    v 3.2

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,7 +6,7 @@ object BuildSettings {
 
   import Dependencies._
 
-  val lilaVersion        = "4.0"
+  val lilaVersion        = "3.2"
   val globalScalaVersion = "2.13.3"
 
   val useEpoll = sys.props.get("epoll").fold(false)(_.toBoolean)
