bdb14ca32fcb5857b8cedee3f4700edb214934e6
Thibault Duplessis <t@lichess.org>
2020-09-07 09:01:20 +0200

    Revert "hotfix login page"
    This reverts commit f86d19259e64ffd224b4cafbb7cc09559dd4e93d.

--- ui/site/src/site.lichess.globals.ts
+++ ui/site/src/site.lichess.globals.ts
@@ -1,5 +1,5 @@
 import StrongSocket from "./component/socket";
-import { requestIdleCallback, formAjax, numberFormat, escapeHtml } from "./component/functions";
+import { requestIdleCallback, numberFormat, escapeHtml } from "./component/functions";
 import makeChat from './component/chat';
 import once from './component/once';
 import spinnerHtml from './component/spinner';
@@ -41,7 +41,6 @@ export default function() {
   l.hopscotch = hopscotch;
   l.slider = slider;
   l.makeChat = makeChat;
-  l.formAjax = formAjax;
   l.numberFormat = numberFormat;
   l.idleTimer = idleTimer;
   l.pubsub = pubsub;
