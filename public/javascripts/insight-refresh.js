53700802ec11b224d71a1a589b24d9e868e9e1d5
Thibault Duplessis <t@lichess.org>
2020-09-08 07:35:50 +0200

    fix insights refresh

--- public/javascripts/insight-refresh.js
+++ public/javascripts/insight-refresh.js
@@ -1,14 +1,12 @@
-window.lichess.load.then(() => {
-  lichess.refreshInsightForm = function() {
-    $('form.insight-refresh:not(.armed)').addClass('armed').submit(function() {
-      fetch(this.action, {
-        method: 'post',
-        credentials: 'same-origin',
-      }).then(lichess.reload);
-      if (lichess.modal) lichess.modal($(this).find('.crunching'));
-      else $(this).replaceWith($(this).find('.crunching').show());
-      return false;
-    });
-  };
-  lichess.refreshInsightForm();
-});
+lichess.refreshInsightForm = function() {
+  $('form.insight-refresh:not(.armed)').addClass('armed').submit(function() {
+    fetch(this.action, {
+      method: 'post',
+      credentials: 'same-origin',
+    }).then(lichess.reload);
+    if (lichess.modal) lichess.modal($(this).find('.crunching'));
+    else $(this).replaceWith($(this).find('.crunching').show());
+    return false;
+  });
+}
+window.lichess.load.then(lichess.refreshInsightForm);
