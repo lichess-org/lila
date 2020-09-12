59a7a6823fce721fa5b9c9695012d8cea617ba4a
Thibault Duplessis <t@lichess.org>
2020-09-12 12:19:16 +0200

    migrate other flatpickrs

---
+++ ui/site/src/flatpickr.ts
@@ -0,0 +1,18 @@
+import flatpickr from "flatpickr";
+
+window.lichess.load.then(() => {
+
+  $(".flatpickr").each(function(this: HTMLInputElement) {
+    const config = this.classList.contains('flatpickr-utc') ? {} : {
+      dateFormat: 'Z',
+      altInput: true,
+      altFormat: 'Y-m-d h:i K',
+    };
+    flatpickr(this, {
+      minDate: 'today',
+      maxDate: new Date(Date.now() + 1000 * 3600 * 24 * 31 * 3),
+      monthSelectorType: 'static'
+      ...config
+    });
+  });
+});
