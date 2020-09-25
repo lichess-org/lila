21c1b77d160f80dcf06cb7ead3436d829fec77c3
Thibault Duplessis <t@lichess.org>
2020-09-25 11:47:39 +0200

    rename round.keyboardMove plugin

--- ui/round/src/keyboardMove.ts
+++ ui/round/src/keyboardMove.ts
@@ -115,7 +115,7 @@ export function render(ctrl: KeyboardMove) {
         autocomplete: false
       },
       hook: onInsert(input =>
-        lichess.loadModule('round.keyboard-move').then(() =>
+        lichess.loadModule('round.keyboardMove').then(() =>
           ctrl.registerHandler(lichess.keyboardMove({ input, ctrl }))
         )
       )
