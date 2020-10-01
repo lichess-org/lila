ebfc175aff01c07275d0b34d5d12ebfeec1cc368
kraktus <56031107+kraktus@users.noreply.github.com>
2020-10-01 17:41:20 +0100

    reorder flags (alphabetically)

--- modules/user/src/main/Countries.scala
+++ modules/user/src/main/Countries.scala
@@ -270,12 +270,12 @@ object Countries {
     C("ZW", "Zimbabwe")
   ).sortBy(_.name) ::: List(
     // whatever
+    C("EU", "European Union"),
     C("_adygea", "Adygea"),
     C("_east-turkestan", "East Turkestan"),
-    C("EU", "European Union"),
-    C("_united-nations", "United Nations"),
+    C("_pirate", "Pirate"),
     C("_rainbow", "Rainbow"),
-    C("_pirate", "Pirate")
+    C("_united-nations", "United Nations")
   )
 
   val allPairs = all map { c =>
