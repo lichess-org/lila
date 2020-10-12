e232091f845c7cc5f142c0eac0c7d90ea74f2574
Thibault Duplessis <t@lichess.org>
2020-10-12 18:19:55 +0200

    tweak and reformat event-countdown.js

--- public/javascripts/event-countdown.js
+++ public/javascripts/event-countdown.js
@@ -1,30 +1,32 @@
-$('.event .countdown').each(function() {
+lichess.load.then(() => {
+  $('.event .countdown').each(function() {
 
-  var $el = $(this);
-  var seconds = parseInt($(this).data('seconds')) - 1;
-  var target = new Date().getTime() + seconds * 1000;
+    var $el = $(this);
+    var seconds = parseInt($(this).data('seconds')) - 1;
+    var target = new Date().getTime() + seconds * 1000;
 
-  var second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
+    var second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
 
-  var redraw = function() {
+    var redraw = function() {
 
-    var distance = target - new Date().getTime();
+      var distance = target - new Date().getTime();
 
-    if (distance > 0) {
-      $el.find('.days').text(Math.floor(distance / (day))),
-      $el.find('.hours').text(Math.floor((distance % (day)) / (hour))),
-      $el.find('.minutes').text(Math.floor((distance % (hour)) / (minute))),
-      $el.find('.seconds').text(Math.floor((distance % (minute)) / second));
-    } else {
-      clearInterval(interval);
-      lichess.reload();
-    }
+      if (distance > 0) {
+        $el.find('.days').text(Math.floor(distance / (day))),
+          $el.find('.hours').text(Math.floor((distance % (day)) / (hour))),
+          $el.find('.minutes').text(Math.floor((distance % (hour)) / (minute))),
+          $el.find('.seconds').text(Math.floor((distance % (minute)) / second));
+      } else {
+        clearInterval(interval);
+        lichess.reload();
+      }
 
-  };
-  var interval = setInterval(redraw, second);
+    };
+    var interval = setInterval(redraw, second);
 
-  redraw();
-});
+    redraw();
+  });
+})
