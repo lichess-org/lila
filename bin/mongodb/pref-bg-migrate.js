b08e8a39d260e335c08e4728122f4a9e8456a4a9
Thibault Duplessis <t@lichess.org>
2020-10-14 08:03:38 +0200

    fix dark board migration script

--- bin/mongodb/pref-bg-migrate.js
+++ bin/mongodb/pref-bg-migrate.js
@@ -1,4 +1,4 @@
 db.pref.find({dark:{$exists:1}}, {dark:1,transp:1}).forEach(p => {
-  const bg = p.dark ? (p.transp ? 400 : 300) : 100;
-  db.pref.update({_id: p._id}, {$set:{bg: bg},$unset:{dark:1,transp:1}});
+  const bg = p.dark ? (p.transp ? 400 : 200) : 100;
+  db.pref.update({_id: p._id}, {$set:{bg: NumberInt(bg)},$unset:{dark:1,transp:1}});
 });
