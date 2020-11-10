55bb95f0de8f486f827878a89ad70b0a4d93e441
Vasia Patov <vasiapatov@gmail.com>
2020-11-10 01:23:04 -0500

    Implement password complexity meter. Uses zxcvbn library for complexity algorithm (only loaded on /signup and /account/passwd).

--- ui/site/src/passwordComplexity.ts
+++ ui/site/src/passwordComplexity.ts
@@ -1,21 +1,24 @@
-
-
+interface PasswordFeedback {
+  suggestions: string[];
+  warning: string;
+}
 
 export function addPasswordChangeListener(id: string) {
   const passwordInput = document.getElementById(id) as HTMLInputElement;
-  passwordInput.addEventListener('input', () => {updatePasswordComplexityMeter(passwordInput.value)});
+  passwordInput.addEventListener("input", () => {
+    updatePasswordComplexityMeter(passwordInput.value);
+  });
 }
 
 function updatePasswordComplexityMeter(password: string): void {
   const analysis = window.zxcvbn(password);
   updateMeter(analysis.score);
-  updateLabel(password.length, analysis.score);
+  updateLabel(password.length, analysis.score, analysis.feedback);
 }
-
 
 function updateMeter(score: number): void {
   const color =
-    score > 3 ? "green" : score > 2 ? "yellow" : score > 1 ? "orange" : "red";
+    score > 3 ? "green" : score > 2 ? "#ffc800" : score > 1 ? "#ff8000" : "red";
   const meter = document.querySelector(".password-complexity-meter");
   const children = meter?.children || [];
 
@@ -28,29 +31,40 @@ function updateMeter(score: number): void {
   }
 }
 
-function updateLabel(passwordLength: number, score: number):void {
+function updateLabel(
+  passwordLength: number,
+  score: number,
+  feedback: PasswordFeedback
+): void {
   const suggestionLabel = document.querySelector(
     ".password-complexity-label"
   ) as HTMLElement;
 
+  const suggestion = feedback.warning || feedback.suggestions[0];
+
   if (passwordLength < 4) {
     suggestionLabel.textContent = "Password must be at least four characters.";
+  } else if (suggestion) {
+    suggestionLabel.textContent = suggestion.endsWith(".")
+      ? suggestion
+      : suggestion + ".";
   } else {
+    // fallback strings in case the suggestion is not included in zxcvbn feedback
     switch (score) {
       case 0:
-        suggestionLabel.textContent = "Password is short and easy to guess."
-        break;
       case 1:
-        suggestionLabel.textContent = "Very weak password.";
+        suggestionLabel.textContent = "Password is short and easy to guess.";
         break;
       case 2:
-        suggestionLabel.textContent = "Weak password.";
+        suggestionLabel.textContent =
+          "Somewhat guessable, but still susceptible to being cracked.";
         break;
       case 3:
-        suggestionLabel.textContent = "Decent password.";
+        suggestionLabel.textContent =
+          "Decent password, would require many guesses to crack.";
         break;
       case 4:
-        suggestionLabel.textContent = "Strong password!";
+        suggestionLabel.textContent = "Strong password and secure password!";
         break;
     }
   }
