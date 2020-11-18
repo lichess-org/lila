62d790723f3ae13861d9ed84cb00bfce377e31c0
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-11-18 20:38:56 +0100

    declare ab function return types as void

--- ui/round/types/ab.d.ts
+++ ui/round/types/ab.d.ts
@@ -1,5 +1,5 @@
 declare module 'ab' {
   import { MoveMetadata } from 'chessground/types';
-  function init (round: unknown): unknown
-  function move (round: unknown, meta: MoveMetadata): unknown
+  function init(round: unknown): void;
+  function move(round: unknown, meta: MoveMetadata): void;
 }
