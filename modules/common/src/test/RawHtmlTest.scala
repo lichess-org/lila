2b20aeb90abc28e3521ce5582e496bbc266490b4
Daniel Dugovic <dandydand@gmail.com>
2020-11-19 05:56:19 -0600

    Update domain in raw HTML test

--- modules/common/src/test/RawHtmlTest.scala
+++ modules/common/src/test/RawHtmlTest.scala
@@ -76,51 +76,51 @@ class RawHtmlTest extends Specification {
     }
 
     "internal links" in {
-      addLinks("lichess.org/@/foo/games") must_==
-        """<a href="/@/foo/games">lichess.org/@/foo/games</a>"""
-      addLinks("lichess.org/@/foo") must_== """<a href="/@/foo">@foo</a>"""
-      addLinks("http://lichess.org/") must_== """<a href="/">lichess.org/</a>"""
-      addLinks("http://lichess.org") must_== """<a href="/">lichess.org</a>"""
+      addLinks("lishogi.org/@/foo/games") must_==
+        """<a href="/@/foo/games">lishogi.org/@/foo/games</a>"""
+      addLinks("lishogi.org/@/foo") must_== """<a href="/@/foo">@foo</a>"""
+      addLinks("http://lishogi.org/") must_== """<a href="/">lishogi.org/</a>"""
+      addLinks("http://lishogi.org") must_== """<a href="/">lishogi.org</a>"""
       addLinks("@foo") must_== """<a href="/@/foo">@foo</a>"""
     }
 
     "handle weird characters" in {
-      addLinks("lichess.org/-–%20") must_== """<a href="/-–%20">lichess.org/-–%20</a>"""
+      addLinks("lishogi.org/-–%20") must_== """<a href="/-–%20">lishogi.org/-–%20</a>"""
     }
 
     "handle multiple links" in {
       addLinks(
-        "@foo blah lichess.org"
-      ) must_== """<a href="/@/foo">@foo</a> blah <a href="/">lichess.org</a>"""
-      addLinks("b foo.com blah lichess.org") must_==
-        """b <a rel="nofollow noopener noreferrer" href="https://foo.com" target="_blank">foo.com</a> blah <a href="/">lichess.org</a>"""
+        "@foo blah lishogi.org"
+      ) must_== """<a href="/@/foo">@foo</a> blah <a href="/">lishogi.org</a>"""
+      addLinks("b foo.com blah lishogi.org") must_==
+        """b <a rel="nofollow noopener noreferrer" href="https://foo.com" target="_blank">foo.com</a> blah <a href="/">lishogi.org</a>"""
     }
 
     "handle trailing punctuation" in {
-      addLinks("lichess.org.") must_== """<a href="/">lichess.org</a>."""
-      addLinks("lichess.org)") must_== """<a href="/">lichess.org</a>)"""
-      addLinks("lichess.org/()") must_== """<a href="/()">lichess.org/()</a>"""
+      addLinks("lishogi.org.") must_== """<a href="/">lishogi.org</a>."""
+      addLinks("lishogi.org)") must_== """<a href="/">lishogi.org</a>)"""
+      addLinks("lishogi.org/()") must_== """<a href="/()">lishogi.org/()</a>"""
 
-      addLinks("lichess.org/())") must_== """<a href="/()">lichess.org/()</a>)"""
-      addLinks("lichess.org/(2)-)?") must_== """<a href="/(2)-">lichess.org/(2)-</a>)?"""
+      addLinks("lishogi.org/())") must_== """<a href="/()">lishogi.org/()</a>)"""
+      addLinks("lishogi.org/(2)-)?") must_== """<a href="/(2)-">lishogi.org/(2)-</a>)?"""
 
-      addLinks("lichess.org.-") must_== """<a href="/">lichess.org</a>.-"""
+      addLinks("lishogi.org.-") must_== """<a href="/">lishogi.org</a>.-"""
 
-      addLinks("lichess.org/foo:bar") must_== """<a href="/foo:bar">lichess.org/foo:bar</a>"""
-      addLinks("lichess.org/foo:bar:") must_== """<a href="/foo:bar">lichess.org/foo:bar</a>:"""
+      addLinks("lishogi.org/foo:bar") must_== """<a href="/foo:bar">lishogi.org/foo:bar</a>"""
+      addLinks("lishogi.org/foo:bar:") must_== """<a href="/foo:bar">lishogi.org/foo:bar</a>:"""
     }
 
     "handle embedded links" in {
-      addLinks(".lichess.org") must_== """.lichess.org"""
-      addLinks("/lichess.org") must_== """/lichess.org"""
-      addLinks(".http://lichess.org") must_== """.<a href="/">lichess.org</a>"""
+      addLinks(".lishogi.org") must_== """.lishogi.org"""
+      addLinks("/lishogi.org") must_== """/lishogi.org"""
+      addLinks(".http://lishogi.org") must_== """.<a href="/">lishogi.org</a>"""
 
-      addLinks("/http://lichess.org") must_== """/<a href="/">lichess.org</a>"""
+      addLinks("/http://lishogi.org") must_== """/<a href="/">lishogi.org</a>"""
     }
 
     "handle ambig path separator" in {
-      addLinks("lichess.org#f") must_== """<a href="/#f">lichess.org/#f</a>"""
-      addLinks("lichess.org?f") must_== """<a href="/?f">lichess.org/?f</a>"""
+      addLinks("lishogi.org#f") must_== """<a href="/#f">lishogi.org/#f</a>"""
+      addLinks("lishogi.org?f") must_== """<a href="/?f">lishogi.org/?f</a>"""
     }
 
     "pass through plain text (fast case)" in {
@@ -152,13 +152,13 @@ class RawHtmlTest extends Specification {
 
   "atUser" should {
     "expand valid" in {
-      expandAtUser("@foo") must_== List("lichess.org/@/foo")
-      expandAtUser("@2foo") must_== List("lichess.org/@/2foo")
-      expandAtUser("@foo.") must_== List("lichess.org/@/foo", ".")
+      expandAtUser("@foo") must_== List("lishogi.org/@/foo")
+      expandAtUser("@2foo") must_== List("lishogi.org/@/2foo")
+      expandAtUser("@foo.") must_== List("lishogi.org/@/foo", ".")
       expandAtUser("@foo.com") must_== List("@foo.com")
 
-      expandAtUser("@foo./") must_== List("lichess.org/@/foo", "./")
-      expandAtUser("@foo/games") must_== List("lichess.org/@/foo", "/games")
+      expandAtUser("@foo./") must_== List("lishogi.org/@/foo", "./")
+      expandAtUser("@foo/games") must_== List("lishogi.org/@/foo", "/games")
     }
   }
 
@@ -166,8 +166,8 @@ class RawHtmlTest extends Specification {
     "at user links" in {
       copyLinkConsistency("http://example.com")
       copyLinkConsistency("https://example.com/@foo")
-      copyLinkConsistency("lichess.org/@/foo")
-      copyLinkConsistency("lichess.org/@/foo/games")
+      copyLinkConsistency("lishogi.org/@/foo")
+      copyLinkConsistency("lishogi.org/@/foo/games")
       copyLinkConsistency("@foo/games")
       copyLinkConsistency("@foo")
     }
