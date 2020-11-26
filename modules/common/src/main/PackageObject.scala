91a1cd3ecaa2a68f7ad9c914b22d6f8d9e6b813e
Thibault Duplessis <t@lichess.org>
2020-11-26 09:27:30 +0100

    tweak type ~

--- modules/common/src/main/PackageObject.scala
+++ modules/common/src/main/PackageObject.scala
@@ -8,7 +8,7 @@ trait PackageObject extends Lilaisms {
   def nowTenths: Long = nowMillis / 100
   def nowSeconds: Int = (nowMillis / 1000).toInt
 
-  type ~[+A, +B] = Tuple2[A, B]
+  type ~[A, B] = Tuple2[A, B]
   object ~ {
     def apply[A, B](x: A, y: B)                              = Tuple2(x, y)
     def unapply[A, B](x: Tuple2[A, B]): Option[Tuple2[A, B]] = Some(x)
