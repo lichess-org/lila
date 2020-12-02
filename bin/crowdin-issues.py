a76d82c33e67f67747de9b857980e4edf054e89d
justremember <nothingpersonal.qwerty@gmail.com>
2020-12-02 18:24:45 +0800

    Update crowdin-issues.py

--- bin/crowdin-issues.py
+++ bin/crowdin-issues.py
@@ -6,7 +6,7 @@ import requests
 CROWDIN_USER = os.environ["CROWDIN_USER"]
 CROWDIN_KEY = os.environ["CROWDIN_KEY"]
 
-r = requests.get("https://api.crowdin.com/api/project/lichess/issues", params={
+r = requests.get("https://api.crowdin.com/api/project/lishogi/issues", params={
     "login": CROWDIN_USER,
     "account-key": CROWDIN_KEY,
     "status": "unresolved",
