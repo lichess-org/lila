8a10feeca671c9dc31247d436f7da051af81c1fb
YaFred <yafred@users.noreply.github.com>
2020-12-16 12:36:19 +0000

    hide password when team password is empty (back to solution 1)

--- app/views/team/request.scala
+++ app/views/team/request.scala
@@ -17,10 +17,12 @@ object request {
 
     val title = s"${joinTeam.txt()} ${t.name}"
 
-    val passwordKlass = {
-      if (t.password == None || t.password.get == "") "hidden"
+    val passwordFrag = {
+      if (t.password == None || t.password.get == "") emptyFrag
       else
-        ""
+        form3.group(form("password"), teamPassword(), help = teamPasswordDescriptionForRequester().some)(
+          form3.input(_)
+        )
     }
 
     views.html.base.layout(
@@ -34,14 +36,7 @@ object request {
           p(style := "margin:2em 0")(richText(t.description)),
           postForm(cls := "form3", action := routes.Team.requestCreate(t.id))(
             form3.group(form("message"), trans.message())(form3.textarea(_)()),
-            form3.group(
-              form("password"),
-              teamPassword(),
-              klass = passwordKlass,
-              help = teamPasswordDescriptionForRequester().some
-            )(
-              form3.input(_)
-            ),
+            passwordFrag,
             form3.globalError(form),
             p(willBeReviewed()),
             form3.actions(
