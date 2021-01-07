30bf5c44a10ba5107427b7e1514e82cbb976f339
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-01-07 13:54:44 +0100

    more fix-normalized-emails.js tweaks

--- bin/mongodb/fix-normalized-emails.js
+++ bin/mongodb/fix-normalized-emails.js
@@ -1,10 +1,10 @@
 function gmailNormalize(email) {
   let [name, domain] = email.toLowerCase().split('@');
-  [name, ] = name.replace(/\./g, '').split('+');
-  return name + '@' + domain;
+  [name, ] = name.split('+');
+  return name.replace(/\./g, '') + '@' + domain;
 }
 
-db.user4.find({email: /.*[+.].*@(protonmail\.com|protonmail\.ch|pm\.me|gmail\.com|googlemail\.com)$/}).forEach(user => {
+db.user4.find({email: /[^+.]+[+.].*@(protonmail\.com|protonmail\.ch|pm\.me|gmail\.com|googlemail\.com)$/i}).forEach(user => {
   const normalized = gmailNormalize(user.email);
   const verbatim = user.verbatimEmail || user.email;
   print(user.username, ': ', verbatim, '->', normalized);
