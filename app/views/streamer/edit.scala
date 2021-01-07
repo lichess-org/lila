366b4c661d0bc4e3607f5a5b0c45d9a6e1240042
Thibault Duplessis <t@lichess.org>
2021-01-07 15:40:25 +0100

    streamer must have a channel - for lichess-org/talk#7870

--- app/views/streamer/edit.scala
+++ app/views/streamer/edit.scala
@@ -1,13 +1,12 @@
 package views.html.streamer
 
+import controllers.routes
 import play.api.data.Form
 
 import lila.api.Context
 import lila.app.templating.Environment._
 import lila.app.ui.ScalatagsTemplate._
 import lila.common.String.html.richText
-
-import controllers.routes
 
 object edit extends Context.ToLang {
 
@@ -175,6 +174,7 @@ object edit extends Context.ToLang {
                     form3.submit(trans.apply())
                   )
                 ),
+                form3.globalError(form),
                 form3.split(
                   form3.group(
                     form("twitch"),
