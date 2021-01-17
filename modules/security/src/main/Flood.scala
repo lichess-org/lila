dd49c6651e90c36a7200afa0b4e1f14444b04f97
Tito <tito.bouzout@gmail.com>
2021-01-17 10:59:25 -0300

    Fixes #7574

--- modules/security/src/main/Flood.scala
+++ modules/security/src/main/Flood.scala
@@ -20,7 +20,7 @@ final class Flood(duration: FiniteDuration) {
   def allowMessage(uid: User.ID, text: String): Boolean = {
     val msg  = Message(text, Instant.now)
     val msgs = ~cache.getIfPresent(uid)
-    !duplicateMessage(msg, msgs) && !quickPost(msg, msgs) ~ {
+    !quickPost(msg, msgs) ~ {
       _ ?? cache.put(uid, msg :: msgs)
     }
   }
@@ -31,31 +31,7 @@ final class Flood(duration: FiniteDuration) {
 
 private object Flood {
 
-  // ui/chat/src/preset.ts
-  private val passList = Set(
-    "Hello",
-    "Good luck",
-    "Have fun!",
-    "You too!",
-    "Good game",
-    "Well played",
-    "Thank you",
-    "I've got to go",
-    "Bye!"
-  )
-
   private[security] case class Message(text: String, date: Instant)
 
   private type Messages = List[Message]
-
-  private[security] def duplicateMessage(msg: Message, msgs: Messages): Boolean =
-    !passList.contains(msg.text) && msgs.headOption.?? { m =>
-      similar(m.text, msg.text) || msgs.tail.headOption.?? { m2 =>
-        similar(m2.text, msg.text)
-      }
-    }
-
-  private def similar(s1: String, s2: String): Boolean = {
-    isLevenshteinDistanceLessThan(s1, s2, (s1.length.min(s2.length) >> 3) atLeast 2)
-  }
 }
