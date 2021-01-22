f1ced899a0270f4d720b10b04b73879fdc71d769
Thibault Duplessis <t@lichess.org>
2021-01-22 22:49:58 +0100

    puzzle storm UI WIP

--- ui/storm/src/ctrl.ts
+++ ui/storm/src/ctrl.ts
@@ -1,12 +1,51 @@
+import { Api as CgApi } from 'chessground/api';
+import { prop, Prop } from 'common';
 import { StormOpts, StormData, StormPuzzle } from './interfaces';
+
 
 export default class StormCtrl {
 
   data: StormData;
   trans: Trans;
+  ground = prop<CgApi | undefined>(undefined) as Prop<CgApi>;
 
   constructor(readonly opts: StormOpts, readonly redraw: () => void) {
     this.data = opts.data;
     this.trans = lichess.trans(opts.i18n);
+  }
+
+  makeCgOpts = (): CgConfig => {
+    const node = vm.node;
+    const color: Color = node.ply % 2 === 0 ? 'white' : 'black';
+    const dests = chessgroundDests(position());
+    const nextNode = vm.node.children[0];
+    const canMove = vm.mode === 'view' ||
+      (color === vm.pov && (!nextNode || nextNode.puzzle == 'fail'));
+    const movable = canMove ? {
+      color: dests.size > 0 ? color : undefined,
+      dests
+    } : {
+        color: undefined,
+        dests: new Map(),
+      };
+    const config = {
+      fen: node.fen,
+      orientation: vm.pov,
+      turnColor: color,
+      movable: movable,
+      premovable: {
+        enabled: false
+      },
+      check: !!node.check,
+      lastMove: uciToLastMove(node.uci)
+    };
+    if (node.ply >= vm.initialNode.ply) {
+      if (vm.mode !== 'view' && color !== vm.pov && !nextNode) {
+        config.movable.color = vm.pov;
+        config.premovable.enabled = true;
+      }
+    }
+    vm.cgConfig = config;
+    return config;
   }
 }
---
+++ ui/storm/src/view/chessground.ts
@@ -0,0 +1,66 @@
+import changeColorHandle from 'common/coordsColor';
+import resizeHandle from 'common/resize';
+import { Chessground } from 'chessground';
+import { Config as CgConfig } from 'chessground/config';
+import { StormCtrl } from '../ctrl';
+import { h } from 'snabbdom'
+import { VNode } from 'snabbdom/vnode';
+
+export default function(ctrl: StormCtrl): VNode {
+  return h('div.cg-wrap', {
+    hook: {
+      insert: vnode => ctrl.ground(Chessground((vnode.elm as HTMLElement), makeConfig(ctrl))),
+      destroy: _ => ctrl.ground()!.destroy()
+    }
+  });
+}
+
+function makeConfig(ctrl: StormCtrl): CgConfig {
+  const opts = ctrl.makeCgOpts();
+  return {
+    fen: opts.fen,
+    orientation: opts.orientation,
+    turnColor: opts.turnColor,
+    check: opts.check,
+    lastMove: opts.lastMove,
+    coordinates: ctrl.pref.coords !== 0,
+    addPieceZIndex: ctrl.pref.is3d,
+    movable: {
+      free: false,
+      color: opts.movable!.color,
+      dests: opts.movable!.dests,
+      showDests: ctrl.pref.destination,
+      rookCastle: ctrl.pref.rookCastle
+    },
+    draggable: {
+      enabled: ctrl.pref.moveEvent > 0,
+      showGhost: ctrl.pref.highlight
+    },
+    selectable: {
+      enabled: ctrl.pref.moveEvent !== 1
+    },
+    events: {
+      move: ctrl.userMove,
+      insert(elements) {
+        resizeHandle(elements, 2, ctrl.vm.node.ply, (_) => true);
+        if (ctrl.pref.coords == 1) changeColorHandle();
+      }
+    },
+    premovable: {
+      enabled: opts.premovable!.enabled
+    },
+    drawable: {
+      enabled: true,
+      defaultSnapToValidMove: (lichess.storage.get('arrow.snap') || 1) != '0'
+    },
+    highlight: {
+      lastMove: ctrl.pref.highlight,
+      check: ctrl.pref.highlight
+    },
+    animation: {
+      enabled: true,
+      duration: ctrl.pref.animation.duration
+    },
+    disableContextMenu: true
+  };
+}
