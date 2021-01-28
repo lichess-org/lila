4fd5bf7e5f9d4a9b312ba9589a4f2b10c1044843
Thibault Duplessis <t@lichess.org>
2021-01-28 18:45:21 +0100

    simplify storm config

--- ui/storm/src/config.ts
+++ ui/storm/src/config.ts
@@ -1,19 +1,3 @@
-// [combo threshold, time bonus]
-const realLevels = [
-  [0, 0],
-  [5, 3],
-  [12, 5],
-  [20, 7],
-  [30, 10]
-];
-const quickLevels = [
-  [0, 0],
-  [3, 5],
-  [9, 10],
-  [12, 15],
-  [15, 20]
-];
-
 const config = {
   // all times in seconds
   clock: {
@@ -22,8 +6,13 @@ const config = {
     malus: 10
   },
   combo: {
-    levels: realLevels,
-    // levels: quickLevels,
+    levels: [
+      [0, 0],
+      [5, 3],
+      [12, 5],
+      [20, 7],
+      [30, 10]
+    ],
   }
 };
 
