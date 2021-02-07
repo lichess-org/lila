78a981b614d30165f8ca3b2360077b736b6bd555
Thibault Duplessis <t@lichess.org>
2021-02-07 11:55:53 +0100

    embedded gamebook can't work. Send the player to the study page
    closes #8106

--- ui/site/src/analyseEmbed.ts
+++ ui/site/src/analyseEmbed.ts
@@ -11,4 +11,11 @@ export default function (opts: any) {
   });
 
   window.addEventListener('resize', () => document.body.dispatchEvent(new Event('chessground.resize')));
+
+  if (opts.study.chapter.gamebook)
+    $('.main-board').append(
+      $(
+        `<a href="/study/${opts.study.id}/${opts.study.chapter.id}" target="_blank" class="button gamebook-embed">Start</a>`
+      )
+    );
 }
