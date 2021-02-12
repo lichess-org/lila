04770f8ad2f29c0b1b00cab83ac742c867b83388
Thibault Duplessis <t@lichess.org>
2021-02-12 16:06:30 +0100

    don't select one-move daily puzzles

--- modules/puzzle/src/main/DailyPuzzle.scala
+++ modules/puzzle/src/main/DailyPuzzle.scala
@@ -52,7 +52,7 @@ final private[puzzle] class DailyPuzzle(
       .path {
         _.aggregateOne() { framework =>
           import framework._
-          Match(pathApi.select(PuzzleTheme.mix.key, PuzzleTier.Top, 1500 to 2000)) -> List(
+          Match(pathApi.select(PuzzleTheme.mix.key, PuzzleTier.Top, 1400 to 2000)) -> List(
             Sample(1),
             Project($doc("ids" -> true, "_id" -> false)),
             UnwindField("ids"),
@@ -70,7 +70,12 @@ final private[puzzle] class DailyPuzzle(
                         )
                       )
                     ),
-                    $doc("$match" -> $doc("day" $exists false))
+                    $doc(
+                      "$match" -> $doc(
+                        Puzzle.BSONFields.day $exists false,
+                        Puzzle.BSONFields.themes $ne PuzzleTheme.oneMove.key.value
+                      )
+                    )
                   )
                 )
               )
