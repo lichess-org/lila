c11a8b11d1e773c6f19f6e7d78f56e31488289b7
Thibault Duplessis <t@lichess.org>
2021-02-14 09:51:40 +0100

    optimize class student bloom filter, compute only once

--- modules/clas/src/main/ClasStudentCache.scala
+++ modules/clas/src/main/ClasStudentCache.scala
@@ -21,36 +21,33 @@ final class ClasStudentCache(colls: ClasColls, cacheApi: CacheApi)(implicit
     mode: Mode
 ) {
 
-  private val expectedElements  = if (mode == Mode.Prod) 300_000 else 10_000
-  private val falsePositiveRate = 0.0001
+  private val falsePositiveRate = 0.00005
   private var bloomFilter       = BloomFilter[User.ID](10, 0.1) // temporary empty filter
 
   def isStudent(userId: User.ID) = bloomFilter mightContain userId
 
   def addStudent(userId: User.ID): Unit = bloomFilter add userId
 
-  private def rebuildBloomFilter(): Unit = {
-    val nextBloom = BloomFilter[User.ID](expectedElements, falsePositiveRate)
-    colls.student
-      .find($empty, $doc("userId" -> true).some)
-      .cursor[Bdoc](ReadPreference.secondaryPreferred)
-      .documentSource()
-      .toMat(Sink.fold[Int, Bdoc](0) { case (total, doc) =>
-        doc string "userId" foreach nextBloom.add
-        total + 1
-      })(Keep.right)
-      .run()
-      .addEffect { nb =>
-        lila.mon.clas.student.bloomFilter.count.update(nb)
-        bloomFilter.dispose()
-        bloomFilter = nextBloom
-      }
-      .monSuccess(_.clas.student.bloomFilter.fu)
-      .addEffectAnyway {
-        scheduler.scheduleOnce(1 hour) { rebuildBloomFilter() }.unit
-      }
-      .unit
-  }
+  private def rebuildBloomFilter(): Unit =
+    colls.student.countAll foreach { count =>
+      val nextBloom = BloomFilter[User.ID](count * 6 / 5, falsePositiveRate)
+      colls.student
+        .find($empty, $doc("userId" -> true).some)
+        .cursor[Bdoc](ReadPreference.secondaryPreferred)
+        .documentSource()
+        .toMat(Sink.fold[Int, Bdoc](0) { case (total, doc) =>
+          doc string "userId" foreach nextBloom.add
+          total + 1
+        })(Keep.right)
+        .run()
+        .addEffect { nb =>
+          lila.mon.clas.student.bloomFilter.count.update(nb)
+          bloomFilter.dispose()
+          bloomFilter = nextBloom
+        }
+        .monSuccess(_.clas.student.bloomFilter.fu)
+        .unit
+    }
 
-  scheduler.scheduleOnce(20 seconds) { rebuildBloomFilter() }.unit
+  scheduler.scheduleOnce(22 seconds) { rebuildBloomFilter() }.unit
 }
