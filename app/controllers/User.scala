bdfe519cd3fb8bf77e1c38492e71e8da4a61badf
Thibault Duplessis <t@lichess.org>
2021-03-02 10:33:26 +0100

    only show patron data to full admins - closes lichess-org/tavern#34

--- app/controllers/User.scala
+++ app/controllers/User.scala
@@ -365,7 +365,7 @@ final class User(
           appeal  <- isGranted(_.Appeals) ?? env.appeal.api.get(user)
         } yield view.modLog(history, appeal)
 
-        val plan = env.plan.api.recentChargesOf(user).map(view.plan).dmap(~_)
+        val plan = isGranted(_.Admin) ?? env.plan.api.recentChargesOf(user).map(view.plan).dmap(~_)
 
         val student = env.clas.api.student.findManaged(user).map2(view.student).dmap(~_)
 
