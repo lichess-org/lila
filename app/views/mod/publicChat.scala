ec23bc4e5e4f5d50400c53a7f2193ef14c60073b
Thibault Duplessis <t@lichess.org>
2021-03-11 10:09:33 +0100

    refactor public chats view

--- app/views/mod/publicChat.scala
+++ app/views/mod/publicChat.scala
@@ -6,12 +6,14 @@ import lila.app.ui.ScalatagsTemplate._
 import lila.common.String.html.richText
 
 import controllers.routes
+import play.api.mvc.Call
+import lila.chat.UserChat
 
 object publicChat {
 
   def apply(
-      tourChats: List[(lila.tournament.Tournament, lila.chat.UserChat)],
-      simulChats: List[(lila.simul.Simul, lila.chat.UserChat)]
+      tourChats: List[(lila.tournament.Tournament, UserChat)],
+      simulChats: List[(lila.simul.Simul, UserChat)]
   )(implicit ctx: Context) =
     views.html.base.layout(
       title = "Public Chats",
@@ -25,36 +27,14 @@ object publicChat {
             h2("Tournament Chats"),
             div(cls := "player_chats")(
               tourChats.map { case (tournament, chat) =>
-                div(cls := "game")(
-                  a(cls := "title", href := routes.Tournament.show(tournament.id))(tournament.name),
-                  div(cls := "chat")(
-                    chat.lines.filter(_.isVisible).map { line =>
-                      div(cls := "line")(
-                        userIdLink(line.author.toLowerCase.some, withOnline = false, withTitle = false),
-                        " ",
-                        richText(line.text, expandImg = false)
-                      )
-                    }
-                  )
-                )
+                chatOf(routes.Tournament.show(tournament.id), tournament.name, chat)
               }
             ),
             div(
               h2("Simul Chats"),
               div(cls := "player_chats")(
                 simulChats.map { case (simul, chat) =>
-                  div(cls := "game")(
-                    a(cls := "title", href := routes.Simul.show(simul.id))(simul.name),
-                    div(cls := "chat")(
-                      chat.lines.filter(_.isVisible).map { line =>
-                        div(cls := "line")(
-                          userIdLink(line.author.toLowerCase.some, withOnline = false, withTitle = false),
-                          " ",
-                          richText(line.text, expandImg = false)
-                        )
-                      }
-                    )
-                  )
+                  chatOf(routes.Simul.show(simul.id), simul.name, chat)
                 }
               )
             )
@@ -62,4 +42,18 @@ object publicChat {
         )
       )
     }
+
+  private def chatOf(url: Call, name: String, chat: UserChat)(implicit ctx: Context) =
+    div(cls := "game")(
+      a(cls := "title", href := url)(name),
+      div(cls := "chat")(
+        chat.lines.filter(_.isVisible).map { line =>
+          div(cls := "line")(
+            userIdLink(line.author.toLowerCase.some, withOnline = false, withTitle = false),
+            " ",
+            richText(line.text, expandImg = false)
+          )
+        }
+      )
+    )
 }
