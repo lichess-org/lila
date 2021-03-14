0caf8c3da6bae1b455d867b7aff7068a9d9dcffc
Thibault Duplessis <t@lichess.org>
2021-03-14 12:11:43 +0100

    racer home design

--- app/views/racer.scala
+++ app/views/racer.scala
@@ -15,19 +15,17 @@ object racer {
 
   def home(implicit ctx: Context) =
     views.html.base.layout(
-      moreCss = frag(cssTag("racer")),
+      moreCss = cssTag("racer-home"),
       title = "Puzzle Racer"
     ) {
-      main(cls := "racer racer--home")(
+      main(cls := "page page-small racer-home box box-pad")(
         h1("Puzzle Racer"),
         div(
-          postForm(cls := "racer__create", action := routes.Racer.create)(
-            submitButton(cls := "button button-fat")("Race your friends")
-          )
-        ),
-        div(
-          postForm(cls := "racer__lobby", action := routes.Racer.lobby)(
+          postForm(cls := "racer-home__lobby", action := routes.Racer.lobby)(
             submitButton(cls := "button button-fat")("Join a public race")
+          ),
+          postForm(cls := "racer-home__create", action := routes.Racer.create)(
+            submitButton(cls := "button button-fat")("Race your friends")
           )
         )
       )
