ce825631616073d2ffe2a1beffb9678b23f233fb
Thibault Duplessis <t@lichess.org>
2021-03-23 13:30:25 +0100

    fix snooze durations

--- modules/report/src/main/model.scala
+++ modules/report/src/main/model.scala
@@ -41,10 +41,10 @@ object Snooze {
   import scala.concurrent.duration._
   sealed abstract class Duration(val value: FiniteDuration, val name: String)
   object Duration {
-    case object TwentyMinutes extends Duration(1 hour, "20 minutes")
+    case object TwentyMinutes extends Duration(20 minutes, "20 minutes")
     case object OneHour       extends Duration(1 hour, "one hour")
-    case object ThreeHours    extends Duration(1 hour, "three hours")
-    case object OneDay        extends Duration(1 hour, "one day")
+    case object ThreeHours    extends Duration(3 hours, "three hours")
+    case object OneDay        extends Duration(1 day, "one day")
     case object NextDeploy    extends Duration(30 days, "until next deploy")
     val all                = List(TwentyMinutes, OneHour, ThreeHours, OneDay, NextDeploy)
     def apply(key: String) = all.find(_.toString == key)
