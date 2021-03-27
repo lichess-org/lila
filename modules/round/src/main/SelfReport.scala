9d45ae18c41d806213782c7501fbb4bfcbcbb8bc
Thibault Duplessis <t@lichess.org>
2021-03-27 13:27:33 +0100

    tweak self report

--- modules/round/src/main/SelfReport.scala
+++ modules/round/src/main/SelfReport.scala
@@ -48,7 +48,7 @@ final class SelfReport(
               )
             }
           }
-        if (name == "kb" || fullId.value == "________") fuccess(doLog())
+        if (name == "kb" || fullId.value == "____________") fuccess(doLog())
         else
           proxyRepo.pov(fullId.value) flatMap {
             _ ?? { pov =>
