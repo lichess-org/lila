1a9e81cc8654709707106d0500bf65591bd02599
Benedikt Werner <1benediktwerner@gmail.com>
2021-04-06 01:14:58 +0200

    Add missing '|' to lameName regex

--- modules/common/src/main/LameName.scala
+++ modules/common/src/main/LameName.scala
@@ -14,11 +14,11 @@ object LameName {
     "^"
     + "(?i:" + titlePattern + "M[^a-z].*)|"                   // title at start, separated by non-letter
     + "(?:(?i:" + titlePattern + ")m[^a-z].*)|"               // title at start with lowercase m, not followed by lowercase letter
-    + "(?:" + titlePattern + "M.*)"                           // uppercase title at start
+    + "(?:" + titlePattern + "M.*)|"                          // uppercase title at start
     + "(?i:.*[^a-z]" + titlePattern + "M)|"                   // title at end, separated by non-letter
     + "(?i:.*[^a-z]" + titlePattern + "M[^a-z].*)|"           // title in middle, surrounded by non-letters
-    + "(?:.*[^A-Z]" + titlePattern + "M(?:[A-Z]?[^A-Z].*)?)"  // uppercase title not preceeded by uppercase letter, either at end or followed by at most one uppercase letter and then something else
-    + "$"
+    + "(?:.*[^A-Z]" + titlePattern + "M(?:[A-Z]?[^A-Z].*)?)"  // uppercase title not preceeded by uppercase letter,
+    + "$"                                                     //   either at end or followed by at most one uppercase letter and then something else
   ).r
 
   private val baseWords = List(
