b64201411e61ffc8bad694c6686552714c00c691
Thibault Duplessis <t@lichess.org>
2021-04-14 18:26:51 +0200

    lock swiss variant & clock after start

--- modules/swiss/src/main/SwissApi.scala
+++ modules/swiss/src/main/SwissApi.scala
@@ -93,8 +93,8 @@ final class SwissApi(
       val swiss =
         old.copy(
           name = data.name | old.name,
-          clock = data.clock,
-          variant = data.realVariant,
+          clock = if (old.isCreated) data.clock else old.clock,
+          variant = if (old.isCreated && data.variant.isDefined) data.realVariant else old.variant,
           startsAt = data.startsAt.ifTrue(old.isCreated) | old.startsAt,
           nextRoundAt =
             if (old.isCreated) Some(data.startsAt | old.startsAt)
