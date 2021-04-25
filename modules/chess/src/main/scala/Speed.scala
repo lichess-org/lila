71f86d599208a4fe25ad0d939a8e896fa7321de6
Daniel Dugovic <dandydand@gmail.com>
2021-04-25 12:33:57 -0500

    Update bullet and blitz translation contexts

--- modules/chess/src/main/scala/Speed.scala
+++ modules/chess/src/main/scala/Speed.scala
@@ -14,7 +14,7 @@ sealed abstract class Speed(
 object Speed {
 
   case object UltraBullet
-      extends Speed(0, "ultraBullet", 0 to 59, "UltraBullet", "Insanely fast games: less than 60 seconds")
+      extends Speed(0, "ultraBullet", 0 to 59, "UltraBullet", "Insanely fast games: less than 1 minute")
   case object Bullet extends Speed(1, "bullet", 60 to 299, "Bullet", "Very fast games: less than 5 minutes")
   case object Blitz  extends Speed(2, "blitz", 300 to 599, "Blitz", "Fast games: 5 to 10 minutes")
   case object Rapid  extends Speed(5, "rapid", 600 to 1499, "Rapid", "Rapid games: 10 to 25 minutes")
