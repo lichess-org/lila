5356378c4a4a392a45d17b154d34c8fafeab61b6
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-04-30 17:31:15 +0200

    Use URLSearchParams in userGamesDownload

--- ui/site/src/userGamesDownload.ts
+++ ui/site/src/userGamesDownload.ts
@@ -1,10 +1,5 @@
 function generateURL() {
-  function addParameter(name: string, value: string | number | boolean): void {
-    if (!url.endsWith('?')) url += '&';
-    url += `${name}=${value}`;
-  }
-
-  let url: string = 'https://lichess.org/api/games/user/' + encodeURIComponent($('#dl-username').val() as string) + '?';
+  const searchParams = new URLSearchParams();
 
   {
     const minTimestamp = 1356998400070;
@@ -14,7 +9,7 @@ function generateURL() {
     if (dateMin.length == 10) {
       // the 00:00:00 is necessary for the time to be interpreted in the local timezone
       const since = new Date(`${dateMin} ${timeMin.length == 8 ? timeMin : midnight}`);
-      addParameter('since', Math.max(since.getTime(), minTimestamp));
+      searchParams.append('since', Math.max(since.getTime(), minTimestamp).toString());
     }
 
     const dateMax = $('#dl-dateMax').val() as string;
@@ -23,19 +18,19 @@ function generateURL() {
       const until = new Date(`${dateMax} ${timeMax.length == 8 ? timeMax : midnight}`);
       // If no time is specified, assume that all games on that day should be included
       if (timeMax.length != 8) until.setDate(until.getDate() + 1);
-      addParameter('until', Math.max(until.getTime(), minTimestamp));
+      searchParams.append('until', Math.max(until.getTime(), minTimestamp).toString());
     }
   }
 
   {
     const max = $('#dl-amount').val() as string;
-    if (max.length > 0) addParameter('max', max);
+    if (max.length > 0) searchParams.append('max', max);
 
     const vs = $('#dl-opponent').val() as string;
-    if (vs.length > 0) addParameter('vs', encodeURIComponent(vs));
+    if (vs.length > 0) searchParams.append('vs', encodeURIComponent(vs));
 
     const rated = $('#dl-rated').val() as string;
-    if (rated.length > 0) addParameter('rated', rated);
+    if (rated.length > 0) searchParams.append('rated', rated);
   }
 
   {
@@ -44,32 +39,37 @@ function generateURL() {
       .filter((e: HTMLInputElement) => e.checked)
       .map((e: HTMLInputElement): string => e.value);
     // don't add parameter if all or no perf types are selected
-    if (perfs.length > 0 && perfs.length < 14) addParameter('perfType', perfs.join(','));
+    if (perfs.length > 0 && perfs.length < 14) searchParams.append('perfType', perfs.join(','));
   }
 
   {
     const color = $('#dl-color').val() as string;
-    if (color.length > 0) addParameter('color', color);
+    if (color.length > 0) searchParams.append('color', color);
 
     const analysed = $('#dl-analysis').val() as string;
-    if (analysed.length > 0) addParameter('analysed', analysed);
+    if (analysed.length > 0) searchParams.append('analysed', analysed);
 
     const ongoing = $('#dl-ongoing').prop('checked') as boolean;
-    if (ongoing) addParameter('ongoing', ongoing);
+    if (ongoing) searchParams.append('ongoing', ongoing.toString());
 
     const tags = $('#dl-tags').prop('checked') as boolean;
-    if (!tags) addParameter('tags', tags);
+    if (!tags) searchParams.append('tags', tags.toString());
 
     const clocks = $('#dl-clocks').prop('checked') as boolean;
-    if (clocks) addParameter('clocks', clocks);
+    if (clocks) searchParams.append('clocks', clocks.toString());
 
     const evals = $('#dl-evals').prop('checked') as boolean;
-    if (evals) addParameter('evals', evals);
+    if (evals) searchParams.append('evals', evals.toString());
 
     const opening = $('#dl-opening').prop('checked') as boolean;
-    if (opening) addParameter('opening', opening);
+    if (opening) searchParams.append('opening', opening.toString());
   }
 
+  const url =
+    'https://lichess.org/api/games/user/' +
+    encodeURIComponent($('#dl-username').val() as string) +
+    '?' +
+    searchParams.toString();
   $('#dl-output').val(url).css('visibility', 'visible');
 }
 
