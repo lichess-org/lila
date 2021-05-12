658b911e8aeb0df0f28372d23d9fd3489b473c73
Scala Steward <me@scala-steward.org>
2021-05-12 12:59:43 +0200

    Update lettuce-core to 6.1.2.RELEASE

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val googleOAuth = "com.google.auth"             % "google-auth-library-oauth2-http" % "0.25.5"
   val scalaUri    = "io.lemonlabs"               %% "scala-uri"                       % "3.2.0"
   val scalatags   = "com.lihaoyi"                %% "scalatags"                       % "0.9.4"
-  val lettuce     = "io.lettuce"                  % "lettuce-core"                    % "5.3.7.RELEASE"
+  val lettuce     = "io.lettuce"                  % "lettuce-core"                    % "6.1.2.RELEASE"
   val epoll       = "io.netty"                    % "netty-transport-native-epoll"    % "4.1.58.Final" classifier "linux-x86_64"
   val autoconfig  = "io.methvin.play"            %% "autoconfig-macros"               % "0.3.2"  % "provided"
   val scalatest   = "org.scalatest"              %% "scalatest"                       % "3.1.0"  % Test
