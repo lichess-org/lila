9b991434bddf0f8c4e8863ce26eab90185899fab
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-05-13 09:53:43 +0200

    create other indexes mentioned in troubleshooting section

--- bin/mongodb/indexes.js
+++ bin/mongodb/indexes.js
@@ -2,3 +2,6 @@ db.challenge.createIndex(
   { seenAt: 1 },
   { partialFilterExpression: { status: 10, timeControl: { $exists: true }, seenAt: { $exists: true } } }
 );
+
+db.simul.createIndex({ hostId: 1 }, { partialFilterExpression: { status: 10 } });
+db.simul.createIndex({ hostSeenAt: -1 }, { partialFilterExpression: { status: 10, featurable: true } });
