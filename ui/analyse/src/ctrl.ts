450e0f19cdead74429c7f332d483a85ee62b2903
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2021-05-19 12:42:13 +0100

    Fix board orientation in Racing Kings correspondence analysis

--- ui/analyse/src/ctrl.ts
+++ ui/analyse/src/ctrl.ts
@@ -228,6 +228,7 @@ export default class AnalyseCtrl {
   }
 
   bottomColor(): Color {
+    if (this.data.game.variant.key === 'racingKings') return this.flipped ? 'black' : 'white';
     return this.flipped ? opposite(this.data.orientation) : this.data.orientation;
   }
 
