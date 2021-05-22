ce4c829eff31f5bd66ba468f8969b54f7abb9324
Benedikt Werner <1benediktwerner@gmail.com>
2021-05-22 15:38:16 +0200

    Revert "Don't translate speechSynthesis text"
    This reverts commit 1c6dc2dc3ac35a326c470f3f847f1fa3a89eab1a.

--- ui/round/src/speech.ts
+++ ui/round/src/speech.ts
@@ -1,4 +1,5 @@
 import RoundController from './ctrl';
+import viewStatus from 'game/view/status';
 import { Step } from './interfaces';
 
 export const setup = (ctrl: RoundController) => {
@@ -11,55 +12,13 @@ const onSpeechChange = (ctrl: RoundController) => (enabled: boolean) => {
   else if (window.LichessSpeech && !enabled) window.LichessSpeech = undefined;
 };
 
-const statusText = (ctrl: RoundController) => {
-  const g = ctrl.data.game;
-  switch (g.status.name) {
-    case 'started':
-      return 'Playing';
-    case 'aborted':
-      return 'Game aborted';
-    case 'mate':
-      return 'Checkmate';
-    case 'resign':
-      return `${g.winner} resigned`;
-    case 'stalemate':
-      return 'Stalemate';
-    case 'timeout':
-      switch (g.winner) {
-        case 'white':
-          return 'Black left the game';
-        case 'black':
-          return 'White left the game';
-      }
-      return 'Draw';
-    case 'draw':
-      return 'Draw';
-    case 'outoftime':
-      return `${g.turns % 2 === 0 ? 'White' : 'Black'} time out${g.winner ? '' : ' - Draw'}`;
-    case 'noStart':
-      return (g.winner == 'white' ? 'Black' : 'White') + " didn't move";
-    case 'cheat':
-      return 'Cheat detected';
-    case 'variantEnd':
-      switch (g.variant.key) {
-        case 'kingOfTheHill':
-          return 'King in the centre';
-        case 'threeCheck':
-          return 'Three checks';
-      }
-      return 'Variant ending';
-    case 'unknownFinish':
-      return 'Finished';
-  }
-};
-
 export const status = (ctrl: RoundController) => {
-  const text = statusText(ctrl);
-  if (text === 'Playing') window.LichessSpeech!.step(ctrl.stepAt(ctrl.ply), false);
-  else if (text) {
-    withSpeech(speech => speech.say(text, false));
+  const s = viewStatus(ctrl);
+  if (s == 'playingRightNow') window.LichessSpeech!.step(ctrl.stepAt(ctrl.ply), false);
+  else {
+    withSpeech(speech => speech.say(s, false));
     const w = ctrl.data.game.winner;
-    if (w) withSpeech(speech => speech.say(`${w} is victorious`, false));
+    if (w) withSpeech(speech => speech.say(ctrl.noarg(w + 'IsVictorious'), false));
   }
 };
 
