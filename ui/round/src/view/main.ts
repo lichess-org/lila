6e1670b02c59eedee42c23114974e3937f307bfd
WandererXII <daniel.w.liska@gmail.com>
2021-05-29 16:17:30 +0200

    Remove unused code in round

--- ui/round/src/view/main.ts
+++ ui/round/src/view/main.ts
@@ -1,32 +1,14 @@
 import { h } from 'snabbdom';
 import { VNode } from 'snabbdom/vnode';
-import { plyStep } from '../round';
 import { renderTable } from './table';
 import * as promotion from '../promotion';
 import { render as renderGround } from '../ground';
-import { read as fenRead } from 'shogiground/fen';
 import * as util from '../util';
 import * as keyboard from '../keyboard';
 import * as gridHacks from './gridHacks';
 import crazyView from '../crazy/crazyView';
 import { render as keyboardMove } from '../keyboardMove';
 import RoundController from '../ctrl';
-import { Position, MaterialDiff, MaterialDiffSide, CheckCount } from '../interfaces';
-
-function renderMaterial(material: MaterialDiffSide, score: number, position: Position, checks?: number) {
-  const children: VNode[] = [];
-  let role: string, i: number;
-  for (role in material) {
-    if (material[role] > 0) {
-      const content: VNode[] = [];
-      for (i = 0; i < material[role]; i++) content.push(h('mpiece.' + role));
-      children.push(h('div', content));
-    }
-  }
-  if (checks) for (i = 0; i < checks; i++) children.push(h('div', h('mpiece.king')));
-  if (score > 0) children.push(h('score', '+' + score));
-  return h('div.material.material-' + position, children);
-}
 
 function wheel(ctrl: RoundController, e: WheelEvent): boolean {
   if (ctrl.isPlaying()) return true;
@@ -37,26 +19,10 @@ function wheel(ctrl: RoundController, e: WheelEvent): boolean {
   return false;
 }
 
-const emptyMaterialDiff: MaterialDiff = {
-  sente: {},
-  gote: {},
-};
-
 export function main(ctrl: RoundController): VNode {
   const d = ctrl.data,
-    cgState = ctrl.shogiground && ctrl.shogiground.state,
     topColor = d[ctrl.flip ? 'player' : 'opponent'].color,
     bottomColor = d[ctrl.flip ? 'opponent' : 'player'].color;
-  let material: MaterialDiff,
-    score: number = 0;
-  if (d.pref.showCaptured) {
-    const pieces = cgState ? cgState.pieces : fenRead(plyStep(ctrl.data, ctrl.ply).fen);
-    material = util.getMaterialDiff(pieces);
-    score = util.getScore(pieces) * (bottomColor === 'sente' ? 1 : -1);
-  } else material = emptyMaterialDiff;
-
-  const checks: CheckCount =
-    d.player.checks || d.opponent.checks ? util.countChecks(ctrl.data.steps, ctrl.ply) : util.noChecks;
 
   return ctrl.nvui
     ? ctrl.nvui.render(ctrl)
@@ -76,10 +42,9 @@ export function main(ctrl: RoundController): VNode {
             },
             [renderGround(ctrl), promotion.view(ctrl)]
           ),
-          crazyView(ctrl, topColor, 'top') || renderMaterial(material[topColor], -score, 'top', checks[topColor]),
+          crazyView(ctrl, topColor, 'top'),
           ...renderTable(ctrl),
-          crazyView(ctrl, bottomColor, 'bottom') ||
-            renderMaterial(material[bottomColor], score, 'bottom', checks[bottomColor]),
+          crazyView(ctrl, bottomColor, 'bottom'),
           ctrl.keyboardMove ? keyboardMove(ctrl.keyboardMove) : null,
         ]
       );
