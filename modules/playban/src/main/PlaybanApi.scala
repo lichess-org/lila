11ab9d0c6741c0f5597e76e25ea2a494af46ae22
Thibault Duplessis <t@lichess.org>
2021-05-31 14:44:56 +0200

    fix autoclosing accounts with ragesit recidive

--- modules/playban/src/main/PlaybanApi.scala
+++ modules/playban/src/main/PlaybanApi.scala
@@ -207,27 +207,55 @@ final class PlaybanApi(
 
   private def save(outcome: Outcome, userId: User.ID, rsUpdate: RageSit.Update): Funit = {
     lila.mon.playban.outcome(outcome.key).increment()
-    coll.ext
-      .findAndUpdate[UserRecord](
-        selector = $id(userId),
-        update = $doc(
-          $push("o" -> $doc("$each" -> List(outcome), "$slice" -> -30)) ++ {
-            rsUpdate match {
-              case RageSit.Reset            => $min("c" -> 0)
-              case RageSit.Inc(v) if v != 0 => $inc("c" -> v)
-              case _                        => $empty
+    for {
+      withOutcome <- coll.ext
+        .findAndUpdate[UserRecord](
+          selector = $id(userId),
+          update = $doc(
+            $push("o" -> $doc("$each" -> List(outcome), "$slice" -> -30)) ++ {
+              rsUpdate match {
+                case RageSit.Reset            => $min("c" -> 0)
+                case RageSit.Inc(v) if v != 0 => $inc("c" -> v)
+                case _                        => $empty
+              }
             }
-          }
-        ),
-        fetchNewObject = true,
-        upsert = true
-      ) orFail s"can't find newly created record for user $userId" flatMap { record =>
-      (outcome != Outcome.Good) ?? {
-        userRepo.createdAtById(userId).flatMap { _ ?? { legiferate(record, _) } }
-      } >>
-        registerRageSit(record, rsUpdate)
-    }
+          ),
+          fetchNewObject = true,
+          upsert = true
+        ) orFail s"can't find newly created record for user $userId"
+      withBan <- {
+        if (outcome == Outcome.Good) fuccess(withOutcome)
+        else
+          for {
+            createdAt <- userRepo.createdAtById(userId) orFail s"Missing user creation date $userId"
+            withBan   <- legiferate(withOutcome, createdAt)
+          } yield withBan
+      }
+      _ <- registerRageSit(withBan, rsUpdate)
+    } yield ()
   }.void logFailure lila.log("playban")
+
+  private def legiferate(record: UserRecord, accCreatedAt: DateTime): Fu[UserRecord] =
+    record
+      .bannable(accCreatedAt)
+      .ifFalse(record.banInEffect)
+      .?? { ban =>
+        lila.mon.playban.ban.count.increment()
+        lila.mon.playban.ban.mins.record(ban.mins)
+        Bus.publish(lila.hub.actorApi.playban.Playban(record.userId, ban.mins), "playban")
+        coll.ext
+          .findAndUpdate[UserRecord](
+            selector = $id(record.userId),
+            update = $unset("o") ++ $push(
+              "b" -> $doc(
+                "$each"  -> List(ban),
+                "$slice" -> -30
+              )
+            ),
+            fetchNewObject = true
+          )
+      }
+      .map(_ | record) >>- cleanUserIds.remove(record.userId)
 
   private def registerRageSit(record: UserRecord, update: RageSit.Update): Funit =
     update match {
@@ -252,26 +280,5 @@ final class PlaybanApi(
           }
         }
       case _ => funit
-    }
-
-  private def legiferate(record: UserRecord, accCreatedAt: DateTime): Funit =
-    record.bannable(accCreatedAt) ?? { ban =>
-      (!record.banInEffect) ?? {
-        lila.mon.playban.ban.count.increment()
-        lila.mon.playban.ban.mins.record(ban.mins)
-        Bus.publish(lila.hub.actorApi.playban.Playban(record.userId, ban.mins), "playban")
-        coll.update
-          .one(
-            $id(record.userId),
-            $unset("o") ++
-              $push(
-                "b" -> $doc(
-                  "$each"  -> List(ban),
-                  "$slice" -> -30
-                )
-              )
-          )
-          .void >>- cleanUserIds.remove(record.userId)
-      }
     }
 }
