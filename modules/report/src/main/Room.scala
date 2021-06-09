461644b18d4204073f5c093f455bdad8b5f45fef
Rookie <68079859+mgrookiedev@users.noreply.github.com>
2021-06-09 13:17:43 +0530

    Playban queue - https://github.com/lichess-org/tavern/issues/69

--- modules/report/src/main/Room.scala
+++ modules/report/src/main/Room.scala
@@ -11,16 +11,17 @@ sealed trait Room {
 
 object Room {
 
-  case object Cheat extends Room
-  case object Boost extends Room
-  case object Print extends Room
-  case object Comm  extends Room
-  case object Other extends Room
-  case object Xfiles extends Room {
+  case object Cheat   extends Room
+  case object Boost   extends Room
+  case object Print   extends Room
+  case object Playban extends Room
+  case object Comm    extends Room
+  case object Other   extends Room
+  case object Xfiles  extends Room {
     override def name = "X-Files"
   }
 
-  val all: List[Room] = List(Cheat, Boost, Print, Comm, Other, Xfiles)
+  val all: List[Room] = List(Cheat, Boost, Print, Playban, Comm, Other, Xfiles)
   val byKey = all map { v =>
     (v.key, v)
   } toMap
@@ -37,18 +38,20 @@ object Room {
       case Reason.Cheat                        => Cheat
       case Reason.Boost                        => Boost
       case Reason.AltPrint | Reason.CheatPrint => Print
+      case Reason.Playbans                     => Playban
       case Reason.Comm                         => Comm
-      case Reason.Other | Reason.Playbans      => Other
+      case Reason.Other                        => Other
     }
 
   def toReasons(room: Room): Set[Reason] =
     room match {
-      case Cheat  => Set(Reason.Cheat)
-      case Boost  => Set(Reason.Boost)
-      case Print  => Set(Reason.AltPrint)
-      case Comm   => Set(Reason.Comm)
-      case Other  => Set(Reason.Playbans, Reason.Other)
-      case Xfiles => Set.empty
+      case Cheat   => Set(Reason.Cheat)
+      case Boost   => Set(Reason.Boost)
+      case Print   => Set(Reason.AltPrint)
+      case Playban => Set(Reason.Playbans)
+      case Comm    => Set(Reason.Comm)
+      case Other   => Set(Reason.Other)
+      case Xfiles  => Set.empty
     }
 
   case class Scores(value: Map[Room, Int]) {
@@ -59,12 +62,13 @@ object Room {
   def isGrantedFor(mod: Holder)(room: Room) = {
     import lila.security.Granter
     room match {
-      case Cheat  => Granter.is(_.MarkEngine)(mod)
-      case Boost  => Granter.is(_.MarkBooster)(mod)
-      case Print  => Granter.is(_.Admin)(mod)
-      case Comm   => Granter.is(_.Shadowban)(mod)
-      case Other  => Granter.is(_.Admin)(mod)
-      case Xfiles => Granter.is(_.MarkEngine)(mod)
+      case Cheat   => Granter.is(_.MarkEngine)(mod)
+      case Boost   => Granter.is(_.MarkBooster)(mod)
+      case Print   => Granter.is(_.Admin)(mod)
+      case Playban => Granter.is(_.Admin)(mod)
+      case Comm    => Granter.is(_.Shadowban)(mod)
+      case Other   => Granter.is(_.Admin)(mod)
+      case Xfiles  => Granter.is(_.MarkEngine)(mod)
     }
   }
 }
