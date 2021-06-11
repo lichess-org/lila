a3de22c15788238a1ee3bf43f530a7bb5123795d
Thibault Duplessis <t@lichess.org>
2021-06-11 08:50:29 +0200

    fetch puzzle game from secondary

--- modules/puzzle/src/main/GameJson.scala
+++ modules/puzzle/src/main/GameJson.scala
@@ -50,7 +50,7 @@ final private class GameJson(
   }
 
   private def generate(gameId: Game.ID, plies: Int, bc: Boolean): Fu[JsObject] =
-    gameRepo game gameId orFail s"Missing puzzle game $gameId!" flatMap { game =>
+    gameRepo gameFromSecondary gameId orFail s"Missing puzzle game $gameId!" flatMap { game =>
       lightUserApi preloadMany game.userIds map { _ =>
         if (bc) generateBc(game, plies)
         else generate(game, plies)
