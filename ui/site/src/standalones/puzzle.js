0e94b405ac351bbc289a82e1fc683bf989b9da42
WandererXII <daniel.w.liska@gmail.com>
2021-06-17 18:31:26 +0200

    Oops

--- ui/site/src/standalones/puzzle.js
+++ ui/site/src/standalones/puzzle.js
@@ -2,7 +2,7 @@ var el = document.querySelector('#daily-puzzle');
 var board = el.querySelector('.mini-board');
 board.target = '_blank';
 var lm = board.getAttribute('data-lastmove');
-var dropOrMove = lm ? (lm.includes('*') ? [lm[2, 3]] : [lm[0] + lm[1], lm[2] + lm[3]]) : undefined;
+var dropOrMove = lm ? (lm.includes('*') ? [lm.slice(2)] : [lm[0] + lm[1], lm[2] + lm[3]]) : undefined;
 var fen = board.getAttribute('data-fen');
 var hands = fen && fen.split(' ').length > 2 ? fen.split(' ')[2] : "";
 board.innerHTML = '<div class="cg-wrap mini-board">';
--- ui/site/src/standalones/tv.js
+++ ui/site/src/standalones/tv.js
@@ -2,7 +2,7 @@ function parseFen($elem) {
   $elem.each(function () {
     var $this = $(this).removeClass('parse-fen');
     var lm = $this.data('lastmove');
-    var dropOrMove = lm ? (lm.includes('*') ? [lm[2, 3]] : [lm[0] + lm[1], lm[2] + lm[3]]) : undefined;
+    var dropOrMove = lm ? (lm.includes('*') ? [lm.slice(2)] : [lm[0] + lm[1], lm[2] + lm[3]]) : undefined;
     var fen = $this.data('fen');
     var hands = fen && fen.split(' ').length > 2 ? fen.split(' ')[2] : "";
     var color = $this.data('color');
