41cb82d29affe93f0f3fa629d76a32864ce71a05
Adam Morris <aimorris@aimorris.com>
2021-06-23 21:28:31 +1000

    use colorChoice rather than finalColor

--- app/views/challenge/bits.scala
+++ app/views/challenge/bits.scala
@@ -42,7 +42,7 @@ object bits {
         )
       ),
       div(cls := "mode")(
-        s"${c.finalColor.name} • ${modeName(c.mode)}",
+        s"${c.colorChoice.toString()} • ${modeName(c.mode)}",
       )
     )
 }
