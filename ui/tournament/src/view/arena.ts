bb00361f7b2e06782d800880f7b9e3655256c4b3
Thibault Duplessis <t@lichess.org>
2021-06-29 11:18:01 +0200

    actually fix arena sheet display, sigh

--- ui/tournament/src/view/arena.ts
+++ ui/tournament/src/view/arena.ts
@@ -10,7 +10,7 @@ const scoreTagNames = ['score', 'streak', 'double'];
 
 function scoreTag(s: [number, number] | number) {
   const [score, tag] = Array.isArray(s) ? s : [s, 1];
-  return h(scoreTagNames[Math.max(0, tag - 1)], [score]);
+  return h(scoreTagNames[(tag || 1) - 1], [score]);
 }
 
 function playerTr(ctrl: TournamentController, player: StandingPlayer) {
