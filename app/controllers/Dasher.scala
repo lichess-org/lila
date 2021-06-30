0a4c2e351ac26b08cd8bd7276f82d27d1dedfca9
Benedikt Werner <1benediktwerner@gmail.com>
2021-06-30 17:40:34 +0200

    Fix system theme translation

--- app/controllers/Dasher.scala
+++ app/controllers/Dasher.scala
@@ -16,6 +16,7 @@ final class Dasher(env: Env) extends LilaController(env) {
     trans.background,
     trans.light,
     trans.dark,
+    trans.system,
     trans.transparent,
     trans.backgroundImageUrl,
     trans.boardGeometry,
