45ca85ac2820ac7710ec49a389a14117371214d4
Konstantinos07 <gkizisk@gmail.com>
2021-06-30 10:58:43 +0300

    translate newPlayer.scala

--- app/views/user/show/newPlayer.scala
+++ app/views/user/show/newPlayer.scala
@@ -1,5 +1,7 @@
 package views.html.user.show
 
+import play.api.i18n.Lang
+
 import lila.app.templating.Environment._
 import lila.app.ui.ScalatagsTemplate._
 import lila.user.User
@@ -8,45 +10,42 @@ import controllers.routes
 
 object newPlayer {
 
-  def apply(u: User) =
+  def apply(u: User)(implicit lang: Lang) =
     div(cls := "new-player")(
-      h2("Welcome to lichess.org!"),
+      h2(trans.welcomeToLichess()),
       p(
-        "This is your profile page.",
+        trans.thisIsYourProfilePage(),
         u.profile.isEmpty option frag(
           br,
-          "Would you like to ",
-          a(href := routes.Account.profile)("improve it"),
-          "?"
+          trans.newPlayerImproveProfile(
+          a(href := routes.Account.profile)(trans.improveIt.txt()),
+          )
         )
       ),
       p(
-        if (u.kid) "Kid mode is enabled."
+        if (u.kid) trans.kidModeIsEnabled()
         else
-          frag(
-            "Will a child use this account? You might want to enable ",
-            a(href := routes.Account.kid)("Kid mode"),
-            "."
+          frag(trans.newPlayerKidMode(
+            a(href := routes.Account.kid)(trans.kidMode.txt())) 
           )
       ),
       p(
-        "What now? Here are a few suggestions:"
+        trans.newPlayerWhatNow()
       ),
       ul(
-        li(a(href := routes.Learn.index)("Learn chess rules")),
-        li(a(href := routes.Puzzle.home)("Improve with chess tactics puzzles")),
-        li(a(href := s"${routes.Lobby.home}#ai")("Play the artificial intelligence")),
-        li(a(href := s"${routes.Lobby.home}#hook")("Play opponents from around the world")),
-        li(a(href := routes.User.list)("Follow your friends on Lichess")),
-        li(a(href := routes.Tournament.home)("Play in tournaments")),
-        li(
-          "Learn from ",
-          a(href := routes.Study.allDefault(1))("studies"),
-          " and ",
-          a(href := routes.Video.index)("videos")
+        li(a(href := routes.Learn.index)(trans.learnChessRules())),
+        li(a(href := routes.Puzzle.home)(trans.improveWithChessTactics())),
+        li(a(href := s"${routes.Lobby.home}#ai")(trans.playTheAI())),
+        li(a(href := s"${routes.Lobby.home}#hook")(trans.playOpponentsAroundWorld())),
+        li(a(href := routes.User.list)(trans.followYourFriends())),
+        li(a(href := routes.Tournament.home)(trans.playInTourns())),
+        li(trans.newPlayerLearnFrom(
+          a(href := routes.Study.allDefault(1))(trans.studies.txt()),
+          a(href := routes.Video.index)(trans.videos.txt())
+          )
         ),
-        li(a(href := routes.Pref.form("game-display"))("Configure Lichess to your liking")),
-        li("Explore the site and have fun :)")
+        li(a(href := routes.Pref.form("game-display"))(trans.configureLichessToYourLiking())),
+        li(trans.newPlayerExploreSite())
       )
     )
 }
