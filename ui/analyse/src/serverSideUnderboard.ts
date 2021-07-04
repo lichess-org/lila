e27e54824e06645b3ad5777d82fad09295c808fa
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-07-04 09:50:05 +0200

    also bump analysis chart loader to sf 14+

--- ui/analyse/src/serverSideUnderboard.ts
+++ ui/analyse/src/serverSideUnderboard.ts
@@ -70,7 +70,7 @@ export default function (element: HTMLElement, ctrl: AnalyseCtrl) {
   }
 
   function chartLoader() {
-    return `<div id="acpl-chart-loader"><span>Stockfish 13+<br>server analysis</span>${lichess.spinnerHtml}</div>`;
+    return `<div id="acpl-chart-loader"><span>Stockfish 14+<br>server analysis</span>${lichess.spinnerHtml}</div>`;
   }
   function startAdvantageChart() {
     if (lichess.advantageChart || lichess.AnalyseNVUI) return;
