ac4f0757e7f0577466a82af8305f2a05470e1f9b
Thibault Duplessis <t@lichess.org>
2021-07-08 08:52:42 +0200

    cleanup lila.oauth.Env after #9366

--- modules/oauth/src/main/Env.scala
+++ modules/oauth/src/main/Env.scala
@@ -1,24 +1,15 @@
 package lila.oauth
 
-import akka.actor._
 import com.softwaremill.macwire._
-import play.api.Configuration
-import scala.concurrent.duration._
 
 import lila.common.config.CollName
 
 @Module
 final class Env(
-    appConfig: Configuration,
     cacheApi: lila.memo.CacheApi,
     userRepo: lila.user.UserRepo,
-    db: lila.db.Db,
-    mongo: lila.db.Env
-)(implicit
-    ec: scala.concurrent.ExecutionContext,
-    system: ActorSystem
-) {
-
+    db: lila.db.Db
+)(implicit ec: scala.concurrent.ExecutionContext) {
   lazy val legacyClientApi  = new LegacyClientApi(db(CollName("oauth2_legacy_client")))
   lazy val authorizationApi = new AuthorizationApi(db(CollName("oauth2_authorization")))
   lazy val tokenApi         = new AccessTokenApi(db(CollName("oauth2_access_token")), cacheApi, userRepo)
