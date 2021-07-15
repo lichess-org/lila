905a9e590991462851936ed47f840a962d8cd87e
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-07-15 15:01:40 +0200

    create indexes for oauth2 collections

--- bin/mongodb/indexes.js
+++ bin/mongodb/indexes.js
@@ -5,3 +5,7 @@ db.challenge.createIndex(
 
 db.simul.createIndex({ hostId: 1 }, { partialFilterExpression: { status: 10 } });
 db.simul.createIndex({ hostSeenAt: -1 }, { partialFilterExpression: { status: 10, featurable: true } });
+
+db.oauth2_access_token.createIndex({ userId: 1 });
+db.oauth2_access_token.createIndex({ expires: 1 }, { expireAfterSeconds: 0 });
+db.oauth2_authorization.createIndex({ expires: 1 }, { expireAfterSeconds: 0 });
