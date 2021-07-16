462f7de17dde82d0d2d8e25bffb82fc7eef64be0
Thibault Duplessis <t@lichess.org>
2021-07-16 19:26:23 +0200

    remove early dismiss report button

--- app/views/report/list.scala
+++ app/views/report/list.scala
@@ -67,13 +67,8 @@ object list {
                           submitButton(dataIcon := "", cls := "text button button-metal")("Reopen")
                         )
                       else
-                        frag(
-                          postForm(action := routes.Report.inquiry(r.id), cls := "inquiry")(
-                            submitButton(dataIcon := "", cls := "button button-metal")
-                          ),
-                          postForm(action := routes.Report.process(r.id), cls := "cancel")(
-                            submitButton(cls := "button button-thin button-empty")("Dismiss")
-                          )
+                        postForm(action := routes.Report.inquiry(r.id), cls := "inquiry")(
+                          submitButton(dataIcon := "", cls := "button button-metal")
                         )
                     case Some(inquiry) =>
                       frag(
