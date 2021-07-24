ef9d1dbf313dba5b0ff61657f096eeba9ae6759d
Scala Steward <me@scala-steward.org>
2021-07-24 20:43:40 +0200

    Update scrimage-core to 4.0.21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val compression = "org.lichess"                %% "compression"                     % "1.6"
   val maxmind     = "com.sanoma.cda"             %% "maxmind-geoip2-scala"            % "1.3.1-THIB"
   val prismic     = "io.prismic"                 %% "scala-kit"                       % "1.2.19-THIB213"
-  val scrimage    = "com.sksamuel.scrimage"       % "scrimage-core"                   % "4.0.20"
+  val scrimage    = "com.sksamuel.scrimage"       % "scrimage-core"                   % "4.0.21"
   val scaffeine   = "com.github.blemale"         %% "scaffeine"                       % "4.0.2"  % "compile"
   val googleOAuth = "com.google.auth"             % "google-auth-library-oauth2-http" % "0.27.0"
   val scalaUri    = "io.lemonlabs"               %% "scala-uri"                       % "3.5.0"
