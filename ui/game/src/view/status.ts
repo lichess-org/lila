b3597cc782fcc4ae066aee7e067bbeb6decd8fb6
Benedikt Werner <1benediktwerner@gmail.com>
2021-07-27 09:06:27 +0200

    Custom insufficient material detection to avoid chessops in round

--- ui/game/src/view/status.ts
+++ ui/game/src/view/status.ts
@@ -1,19 +1,36 @@
 import { Ctrl } from '../interfaces';
-import { parseFen } from 'chessops/fen';
-import { setupPosition } from 'chessops/variant';
 
-const CHESSOPS_VARIANT_MAP = {
-  standard: 'chess',
-  chess960: 'chess',
-  antichess: 'antichess',
-  fromPosition: 'chess',
-  kingOfTheHill: 'kingofthehill',
-  threeCheck: '3check',
-  atomic: 'atomic',
-  horde: 'horde',
-  racingKings: 'racingkings',
-  crazyhouse: 'crazyhouse',
-} as const;
+function bishopOnColor(expandedFen: string, offset: 0 | 1): boolean {
+  for (let row = 0; row < 8; row++) {
+    for (let col = row % 2 === offset ? 0 : 1; col < 8; col += 2) {
+      if (/[bB]/.test(expandedFen[row * 8 + col])) return true;
+    }
+  }
+  return false;
+}
+
+function insufficientMaterial(variant: VariantKey, fullFen: Fen): boolean {
+  // TODO: atomic and antichess
+  if (
+    variant === 'horde' ||
+    variant === 'kingOfTheHill' ||
+    variant === 'racingKings' ||
+    variant === 'crazyhouse' ||
+    variant === 'atomic' ||
+    variant === 'antichess'
+  )
+    return false;
+  let fen = fullFen.split(' ')[0].replace(/[^a-z]/gi, '');
+  if (variant === 'threeCheck') return !/[pbnrq]/.test(fen) || !/[PBNRQ]/.test(fen);
+  if (/[prq]/i.test(fen)) return false;
+  if (/n/.test(fen)) return fen.length - fen.replace(/[a-z]/g, '').length <= 2 && !/[PBNR]/.test(fen);
+  if (/N/.test(fen)) return fen.length - fen.replace(/[A-Z]/g, '').length <= 2 && !/[pbnr]/.test(fen);
+  if (/b/i.test(fen)) {
+    for (let i = 8; i > 1; i--) fen = fen.replace('' + i, '1' + (i - 1));
+    return (!bishopOnColor(fen, 0) || !bishopOnColor(fen, 1)) && !/[pPnN]/.test(fen);
+  }
+  return false;
+}
 
 export default function status(ctrl: Ctrl): string {
   const noarg = ctrl.trans.noarg,
@@ -43,10 +60,8 @@ export default function status(ctrl: Ctrl): string {
       if (d.game.drawOffers?.some(turn => turn >= d.game.turns)) return noarg('drawByMutualAgreement');
       if (d.game.fen.split(' ')[4] === '100') return `${noarg('fiftyMovesWithoutProgress')} • ${noarg('draw')}`;
       if (d.game.threefold) return `${noarg('threefoldRepetition')} • ${noarg('draw')}`;
-      const setup = parseFen(d.game.fen).unwrap();
-      const variant = CHESSOPS_VARIANT_MAP[d.game.variant.key];
-      const pos = setupPosition(variant, setup).unwrap();
-      if (pos.isInsufficientMaterial()) return `${noarg('insufficientMaterial')} • ${noarg('draw')}`;
+      if (insufficientMaterial(d.game.variant.key, d.game.fen))
+        return `${noarg('insufficientMaterial')} • ${noarg('draw')}`;
       return noarg('draw');
     }
     case 'outoftime':
