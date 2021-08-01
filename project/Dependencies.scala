4dce2c1518a9b0389b1828227bf6ada4929d9e17
Thibault Duplessis <t@lichess.org>
2021-08-01 10:52:16 +0200

    reactivemongo 1.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
   }
 
   object reactivemongo {
-    val version = "1.0.5"
+    val version = "1.0.6"
 
     val driver = "org.reactivemongo" %% "reactivemongo"               % version
     val stream = "org.reactivemongo" %% "reactivemongo-akkastream"    % version
