d85e067e50601b09748c1187b5eabd360fc93284
WandererXII <daniel.w.liska@gmail.com>
2021-08-18 19:24:36 +0200

    Support Shift-jis encoding in importer

--- public/javascripts/importer.js
+++ public/javascripts/importer.js
@@ -5,16 +5,23 @@ $(function () {
       $form.html(lishogi.spinnerHtml);
     }, 50);
   });
-
   if (window.FileReader) {
-    $form.find('input[type=file]').on('change', function () {
-      var file = this.files[0];
+    function readFile(file, encoding) {
       if (!file) return;
       var reader = new FileReader();
-      reader.onload = function (e) {
-        $form.find('textarea').val(e.target.result);
-      };
-      reader.readAsText(file);
+      reader.onload = function(e) {
+        var res = e.target.result;
+        if(encoding === 'UTF-8' && res.match(/�/)){
+          console.log("UTF-8 didn't work, trying shift-jis, if you still have problems with your import, try converting the file to a different encoding")
+          readFile(file, 'shift-jis')
+        } else {
+          $form.find('textarea').val(res);
+        }
+      }
+      reader.readAsText(file, encoding);
+    }
+    $form.find('input[type=file]').on('change', function () {
+        readFile(this.files[0], 'UTF-8');
     });
   } else $form.find('.upload').remove();
 });
