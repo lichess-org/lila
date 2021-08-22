093972895d599cd13890d6b9c5a0bd85e005a937
Thibault Duplessis <t@lichess.org>
2021-08-22 10:27:56 +0200

    fix UTC flatpickr can't select today

--- ui/site/src/flatpickr.ts
+++ ui/site/src/flatpickr.ts
@@ -2,6 +2,7 @@ import flatpickr from 'flatpickr';
 
 lichess.load.then(() => {
   $('.flatpickr').each(function (this: HTMLInputElement) {
+    const minDate = this.dataset['mindate'];
     const config = this.classList.contains('flatpickr-utc')
       ? {}
       : {
@@ -10,7 +11,7 @@ lichess.load.then(() => {
           altFormat: 'Y-m-d h:i K',
         };
     flatpickr(this, {
-      minDate: this.dataset['mindate'],
+      minDate: minDate == 'yesterday' ? new Date(Date.now() - 1000 * 3600 * 24) : minDate,
       maxDate: new Date(Date.now() + 1000 * 3600 * 24 * 31 * 3),
       monthSelectorType: 'static',
       disableMobile: true, // https://flatpickr.js.org/mobile-support/ https://github.com/ornicar/lila/issues/8110
