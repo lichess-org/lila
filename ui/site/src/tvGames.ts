cb492a9419789b948c09479650474c1a457fef01
Albert Ford <albert@albertford.com>
2021-08-24 09:51:32 -0700

    Fix request queue

--- ui/site/src/tvGames.ts
+++ ui/site/src/tvGames.ts
@@ -5,37 +5,40 @@ interface ReplacementResponse {
   html: string;
 }
 
-const finishedIdsQueue: string[] = [];
+let isRequestPending = false;
+const finishedIdQueue: string[] = [];
 
 function requestReplacementGame() {
   // Make sure to only make one request at a time.
   // This avoids getting copies of the same game to replace two different finished games.
-  if (finishedIdsQueue.length !== 1) return;
+  if (isRequestPending) return;
+  const oldId = finishedIdQueue.shift();
+  if (!oldId) return;
+  isRequestPending = true;
 
   // Use requestAnimationFrame to avoid requesting games in background tabs
   requestAnimationFrame(() => {
     const url = new URL(`${window.location.pathname}/replacement`, window.location.origin);
     $('.mini-game').each((_i, el) => url.searchParams.append('exclude', el.dataset.live!));
-
     xhr
       .json(url.toString())
       .then((data: ReplacementResponse) => {
-        const oldId = finishedIdsQueue.shift()!;
         $(`.mini-game[data-live="${oldId}"]`).parent().html(data.html);
         lichess.contentLoaded();
-        requestReplacementGame();
       })
-      .catch(() => {
-        finishedIdsQueue.shift();
-        requestReplacementGame();
-      });
+      .then(done, done);
   });
+}
+
+function done() {
+  isRequestPending = false;
+  requestReplacementGame();
 }
 
 lichess.load.then(() =>
   lichess.pubsub.on('socket.in.finish', ({ id }) => {
     setTimeout(() => {
-      finishedIdsQueue.push(id);
+      finishedIdQueue.push(id);
       requestReplacementGame();
     }, 7000); // 7000 matches the rematch wait duration in /modules/tv/main/Tv.scala
   })
