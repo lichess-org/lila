b8ba50d6222885600ae142917f729fa3fc6629dc
Thibault Duplessis <t@lichess.org>
2021-08-26 11:39:30 +0200

    upgrade playframework-lila

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,7 +53,7 @@ object Dependencies {
   }
 
   object play {
-    val version = "2.8.7-lila_1.6"
+    val version = "2.8.8-lila_1.7"
     val api     = "com.typesafe.play" %% "play"        % version
     val json    = "com.typesafe.play" %% "play-json"   % "2.9.2"
     val mailer  = "com.typesafe.play" %% "play-mailer" % "8.0.1"
