e316d9851ac7acaec833aa750b9dd9aba90664fd
andresrcom <andres.r.com@gmail.com>
2021-08-27 19:29:38 +0300

    add index to cache collection in mongodb

--- bin/mongodb/indexes.js
+++ bin/mongodb/indexes.js
@@ -9,3 +9,5 @@ db.simul.createIndex({ hostSeenAt: -1 }, { partialFilterExpression: { status: 10
 db.oauth2_access_token.createIndex({ userId: 1 });
 db.oauth2_access_token.createIndex({ expires: 1 }, { expireAfterSeconds: 0 });
 db.oauth2_authorization.createIndex({ expires: 1 }, { expireAfterSeconds: 0 });
+
+db.cache.createIndex({ e: 1 }, { expireAfterSeconds: 0 });
