2e545feb99a8900646da70ff5c1978a5a859df37
Thibault Duplessis <t@lichess.org>
2021-09-09 20:17:08 +0200

    fix post card intro

--- app/views/ublog/post.scala
+++ app/views/ublog/post.scala
@@ -120,6 +120,7 @@ object post {
       thumbnail(post, _.Small)(cls := "ublog-post-card__image"),
       span(cls := "ublog-post-card__content")(
         h2(cls := "ublog-post-card__title")(post.title),
+        showIntro option span(cls := "ublog-post-card__intro")(shorten(post.intro, 100)),
         post.lived map { live => semanticDate(live.at)(ctx.lang)(cls := "ublog-post-card__over-image") },
         showAuthor option userIdSpanMini(post.created.by)(ctx.lang)(cls := "ublog-post-card__over-image")
       )
