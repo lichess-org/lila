7454787085386216cbb5362349255d002f775123
Thibault Duplessis <t@lichess.org>
2021-09-11 11:49:05 +0200

    in mixed languages ublog mashups, favour english posts

--- modules/ublog/src/main/UblogRank.scala
+++ modules/ublog/src/main/UblogRank.scala
@@ -6,6 +6,7 @@ import scala.concurrent.ExecutionContext
 import lila.db.dsl._
 import lila.user.User
 import org.joda.time.DateTime
+import play.api.i18n.Lang
 
 final class UblogRank(colls: UblogColls)(implicit ec: ExecutionContext) {
 
@@ -36,21 +37,22 @@ final class UblogRank(colls: UblogColls)(implicit ec: ExecutionContext) {
       }
       .map { docOption =>
         for {
-          doc    <- docOption
-          likes  <- doc.getAsOpt[UblogPost.Likes]("likes")
-          topics <- doc.getAsOpt[List[UblogTopic]]("topics")
-          liveAt <- doc.getAsOpt[DateTime]("at")
-          tier   <- doc int "tier"
-        } yield (topics, likes, liveAt, tier)
+          doc      <- docOption
+          likes    <- doc.getAsOpt[UblogPost.Likes]("likes")
+          topics   <- doc.getAsOpt[List[UblogTopic]]("topics")
+          liveAt   <- doc.getAsOpt[DateTime]("at")
+          language <- doc.getAsOpt[Lang]("language")
+          tier     <- doc int "tier"
+        } yield (topics, likes, liveAt, language, tier)
       }
       .flatMap {
-        _.fold(fuccess(UblogPost.Likes(v ?? 1))) { case (topics, prevLikes, liveAt, tier) =>
+        _.fold(fuccess(UblogPost.Likes(v ?? 1))) { case (topics, prevLikes, liveAt, language, tier) =>
           val likes = UblogPost.Likes(prevLikes.value + (if (v) 1 else -1))
           colls.post.update.one(
             $id(postId),
             $set(
               "likes" -> likes,
-              "rank"  -> computeRank(topics, likes, liveAt, tier)
+              "rank"  -> computeRank(topics, likes, liveAt, language, tier)
             ) ++ {
               if (v) $addToSet("likers" -> user.id) else $pull("likers" -> user.id)
             }
@@ -62,7 +64,10 @@ final class UblogRank(colls: UblogColls)(implicit ec: ExecutionContext) {
     colls.blog.byId[UblogBlog](blogId.full) flatMap {
       _ ?? { blog =>
         colls.post
-          .find($doc("blog" -> blog.id), $doc("topics" -> true, "likes" -> true, "lived" -> true).some)
+          .find(
+            $doc("blog" -> blog.id),
+            $doc("topics" -> true, "likes" -> true, "lived" -> true, "language" -> true).some
+          )
           .cursor[Bdoc]()
           .list() flatMap { docs =>
           lila.common.Future.applySequentially(docs) { doc =>
@@ -70,9 +75,12 @@ final class UblogRank(colls: UblogColls)(implicit ec: ExecutionContext) {
               doc.string("_id"),
               doc.getAsOpt[List[UblogTopic]]("topics"),
               doc.getAsOpt[UblogPost.Likes]("likes"),
-              doc.getAsOpt[UblogPost.Recorded]("lived")
-            ).tupled ?? { case (id, topics, likes, lived) =>
-              colls.post.updateField($id(id), "rank", computeRank(topics, likes, lived.at, blog.tier)).void
+              doc.getAsOpt[UblogPost.Recorded]("lived"),
+              doc.getAsOpt[Lang]("language")
+            ).tupled ?? { case (id, topics, likes, lived, language) =>
+              colls.post
+                .updateField($id(id), "rank", computeRank(topics, likes, lived.at, language, blog.tier))
+                .void
             }
           }
         }
@@ -81,13 +89,14 @@ final class UblogRank(colls: UblogColls)(implicit ec: ExecutionContext) {
 
   def computeRank(blog: UblogBlog, post: UblogPost): Option[UblogPost.Rank] =
     post.lived map { lived =>
-      computeRank(post.topics, post.likes, lived.at, blog.tier)
+      computeRank(post.topics, post.likes, lived.at, post.language, blog.tier)
     }
 
   private def computeRank(
       topics: List[UblogTopic],
       likes: UblogPost.Likes,
       liveAt: DateTime,
+      language: Lang,
       tier: UblogBlog.Tier
   ) = UblogPost.Rank {
     liveAt plusHours {
@@ -99,11 +108,12 @@ final class UblogRank(colls: UblogColls)(implicit ec: ExecutionContext) {
         case 1 => 1
         case _ => 1.2
       }
+      val langMultiplier = if (language.language == lila.i18n.defaultLang.language) 1 else 0.5
       val tiered = tier match {
-        case UblogBlog.Tier.LOW    => likeHours * 0.3 * topicsMultiplier
-        case UblogBlog.Tier.NORMAL => likeHours * topicsMultiplier
-        case UblogBlog.Tier.HIGH   => likeHours * 3 * topicsMultiplier
-        case UblogBlog.Tier.BEST   => likeHours * 10 * topicsMultiplier
+        case UblogBlog.Tier.LOW    => likeHours * 0.3 * topicsMultiplier * langMultiplier
+        case UblogBlog.Tier.NORMAL => likeHours * topicsMultiplier * langMultiplier
+        case UblogBlog.Tier.HIGH   => likeHours * 3 * topicsMultiplier * langMultiplier
+        case UblogBlog.Tier.BEST   => likeHours * 10 * topicsMultiplier * langMultiplier
         case _                     => -99999
       }
       tiered.toInt
