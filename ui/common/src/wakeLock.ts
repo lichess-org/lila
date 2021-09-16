83a4af8adba2d3fde7f7aed3a049e05a964f5da2
Thibault Duplessis <t@lichess.org>
2021-09-16 08:11:52 +0200

    navigator.wakeLock.request can fail
    DOMException: Failed to execute 'request' on 'WakeLock': The requesting page is not visible

--- ui/common/src/wakeLock.ts
+++ ui/common/src/wakeLock.ts
@@ -1,16 +1,19 @@
 let wakeLock: WakeLockSentinel | null = null;
 
 export const request = () => {
-  if ('wakeLock' in navigator) {
-    navigator.wakeLock
-      .request('screen')
-      .then(sentinel => {
-        wakeLock = sentinel;
-      })
-      .catch((error: Error) => {
-        console.error('wakeLock - request failed: ' + error.message);
-      });
-  }
+  if ('wakeLock' in navigator)
+    try {
+      navigator.wakeLock
+        .request('screen')
+        .then(sentinel => {
+          wakeLock = sentinel;
+        })
+        .catch((error: Error) => {
+          console.error('wakeLock - request failed: ' + error.message);
+        });
+    } catch (e) {
+      console.error(e);
+    }
 };
 
 export const release = () =>
