7ee0d0a70951f769930ef1b61d901b357c288af8
Benedikt Werner <1benediktwerner@gmail.com>
2021-09-19 14:30:53 +0200

    Make shepherd close button visible

--- public/javascripts/study/tour-chapter.js
+++ public/javascripts/study/tour-chapter.js
@@ -1,6 +1,6 @@
 function loadShepherd(f) {
   var theme = 'shepherd-theme-' + ($('body').hasClass('dark') ? 'dark' : 'default');
-  lichess.loadCss('vendor/shepherd/dist/css/' + theme + '.css');
+  lichess.loadCss('vendor/' + theme + '.css');
   lichess.loadScript('vendor/shepherd/dist/js/tether.js', { noVersion: true }).then(function () {
     lichess.loadScript('vendor/shepherd/dist/js/shepherd.min.js', { noVersion: true }).then(function () {
       f(theme);
