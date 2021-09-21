4cea01ce52101b35e4884f3c07d288d6955648ed
WandererXII <daniel.w.liska@gmail.com>
2021-09-21 19:57:31 +0200

    Bump deps

--- ui/ceval/src/ctrl.ts
+++ ui/ceval/src/ctrl.ts
@@ -1,7 +1,7 @@
 import { CevalCtrl, CevalOpts, CevalTechnology, Work, Step, Hovering, Started } from './types';
 
 import { Pool } from './pool';
-import { defined, prop } from 'common';
+import { prop } from 'common';
 import { storedProp } from 'common/storage';
 import throttle from 'common/throttle';
 import { povChances } from './winningChances';
@@ -64,7 +64,6 @@ export default function (opts: CevalOpts): CevalCtrl {
     const sharedMem = sharedWasmMemory(8, 16);
     if (sharedMem) {
       technology = 'wasmx';
-      if (!defined(window['crossOriginIsolated'])) window['crossOriginIsolated'] = true; // polyfill
       try {
         sharedMem.grow(8);
         growableSharedMem = true;
