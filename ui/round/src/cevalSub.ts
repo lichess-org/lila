b51e9a0abc038f830e5f67a65c6d11aff350be75
Thibault Duplessis <t@lichess.org>
2021-09-23 16:11:53 +0200

    use ceval sub in casual games too

--- ui/round/src/cevalSub.ts
+++ ui/round/src/cevalSub.ts
@@ -10,8 +10,6 @@ const truncateFen = (fen: Fen): string => fen.split(' ')[0];
 export function subscribe(ctrl: RoundController): void {
   // allow everyone to cheat against the AI
   if (ctrl.data.opponent.ai) return;
-  // allow registered players to use assistance in casual games
-  if (!ctrl.data.game.rated && ctrl.opts.userId) return;
   // bots can cheat alright
   if (ctrl.data.player.user?.title == 'BOT') return;
 
