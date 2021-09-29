63ab3e23aaf15344e5c0f02fd3f096c72b934e0e
Thibault Duplessis <t@lichess.org>
2021-09-29 16:10:37 +0200

    code tweak

--- app/controllers/Notify.scala
+++ app/controllers/Notify.scala
@@ -13,8 +13,7 @@ final class Notify(env: Env) extends LilaController(env) {
       }
     }
 
-  def clear =
-    Auth { implicit ctx => me =>
-      XhrOrRedirectHome { (env.notifyM.api.remove(Notifies(me.id))) }
-    }
+  def clear = Auth { implicit ctx => me =>
+    env.notifyM.api.remove(Notifies(me.id))
+  }
 }
