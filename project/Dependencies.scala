4f48e2bd1c434871b58b9fafbe2fb7b99267cfd5
Thibault Duplessis <t@lichess.org>
2021-10-07 08:38:57 +0200

    upgrape macros

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
   }
 
   object macwire {
-    val version = "2.4.1"
+    val version = "2.4.2"
     val macros  = "com.softwaremill.macwire" %% "macros"  % version % "provided"
     val util    = "com.softwaremill.macwire" %% "util"    % version % "provided"
     val tagging = "com.softwaremill.common"  %% "tagging" % "2.3.1"
