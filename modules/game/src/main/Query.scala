7219cccda9f53021faee821e7dad5f62c3811208
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-10-13 13:53:22 +0200

    make game api date range inclusive to exclusive

--- modules/game/src/main/Query.scala
+++ modules/game/src/main/Query.scala
@@ -122,13 +122,13 @@ object Query {
     F.variant $ne chess.variant.FromPosition.id
 
   def createdSince(d: DateTime): Bdoc =
-    F.createdAt $gt d
+    F.createdAt $gte d
 
   def createdBetween(since: Option[DateTime], until: Option[DateTime]): Bdoc =
     (since, until) match {
       case (Some(since), None)        => createdSince(since)
       case (None, Some(until))        => F.createdAt $lt until
-      case (Some(since), Some(until)) => F.createdAt $gt since $lt until
+      case (Some(since), Some(until)) => F.createdAt $gte since $lt until
       case _                          => $empty
     }
 
