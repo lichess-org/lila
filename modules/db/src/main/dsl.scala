f46dc7536f9e3672035ef60f5ddfd9179878dd95
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-11-13 22:27:38 +0100

    remove unused LogicalOperators from dsl

--- modules/db/src/main/dsl.scala
+++ modules/db/src/main/dsl.scala
@@ -295,13 +295,6 @@ trait dsl {
 
   }
 
-  trait LogicalOperators { self: ElementBuilder =>
-    def $not(f: String => Expression[Bdoc]): SimpleExpression[Bdoc] = {
-      val expression = f(field)
-      SimpleExpression(field, $doc("$not" -> expression.value))
-    }
-  }
-
   trait ElementOperators { self: ElementBuilder =>
     def $exists(v: Boolean): SimpleExpression[Bdoc] = {
       SimpleExpression(field, $doc("$exists" -> v))
@@ -385,7 +378,6 @@ trait dsl {
       with ComparisonOperators
       with ElementOperators
       with EvaluationOperators
-      with LogicalOperators
       with ArrayOperators
 
   implicit def toBSONDocument[V: BSONWriter](expression: Expression[V]): Bdoc =
