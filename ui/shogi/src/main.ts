0d2df30aef150b523236c7946435568110a7a330
WandererXII <daniel.w.liska@gmail.com>
2021-11-18 18:07:55 +0100

    Remove initial fen from shogi

--- ui/shogi/src/main.ts
+++ ui/shogi/src/main.ts
@@ -1,7 +1,5 @@
 import { piotr } from './piotr';
 
-export const initialFen: Fen = 'lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1';
-
 export type Dests = Map<Key, Key[]>;
 
 export function readDests(lines?: string): Dests | null {
