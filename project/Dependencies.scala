63fc6b5bf5eb9663a1822e2649897f848eea1e86
Scala Steward <me@scala-steward.org>
2021-12-02 18:35:39 +0100

    Update scalatags to 0.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val scaffeine   = "com.github.blemale"         %% "scaffeine"                       % "5.1.1"  % "compile"
   val googleOAuth = "com.google.auth"             % "google-auth-library-oauth2-http" % "1.3.0"
   val scalaUri    = "io.lemonlabs"               %% "scala-uri"                       % "3.6.0"
-  val scalatags   = "com.lihaoyi"                %% "scalatags"                       % "0.10.0"
+  val scalatags   = "com.lihaoyi"                %% "scalatags"                       % "0.11.0"
   val lettuce     = "io.lettuce"                  % "lettuce-core"                    % "6.1.5.RELEASE"
   val epoll       = "io.netty"                    % "netty-transport-native-epoll"    % "4.1.65.Final" classifier "linux-x86_64"
   val autoconfig  = "io.methvin.play"            %% "autoconfig-macros"               % "0.3.2"  % "provided"
