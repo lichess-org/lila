aa2273d2fdb098d6f24a69e29c71f04dbca4059a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-12-07 22:57:03 +0100

    use scalatags styles only explicitly
    fixes accidental inline styles that should have been attributes. thanks @bigsee

--- app/views/mobile.scala
+++ app/views/mobile.scala
@@ -47,16 +47,16 @@ object mobile {
             ),
             div(cls := "right-side")(
               img(
-                width := "437",
-                height := "883",
+                widthA := "437",
+                heightA := "883",
                 cls := "mobile-playing",
                 src := assetUrl("images/mobile/lichesstv-mobile.png"),
                 alt := "Lichess TV on mobile"
               ),
               img(
                 cls := "qrcode",
-                width := "200",
-                height := "200",
+                widthA := "200",
+                heightA := "200",
                 src := assetUrl("images/mobile/dynamic-qrcode.png"),
                 alt := "Download QR code"
               )
