64a132ca4cc931be11fa537c2ddbca125608ee63
WandererXII <daniel.w.liska@gmail.com>
2021-12-12 20:15:27 +0100

    Bump deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,14 +14,14 @@ object Dependencies {
 
   val scalalib    = "com.github.ornicar"   %% "scalalib"                        % "7.0.2"
   val hasher      = "com.roundeights"      %% "hasher"                          % "1.2.1"
-  val jodaTime    = "joda-time"             % "joda-time"                       % "2.10.12"
+  val jodaTime    = "joda-time"             % "joda-time"                       % "2.10.13"
   val maxmind     = "com.sanoma.cda"       %% "maxmind-geoip2-scala"            % "1.3.1-THIB"
   val prismic     = "io.prismic"           %% "scala-kit"                       % "1.2.18-THIB213"
   val scrimage    = "com.sksamuel.scrimage" % "scrimage-core"                   % "4.0.22"
   val scaffeine   = "com.github.blemale"   %% "scaffeine"                       % "4.0.2" % "compile"
-  val googleOAuth = "com.google.auth"       % "google-auth-library-oauth2-http" % "1.2.1"
+  val googleOAuth = "com.google.auth"       % "google-auth-library-oauth2-http" % "1.3.0"
   val scalaUri    = "io.lemonlabs"         %% "scala-uri"                       % "3.6.0"
-  val scalatags   = "com.lihaoyi"          %% "scalatags"                       % "0.9.4"
+  val scalatags   = "com.lihaoyi"          %% "scalatags"                       % "0.10.0"
   val lettuce     = "io.lettuce"            % "lettuce-core"                    % "6.1.5.RELEASE"
   val epoll       = "io.netty"              % "netty-transport-native-epoll"    % "4.1.65.Final" classifier "linux-x86_64"
   val autoconfig  = "io.methvin.play"      %% "autoconfig-macros"               % "0.3.2" % "provided"
@@ -44,7 +44,7 @@ object Dependencies {
   }
 
   object reactivemongo {
-    val version = "1.0.7"
+    val version = "1.0.8"
 
     val driver     = "org.reactivemongo" %% "reactivemongo"               % version
     val stream     = "org.reactivemongo" %% "reactivemongo-akkastream"    % version
