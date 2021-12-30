83f584d8018b873d69164f7fb02f08111e254fa8
Thibault Duplessis <t@lichess.org>
2021-12-30 10:06:29 +0100

    turn inquiry.js into a TS module

--- ui/site/src/inquiry.ts
+++ ui/site/src/inquiry.ts
@@ -1,19 +1,21 @@
-$(function () {
+lichess.load.then(() => {
   const noteStore = lichess.storage.make('inquiry-note');
   const usernameNoteStore = lichess.storage.make('inquiry-note-user');
   const username = $('#inquiry .meat > .user-link').text().split(' ')[0];
   if (username != usernameNoteStore.get()) noteStore.remove();
   usernameNoteStore.set(username);
-  const noteTextArea = $('#inquiry .notes').find('textarea')[0];
+  const noteTextArea = $('#inquiry .notes').find('textarea')[0] as HTMLTextAreaElement;
 
   $('#inquiry .notes').on('mouseenter', () => {
     noteTextArea.focus();
-    noteTextArea.value = noteStore.get();
+    noteTextArea.value = noteStore.get() || '';
   });
 
-  $('#inquiry .notes').on('input', () => setTimeout(() => noteStore.set(noteTextArea.value), 50));
-
-  $('#inquiry .notes').on('submit', noteStore.remove);
+  const $notes = $('#inquiry .notes');
+  $notes.on('input', () => setTimeout(() => noteStore.set(noteTextArea.value), 50));
+  $notes.find('form').on('submit', () => {
+    noteStore.remove();
+  });
 
   $('#inquiry .costello').on('click', () => {
     $('#inquiry').toggleClass('hidden');
@@ -27,22 +29,22 @@ $(function () {
     $('#inquiry input.auto-next').val('0');
   }
 
-  $('#inquiry .switcher input').on('change', function () {
+  $('#inquiry .switcher input').on('change', function (this: HTMLInputElement) {
     nextStore.set(this.checked);
     $('#inquiry input.auto-next').val(this.checked ? '1' : '0');
   });
 
-  Mousetrap.bind('d', () => $('#inquiry .actions.close form.process button[type="submit"]').trigger('click'));
+  window.Mousetrap.bind('d', () => $('#inquiry .actions.close form.process button[type="submit"]').trigger('click'));
 
-  $('#inquiry .atom p').each(function () {
+  $('#inquiry .atom p').each(function (this: HTMLParagraphElement) {
     $(this).html(
       $(this)
         .html()
-        .replaceAll(/(?:https:\/\/)?lichess\.org\/([\w\/]+)/g, '<a href="/$1">lichess.org/$1</a>')
+        .replace(/(?:https:\/\/)?lichess\.org\/([\w\/]+)/g, '<a href="/$1">lichess.org/$1</a>')
     );
   });
 
-  $('#communication').on('click', '.line.author, .post.author', function () {
+  $('#communication').on('click', '.line.author, .post.author', function (this: HTMLElement) {
     // Need to take username from the communcation page so that when being in inquiry for user A and checking communication of user B
     // the notes cannot be mistakenly attributed to user A.
     const username = $('#communication').find('.title').text().split(' ')[0];
