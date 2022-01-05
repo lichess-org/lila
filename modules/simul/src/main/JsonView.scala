96fd4e15f7981f82147d827a2c6c6321511a1dfd
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-01-05 10:47:24 +0000

    Take into account simul player online status

--- modules/simul/src/main/JsonView.scala
+++ modules/simul/src/main/JsonView.scala
@@ -110,7 +110,7 @@ final class JsonView(
         .add("title" -> light.map(_.title))
         .add("provisional" -> ~player.provisional)
         .add("patron" -> light.??(_.isPatron))
-        .add("online" -> true)
+        .add("online" -> isOnline(player.user))
     }
 
   private def applicantJson(app: SimulApplicant): Fu[JsObject] =
