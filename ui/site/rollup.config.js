a7a8e082cfafd0b540d10197940937e18b385f5f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-01-26 13:39:04 +0100

    show longer commit hashes on /source

--- ui/site/rollup.config.js
+++ ui/site/rollup.config.js
@@ -76,7 +76,7 @@ export default rollupProject({
         values: {
           __info__: JSON.stringify({
             date: new Date(new Date().toUTCString()).toISOString().split('.')[0] + '+00:00',
-            commit: execSync('git rev-parse -q --short HEAD', { encoding: 'utf-8' }).trim(),
+            commit: execSync('git rev-parse -q HEAD', { encoding: 'utf-8' }).trim(),
             message: execSync('git log -1 --pretty=%s', { encoding: 'utf-8' }).trim(),
           }),
         },
