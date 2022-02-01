672e424fca5df4f996ff75d8a2623635b38be2d7
kraktus <kraktus@users.noreply.github.com>
2022-02-01 16:23:51 +0100

    Monitor number of users with email notification on

--- modules/pref/src/main/Env.scala
+++ modules/pref/src/main/Env.scala
@@ -1,6 +1,8 @@
 package lila.pref
 
+import akka.actor._
 import com.softwaremill.macwire.Module
+import scala.concurrent.duration._
 
 import lila.common.config.CollName
 
@@ -8,7 +10,11 @@ import lila.common.config.CollName
 final class Env(
     cacheApi: lila.memo.CacheApi,
     db: lila.db.Db
-)(implicit ec: scala.concurrent.ExecutionContext) {
+)(implicit ec: scala.concurrent.ExecutionContext, system: ActorSystem) {
 
   val api = new PrefApi(db(CollName("pref")), cacheApi)
+
+  system.scheduler.scheduleWithFixedDelay(10 minute, 10 minute) { () =>
+    api.corresEmailNotifUsers foreach { lila.mon.pref.emailNotifNbUsers.update(_)}
+  }
 }
