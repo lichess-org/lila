e0fe233c262a48ea0891dac6ccfc80473611d3d5
Thibault Duplessis <t@lichess.org>
2022-02-16 18:11:54 +0100

    always show the tournament creator - closes #10542

--- app/views/tournament/side.scala
+++ app/views/tournament/side.scala
@@ -91,10 +91,7 @@ object side {
         ),
         tour.noBerserk option div(cls := "text", dataIcon := "")("No Berserk allowed"),
         tour.noStreak option div(cls := "text", dataIcon := "")("No Arena streaks"),
-        !tour.isScheduled && tour.description.isEmpty option frag(
-          trans.by(userIdLink(tour.createdBy.some)),
-          br
-        ),
+        !tour.isScheduled option frag(small(trans.by(userIdLink(tour.createdBy.some))), br),
         (!tour.isStarted || (tour.isScheduled && tour.position.isDefined)) option absClientDateTime(
           tour.startsAt
         ),
