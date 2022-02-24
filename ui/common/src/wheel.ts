efc9406b7890b45e9b8180d0f9a1116e43ac13b4
Thibault Duplessis <t@lichess.org>
2022-02-24 09:58:54 +0100

    stepwiseScroll: figure out if the device has scroll steps

--- ui/common/src/wheel.ts
+++ ui/common/src/wheel.ts
@@ -1,12 +1,25 @@
 export default function stepwiseScroll(
   inner: (e: WheelEvent, scroll: boolean) => void,
-  threshold: number = 120
+  threshold: number = 100
 ): (e: WheelEvent) => void {
   /** Track distance scrolled across multiple wheel events, resetting after 500 ms.  */
   let lastScrollDirection: string;
   let scrollTotal = 0;
   let lastScrollTimestamp = -9999;
+  let lastScrollStable: undefined | number | false; // last scroll delta, or false if previously found to be variable
   return (e: WheelEvent) => {
+    const absDelta = Math.abs(e.deltaY);
+    if (!absDelta) return;
+    if (lastScrollStable == undefined) {
+      // first scroll
+      lastScrollStable = absDelta;
+      lastScrollTimestamp = e.timeStamp;
+      return inner(e, true);
+    } else {
+      // scroll amount is stable, assume step wheel
+      if (absDelta == lastScrollStable) return inner(e, true);
+      else lastScrollStable = false;
+    }
     if (e.deltaY > 0) {
       if (lastScrollDirection != 'forward' || e.timeStamp - lastScrollTimestamp > 500) scrollTotal = 0;
       lastScrollDirection = 'forward';
