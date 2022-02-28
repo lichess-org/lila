7bc235633be474a8337ba9139d31db08e9854700
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-03-01 00:07:41 +0100

    cut back scroll wheel heuristics (#10568)
    - Remove fixed step and timeout heuristics.
    - Do not reset if intermittently changing direction.
    - Use much lower threshold. 4,000244140625 (with deltaMode 0) reported by
      Discord user. Feels very sensitive on my touchpad, but still
      significantly better than nothing.

--- ui/common/src/wheel.ts
+++ ui/common/src/wheel.ts
@@ -1,36 +1,12 @@
-export default function stepwiseScroll(
-  inner: (e: WheelEvent, scroll: boolean) => void,
-  threshold = 100
-): (e: WheelEvent) => void {
-  /** Track distance scrolled across multiple wheel events, resetting after 500 ms.  */
-  let lastScrollForward: boolean;
+export default function stepwiseScroll(inner: (e: WheelEvent, scroll: boolean) => void): (e: WheelEvent) => void {
   let scrollTotal = 0;
-  let lastScrollTimestamp = -9999;
-  let lastScrollStable: undefined | number | false; // last scroll delta, or false if previously found to be variable
   return (e: WheelEvent) => {
-    const absDelta = Math.abs(e.deltaY);
-    if (!absDelta) return;
-
-    if (lastScrollStable == undefined) {
-      // first scroll
-      lastScrollStable = absDelta;
-      lastScrollTimestamp = e.timeStamp;
-      return inner(e, true);
-    } else {
-      // scroll amount is stable, assume step wheel
-      if (absDelta == lastScrollStable) return inner(e, true);
-      else lastScrollStable = false;
-    }
-
-    const forward = e.deltaY > 0;
-    if (lastScrollForward != forward || e.timeStamp - lastScrollTimestamp > 500) scrollTotal = 0;
-    lastScrollForward = forward;
-    scrollTotal += absDelta;
-    if (scrollTotal >= threshold) {
+    scrollTotal += e.deltaY * (e.deltaMode ? 40 : 1);
+    if (Math.abs(scrollTotal) >= 4) {
       inner(e, true);
-      scrollTotal -= threshold;
-    } else inner(e, false);
-
-    lastScrollTimestamp = e.timeStamp;
+      scrollTotal = 0;
+    } else {
+      inner(e, false);
+    }
   };
 }
