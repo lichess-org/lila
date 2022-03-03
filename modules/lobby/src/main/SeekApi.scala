2e8aa4f20e19d02a3acfa3d3b302db6d25bf3472
Thibault Duplessis <t@lichess.org>
2022-03-03 10:37:31 +0100

    simplify SeekApi cache key

--- modules/lobby/src/main/SeekApi.scala
+++ modules/lobby/src/main/SeekApi.scala
@@ -16,9 +16,9 @@ final class SeekApi(
 )(implicit ec: scala.concurrent.ExecutionContext) {
   import config._
 
-  sealed private trait CacheKey
-  private object ForAnon extends CacheKey
-  private object ForUser extends CacheKey
+  private type CacheKey = Boolean
+  private val ForAnon = false
+  private val ForUser = true
 
   private def allCursor =
     coll
