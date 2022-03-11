f2a952c8ed3a8db16820f9c349e7fea31ddc07c2
Thibault Duplessis <t@lichess.org>
2022-03-11 22:45:44 +0100

    fallback to lila after lila-http - closes #10653

--- ui/tournament/src/xhr.ts
+++ ui/tournament/src/xhr.ts
@@ -59,29 +59,40 @@ export const loadPage = throttlePromise(
 export const loadPageOf = (ctrl: TournamentController, userId: string) =>
   xhr.json(`/tournament/${ctrl.data.id}/page-of/${userId}`);
 
+// use lila when lila-http is offline
+const reloadEndpointFallback = (ctrl: TournamentController) => `/tournament/${ctrl.data.id}`;
+
 // don't use xhr.json to avoid getting the X-Requested-With header
 // that causes a CORS preflight check
-// TODO FIXME
-export const reloadNow = (ctrl: TournamentController): Promise<void> =>
-  fetch(
-    xhr.url(ctrl.data.reloadEndpoint, {
-      page: ctrl.focusOnMe ? undefined : ctrl.page,
-      playerInfo: ctrl.playerInfo.id,
-      partial: true,
-      me: ctrl.data.myUsername,
-    }),
-    {
-      ...xhr.defaultInit,
-      headers: xhr.jsonHeader,
-    }
-  )
-    .then(res => xhr.ensureOk(res).json())
-    .then(data => {
-      ctrl.reload(data);
-      ctrl.redraw();
-      const extraDelay = Math.floor(ctrl.nbWatchers / 2) * (data.me ? 1 : 3);
-      return new Promise(resolve => setTimeout(resolve, extraDelay));
-    }, onFail);
+export const reloadNow: (ctrl: TournamentController) => Promise<void> = finallyDelay(
+  ctrl => Math.floor(ctrl.nbWatchers / 2) * (ctrl.data.me ? 1 : 3),
+  ctrl =>
+    fetch(
+      xhr.url(ctrl.data.reloadEndpoint, {
+        page: ctrl.focusOnMe ? undefined : ctrl.page,
+        playerInfo: ctrl.playerInfo.id,
+        partial: true,
+        me: ctrl.data.myUsername,
+      }),
+      {
+        ...xhr.defaultInit,
+        headers: xhr.jsonHeader,
+      }
+    )
+      .then(res => xhr.ensureOk(res).json())
+      .then(
+        data => {
+          ctrl.reload(data);
+          ctrl.redraw();
+        },
+        () => {
+          if (ctrl.data.reloadEndpoint != reloadEndpointFallback(ctrl)) {
+            ctrl.data.reloadEndpoint = reloadEndpointFallback(ctrl);
+            return reloadNow(ctrl);
+          } else return onFail();
+        }
+      )
+);
 
 export const reloadSoon = throttlePromise(finallyDelay(() => 4000 + Math.floor(Math.random() * 1000), reloadNow));
 
