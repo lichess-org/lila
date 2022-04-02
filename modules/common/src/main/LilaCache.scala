4f31a5f25ef6bb684dab21ac25074c7ed3e16bdb
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-04-02 11:50:51 +0200

    remove unused methods of SyncActorMap

--- modules/common/src/main/LilaCache.scala
+++ modules/common/src/main/LilaCache.scala
@@ -6,7 +6,5 @@ import com.github.blemale.scaffeine.Scaffeine
 
 object LilaCache {
 
-  def caffeine: Caffeine[Any, Any] = Caffeine.newBuilder().scheduler(Scheduler.systemScheduler)
-
   def scaffeine: Scaffeine[Any, Any] = Scaffeine().scheduler(Scheduler.systemScheduler)
 }
