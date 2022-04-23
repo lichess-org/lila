de37f6a50f4563f44dc90c5bbd5e57b0becf2beb
Thibault Duplessis <t@lichess.org>
2022-04-23 11:04:11 +0200

    fix (name: 'level') and codegolf ui/lobby levelButtons

--- ui/lobby/src/view/setup/components/levelButtons.ts
+++ ui/lobby/src/view/setup/components/levelButtons.ts
@@ -2,21 +2,20 @@ import { h } from 'snabbdom';
 import LobbyController from '../../../ctrl';
 import { option } from './option';
 
-export const levelButtons = (ctrl: LobbyController) =>
-  ctrl.opts.blindMode
+export const levelButtons = (ctrl: LobbyController) => {
+  const { trans, setupCtrl } = ctrl;
+  return ctrl.opts.blindMode
     ? [
-        h('label', { attrs: { for: 'sf_level' } }, ctrl.trans('strength')),
+        h('label', { attrs: { for: 'sf_level' } }, trans('strength')),
         h(
           'select#sf_level',
-          { on: { change: (e: Event) => ctrl.setupCtrl.aiLevel(parseInt((e.target as HTMLSelectElement).value)) } },
-          [1, 2, 3, 4, 5, 6, 7, 8].map(level =>
-            option({ key: `${level}`, name: `${level}` }, ctrl.setupCtrl.aiLevel().toString())
-          )
+          { on: { change: (e: Event) => setupCtrl.aiLevel(parseInt((e.target as HTMLSelectElement).value)) } },
+          '12345678'.split('').map(key => option({ key, name: key }, setupCtrl.aiLevel().toString()))
         ),
       ]
     : [
         h('br'),
-        ctrl.trans('strength'),
+        trans('strength'),
         h('div.level.buttons', [
           h(
             'div.config_level',
@@ -25,12 +24,14 @@ export const levelButtons = (ctrl: LobbyController) =>
               [1, 2, 3, 4, 5, 6, 7, 8].map(level =>
                 h('div', [
                   h(`input#sf_level_${level}`, {
-                    attrs: { type: 'radio', value: level, checked: level === ctrl.setupCtrl.aiLevel() },
+                    attrs: { name: 'level', type: 'radio', value: level, checked: level === setupCtrl.aiLevel() },
                     on: {
-                      change: (e: Event) => ctrl.setupCtrl.aiLevel(parseInt((e.target as HTMLInputElement).value)),
+                      change: (e: Event) => 
+                        setupCtrl.aiLevel(parseInt((e.target as HTMLInputElement).value))
+                      ,
                     },
                   }),
-                  h('label.required', { attrs: { for: `sf_level_${level}` } }, level),
+                  h('label', { attrs: { for: `sf_level_${level}` } }, level),
                 ])
               )
             )
@@ -38,9 +39,10 @@ export const levelButtons = (ctrl: LobbyController) =>
           h(
             'div.ai_info',
             h(
-              `div.sf_level_${ctrl.setupCtrl.aiLevel()}`,
-              ctrl.trans('aiNameLevelAiLevel', 'Fairy-Stockfish 14', ctrl.setupCtrl.aiLevel())
+              `div.sf_level_${setupCtrl.aiLevel()}`,
+              trans('aiNameLevelAiLevel', 'Fairy-Stockfish 14', setupCtrl.aiLevel())
             )
           ),
         ]),
       ];
+};
