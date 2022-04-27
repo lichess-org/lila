08bbcc6a49aa85636c68f596ef9d6489c50710c4
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-04-27 17:19:45 +0100

    Fix types

--- ui/common/src/throttle.ts
+++ ui/common/src/throttle.ts
@@ -31,7 +31,7 @@ export function throttlePromise<T extends (...args: any) => Promise<void>>(
  * or rejects).
  */
 export function finallyDelay<T extends (...args: any) => Promise<void>>(
-  delay: number,
+  delay: ((...args: Parameters<T>) => number) | any,
   wrapped: T
 ): (...args: Parameters<T>) => Promise<void> {
   return function (this: any, ...args: Parameters<T>): Promise<void> {
