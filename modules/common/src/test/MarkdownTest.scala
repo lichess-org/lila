84a9028707e931f547f083f1653a3bf69924af9a
Thibault Duplessis <t@lichess.org>
2022-05-12 11:24:51 +0200

    failing test: markdown autolinks don't get the rel attributes

--- modules/common/src/test/MarkdownTest.scala
+++ modules/common/src/test/MarkdownTest.scala
@@ -6,15 +6,15 @@ class MarkdownTest extends Specification {
 
   val render = new MarkdownRender()("test") _
 
-  // "autolinks" should {
-  //   "remove tracking tags" in {
-  //     val md = Markdown("http://example.com?utm_campaign=spy&utm_source=evil")
-  //     render(
-  //       md
-  //     ) must_== """<p><a href="http://example.com">Example</a></p>
-// """
-  //   }
-  // }
+  "autolinks" should {
+    "add rel" in {
+      val md = Markdown("http://example.com")
+      render(
+        md
+      ) must_== """<p><a href="http://example.com" rel="nofollow noopener noreferrer">Example</a></p>
+"""
+    }
+  }
   "markdown links" should {
     "remove tracking tags" in {
       val md = Markdown("[Example](http://example.com?utm_campaign=spy&utm_source=evil)")
