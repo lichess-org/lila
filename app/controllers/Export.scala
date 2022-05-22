05a5bf6ae59a10b35c185444be43d517bcc6f91c
Thibault Duplessis <t@lichess.org>
2022-05-22 10:31:04 +0200

    refactor export controller, allow all UAs to export gifs

--- app/controllers/Export.scala
+++ app/controllers/Export.scala
@@ -4,10 +4,11 @@ import akka.stream.scaladsl._
 import akka.util.ByteString
 import chess.Color
 import chess.format.{ FEN, Forsyth, Uci }
-import play.api.mvc.Result
+import play.api.mvc.{ RequestHeader, Result }
 import scala.concurrent.duration._
 
 import lila.app._
+import lila.common.{ HTTPRequest, IpAddress }
 import lila.game.Pov
 import lila.puzzle.Puzzle.Id
 
@@ -18,24 +19,29 @@ final class Export(env: Env) extends LilaController(env) {
     duration = 1.minute,
     key = "export.image.global"
   )
-  private val ExportGifRateLimitGlobal = new lila.memo.RateLimit[String](
-    credits = 240,
+  private val ExportImageRateLimitByIp = new lila.memo.RateLimit[IpAddress](
+    credits = 15,
     duration = 1.minute,
-    key = "export.gif.global"
+    key = "export.image.ip"
   )
 
-  def gif(id: String, color: String) =
-    Open { implicit ctx =>
-      OnlyHumansAndFacebookOrTwitter {
-        ExportGifRateLimitGlobal("-", msg = ctx.ip.value) {
-          OptionFuResult(env.game.gameRepo gameWithInitialFen id) { case (game, initialFen) =>
-            val pov = Pov(game, Color.fromName(color) | Color.white)
-            env.game.gifExport.fromPov(pov, initialFen) map
-              stream("image/gif") map
-              gameImageCacheSeconds(game)
-          }
-        }(rateLimitedFu)
+  private def exportImageOf[A](fetch: Fu[Option[A]])(convert: A => Fu[Result]) =
+    Action.async { implicit req =>
+      fetch flatMap {
+        _.fold(notFoundJson()) { res =>
+          ExportImageRateLimitByIp(HTTPRequest ipAddress req) {
+            ExportImageRateLimitGlobal("-") {
+              convert(res)
+            }(rateLimitedFu)
+          }(rateLimitedFu)
+        }
       }
+    }
+
+  def gif(id: String, color: String) =
+    exportImageOf(env.game.gameRepo gameWithInitialFen id) { case (game, initialFen) =>
+      env.game.gifExport.fromPov(Pov(game, Color.fromName(color) | Color.white), initialFen) map
+        stream(cacheSeconds = if (game.finishedOrAborted) 3600 * 24 else 10)
     }
 
   def legacyGameThumbnail(id: String) =
@@ -43,57 +49,34 @@ final class Export(env: Env) extends LilaController(env) {
       MovedPermanently(routes.Export.gameThumbnail(id).url)
     }
 
-  def gameThumbnail(id: String) =
-    Open { implicit ctx =>
-      ExportImageRateLimitGlobal("-", msg = ctx.ip.value) {
-        OptionFuResult(env.game.gameRepo game id) { game =>
-          env.game.gifExport.gameThumbnail(game) map
-            stream("image/gif") map
-            gameImageCacheSeconds(game)
-        }
-      }(rateLimitedFu)
-    }
+  def gameThumbnail(id: String) = exportImageOf(env.game.gameRepo game id) { game =>
+    env.game.gifExport.gameThumbnail(game) map
+      stream(cacheSeconds = if (game.finishedOrAborted) 3600 * 24 else 10)
+  }
 
   def puzzleThumbnail(id: String) =
-    Open { implicit ctx =>
-      ExportImageRateLimitGlobal("-", msg = ctx.ip.value) {
-        OptionFuResult(env.puzzle.api.puzzle find Id(id)) { puzzle =>
-          env.game.gifExport.thumbnail(
-            fen = puzzle.fenAfterInitialMove,
-            lastMove = puzzle.line.head.uci.some,
-            orientation = puzzle.color
-          ) map stream("image/gif") map { res =>
-            res.withHeaders(CACHE_CONTROL -> "max-age=1209600")
-          }
-        }
-      }(rateLimitedFu)
+    exportImageOf(env.puzzle.api.puzzle find Id(id)) { puzzle =>
+      env.game.gifExport.thumbnail(
+        fen = puzzle.fenAfterInitialMove,
+        lastMove = puzzle.line.head.uci.some,
+        orientation = puzzle.color
+      ) map stream()
     }
 
   def fenThumbnail(fen: String, color: String, lastMove: Option[String]) =
-    Open { implicit ctx =>
-      ExportImageRateLimitGlobal("-", msg = ctx.ip.value) {
-        val cleanFen = FEN.clean(fen)
-        Forsyth << cleanFen match {
-          case None => BadRequest.fuccess
-          case Some(_) =>
-            env.game.gifExport.thumbnail(
-              fen = cleanFen,
-              lastMove = lastMove flatMap (Uci.Move(_).map(_.uci)),
-              orientation = Color.fromName(color) | Color.White
-            ) map stream("image/gif") map { res =>
-              res.withHeaders(CACHE_CONTROL -> "max-age=1209600")
-            }
-        }
-      }(rateLimitedFu)
+    exportImageOf(fuccess(Forsyth << FEN.clean(fen))) { _ =>
+      env.game.gifExport.thumbnail(
+        fen = FEN clean fen,
+        lastMove = lastMove flatMap (Uci.Move(_).map(_.uci)),
+        orientation = Color.fromName(color) | Color.White
+      ) map stream()
     }
 
-  private def gameImageCacheSeconds(game: lila.game.Game)(res: Result): Result = {
-    val cacheSeconds =
-      if (game.finishedOrAborted) 3600 * 24
-      else 10
-    res.withHeaders(CACHE_CONTROL -> s"max-age=$cacheSeconds")
-  }
-
-  private def stream(contentType: String)(stream: Source[ByteString, _]) =
-    Ok.chunked(stream).withHeaders(noProxyBufferHeader) as contentType
+  private def stream(contentType: String = "image/gif", cacheSeconds: Int = 1209600)(
+      stream: Source[ByteString, _]
+  ) =
+    Ok.chunked(stream)
+      .withHeaders(noProxyBufferHeader)
+      .withHeaders(CACHE_CONTROL -> s"max-age=$cacheSeconds")
+      .as(contentType)
 }
