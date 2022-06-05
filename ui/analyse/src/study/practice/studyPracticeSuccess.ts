abcb2f8d853e8066762ca477b2a076304f16cd73
Thibault Duplessis <t@lichess.org>
2022-06-05 10:13:12 +0200

    TS code golf

--- ui/analyse/src/study/practice/studyPracticeSuccess.ts
+++ ui/analyse/src/study/practice/studyPracticeSuccess.ts
@@ -3,31 +3,28 @@ import { Goal } from './interfaces';
 import { Comment } from '../../practice/practiceCtrl';
 
 // returns null if not deep enough to know
-function isDrawish(node: Tree.Node): boolean | null {
-  if (!hasSolidEval(node)) return null;
-  return !node.ceval!.mate && Math.abs(node.ceval!.cp!) < 150;
-}
+const isDrawish = (node: Tree.Node): boolean | null =>
+  hasSolidEval(node) ? !node.ceval!.mate && Math.abs(node.ceval!.cp!) < 150 : null;
+
 // returns null if not deep enough to know
-function isWinning(node: Tree.Node, goalCp: number, color: Color): boolean | null {
+const isWinning = (node: Tree.Node, goalCp: number, color: Color): boolean | null => {
   if (!hasSolidEval(node)) return null;
   const cp = node.ceval!.mate! > 0 ? 99999 : node.ceval!.mate! < 0 ? -99999 : node.ceval!.cp;
   return color === 'white' ? cp! >= goalCp : cp! <= goalCp;
-}
+};
+
 // returns null if not deep enough to know
-function myMateIn(node: Tree.Node, color: Color): number | boolean | null {
+const myMateIn= (node: Tree.Node, color: Color): number | boolean | null => {
   if (!hasSolidEval(node)) return null;
   if (!node.ceval!.mate) return false;
   const mateIn = node.ceval!.mate! * (color === 'white' ? 1 : -1);
   return mateIn > 0 ? mateIn : false;
 }
 
-function hasSolidEval(node: Tree.Node) {
-  return node.ceval && node.ceval.depth >= 16;
-}
+const hasSolidEval = (node: Tree.Node) => node.ceval && node.ceval.depth >= 16;
 
-function hasBlundered(comment: Comment | null) {
-  return comment && (comment.verdict === 'mistake' || comment.verdict === 'blunder');
-}
+const hasBlundered = (comment: Comment | null) =>
+  comment && (comment.verdict === 'mistake' || comment.verdict === 'blunder');
 
 // returns null = ongoing, true = win, false = fail
 export default function (root: AnalyseCtrl, goal: Goal, nbMoves: number): boolean | null {
