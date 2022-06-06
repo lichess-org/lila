a28b96b47bed287103f359a3bf31a4daef8421ab
Ben Rollin <ben.rollin@clever.com>
2022-06-06 02:34:20 -0700

    remove extraneous div

--- ui/lobby/src/view/setup/components/ratingDifferenceSliders.ts
+++ ui/lobby/src/view/setup/components/ratingDifferenceSliders.ts
@@ -1,51 +1,49 @@
 import { h } from 'snabbdom';
 import LobbyController from '../../../ctrl';
 
+// TODO allow zero zero, but change on backend to -25 to +25 ?
 export const ratingDifferenceSliders = (ctrl: LobbyController) => {
   if (!ctrl.me || ctrl.opts.blindMode) return null;
 
   const { trans, setupCtrl } = ctrl;
-  return h(
-    'div.optional_config',
-    h('div.rating-range-config', [
-      trans('ratingRange'),
-      h('div.rating-range', [
-        h('input.range.rating-range__min', {
-          attrs: {
-            type: 'range',
-            min: '-500',
-            max: '0',
-            step: '50',
-            value: setupCtrl.ratingMin(),
+  return h('div.rating-range-config.optional-config', [
+    trans('ratingRange'),
+    h('div.rating-range', [
+      h('input.range.rating-range__min', {
+        attrs: {
+          type: 'range',
+          min: '-500',
+          max: '0',
+          step: '50',
+          value: setupCtrl.ratingMin(),
+        },
+        on: {
+          input: (e: Event) => {
+            const newVal = parseInt((e.target as HTMLInputElement).value);
+            if (newVal === 0 && setupCtrl.ratingMax() === 0) setupCtrl.ratingMax(50);
+            setupCtrl.ratingMin(newVal);
           },
-          on: {
-            input: (e: Event) => {
-              const newVal = parseInt((e.target as HTMLInputElement).value);
-              if (newVal === 0 && setupCtrl.ratingMax() === 0) setupCtrl.ratingMax(50);
-              setupCtrl.ratingMin(newVal);
-            },
+        },
+      }),
+      h('span.rating-min', '-' + Math.abs(setupCtrl.ratingMin())),
+      '/',
+      h('span.rating-max', '+' + setupCtrl.ratingMax()),
+      h('input.range.rating-range__max', {
+        attrs: {
+          type: 'range',
+          min: '0',
+          max: '500',
+          step: '50',
+          value: setupCtrl.ratingMax(),
+        },
+        on: {
+          input: (e: Event) => {
+            const newVal = parseInt((e.target as HTMLInputElement).value);
+            if (newVal === 0 && setupCtrl.ratingMin() === 0) setupCtrl.ratingMin(-50);
+            setupCtrl.ratingMax(newVal);
           },
-        }),
-        h('span.rating-min', '-' + Math.abs(setupCtrl.ratingMin())),
-        '/',
-        h('span.rating-max', '+' + setupCtrl.ratingMax()),
-        h('input.range.rating-range__max', {
-          attrs: {
-            type: 'range',
-            min: '0',
-            max: '500',
-            step: '50',
-            value: setupCtrl.ratingMax(),
-          },
-          on: {
-            input: (e: Event) => {
-              const newVal = parseInt((e.target as HTMLInputElement).value);
-              if (newVal === 0 && setupCtrl.ratingMin() === 0) setupCtrl.ratingMin(-50);
-              setupCtrl.ratingMax(newVal);
-            },
-          },
-        }),
-      ]),
-    ])
-  );
+        },
+      }),
+    ]),
+  ]);
 };
