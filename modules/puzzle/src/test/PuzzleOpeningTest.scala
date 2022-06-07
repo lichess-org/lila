e56e0cd8042129d2eede6db39f6f38bffe5ad6b5
Thibault Duplessis <t@lichess.org>
2022-06-07 10:47:49 +0200

    fix puzzle test

--- modules/puzzle/src/test/PuzzleOpeningTest.scala
+++ modules/puzzle/src/test/PuzzleOpeningTest.scala
@@ -4,12 +4,12 @@ import org.specs2.mutable.Specification
 
 class PuzzleOpeningTest extends Specification {
 
-  "slugify" in {
-    import PuzzleOpening.slugify
+  "nameToKey" in {
+    import PuzzleOpening.{ Key, nameToKey }
     "opening name" in {
-      slugify("Grünfeld Defense") must_== "Grunfeld_Defense"
-      slugify("King's Pawn Game") must_== "Kings_Pawn_Game"
-      slugify("Neo-Grünfeld Defense") must_== "Neo-Grunfeld_Defense"
+      nameToKey("Grünfeld Defense") must_== Key("Grunfeld_Defense")
+      nameToKey("King's Pawn Game") must_== Key("Kings_Pawn_Game")
+      nameToKey("Neo-Grünfeld Defense") must_== Key("Neo-Grunfeld_Defense")
     }
   }
 
