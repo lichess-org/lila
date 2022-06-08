28691aa892b2dd726357c66289c1b76013ae5d23
MiauiKatze <86127109+Rabergsel@users.noreply.github.com>
2022-06-08 19:55:40 +0200

    Update spam.ts
    Ran prettier on it

--- ui/chat/src/spam.ts
+++ ui/chat/src/spam.ts
@@ -1,57 +1,56 @@
-import * as xhr from 'common/xhr';
+import * as xhr from "common/xhr";
 
-export const skip = (txt: string) => (suspLink(txt) || suspWords(txt) || followMe(txt)) && !isKnownSpammer();
+export const skip = (txt: string) =>
+  (suspLink(txt) || suspWords(txt) || followMe(txt)) && !isKnownSpammer();
 
 export const selfReport = (txt: string) => {
   if (isKnownSpammer()) return;
   const hasSuspLink = suspLink(txt);
   const hasSusWords = suspWords(txt);
-  if (hasSuspLink || hasSusWords) xhr.text(`/jslog/${window.location.href.substr(-12)}?n=spam`, { method: 'post' });
-  if (hasSuspLink || followMe(txt) || hasSusWords) lichess.storage.set('chat-spam', '1');
+  if (hasSuspLink || hasSusWords)
+    xhr.text(`/jslog/${window.location.href.substr(-12)}?n=spam`, {
+      method: "post",
+    });
+  if (hasSuspLink || followMe(txt) || hasSusWords)
+    lichess.storage.set("chat-spam", "1");
 };
 
-const isKnownSpammer = () => lichess.storage.get('chat-spam') == '1';
+const isKnownSpammer = () => lichess.storage.get("chat-spam") == "1";
 
 const spamRegex = new RegExp(
   [
-    'xcamweb.com',
-    '(^|[^i])chess-bot',
-    'chess-cheat',
-    'coolteenbitch',
-    'letcafa.webcam',
-    'tinyurl.com/',
-    'wooga.info/',
-    'bit.ly/',
-    'wbt.link/',
-    'eb.by/',
-    '001.rs/',
-    'shr.name/',
-    'u.to/',
-    '.3-a.net',
-    '.ssl443.org',
-    '.ns02.us',
-    '.myftp.info',
-    '.flinkup.com',
-    '.serveusers.com',
-    'badoogirls.com',
-    'hide.su',
-    'wyon.de',
-    'sexdatingcz.club',
-    'qps.ru',
-    'tiny.cc/',
-    'trasderk.blogspot.com',
+    "xcamweb.com",
+    "(^|[^i])chess-bot",
+    "chess-cheat",
+    "coolteenbitch",
+    "letcafa.webcam",
+    "tinyurl.com/",
+    "wooga.info/",
+    "bit.ly/",
+    "wbt.link/",
+    "eb.by/",
+    "001.rs/",
+    "shr.name/",
+    "u.to/",
+    ".3-a.net",
+    ".ssl443.org",
+    ".ns02.us",
+    ".myftp.info",
+    ".flinkup.com",
+    ".serveusers.com",
+    "badoogirls.com",
+    "hide.su",
+    "wyon.de",
+    "sexdatingcz.club",
+    "qps.ru",
+    "tiny.cc/",
+    "trasderk.blogspot.com",
   ]
-    .map(url => url.replace(/\./g, '\\.').replace(/\//g, '\\/'))
-    .join('|')
+    .map((url) => url.replace(/\./g, "\\.").replace(/\//g, "\\/"))
+    .join("|")
 );
 
-const susWordsRegex = new RegExp(
-  [
-    'penis',
-    'fuck you',
-  ]
-  .join('|')
-  );
+const susWordsRegex = new RegExp(["penis", "fuck you"].join("|"));
 
 const suspLink = (txt: string) => !!txt.match(spamRegex);
 
