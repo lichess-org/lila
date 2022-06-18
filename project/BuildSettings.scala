d02f7ad5fb8483ea0bd11f96a24d2b328f72e4af
WandererXII <43043181+WandererXII@users.noreply.github.com>
2022-06-18 16:38:55 +0200

    Use scala 2.13.8

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,7 +6,7 @@ object BuildSettings {
   import Dependencies._
 
   val lilaVersion        = "3.0"
-  val globalScalaVersion = "2.13.6"
+  val globalScalaVersion = "2.13.8"
 
   val useEpoll = sys.props.get("epoll").fold(false)(_.toBoolean)
   if (useEpoll) println("--- epoll build ---")
