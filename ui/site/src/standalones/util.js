e8f9fe52fa690b690321a905c16836ff1bf200a3
WandererXII <43043181+WandererXII@users.noreply.github.com>
2022-06-18 17:13:50 +0200

    Close overlay on mousedown

--- ui/site/src/standalones/util.js
+++ ui/site/src/standalones/util.js
@@ -410,12 +410,10 @@ $.modal = function (html, cls, onClose) {
     .prepend('<span class="close" data-icon="L"></span>');
   var $overlay = $('<div id="modal-overlay">').addClass(cls).data('onClose', onClose).html($wrap);
   $wrap.find('.close').on('click', $.modal.close);
-  $overlay.on('click', function () {
-    // disgusting hack
-    // dragging slider out of a modal closes the modal
-    if (!$('.ui-slider-handle.ui-state-focus').length) $.modal.close();
+  $overlay.on('mousedown', function () {
+    $.modal.close();
   });
-  $wrap.on('click', function (e) {
+  $wrap.on('mousedown', function (e) {
     e.stopPropagation();
   });
   $('body').addClass('overlayed').prepend($overlay);
