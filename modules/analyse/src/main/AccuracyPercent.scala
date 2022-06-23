483042d894a135a70fec3c1651676abddc6d6ebf
Thibault Duplessis <t@lichess.org>
2022-06-23 11:58:09 +0200

    new acc% formula that ensures f(0) = 100

--- modules/analyse/src/main/AccuracyPercent.scala
+++ modules/analyse/src/main/AccuracyPercent.scala
@@ -23,21 +23,21 @@ def model_func(x, a, k, b):
     return a * np.exp(-k*x) + b
 
 # sample data
-x = np.array([  0,  5, 10, 20, 40, 60, 80, 90, 100])
-y = np.array([100, 75, 60, 42, 20,  5,  0,  0,   0])
+xs      = np.array([    0,  5, 10, 20, 40, 60,    80, 90, 100])
+ys      = np.array([  100, 75, 60, 42, 20,  5,     0,  0,   0])
+sigma   = np.array([0.005,  1,  1,  1,  1,  1, 0.005,  1,   1]) # error stdev
 
-# curve fit
-p0 = (1.,1.e-5,1.) # starting search koefs
-opt, pcov = curve_fit(model_func, x, y, p0)
+opt, pcov = curve_fit(model_func, xs, ys, None, sigma)
 a, k, b = opt
 print(f"{a} * exp(-{k} * x) + {b}")
+for x in xs:
+    print(f"f({x}) = {model_func(x, a, k, b)}");
    */
   def fromWinPercents(before: WinPercent, after: WinPercent): AccuracyPercent = AccuracyPercent {
     if (after.value >= before.value) 100d
     else {
-      val diff = before.value - after.value
-      // 105.571942 * Math.exp(-0.037088731 * diff) - 4.08425894
-      98.70882650493327 * Math.exp(-0.04227403940198299 * diff) + -1.852425254357673
+      val winDiff = before.value - after.value
+      103.1668100711649 * Math.exp(-0.04354415386753951 * winDiff) + -3.166924740191411;
     } atMost 100 atLeast 0
   }
 
