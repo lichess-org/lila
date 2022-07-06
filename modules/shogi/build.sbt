43c54d27c237a587ce466d26972469bc1f6d1bfd
WandererXII <43043181+WandererXII@users.noreply.github.com>
2022-07-06 03:48:43 +0200

    Update dependencies

--- modules/shogi/build.sbt
+++ modules/shogi/build.sbt
@@ -8,10 +8,10 @@ scalaVersion := "2.13.8"
 
 libraryDependencies ++= List(
   "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2",
-  "org.specs2"             %% "specs2-core"              % "4.13.2" % Test,
-  "org.specs2"             %% "specs2-cats"              % "4.13.2" % Test,
+  "org.specs2"             %% "specs2-core"              % "4.15.0" % Test,
+  "org.specs2"             %% "specs2-cats"              % "4.15.0" % Test,
   "com.github.ornicar"     %% "scalalib"                 % "7.0.2",
-  "joda-time"              % "joda-time"                 % "2.10.13",
+  "joda-time"              % "joda-time"                 % "2.10.14",
   "org.typelevel"          %% "cats-core"                % "2.7.0"
 )
 
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,18 +14,18 @@ object Dependencies {
 
   val scalalib    = "com.github.ornicar"   %% "scalalib"                        % "7.0.2"
   val hasher      = "com.roundeights"      %% "hasher"                          % "1.2.1"
-  val jodaTime    = "joda-time"             % "joda-time"                       % "2.10.13"
+  val jodaTime    = "joda-time"             % "joda-time"                       % "2.10.14"
   val maxmind     = "com.sanoma.cda"       %% "maxmind-geoip2-scala"            % "1.3.1-THIB"
   val prismic     = "io.prismic"           %% "scala-kit"                       % "1.2.18-THIB213"
   val scrimage    = "com.sksamuel.scrimage" % "scrimage-core"                   % "4.0.22"
-  val scaffeine   = "com.github.blemale"   %% "scaffeine"                       % "5.1.2" % "compile"
-  val googleOAuth = "com.google.auth"       % "google-auth-library-oauth2-http" % "1.4.0"
-  val scalaUri    = "io.lemonlabs"         %% "scala-uri"                       % "3.6.0"
+  val scaffeine   = "com.github.blemale"   %% "scaffeine"                       % "5.2.0" % "compile"
+  val googleOAuth = "com.google.auth"       % "google-auth-library-oauth2-http" % "1.7.0"
+  val scalaUri    = "io.lemonlabs"         %% "scala-uri"                       % "4.0.2"
   val scalatags   = "com.lihaoyi"          %% "scalatags"                       % "0.11.1"
-  val lettuce     = "io.lettuce"            % "lettuce-core"                    % "6.1.6.RELEASE"
-  val epoll       = "io.netty"              % "netty-transport-native-epoll"    % "4.1.65.Final" classifier "linux-x86_64"
+  val lettuce     = "io.lettuce"            % "lettuce-core"                    % "6.1.8.RELEASE"
+  val epoll       = "io.netty"              % "netty-transport-native-epoll"    % "4.1.75.Final" classifier "linux-x86_64"
   val autoconfig  = "io.methvin.play"      %% "autoconfig-macros"               % "0.3.2" % "provided"
-  val scalatest   = "org.scalatest"        %% "scalatest"                       % "3.1.0" % Test
+  val scalatest   = "org.scalatest"        %% "scalatest"                       % "3.2.11" % Test
   val uaparser    = "org.uaparser"         %% "uap-scala"                       % "0.13.0"
 
   object flexmark {
@@ -38,7 +38,7 @@ object Dependencies {
   }
 
   object macwire {
-    val version = "2.5.3"
+    val version = "2.5.6"
     val macros = "com.softwaremill.macwire" %% "macros" % version % "provided"
     val util   = "com.softwaremill.macwire" %% "util"   % version % "provided"
   }
@@ -53,7 +53,7 @@ object Dependencies {
   }
 
   object play {
-    val version = "2.8.8"
+    val version = "2.8.16"
     val api     = "com.typesafe.play" %% "play"      % version
     val json    = "com.typesafe.play" %% "play-json" % "2.9.2"
   }
@@ -65,7 +65,7 @@ object Dependencies {
     val prometheus = "io.kamon" %% "kamon-prometheus"     % version
   }
   object akka {
-    val version    = "2.6.14"
+    val version    = "2.6.19"
     val akka       = "com.typesafe.akka" %% "akka-actor"       % version
     val akkaTyped  = "com.typesafe.akka" %% "akka-actor-typed" % version
     val akkaStream = "com.typesafe.akka" %% "akka-stream"      % version
