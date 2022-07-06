ff7c85070c41ce00680e7dff2d4b84d2a015b33b
Thibault Duplessis <t@lichess.org>
2022-07-06 11:21:17 +0200

    disable kaladin by default

--- modules/irwin/src/main/Env.scala
+++ modules/irwin/src/main/Env.scala
@@ -1,15 +1,16 @@
 package lila.irwin
 
-import akka.actor._
 import com.softwaremill.macwire._
 import com.softwaremill.tagging._
+import play.api.Configuration
 import scala.concurrent.duration._
 
 import lila.common.config._
-import lila.tournament.TournamentApi
 import lila.report.Suspect
+import lila.tournament.TournamentApi
 
 final class Env(
+    appConfig: Configuration,
     tournamentApi: TournamentApi,
     modApi: lila.mod.ModApi,
     reportApi: lila.report.ReportApi,
@@ -25,7 +26,7 @@ final class Env(
     insightDb: lila.db.AsyncDb @@ lila.insight.InsightDb
 )(implicit
     ec: scala.concurrent.ExecutionContext,
-    system: ActorSystem
+    system: akka.actor.ActorSystem
 ) {
 
   private lazy val irwinReportColl = db(CollName("irwin_report")).taggedWith[IrwinColl]
@@ -38,35 +39,38 @@ final class Env(
 
   val kaladinApi = wire[KaladinApi]
 
-  system.scheduler.scheduleWithFixedDelay(5 minutes, 5 minutes) { () =>
-    (for {
-      leaders <- tournamentApi.allCurrentLeadersInStandard
-      suspects <- lila.common.Future
-        .linear(leaders.toList) { case (tour, top) =>
-          userRepo byIds top.value.zipWithIndex
-            .filter(_._2 <= tour.nbPlayers * 2 / 100)
-            .map(_._1.userId)
-            .take(20)
-        }
-        .map(_.flatten.map(Suspect))
-      _ <- irwinApi.requests.fromTournamentLeaders(suspects)
-      _ <- kaladinApi.tournamentLeaders(suspects)
-    } yield ()).unit
-  }
-  system.scheduler.scheduleWithFixedDelay(15 minutes, 15 minutes) { () =>
-    (for {
-      topOnline <- userCache.getTop50Online.map(_ map Suspect)
-      _         <- irwinApi.requests.topOnline(topOnline)
-      _         <- kaladinApi.topOnline(topOnline)
-    } yield ()).unit
-  }
+  if (appConfig.get[Boolean]("kaladin.enabled")) {
 
-  system.scheduler.scheduleWithFixedDelay(83 seconds, 5 seconds) { () =>
-    kaladinApi.readResponses.unit
-  }
+    system.scheduler.scheduleWithFixedDelay(5 minutes, 5 minutes) { () =>
+      (for {
+        leaders <- tournamentApi.allCurrentLeadersInStandard
+        suspects <- lila.common.Future
+          .linear(leaders.toList) { case (tour, top) =>
+            userRepo byIds top.value.zipWithIndex
+              .filter(_._2 <= tour.nbPlayers * 2 / 100)
+              .map(_._1.userId)
+              .take(20)
+          }
+          .map(_.flatten.map(Suspect))
+        _ <- irwinApi.requests.fromTournamentLeaders(suspects)
+        _ <- kaladinApi.tournamentLeaders(suspects)
+      } yield ()).unit
+    }
+    system.scheduler.scheduleWithFixedDelay(15 minutes, 15 minutes) { () =>
+      (for {
+        topOnline <- userCache.getTop50Online.map(_ map Suspect)
+        _         <- irwinApi.requests.topOnline(topOnline)
+        _         <- kaladinApi.topOnline(topOnline)
+      } yield ()).unit
+    }
 
-  system.scheduler.scheduleWithFixedDelay(1 minute, 1 minute) { () =>
-    kaladinApi.monitorQueued.unit
+    system.scheduler.scheduleWithFixedDelay(83 seconds, 5 seconds) { () =>
+      kaladinApi.readResponses.unit
+    }
+
+    system.scheduler.scheduleWithFixedDelay(1 minute, 1 minute) { () =>
+      kaladinApi.monitorQueued.unit
+    }
   }
 }
 
