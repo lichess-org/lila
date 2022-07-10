274b9dfacebc452038446224ad8b168dab1a5d8b
Thibault Duplessis <t@lichess.org>
2022-07-10 23:50:09 +0200

    try a same thread executor for the expire callback memo caffeine - for #11166

--- modules/common/src/main/LilaCache.scala
+++ modules/common/src/main/LilaCache.scala
@@ -1,10 +1,18 @@
 package lila.common
 
-import play.api.Mode
 import com.github.benmanes.caffeine.cache.{ Caffeine, Scheduler }
 import com.github.blemale.scaffeine.Scaffeine
+import java.util.concurrent.Executor
+import play.api.Mode
 
 object LilaCache {
 
   def scaffeine: Scaffeine[Any, Any] = Scaffeine().scheduler(Scheduler.systemScheduler)
+
+  private val singleThreadExecutor = new Executor {
+    def execute(r: Runnable): Unit = r.run()
+  }
+
+  def scaffeineSameThreadExecutor: Scaffeine[Any, Any] =
+    Scaffeine().executor(singleThreadExecutor).scheduler(Scheduler.systemScheduler)
 }
