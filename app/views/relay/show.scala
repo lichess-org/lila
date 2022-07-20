fbbccf64c31fdc7a55e5e5dc91f0bd1109e97dcc
WandererXII <43043181+WandererXII@users.noreply.github.com>
2022-07-20 02:58:11 +0200

    Drop twitch in study and relay

--- app/views/relay/show.scala
+++ app/views/relay/show.scala
@@ -56,7 +56,7 @@ object show {
       ),
       shogiground = false,
       zoomable = true,
-      csp = defaultCsp.withWebAssembly.withTwitch.some,
+      csp = defaultCsp.withWebAssembly.some,
       openGraph = lila.app.ui
         .OpenGraph(
           title = r.name,
