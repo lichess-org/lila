5865bc8fbc5c83081a2c6ff63bdcfb86c82323c4
WandererXII <43043181+WandererXII@users.noreply.github.com>
2022-07-20 03:00:10 +0200

    Go back to our old ceval chances

--- ui/ceval/src/winningChances.ts
+++ ui/ceval/src/winningChances.ts
@@ -2,18 +2,17 @@ import { Eval } from './types';
 
 const toPov = (color: Color, diff: number): number => (color === 'sente' ? diff : -diff);
 
-/**
- * https://github.com/lichess-org/lila/pull/11148
- */
+// https://graphsketch.com/?eqn1_color=1&eqn1_eqn=100+*+%282+%2F+%281+%2B+exp%28-0.005+*+x%29%29+-+1%29&eqn2_color=2&eqn2_eqn=100+*+%282+%2F+%281+%2B+exp%28-0.004+*+x%29%29+-+1%29&eqn3_color=3&eqn3_eqn=&eqn4_color=4&eqn4_eqn=&eqn5_color=5&eqn5_eqn=&eqn6_color=6&eqn6_eqn=&x_min=-1000&x_max=1000&y_min=-100&y_max=100&x_tick=100&y_tick=10&x_label_freq=2&y_label_freq=2&do_grid=0&do_grid=1&bold_labeled_lines=0&bold_labeled_lines=1&line_width=4&image_w=850&image_h=525
 const rawWinningChances = (cp: number): number => {
-  const MULTIPLIER = -0.00368208; // https://github.com/lichess-org/lila/pull/11148
+  const MULTIPLIER = -0.0007;
   return 2 / (1 + Math.exp(MULTIPLIER * cp)) - 1;
 };
 
-const cpWinningChances = (cp: number): number => rawWinningChances(Math.min(Math.max(-1000, cp), 1000));
+const cpWinningChances = (cp: number): number => rawWinningChances(Math.min(Math.max(-5500, cp), 5500));
 
 const mateWinningChances = (mate: number): number => {
-  const cp = (21 - Math.min(10, Math.abs(mate))) * 100;
+  // note: the minimum value of cp here (i.e. when mate >= 20) must be above the range in cpWinningChances (5500)
+  const cp = (80 - Math.min(20, Math.abs(mate))) * 100;
   const signed = cp * (mate > 0 ? 1 : -1);
   return rawWinningChances(signed);
 };
