b3f1c9d5086508b1880735cf7530b9fb005f5cb5
WandererXII <43043181+WandererXII@users.noreply.github.com>
2022-07-23 23:42:42 +0200

    Fix keyboard control in puzzles

--- ui/puzzle/src/keyboard.ts
+++ ui/puzzle/src/keyboard.ts
@@ -1,30 +1,62 @@
 import * as control from './control';
 import { KeyboardController } from './interfaces';
 
+const preventing = (f: () => void) => (e: MouseEvent) => {
+  e.preventDefault();
+  f();
+};
+
 export default function (ctrl: KeyboardController): void {
   window.Mousetrap.bind(['left', 'k'], () => {
     control.prev(ctrl);
     ctrl.redraw();
   })
-    .bind(['right', 'j'], () => {
-      control.next(ctrl);
-      ctrl.redraw();
-    })
-    .bind(['up', '0'], () => {
-      control.first(ctrl);
-      ctrl.redraw();
-    })
-    .bind(['down', '$'], () => {
-      control.last(ctrl);
-      ctrl.redraw();
-    })
-    .bind('l', ctrl.toggleCeval)
-    .bind('x', ctrl.toggleThreatMode)
-    .bind('space', () => {
-      if (ctrl.vm.mode === 'view') {
-        if (ctrl.getCeval().enabled()) ctrl.playBestMove();
-        else ctrl.toggleCeval();
-      }
-    })
-    .bind('z', () => window.lishogi.pubsub.emit('zen'));
+    .bind(
+      ['right', 'j'],
+      preventing(() => {
+        control.next(ctrl);
+        ctrl.redraw();
+      })
+    )
+    .bind(
+      ['up', '0'],
+      preventing(() => {
+        control.first(ctrl);
+        ctrl.redraw();
+      })
+    )
+    .bind(
+      ['down', '$'],
+      preventing(() => {
+        control.last(ctrl);
+        ctrl.redraw();
+      })
+    )
+    .bind(
+      'l',
+      preventing(() => {
+        ctrl.toggleCeval;
+      })
+    )
+    .bind(
+      'x',
+      preventing(() => {
+        ctrl.toggleThreatMode;
+      })
+    )
+    .bind(
+      'space',
+      preventing(() => {
+        if (ctrl.vm.mode === 'view') {
+          if (ctrl.getCeval().enabled()) ctrl.playBestMove();
+          else ctrl.toggleCeval();
+        }
+      })
+    )
+    .bind(
+      'z',
+      preventing(() => {
+        window.lishogi.pubsub.emit('zen');
+      })
+    );
 }
