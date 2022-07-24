937db773e75d24694783e0ce51f0709b811f5eb9
Albert Ford <albert@albertford.com>
2022-07-24 00:12:32 -0700

    fix regression where practice was invisible in /analysis

--- ui/analyse/src/study/studyDeps.ts
+++ ui/analyse/src/study/studyDeps.ts
@@ -1,10 +1,9 @@
 import relayManager from './relay/relayManagerView';
 import relayTour from './relay/relayTourView';
 import renderPlayerBars from './playerBars';
-import practiceView from '../practice/practiceView';
 import makeStudy from './studyCtrl';
 
-export { relayManager, relayTour, renderPlayerBars, practiceView, makeStudy };
+export { relayManager, relayTour, renderPlayerBars, makeStudy };
 
 export * as gbEdit from './gamebook/gamebookEdit';
 export * as gbPlay from './gamebook/gamebookPlayView';
