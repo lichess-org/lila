89a14ce96772b751e55a2d2c2bbccc8942b100eb
Ben Rollin <ben.rollin@clever.com>
2022-07-27 02:04:23 -0700

    throttle -> throttlePromiseDelay where appropriate

--- ui/simul/src/xhr.ts
+++ ui/simul/src/xhr.ts
@@ -1,4 +1,4 @@
-import throttle from 'common/throttle';
+import { throttlePromiseDelay } from 'common/throttle';
 import { json } from 'common/xhr';
 
 // when the simul no longer exists
@@ -10,7 +10,10 @@ export default {
   ping: post('host-ping'),
   start: post('start'),
   abort: post('abort'),
-  join: throttle(4000, (id: string, variant: VariantKey) => post(`join/${variant}`)(id)),
+  join: throttlePromiseDelay(
+    () => 4000,
+    (id: string, variant: VariantKey) => post(`join/${variant}`)(id)
+  ),
   withdraw: post('withdraw'),
   accept: (user: string) => post(`accept/${user}`),
   reject: (user: string) => post(`reject/${user}`),
