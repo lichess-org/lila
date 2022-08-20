c6000600ae60c6e11221657f942e089c87e90db3
Allan Joseph <allanjoseph98@gmail.com>
2022-08-20 16:31:20 +0000

    create separate headings for socials and teams

--- app/views/user/show/header.scala
+++ app/views/user/show/header.scala
@@ -195,11 +195,15 @@ object header {
                     )
                   },
                   !hideTroll option div(cls := "social_links col2")(
+                    if (!profile.actualLinks.isEmpty)
+                      strong(trans.socialMediaLinks()),
                     profile.actualLinks.map { link =>
                       a(href := link.url, targetBlank, noFollow)(link.site.name)
                     }
                   ),
                   div(cls := "teams col2")(
+                    if (!info.teamIds.isEmpty)
+                      strong(trans.team.teams()),
                     info.teamIds.sorted.map { t =>
                       teamLink(t, withIcon = false)
                     }
