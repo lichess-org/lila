6177556ac359ab645cd8320d92afedc75fed90e4
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-08-22 21:21:57 +0100

    Rewrite Emil Kemény as Kemény

--- modules/common/src/main/GreatPlayer.scala
+++ modules/common/src/main/GreatPlayer.scala
@@ -226,7 +226,7 @@ object GreatPlayer {
     ("Kasparyan", "Genrikh_Kasparyan"),
     ("Kavalek", "Lubomir_Kavalek"),
     ("Kazantsev", "Alexander_Kazantsev"),
-    ("Emil Kemény", "Emil_Kem%C3%A9ny"),
+    ("Kemény", "Emil_Kem%C3%A9ny"),
     ("Keres", "Paul_Keres"),
     ("Khalifman", "Alexander_Khalifman"),
     ("Kholmov", "Ratmir_Kholmov"),
