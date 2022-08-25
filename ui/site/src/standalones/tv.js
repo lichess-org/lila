663fd962ae2f96b4afb3a278b583fa213dce2c31
WandererXII <43043181+WandererXII@users.noreply.github.com>
2022-08-26 01:51:52 +0200

    Fix tv embed

--- ui/site/src/standalones/tv.js
+++ ui/site/src/standalones/tv.js
@@ -33,7 +33,7 @@ function resize() {
 $(function () {
   var $featured = $('#featured-game');
   var board = function () {
-    return $featured.find('.mini-board');
+    return $featured.find('a.mini-board');
   };
   parseSfen(board());
   if (!window.EventSource) return;
