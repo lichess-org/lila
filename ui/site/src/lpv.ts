727ccb9652fc337d9a470de7679cba65b1500d92
Jonathan Gamble <gamblej@gmail.com>
2022-08-28 11:43:40 -0500

    shortcut lpv viewport targeting with mouse events
    add slight mouseenter border, which coincidentally does a nice job of conveying when lpv will be stealing scroll wheel events.

--- ui/site/src/lpv.ts
+++ ui/site/src/lpv.ts
@@ -5,9 +5,21 @@ import { text as xhrText } from 'common/xhr';
 
 export function autostart() {
   const lpvs = new Map<Element, Element>();
+  var mouseTarget: HTMLElement | undefined;
 
   $('.lpv--autostart').each(function (this: HTMLElement) {
-    lpvs.set(this.parentElement!, this);
+    const parent = this.parentElement;
+    if (parent) {
+      lpvs.set(parent, this);
+      parent.onmouseenter = _ => {
+        mouseTarget = parent;
+        mouseTarget.setAttribute('class', 'lpv-mouseover');
+      };
+      parent.onmouseleave = _ => {
+        if (mouseTarget) mouseTarget.removeAttribute('class');
+        mouseTarget = undefined; // no need for fanciness, lpvs don't overlap
+      };
+    }
     Lpv(this, {
       pgn: this.dataset['pgn']!.replace(/<br>/g, '\n'),
       orientation: this.dataset['orientation'] as Color | undefined,
@@ -18,17 +30,19 @@ export function autostart() {
   if (lpvs.size == 0) return;
 
   function bindKeyGoto(key: string, goto: string): void {
-    window.Mousetrap.bind(key, () => keyboardTarget()?.dispatchEvent(new Event(goto)));
+    window.Mousetrap.bind(key, () => eventTarget()?.dispatchEvent(new Event(goto)));
   }
 
-  function keyboardTarget(): Element | undefined {
-    let maxVisibleY = 0,
-      targetLpv;
+  function eventTarget(): Element | undefined {
+    let targetLpv = mouseTarget ? lpvs.get(mouseTarget) : undefined;
+    if (targetLpv) return targetLpv;
+
+    let maxVisibleY = 0;
     lpvs.forEach((lpv, div, __) => {
       const r = div.getBoundingClientRect();
-      const intersectionY = Math.min(window.innerHeight, r.bottom) - Math.max(0, r.top);
-      if (intersectionY > maxVisibleY) {
-        maxVisibleY = intersectionY;
+      const visibleY = Math.min(window.innerHeight, r.bottom) - Math.max(0, r.top); // negative values fine
+      if (visibleY > maxVisibleY) {
+        maxVisibleY = visibleY;
         targetLpv = lpv;
       }
     });
