e54dbefab2c2472374e6e0dfa856364337df6733
Thibault Duplessis <t@lichess.org>
2022-08-28 11:54:39 +0200

    alert about dangerous scopes during oauth token creation

--- modules/oauth/src/main/AccessToken.scala
+++ modules/oauth/src/main/AccessToken.scala
@@ -21,6 +21,8 @@ case class AccessToken(
   def isBrandNew = createdAt.exists(DateTime.now.minusSeconds(5).isBefore)
 
   def expiresOrFarFuture = expires | DateTime.now.plusYears(1)
+
+  def isDangerous = scopes.exists(OAuthScope.dangerList.contains)
 }
 
 object AccessToken {
