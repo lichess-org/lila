e823f11310d7916bc0a8051439059b25897edb74
Benedikt Werner <1benediktwerner@gmail.com>
2022-09-19 03:56:35 +0200

    Remove unused boardApiSeek form
    Was already unused when added in baaad5268f2e064b55ea34c4072dd57cd2f5700a

--- modules/setup/src/main/SetupForm.scala
+++ modules/setup/src/main/SetupForm.scala
@@ -104,33 +104,6 @@ object SetupForm {
       )
   )
 
-  lazy val boardApiSeek = Form(
-    mapping(
-      "time"        -> time,
-      "increment"   -> increment,
-      "variant"     -> optional(boardApiVariantKeys),
-      "rated"       -> optional(boolean),
-      "color"       -> optional(color),
-      "ratingRange" -> optional(ratingRange)
-    )((t, i, v, r, c, g) =>
-      HookConfig(
-        variant = v.flatMap(Variant.apply) | Variant.default,
-        timeMode = TimeMode.RealTime,
-        time = t,
-        increment = i,
-        days = 1,
-        mode = chess.Mode(~r),
-        color = lila.lobby.Color.orDefault(c),
-        ratingRange = g.fold(RatingRange.default)(RatingRange.orDefault)
-      )
-    )(_ => none)
-      .verifying("Invalid clock", _.validClock)
-      .verifying(
-        "Invalid time control",
-        hook => hook.makeClock ?? lila.game.Game.isBoardCompatible
-      )
-  )
-
   object api {
 
     lazy val clockMapping =
