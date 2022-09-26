0e06d64bc49027022ec3c8da5b27e8150a5751df
Thibault Duplessis <t@lichess.org>
2022-09-26 08:00:07 +0200

    upgrade deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val galimatias  = "io.mola.galimatias"            % "galimatias"                      % "0.2.2-NF"
   val scalatags   = "com.lihaoyi"                  %% "scalatags"                       % "0.11.1"
   val lettuce     = "io.lettuce"                    % "lettuce-core"                    % "6.2.0.RELEASE"
-  val epoll = "io.netty" % "netty-transport-native-epoll" % "4.1.79.Final" classifier "linux-x86_64"
+  val epoll = "io.netty" % "netty-transport-native-epoll" % "4.1.82.Final" classifier "linux-x86_64"
   val autoconfig   = "io.methvin.play"            %% "autoconfig-macros" % "0.3.2"  % "provided"
   val scalatest    = "org.scalatest"              %% "scalatest"         % "3.2.11" % Test
   val uaparser     = "org.uaparser"               %% "uap-scala"         % "0.13.0"
@@ -80,7 +80,7 @@ object Dependencies {
     val prometheus = "io.kamon" %% "kamon-prometheus"     % version
   }
   object akka {
-    val version    = "2.6.19"
+    val version    = "2.6.20"
     val akka       = "com.typesafe.akka" %% "akka-actor"       % version
     val akkaTyped  = "com.typesafe.akka" %% "akka-actor-typed" % version
     val akkaStream = "com.typesafe.akka" %% "akka-stream"      % version
