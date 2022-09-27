89facfe036343563001cbbeb1cda82675fb8cdbd
Thibault Duplessis <t@lichess.org>
2022-09-27 20:30:57 +0200

    implement variation name

--- modules/opening/src/main/Opening.scala
+++ modules/opening/src/main/Opening.scala
@@ -23,14 +23,26 @@ object Opening {
    * "Mieses Opening" -> "Mieses Opening: Reversed Rat" -> "Reversed Rat"
    * For harder ones, see modules/opening/src/test/OpeningTest.scala
    */
-  def variationName(prev: String, next: String): Option[String] = {
-    val name =
-      prev
-        .zipAll(next, ' ', ' ')
-        .dropWhile { case (a, b) => a == b }
-        .map(_._2)
-        .mkString
-        .dropWhile(Set(' ', ':', ',', '-').contains _)
-    name.nonEmpty option name
-  }
+  def variationName(prev: String, next: String): String =
+    sectionsOf(prev)
+      .zipAll(sectionsOf(next), "", "")
+      .dropWhile { case (a, b) => a == b }
+      .headOption
+      .map(_._2)
+      .filter(_.nonEmpty)
+      .orElse(sectionsOf(next).lastOption)
+      .getOrElse(next.takeWhile(':' !=))
+
+  def variationName(prev: Option[FullOpening], next: Option[FullOpening]): Option[String] =
+    (prev, next) match {
+      case (Some(p), Some(n)) => variationName(p.name, n.name).some
+      case (None, Some(n))    => n.family.name.some
+      case _                  => none
+    }
+
+  private def sectionsOf(openingName: String): List[String] =
+    openingName.split(":", 2) match {
+      case Array(f, v) => f :: v.split(",").toList.map(_.trim)
+      case _           => openingName :: Nil
+    }
 }
