ee8f263c03ec788853fe27a03620787879e3ce91
Thibault Duplessis <t@lichess.org>
2022-10-01 09:32:57 +0200

    fix opening page name

--- app/views/opening/show.scala
+++ app/views/opening/show.scala
@@ -17,7 +17,7 @@ object show {
     views.html.base.layout(
       moreCss = cssTag("opening"),
       moreJs = moreJs(page),
-      title = s"${trans.opening.txt()} • $name",
+      title = s"${trans.opening.txt()} • ${page.name}",
       openGraph = lila.app.ui
         .OpenGraph(
           `type` = "article",
