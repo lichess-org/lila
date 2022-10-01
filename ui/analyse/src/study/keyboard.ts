16af1a1ca85128e7a052e01b20c07c43971c1ab2
Erdogan Seref <erdogan872@gmail.com>
2022-10-01 16:42:01 +0000

    Add remaining shortcuts
    Use numerals as keyboard shortcut

--- ui/analyse/src/study/keyboard.ts
+++ ui/analyse/src/study/keyboard.ts
@@ -1,12 +1,31 @@
-import { GlyphCtrl } from "./studyGlyph";
+import { GlyphCtrl } from './studyGlyph';
 
 export const bind = (ctrl: GlyphCtrl) => {
-    const kbd = window.Mousetrap;
-    if (!kbd) return;
-    kbd
-        .bind(['shift+g'], () => {
-            ctrl.toggleGlyph(1);
-            ctrl.redraw();
-        })
-        ;
-}
+  const kbd = window.Mousetrap;
+  if (!kbd) return;
+  kbd
+    .bind(['1'], () => {
+      ctrl.toggleGlyph(1);
+      ctrl.redraw();
+    })
+    .bind(['2'], () => {
+      ctrl.toggleGlyph(2);
+      ctrl.redraw();
+    })
+    .bind(['3'], () => {
+      ctrl.toggleGlyph(3);
+      ctrl.redraw();
+    })
+    .bind(['4'], () => {
+      ctrl.toggleGlyph(4);
+      ctrl.redraw();
+    })
+    .bind(['5'], () => {
+      ctrl.toggleGlyph(5);
+      ctrl.redraw();
+    })
+    .bind(['6'], () => {
+      ctrl.toggleGlyph(6);
+      ctrl.redraw();
+    });
+};
