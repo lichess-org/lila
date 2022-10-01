17deaa6c330b2467f69401efbf8315cc8c8de70a
Erdogan Seref <erdogan872@gmail.com>
2022-10-01 17:20:17 +0000

    code golf

--- ui/analyse/src/study/keyboard.ts
+++ ui/analyse/src/study/keyboard.ts
@@ -3,29 +3,10 @@ import { GlyphCtrl } from './studyGlyph';
 export const bind = (ctrl: GlyphCtrl) => {
   const kbd = window.Mousetrap;
   if (!kbd) return;
-  kbd
-    .bind(['1'], () => {
-      ctrl.toggleGlyph(1);
-      ctrl.redraw();
-    })
-    .bind(['2'], () => {
-      ctrl.toggleGlyph(2);
-      ctrl.redraw();
-    })
-    .bind(['3'], () => {
-      ctrl.toggleGlyph(3);
-      ctrl.redraw();
-    })
-    .bind(['4'], () => {
-      ctrl.toggleGlyph(4);
-      ctrl.redraw();
-    })
-    .bind(['5'], () => {
-      ctrl.toggleGlyph(5);
-      ctrl.redraw();
-    })
-    .bind(['6'], () => {
-      ctrl.toggleGlyph(6);
+  for (let i = 0; i < 7; i++) {
+    kbd.bind([i.toString()], () => {
+      ctrl.toggleGlyph(i);
       ctrl.redraw();
     });
+  }
 };
