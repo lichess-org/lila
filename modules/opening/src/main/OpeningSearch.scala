4a49f1b9daa689e2e69bded73c86bcc62b64e5f9
Thibault Duplessis <t@lichess.org>
2022-10-02 12:30:55 +0200

    improve opening search tokenizer

--- modules/opening/src/main/OpeningSearch.scala
+++ modules/opening/src/main/OpeningSearch.scala
@@ -27,23 +27,26 @@ object OpeningSearch {
   private type Freq     = Int
   private type NameSize = Int
 
-  private val tokenR              = """[^\w-]""".r
-  private val tokenMultiDashRegex = """-{2,}""".r
-  private val excludeTokens       = Set("opening", "variation")
-
-  private def tokenize(str: String): List[Token] = {
-    val singleDashes = tokenMultiDashRegex.replaceAllIn(str.trim.replace(' ', '-'), "-")
-    val normalized   = Normalizer.normalize(singleDashes, Normalizer.Form.NFD)
-    tokenR
-      .replaceAllIn(normalized, "")
-      .toLowerCase
-      .split('-')
-      .view
-      .filterNot(excludeTokens.contains)
-      .toList
+  private[opening] object tokenize {
+    private val nonLetterRegex = """[^a-zA-Z]+""".r
+    private val exclude        = Set("opening", "variation")
+    def apply(str: String): List[Token] = {
+      str
+        .split(' ')
+        .map(_.trim)
+        .filter(_.nonEmpty)
+        .toList
+        .map { token =>
+          Normalizer
+            .normalize(token, Normalizer.Form.NFD)
+            .toLowerCase
+            .replaceAllIn(nonLetterRegex, "")
+        }
+        .filterNot(exclude.contains)
+    }
+    def apply(opening: FullOpening): List[Token] =
+      opening.key.toLowerCase.split('_').view.filterNot(exclude.contains).toList
   }
-  private def tokenize(opening: FullOpening): List[Token] =
-    opening.key.toLowerCase.split('_').view.filterNot(excludeTokens.contains).toList
 
   private val index: Map[Token, Set[Position]] =
     openings.zipWithIndex.foldLeft(Map.empty[Token, Set[Position]]) { case (index, (opening, position)) =>
