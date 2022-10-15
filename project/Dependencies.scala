360e7c2dd35f2106b8a6f63ddf06dbff6dcb198d
Scala Steward <scala_steward@virtuslab.com>
2022-10-15 03:40:37 +0000

    Update lettuce-core to 6.2.1.RELEASE

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val googleOAuth = "com.google.auth"               % "google-auth-library-oauth2-http" % "1.11.0"
   val galimatias  = "io.mola.galimatias"            % "galimatias"                      % "0.2.2-NF"
   val scalatags   = "com.lihaoyi"                  %% "scalatags"                       % "0.11.1"
-  val lettuce     = "io.lettuce"                    % "lettuce-core"                    % "6.2.0.RELEASE"
+  val lettuce     = "io.lettuce"                    % "lettuce-core"                    % "6.2.1.RELEASE"
   val epoll = "io.netty" % "netty-transport-native-epoll" % "4.1.82.Final" classifier "linux-x86_64"
   val autoconfig   = "io.methvin.play"            %% "autoconfig-macros" % "0.3.2"  % "provided"
   val scalatest    = "org.scalatest"              %% "scalatest"         % "3.2.11" % Test
