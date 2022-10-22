4f91bedb381b32e830a2afabf8f3dfbfce4351b4
Benedikt Werner <1benediktwerner@gmail.com>
2022-10-22 09:48:31 +0200

    Fix type

--- app/controllers/Mod.scala
+++ app/controllers/Mod.scala
@@ -251,7 +251,7 @@ final class Mod(
   def createNameCloseVote(username: String) = SendToZulip(username, env.irc.api.nameCloseVote)
   def askUsertableCheck(username: String)   = SendToZulip(username, env.irc.api.usertableCheck)
 
-  private def SendToZulip(username: String, method: (User, Holder) => Funit) =
+  private def SendToZulip(username: String, method: (UserModel, Holder) => Funit) =
     Secure(_.SendToZulip) { _ => me =>
       env.user.repo named username flatMap {
         _ ?? { user => method(user, me) inject NoContent }
