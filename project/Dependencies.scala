20d1b471766072e066823b4ce34eee0048e2a0f2
Thibault Duplessis <t@lichess.org>
2022-11-08 17:50:09 +0100

    use hacked play-ws to avoid pulling akka_2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -75,7 +75,7 @@ object Dependencies {
   }
 
   object playWs {
-    val version = "2.2.0-M2"
+    val version = "2.2.0-M2_lila-1"
     val ahc     = "com.typesafe.play" %% "play-ahc-ws-standalone"  % version
     val json    = "com.typesafe.play" %% "play-ws-standalone-json" % version
     val bundle  = Seq(ahc, json)
