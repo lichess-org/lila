2016c5b886e95adc64847e0b95b380cb2c1c7f04
Thibault Duplessis <t@lichess.org>
2022-11-10 15:12:42 +0100

    move all extensions to the same file to avoid function name collision

--- modules/common/src/main/base/LilaLibraryExtensions.scala
+++ modules/common/src/main/base/LilaLibraryExtensions.scala
@@ -7,12 +7,15 @@ import java.util.concurrent.TimeUnit
 import java.lang.Math.{ max, min }
 import org.joda.time.{ DateTime, Duration }
 import scala.concurrent.duration.*
-import scala.concurrent.Future
+import scala.concurrent.{ Await, ExecutionContext as EC, Future }
 import scala.util.matching.Regex
 import scala.util.Try
 
 import cats.data.NonEmptyList
 import java.util.Base64
+import akka.actor.Scheduler
+import lila.common.Chronometer
+import scala.collection.BuildFrom
 
 trait LilaLibraryExtensions extends LilaTypes:
 
@@ -139,3 +142,205 @@ trait LilaLibraryExtensions extends LilaTypes:
   extension [A](seq: Seq[A]) def has(a: A) = seq contains a
 
   extension (self: Array[Byte]) def toBase64 = Base64.getEncoder.encodeToString(self)
+
+  extension [A](fua: Fu[A])
+
+    inline def dmap[B](f: A => B): Fu[B]       = fua.map(f)(EC.parasitic)
+    inline def dforeach[B](f: A => Unit): Unit = fua.foreach(f)(EC.parasitic)
+
+    def >>-(sideEffect: => Unit)(using ec: EC): Fu[A] =
+      fua andThen { case _ =>
+        sideEffect
+      }
+
+    def >>[B](fub: => Fu[B])(using ec: EC): Fu[B] =
+      fua flatMap { _ =>
+        fub
+      }
+
+    inline def void: Fu[Unit] =
+      dmap { _ =>
+        ()
+      }
+
+    inline def inject[B](b: => B): Fu[B] =
+      dmap { _ =>
+        b
+      }
+
+    def injectAnyway[B](b: => B)(using ec: EC): Fu[B] = fold(_ => b, _ => b)
+
+    def effectFold(fail: Exception => Unit, succ: A => Unit)(using ec: EC): Unit =
+      fua onComplete {
+        case scala.util.Failure(e: Exception) => fail(e)
+        case scala.util.Failure(e)            => throw e // Throwables
+        case scala.util.Success(e)            => succ(e)
+      }
+
+    def fold[B](fail: Exception => B, succ: A => B)(using ec: EC): Fu[B] =
+      fua map succ recover { case e: Exception => fail(e) }
+
+    def flatFold[B](fail: Exception => Fu[B], succ: A => Fu[B])(using ec: EC): Fu[B] =
+      fua flatMap succ recoverWith { case e: Exception => fail(e) }
+
+    def logFailure(logger: => lila.log.Logger, msg: Throwable => String)(using ec: EC): Fu[A] =
+      addFailureEffect { e =>
+        logger.warn(msg(e), e)
+      }
+    def logFailure(logger: => lila.log.Logger)(using ec: EC): Fu[A] = logFailure(logger, _.toString)
+
+    def addFailureEffect(effect: Throwable => Unit)(using ec: EC) =
+      fua.failed.foreach { (e: Throwable) =>
+        effect(e)
+      }
+      fua
+
+    def addEffect(effect: A => Unit)(using ec: EC): Fu[A] =
+      fua foreach effect
+      fua
+
+    def addEffects(fail: Exception => Unit, succ: A => Unit)(using ec: EC): Fu[A] =
+      fua onComplete {
+        case scala.util.Failure(e: Exception) => fail(e)
+        case scala.util.Failure(e)            => throw e // Throwables
+        case scala.util.Success(e)            => succ(e)
+      }
+      fua
+
+    def addEffects(f: Try[A] => Unit)(using ec: EC): Fu[A] =
+      fua onComplete f
+      fua
+
+    def addEffectAnyway(inAnyCase: => Unit)(using ec: EC): Fu[A] =
+      fua onComplete { _ =>
+        inAnyCase
+      }
+      fua
+
+    def mapFailure(f: Exception => Exception)(using ec: EC): Fu[A] =
+      fua recoverWith { case cause: Exception =>
+        fufail(f(cause))
+      }
+
+    def prefixFailure(p: => String)(using ec: EC): Fu[A] =
+      mapFailure { e =>
+        LilaException(s"$p ${e.getMessage}")
+      }
+
+    def thenPp(using ec: EC): Fu[A] =
+      effectFold(
+        e => println("[failure] " + e),
+        a => println("[success] " + a)
+      )
+      fua
+
+    def thenPp(msg: String)(using ec: EC): Fu[A] =
+      effectFold(
+        e => println(s"[$msg] [failure] $e"),
+        a => println(s"[$msg] [success] $a")
+      )
+      fua
+
+    def await(duration: FiniteDuration, name: String): A =
+      Chronometer.syncMon(_.blocking.time(name)) {
+        try Await.result(fua, duration)
+        catch
+          case e: Exception =>
+            lila.mon.blocking.timeout(name).increment()
+            throw e
+      }
+
+    def awaitOrElse(duration: FiniteDuration, name: String, default: => A): A =
+      try await(duration, name)
+      catch case _: Exception => default
+
+    def withTimeout(duration: FiniteDuration)(using ec: EC, scheduler: Scheduler): Fu[A] =
+      withTimeout(duration, LilaTimeout(s"Future timed out after $duration"))
+
+    def withTimeout(
+        duration: FiniteDuration,
+        error: => Throwable
+    )(using ec: EC, scheduler: Scheduler): Fu[A] =
+      Future firstCompletedOf Seq(
+        fua,
+        akka.pattern.after(duration, scheduler)(Future failed error)
+      )
+
+    def withTimeoutDefault(
+        duration: FiniteDuration,
+        default: => A
+    )(using ec: EC, scheduler: Scheduler): Fu[A] =
+      Future firstCompletedOf Seq(
+        fua,
+        akka.pattern.after(duration, scheduler)(Future(default))
+      )
+
+    def delay(duration: FiniteDuration)(using ec: EC, scheduler: Scheduler) =
+      lila.common.Future.delay(duration)(fua)
+
+    def chronometer    = Chronometer(fua)
+    def chronometerTry = Chronometer.lapTry(fua)
+
+    def mon(path: lila.mon.TimerPath): Fu[A]              = chronometer.mon(path).result
+    def monTry(path: Try[A] => lila.mon.TimerPath): Fu[A] = chronometerTry.mon(r => path(r)(lila.mon)).result
+    def monSuccess(path: lila.mon.type => Boolean => kamon.metric.Timer): Fu[A] =
+      chronometerTry.mon { r =>
+        path(lila.mon)(r.isSuccess)
+      }.result
+    def monValue(path: A => lila.mon.TimerPath): Fu[A] = chronometer.monValue(path).result
+
+    def logTime(name: String): Fu[A]                               = chronometer pp name
+    def logTimeIfGt(name: String, duration: FiniteDuration): Fu[A] = chronometer.ppIfGt(name, duration)
+
+    def recoverDefault(using z: Zero[A], ec: EC): Fu[A] = recoverDefault(z.zero)
+
+    def recoverDefault(default: => A)(using ec: EC): Fu[A] =
+      fua recover {
+        case _: LilaException                         => default
+        case _: java.util.concurrent.TimeoutException => default
+        case e: Exception =>
+          lila.log("common").warn("Future.recoverDefault", e)
+          default
+      }
+
+  extension (fua: Fu[Boolean])
+
+    def >>&(fub: => Fu[Boolean]): Fu[Boolean] =
+      fua.flatMap { if (_) fub else fuFalse }(EC.parasitic)
+
+    def >>|(fub: => Fu[Boolean]): Fu[Boolean] =
+      fua.flatMap { if (_) fuTrue else fub }(EC.parasitic)
+
+    inline def unary_! = fua.map { !_ }(EC.parasitic)
+
+  extension [A](fua: Fu[Option[A]])
+
+    def orFail(msg: => String)(using ec: EC): Fu[A] =
+      fua flatMap {
+        _.fold[Fu[A]](fufail(msg))(fuccess)
+      }
+
+    def orFailWith(err: => Exception)(using ec: EC): Fu[A] =
+      fua flatMap {
+        _.fold[Fu[A]](fufail(err))(fuccess)
+      }
+
+    def orElse(other: => Fu[Option[A]])(using ec: EC): Fu[Option[A]] =
+      fua flatMap {
+        _.fold(other) { x =>
+          fuccess(Some(x))
+        }
+      }
+
+    def getOrElse(other: => Fu[A])(using ec: EC): Fu[A] = fua flatMap { _.fold(other)(fuccess) }
+
+    def map2[B](f: A => B)(using ec: EC): Fu[Option[B]] = fua.map(_ map f)
+    def dmap2[B](f: A => B): Fu[Option[B]]              = fua.map(_ map f)(EC.parasitic)
+
+    def getIfPresent: Option[A] =
+      fua.value match
+        case Some(scala.util.Success(v)) => v
+        case _                           => None
+
+  extension [A, M[X] <: IterableOnce[X]](t: M[Fu[A]])
+    def sequenceFu(using bf: BuildFrom[M[Fu[A]], A, M[A]], ec: EC): Fu[M[A]] = Future.sequence(t)
