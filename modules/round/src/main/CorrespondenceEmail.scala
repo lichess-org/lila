e05dbe7300862d820ef477033064451ad848529c
Jonathan Gamble <101470903+schlawg@users.noreply.github.com>
2022-11-23 21:18:03 -0600

    Update CorrespondenceEmail.scala

--- modules/round/src/main/CorrespondenceEmail.scala
+++ modules/round/src/main/CorrespondenceEmail.scala
@@ -39,7 +39,7 @@ final private class CorrespondenceEmail(gameRepo: GameRepo, userRepo: UserRepo, 
         import framework.*
         // hit partial index
         List(
-          Match($doc("notification.correspondenceEmail" -> 1)),
+          Match($doc("notification.correspondenceEmail" -> true)),
           Project($id(true)),
           PipelineOperator(
             $lookup.pipeline(
