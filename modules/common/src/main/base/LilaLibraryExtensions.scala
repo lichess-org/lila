8b9223ea62adbb818b1a4edea1313592f0f22cc0
Thibault Duplessis <t@lichess.org>
2022-12-15 21:47:46 +0100

    generalize Fu[Boolean].ifThen to A: Zero

--- modules/common/src/main/base/LilaLibraryExtensions.scala
+++ modules/common/src/main/base/LilaLibraryExtensions.scala
@@ -314,8 +314,8 @@ trait LilaLibraryExtensions extends LilaTypes:
     def >>|(fub: => Fu[Boolean]): Fu[Boolean] =
       fua.flatMap { if (_) fuTrue else fub }(EC.parasitic)
 
-    def ifThen(fub: => Funit): Funit =
-      fua.flatMap { if (_) fub else funit }(EC.parasitic)
+    def ifThen[A](fub: => Fu[A])(using zero: Zero[A]): Fu[A] =
+      fua.flatMap { if (_) fub else fuccess(zero.zero) }(EC.parasitic)
 
     inline def unary_! = fua.map { !_ }(EC.parasitic)
 
