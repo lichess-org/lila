96969d78fd356cad783aebd2501b6665453298f9
WandererXII <43043181+WandererXII@users.noreply.github.com>
2022-12-22 18:24:07 +0100

    Tweak transientMove

--- ui/round/src/transientMove.ts
+++ ui/round/src/transientMove.ts
@@ -1,4 +1,3 @@
-// import RoundController from './ctrl';
 import { RoundSocket } from './socket';
 
 /* Tracks moves that were played on the board,
@@ -13,10 +12,12 @@ import { RoundSocket } from './socket';
 export default class TransientMove {
   constructor(readonly socket: RoundSocket) {}
 
+  defaultTimeout = 7500;
   current: number | undefined = undefined;
 
-  register = () => {
-    this.current = setTimeout(this.expire, 5000);
+  register = (remaining: number | undefined) => {
+    const toTrigger = remaining && remaining <= this.defaultTimeout + 1000 ? 3500 : this.defaultTimeout;
+    this.current = setTimeout(this.expire, toTrigger);
   };
 
   clear = () => {
