157663f05c21b1c56fa9fd4a980ec64645444231
Thibault Duplessis <t@lichess.org>
2022-12-23 08:51:37 +0100

    tweak notification migration script

--- bin/mongodb/pref-to-notify-pref.js
+++ bin/mongodb/pref-to-notify-pref.js
@@ -1,13 +1,13 @@
 db.pref.find({ $or: [{ mention: false }, { corresEmailNotif: true }] }).forEach(p => {
   db.notify_pref.insertOne({
     _id: p._id,
-    privateMessage: 7,
-    challenge: 7,
-    mention: p.mention ? 7 : 0,
-    streamStart: 7,
-    tournamentSoon: 6,
-    gameEvent: 6,
-    invitedStudy: 7,
+    privateMessage: NumberInt(7),
+    challenge: NumberInt(7),
+    mention: NumberInt(p.mention ? 7 : 0),
+    streamStart: NumberInt(7),
+    tournamentSoon: NumberInt(6),
+    gameEvent: NumberInt(6),
+    invitedStudy: NumberInt(7),
     correspondenceEmail: p.corresEmailNotif,
   });
 });
