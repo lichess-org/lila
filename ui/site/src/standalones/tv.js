e8ff706c4de83d559fdfb19485f2ab45ee204d61
WandererXII <43043181+WandererXII@users.noreply.github.com>
2022-12-24 19:00:45 +0100

    Chushogi in tv

--- ui/site/src/standalones/tv.js
+++ ui/site/src/standalones/tv.js
@@ -1,20 +1,124 @@
+// Copied from shogiops
+// todo - refactor and rewrite site in ts and get rid of this
+function chushogiForsythToRole(str) {
+  switch (str.toLowerCase()) {
+    case 'l':
+      return 'lance';
+    case '+l':
+      return 'whitehorse';
+    case 'f':
+      return 'leopard';
+    case '+f':
+      return 'bishoppromoted';
+    case 'c':
+      return 'copper';
+    case '+c':
+      return 'sidemoverpromoted';
+    case 's':
+      return 'silver';
+    case '+s':
+      return 'verticalmoverpromoted';
+    case 'g':
+      return 'gold';
+    case '+g':
+      return 'rookpromoted';
+    case 'k':
+      return 'king';
+    case 'e':
+      return 'elephant';
+    case '+e':
+      return 'prince';
+    case 'a':
+      return 'chariot';
+    case '+a':
+      return 'whale';
+    case 'b':
+      return 'bishop';
+    case '+b':
+      return 'horsepromoted';
+    case 't':
+      return 'tiger';
+    case '+t':
+      return 'stag';
+    case 'o':
+      return 'kirin';
+    case '+o':
+      return 'lionpromoted';
+    case 'x':
+      return 'phoenix';
+    case '+x':
+      return 'queenpromoted';
+    case 'm':
+      return 'sidemover';
+    case '+m':
+      return 'boar';
+    case 'v':
+      return 'verticalmover';
+    case '+v':
+      return 'ox';
+    case 'r':
+      return 'rook';
+    case '+r':
+      return 'dragonpromoted';
+    case 'h':
+      return 'horse';
+    case '+h':
+      return 'falcon';
+    case 'd':
+      return 'dragon';
+    case '+d':
+      return 'eagle';
+    case 'n':
+      return 'lion';
+    case 'q':
+      return 'queen';
+    case 'p':
+      return 'pawn';
+    case '+p':
+      return 'promotedpawn';
+    case 'i':
+      return 'gobetween';
+    case '+i':
+      return 'elephantpromoted';
+    default:
+      return;
+  }
+}
+
+function loadChushogiPieceSprite() {
+  if (!document.getElementById('chu-piece-sprite')) {
+    const cps = document.body.dataset.chuPieceSet || 'Chu_Ryoko_1Kanji';
+    $('head').append(
+      $('<link id="chu-piece-sprite" rel="stylesheet" type="text/css" />').attr(
+        'href',
+        `https://lishogi1.org/assets/piece-css/${cps}.css`
+      )
+    );
+  }
+}
+
 function parseSfen($elem) {
   $elem.each(function () {
-    var $this = $(this).removeClass('parse-sfen');
-    var lm = $this.data('lastmove');
-    var dropOrMove = lm ? (lm.includes('*') ? [lm.slice(2)] : [lm[0] + lm[1], lm[2] + lm[3]]) : undefined;
-    var sfen = $this.data('sfen');
-    var hands = sfen && sfen.split(' ').length > 2 ? sfen.split(' ')[2] : '';
-    var color = $this.data('color');
-    var ground = $this.data('shogiground');
-    var config = {
-      coordinates: { enabled: false },
-      drawable: { enabled: false, visible: false },
-      viewOnly: true,
-      sfen: { board: sfen, hands: hands },
-      hands: { inlined: true },
-      lastDests: dropOrMove,
-    };
+    const $this = $(this).removeClass('parse-sfen'),
+      lm = $this.data('lastmove'),
+      dropOrMove = lm ? (lm.includes('*') ? [lm.slice(2)] : [lm[0] + lm[1], lm[2] + lm[3]]) : undefined,
+      sfen = $this.data('sfen'),
+      hands = sfen && sfen.split(' ').length > 2 ? sfen.split(' ')[2] : '',
+      color = $this.data('color'),
+      variant = $this.data('variant'),
+      ground = $this.data('shogiground'),
+      config = {
+        coordinates: { enabled: false },
+        drawable: { enabled: false, visible: false },
+        viewOnly: true,
+        sfen: { board: sfen, hands: hands },
+        hands: { inlined: true },
+        lastDests: dropOrMove,
+        forsyth: {
+          fromForsyth: variant === 'chushogi' ? chushogiForsythToRole : undefined,
+        },
+      };
+    if (variant === 'chushogi') loadChushogiPieceSprite();
     if (color) config.orientation = color;
     if (ground) ground.set(config);
     else {
