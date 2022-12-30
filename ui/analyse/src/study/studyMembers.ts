55cd27c470ef2ca4166f1cc29d680f2899dc81ec
Thibault Duplessis <t@lichess.org>
2022-12-30 10:27:16 +0100

    autoscroll to ongoing broadcast round

--- ui/analyse/src/study/studyMembers.ts
+++ ui/analyse/src/study/studyMembers.ts
@@ -3,8 +3,8 @@ import { h, VNode } from 'snabbdom';
 import { iconTag, bind, onInsert, dataIcon, bindNonPassive } from 'common/snabbdom';
 import { makeCtrl as inviteFormCtrl, StudyInviteFormCtrl } from './inviteForm';
 import { NotifCtrl } from './notif';
-import { prop, Prop } from 'common';
-import { scrollTo, titleNameToId } from '../view/util';
+import { prop, Prop, scrollTo } from 'common';
+import { titleNameToId } from '../view/util';
 import { StudyCtrl, StudyMember, StudyMemberMap, Tab } from './interfaces';
 import { textRaw as xhrTextRaw } from 'common/xhr';
 
