a766e2fbba6f50c20ce288556315623a1bb26399
Benedikt Werner <1benediktwerner@gmail.com>
2023-01-14 23:54:29 +0100

    rtl: appeal

--- app/views/base/navTree.scala
+++ app/views/base/navTree.scala
@@ -13,19 +13,19 @@ object navTree:
     case Branch(id: String, name: Frag, children: List[Node], content: Option[Frag] = None)
     case Leaf(id: String, name: Frag, content: Frag)
 
-  def renderNode(node: Node, parent: Option[Node])(implicit ctx: Context): Frag =
+  def renderNode(node: Node, parent: Option[Node], forceLtr: Boolean = false)(implicit ctx: Context): Frag =
     node match
       case Node.Leaf(id, name, content) =>
         List(
           div(makeId(id), cls := "node leaf")(
-            h2(parent map goBack, name),
+            h2(parent.map(goBack(_, forceLtr)), name),
             div(cls := "content")(content)
           )
         )
       case b @ Node.Branch(id, name, children, content) =>
         frag(
           div(makeId(id), cls := s"node branch $id")(
-            h2(parent map goBack, name),
+            h2(parent.map(goBack(_, forceLtr)), name),
             content map { div(cls := "content")(_) },
             div(cls := "links")(
               children map { child =>
@@ -33,12 +33,17 @@ object navTree:
               }
             )
           ),
-          children map { renderNode(_, b.some) }
+          children map { renderNode(_, b.some, forceLtr) }
         )
 
   private def makeId(id: String) = st.id := s"help-$id"
 
   private def makeLink(id: String) = href := s"#help-$id"
 
-  private def goBack(parent: Node): Frag =
-    a(makeLink(parent.id), cls := "back", dataIcon := "", title := "Go back")
+  private def goBack(parent: Node, forceLtr: Boolean): Frag =
+    a(
+      makeLink(parent.id),
+      cls      := List("back" -> true, "no-mirror" -> forceLtr),
+      dataIcon := "",
+      title    := "Go back"
+    )
