e84feb834ec891a7071c5bfac5385bf4bf6c289f
Thibault Duplessis <t@lichess.org>
2023-01-14 14:09:23 +0100

    fix match warning

--- modules/i18n/src/main/Translator.scala
+++ modules/i18n/src/main/Translator.scala
@@ -36,7 +36,6 @@ object Translator:
     private def escapeArgs(args: Seq[Matchable]): Seq[RawFrag] =
       args.map {
         case s: String     => escapeHtml(Html(s))
-        case h: Html       => escapeHtml(h)
         case r: RawFrag    => r
         case f: StringFrag => RawFrag(f.render)
         case a             => RawFrag(a.toString)
