5f013c0743dad4ccfba6ede120a8b96e51ca0885
Thibault Duplessis <t@lichess.org>
2023-01-25 18:40:25 +0100

    all hail mapping macros

--- modules/streamer/src/main/BsonHandlers.scala
+++ modules/streamer/src/main/BsonHandlers.scala
@@ -6,16 +6,7 @@ import reactivemongo.api.bson.*
 
 private object BsonHandlers:
 
-  given BSONDocumentHandler[Streamer.YouTube] = new BSON[Streamer.YouTube]:
-    def reads(r: BSON.Reader) =
-      Streamer.YouTube(
-        r.get[String]("channelId"),
-        r.getO[String]("liveVideoId"),
-        r.getO[String]("pubsubVideoId")
-      )
-    def writes(w: BSON.Writer, o: Streamer.YouTube) =
-      $doc("channelId" -> o.channelId, "liveVideoId" -> o.liveVideoId, "pubsubVideoId" -> o.pubsubVideoId)
-
+  given BSONDocumentHandler[Streamer.YouTube]  = Macros.handler
   given BSONDocumentHandler[Streamer.Twitch]   = Macros.handler
   given BSONDocumentHandler[Streamer.Approval] = Macros.handler
   given BSONDocumentHandler[Streamer]          = Macros.handler
