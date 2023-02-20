212f82c3862a1d434e421ba2a20ac5f6e41e5e8e
Ben Rollin <ben.rollin@clever.com>
2023-02-20 01:44:05 -0800

    simple disambiguate/substitutions implementation

--- ui/input/src/voiceMoveHandler.ts
+++ ui/input/src/voiceMoveHandler.ts
@@ -4,12 +4,28 @@ import { Dests } from 'chessground/types';
 import { sanWriter, SanToUci } from 'chess';
 import * as util from './handlerUtil';
 
+const substitutionsMap = {
+  a: ['a8', '8'],
+  '3': ['3e'],
+  '8': ['a8', 'a'],
+  c: ['ce'],
+  d: ['de'],
+  g: ['ge'],
+};
+const closestLegalUci = (input: string, legalSans?: SanToUci): Uci | null => {
+  if (!legalSans) return null;
+  for (const [key, substitutions] of Object.entries(substitutionsMap)) {
+    for (const substitution of substitutions) {
+      const substituted = input.replace(key, substitution);
+      if (legalSans[substituted]) return legalSans[substituted];
+    }
+  }
+  return null;
+};
+
 export function makeVoiceHandler(ctrl: MoveCtrl): MoveHandler {
   let legalSans: SanToUci | undefined;
   function submit(v: string) {
-    const selectedKey = ctrl.hasSelected() || '';
-    const uci = util.sanToUci(v, legalSans);
-
     if (v.match(util.partialMoveRegex)) {
       ctrl.voice.partialMove(v);
       return;
@@ -19,6 +35,10 @@ export function makeVoiceHandler(ctrl: MoveCtrl): MoveHandler {
       return;
     }
 
+    const selectedKey = ctrl.hasSelected() || '';
+    const uci = util.sanToUci(v, legalSans);
+    const closeUci = closestLegalUci(v, legalSans);
+
     if (legalSans && v.match(util.fullUciRegex)) {
       ctrl.san(v.slice(0, 2) as Key, v.slice(2) as Key);
     } else if (legalSans && v.match(util.keyRegex)) {
@@ -26,17 +46,13 @@ export function makeVoiceHandler(ctrl: MoveCtrl): MoveHandler {
       else ctrl.select(v as Key);
     } else if (legalSans && uci) {
       ctrl.san(uci.slice(0, 2) as Key, uci.slice(2) as Key);
+    } else if (closeUci) {
+      ctrl.san(closeUci.slice(0, 2) as Key, closeUci.slice(2) as Key);
     } else if (legalSans && (selectedKey.slice(0, 1) + v).match(util.promotionRegex)) {
       const promotionSan = selectedKey && selectedKey.slice(0, 1) !== v.slice(0, 1) ? selectedKey.slice(0, 1) + v : v;
       const foundUci = util.sanToUci(promotionSan.replace('=', '').slice(0, -1), legalSans);
       if (!foundUci) return;
       ctrl.promote(foundUci.slice(0, 2) as Key, foundUci.slice(2) as Key, v.slice(-1).toUpperCase());
-    } else if (v.match(util.crazyhouseRegex)) {
-      // Incomplete crazyhouse strings such as Q@ or Q@a should do nothing.
-      if (v.length > 3 || (v.length > 2 && v.startsWith('@'))) {
-        if (v.length === 3) v = 'P' + v;
-        ctrl.drop(v.slice(2) as Key, v[0].toUpperCase());
-      }
     } else if (
       !util.nonMoveCommand(v, ctrl) &&
       v.length &&
