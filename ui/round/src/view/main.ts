b5dd24c51fcef2e8d128d6dbcbcb4cfbcd1e19aa
ryg-git <ygretharekar@outlook.com>
2023-03-16 17:20:18 +0530

    show first move expiration timer of oppoent in mobile view

--- ui/round/src/view/main.ts
+++ ui/round/src/view/main.ts
@@ -8,6 +8,7 @@ import { render as renderKeyboardMove } from 'keyboardMove';
 import { render as renderGround } from '../ground';
 import { renderTable } from './table';
 import { renderMaterialDiffs } from 'game/view/material';
+import { isPlayerTurn, playable } from 'game';
 
 export function main(ctrl: RoundController): VNode {
   const d = ctrl.data,
@@ -22,35 +23,40 @@ export function main(ctrl: RoundController): VNode {
       ctrl.ply
     );
 
+  const showExpiTop = playable(ctrl.data) && ctrl.data.expiration && !isPlayerTurn(ctrl.data);
+
   return ctrl.nvui
     ? ctrl.nvui.render(ctrl)
-    : h('div.round__app.variant-' + d.game.variant.key, [
-        h(
-          'div.round__app__board.main-board' + (ctrl.data.pref.blindfold ? '.blindfold' : ''),
-          {
-            hook:
-              'ontouchstart' in window || !lichess.storage.boolean('scrollMoves').getOrDefault(true)
-                ? undefined
-                : util.bind(
-                    'wheel',
-                    stepwiseScroll((e: WheelEvent, scroll: boolean) => {
-                      if (!ctrl.isPlaying()) {
-                        e.preventDefault();
-                        if (e.deltaY > 0 && scroll) keyboard.next(ctrl);
-                        else if (e.deltaY < 0 && scroll) keyboard.prev(ctrl);
-                        ctrl.redraw();
-                      }
-                    }),
-                    undefined,
-                    false
-                  ),
-          },
-          [renderGround(ctrl), ctrl.promotion.view(ctrl.data.game.variant.key === 'antichess')]
-        ),
-        ctrl.keyboardHelp ? keyboard.view(ctrl) : null,
-        crazyView(ctrl, topColor, 'top') || materialDiffs[0],
-        ...renderTable(ctrl),
-        crazyView(ctrl, bottomColor, 'bottom') || materialDiffs[1],
-        ctrl.keyboardMove ? renderKeyboardMove(ctrl.keyboardMove) : null,
-      ]);
+    : h(
+        (showExpiTop ? 'div.round__app.round__app__expi_top.variant-' : 'div.round__app.variant-') + d.game.variant.key,
+        [
+          h(
+            'div.round__app__board.main-board' + (ctrl.data.pref.blindfold ? '.blindfold' : ''),
+            {
+              hook:
+                'ontouchstart' in window || !lichess.storage.boolean('scrollMoves').getOrDefault(true)
+                  ? undefined
+                  : util.bind(
+                      'wheel',
+                      stepwiseScroll((e: WheelEvent, scroll: boolean) => {
+                        if (!ctrl.isPlaying()) {
+                          e.preventDefault();
+                          if (e.deltaY > 0 && scroll) keyboard.next(ctrl);
+                          else if (e.deltaY < 0 && scroll) keyboard.prev(ctrl);
+                          ctrl.redraw();
+                        }
+                      }),
+                      undefined,
+                      false
+                    ),
+            },
+            [renderGround(ctrl), ctrl.promotion.view(ctrl.data.game.variant.key === 'antichess')]
+          ),
+          ctrl.keyboardHelp ? keyboard.view(ctrl) : null,
+          crazyView(ctrl, topColor, 'top') || materialDiffs[0],
+          ...renderTable(ctrl),
+          crazyView(ctrl, bottomColor, 'bottom') || materialDiffs[1],
+          ctrl.keyboardMove ? renderKeyboardMove(ctrl.keyboardMove) : null,
+        ]
+      );
 }
