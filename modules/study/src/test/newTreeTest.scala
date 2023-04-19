28a3d25b02a5dfacdd7a4cf35b3057932469ef90
kraktus <kraktus@users.noreply.github.com>
2023-04-19 10:41:49 +0200

    WIP progress toward NewTree -> Tree, fix NewTree model

--- modules/study/src/test/newTreeTest.scala
+++ modules/study/src/test/newTreeTest.scala
@@ -44,11 +44,20 @@ class NewTreeTest extends lila.common.LilaTest:
         node.glyphs,
         node.opening,
         node.comp,
-        node.crazyData,
-        node.dropFirstChild,
         node.clock,
+        node.crazyData,
         node.forceVariation
-      ) 
+      )
+
+  // Convertor
+  object NewBranchC:
+    def fromBranch(branch: Branch) =
+      NewBranch(
+        branch.id,
+        branch.move,
+        MetasC.fromNode(branch)
+      )
+  // extension (newBranch: NewBranch)
 
   // Convertor
   object NewTreeC:
@@ -56,32 +65,65 @@ class NewTreeTest extends lila.common.LilaTest:
       // Assumes `Root` has a main move
       val first = root.children.first.get
       NewTree.make(
-        value=NewBranchC.fromBranch(first),
-        child=NewTreeC.fromBranch(first),
-        variations=root.children.variations.flatMap(NewTreeC.fromBranch)
+        value = NewBranchC.fromBranch(first),
+        child = NewTreeC.fromBranch(first),
+        variations = root.children.variations.flatMap(NewTreeC.fromBranch)
       )
-    
-    def fromBranch(branch: Branch): Option[NewTree] = 
+
+    def fromBranch(branch: Branch): Option[NewTree] =
       branch.children.first.map { first =>
-      NewTree.make(
-        value=NewBranchC.fromBranch(first),
-        child=NewTreeC.fromBranch(first),
-        variations=branch.children.variations.flatMap(NewTreeC.fromBranch)
-      )
-    }
-  // Convertor
-  object NewBranchC:
-    def fromBranch(branch: Branch) =
-      NewBranch(
-        branch.id,
-        MetasC.fromNode(branch),
-      )
+        NewTree.make(
+          value = NewBranchC.fromBranch(first),
+          child = NewTreeC.fromBranch(first),
+          variations = branch.children.variations.flatMap(NewTreeC.fromBranch)
+        )
+      }
+  extension (newTree: NewTree)
+    def toBranch: Branch = Branch(
+      newTree.value.id,
+      newTree.value.metas.ply,
+      newTree.value.move,
+      newTree.value.metas.fen,
+      newTree.value.metas.check,
+      newTree.value.metas.dests,
+      newTree.value.metas.drops,
+      newTree.value.metas.eval,
+      newTree.value.metas.shapes,
+      newTree.value.metas.comments,
+      newTree.value.metas.gamebook,
+      newTree.value.metas.glyphs,
+      Branches(newTree.children.map(_.toBranch)),
+      newTree.value.metas.opening,
+      newTree.value.metas.comp,
+      newTree.value.metas.clock,
+      newTree.value.metas.crazyData,
+      newTree.value.metas.forceVariation
+    )
+
   // Convertor
   object NewRootC:
-    def fromRoot(root: Root) = 
+    def fromRoot(root: Root) =
       NewRoot(MetasC.fromNode(root), NewTreeC.fromRoot(root))
+  extension (newRoot: NewRoot)
+    def toRoot =
+      Root(
+        newRoot.metas.ply,
+        newRoot.metas.fen,
+        newRoot.metas.check,
+        newRoot.metas.dests,
+        newRoot.metas.drops,
+        newRoot.metas.eval,
+        newRoot.metas.shapes,
+        newRoot.metas.comments,
+        newRoot.metas.gamebook,
+        newRoot.metas.glyphs,
+        Branches(newRoot.tree.mainlineAndVariations.map(_.toBranch)),
+        newRoot.metas.opening,
+        newRoot.metas.clock,
+        newRoot.metas.crazyData
+      )
 
-  val pgn                          = """
+  val pgn = """
   { Root comment }
 1. e4! $16 $40 $32 (1. d4?? d5 $146 { d5 is a good move }) 1... e6?! { e6 is a naughty move } *
   """
