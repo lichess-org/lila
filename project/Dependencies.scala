e11bde923838371a3d5a0b0d2105ab0e0f6f1eca
WandererXII <43043181+WandererXII@users.noreply.github.com>
2023-04-26 00:32:37 +0200

    Bump deps, plugins and sbt

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,29 +5,29 @@ object Dependencies {
 
   object Resolvers {
 
-    val sonatype  = Resolver.sonatypeRepo("releases")
-    val sonatypeS = Resolver.sonatypeRepo("snapshots")
+    val sonatype  = Resolver.sonatypeOssRepos("releases")
+    val sonatypeS = Resolver.sonatypeOssRepos("snapshots")
     val lilaMaven = "lila-maven" at "https://raw.githubusercontent.com/lichess-org/lila-maven/master"
 
-    val commons = Seq(sonatype, lilaMaven, sonatypeS)
+    val commons = Seq(lilaMaven) ++ sonatype ++ sonatypeS
   }
 
-  val shogi       = "io.github.WandererXII" %% "scalashogi"                     % "5.0.2"
+  val shogi       = "io.github.WandererXII" %% "scalashogi"                     % "7.0.1"
   val scalalib    = "com.github.ornicar"   %% "scalalib"                        % "7.0.2"
   val hasher      = "com.roundeights"      %% "hasher"                          % "1.2.1"
-  val jodaTime    = "joda-time"             % "joda-time"                       % "2.12.1"
+  val jodaTime    = "joda-time"             % "joda-time"                       % "2.12.5"
   val maxmind     = "com.sanoma.cda"       %% "maxmind-geoip2-scala"            % "1.3.1-THIB"
   val prismic     = "io.prismic"           %% "scala-kit"                       % "1.2.18-THIB213"
   val scrimage    = "com.sksamuel.scrimage" % "scrimage-core"                   % "4.0.22"
   val scaffeine   = "com.github.blemale"   %% "scaffeine"                       % "5.2.1" % "compile"
-  val googleOAuth = "com.google.auth"       % "google-auth-library-oauth2-http" % "1.7.0"
+  val googleOAuth = "com.google.auth"       % "google-auth-library-oauth2-http" % "1.16.0"
   val galimatias  = "io.mola.galimatias"    % "galimatias"                      % "0.2.1"
   val scalatags   = "com.lihaoyi"          %% "scalatags"                       % "0.12.0"
-  val lettuce     = "io.lettuce"            % "lettuce-core"                    % "6.2.1.RELEASE"
-  val epoll       = "io.netty"              % "netty-transport-native-epoll"    % "4.1.86.Final" classifier "linux-x86_64"
+  val lettuce     = "io.lettuce"            % "lettuce-core"                    % "6.2.3.RELEASE"
+  val epoll       = "io.netty"              % "netty-transport-native-epoll"    % "4.1.91.Final" classifier "linux-x86_64"
   val autoconfig  = "io.methvin.play"      %% "autoconfig-macros"               % "0.3.2" % "provided"
   val scalatest   = "org.scalatest"        %% "scalatest"                       % "3.2.11" % Test
-  val uaparser    = "org.uaparser"         %% "uap-scala"                       % "0.13.0"
+  val uaparser    = "org.uaparser"         %% "uap-scala"                       % "0.14.0"
 
   object flexmark {
     val version = "0.50.50"
@@ -54,9 +54,9 @@ object Dependencies {
   }
 
   object play {
-    val version = "2.8.16"
+    val version = "2.8.19"
     val api     = "com.typesafe.play" %% "play"      % version
-    val json    = "com.typesafe.play" %% "play-json" % "2.9.3"
+    val json    = "com.typesafe.play" %% "play-json" % "2.9.4"
   }
   object kamon {
     val version    = "2.2.3"
@@ -66,7 +66,7 @@ object Dependencies {
     val prometheus = "io.kamon" %% "kamon-prometheus"     % version
   }
   object akka {
-    val version    = "2.6.19"
+    val version    = "2.6.20"
     val akka       = "com.typesafe.akka" %% "akka-actor"       % version
     val akkaTyped  = "com.typesafe.akka" %% "akka-actor-typed" % version
     val akkaStream = "com.typesafe.akka" %% "akka-stream"      % version
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,4 @@
 addSbtPlugin("com.typesafe.play" % "sbt-plugin"   % "2.8.16")
-addSbtPlugin("org.scalameta"     % "sbt-scalafmt" % "2.4.6")
-addSbtPlugin("ch.epfl.scala"     % "sbt-bloop"    % "1.4.8")
+addSbtPlugin("org.scalameta"     % "sbt-scalafmt" % "2.5.0")
+// addSbtPlugin("org.wartremover" % "sbt-wartremover" % "3.0.7")
+ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
