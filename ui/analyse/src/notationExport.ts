39a7c5b5088566208a6b12ec6742791e464a6990
WandererXII <43043181+WandererXII@users.noreply.github.com>
2023-05-02 01:38:10 +0200

    Fix notation export
    Since we no longer send tags as kif tags

--- ui/analyse/src/notationExport.ts
+++ ui/analyse/src/notationExport.ts
@@ -9,6 +9,7 @@ import { Shogi } from 'shogiops/variant/shogi';
 import { ops as treeOps } from 'tree';
 import { renderTime } from './clocks';
 import AnalyseCtrl from './ctrl';
+import { isHandicap } from 'shogiops/handicaps';
 
 function makeKifTime(moveTime: number, totalTime: number): string {
   return '   (' + renderTime(moveTime, false) + '/' + renderTime(totalTime, true) + ')';
@@ -65,28 +66,57 @@ function makeKifNodes(node: Tree.Node, pos: Position, offset: number): string[] 
 
   return res;
 }
-
+const toKanji = {
+  Start: '開始日時',
+  End: '終了日時',
+  Event: '棋戦',
+  Site: '場所',
+  Opening: '戦型',
+  Timecontrol: '持ち時間',
+  Byoyomi: '秒読み',
+  Handicap: '手合割',
+  Sente: '先手',
+  Gote: '後手',
+  Senteteam: '先手のチーム',
+  Goteteam: '後手のチーム',
+  Annotator: '注釈者',
+  Termination: '図',
+  Problemname: '作品名',
+  Problemid: '作品番号',
+  Composer: '作者',
+  Dateofpublicatio: '発表年月',
+  Publication: '発表誌',
+  Collection: '出典',
+  Length: '手数',
+  Prize: '受賞',
+};
 export function renderFullKif(ctrl: AnalyseCtrl): string {
-  const g = ctrl.data.game;
-  const offset = ctrl.plyOffset();
+  const g = ctrl.data.game,
+    offset = ctrl.plyOffset(),
+    sfen = g.initialSfen || initialSfen(ctrl.data.game.variant.key);
 
-  const pos = parseSfen(
-    ctrl.data.game.variant.key,
-    g.initialSfen || initialSfen(ctrl.data.game.variant.key),
-    false
-  ).unwrap();
-  const moves = makeKifNodes(ctrl.tree.root, pos, offset % 2).join('\n');
+  const pos = parseSfen(ctrl.data.game.variant.key, sfen, false).unwrap(),
+    moves = makeKifNodes(ctrl.tree.root, pos, offset % 2).join('\n');
 
-  const tags = ctrl.data.tags ?? [];
+  const tags = ctrl.data.tags ?? [],
+    handicap = isHandicap({ sfen: sfen, rules: g.variant.key }),
+    colorTags = [handicap ? '下手' : '先手', handicap ? '上手' : '後手'];
   // We either don't want to display these or we display them through other means
-  const unwatedTagNames = ['先手', '下手', '後手', '上手', '手合割', '図', 'Sfen', 'Result', 'Variant'];
-  const otherTags = tags.filter(t => !unwatedTagNames.includes(t[0])).map(t => t[0] + '：' + t[1]);
+  const unwatedTagNames = ['Sente', 'Shitate', 'Gote', 'Uwate', 'Handicap', 'Termination', 'Sfen', 'Result', 'Variant'],
+    otherTags = tags.filter(t => !unwatedTagNames.includes(t[0])).map(t => (toKanji[t[0]] || t[0]) + '：' + t[1]);
 
   // We want these even empty
-  const sente = tags.find(t => t[0] === '先手' || t[0] === '下手') ?? ['先手', ''];
-  const gote = tags.find(t => t[0] === '後手' || t[0] === '上手') ?? ['後手', ''];
+  const sente = tags.find(t => t[0] === 'Sente' || t[0] === 'Shitate')?.[1] ?? '',
+    gote = tags.find(t => t[0] === 'Gote' || t[0] === 'Uwate')?.[1] ?? '';
 
-  return [...otherTags, makeKifHeader(pos), sente.join('：'), gote.join('：'), '手数----指手---------消費時間--', moves]
+  return [
+    ...otherTags,
+    makeKifHeader(pos),
+    `${colorTags[0]}：${sente}`,
+    `${colorTags[1]}：${gote}`,
+    '手数----指手---------消費時間--',
+    moves,
+  ]
     .filter(l => l.length)
     .join('\n');
 }
@@ -123,22 +153,24 @@ function makeCsaMainline(node: Tree.Node, pos: Shogi): string[] {
 
 function processCsaTags(tags: string[][]): string[] {
   function kifTagToCsaTag(kifTag: string[]): string {
-    switch (kifTag[0]) {
-      case '開始日時':
+    switch (kifTag[0].toLowerCase()) {
+      case 'start':
         return `$START_TIME:${kifTag[1]}`;
-      case '終了日時':
+      case 'end':
         return `$END_TIME:${kifTag[1]}`;
-      case '棋戦':
+      case 'event':
         return `$EVENT:${kifTag[1]}`;
-      case '場所':
+      case 'site':
         return `$SITE:${kifTag[1]}`;
-      case '持ち時間':
+      case 'timecontrol':
         return `$TIME_LIMIT:${kifTag[1].replace(/[^\d+\|]/g, '')}`;
-      case '戦型':
+      case 'opening':
         return `$OPENING:${kifTag[1]}`;
-      case '先手':
+      case 'sente':
+      case 'shitate':
         return `N+${kifTag[1]}`;
-      case '後手':
+      case 'gote':
+      case 'uwate':
         return `N-${kifTag[1]}`;
       default:
         return '';
