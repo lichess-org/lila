a52d666409fbaf82959c8575a9f765553f17bfa8
Thibault Duplessis <t@lichess.org>
2023-05-13 09:14:25 +0200

    scala code golf

--- modules/i18n/src/main/JsDump.scala
+++ modules/i18n/src/main/JsDump.scala
@@ -5,14 +5,13 @@ import play.api.i18n.Lang
 
 object JsDump:
 
-  private def quantitySuffix(q: I18nQuantity): String =
-    q match
-      case I18nQuantity.Zero  => ":zero"
-      case I18nQuantity.One   => ":one"
-      case I18nQuantity.Two   => ":two"
-      case I18nQuantity.Few   => ":few"
-      case I18nQuantity.Many  => ":many"
-      case I18nQuantity.Other => ""
+  private val quantitySuffix: I18nQuantity => String =
+    case I18nQuantity.Zero  => ":zero"
+    case I18nQuantity.One   => ":one"
+    case I18nQuantity.Two   => ":two"
+    case I18nQuantity.Few   => ":few"
+    case I18nQuantity.Many  => ":many"
+    case I18nQuantity.Other => ""
 
   private type JsTrans = Iterable[(String, JsString)]
 
@@ -29,13 +28,10 @@ object JsDump:
         plurals.messages.toList match
           case List((_, msg)) => Map(k -> JsString(msg))
           case list =>
-            list.map { (quantity, msg) =>
+            list.map: (quantity, msg) =>
               s"$k${quantitySuffix(quantity)}" -> JsString(msg)
-            }
 
   def keysToObject(keys: Seq[I18nKey], lang: Lang): JsObject =
-    JsObject {
-      keys.flatMap { k =>
+    JsObject:
+      keys.flatMap: k =>
         Translator.findTranslation(k, lang).fold[JsTrans](Nil) { translatedJs(k, _) }
-      }
-    }
