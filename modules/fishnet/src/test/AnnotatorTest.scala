50e9d36cf7eb548e1bb575b0973b7a3c47209857
Thanh Le <lenguyenthanh@hotmail.com>
2023-05-19 22:56:05 +0200

    Remove unused imports

--- modules/fishnet/src/test/AnnotatorTest.scala
+++ modules/fishnet/src/test/AnnotatorTest.scala
@@ -1,36 +1,5 @@
 package lila.fishnet
 
-import JsonApi.*
-import play.api.libs.json.Reads
-import play.api.libs.json.*
-import lila.game.PgnImport
-import lila.fishnet.JsonApi.Request.Evaluation
-import chess.format.pgn.{
-  Move,
-  Dumper,
-  SanStr,
-  Pgn,
-  PgnStr,
-  Initial,
-  Tag,
-  Tags,
-  Parser,
-  PgnTree,
-  ParsedPgnTree,
-  ParsedPgn,
-  PgnNodeData
-}
-import chess.{ Node, Ply, MoveOrDrop, Situation }
-import chess.MoveOrDrop.*
-import lila.analyse.{ Analysis, Annotator }
-import lila.common.config.NetDomain
-import lila.game.PgnDump
-import lila.common.config.BaseUrl
-import chess.variant.Standard
-import lila.fishnet.Work.Analysis
-import java.time.Instant
-import chess.Clock
-
 final class AnnotatorTest extends munit.FunSuite:
 
   test("annotated game with fishnet input"):
