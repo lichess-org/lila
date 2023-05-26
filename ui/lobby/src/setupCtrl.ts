b66b3232e7a8fb4517f8971fb305e87d8d94d074
Aditya Jadhav <77366709+adityaj2003@users.noreply.github.com>
2023-05-26 06:27:05 -0700

    Update setupCtrl.ts
    Lint issues with tabs and spaces.

--- ui/lobby/src/setupCtrl.ts
+++ ui/lobby/src/setupCtrl.ts
@@ -1,8 +1,8 @@
-import { Prop, propWithEffect } from 'common';
-import debounce from 'common/debounce';
-import * as xhr from 'common/xhr';
-import { storedJsonProp, StoredJsonProp } from 'common/storage';
-import LobbyController from './ctrl';
+import { Prop, propWithEffect } from "common";
+import debounce from "common/debounce";
+import * as xhr from "common/xhr";
+import { storedJsonProp, StoredJsonProp } from "common/storage";
+import LobbyController from "./ctrl";
 import {
   ForceSetupOptions,
   GameMode,
@@ -12,7 +12,7 @@ import {
   RealValue,
   SetupStore,
   TimeMode,
-} from './interfaces';
+} from "./interfaces";
 import {
   daysVToDays,
   incrementVToIncrement,
@@ -21,33 +21,38 @@ import {
   timeModes,
   timeVToTime,
   variants,
-} from './options';
+} from "./options";
 
-const getPerf = (variant: VariantKey, timeMode: TimeMode, time: RealValue, increment: RealValue): Perf => {
-  if (!['standard', 'fromPosition'].includes(variant)) return variant as Perf;
-  if (timeMode !== 'realTime') return 'correspondence';
+const getPerf = (
+  variant: VariantKey,
+  timeMode: TimeMode,
+  time: RealValue,
+  increment: RealValue
+): Perf => {
+  if (!["standard", "fromPosition"].includes(variant)) return variant as Perf;
+  if (timeMode !== "realTime") return "correspondence";
 
   const totalGameTime = time * 60 + increment * 40;
   return totalGameTime < 30
-    ? 'ultraBullet'
+    ? "ultraBullet"
     : totalGameTime < 180
-    ? 'bullet'
+    ? "bullet"
     : totalGameTime < 480
-    ? 'blitz'
+    ? "blitz"
     : totalGameTime < 1500
-    ? 'rapid'
-    : 'classical';
+    ? "rapid"
+    : "classical";
 };
 
 export default class SetupController {
   root: LobbyController;
   store: Record<GameType, StoredJsonProp<SetupStore>>;
   gameType: GameType | null = null;
-  lastValidFen = '';
+  lastValidFen = "";
   fenError = false;
-  friendUser = '';
+  friendUser = "";
   loading = false;
-  blindModeColor: Prop<Color | 'random'>;
+  blindModeColor: Prop<Color | "random">;
 
   // Store props
   variant: Prop<VariantKey>;
@@ -71,27 +76,28 @@ export default class SetupController {
 
   constructor(ctrl: LobbyController) {
     this.root = ctrl;
-    this.blindModeColor = propWithEffect('random', this.onPropChange);
+    this.blindModeColor = propWithEffect("random", this.onPropChange);
     // Initialize stores with default props as necessary
     this.store = {
-      hook: this.makeSetupStore('hook'),
-      friend: this.makeSetupStore('friend'),
-      ai: this.makeSetupStore('ai'),
+      hook: this.makeSetupStore("hook"),
+      friend: this.makeSetupStore("friend"),
+      ai: this.makeSetupStore("ai"),
     };
   }
 
   // Namespace the store by username for user specific modal settings
-  private storeKey = (gameType: GameType) => `lobby.setup.${this.root.me?.username || 'anon'}.${gameType}`;
+  private storeKey = (gameType: GameType) =>
+    `lobby.setup.${this.root.me?.username || "anon"}.${gameType}`;
 
   makeSetupStore = (gameType: GameType) =>
     storedJsonProp<SetupStore>(this.storeKey(gameType), () => ({
-      variant: 'standard',
-      fen: '',
-      timeMode: gameType === 'hook' ? 'realTime' : 'unlimited',
+      variant: "standard",
+      fen: "",
+      timeMode: gameType === "hook" ? "realTime" : "unlimited",
       time: 5,
       increment: 3,
       days: 2,
-      gameMode: gameType === 'ai' || !this.root.me ? 'casual' : 'rated',
+      gameMode: gameType === "ai" || !this.root.me ? "casual" : "rated",
       ratingMin: -500,
       ratingMax: 500,
       aiLevel: 1,
@@ -100,40 +106,49 @@ export default class SetupController {
   private loadPropsFromStore = (forceOptions?: ForceSetupOptions) => {
     const storeProps = this.store[this.gameType!]();
     // Load props from the store, but override any store values with values found in forceOptions
-    this.variant = this.propWithApplyVariant(forceOptions?.variant || storeProps.variant);
+    this.variant = this.propWithApplyVariant(
+      forceOptions?.variant || storeProps.variant
+    );
     this.fen = this.propWithApply(forceOptions?.fen || storeProps.fen);
-    this.timeMode = this.propWithApply(forceOptions?.timeMode || storeProps.timeMode);
-    this.timeV = this.propWithApply(sliderInitVal(storeProps.time, timeVToTime, 100)!);
-    this.incrementV = this.propWithApply(sliderInitVal(storeProps.increment, incrementVToIncrement, 100)!);
-    this.daysV = this.propWithApply(sliderInitVal(storeProps.days, daysVToDays, 20)!);
+    this.timeMode = this.propWithApply(
+      forceOptions?.timeMode || storeProps.timeMode
+    );
+    this.timeV = this.propWithApply(
+      sliderInitVal(storeProps.time, timeVToTime, 100)!
+    );
+    this.incrementV = this.propWithApply(
+      sliderInitVal(storeProps.increment, incrementVToIncrement, 100)!
+    );
+    this.daysV = this.propWithApply(
+      sliderInitVal(storeProps.days, daysVToDays, 20)!
+    );
     this.gameMode = this.propWithApply(storeProps.gameMode);
     this.ratingMin = this.propWithApply(storeProps.ratingMin);
-		this.ratingMax = this.propWithApply(storeProps.ratingMax);
+    this.ratingMax = this.propWithApply(storeProps.ratingMax);
     this.aiLevel = this.propWithApply(storeProps.aiLevel);
-     
+
     this.enforcePropRules();
     // Upon loading the props from the store, overriding with forced options, and enforcing rules,
     // immediately save them to the store. This way, the user can know that whatever they saw last
     // in the modal will be there when they open it at a later time.
     this.savePropsToStore();
-	
   };
 
   private enforcePropRules = () => {
     // reassign with this.propWithApply in this function to avoid calling this.onPropChange
 
     // replace underscores with spaces in FEN
-    if (this.variant() === 'fromPosition') this.fen = this.propWithApply(this.fen().replace(/_/g, ' '));
+    if (this.variant() === "fromPosition")
+      this.fen = this.propWithApply(this.fen().replace(/_/g, " "));
 
-    if (this.gameMode() === 'rated' && this.ratedModeDisabled()) {
-      this.gameMode = this.propWithApply('casual');
+    if (this.gameMode() === "rated" && this.ratedModeDisabled()) {
+      this.gameMode = this.propWithApply("casual");
     }
 
     if (this.ratingMin() === 0 && this.ratingMax() === 0) {
       this.ratingMax = this.propWithApply(50);
     }
   };
-
 
   private savePropsToStore = () =>
     this.gameType &&
@@ -149,63 +164,73 @@ export default class SetupController {
       ratingMax: this.ratingMax(),
       aiLevel: this.aiLevel(),
     });
-private savePropsToStoreExceptRating = () => {
-  if (this.gameType && this.store[this.gameType]) {
-    const ratingMin = this.store[this.gameType]().ratingMin;
-    const ratingMax = this.store[this.gameType]().ratingMax;
-    this.store[this.gameType]({
-      variant: this.variant(),
-      fen: this.fen(),
-      timeMode: this.timeMode(),
-      time: this.time(),
-      increment: this.increment(),
-      days: this.days(),
-      gameMode: this.gameMode(),
-      ratingMin: ratingMin,
-      ratingMax: ratingMax,
-      aiLevel: this.aiLevel(),
-    });
-  }
-};
+  private savePropsToStoreExceptRating = () => {
+    if (this.gameType && this.store[this.gameType]) {
+      const ratingMin = this.store[this.gameType]().ratingMin;
+      const ratingMax = this.store[this.gameType]().ratingMax;
+      this.store[this.gameType]({
+        variant: this.variant(),
+        fen: this.fen(),
+        timeMode: this.timeMode(),
+        time: this.time(),
+        increment: this.increment(),
+        days: this.days(),
+        gameMode: this.gameMode(),
+        ratingMin: ratingMin,
+        ratingMax: ratingMax,
+        aiLevel: this.aiLevel(),
+      });
+    }
+  };
   private onPropChange = () => {
     this.enforcePropRules();
-	if(this.root.data.ratingMap && this.selectedPerf() && !!this.root.data.ratingMap[this.selectedPerf()].prov) {
-		this.savePropsToStoreExceptRating();
-	}
-	else {
-		this.savePropsToStore();
-	}
+    if (
+      this.root.data.ratingMap &&
+      this.selectedPerf() &&
+      !!this.root.data.ratingMap[this.selectedPerf()].prov
+    ) {
+      this.savePropsToStoreExceptRating();
+    } else {
+      this.savePropsToStore();
+    }
     this.root.redraw();
   };
 
-  	private propWithApply = <A>(value: A) => propWithEffect(value, this.onPropChange);
+  private propWithApply = <A>(value: A) =>
+    propWithEffect(value, this.onPropChange);
 
-  	private propWithApplyVariant = <A>(value: A) => {
+  private propWithApplyVariant = <A>(value: A) => {
     const prop = propWithEffect(value, this.onVariantChange);
     return prop;
-};
-	
+  };
 
-  openModal = (gameType: GameType, forceOptions?: ForceSetupOptions, friendUser?: string) => {
+  openModal = (
+    gameType: GameType,
+    forceOptions?: ForceSetupOptions,
+    friendUser?: string
+  ) => {
     this.root.leavePool();
     this.gameType = gameType;
     this.loading = false;
     this.fenError = false;
-    this.lastValidFen = '';
-    this.friendUser = friendUser || '';
+    this.lastValidFen = "";
+    this.friendUser = friendUser || "";
     this.loadPropsFromStore(forceOptions);
   };
 
-	private onVariantChange = () => {
+  private onVariantChange = () => {
     // Handle rating update here
-    if(this.root.data.ratingMap && this.selectedPerf() && !!this.root.data.ratingMap[this.selectedPerf()].prov) {
-		this.savePropsToStoreExceptRating();
-	}
-	else {
-		this.savePropsToStore();
-	}
+    if (
+      this.root.data.ratingMap &&
+      this.selectedPerf() &&
+      !!this.root.data.ratingMap[this.selectedPerf()].prov
+    ) {
+      this.savePropsToStoreExceptRating();
+    } else {
+      this.savePropsToStore();
+    }
     this.root.redraw();
-};	
+  };
 
   closeModal = () => {
     this.gameType = null;
@@ -215,47 +240,55 @@ private savePropsToStoreExceptRating = () => {
   validateFen = debounce(() => {
     const fen = this.fen();
     if (!fen) return;
-    xhr.text(xhr.url('/setup/validate-fen', { fen, strict: this.gameType === 'ai' ? 1 : undefined })).then(
-      () => {
-        this.fenError = false;
-        this.lastValidFen = fen;
-        this.root.redraw();
-      },
-      () => {
-        this.fenError = true;
-        this.root.redraw();
-      }
-    );
+    xhr
+      .text(
+        xhr.url("/setup/validate-fen", {
+          fen,
+          strict: this.gameType === "ai" ? 1 : undefined,
+        })
+      )
+      .then(
+        () => {
+          this.fenError = false;
+          this.lastValidFen = fen;
+          this.root.redraw();
+        },
+        () => {
+          this.fenError = true;
+          this.root.redraw();
+        }
+      );
   }, 300);
 
   ratedModeDisabled = (): boolean =>
     // anonymous games cannot be rated
     !this.root.me ||
     // unlimited games cannot be rated
-    (this.gameType === 'hook' && this.timeMode() === 'unlimited') ||
+    (this.gameType === "hook" && this.timeMode() === "unlimited") ||
     // variants with very low time cannot be rated
-    (this.variant() !== 'standard' &&
-      (this.timeMode() !== 'realTime' ||
+    (this.variant() !== "standard" &&
+      (this.timeMode() !== "realTime" ||
         (this.time() < 0.5 && this.increment() == 0) ||
         (this.time() == 0 && this.increment() < 2)));
 
-  selectedPerf = (): Perf => getPerf(this.variant(), this.timeMode(), this.time(), this.increment());
+  selectedPerf = (): Perf =>
+    getPerf(this.variant(), this.timeMode(), this.time(), this.increment());
 
   ratingRange = (): string => {
-    if (!this.root.data.ratingMap) return '';
+    if (!this.root.data.ratingMap) return "";
     const rating = this.root.data.ratingMap[this.selectedPerf()].rating;
     return `${rating + this.ratingMin()}-${rating + this.ratingMax()}`;
   };
 
-  hookToPoolMember = (color: Color | 'random'): PoolMember | null => {
+  hookToPoolMember = (color: Color | "random"): PoolMember | null => {
     const valid =
-      color == 'random' &&
-      this.gameType === 'hook' &&
-      this.variant() == 'standard' &&
-      this.gameMode() == 'rated' &&
-      this.timeMode() == 'realTime';
+      color == "random" &&
+      this.gameType === "hook" &&
+      this.variant() == "standard" &&
+      this.gameMode() == "rated" &&
+      this.timeMode() == "realTime";
     const id = `${this.time()}+${this.increment()}`;
-    return valid && this.root.pools.find(p => p.id === id)
+    return valid && this.root.pools.find((p) => p.id === id)
       ? {
           id,
           range: this.ratingRange(),
@@ -263,7 +296,7 @@ private savePropsToStoreExceptRating = () => {
       : null;
   };
 
-  propsToFormData = (color: Color | 'random'): FormData =>
+  propsToFormData = (color: Color | "random"): FormData =>
     xhr.form({
       variant: keyToId(this.variant(), variants).toString(),
       fen: this.fen(),
@@ -274,7 +307,7 @@ private savePropsToStoreExceptRating = () => {
       increment_range: this.incrementV().toString(),
       days: this.days().toString(),
       days_range: this.daysV().toString(),
-      mode: this.gameMode() === 'casual' ? '0' : '1',
+      mode: this.gameMode() === "casual" ? "0" : "1",
       ratingRange: this.ratingRange(),
       ratingRange_range_min: this.ratingMin().toString(),
       ratingRange_range_max: this.ratingMax().toString(),
@@ -282,13 +315,18 @@ private savePropsToStoreExceptRating = () => {
       color,
     });
 
-  validFen = (): boolean => this.variant() !== 'fromPosition' || !this.fenError;
-  validTime = (): boolean => this.timeMode() !== 'realTime' || this.time() > 0 || this.increment() > 0;
+  validFen = (): boolean => this.variant() !== "fromPosition" || !this.fenError;
+  validTime = (): boolean =>
+    this.timeMode() !== "realTime" || this.time() > 0 || this.increment() > 0;
   validAiTime = (): boolean =>
-    this.gameType !== 'ai' || this.timeMode() !== 'realTime' || this.variant() !== 'fromPosition' || this.time() >= 1;
-  valid = (): boolean => this.validFen() && this.validTime() && this.validAiTime();
+    this.gameType !== "ai" ||
+    this.timeMode() !== "realTime" ||
+    this.variant() !== "fromPosition" ||
+    this.time() >= 1;
+  valid = (): boolean =>
+    this.validFen() && this.validTime() && this.validAiTime();
 
-  submit = async (color: Color | 'random') => {
+  submit = async (color: Color | "random") => {
     const poolMember = this.hookToPoolMember(color);
     if (poolMember) {
       this.root.enterPool(poolMember);
@@ -296,23 +334,26 @@ private savePropsToStoreExceptRating = () => {
       return;
     }
 
-    if (this.gameType === 'hook') this.root.setTab(this.timeMode() === 'realTime' ? 'real_time' : 'seeks');
+    if (this.gameType === "hook")
+      this.root.setTab(this.timeMode() === "realTime" ? "real_time" : "seeks");
     this.loading = true;
     this.root.redraw();
 
     let urlPath = `/setup/${this.gameType}`;
-    if (this.gameType === 'hook') urlPath += `/${lichess.sri}`;
+    if (this.gameType === "hook") urlPath += `/${lichess.sri}`;
     const urlParams = { user: this.friendUser || undefined };
     let response;
     try {
       response = await xhr.textRaw(xhr.url(urlPath, urlParams), {
-        method: 'post',
+        method: "post",
         body: this.propsToFormData(color),
       });
     } catch (e) {
       this.loading = false;
       this.root.redraw();
-      alert('Sorry, we encountered an error while creating your game. Please try again.');
+      alert(
+        "Sorry, we encountered an error while creating your game. Please try again."
+      );
       return;
     }
 
@@ -323,9 +364,9 @@ private savePropsToStoreExceptRating = () => {
       alert(
         errs
           ? Object.keys(errs)
-              .map(k => `${k}: ${errs[k]}`)
-              .join('\n')
-          : 'Invalid setup'
+              .map((k) => `${k}: ${errs[k]}`)
+              .join("\n")
+          : "Invalid setup"
       );
       if (response.status == 403) {
         // 403 FORBIDDEN closes this modal because challenges to the recipient
