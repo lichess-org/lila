609a20c56cec5d3c973cb380a4b4898d05012d32
Aditya Jadhav <77366709+adityaj2003@users.noreply.github.com>
2023-05-26 06:29:39 -0700

    Update variantPicker.ts
    Mixed tabs and spaces

--- ui/lobby/src/view/setup/components/variantPicker.ts
+++ ui/lobby/src/view/setup/components/variantPicker.ts
@@ -1,35 +1,44 @@
-import { onInsert } from 'common/snabbdom';
-import { h } from 'snabbdom';
-import LobbyController from '../../../ctrl';
-import { variantsBlindMode, variants, variantsForGameType } from '../../../options';
-import { option } from './option';
+import { onInsert } from "common/snabbdom";
+import { h } from "snabbdom";
+import LobbyController from "../../../ctrl";
+import {
+  variantsBlindMode,
+  variants,
+  variantsForGameType,
+} from "../../../options";
+import { option } from "./option";
 
 export const variantPicker = (ctrl: LobbyController) => {
   const { trans, setupCtrl, opts } = ctrl;
   const baseVariants = opts.blindMode ? variantsBlindMode : variants;
-  return h('div.variant.label-select', [
-    h('label', { attrs: { for: 'sf_variant' } }, trans('variant')),
+  return h("div.variant.label-select", [
+    h("label", { attrs: { for: "sf_variant" } }, trans("variant")),
     h(
-      'select#sf_variant',
+      "select#sf_variant",
       {
         on: {
-          change: (e: Event) => {setupCtrl.variant((e.target as HTMLSelectElement).value as VariantKey);
-if (
-        ctrl.data.ratingMap !== null &&
-        setupCtrl.selectedPerf() !== null &&
-        ctrl.data.ratingMap[setupCtrl.selectedPerf()].prov
-      ) {
-        // Check if the rating is provisional
-        if (ctrl.data.ratingMap[setupCtrl.selectedPerf()].prov) {
-          setupCtrl.ratingMin(-500); // Set ratingMin to -500 for provisional rating
-		   setupCtrl.ratingMax(500);
-        }
-      } 
-},
+          change: (e: Event) => {
+            setupCtrl.variant(
+              (e.target as HTMLSelectElement).value as VariantKey
+            );
+            if (
+              ctrl.data.ratingMap !== null &&
+              setupCtrl.selectedPerf() !== null &&
+              ctrl.data.ratingMap[setupCtrl.selectedPerf()].prov
+            ) {
+              // Check if the rating is provisional
+              if (ctrl.data.ratingMap[setupCtrl.selectedPerf()].prov) {
+                setupCtrl.ratingMin(-500); // Set ratingMin to -500 for provisional rating
+                setupCtrl.ratingMax(500);
+              }
+            }
+          },
         },
-        hook: onInsert<HTMLSelectElement>(element => element.focus()),
+        hook: onInsert<HTMLSelectElement>((element) => element.focus()),
       },
-      variantsForGameType(baseVariants, setupCtrl.gameType!).map(variant => option(variant, setupCtrl.variant()))
+      variantsForGameType(baseVariants, setupCtrl.gameType!).map((variant) =>
+        option(variant, setupCtrl.variant())
+      )
     ),
   ]);
 };
