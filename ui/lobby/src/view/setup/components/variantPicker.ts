4b1a6dd3efcf42676e6ffbd111af41336e27fd42
Aditya Jadhav <adityaj2003@Adityas-MacBook-Air.local>
2023-05-26 07:09:23 -0700

    Hopefully resolves formatting issues

--- ui/lobby/src/view/setup/components/variantPicker.ts
+++ ui/lobby/src/view/setup/components/variantPicker.ts
@@ -1,26 +1,20 @@
-import { onInsert } from "common/snabbdom";
-import { h } from "snabbdom";
-import LobbyController from "../../../ctrl";
-import {
-  variantsBlindMode,
-  variants,
-  variantsForGameType,
-} from "../../../options";
-import { option } from "./option";
+import { onInsert } from 'common/snabbdom';
+import { h } from 'snabbdom';
+import LobbyController from '../../../ctrl';
+import { variantsBlindMode, variants, variantsForGameType } from '../../../options';
+import { option } from './option';
 
 export const variantPicker = (ctrl: LobbyController) => {
   const { trans, setupCtrl, opts } = ctrl;
   const baseVariants = opts.blindMode ? variantsBlindMode : variants;
-  return h("div.variant.label-select", [
-    h("label", { attrs: { for: "sf_variant" } }, trans("variant")),
+  return h('div.variant.label-select', [
+    h('label', { attrs: { for: 'sf_variant' } }, trans('variant')),
     h(
-      "select#sf_variant",
+      'select#sf_variant',
       {
         on: {
           change: (e: Event) => {
-            setupCtrl.variant(
-              (e.target as HTMLSelectElement).value as VariantKey
-            );
+            setupCtrl.variant((e.target as HTMLSelectElement).value as VariantKey);
             if (
               ctrl.data.ratingMap !== null &&
               setupCtrl.selectedPerf() !== null &&
@@ -34,11 +28,9 @@ export const variantPicker = (ctrl: LobbyController) => {
             }
           },
         },
-        hook: onInsert<HTMLSelectElement>((element) => element.focus()),
+        hook: onInsert<HTMLSelectElement>(element => element.focus()),
       },
-      variantsForGameType(baseVariants, setupCtrl.gameType!).map((variant) =>
-        option(variant, setupCtrl.variant())
-      )
+      variantsForGameType(baseVariants, setupCtrl.gameType!).map(variant => option(variant, setupCtrl.variant()))
     ),
   ]);
 };
