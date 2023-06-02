7238a96f4941392bb01a0a1b3a2bd0a7014f8053
Thibault Duplessis <t@lichess.org>
2023-06-02 19:29:39 +0200

    remove superfluous team battle tag attr

--- ui/tournament/src/view/battle.ts
+++ ui/tournament/src/view/battle.ts
@@ -121,7 +121,6 @@ function teamTr(ctrl: TournamentController, battle: TeamBattle, team: RankedTeam
           class: { top: i === 0 },
           attrs: {
             'data-href': '/@/' + p.user.name,
-            'data-name': p.user.name,
           },
           hook: {
             destroy: vnode => $.powerTip.destroy(vnode.elm as HTMLElement),
