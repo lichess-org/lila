1af7bf00d0f85743d3b3be61587ec5cc89db2ccd
Thibault Duplessis <t@lichess.org>
2023-06-07 19:09:38 +0200

    Revert "Revert "Update play-ahc-ws-standalone, ... to 2.2.0-M4""
    This reverts commit a9d4a3decc3dc3c3ae59524f7ed3d361b1c44389.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -75,7 +75,7 @@ object Dependencies {
   }
 
   object playWs {
-    val version = "2.2.0-M3"
+    val version = "2.2.0-M4"
     val ahc     = "com.typesafe.play" %% "play-ahc-ws-standalone"  % version
     val json    = "com.typesafe.play" %% "play-ws-standalone-json" % version
     val bundle  = Seq(ahc, json)
