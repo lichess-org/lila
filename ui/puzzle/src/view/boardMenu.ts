c6834a4901ec2d8928612ee5efabf4be05d7e396
Thibault Duplessis <t@lichess.org>
2023-06-08 11:37:56 +0200

    fix merge

--- ui/puzzle/src/view/boardMenu.ts
+++ ui/puzzle/src/view/boardMenu.ts
@@ -1,9 +1,9 @@
 import { h } from 'snabbdom';
-import { modal as menuModal } from 'board/menu';
+import { menu as menuDropdown } from 'board/menu';
 import PuzzleController from '../ctrl';
 
 export default function (ctrl: PuzzleController) {
- return menuDropdown(ctrl.trans, ctrl.redraw, ctrl.menu, menu => [
+  return menuDropdown(ctrl.trans, ctrl.redraw, ctrl.menu, menu => [
     h('section', [menu.flip(ctrl.trans.noarg('flipBoard'), ctrl.flipped(), ctrl.flip)]),
     h('section', [
       menu.zenMode(true),
