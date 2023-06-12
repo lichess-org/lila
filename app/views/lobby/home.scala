be6abce5d816e063ef5e1c303635f1ec910227ae
WandererXII <43043181+WandererXII@users.noreply.github.com>
2023-06-12 02:12:29 +0200

    Send data-count with counters
    UI expects it

--- app/views/lobby/home.scala
+++ app/views/lobby/home.scala
@@ -87,11 +87,23 @@ object home {
           ),
           div(cls := "lobby__counters")(
             ctx.blind option h2("Counters"),
-            a(id := "nb_connected_players", href := ctx.noBlind.option(langHref(routes.User.list)))(
-              trans.nbPlayers.plural(homepage.counters.members, strong(homepage.counters.members.localize))
+            a(
+              id   := "nb_connected_players",
+              href := ctx.noBlind.option(langHref(routes.User.list))
+            )(
+              trans.nbPlayers.plural(
+                homepage.counters.members,
+                strong(dataCount := homepage.counters.members)(homepage.counters.members.localize)
+              )
             ),
-            a(id := "nb_games_in_play", href := ctx.noBlind.option(langHref(routes.Tv.games)))(
-              trans.nbGamesInPlay.plural(homepage.counters.rounds, strong(homepage.counters.rounds.localize))
+            a(
+              id   := "nb_games_in_play",
+              href := ctx.noBlind.option(langHref(routes.Tv.games))
+            )(
+              trans.nbGamesInPlay.plural(
+                homepage.counters.rounds,
+                strong(dataCount := homepage.counters.rounds)(homepage.counters.rounds.localize)
+              )
             )
           )
         ),
