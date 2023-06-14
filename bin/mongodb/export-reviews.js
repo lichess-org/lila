7818c46507da31787bfe53d266eaf53f7c756f31
Thibault Duplessis <t@lichess.org>
2023-06-14 23:41:36 +0200

    export reviews as json

--- bin/mongodb/export-reviews.js
+++ bin/mongodb/export-reviews.js
@@ -1,6 +1,5 @@
-db.coach_review
-  .aggregate([{ $group: { _id: '$coachId', reviews: { $push: { from: '$userId', text: '$text', score: '$score' } } } }])
-  .forEach(coach => {
+const text = () =>
+  aggregate().forEach(coach => {
     print();
     print('===================================');
     print('https://lichess.org/coach/' + coach._id);
@@ -12,3 +11,12 @@ db.coach_review
       print('-----------------------------------');
     });
   });
+
+const json = () => printjson(aggregate().toArray());
+
+const aggregate = () =>
+  db.coach_review.aggregate([
+    { $group: { _id: '$coachId', reviews: { $push: { from: '$userId', text: '$text', score: '$score' } } } },
+  ]);
+
+json();
