5e8dfe15b47fb3563e6b65a4660298e6cd0a6f5e
Thibault Duplessis <t@lichess.org>
2023-06-21 17:21:48 +0200

    fix another Option.has bug. looks like it's not quite type safe yet
    closes #13068

--- modules/challenge/src/main/Challenge.scala
+++ modules/challenge/src/main/Challenge.scala
@@ -172,8 +172,9 @@ object Challenge:
     def apply(c: Color) = c.fold[ColorChoice](White, Black)
 
   case class Open(userIds: Option[(UserId, UserId)]):
-    def userIdList                    = userIds.map { (u1, u2) => List(u1, u2) }
-    def canJoin(using me: Option[Me]) = userIdList.fold(true)(ids => me.exists(ids.has))
+    def userIdList = userIds.map { (u1, u2) => List(u1, u2) }
+    def canJoin(using me: Option[Me]) =
+      userIdList.fold(true)(ids => me.exists(me => ids.exists(me is _)))
     def colorFor(requestedColor: Option[Color])(using me: Option[Me]): Option[ColorChoice] =
       userIds.fold(requestedColor.fold(ColorChoice.Random)(ColorChoice.apply).some): (u1, u2) =>
         me.flatMap: m =>
