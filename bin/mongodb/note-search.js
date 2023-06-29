3b523bd20121eda7b63519814e4f2b7b42867fb4
Thibault Duplessis <t@lichess.org>
2023-06-29 07:00:06 +0200

    don't search through auto appeal notes

--- bin/mongodb/note-search.js
+++ bin/mongodb/note-search.js
@@ -5,7 +5,18 @@ db.note
       mod: true,
       dox: { $ne: true },
       from: { $nin: ['watcherbot', 'lichess'] },
+      text: { $not: /^Appeal reply:/ },
     },
     { _id: 1 }
   )
   .forEach(n => db.note.updateOne(n, { $set: { s: true } }));
+
+db.note
+  .find(
+    {
+      s: true,
+      text: /^Appeal reply:/,
+    },
+    { _id: 1 }
+  )
+  .forEach(n => db.note.updateOne(n, { $unset: { s: true } }));
