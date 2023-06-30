546ac450b039cff6691a8ab6d395b93e84ed6c1a
WandererXII <43043181+WandererXII@users.noreply.github.com>
2023-06-30 22:03:03 +0200

    Fix timeago python script

--- bin/gen/timeago-to-scala.py
+++ bin/gen/timeago-to-scala.py
@@ -54,7 +54,7 @@ def main(args):
 
         if locale == "en_short":
             print("    // Hardcoded locale: en", file=sys.stderr)
-            print('''    "en" -> """lichess.timeagoLocale=function(s,n){return[["just now","right now"],["%s seconds ago","in %s seconds"],["1 minute ago","in 1 minute"],["%s minutes ago","in %s minutes"],["1 hour ago","in 1 hour"],["%s hours ago","in %s hours"],["1 day ago","in 1 day"],["%s days ago","in %s days"],["1 week ago","in 1 week"],["%s weeks ago","in %s weeks"],["1 month ago","in 1 month"],["%s months ago","in %s months"],["1 year ago","in 1 year"],["%s years ago","in %s years"]][n]};"""''', end="")
+            print('''    "en" -> """lishogi.timeagoLocale=function(s,n){return[["just now","right now"],["%s seconds ago","in %s seconds"],["1 minute ago","in 1 minute"],["%s minutes ago","in %s minutes"],["1 hour ago","in 1 hour"],["%s hours ago","in %s hours"],["1 day ago","in 1 day"],["%s days ago","in %s days"],["1 week ago","in 1 week"],["%s weeks ago","in %s weeks"],["1 month ago","in 1 month"],["%s months ago","in %s months"],["1 year ago","in 1 year"],["%s years ago","in %s years"]][n]};"""''', end="")
             continue
 
         print("    // {} -> {}".format(arg, locale), file=sys.stderr)
@@ -79,7 +79,7 @@ def terser(js):
 
 
 def preprocess(js):
-    return js.replace("export default function", "lichess.timeagoLocale=function");
+    return js.replace("export default function", "lishogi.timeagoLocale=function");
 
 def postprocess(js):
     return "(function(){" + js.strip() + "})()"
