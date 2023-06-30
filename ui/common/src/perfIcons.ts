68653bb14a79dd7bfad5c048268428b31a594fe1
WandererXII <43043181+WandererXII@users.noreply.github.com>
2023-06-30 22:12:48 +0200

    Fix and tweak getPerfIcon

--- ui/common/src/perfIcons.ts
+++ ui/common/src/perfIcons.ts
@@ -1,15 +1,16 @@
-export function getPerfIcon(key: Perf): string;
-export function getPerfIcon(key: string): string | undefined;
-export function getPerfIcon(key: string): string | undefined {
-  return perfIcons[key.toLowerCase()];
+export function getPerfIcon(key: VariantKey | Perf): string;
+export function getPerfIcon(key: string | undefined): string | undefined;
+export function getPerfIcon(key: string | undefined): string | undefined {
+  return perfIcons[(key || '').toLowerCase()];
 }
 
 const perfIcons: Record<string, string> = {
   blitz: ')',
-  ultraBullet: '{',
+  ultrabullet: '{',
   bullet: 'T',
   classical: '+',
   rapid: '#',
+  standard: 'C',
   minishogi: ',',
   chushogi: '(',
   annanshogi: '',
