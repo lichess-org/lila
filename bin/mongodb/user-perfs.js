7f32f4e2a3e558346fa21fc2f917819879aacb97
Thibault Duplessis <t@lichess.org>
2023-07-07 06:04:17 +0200

    faster user-perfs update

--- bin/mongodb/user-perfs.js
+++ bin/mongodb/user-perfs.js
@@ -1,8 +1,9 @@
 db.user4.aggregate([
-  { $match: { perfs: { $exists: true, $ne: {} } } },
+  { $match: { perfs: { $exists: true, $ne: {} }, seenAt: { $gt: new Date(Date.now() - 1000 * 3600) } } },
   { $project: { perfs: 1 } },
   {
     $replaceWith: { $mergeObjects: [{ _id: '$_id' }, '$perfs'] },
   },
-  { $out: 'user_perf' },
+  // { $out: 'user_perf' },
+  { $merge: { into: 'user_perf', on: '_id', whenMatched: 'replace', whenNotMatched: 'insert' } },
 ]);
