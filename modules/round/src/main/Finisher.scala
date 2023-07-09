be57092611d610fa88d7562524388a9a7c4d8134
Thanh Le <lenguyenthanh@hotmail.com>
2023-07-09 18:13:11 +0200

    Use new shiny tupled and mapN from ByColor

--- modules/round/src/main/Finisher.scala
+++ modules/round/src/main/Finisher.scala
@@ -143,7 +143,7 @@ final private class Finisher(
 
   private def updateCountAndPerfs(finish: FinishGame): Fu[Option[RatingDiffs]] =
     (!finish.isVsSelf && !finish.game.aborted).so:
-      (finish.white, finish.black).mapN((_, _)) so { (white, black) =>
+      finish.users.tupled.so { (white, black) =>
         crosstableApi.add(finish.game) zip perfsUpdater.save(finish.game, white, black) dmap (_._2)
       } zip
         (finish.white so incNbGames(finish.game)) zip
