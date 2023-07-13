477d1fd637e226d77bea32c40dd487f9fea234c9
Thibault Duplessis <t@lichess.org>
2023-07-13 10:42:59 +0200

    update user perfs migration script

--- bin/mongodb/user-perfs.js
+++ bin/mongodb/user-perfs.js
@@ -1,9 +1,9 @@
 db.user4.aggregate([
-  { $match: { perfs: { $exists: true, $ne: {} }, seenAt: { $gt: new Date(Date.now() - 1000 * 3600) } } },
+  { $match: { perfs: { $exists: true, $ne: {} } } },
   { $project: { perfs: 1 } },
   {
     $replaceWith: { $mergeObjects: [{ _id: '$_id' }, '$perfs'] },
   },
-  // { $out: 'user_perf' },
-  { $merge: { into: 'user_perf', on: '_id', whenMatched: 'replace', whenNotMatched: 'insert' } },
+  { $out: 'user_perf' },
 ]);
+db.user4.updateMany({ perfs: { $exists: 1 } }, { $unset: { perfs: 1 } });
