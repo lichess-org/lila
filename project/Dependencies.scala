078c238c6559deb4b40caf8867639732b636a945
Thanh Le <lenguyenthanh@hotmail.com>
2023-07-16 11:07:22 +0200

    Bump akkastream to 1.1.0-RC11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -59,7 +59,7 @@ object Dependencies {
 
   object reactivemongo {
     val driver = "org.reactivemongo" %% "reactivemongo"               % "1.1.0-RC9"
-    val stream = "org.reactivemongo" %% "reactivemongo-akkastream"    % "1.1.0-RC10"
+    val stream = "org.reactivemongo" %% "reactivemongo-akkastream"    % "1.1.0-RC11"
     val shaded = "org.reactivemongo"  % "reactivemongo-shaded-native" % s"1.1.0-RC6-$os-x86-64"
     // val kamon  = "org.reactivemongo" %% "reactivemongo-kamon"         % "1.0.8"
     def bundle = Seq(driver, stream)
