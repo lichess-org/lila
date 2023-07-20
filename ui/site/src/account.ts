5bd1e61ef3900ae6aee74a45cacaad00fcb0a88c
Thibault Duplessis <t@lichess.org>
2023-07-20 10:01:07 +0200

    compute bit inputs on form load - for #13244

--- ui/site/src/account.ts
+++ ui/site/src/account.ts
@@ -20,8 +20,9 @@ lichess.load.then(() => {
     const form = this,
       $form = $(form),
       showSaved = () => $form.find('.saved').removeClass('none');
+    computeBitChoices($form, 'behavior.submitMove');
     $form.find('input').on('change', function (this: HTMLInputElement) {
-      submitMoveChoices('behavior.submitMove');
+      computeBitChoices($form, 'behavior.submitMove');
       localPrefs.forEach(([categ, name, storeKey]) => {
         if (this.name == `${categ}.${name}`) {
           lichess.storage.boolean(storeKey).set(this.value == '1');
@@ -60,10 +61,10 @@ lichess.load.then(() => {
   });
 });
 
-function submitMoveChoices(name: string) {
+function computeBitChoices($form: Cash, name: string) {
   let sum = 0;
-  $(`input[type="checkbox"][data-name="${name}"]:checked`).each(function (this: HTMLInputElement) {
+  $form.find(`input[type="checkbox"][data-name="${name}"]:checked`).each(function (this: HTMLInputElement) {
     sum |= parseInt(this.value);
   });
-  $(`input[type="hidden"][name="${name}"]`).val(sum.toString());
+  $form.find(`input[type="hidden"][name="${name}"]`).val(sum.toString());
 }
