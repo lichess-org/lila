50bb0cecd0154503bd522a0156243ee25560b95d
Thibault Duplessis <t@lichess.org>
2023-07-22 08:48:06 +0200

    memoize often called isIOS function

--- ui/common/src/mobile.ts
+++ ui/common/src/mobile.ts
@@ -43,7 +43,8 @@ export const isMobile = (): boolean => isAndroid() || isIOS();
 
 export const isAndroid = (): boolean => /Android/.test(navigator.userAgent);
 
-export const isIOS = (): boolean => /iPhone|iPod/.test(navigator.userAgent) || isIPad();
+export const isIOS = memoize<boolean>(() => /iPhone|iPod/.test(navigator.userAgent) || isIPad());
+
 export const isSafariUnder = (version: number) => {
   if (!isIOS()) return false;
   return parseFloat(navigator.userAgent.slice(navigator.userAgent.indexOf('Version/') + 8)) < version;
