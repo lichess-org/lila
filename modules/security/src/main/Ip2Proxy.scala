bf65639b8b42ed9028dddf9535b549d18885dcbc
Thibault Duplessis <t@lichess.org>
2023-07-23 09:37:04 +0200

    Revert "disallow public proxies"
    This reverts commit 50b9af0002ae5da4ab9e8a77689ccd1bfe2767df.

--- modules/security/src/main/Ip2Proxy.scala
+++ modules/security/src/main/Ip2Proxy.scala
@@ -15,9 +15,7 @@ trait Ip2Proxy:
 
 opaque type IsProxy = Option[String]
 object IsProxy extends TotalWrapper[IsProxy, Option[String]]:
-  extension (a: IsProxy)
-    def is  = a.value.isDefined
-    def pub = a.value.has("PUB")
+  extension (a: IsProxy) def is           = a.value.isDefined
   def unapply(a: IsProxy): Option[String] = a.value
 
 final class Ip2ProxySkip extends Ip2Proxy:
