bc5f9bd216587c957280ed83ef16c4798f325bf2
raulriverarojas <72164424+raulriverarojas@users.noreply.github.com>
2023-08-22 14:57:18 -0400

    add 960 to board editor

--- ui/editor/src/view.ts
+++ ui/editor/src/view.ts
@@ -72,7 +72,7 @@ function variant2option(key: Rules, name: string, ctrl: EditorCtrl): VNode {
     {
       attrs: {
         value: key,
-        selected: key == ctrl.rules,
+        selected: key == ctrl.rules && name==ctrl.variant,
       },
     },
     `${ctrl.trans.noarg('variant')} | ${name}`,
@@ -81,6 +81,7 @@ function variant2option(key: Rules, name: string, ctrl: EditorCtrl): VNode {
 
 const allVariants: Array<[Rules, string]> = [
   ['chess', 'Standard'],
+  ['chess', 'Chess 960'],
   ['antichess', 'Antichess'],
   ['atomic', 'Atomic'],
   ['crazyhouse', 'Crazyhouse'],
@@ -222,7 +223,9 @@ function controls(ctrl: EditorCtrl, state: EditorState): VNode {
                 attrs: { id: 'variants' },
                 on: {
                   change(e) {
-                    ctrl.setRules((e.target as HTMLSelectElement).value as Rules);
+                    const option=(e.target as HTMLSelectElement).item((e.target as HTMLSelectElement).selectedIndex);
+                    if(option)ctrl.setVariant(option.text.slice(10));
+                    ctrl.setRules((e.target as HTMLSelectElement).value as Rules);                                     
                   },
                 },
               },
