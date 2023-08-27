36ef074d439029d6334e34980d30436cc29039fc
Trevor Bayless <trevorbayless1@gmail.com>
2023-08-27 14:01:45 -0500

    Fix toggling zen mode via board menu

--- ui/round/src/view/boardMenu.ts
+++ ui/round/src/view/boardMenu.ts
@@ -17,7 +17,7 @@ export default function (ctrl: RoundController) {
         }),
       ]),
       h('section', [
-        menu.zenMode(!spectator),
+        menu.zenMode(ctrl.zenable),
         menu.voiceInput(boolPrefXhrToggle('voice', !!ctrl.voiceMove), !spectator),
         menu.keyboardInput(boolPrefXhrToggle('keyboardMove', !!ctrl.keyboardMove), !spectator),
         !spectator && d.pref.submitMove ? menu.confirmMove(ctrl.confirmMoveEnabled) : undefined,
