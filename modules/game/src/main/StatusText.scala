9138aa56d6969036b8fdc970860a37e6ff4c8556
Niklas Fiekas <niklas.fiekas@backscattering.de>
2023-08-30 09:33:44 +0200

    Fix NoStart PGN termination comment (fixes lichess-org/pgn-viewer#26)

--- modules/game/src/main/StatusText.scala
+++ modules/game/src/main/StatusText.scala
@@ -20,7 +20,7 @@ object StatusText:
         win match
           case Some(value) => s"${value} wins on time."
           case None        => "Draw by time and insufficient material."
-      case NoStart => s"${loser(win)} wins by forfeit."
+      case NoStart => s"${winner(win)} wins by forfeit."
       case Cheat   => "Cheat detected."
       case VariantEnd =>
         variant match
