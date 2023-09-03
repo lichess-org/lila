2ebdb85c1aaa63aa50454b081115d6a91d333ffb
Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>
2023-09-03 08:41:36 +0700

    Update bits.scala

--- app/views/user/bits.scala
+++ app/views/user/bits.scala
@@ -22,8 +22,8 @@ object bits:
       a(cls := active.active("tournament"), href := routes.Tournament.leaderboard)(
         trans.tournamentWinners()
       ),
-      a(cls := active.active("shield"), href := routes.Tournament.shields)("Shields"),
-      a(cls := active.active("bots"), href := routes.PlayApi.botOnline)("Online bots")
+      a(cls := active.active("shield"), href := routes.Tournament.shields)(trans.shield()),
+      a(cls := active.active("bots"), href := routes.PlayApi.botOnline)(trans.onlineBot())
     )
 
   def miniClosed(u: User)(using Context) =
