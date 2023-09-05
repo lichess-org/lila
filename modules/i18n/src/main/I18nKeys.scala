c75ffdfd406a125032beb74174ee8cd537a6b47f
Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>
2023-09-05 19:49:49 +0700

    Update I18nKeys.scala

--- modules/i18n/src/main/I18nKeys.scala
+++ modules/i18n/src/main/I18nKeys.scala
@@ -946,7 +946,7 @@ object I18nKeys:
     val `pawnsMoveForward` = I18nKey("learn:pawnsMoveForward")
     val `captureThenPromote` = I18nKey("learn:captureThenPromote")
     val `useAllThePawns` = I18nKey("learn:useAllThePawns")
-    val `aPawnOnTheSecondRank` = I18nKey("learn:aPawnOnTheSecondRank")
+    val `aPawnOnTheSecondAndSevenRank` = I18nKey("learn:aPawnOnTheSecondAndSevenRank")
     val `grabAllTheStarsNoNeedToPromote` = I18nKey("learn:grabAllTheStarsNoNeedToPromote")
     val `pawnComplete` = I18nKey("learn:pawnComplete")
     val `pawnPromotion` = I18nKey("learn:pawnPromotion")
