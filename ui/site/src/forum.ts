lichess-org/lila

commit db1b01ac8c24092733fb5e6ba02eaaffe403473a

refactor dialogs

ui/site/src/forum.ts
@@ -1,23 +1,20 @@
 import * as xhr from 'common/xhr';
-import modal from 'common/modal';
+import { domDialog } from 'common/dialog';
 
 lichess.load.then(() => {
   $('.forum')
     .on('click', 'a.delete', function (this: HTMLAnchorElement) {
       const link = this;
-      modal({
-        content: $('.forum-delete-modal'),
-        onInsert($wrap) {
-          $wrap
-            .find('form')
-            .attr('action', link.href)
-            .on('submit', function (this: HTMLFormElement, e: Event) {
-              e.preventDefault();
-              xhr.formToXhr(this);
-              modal.close();
-              $(link).closest('.forum-post').hide();
-            });
-        },
+      domDialog({
+        cash: $('.forum-delete-modal'),
+        attrs: { view: { action: link.href } },
+      }).then(dlg => {
+        $('form', dlg.view).on('submit', () => {
+          //e.preventDefault();
+          xhr.text(link.href, { method: 'post' });
+          $(link).closest('.forum-post').hide();
+        });
+        dlg.showModal();
       });
       return false;
     })

commit 2586975922d116fa9fe020485460a2281dbe7048

fix forum deletion submitting and canceling from the dialog
broken by https://github.com/lichess-org/lila/commit/db1b01ac8c24092733fb5e6ba02eaaffe403473a#diff-9bd5d13516bc65ded5bbecc2aad2e033c396736eda77b32b6

ui/site/src/forum.ts
@@ -9,11 +9,16 @@ lichess.load.then(() => {
         cash: $('.forum-delete-modal'),
         attrs: { view: { action: link.href } },
       }).then(dlg => {
-        $('form', dlg.view).on('submit', () => {
-          //e.preventDefault();
-          xhr.text(link.href, { method: 'post' });
-          $(link).closest('.forum-post').hide();
-        });
+        $(dlg.view)
+          .find('form')
+          .attr('action', link.href)
+          .on('submit', function (this: HTMLFormElement, e: Event) {
+            e.preventDefault();
+            xhr.formToXhr(this);
+            $(link).closest('.forum-post').hide();
+            dlg.close();
+          });
+        $(dlg.view).find('form button.cancel').on('click', dlg.close);
         dlg.showModal();
       });
       return false;