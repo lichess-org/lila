45853fc4f47ba23ebb7d1bc663a977a8782e6873
Thibault Duplessis <t@lichess.org>
2023-10-04 13:42:36 +0200

    simplify and reformat TeamApi.kick

--- modules/team/src/main/TeamApi.scala
+++ modules/team/src/main/TeamApi.scala
@@ -275,24 +275,19 @@ final class TeamApi(
     }
 
   def kick(team: Team, userId: UserId)(using me: Me): Funit =
-    (userId != team.createdBy) so {
-
-      //create a request to set declined in order to prevent kicked use to rejoin
+    (userId != team.createdBy).so:
+      // create a request to set declined in order to prevent kicked use to rejoin
       val request = Request.make(
         team = team.id,
         user = userId,
-        message = "kicked from team",
+        message = "Kicked from team",
         declined = true
       )
-
-      requestRepo.coll.insert.one(request).void andDo cached.nbRequests.invalidate(team.createdBy)
-
-      quit(team, userId) >>
-        (!team.leaders(me)).so {
-          modLog.teamKick(userId, team.name)
-        } andDo
-        Bus.publish(KickFromTeam(teamId = team.id, userId = userId), "teamLeave")
-    }
+      for
+        _ <- requestRepo.coll.insert.one(request)
+        _ <- quit(team, userId)
+        _ <- !team.leaders(me) so modLog.teamKick(userId, team.name)
+      yield Bus.publish(KickFromTeam(teamId = team.id, userId = userId), "teamLeave")
 
   def kickMembers(team: Team, json: String)(using me: Me, req: RequestHeader): Funit =
     val users  = parseTagifyInput(json).toList
