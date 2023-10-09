lichess-org/lila

commit d9cb18bdcd12ac2b40ba950559c62e348f763712

WIP streamer notify prefs scala3

app/views/account/notification.scala
@@ -1,0 +1,66 @@
+package views.html
+package account
+
+import lila.api.{ Context, given }
+import lila.app.templating.Environment.{ given, * }
+import lila.app.ui.ScalatagsTemplate.{ *, given }
+
+object notification {
+  import bits.*
+  import trans.preferences.*
+
+  def fieldSet(form: play.api.data.Form[_], inactive: Boolean)(using ctx: Context) =
+    div(cls := List("none" -> inactive))(
+      table(cls := "allows")(
+        thead(
+          tr(th, th(notifyBell()), th(notifyPush()))
+        ),
+        tbody(
+          makeRow(form, notifyStreamStart(), "streamStart"),
+          makeRow(form, notifyForumMention(), "mention"),
+          makeRow(form, notifyInvitedStudy(), "invitedStudy"),
+          makeRow(form, notifyInboxMsg(), "privateMessage"),
+          makeRow(form, notifyChallenge(), "challenge"),
+          makeRow(form, notifyTournamentSoon(), "tournamentSoon"),
+          makeRow(form, notifyGameEvent(), "gameEvent")
+        )
+      ),
+      setting(
+        correspondenceEmailNotification(),
+        radios(form("notification.correspondenceEmail"), translatedBooleanIntChoices)
+      )
+    )
+
+  private def makeRow(form: play.api.data.Form[_], transTxt: Frag, filterName: String) =
+    tr(
+      td(transTxt),
+      Seq("bell", "push") map { allow =>
+        val name    = s"notification.$filterName.$allow"
+        val checked = form.data(name).contains("true")
+        td(
+          if (editable(s"$filterName.$allow"))
+            div(cls := "toggle", form3.cmnToggle(name, name, checked))
+          else if (!checked)
+            div(iconTag('\ue03f'))
+          else
+            div(
+              cls := "always-on",
+              form3.hidden(name, "true"),
+              filterName match {
+                case "challenge"      => iconTag('\ue048')
+                case "privateMessage" => iconTag('\ue00f')
+                case _                => emptyFrag
+              }
+            )
+        )
+      }
+    )
+
+  private def editable(name: String) = name match {
+    case "privateMessage.bell" => false
+    case "tournamentSoon.bell" => false
+    case "gameEvent.bell"      => false
+    case "challenge.bell"      => false
+    case _                     => true
+  }
+}

commit 1f50a9f5f3323818fa6944b0a62fcbca08f6b740

Fix unsubscribe link in correspondence notification emails
was broken since d9cb18bdcd12ac2b40ba950559c62e348f763712

For now the `id` link is a bit overkill, but I think it's a nice thing to have, and future proof in case the notification page grows longer

app/views/account/notification.scala
@@ -38,9 +38,13 @@ object notification:
                 ).map(makeRow(form))
               )
             ),
-            setting(
-              correspondenceEmailNotification(),
-              radios(form("correspondenceEmail"), translatedBooleanChoices)
+            div(
+              id := "correspondence-email-notif"
+            )( // id is set to allow direct unsubcribe link in correspondence emails
+              setting(
+                correspondenceEmailNotification(),
+                radios(form("correspondenceEmail"), translatedBooleanChoices)
+              )
             ),
             setting(
               bellNotificationSound(),

modules/mailer/src/main/AutomaticEmail.scala
@@ -183,7 +183,7 @@ To make a new donation, head to $baseUrl/patron"""
               "Hello and thank you for playing correspondence chess on Lichess!"
             val disableSettingNotice =
               "You are receiving this email because you have correspondence email notification turned on. You can turn it off in your settings:"
-            val disableLink = s"$baseUrl/account/preferences/game-behavior#correspondence-email-notif"
+            val disableLink = s"$baseUrl/account/preferences/notification#correspondence-email-notif"
             mailer send Mailer.Message(
               to = email,
               subject = "Daily correspondence notice",