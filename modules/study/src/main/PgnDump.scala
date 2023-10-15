d49d2ce736ad41a0f46b48ae879555989725316d
Sérgio Glórias <9739913+SergioGlorias@users.noreply.github.com>
2023-10-16 00:15:55 +0100

    remove tag site and reverse source

--- modules/study/src/main/PgnDump.scala
+++ modules/study/src/main/PgnDump.scala
@@ -65,7 +65,6 @@ final class PgnDump(
       val opening = chapter.opening
       val genTags = List(
         Tag(_.Event, s"${study.name}: ${chapter.name}"),
-        Tag(_.Site, chapterUrl(study.id, chapter.id)),
         Tag(_.UTCDate, Tag.UTCDate.format.print(chapter.createdAt)),
         Tag(_.UTCTime, Tag.UTCTime.format.print(chapter.createdAt)),
         Tag(_.Variant, chapter.setup.variant.name.capitalize),
@@ -79,7 +78,7 @@ final class PgnDump(
           Tag("SetUp", "1")
         )
       ) :::
-        flags.source.so(List(Tag("Source", chapterUrl(study.id, chapter.id)))) :::
+        (!flags.source).so(List(Tag("Source", chapterUrl(study.id, chapter.id)))) :::
         flags.orientation.so(List(Tag("Orientation", chapter.setup.orientation.name))) :::
         chapter.isGamebook.so(List(Tag("ChapterMode", "gamebook")))
       genTags
