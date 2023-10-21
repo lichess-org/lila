599686cc461b705660152626c40ecb40bf9f8200
Thibault Duplessis <t@lichess.org>
2023-10-21 11:38:55 +0200

    reposition lobby timeline with JS, without FOUC or layout shifting

--- ui/lobby/src/main.ts
+++ ui/lobby/src/main.ts
@@ -20,5 +20,24 @@ export default function main(opts: LobbyOpts) {
     tableVNode = patch(tableVNode, tableView(ctrl));
   }
 
+  lichess.requestIdleCallback(() => {
+    layoutHacks();
+    window.addEventListener('resize', layoutHacks);
+  });
+
   return ctrl;
 }
+
+let cols = 0;
+
+const layoutHacks = () =>
+  requestAnimationFrame(() => {
+    $('main.lobby').each(function (this: HTMLElement) {
+      const newCols = Number(window.getComputedStyle(this).getPropertyValue('--cols'));
+      if (newCols != cols) {
+        cols = newCols;
+        if (cols > 2) $('.lobby .lobby__timeline').appendTo('.lobby__side');
+        else $('.lobby__side .lobby__timeline').appendTo('.lobby');
+      }
+    });
+  });
