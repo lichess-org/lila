681038c6b7a47a460257eb8ca8cc58fbbe5028a0
zamfofex <zamfofex@twdb.moe>
2023-10-21 21:45:10 +0000

    make small improvements to the lobby

--- ui/lobby/src/view/main.ts
+++ ui/lobby/src/view/main.ts
@@ -1,4 +1,4 @@
-import { h, VNodeData } from 'snabbdom';
+import { h, VNodeData, thunk } from 'snabbdom';
 import { spinnerVdom as spinner } from 'common/spinner';
 import renderTabs from './tabs';
 import * as renderPools from './pools';
@@ -6,6 +6,9 @@ import renderRealTime from './realTime/main';
 import renderSeeks from './correspondence';
 import renderPlaying from './playing';
 import LobbyController from '../ctrl';
+import { onInsert } from 'common/snabbdom';
+import renderSetupModal from './setup/modal';
+import { numberFormat } from 'common/number';
 
 export default function (ctrl: LobbyController) {
   let body,
@@ -27,8 +30,56 @@ export default function (ctrl: LobbyController) {
         body = renderPlaying(ctrl);
         break;
     }
+  const { trans } = ctrl;
+  const { members, rounds } = ctrl.data.counters;
   return h('div.lobby__app.lobby__app-' + ctrl.tab, [
     h('div.tabs-horiz', { attrs: { role: 'tablist' } }, renderTabs(ctrl)),
     h('div.lobby__app__content.l' + (ctrl.redirecting ? 'redir' : ctrl.tab), data, body),
+    renderSetupModal(ctrl),
+    // Use a thunk here so that snabbdom does not rerender; we will do so manually after insert
+    thunk(
+      'div.lobby__counters',
+      () =>
+        h('div.lobby__counters', [
+          lichess.blindMode ? h('h2', 'Counters') : null,
+          h(
+            'a',
+            { attrs: lichess.blindMode ? {} : { href: '/player' } },
+            trans.vdomPlural(
+              'nbPlayers',
+              members,
+              h(
+                'strong',
+                {
+                  attrs: { 'data-count': members },
+                  hook: onInsert<HTMLAnchorElement>(elm => {
+                    ctrl.spreadPlayersNumber = ctrl.initNumberSpreader(elm, 10, members);
+                  }),
+                },
+                numberFormat(members),
+              ),
+            ),
+          ),
+          h(
+            'a',
+            lichess.blindMode ? {} : { attrs: { href: '/games' } },
+            trans.vdomPlural(
+              'nbGamesInPlay',
+              rounds,
+              h(
+                'strong',
+                {
+                  attrs: { 'data-count': rounds },
+                  hook: onInsert<HTMLAnchorElement>(elm => {
+                    ctrl.spreadGamesNumber = ctrl.initNumberSpreader(elm, 8, rounds);
+                  }),
+                },
+                numberFormat(rounds),
+              ),
+            ),
+          ),
+        ]),
+      [],
+    ),
   ]);
 }
--- ui/lobby/src/view/table.ts
+++ ui/lobby/src/view/table.ts
@@ -1,16 +1,13 @@
-import { h, thunk } from 'snabbdom';
-import { bind, onInsert } from 'common/snabbdom';
+import { h } from 'snabbdom';
+import { bind } from 'common/snabbdom';
 import LobbyController from '../ctrl';
 import { GameType } from '../interfaces';
-import renderSetupModal from './setup/modal';
-import { numberFormat } from 'common/number';
 
 export default function table(ctrl: LobbyController) {
-  const { data, trans, opts } = ctrl;
+  const { trans, opts } = ctrl;
   const hasOngoingRealTimeGame = ctrl.hasOngoingRealTimeGame();
   const hookDisabled =
     opts.playban || opts.hasUnreadLichessMessage || ctrl.me?.isBot || hasOngoingRealTimeGame;
-  const { members, rounds } = data.counters;
   return h('div.lobby__table', [
     h('div.bg-switch', { attrs: { title: 'Dark mode' } }, [
       h('div.bg-switch__track'),
@@ -41,52 +38,6 @@ export default function table(ctrl: LobbyController) {
           ),
         ),
       ),
-    ),
-    renderSetupModal(ctrl),
-    // Use a thunk here so that snabbdom does not rerender; we will do so manually after insert
-    thunk(
-      'div.lobby__counters',
-      () =>
-        h('div.lobby__counters', [
-          lichess.blindMode ? h('h2', 'Counters') : null,
-          h(
-            'a',
-            { attrs: lichess.blindMode ? {} : { href: '/player' } },
-            trans.vdomPlural(
-              'nbPlayers',
-              members,
-              h(
-                'strong',
-                {
-                  attrs: { 'data-count': members },
-                  hook: onInsert<HTMLAnchorElement>(elm => {
-                    ctrl.spreadPlayersNumber = ctrl.initNumberSpreader(elm, 10, members);
-                  }),
-                },
-                numberFormat(members),
-              ),
-            ),
-          ),
-          h(
-            'a',
-            lichess.blindMode ? {} : { attrs: { href: '/games' } },
-            trans.vdomPlural(
-              'nbGamesInPlay',
-              rounds,
-              h(
-                'strong',
-                {
-                  attrs: { 'data-count': rounds },
-                  hook: onInsert<HTMLAnchorElement>(elm => {
-                    ctrl.spreadGamesNumber = ctrl.initNumberSpreader(elm, 8, rounds);
-                  }),
-                },
-                numberFormat(rounds),
-              ),
-            ),
-          ),
-        ]),
-      [],
     ),
   ]);
 }
