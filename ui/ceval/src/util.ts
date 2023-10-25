c6b5267c26a16b40f836260fa0d837f7f46e2be5
Jonathan Gamble <gamblej@gmail.com>
2023-10-25 15:45:15 -0500

    always try lo==hi when not growable

--- ui/ceval/src/util.ts
+++ ui/ceval/src/util.ts
@@ -24,13 +24,13 @@ export const pow2floor = (n: number) => {
 };
 
 export const sharedWasmMemory = (lo: number, hi = 32767): WebAssembly.Memory => {
-  let shrink = 4;
+  let shrink = 4; // 32767 -> 24576 -> 16384 -> 12288 -> 8192 -> 6144 -> etc
   while (true) {
     try {
       return new WebAssembly.Memory({ shared: true, initial: lo, maximum: hi });
     } catch (e) {
       if (hi <= lo || !(e instanceof RangeError)) throw e;
-      hi = Math.ceil(hi - hi / shrink);
+      hi = Math.max(lo, Math.ceil(hi - hi / shrink));
       shrink = shrink === 4 ? 3 : 4;
     }
   }
