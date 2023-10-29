66c996f3c10c4ec1281d2088a05686d51d090c6a
Thibault Duplessis <t@lichess.org>
2023-10-29 10:06:52 +0100

    upgrade play

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -66,8 +66,8 @@ object Dependencies {
   }
 
   object play {
-    val playVersion = "2.8.18-lila_3.16"
-    val json        = "com.typesafe.play" %% "play-json"         % "2.10.2"
+    val playVersion = "2.8.18-lila_3.18"
+    val json        = "org.playframework" %% "play-json"         % "3.0.0"
     val api         = "com.typesafe.play" %% "play"              % playVersion
     val server      = "com.typesafe.play" %% "play-server"       % playVersion
     val netty       = "com.typesafe.play" %% "play-netty-server" % playVersion
