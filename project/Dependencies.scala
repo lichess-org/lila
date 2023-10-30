ab53e1228d23541bde8e2e43f8d7bad3dd0c2b44
Scala Steward <scala_steward@virtuslab.com>
2023-10-30 16:31:47 +0000

    Update play-mailer to 10.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -72,7 +72,7 @@ object Dependencies {
     val server      = "com.typesafe.play" %% "play-server"       % playVersion
     val netty       = "com.typesafe.play" %% "play-netty-server" % playVersion
     val logback     = "com.typesafe.play" %% "play-logback"      % playVersion
-    val mailer      = "com.typesafe.play" %% "play-mailer"       % "9.0.0"
+    val mailer      = "org.playframework" %% "play-mailer"       % "10.0.0"
   }
 
   object playWs {
