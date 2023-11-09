737befab6cf59067fab662be209455ffa8db1dff
Thibault Duplessis <t@lichess.org>
2023-11-09 16:36:02 +0100

    new PGN export patron tags, optional

--- modules/study/src/main/StudyMaker.scala
+++ modules/study/src/main/StudyMaker.scala
@@ -57,7 +57,14 @@ final private class StudyMaker(
   ): Fu[Study.WithChapter] = {
     for
       root <- chapterMaker.makeRoot(pov.game, data.form.pgnStr, initialFen)
-      tags <- pgnDump.tags(pov.game, initialFen, none, withOpening = true, withRatings)
+      tags <- pgnDump.tags(
+        pov.game,
+        initialFen,
+        none,
+        withOpening = true,
+        withRating = withRatings,
+        withPatron = false
+      )
       name <- StudyChapterName from Namer.gameVsText(pov.game, withRatings)(using lightUserApi.async)
       study = Study.make(user, Study.From.Game(pov.gameId), data.id, StudyName("Game study").some)
       chapter = Chapter.make(
