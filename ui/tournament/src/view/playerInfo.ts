81627099b3be683561bf33ca42aa38616006b04c
Sammy Humphreys <sammyh2009@outlook.com>
2023-11-12 20:52:09 +0000

    Moved beserk icon after rating

--- ui/tournament/src/view/playerInfo.ts
+++ ui/tournament/src/view/playerInfo.ts
@@ -103,10 +103,11 @@ export default function (ctrl: TournamentController): VNode {
               [
                 h('th', '' + (Math.max(nb.game, pairingsLen) - i)),
                 h('td', playerName(p.op)),
+                
+                ctrl.opts.showRatings ? h('td', p.op.rating) : null,
                 h('td.berserk', [
                   p.op.berserk ? h('i', { attrs: { 'data-icon': licon.Berserk, title: 'Berserk' } }) : null,
                 ]),
-                ctrl.opts.showRatings ? h('td', p.op.rating) : null,
                 h('td.is.color-icon.' + p.color),
                 h('td' + '.result.', res),
                 h('td.berserk', [
