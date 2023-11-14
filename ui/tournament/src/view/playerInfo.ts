0a71ab884c085b461a56aa54fb506c7ad2af8f9b
Sammy Humphreys <sammyh2009@outlook.com>
2023-11-14 15:41:38 +0000

    Use $c-font-dim and fixed bug with beserk on front-end
    Uses $c-font-dim to display beserk status now instead of $c-accent.
    Fixed bug where player beserk status was shown inseas of opponent

--- ui/tournament/src/view/playerInfo.ts
+++ ui/tournament/src/view/playerInfo.ts
@@ -104,7 +104,7 @@ export default function (ctrl: TournamentController): VNode {
                 h('th', '' + (Math.max(nb.game, pairingsLen) - i)),
                 h('td', playerName(p.op)),
                 ctrl.opts.showRatings ? h('td', p.op.rating) : null,
-                p.berserk
+                p.op.berserk
                   ? h('td.berserk', { attrs: { 'data-icon': licon.Berserk, title: 'Berserk' } })
                   : h('td.berserk'),
                 h('td.is.color-icon.' + p.color),
