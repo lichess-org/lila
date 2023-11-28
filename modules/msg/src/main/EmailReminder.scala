484a7c0033cb83d429eeefae26acda7c1db1e5d2
Thibault Duplessis <t@lichess.org>
2023-11-28 10:36:40 +0100

    tweak email remind wording; no, you're not (that) old

--- modules/msg/src/main/EmailReminder.scala
+++ modules/msg/src/main/EmailReminder.scala
@@ -12,12 +12,12 @@ final class EmailReminder(mongoCache: MongoCache.Api, userRepo: UserRepo, api: M
 
   private val emailReminderMsg = s"""No email associated with your account
 
-Hello, as you are one of lichess oldest users, no email was required when you registered.
+Hello, as you have an early Lichess account, no email was required when you registered.
 
 However this makes it easy for you to lose access to your account.
-If you forget your password, if your password is leaked from another website, or if we decide that your password is too easy-to-guess to be secure, your account will be lost.
+If you forget your password, or if your password is leaked from another website, or if we decide that your password is too easy-to-guess to be secure, your account will be lost.
 
-You can visit $baseUrl/account/email to set your personal email address."""
+You can visit $baseUrl/account/email to set your personal email address. That way, you'll be able to reset your password when needed."""
 
   private val cache = mongoCache[UserId, Boolean](1024, "security:email:reminder", 10 days, _.value):
     loader =>
