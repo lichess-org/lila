75abdb28046c92cb4d33697c6d217ece67064cd8
Allan Joseph <allanjoseph98@gmail.com>
2023-12-01 14:39:19 +0530

    fix chart anim weirding out on window resize

--- ui/chart/src/common.ts
+++ ui/chart/src/common.ts
@@ -94,7 +94,7 @@ export function animation(duration: number): ChartOptions<'line'>['animations'] 
       easing: 'easeOutQuad',
       duration: duration,
       from: NaN, // the point is initially skipped
-      delay: ctx => ctx.dataIndex * duration,
+      delay: ctx => (ctx.mode == 'resize' ? 0 : ctx.dataIndex * duration),
     },
     y: {
       type: 'number',
@@ -104,7 +104,7 @@ export function animation(duration: number): ChartOptions<'line'>['animations'] 
         !ctx.dataIndex
           ? ctx.chart.scales.y.getPixelForValue(100)
           : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.dataIndex - 1].getProps(['y'], true).y,
-      delay: ctx => ctx.dataIndex * duration,
+      delay: ctx => (ctx.mode == 'resize' ? 0 : ctx.dataIndex * duration),
     },
   };
 }
