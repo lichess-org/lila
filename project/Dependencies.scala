00b1375de6bfe5c0a099d0e295326de3ab4d79a0
Scala Steward <scala_steward@virtuslab.com>
2023-12-09 17:11:10 +0000

    Update play-ahc-ws-standalone, ... to 3.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -77,9 +77,9 @@ object Dependencies {
   }
 
   object playWs {
-    val version = "2.2.5"
-    val ahc     = "com.typesafe.play" %% "play-ahc-ws-standalone"  % version
-    val json    = "com.typesafe.play" %% "play-ws-standalone-json" % version
+    val version = "3.0.1"
+    val ahc     = "org.playframework" %% "play-ahc-ws-standalone"  % version
+    val json    = "org.playframework" %% "play-ws-standalone-json" % version
     val bundle  = Seq(ahc, json)
   }
 
