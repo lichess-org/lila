906b0ab24e51417970663f1ec80bd6f43c9ba0c4
Allan Joseph <allanjoseph98@gmail.com>
2023-12-19 05:33:13 +0530

    Rating-history performance improvements

--- ui/chart/src/ratingHistory.ts
+++ ui/chart/src/ratingHistory.ts
@@ -28,7 +28,6 @@ type TsAndRating = { ts: number; rating: number };
 
 type ChartPerf = {
   color: string;
-  borderDash: number[] | undefined;
   symbol: PointStyle;
   name: string;
 };
@@ -38,32 +37,30 @@ Chart.defaults.font = fontFamily();
 
 // order from RatingChartApi
 const styles: ChartPerf[] = [
-  { color: '#56B4E9', borderDash: undefined, symbol: 'circle', name: 'Bullet' },
-  { color: '#0072B2', borderDash: undefined, symbol: 'rectRot', name: 'Blitz' },
-  { color: '#009E73', borderDash: undefined, symbol: 'rect', name: 'Rapid' },
-  { color: '#009E73', borderDash: undefined, symbol: 'triangle', name: 'Classical' },
-  { color: '#F0E442', borderDash: [3], symbol: 'triangle', name: 'Correspondence' },
-  { color: '#E69F00', borderDash: [3], symbol: 'circle', name: 'Chess960' },
-  { color: '#D55E00', borderDash: [3], symbol: 'rectRot', name: 'KingOfTheHill' },
-  { color: '#CC79A7', borderDash: [3], symbol: 'rect', name: 'ThreeCheck' },
-  { color: '#DF5353', borderDash: [3], symbol: 'triangle', name: 'Antichess' },
-  { color: '#66558C', borderDash: [3], symbol: 'triangle', name: 'Atomic' },
-  { color: '#99E699', borderDash: [10], symbol: 'circle', name: 'Horde' },
-  { color: '#FFAEAA', borderDash: [3], symbol: 'rectRot', name: 'RacingKings' },
-  { color: '#56B4E9', borderDash: [10], symbol: 'rectRounded', name: 'Crazyhouse' },
-  { color: '#0072B2', borderDash: [10], symbol: 'triangle', name: 'Puzzle' },
-  { color: '#0072B2', borderDash: [10], symbol: 'triangle', name: 'Ultrabullet' },
+  { color: '#56B4E9', symbol: 'circle', name: 'Bullet' },
+  { color: '#0072B2', symbol: 'rectRot', name: 'Blitz' },
+  { color: '#009E73', symbol: 'rect', name: 'Rapid' },
+  { color: '#459f3b', symbol: 'triangle', name: 'Classical' },
+  { color: '#F0E442', symbol: 'triangle', name: 'Correspondence' },
+  { color: '#E69F00', symbol: 'circle', name: 'Chess960' },
+  { color: '#D55E00', symbol: 'rectRot', name: 'KingOfTheHill' },
+  { color: '#CC79A7', symbol: 'rect', name: 'ThreeCheck' },
+  { color: '#DF5353', symbol: 'triangle', name: 'Antichess' },
+  { color: '#66558C', symbol: 'triangle', name: 'Atomic' },
+  { color: '#99E699', symbol: 'circle', name: 'Horde' },
+  { color: '#FFAEAA', symbol: 'rectRot', name: 'RacingKings' },
+  { color: '#cc9c75', symbol: 'rectRounded', name: 'Crazyhouse' },
+  { color: '#737beb', symbol: 'triangle', name: 'Puzzle' },
+  { color: '#73eb99', symbol: 'triangle', name: 'Ultrabullet' },
 ];
 
 const oneDay = 24 * 60 * 60 * 1000;
 
 export async function initModule({ data, singlePerfName }: Opts) {
-
   $('.spinner').remove();
   const timeBtn = (t: string) => `<button class = "btn-rack__btn">${t}</a>`;
-  const buttons = ['1m', '3m', '6m', 'ytd', '1y', 'all'].flatMap(s => timeBtn(s));
+  const buttons = ['1m', '3m', '6m', 'YTD', '1y', 'all'].map(s => timeBtn(s));
   $('.time-selector-buttons').html(buttons.join(''));
-  $('button.btn-rack__btn:nth-child(2)').addClass('active');
 
   const $el = $('canvas.rating-history');
   const singlePerfIndex = data.findIndex(x => x.name === singlePerfName);
@@ -71,13 +68,13 @@ export async function initModule({ data, singlePerfName }: Opts) {
     $el.hide();
     return;
   }
-  const allData = makeDataset(1, data, singlePerfName, singlePerfIndex);
+  const allData = makeDatasets(1, data, singlePerfName);
   const startDate = allData.startDate;
   const endDate = allData.endDate;
-  const downsampledData = makeDataset(7, data, singlePerfName, singlePerfIndex);
+  const weeklyData = makeDatasets(7, data, singlePerfName);
   const threeMonthsAgo = dayjs(endDate).subtract(3, 'M').valueOf();
-  const start = startDate < threeMonthsAgo ? threeMonthsAgo : startDate;
-  let zoomedOut = start == threeMonthsAgo;
+  const initial = startDate < threeMonthsAgo ? threeMonthsAgo : startDate;
+  let zoomedOut = initial == threeMonthsAgo;
   const config: ChartConfiguration<'line'> = {
     type: 'line',
     data: {
@@ -96,7 +93,7 @@ export async function initModule({ data, singlePerfName }: Opts) {
       responsive: true,
       scales: {
         x: {
-          min: start,
+          min: initial,
           max: endDate,
           type: 'time',
           display: false,
@@ -104,11 +101,13 @@ export async function initModule({ data, singlePerfName }: Opts) {
             tooltipFormat: 'dddd, MMM DD, YYYY',
             minUnit: 'day',
           },
-          offset: true,
+          clip: false,
           ticks: {
+            source: 'data',
             maxRotation: 0,
             minRotation: 0,
             sampleSize: 0,
+            autoSkip: true,
           },
         },
         y: {
@@ -126,6 +125,7 @@ export async function initModule({ data, singlePerfName }: Opts) {
             maxTicksLimit: 7,
             format: {
               useGrouping: false,
+              minimumFractionDigits: 0,
               maximumFractionDigits: 0,
             },
             minRotation: 0,
@@ -144,6 +144,7 @@ export async function initModule({ data, singlePerfName }: Opts) {
               $el.addClass('panning');
               return true; // why
             },
+            onPan: () => lichess.pubsub.emit('chart.panning'),
             onPanComplete: ctx => {
               toggleEvents(ctx.chart as Chart<'line'>, false);
               $el.removeClass('panning');
@@ -158,7 +159,6 @@ export async function initModule({ data, singlePerfName }: Opts) {
         },
         tooltip: {
           usePointStyle: true,
-          intersect: false,
           backgroundColor: tooltipBgColor,
           bodyColor: fontColor,
           titleColor: fontColor,
@@ -172,8 +172,13 @@ export async function initModule({ data, singlePerfName }: Opts) {
   };
   const chart = new Chart($el[0] as HTMLCanvasElement, config);
   const handlesSlider = $('#time-range-slider')[0];
+  const yearPips = [];
+  for (let i = startDate; i <= endDate; i += oneDay) {
+    const date = dayjs(i);
+    if (date.date() == 1 && date.month() == 0) yearPips.push(i);
+  }
   const opts: Options = {
-    start: [start, endDate],
+    start: [initial, endDate],
     connect: true,
     margin: 1000 * 60 * 60 * 24 * 7,
     direction: 'ltr',
@@ -184,26 +189,27 @@ export async function initModule({ data, singlePerfName }: Opts) {
       max: endDate,
     },
     pips: {
-      mode: PipsMode.Count,
-      values: 5,
+      mode: PipsMode.Values,
+      values: yearPips,
       filter: (val, tpe) => (tpe == 1 ? val : -1),
       format: {
-        to: val => dayjs(val).format('MMM YYYY'),
+        to: val => dayjs(val).format('YYYY'),
       },
     },
   };
   if (handlesSlider) {
     const slider = noUiSlider.create(handlesSlider, opts);
     slider.on('update', values => {
+      $('.time-selector-buttons button').removeClass('active');
+      if ($el.hasClass('panning')) return;
       const [min, max] = values.map(v => Number(v));
-      // Downsample data for ranges > 1 year. For performance as well as aesthetics.
-      // Previously done automatically for ranges > 3 years.
+      // Downsample data for ranges > 2 years. For performance as well as aesthetics.
       const yearDiff = (max: number, min: number) => dayjs(max).diff(min, 'year');
       const chartYear = yearDiff(chart.scales.x.max, chart.scales.x.min);
       const sliderYear = yearDiff(max, min);
-      if (chartYear >= 1 && chartYear <= 2 && sliderYear >= 1 && sliderYear <= 2 && chartYear != sliderYear) {
-        zoomedOut = chartYear < sliderYear;
-        chart.data.datasets = zoomedOut ? downsampledData.ds : allData.ds;
+      if (Math.abs(chartYear - sliderYear) >= 1) {
+        zoomedOut = dayjs(max).diff(min, 'year') >= 2;
+        chart.data.datasets = zoomedOut ? weeklyData.ds : allData.ds;
         chart.update('none');
       }
       if (chart.scales.x.min != min || chart.scales.x.max != max)
@@ -212,44 +218,61 @@ export async function initModule({ data, singlePerfName }: Opts) {
     // Disable events while dragging for a slight performance boost
     slider.on('start', () => toggleEvents(chart, true));
     slider.on('end', () => toggleEvents(chart, false));
+    lichess.pubsub.on('chart.panning', () => {
+      slider.set([chart.scales.x.min, chart.scales.x.max]);
+    });
+    const btnClick = (min: number) => {
+      $('.time-selector-buttons .button').removeClass('active');
+      slider.set([min, endDate]);
+      chart.zoomScale('x', { min: min, max: endDate });
+    };
+    $('.time-selector-buttons').on('click', 'button', function (this: HTMLButtonElement) {
+      let min;
+      if (this.textContent == 'all') min = dayjs(startDate);
+      if (this.textContent == '1y') min = dayjs(endDate).subtract(1, 'year');
+      if (this.textContent == 'YTD') min = dayjs(`01-01-${dayjs(endDate).year()}`, 'DD-MM-YYYY');
+      if (this.textContent == '6m') min = dayjs(endDate).subtract(6, 'months');
+      if (this.textContent == '3m') min = dayjs(endDate).subtract(3, 'months');
+      if (this.textContent == '1m') min = dayjs(endDate).subtract(1, 'months');
+      if (min) btnClick(Math.max(startDate, min.valueOf()));
+      this.classList.add('active');
+    });
   }
 }
 
-function makeDataset(step: number, data: PerfRatingHistory[], name: string | undefined, index: number) {
-  const indexFilter = (_: any, i: number) => !name || i === index;
+function makeDatasets(step: number, data: PerfRatingHistory[], name: string | undefined) {
+  const indexFilter = (p: ChartPerf | PerfRatingHistory) => !name || p.name === name;
   const minMax = (d: PerfRatingHistory) => [getDate(d.points[0]), getDate(d.points[d.points.length - 1])];
-  const dates = data.filter(d => d.points.length).flatMap(minMax);
+  const filtered = data.filter(indexFilter);
+  const dates = filtered.filter(p => p.points.length).flatMap(minMax);
   const startDate = Math.min(...dates);
   const endDate = Math.max(...dates);
-  const ds: ChartDataset<'line'>[] = data
-    .filter(p => !name || p.name === name)
-    .map((serie, i) => {
-      const originalDatesAndRatings = serie.points.map(r => ({
-        ts: getDate(r),
-        rating: r[3],
-      }));
-      const color = styles.filter(indexFilter)[i].color;
-      const data = smoothDates(originalDatesAndRatings, step, startDate);
-      return {
-        label: serie.name,
-        borderColor: color,
-        hoverBorderColor: hoverBorderColor,
-        backgroundColor: color,
-        pointRadius: 0,
-        pointHoverRadius: 6,
-        data: data,
-        pointStyle: styles.filter(indexFilter)[i].symbol,
-        segment: {
-          borderDash: styles.filter(indexFilter)[i].borderDash,
-        },
-        borderWidth: 2,
-        tension: 0,
-        borderDash: [],
-        stepped: false,
-        animation: false,
-      };
-    });
-  return { ds: ds.filter(ds => !!ds.data.length), startDate, endDate };
+  const ds: ChartDataset<'line'>[] = filtered.map((serie, i) => {
+    const originalDatesAndRatings = serie.points.map(r => ({
+      ts: getDate(r),
+      rating: r[3],
+    }));
+    const color = styles.filter(indexFilter)[i].color;
+    const data = smoothDates(originalDatesAndRatings, step, startDate);
+    return {
+      indexAxis: 'x',
+      type: 'line',
+      label: serie.name,
+      borderColor: color,
+      hoverBorderColor: hoverBorderColor,
+      backgroundColor: color,
+      pointRadius: 0,
+      pointHoverRadius: 6,
+      data: data,
+      pointStyle: styles.filter(indexFilter)[i].symbol,
+      borderWidth: 2,
+      tension: 0,
+      borderDash: [],
+      stepped: false,
+      animation: false,
+    };
+  });
+  return { ds: ds.filter(ds => ds.data.length), startDate, endDate };
 }
 function smoothDates(data: TsAndRating[], step: number, begin: number) {
   const oneStep = oneDay * step;
