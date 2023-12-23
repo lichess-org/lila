37452d6610682dcaafb0c9dc0ba9b12d63f1ed13
Thibault Duplessis <t@lichess.org>
2023-12-23 16:36:47 +0100

    refactor ui/site/component/flairPicker and fix code duplication

--- ui/site/src/dailyFeed.ts
+++ ui/site/src/dailyFeed.ts
@@ -1,25 +1,5 @@
+import flairPicker from './component/flairPicker';
+
 $('.emoji-details').each(function (this: HTMLElement) {
-  const details = this;
-  const parent = $(details).parent();
-  const close = () => details.removeAttribute('open');
-  const onEmojiSelect = (i?: { id: string; src: string }) => {
-    parent.find('input[name="flair"]').val(i?.id ?? '');
-    parent.find('.uflair').remove();
-    if (i) parent.find('.flair-container').append('<img class="uflair" src="' + i.src + '" />');
-    close();
-  };
-  parent.find('.emoji-remove').on('click', e => {
-    e.preventDefault();
-    onEmojiSelect();
-    $(e.target).remove();
-  });
-  $(details).on('toggle', () =>
-    lichess.asset.loadEsm('flairPicker', {
-      init: {
-        element: details.querySelector('.flair-picker')!,
-        close,
-        onEmojiSelect,
-      },
-    }),
-  );
+  flairPicker(this);
 });
