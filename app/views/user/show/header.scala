c82d0b6922bb37236a3dcffd998c7dba15951fd5
Thibault Duplessis <t@lichess.org>
2024-01-04 10:39:58 +0100

    refactor code duplication

--- app/views/user/show/header.scala
+++ app/views/user/show/header.scala
@@ -155,12 +155,7 @@ object header:
           val hideTroll = u.marks.troll && !ctx.is(u)
           div(id := "us_profile")(
             if info.ratingChart.isDefined && (!u.lame || ctx.is(u) || isGranted(_.UserModView)) then
-              div(cls := "rating-history-container")(
-                div(cls := "time-selector-buttons"),
-                (spinner),
-                div(cls := "chart-container")(canvas(cls := "rating-history")),
-                div(id := "time-range-slider")
-              )
+              views.html.user.perfStat.ratingHistoryContainer
             else (ctx.is(u) && u.count.game < 10) option newPlayer(u),
             div(cls := "profile-side")(
               div(cls := "user-infos")(
