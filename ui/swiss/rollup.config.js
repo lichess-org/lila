c5d66f72cb3be9b16de3a5a52c6e02d9605f86ae
WandererXII <43043181+WandererXII@users.noreply.github.com>
2024-01-11 16:06:58 +0100

    Delete ui/swiss
    TODO: Remove all swiss code, since I don't plan to support it

--- ui/swiss/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import { rollupProject } from '@build/rollupProject';
-
-export default rollupProject({
-  main: {
-    name: 'LishogiSwiss',
-    input: 'src/main.ts',
-    output: 'lishogi.swiss',
-  },
-});
--- ui/swiss/src/util.ts
+++
@@ -1,5 +0,0 @@
-import { Outcome } from './interfaces';
-
-export function isOutcome(s: any): s is Outcome {
-  return s == 'absent' || s == 'late' || s == 'bye';
-}
