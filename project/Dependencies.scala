5130d31fd554bde85eb79ee637d61b46e01180f8
Scala Steward <scala_steward@virtuslab.com>
2024-01-13 01:55:58 +0000

    Update play, play-logback, ... to 2.9.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -66,7 +66,7 @@ object Dependencies {
   }
 
   object play {
-    val playVersion = "2.8.18-lila_3.18"
+    val playVersion = "2.9.1"
     val json        = "org.playframework" %% "play-json"         % "3.0.1"
     val api         = "com.typesafe.play" %% "play"              % playVersion
     val server      = "com.typesafe.play" %% "play-server"       % playVersion
