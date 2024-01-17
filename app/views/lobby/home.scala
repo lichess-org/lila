986087d414a638fae6d6a85e4dc74d5611e67ad1
Carbrex <95964955+Carbrex@users.noreply.github.com>
2024-01-18 00:37:32 +0530

    Hidden home page feed from bot accounts

--- app/views/lobby/home.scala
+++ app/views/lobby/home.scala
@@ -118,7 +118,7 @@ object home:
           views.html.puzzle.embed.dailyLink(p)(cls := "lobby__puzzle"),
         bits.lastPosts(lastPost, ublogPosts),
         ctx.noBot option bits.underboards(tours, simuls, leaderboard, tournamentWinners),
-        div(cls := "lobby__feed"):
+        ctx.noBot option div(cls := "lobby__feed"):
           views.html.dailyFeed.lobbyUpdates(lastUpdates)
         ,
         div(cls := "lobby__support")(
