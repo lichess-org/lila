5c57bd77f1d7f22833c89ff5ed130fdaba9982fb
Thibault Duplessis <t@lichess.org>
2024-01-24 15:38:06 +0100

    allow partial pull/push of broadcast round PGN
    where only some of the games are present,
    and match existing chapters by static tags.
    
    Cannot be used to add new chapters, only update existing ones.

--- modules/relay/src/main/RelaySync.scala
+++ modules/relay/src/main/RelaySync.scala
@@ -49,8 +49,8 @@ final private class RelaySync(
                   .some
 
   /*
-   * If the source contains several games, use their index to match them with the study chapter.
-   * If the source contains only one game, use the player tags (and site) to match with the study chapter.
+   * If the source contains all expected games, use their index to match them with the study chapter.
+   * If the source contains only fewer games, use the player tags (and site) to match with the study chapters.
    * So the TCEC style - one game per file, reusing the file for all games - is supported.
    * lichess will create a new chapter when the game player tags differ.
    */
@@ -59,7 +59,7 @@ final private class RelaySync(
       chapters: List[Chapter],
       nbGames: Int
   ): Option[Chapter] =
-    if nbGames == 1 || game.looksLikeLichess
+    if chapters.sizeIs > nbGames || game.looksLikeLichess
     then chapters.find(c => game.staticTagsMatch(c.tags))
     else chapters.find(_.relay.exists(_.index == game.index))
 
