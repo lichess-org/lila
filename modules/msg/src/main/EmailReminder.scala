b69a67a2b2bdff22dd82329e21f457b655424c48
Thibault Duplessis <t@lichess.org>
2024-02-03 12:24:01 +0100

    tweak email reminder wording

--- modules/msg/src/main/EmailReminder.scala
+++ modules/msg/src/main/EmailReminder.scala
@@ -17,7 +17,7 @@ Hello, as you have an early Lichess account, no email was required when you regi
 However this makes it easy for you to lose access to your account.
 If you forget your password, or if your password is leaked from another website, or if we decide that your password is too easy-to-guess to be secure, your account will be lost.
 
-You can visit $baseUrl/account/email to set your personal email address. That way, you'll be able to reset your password when needed."""
+Please visit $baseUrl/account/email to set your account email address. That way, you'll be able to reset your password when needed."""
 
   private val cache = mongoCache[UserId, Boolean](1024, "security:email:reminder", 10 days, _.value):
     loader =>
