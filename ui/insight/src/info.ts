eca5e9c909740907e58e7969f17cb40575d1ae4f
Sérgio Glórias <9739913+SergioGlorias@users.noreply.github.com>
2024-02-05 17:53:47 +0000

    ok prettier...

--- ui/insight/src/info.ts
+++ ui/insight/src/info.ts
@@ -1,51 +1,75 @@
-import * as licon from 'common/licon';
-import { onInsert } from 'common/snabbdom';
-import { numberFormat } from 'common/number';
-import { userLink } from 'common/userLink';
-import { h } from 'snabbdom';
-import Ctrl from './ctrl';
+import * as licon from "common/licon";
+import { onInsert } from "common/snabbdom";
+import { numberFormat } from "common/number";
+import { userLink } from "common/userLink";
+import { h } from "snabbdom";
+import Ctrl from "./ctrl";
 
-const shareStates = ['nobody', 'friends only', 'everybody'];
+const shareStates = ["nobody", "friends only", "everybody"];
 
 export default function (ctrl: Ctrl) {
-  const shareText = 'Shared with ' + shareStates[ctrl.user.shareId] + '.';
-  return h('div.info.box', [
-    h('div.top', userLink(ctrl.user)),
-    h('div.content', [
-      h('p', ['Insights over ', h('strong', numberFormat(ctrl.user.nbGames)), ' rated games.']),
+  const shareText = "Shared with " + shareStates[ctrl.user.shareId] + ".";
+  return h("div.info.box", [
+    h("div.top", userLink(ctrl.user)),
+    h("div.content", [
+      h("p", [
+        "Insights over ",
+        h("strong", numberFormat(ctrl.user.nbGames)),
+        " rated games.",
+      ]),
       h(
-        'p.share',
+        "p.share",
         ctrl.own
           ? h(
-              'a',
-              { attrs: { href: '/account/preferences/privacy#shareYourInsightsData', target: '_blank', rel: 'noopener' } },
-              shareText,
+              "a",
+              {
+                attrs: {
+                  href: "/account/preferences/privacy#shareYourInsightsData",
+                  target: "_blank",
+                  rel: "noopener",
+                },
+              },
+              shareText
             )
-          : shareText,
+          : shareText
       ),
     ]),
     h(
-      'div.refresh',
+      "div.refresh",
       ctrl.env.user.stale
-        ? h('div.insight-stale', [
-            h('p', 'There are new games to learn from!'),
+        ? h("div.insight-stale", [
+            h("p", "There are new games to learn from!"),
             h(
-              'form.insight-refresh',
+              "form.insight-refresh",
               {
-                attrs: { action: `/insights/refresh/${ctrl.env.user.id}`, method: 'post' },
-                hook: onInsert(_el => lichess.refreshInsightForm()),
+                attrs: {
+                  action: `/insights/refresh/${ctrl.env.user.id}`,
+                  method: "post",
+                },
+                hook: onInsert((_el) => lichess.refreshInsightForm()),
               },
               [
-                h('button.button.text', { attrs: { 'data-icon': licon.Checkmark } }, 'Update insights'),
                 h(
-                  'div.crunching.none',
-                  { hook: onInsert(el => el.insertAdjacentHTML('afterbegin', lichess.spinnerHtml)) },
-                  [h('br'), h('p', h('strong', 'Now crunching data just for you!'))],
+                  "button.button.text",
+                  { attrs: { "data-icon": licon.Checkmark } },
+                  "Update insights"
                 ),
-              ],
+                h(
+                  "div.crunching.none",
+                  {
+                    hook: onInsert((el) =>
+                      el.insertAdjacentHTML("afterbegin", lichess.spinnerHtml)
+                    ),
+                  },
+                  [
+                    h("br"),
+                    h("p", h("strong", "Now crunching data just for you!")),
+                  ]
+                ),
+              ]
             ),
           ])
-        : null,
+        : null
     ),
   ]);
 }
