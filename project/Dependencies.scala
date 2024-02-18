396789bf0f31e0e3999470b6e4c81beff12226a8
Thibault Duplessis <t@lichess.org>
2024-02-18 13:57:13 +0100

    scalachess 15.7.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val alleycats   = "org.typelevel"                %% "alleycats-core"                  % "2.10.0"
   val scalalib    = "com.github.ornicar"           %% "scalalib"                        % "9.5.5"
   val hasher      = "com.roundeights"              %% "hasher"                          % "1.3.1"
-  val chess       = "org.lichess"                  %% "scalachess"                      % "15.7.8"
+  val chess       = "org.lichess"                  %% "scalachess"                      % "15.7.9"
   val compression = "org.lichess"                  %% "compression"                     % "1.10"
   val maxmind     = "com.maxmind.geoip2"            % "geoip2"                          % "4.0.1"
   val caffeine    = "com.github.ben-manes.caffeine" % "caffeine"                        % "3.1.8" % "compile"
