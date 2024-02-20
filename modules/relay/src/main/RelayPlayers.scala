a5879cff7dd4b86ca78ac3d6a5cce6a6f73a2d1f
Thibault Duplessis <t@lichess.org>
2024-02-20 14:34:14 +0100

    broadcast players parser v2

--- modules/relay/src/main/RelayPlayers.scala
+++ modules/relay/src/main/RelayPlayers.scala
@@ -10,17 +10,31 @@ private class RelayPlayers(val text: String):
 
   def sortedText = text.linesIterator.toList.sorted.mkString("\n")
 
-  lazy val players: Map[PlayerName, RelayPlayer] = text.linesIterator
-    .take(1000)
-    .toList
-    .flatMap: line =>
+  lazy val players: Map[PlayerName, RelayPlayer] =
+    val lines = text.linesIterator
+    lines.nonEmpty.so:
+      val parse = parser.pick(lines.next)
+      text.linesIterator.take(1000).toList.flatMap(parse).toMap
+
+  private object parser:
+    def pick(line: String) = if line.contains(';') then parser.v1 else parser.v2
+    // Original name; Replacement name; Optional rating; Optional title
+    val v1 = (line: String) =>
       line.split(';').map(_.trim) match
         case Array(id, name, rating, title) =>
           Some(id -> RelayPlayer(name, rating.toIntOption, lila.user.Title.get(title)))
         case Array(id, name, rating) => Some(id -> RelayPlayer(name, rating.toIntOption, none))
         case Array(id, name)         => Some(id -> RelayPlayer(name, none, none))
         case _                       => none
-    .toMap
+    // Original name / Optional rating / Optional title / Optional replacement name
+    val v2 = (line: String) =>
+      val arr = line.split('/').map(_.trim)
+      arr lift 0 map: fromName =>
+        fromName -> RelayPlayer(
+          name = arr.lift(3).filter(_.nonEmpty) | fromName,
+          rating = arr.lift(1).flatMap(_.toIntOption),
+          title = arr.lift(2).flatMap(lila.user.Title.get)
+        )
 
   def update(games: RelayGames): RelayGames = games.map: game =>
     game.copy(tags = update(game.tags))
