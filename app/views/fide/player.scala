94666c5554dfcd52850dd3d3963b8b30a0db96de
Thibault Duplessis <t@lichess.org>
2024-02-28 13:38:37 +0100

    link to the federation page

--- app/views/fide/player.scala
+++ app/views/fide/player.scala
@@ -40,7 +40,7 @@ object player:
             td(a(href := routes.Fide.show(player.id, player.slug))(player.name)),
             withFlag option td:
               player.fed.map: fed =>
-                federation.flag(fed, Federation.name(fed))
+                a(href := routes.Fide.federation(Federation.name(fed)))(federation.flag(fed, Federation.name(fed)))
             ,
             td(player.standard),
             td(player.rapid),
