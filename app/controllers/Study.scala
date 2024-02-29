a1094dfa5708b1be79c7182dfe076383fafc6080
Thibault Duplessis <t@lichess.org>
2024-02-29 08:22:31 +0100

    api endpoint to delete a study chapter - closes #14759

--- app/controllers/Study.scala
+++ app/controllers/Study.scala
@@ -274,6 +274,11 @@ final class Study(
           case Some(tour) => Redirect(routes.RelayTour.show(tour.slug, tour.id))
   }
 
+  def apiChapterDelete(id: StudyId, chapterId: StudyChapterId) = ScopedBody(_.Study.Write) { _ ?=> me ?=>
+    Found(env.study.api.byIdAndOwnerOrAdmin(id, me)): study =>
+      env.study.api.deleteChapter(id, chapterId)(Who(me.userId, Socket.Sri("api"))) inject NoContent
+  }
+
   def clearChat(id: StudyId) = Auth { _ ?=> me ?=>
     env.study.api.isOwnerOrAdmin(id, me) flatMapz {
       env.chat.api.userChat.clear(id into ChatId)
