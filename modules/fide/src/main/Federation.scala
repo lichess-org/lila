e87a22dcf3ee5cadbe568f89cfe8ffbd1d23c49b
Sérgio Glórias <9739913+SergioGlorias@users.noreply.github.com>
2024-03-02 06:35:38 +0000

    The FID Federation is missing (#14778)
    * ADD FID
    
    * fix

--- modules/fide/src/main/Federation.scala
+++ modules/fide/src/main/Federation.scala
@@ -46,6 +46,7 @@ object Federation:
       nameToSlug(name) -> id
 
   val names: Map[Id, Name] = Map(
+    "FID" -> "FIDE",
     "USA" -> "United States of America",
     "IND" -> "India",
     "CHN" -> "China",
