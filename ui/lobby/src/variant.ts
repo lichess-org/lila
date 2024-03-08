5aafae5ca546e5cfd1065ea5af6c3e3222f8f799
WandererXII <43043181+WandererXII@users.noreply.github.com>
2024-03-08 23:19:13 +0100

    No confirmation for standard variant in lobby
    And add the word 'variant' to the confirm

--- ui/lobby/src/variant.ts
+++ ui/lobby/src/variant.ts
@@ -3,9 +3,9 @@ function storageKey(key) {
 }
 
 export default function (variant: VariantKey, trans: TransNoArg) {
-  if (!window.lishogi.storage.get(storageKey(variant))) {
-    const v = trans(variant),
-      c = confirm(v);
+  if (variant !== 'standard' && !window.lishogi.storage.get(storageKey(variant))) {
+    const txt = `${trans('variant')} - ${trans(variant)}`,
+      c = confirm(txt);
     if (c) window.lishogi.storage.set(storageKey(variant), '1');
     return c;
   } else return true;
