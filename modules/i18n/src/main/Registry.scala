56f5d62a275590673497c3474532872b363dff5a
Thibault Duplessis <t@lichess.org>
2024-03-14 11:37:37 +0100

    truely lazy load languages without parking threads - closes #14827
    loads the most popular languages first,
    serves default translations until loaded

--- modules/i18n/src/main/Registry.scala
+++ modules/i18n/src/main/Registry.scala
@@ -3,43 +3,53 @@ package lila.i18n
 import java.io.{ File, FileInputStream, ObjectInputStream }
 import java.util.{ Map as JMap }
 import java.util.concurrent.ConcurrentHashMap
+import play.api.Mode
 import play.api.i18n.Lang
 import scala.jdk.CollectionConverters.*
 import lila.common.Chronometer
 
 object Registry:
 
-  private val all = ConcurrentHashMap[Lang, MessageMap](128)
+  private var all = Map[Lang, MessageMap]()
 
-  def get(lang: Lang) = Option(all.computeIfAbsent(lang, loadSerialized))
+  inline def get(inline lang: Lang) = all.get(lang)
 
-  private def loadSerialized(lang: Lang): MessageMap = try
-    val lap = Chronometer.sync:
-      val istream = ObjectInputStream(getClass.getClassLoader.getResourceAsStream(s"i18n.${lang.code}.ser"))
-      val messageMap = istream.readObject().asInstanceOf[JMap[String, Object]]
-      istream.close()
-      messageMap.asScala.toMap
-        .map: (key, value) =>
-          key -> value.match
-            case s: String => singleOrEscaped(s)
-            case m: JMap[?, ?] =>
-              val plurals = m
-                .asInstanceOf[JMap[String, String]]
-                .asScala
-                .flatMap: (q, i) =>
-                  I18nQuantity.fromString(q).map(_ -> i)
-              Plurals(plurals.toMap)
-            case _ => throw Exception(s"i18n oh noes $key: $value")
-        .asJava
-    logger.info(s"Unserialized ${lang.code} ${lap.result.size} messages in ${lap.showDuration}")
-    lap.result
+  val empty: MessageMap   = java.util.HashMap[MessageKey, Translation]()
+  val default: MessageMap = get(defaultLang) | empty
+
+  def asyncLoadLanguages()(using Executor)(using scheduler: Scheduler, mode: Mode): Unit =
+    scheduler.scheduleOnce(2.seconds):
+      LangList.popular
+        .grouped(10)
+        .zipWithIndex
+        .foreach: (langs, i) =>
+          scheduler.scheduleOnce(i.seconds):
+            val lap = Chronometer.sync:
+              langs.foreach: lang =>
+                all = all + (lang -> loadSerialized(lang))
+            logger.info(s"Loaded ${langs.size} languages in ${lap.showDuration}")
+
+  private def loadSerialized(lang: Lang)(using mode: Mode): MessageMap = try
+    val istream    = ObjectInputStream(getClass.getClassLoader.getResourceAsStream(s"i18n.${lang.code}.ser"))
+    val messageMap = istream.readObject().asInstanceOf[JMap[String, Object]]
+    istream.close()
+    messageMap.asScala.toMap
+      .map: (key, value) =>
+        key -> value.match
+          case s: String => singleOrEscaped(s)
+          case m: JMap[?, ?] =>
+            val plurals = m
+              .asInstanceOf[JMap[String, String]]
+              .asScala
+              .flatMap: (q, i) =>
+                I18nQuantity.fromString(q).map(_ -> i)
+            Plurals(plurals.toMap)
+          case _ => throw Exception(s"i18n oh noes $key: $value")
+      .asJava
   catch
     case e: Exception =>
       logger.error(s"Failed to load i18n for ${lang.code}", e)
       empty
-
-  val empty: MessageMap   = java.util.HashMap[MessageKey, Translation]()
-  val default: MessageMap = get(defaultLang) | empty
 
   private def singleOrEscaped(s: String) =
     val sb = java.lang.StringBuilder(s.length + 10)
