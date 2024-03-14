26cd13b891e5305d761a97f7aee604ab085ff67b
Thibault Duplessis <t@lichess.org>
2024-03-14 10:58:20 +0100

    lazy load translation on start - for #14827

--- modules/i18n/src/main/Registry.scala
+++ modules/i18n/src/main/Registry.scala
@@ -2,44 +2,44 @@ package lila.i18n
 
 import java.io.{ File, FileInputStream, ObjectInputStream }
 import java.util.{ Map as JMap }
+import java.util.concurrent.ConcurrentHashMap
 import play.api.i18n.Lang
 import scala.jdk.CollectionConverters.*
 import lila.common.Chronometer
 
 object Registry:
 
-  val all: Map[Lang, MessageMap] = Chronometer.syncEffect(loadSerialized): lap =>
-    logger.info(s"Loaded ${lap.result.size} langs in ${lap.showDuration}")
+  private val all = ConcurrentHashMap[Lang, MessageMap](128)
 
-  private def loadSerialized: Map[Lang, MessageMap] =
-    val istream = ObjectInputStream(getClass.getClassLoader.getResourceAsStream("I18n.ser"))
-    val unserialized =
-      Chronometer.syncEffect(
-        istream.readObject().asInstanceOf[JMap[String, JMap[String, Object]]].asScala.toMap
-      ): lap =>
-        logger.info(s"Unserialized I18n.ser in ${lap.showDuration}")
-    istream.close()
+  def get(lang: Lang) = Option(all.computeIfAbsent(lang, loadSerialized))
 
-    unserialized.map:
-      case (langCode, messageMap) =>
-        Lang(langCode) -> messageMap.asScala
-          .map: (key, value) =>
-            key -> value.match
-              case s: String => singleOrEscaped(s)
-              case m: JMap[?, ?] =>
-                val plurals = m
-                  .asInstanceOf[JMap[String, String]]
-                  .asScala
-                  .flatMap: (q, i) =>
-                    I18nQuantity
-                      .fromString(q)
-                      .map: quantity =>
-                        quantity -> i
-                Plurals(plurals.toMap)
-              case _ => throw Exception(s"i18n oh noes $key: $value")
-          .asJava
+  private def loadSerialized(lang: Lang): MessageMap = try
+    val lap = Chronometer.sync:
+      val istream = ObjectInputStream(getClass.getClassLoader.getResourceAsStream(s"i18n.${lang.code}.ser"))
+      val messageMap = istream.readObject().asInstanceOf[JMap[String, Object]]
+      istream.close()
+      messageMap.asScala.toMap
+        .map: (key, value) =>
+          key -> value.match
+            case s: String => singleOrEscaped(s)
+            case m: JMap[?, ?] =>
+              val plurals = m
+                .asInstanceOf[JMap[String, String]]
+                .asScala
+                .flatMap: (q, i) =>
+                  I18nQuantity.fromString(q).map(_ -> i)
+              Plurals(plurals.toMap)
+            case _ => throw Exception(s"i18n oh noes $key: $value")
+        .asJava
+    logger.info(s"Unserialized ${lang.code} ${lap.result.size} messages in ${lap.showDuration}")
+    lap.result
+  catch
+    case e: Exception =>
+      logger.error(s"Failed to load i18n for ${lang.code}", e)
+      empty
 
-  val default: MessageMap = all.getOrElse(defaultLang, java.util.HashMap[MessageKey, Translation])
+  val empty: MessageMap   = java.util.HashMap[MessageKey, Translation]()
+  val default: MessageMap = get(defaultLang) | empty
 
   private def singleOrEscaped(s: String) =
     val sb = java.lang.StringBuilder(s.length + 10)
