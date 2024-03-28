d03a542cc5b03ad0e53cc818ac484782e313bc71
Scala Steward <scala_steward@virtuslab.com>
2024-03-28 18:59:53 +0000

    Update scala3-library to 3.4.1

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,7 +6,7 @@ object BuildSettings {
   import Dependencies._
 
   val lilaVersion        = "4.0"
-  val globalScalaVersion = "3.4.0"
+  val globalScalaVersion = "3.4.1"
 
   def buildSettings =
     Defaults.coreDefaultSettings ++ Seq(
