dbbbc1319d824783248ea6e82823a7d0bbcba15d
Bastian Pedersen <bastian.tangedal@gmail.com>
2024-03-29 09:30:59 +0100

    Add API option to retrieve puzzle activity after a specific datetime

--- app/controllers/Puzzle.scala
+++ app/controllers/Puzzle.scala
@@ -368,7 +368,8 @@ final class Puzzle(env: Env, apiC: => Api) extends LilaController(env):
     val config = lila.puzzle.PuzzleActivity.Config(
       user = me,
       max = getInt("max").map(_.atLeast(1)),
-      before = getTimestamp("before")
+      before = getTimestamp("before"),
+      since = getTimestamp("since")
     )
     apiC.GlobalConcurrencyLimitPerIpAndUserOption(me.some)(env.puzzle.activity.stream(config))(jsToNdJson)
   }
