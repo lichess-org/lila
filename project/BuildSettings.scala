116b83c194a816804fd430ea5dc01e43be8e9a58
Thanh Le <lenguyenthanh@hotmail.com>
2024-03-29 10:27:56 +0700

    Update private[this] deprecation warning message
    They changed it in: https://github.com/scala/scala3/commit/18795e4117d6e50121b5c139dbd6cba74e137e02

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -57,7 +57,7 @@ object BuildSettings {
     "-language:implicitConversions",
     "-release:21",
     // "-Wunused:all",
-    "-Wconf:msg=qualifier will be deprecated:s"
+    "-Wconf:msg= Ignoring [this] qualifier:s"
   )
 
   val srcMain = Seq(
