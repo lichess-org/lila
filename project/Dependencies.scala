d7f24f03cfb7a435b3f7c85eea807cf99a431df3
Thanh Le <lenguyenthanh@hotmail.com>
2024-03-31 15:28:21 +0700

    Use chessVersion constant for scalachess & testKit

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,11 +13,13 @@ object Dependencies {
   val lilaMaven = "lila-maven".at("https://raw.githubusercontent.com/lichess-org/lila-maven/master")
   val sonashots = "sonashots".at("https://oss.sonatype.org/content/repositories/snapshots")
 
+  val chessVersion = "15.9.4"
+
   val cats        = "org.typelevel"                %% "cats-core"                       % "2.10.0"
   val alleycats   = "org.typelevel"                %% "alleycats-core"                  % "2.10.0"
   val scalalib    = "com.github.lichess-org"       %% "scalalib"                        % "10.0.1"
   val hasher      = "com.roundeights"              %% "hasher"                          % "1.3.1"
-  val chess       = "org.lichess"                  %% "scalachess"                      % "15.9.4"
+  val chess       = "org.lichess"                  %% "scalachess"                      % chessVersion
   val compression = "org.lichess"                  %% "compression"                     % "1.10"
   val maxmind     = "com.maxmind.geoip2"            % "geoip2"                          % "4.0.1"
   val caffeine    = "com.github.ben-manes.caffeine" % "caffeine"                        % "3.1.8" % "compile"
@@ -35,9 +37,9 @@ object Dependencies {
   val bloomFilter = "com.github.alexandrnikitin" %% "bloom-filter"  % "0.13.1_lila-1"
   val kittens     = "org.typelevel"              %% "kittens"       % "3.3.0"
 
-  val testKit    = "org.lichess"    %% "scalachess-test-kit" % "15.9.4"    % Test
-  val scalacheck = "org.scalacheck" %% "scalacheck"          % "1.17.0"    % Test
-  val munitCheck = "org.scalameta"  %% "munit-scalacheck"    % "1.0.0-M11" % Test
+  val testKit    = "org.lichess"    %% "scalachess-test-kit" % chessVersion % Test
+  val scalacheck = "org.scalacheck" %% "scalacheck"          % "1.17.0"     % Test
+  val munitCheck = "org.scalameta"  %% "munit-scalacheck"    % "1.0.0-M11"  % Test
 
   object tests {
     val bundle = Seq(munit)
