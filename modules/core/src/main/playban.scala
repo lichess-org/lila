6ad8d2e7cd518bc2bef7dc8ce1fa67248b627f9c
Thibault Duplessis <t@lichess.org>
2024-04-03 08:49:05 +0200

    scalafmt

--- modules/core/src/main/playban.scala
+++ modules/core/src/main/playban.scala
@@ -5,6 +5,6 @@ opaque type RageSit = Int
 object RageSit extends OpaqueInt[RageSit]:
   extension (a: RageSit) def counterView = a.value / 10
 
-type BansOf        = List[UserId] => Fu[Map[UserId, Int]]
-type RageSitOf     = UserId => Fu[RageSit]
+type BansOf            = List[UserId] => Fu[Map[UserId, Int]]
+type RageSitOf         = UserId => Fu[RageSit]
 type HasCurrentPlayban = UserId => Fu[Boolean]
