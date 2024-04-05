5ff53277a25bec5bd729bb890a236b6a20a6f25b
Thibault Duplessis <t@lichess.org>
2024-04-05 13:15:29 +0200

    fix fide monitoring

--- modules/fide/src/main/FidePlayerSync.scala
+++ modules/fide/src/main/FidePlayerSync.scala
@@ -119,7 +119,7 @@ final private class FidePlayerSync(repo: FideRepo, ws: StandaloneWSClient)(using
               .flatMap: nb =>
                 lila.mon.fideSync.players.update(nb)
                 setDeletedFlags(startAt).map: deleted =>
-                  lila.mon.fideSync.deleted.update(nb)
+                  lila.mon.fideSync.deleted.update(deleted)
                   logger.info(s"RelayFidePlayerApi.update upserted: $nb, deleted: $nb")
 
           case res => fufail(s"RelayFidePlayerApi.pull ${res.status} ${res.statusText}")
