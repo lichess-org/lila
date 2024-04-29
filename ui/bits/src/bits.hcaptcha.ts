d4aee913fbc8accd589b8c537fe55a80c112b5b7
Jonathan Gamble <gamblej@gmail.com>
2024-04-29 15:15:42 -0500

    does it count as monkey patching if the monkey is real small?

--- ui/bits/src/bits.hcaptcha.ts
+++ ui/bits/src/bits.hcaptcha.ts
@@ -1,19 +1,18 @@
-(window as any).initHcaptcha = () => {
-  const div = document.querySelector('.h-captcha');
-  if (!div) return;
-  const parent = div.parentElement as HTMLElement;
-  const next = div.nextSibling;
-  div.remove();
-  (window as any).hcaptcha.render(div);
-  if ('credentialless' in window && window.crossOriginIsolated) {
-    div.querySelector('iframe')?.setAttribute('credentialless', '');
-  }
-  parent.insertBefore(div, next);
-};
-
 export function initModule() {
   const script = document.createElement('script');
-  script.src = 'https://hcaptcha.com/1/api.js?onload=initHcaptcha&render=explicit';
-  script.type = 'text/javascript';
+  script.src = 'https://hcaptcha.com/1/api.js';
+
+  if ('credentialless' in window && window.crossOriginIsolated) {
+    const documentCreateElement = document.createElement;
+    document.createElement = function () {
+      const element = documentCreateElement.apply(this, arguments as any);
+      if (element instanceof HTMLIFrameElement) element.setAttribute('credentialless', '');
+      return element;
+    };
+
+    script.src = 'https://hcaptcha.com/1/api.js?onload=initHcaptcha';
+    (window as any).initHcaptcha = () => (document.createElement = documentCreateElement);
+  }
+
   document.head.appendChild(script);
 }
