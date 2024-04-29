ae1cb323d48167269afa27c8edd4509ec9975d8c
Jonathan Gamble <gamblej@gmail.com>
2024-04-29 17:07:37 -0500

    monkey is now microscopic

--- ui/bits/src/bits.hcaptcha.ts
+++ ui/bits/src/bits.hcaptcha.ts
@@ -4,13 +4,14 @@ export function initModule() {
 
   if ('credentialless' in window && window.crossOriginIsolated) {
     const documentCreateElement = document.createElement;
-    document.createElement = function () {
-      const element = documentCreateElement.apply(this, arguments as any);
-      if (element instanceof HTMLIFrameElement) element.setAttribute('credentialless', '');
-      return element;
-    };
-
     script.src = 'https://hcaptcha.com/1/api.js?onload=initHcaptcha';
+    script.onload = () => {
+      document.createElement = function () {
+        const element = documentCreateElement.apply(this, arguments as any);
+        if (element instanceof HTMLIFrameElement) element.setAttribute('credentialless', '');
+        return element;
+      };
+    };
     (window as any).initHcaptcha = () => (document.createElement = documentCreateElement);
   }
 
