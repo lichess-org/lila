e649a3906821af2f413195587e2fa7b62ce0016c
Thibault Duplessis <t@lichess.org>
2024-05-01 10:26:30 +0200

    remove dasher .category

--- ui/dasher/src/util.ts
+++ ui/dasher/src/util.ts
@@ -1,16 +1,13 @@
 import * as licon from 'common/licon';
-import { bind, looseH as h, VNodeKids } from 'common/snabbdom';
+import { bind, looseH as h } from 'common/snabbdom';
 import { memoize } from 'common';
 
-export const header = (name: string, close: () => void, kidz?: VNodeKids) =>
-  h('div.category', [
-    h(
-      'button.head.text',
-      { attrs: { 'data-icon': licon.LessThan, type: 'button' }, hook: bind('click', close) },
-      name,
-    ),
-    kidz,
-  ]);
+export const header = (name: string, close: () => void) =>
+  h(
+    'button.head.text',
+    { attrs: { 'data-icon': licon.LessThan, type: 'button' }, hook: bind('click', close) },
+    name,
+  );
 
 export const elementScrollBarWidthSlowGuess = memoize<number>(() => {
   const ruler = document.createElement('div');
