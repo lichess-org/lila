614a002753109b214a5c9c5d36bfcea0477dc2f2
Ben Rollin <ben.rollin@clever.com>
2024-05-05 17:09:42 -0700

    implement map ribbons

--- ui/learn/src/v2/ctrl.ts
+++ ui/learn/src/v2/ctrl.ts
@@ -1,4 +1,6 @@
 import { LearnOpts, LearnProgress } from '../learn';
+import { Stage } from '../stage/list';
+import * as scoring from '../score';
 
 export class LearnCtrl {
   data: LearnProgress = this.opts.storage.data;
@@ -10,4 +12,10 @@ export class LearnCtrl {
   ) {}
 
   isStageIdComplete = (id: number) => false && id;
+
+  stageProgress = (stage: Stage) => {
+    const result = this.data.stages[stage.key];
+    const complete = result ? result.scores.filter(scoring.gtz).length : 0;
+    return [complete, stage.levels.length];
+  };
 }
--- ui/learn/src/v2/view.ts
+++ ui/learn/src/v2/view.ts
@@ -1,6 +1,9 @@
+import * as licon from 'common/licon';
 import { h } from 'snabbdom';
 import { LearnCtrl } from './ctrl';
-import { categs } from '../stage/list';
+import { Stage, categs } from '../stage/list';
+import { StageProgress } from '../learn';
+import * as scoring from '../score';
 
 export const view = (ctrl: LearnCtrl) => {
   return h('div', stageListView(ctrl));
@@ -30,7 +33,7 @@ const stageListView = (ctrl: LearnCtrl) => {
                   // config: m.route,
                 },
                 [
-                  // ribbon(ctrl, s, status, res),
+                  ribbon(ctrl, stage, status, stageProgress),
                   h('img', { attrs: { src: stage.image } }),
                   h('div.text', [h('h3', title), h('p.subtitle', ctrl.trans.noarg(stage.subtitle))]),
                 ],
@@ -48,43 +51,27 @@ function titleVerbosityClass(title: string) {
   return title.length > 13 ? (title.length > 18 ? ' vvv' : ' vv') : '';
 }
 
-// import * as licon from 'common/licon';
 // import m from '../mithrilFix';
 // import * as util from '../util';
-// import * as scoring from '../score';
 // import { MapCtrl } from './mapMain';
 // import { StageProgress } from '../learn';
 
-// function makeStars(nb: number) {
-//   const stars = [];
-//   for (let i = 0; i < 4 - nb; i++)
-//     stars.push(
-//       m('i', {
-//         'data-icon': licon.Star,
-//       }),
-//     );
-//   return stars;
-// }
+function makeStars(nb: number) {
+  const stars = [];
+  for (let i = 0; i < 4 - nb; i++) stars.push(h('i', { attrs: { 'data-icon': licon.Star } }));
+  return stars;
+}
 
-// function ribbon(ctrl: MapCtrl, s: stages.Stage, status: string, res: StageProgress) {
-//   if (status === 'future') return;
-//   let content;
-//   if (status === 'ongoing') {
-//     const p = ctrl.stageProgress(s);
-//     content = p[0] ? p.join(' / ') : ctrl.trans.noarg('play');
-//   } else content = makeStars(scoring.getStageRank(s, res.scores));
-//   if (status === 'future') return;
-//   return m(
-//     'span.ribbon-wrapper',
-//     m(
-//       'span.ribbon',
-//       {
-//         class: status,
-//       },
-//       content,
-//     ),
-//   );
-// }
+function ribbon(ctrl: LearnCtrl, s: Stage, status: string, stageProgress: StageProgress) {
+  if (status === 'future') return;
+  let content;
+  if (status === 'ongoing') {
+    const p = ctrl.stageProgress(s);
+    content = p[0] ? p.join(' / ') : ctrl.trans.noarg('play');
+  } else content = makeStars(scoring.getStageRank(s, stageProgress.scores));
+  if (status === 'future') return;
+  return h('span.ribbon-wrapper', h(`span.ribbon.${status}`, content));
+}
 
 // function whatNext(ctrl: MapCtrl) {
 //   const makeStage = function (href: string, img: string, title: string, subtitle: string, done?: boolean) {
