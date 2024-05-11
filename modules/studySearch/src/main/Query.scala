8695b2d91dbf6792eb208e34977968f9d03de647
Thanh Le <lenguyenthanh@hotmail.com>
2024-05-11 09:59:59 +0700

    Migrate studySearch to lila-search-v3

--- modules/studySearch/src/main/Query.scala
+++ modules/studySearch/src/main/Query.scala
@@ -4,6 +4,7 @@ import play.api.libs.json.*
 
 import lila.common.Json.given
 
-private[studySearch] case class Query(text: String, userId: Option[UserId])
+private[studySearch] case class Query(text: String, userId: Option[UserId]):
+  def transform = lila.search.spec.Query.study(text, userId.map(_.value))
 
 given OWrites[Query] = Json.writes
