d9d56dbacef9c7f27af597ec7f616d290127f939
Thanh Le <lenguyenthanh@hotmail.com>
2024-05-13 21:19:22 +0700

    Remove unused date formatter

--- modules/search/src/main/model.scala
+++ modules/search/src/main/model.scala
@@ -7,7 +7,3 @@ object From extends OpaqueInt[From]
 
 opaque type Size = Int
 object Size extends OpaqueInt[Size]
-
-object Date:
-  val format    = "yyyy-MM-dd HH:mm:ss"
-  val formatter = java.time.format.DateTimeFormatter.ofPattern(format)
