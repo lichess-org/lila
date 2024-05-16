250a97eadbc530b76c69945ce6008e9c98ba39fc
Ben Rollin <ben.rollin@clever.com>
2024-05-16 03:52:41 -0700

    fix map side view category clicking

--- ui/learn/src/sideCtrl.ts
+++ ui/learn/src/sideCtrl.ts
@@ -1,6 +1,6 @@
 import * as stages from './stage/list';
 import * as scoring from './score';
-import { Prop, prop } from 'common';
+import { Prop, propWithEffect } from 'common';
 import { LearnProgress, LearnOpts } from './learn';
 import { LearnCtrl } from './ctrl';
 
@@ -9,21 +9,21 @@ export class SideCtrl {
   trans: Trans;
   data: LearnProgress;
 
-  categId: Prop<number> = prop(0);
+  categId: Prop<number>;
 
   constructor(ctrl: LearnCtrl, opts: LearnOpts) {
     this.opts = opts;
     this.trans = ctrl.trans;
     this.data = ctrl.data;
 
-    this.updateCategId();
+    this.categId = propWithEffect(this.getCategIdFromStageId(), ctrl.redraw);
   }
 
   reset = () => this.opts.storage.reset();
 
-  activeStageId = () => this.opts.stageId;
-
-  updateCategId = () => this.categId(stages.stageIdToCategId(this.opts.stageId || 1) || this.categId());
+  activeStageId = () => this.opts.stageId || 1;
+  getCategIdFromStageId = () => stages.stageIdToCategId(this.activeStageId()) || this.categId();
+  updateCategId = () => this.categId(this.getCategIdFromStageId());
 
   progress = () => {
     const max = stages.list.length * 10;
