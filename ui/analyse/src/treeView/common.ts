4e598bc47b81d563c9131d2284cc12436b894df0
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2024-06-12 19:03:48 -0700

    Fix autoscroll offset
    We've added "position: relative" to the <line> elements for styling purposes, but that affects the calculation of the "offsetTop" property. We can instead calculate the offset ourselves.

--- ui/analyse/src/treeView/common.ts
+++ ui/analyse/src/treeView/common.ts
@@ -59,7 +59,8 @@ const autoScroll = throttle(200, (ctrl: AnalyseCtrl, el: HTMLElement) => {
     cont.scrollTop = ctrl.path ? 99999 : 0;
     return;
   }
-  cont.scrollTop = target.offsetTop - cont.offsetHeight / 2 + target.offsetHeight;
+  const targetOffset = target.getBoundingClientRect().y - el.getBoundingClientRect().y;
+  cont.scrollTop = targetOffset - cont.offsetHeight / 2 + target.offsetHeight;
 });
 
 export interface NodeClasses {
