f1d34f5a657a2b1d0bfbc4fc6ca045a76c8654c7
Thibault Duplessis <t@lichess.org>
2024-06-25 20:18:08 +0200

    don't add typing that's not actually used
    we only care about the presence of rules, not the rules themselves,
    so let's not hardcode them here

--- ui/challenge/src/interfaces.ts
+++ ui/challenge/src/interfaces.ts
@@ -9,7 +9,6 @@ export interface ChallengeOpts {
 }
 
 type ChallengeStatus = 'created' | 'offline' | 'canceled' | 'declined' | 'accepted';
-type GameRule = 'noAbort' | 'noRematch' | 'noGiveTime' | 'noClaimWin' | 'noEarlyDraw';
 export type ChallengeDirection = 'in' | 'out';
 
 export interface ChallengeUser {
@@ -38,7 +37,7 @@ export interface Challenge {
   status: ChallengeStatus;
   challenger?: ChallengeUser;
   destUser?: ChallengeUser;
-  rules?: GameRule[];
+  rules?: unknown[];
   variant: Variant;
   initialFen: FEN;
   rated: boolean;
