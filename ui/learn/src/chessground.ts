e6aae67c6bc71bb99d344f5297cd9eb1d9464947
Henrique Barros <henriquesb6@hotmail.com>
2024-06-27 00:39:28 -0300

    update coordinates preference to add new type for all squares and use it on puzzles

--- ui/learn/src/chessground.ts
+++ ui/learn/src/chessground.ts
@@ -19,17 +19,18 @@ export default function (ctrl: RunCtrl): VNode {
       insert: vnode => {
         const el = vnode.elm as HTMLElement;
         el.addEventListener('contextmenu', e => e.preventDefault());
-        ctrl.setChessground(site.makeChessground(el, makeConfig()));
+        ctrl.setChessground(site.makeChessground(el, makeConfig(ctrl)));
       },
       destroy: () => ctrl.chessground!.destroy(),
     },
   });
 }
 
-const makeConfig = (): CgConfig => ({
+const makeConfig = (ctrl: RunCtrl): CgConfig => ({
   fen: '8/8/8/8/8/8/8/8',
   blockTouchScroll: true,
   coordinates: true,
+  coordinatesOnSquares: ctrl.pref.coords === Prefs.Coords.All,
   movable: { free: false, color: undefined },
   drawable: { enabled: false },
   draggable: { enabled: true },
