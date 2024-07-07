0031e1d814b5a45f5bff853966fe52b3c58197fe
Thibault Duplessis <t@lichess.org>
2024-07-07 11:40:08 +0200

    fix repeatedly hardcoded relay chat separator

--- ui/analyse/src/study/relay/chatHandler.ts
+++ ui/analyse/src/study/relay/chatHandler.ts
@@ -2,26 +2,28 @@ import { BroadcastChatHandler } from 'chat/src/interfaces';
 import AnalyseCtrl from '../../ctrl';
 
 export function broadcastChatHandler(ctrl: AnalyseCtrl): BroadcastChatHandler {
+  // '\ue666' was arbitrarily chosen from the unicode private use area to separate the text from the chapterId and ply
+  const separator = '\ue666';
+
   const encodeMsg = (text: string): string => {
     if (ctrl.study?.relay && !ctrl.study.relay.tourShow()) {
       const chapterId = ctrl.study.currentChapter().id;
       const ply = ctrl.study.currentNode().ply;
-      // '\ue666' was arbitrarily chosen from the unicode private use area to separate the text from the chapterId and ply
-      text = text + '\ue666' + chapterId + '\ue666' + ply;
+      text = text + separator + chapterId + separator + ply;
     }
     return text;
   };
 
   const cleanMsg = (msg: string): string => {
-    if (msg.includes('\ue666') && ctrl.study?.relay) {
-      return msg.split('\ue666')[0];
+    if (msg.includes(separator) && ctrl.study?.relay) {
+      return msg.split(separator)[0];
     }
     return msg;
   };
 
   const jumpToMove = (msg: string): void => {
-    if (msg.includes('\ue666') && ctrl.study?.relay) {
-      const segs = msg.split('\ue666');
+    if (msg.includes(separator) && ctrl.study?.relay) {
+      const segs = msg.split(separator);
       if (segs.length == 3) {
         const [_, chapterId, ply] = segs;
         ctrl.study.setChapter(chapterId);
@@ -31,8 +33,8 @@ export function broadcastChatHandler(ctrl: AnalyseCtrl): BroadcastChatHandler {
   };
 
   const canJumpToMove = (msg: string): string | null => {
-    if (msg.includes('\ue666') && ctrl.study?.relay) {
-      const segs = msg.split('\ue666');
+    if (msg.includes(separator) && ctrl.study?.relay) {
+      const segs = msg.split(separator);
       if (segs.length == 3) {
         const [_, chapterId, ply] = segs;
         return `${chapterId}#${ply}`;
