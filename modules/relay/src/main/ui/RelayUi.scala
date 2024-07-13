cfdd1bb27cdd3713226173baa831483f90972c90
Sérgio Glórias <sergioglorias@proton.me>
2024-07-13 00:16:51 +0000

    Add image the relay to embed
    image in the relay is the cover of the event
    we will show it in embed

--- modules/relay/src/main/ui/RelayUi.scala
+++ modules/relay/src/main/ui/RelayUi.scala
@@ -48,9 +48,12 @@ final class RelayUi(helpers: Helpers)(
       )
       .zoom
       .graph(
-        title = rt.fullName,
-        url = s"$netBaseUrl${rt.path}",
-        description = shorten(rt.tour.info.toString, 152)
+        OpenGraph(
+          title = rt.fullName,
+          url = s"$netBaseUrl${rt.path}",
+          description = shorten(rt.tour.info.toString, 152),
+          image = rt.tour.image.flatMap(imgId => Some(thumbnail.url(imgId, _.Size.Large)))
+        )
       ):
         main(cls := "analyse is-relay has-relay-tour")(
           div(cls := "box relay-tour")(
