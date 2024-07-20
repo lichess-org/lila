a3dd89a1e163078a7785b937ad8eb4785a9ae713
Thibault Duplessis <t@lichess.org>
2024-07-20 12:16:34 +0200

    game blurs tests

---
+++ modules/game/src/test/BlurTest.scala
@@ -0,0 +1,28 @@
+package lila.game
+
+import lila.core.game.Blurs
+import lila.game.Blurs.{ given, * }
+import lila.db.BSON
+import reactivemongo.api.bson.*
+
+class BlurTest extends munit.FunSuite:
+
+  def fromMoveIndexes(indexes: Iterable[Int]): Blurs =
+    indexes.foldLeft(Blurs(0L)): (b, i) =>
+      b.addAtMoveIndex(i)
+
+  test("addAtMoveIndex"):
+    assertEquals(fromMoveIndexes(List(1, 2, 3, 6, 8)).binaryString, "011100101")
+
+  val lilBlur = fromMoveIndexes(0 until 4)
+  val bigBlur = fromMoveIndexes(0 until 40)
+
+  test("BSON handler, lil blur"):
+    val bson = blursHandler.writeOpt(lilBlur).get
+    assertEquals(bson, BSONInteger(15))
+    assertEquals(lilBlur, blursHandler.readOpt(bson).get)
+
+  test("BSON handler, big blur"):
+    val bson = blursHandler.writeOpt(bigBlur).get
+    assertEquals(bson, BSONLong(1099511627775L))
+    assertEquals(bigBlur, blursHandler.readOpt(bson).get)
