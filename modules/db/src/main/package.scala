b3160dce07ccee08101009e189ea9bf8b90f8cf5
Thanh Le <lenguyenthanh@hotmail.com>
2024-07-20 21:14:52 +0700

    Add NoBSONReader

--- modules/db/src/main/package.scala
+++ modules/db/src/main/package.scala
@@ -5,7 +5,8 @@ import reactivemongo.api.commands.WriteResult
 export lila.core.lilaism.Lilaism.{ *, given }
 export lila.common.extensions.*
 
-trait NoDbHandler[A] // don't create default BSON handlers for this type
+trait NoDbHandler[A]  // don't create default BSONWiter for this type
+trait NoBSONReader[A] // don't create default BSONReader for this type
 
 def recoverDuplicateKey[A](f: WriteResult => A): PartialFunction[Throwable, A] =
   case wr: WriteResult if isDuplicateKey(wr) => f(wr)
