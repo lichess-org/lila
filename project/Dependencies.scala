d8a2290ac8ccaa2fe8ffc665cef5a949585d9d42
Thanh Le <lenguyenthanh@hotmail.com>
2024-08-17 15:26:08 +0200

    scalalib 11.2.5 and scalachess 16.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -42,7 +42,7 @@ object Dependencies {
   }
 
   object chess {
-    val version  = "16.2.3"
+    val version  = "16.2.4"
     val core     = "org.lichess" %% "scalachess"           % version
     val testKit  = "org.lichess" %% "scalachess-test-kit"  % version % Test
     val playJson = "org.lichess" %% "scalachess-play-json" % version
@@ -50,7 +50,7 @@ object Dependencies {
   }
 
   object scalalib {
-    val version  = "11.2.4"
+    val version  = "11.2.5"
     val core     = "org.lichess" %% "scalalib-core"      % version
     val model    = "org.lichess" %% "scalalib-model"     % version
     val playJson = "org.lichess" %% "scalalib-play-json" % version
