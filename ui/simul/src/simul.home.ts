ff7411b6525a9fd87cc1edf67eff7a67c57a9730
Jonathan Gamble <gamblej@gmail.com>
2024-09-12 10:05:36 -0500

    fetch updates on socket.in.reload

--- ui/simul/src/simul.home.ts
+++ ui/simul/src/simul.home.ts
@@ -1,9 +1,11 @@
 import StrongSocket from 'common/socket';
 
-site.load.then(async() => {
+site.load.then(() => {
   site.socket = new StrongSocket(`/socket/v5`, false, { params: { flag: 'simul' } });
-  const rsp = await fetch('/simul/reload');
-  const html = await rsp.text();
-  $('.simul-list__content').html(html);
-  site.pubsub.emit('content-loaded');
+  site.pubsub.on('socket.in.reload', async() => {
+    const rsp = await fetch('/simul/reload');
+    const html = await rsp.text();
+    $('.simul-list__content').html(html);
+    site.pubsub.emit('content-loaded');
+  });
 });
