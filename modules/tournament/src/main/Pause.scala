lichess-org/lila

commit c8059e97d696d0bc3056f42d3001ab240a408cba

Rewrite infix by hand
Get rid of ugly back sticks

modules/tournament/src/main/Pause.scala
@@ -21,7 +21,7 @@ final private class Pause:
   private def delayOf(record: Record, tour: Tournament) =
     // 10s for first pause
     // next ones increasing linearly until 120s
-    baseDelayOf(tour).map(_ * (record.pauses - 1) `atLeast` 10 `atMost` 120)
+    baseDelayOf(tour).map(_ * (record.pauses - 1).atLeast(10).atMost(120))
 
   def add(userId: UserId): Unit =
     cache.put(

app/controllers/Account.scala
@@ -328,7 +328,7 @@ final class Account(
   ): Fu[Frag] =
     renderAsync:
       env.security.forms.reopen.map: baseForm =>
-        html.account.reopen.form(form.foldLeft(baseForm)(_ `withForm` _), msg)
+        html.account.reopen.form(form.foldLeft(baseForm)(_.withForm(_)), msg)
 
   def reopen = Open:
     auth.RedirectToProfileIfLoggedIn:

app/controllers/Auth.scala
@@ -309,7 +309,7 @@ final class Auth(
   private def renderPasswordReset(form: Option[Form[PasswordReset]], fail: Boolean)(using ctx: Context) =
     renderAsync:
       env.security.forms.passwordReset.map: baseForm =>
-        html.auth.bits.passwordReset(form.foldLeft(baseForm)(_ `withForm` _), fail)
+        html.auth.bits.passwordReset(form.foldLeft(baseForm)(_.withForm(_)), fail)
 
   def passwordReset = Open:
     renderPasswordReset(none, fail = false).map { Ok(_) }
@@ -390,7 +390,7 @@ final class Auth(
   private def renderMagicLink(form: Option[Form[MagicLink]], fail: Boolean)(using Context) =
     renderAsync:
       env.security.forms.magicLink.map: baseForm =>
-        html.auth.bits.magicLink(form.foldLeft(baseForm)(_ `withForm` _), fail)
+        html.auth.bits.magicLink(form.foldLeft(baseForm)(_.withForm(_)), fail)
 
   def magicLink = Open:
     Firewall:

app/controllers/Clas.scala
@@ -191,7 +191,7 @@ final class Clas(env: Env, authC: Auth) extends LilaController(env):
             env.clas.api.student.activeWithUsers(clas).flatMap { students =>
               Reasonable(clas, students, "notify"):
                 val url  = routes.Clas.show(clas.id.value).url
-                val full = if text `contains` url then text else s"$text\n\n${env.net.baseUrl}$url"
+                val full = if text.contains(url) then text else s"$text\n\n${env.net.baseUrl}$url"
                 env.msg.api
                   .multiPost(Source(students.map(_.user.id)), full)
                   .addEffect: nb =>

app/controllers/Plan.scala
@@ -275,7 +275,7 @@ final class Plan(env: Env) extends LilaController(env):
                     JsonOk(Json.obj("subscription" -> Json.obj("id" -> sub.id.value)))
               else
                 for
-                  gifted <- checkout.giftTo.filterNot(ctx `is` _).so(env.user.repo.enabledById)
+                  gifted <- checkout.giftTo.filterNot(ctx.is(_)).so(env.user.repo.enabledById)
                   // customer <- env.plan.api.userCustomer(me)
                   order <- env.plan.api.payPal.createOrder(checkout, me, gifted)
                 yield JsonOk(Json.obj("order" -> Json.obj("id" -> order.id.value)))

app/controllers/Swiss.scala
@@ -90,7 +90,7 @@ final class Swiss(
         case _           => notFoundJson()
 
   private def isUserInTheTeam(teamId: lila.team.TeamId)(user: UserId) =
-    env.team.cached.teamIds(user).dmap(_ `contains` teamId)
+    env.team.cached.teamIds(user).dmap(_.contains(teamId))
 
   private def cachedSwissAndTeam(id: SwissId): Fu[Option[(SwissModel, LightTeam)]] =
     env.swiss.cache.swissCache

app/templating/AssetHelper.scala
@@ -110,7 +110,7 @@ if (window.matchMedia('(prefers-color-scheme: dark)').media === 'not all')
     )
 
   def defaultCsp(using ctx: PageContext): ContentSecurityPolicy =
-    ctx.nonce.foldLeft(basicCsp)(_ `withNonce` _)
+    ctx.nonce.foldLeft(basicCsp)(_.withNonce(_))
 
   def analysisCsp(using PageContext): ContentSecurityPolicy =
     defaultCsp.withWebAssembly.withExternalEngine(env.externalEngineEndpoint)

modules/api/src/main/Env.scala
@@ -130,7 +130,7 @@ final class Env(
       promise.completeWith(chatFreshness.of(source))
     },
     "announce" -> {
-      case Announce(msg, date, _) if msg `contains` "will restart" => pagerDuty.lilaRestart(date)
+      case Announce(msg, date, _) if msg.contains("will restart") => pagerDuty.lilaRestart(date)
     },
     "lpv" -> {
       case AllPgnsFromText(text, p)       => p.completeWith(textLpvExpand.allPgnsFromText(text))

commit 610dc6de00f49ce52017d49ba5ce503341095d0a

fix tournament pause duration
broken by operator precedence since:
https://github.com/lichess-org/lila/commit/c8059e97d696d0bc3056f42d3001ab240a408cba#diff-17ba44ea574ad025f147c1724a486e35e856df0acbcdb0551e66036a4809991d

modules/tournament/src/main/Pause.scala
@@ -21,7 +21,8 @@ final private class Pause(using Executor):
   private def delayOf(record: Record, tour: Tournament) =
     // 10s for first pause
     // next ones increasing linearly until 120s
-    baseDelayOf(tour).map(_ * (record.pauses - 1).atLeast(10).atMost(120))
+    baseDelayOf(tour).map: delay =>
+      (delay * (record.pauses - 1)).atLeast(10).atMost(120)
 
   def add(userId: UserId): Unit =
     cache.put(