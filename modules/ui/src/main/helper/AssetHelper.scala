7d30a24bbe05dae2f49b0c2e5827abd06cbd1fac
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-10-10 19:17:01 -0400

    convert to top level esm module

--- modules/ui/src/main/helper/AssetHelper.scala
+++ modules/ui/src/main/helper/AssetHelper.scala
@@ -59,7 +59,7 @@ trait AssetHelper:
 
   def iconFlair(flair: Flair): Tag = img(cls := "icon-flair", src := flairSrc(flair))
 
-  def fingerprintTag: EsmList = esmInit("bits.fipr")
+  def fingerprintTag: EsmList = Esm("bits.fipr")
 
   def hcaptchaScript(re: lila.core.security.HcaptchaForm[?]): EsmList =
     re.enabled.so(esmInitBit("hcaptcha"))
--- ui/bits/src/bits.fipr.ts
+++ ui/bits/src/bits.fipr.ts
@@ -1,26 +1,24 @@
 import FingerprintJS from '@fingerprintjs/fingerprintjs';
 
-export function initModule(): void {
-  site.load.then(() =>
-    setTimeout(async () => {
-      const storageKey = 'fipr';
-      const t = performance.now();
-      const send = (hash: string) => {
-        localStorage.setItem(storageKey, hash);
-        const $i = $('#signup-fp-input');
-        if ($i.length) $i.val(hash);
-        else
-          fetch('/auth/set-fp/' + hash + '/' + Math.round(performance.now() - t), {
-            method: 'post',
-            credentials: 'same-origin',
-          });
-      };
-      const existingFp = localStorage.getItem(storageKey);
-      if (existingFp) send(existingFp);
-      else {
-        const fipr = await FingerprintJS.load();
-        fipr.get().then(result => send(result.visitorId));
-      }
-    }, 1000),
-  );
-}
+site.load.then(() =>
+  setTimeout(async () => {
+    const storageKey = 'fipr';
+    const t = performance.now();
+    const send = (hash: string) => {
+      localStorage.setItem(storageKey, hash);
+      const $i = $('#signup-fp-input');
+      if ($i.length) $i.val(hash);
+      else
+        fetch('/auth/set-fp/' + hash + '/' + Math.round(performance.now() - t), {
+          method: 'post',
+          credentials: 'same-origin',
+        });
+    };
+    const existingFp = localStorage.getItem(storageKey);
+    if (existingFp) send(existingFp);
+    else {
+      const fipr = await FingerprintJS.load();
+      fipr.get().then(result => send(result.visitorId));
+    }
+  }, 1000),
+);
