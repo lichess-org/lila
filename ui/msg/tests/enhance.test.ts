8e3c1d2c773d9567e9237b97c1c2f04f567ce6c4
Thibault Duplessis <t@lichess.org>
2024-10-13 09:14:12 +0200

    update ui/msg tests

--- ui/msg/tests/enhance.test.ts
+++ ui/msg/tests/enhance.test.ts
@@ -8,7 +8,7 @@ describe('test imgur matching', () => {
   ])('embed individual images', (input, link) => {
     expect(imgurRegex.test(input)).toBe(true);
     expect(enhance(input)).toBe(
-      `<a target="_blank" rel="noopener nofollow noreferrer" href="${link}"><img src="${link}"/></a>`,
+      `<a target="_blank" rel="nofollow noopener noreferrer" href="${link}"><img src="${link}"/></a>`,
     );
   });
 
@@ -21,7 +21,7 @@ describe('test imgur matching', () => {
   ])('albums and galleries should not be embedded', (input, link) => {
     expect(imgurRegex.test(input)).toBe(false);
     expect(enhance(input)).toBe(
-      `<a target="_blank" rel="noopener nofollow noreferrer" href="${input}">${link}</a>`,
+      `<a target="_blank" rel="nofollow noopener noreferrer" href="${input}">${link}</a>`,
     );
   });
 });
