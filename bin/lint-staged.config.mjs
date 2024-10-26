49651af368b878bcc76c32d1bb0a4ec24b24f02a
Jonathan Gamble <gamblej@gmail.com>
2024-10-26 15:42:15 -0500

    lint-staged doesnt actually support async, their github lies

--- bin/lint-staged.config.mjs
+++ bin/lint-staged.config.mjs
@@ -1,19 +1,8 @@
-import { lstat } from 'fs/promises';
-
-async function filterSymlinks(files) {
-  const checks = await Promise.all(
-    files.map(async file =>
-      lstat(file)
-        .then(s => s && !s.isSymbolicLink() && file)
-        .catch(() => null),
-    ),
-  );
-  return checks.filter(Boolean);
-}
+import { lstatSync } from 'fs';
 
 export default {
-  '*.{json,scss,ts}': async files => {
-    const regularFiles = await filterSymlinks(files);
+  '*.{json,scss,ts}': files => {
+    const regularFiles = files.filter(f => !lstatSync(f).isSymbolicLink());
     return regularFiles.length ? `prettier --write ${regularFiles.join(' ')}` : 'true';
   },
 };
