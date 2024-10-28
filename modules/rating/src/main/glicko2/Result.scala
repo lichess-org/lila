1f2aff785fbe7c8b11d07b1aa7949624c87ab9ca
Daniel Dugovic <ddugovic@users.noreply.github.com>
2024-10-28 00:53:44 -0500

    Code cleanup

--- modules/rating/src/main/glicko2/Result.scala
+++ modules/rating/src/main/glicko2/Result.scala
@@ -2,22 +2,23 @@ package lila.rating.glicko2
 
 trait Result:
 
-  def getAdvantage(advantage: Double, player: Rating): Double
+  val first: Rating
+  val second: Rating
+
+  def getAdvantage(advantage: Double, player: Rating): Double =
+    if player == first then advantage / 2.0d else -advantage / 2.0d
 
   def getScore(player: Rating): Double
 
   def getOpponent(player: Rating): Rating
 
-  def participated(player: Rating): Boolean
+  def participated(player: Rating): Boolean = player == first || player == second
 
-  def players: List[Rating]
+  def players: List[Rating] = List(first, second)
 
-class BinaryResult(first: Rating, second: Rating, score: Boolean) extends Result:
+class BinaryResult(val first: Rating, val second: Rating, score: Boolean) extends Result:
   private val POINTS_FOR_WIN  = 1.0d
   private val POINTS_FOR_LOSS = 0.0d
-
-  def getAdvantage(advantage: Double, player: Rating) =
-    if player == first then advantage / 2.0d else -advantage / 2.0d
 
   def getScore(p: Rating) = if p == first then if score then POINTS_FOR_WIN else POINTS_FOR_LOSS
   else if score then POINTS_FOR_LOSS
@@ -25,21 +26,10 @@ class BinaryResult(first: Rating, second: Rating, score: Boolean) extends Result
 
   def getOpponent(p: Rating) = if p == first then second else first
 
-  def participated(p: Rating) = p == first || p == second
-
-  def players = List(first, second)
-
-final class GameResult(first: Rating, second: Rating, outcome: Option[Boolean]) extends Result:
+final class GameResult(val first: Rating, val second: Rating, outcome: Option[Boolean]) extends Result:
   private val POINTS_FOR_WIN  = 1.0d
   private val POINTS_FOR_DRAW = 0.5d
   private val POINTS_FOR_LOSS = 0.0d
-
-  def players = List(first, second)
-
-  def participated(player: Rating) = player == first || player == second
-
-  def getAdvantage(advantage: Double, player: Rating) =
-    if player == first then advantage / 2.0d else -advantage / 2.0d
 
   /** Returns the "score" for a match.
     *
