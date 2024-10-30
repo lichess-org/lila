361fe3ef1c00100cff5142b33c6a145c1288acfa
Thibault Duplessis <t@lichess.org>
2024-10-30 14:19:08 +0100

    temporarily reinstate site.pubsub

--- ui/site/src/site.ts
+++ ui/site/src/site.ts
@@ -7,12 +7,14 @@ import { unload, redirect, reload } from './reload';
 import announce from './announce';
 import { displayLocale } from 'common/i18n';
 import sound from './sound';
+import { pubsub } from 'common/pubsub';
 
 const site = window.site;
 // site.load is initialized in layout.scala embedded script tags
 // site.manifest is fetched
 // site.info, site.debug are populated by ui/build
 // site.socket, site.quietMode, site.analysis are set elsewhere
+(site as any).pubsub = pubsub; // do not declare in index.d.ts. some extensions need this here
 site.sri = randomToken();
 site.displayLocale = displayLocale;
 site.blindMode = document.body.classList.contains('blind-mode');
