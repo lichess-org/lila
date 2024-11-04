1cdb38707f91295be8a0d287f0533174e4a78be4
kraktus <kraktus@users.noreply.github.com>
2024-11-04 15:02:48 +0100

    ts more correct type checking

--- ui/analyse/src/study/notif.ts
+++ ui/analyse/src/study/notif.ts
@@ -7,12 +7,12 @@ interface Notif {
 
 export class NotifCtrl {
   current: Notif | undefined;
-  timeout: number;
+  timeoutId: number | undefined;
   constructor(readonly redraw: () => void) {}
   set = (n: Notif) => {
-    clearTimeout(this.timeout);
+    clearTimeout(this.timeoutId);
     this.current = n;
-    this.timeout = setTimeout(() => {
+    this.timeoutId = setTimeout(() => {
       this.current = undefined;
       this.redraw();
     }, n.duration);
