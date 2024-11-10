021a70a62978a7f45a90d1446954d0ac8954410e
Thanh Le <lenguyenthanh@hotmail.com>
2024-11-10 12:20:39 +0100

    Bump lila-search 3.1.0
    Remove all write endpoint from lila-search client as we use
    lila-search-ingestor for indexing data

--- modules/search/src/main/LilaSearchClient.scala
+++ modules/search/src/main/LilaSearchClient.scala
@@ -5,35 +5,6 @@ import lila.search.spec.*
 
 class LilaSearchClient(client: SearchClient, writeable: Boolean)(using Executor) extends SearchClient:
 
-  override def storeBulkTeam(sources: List[TeamSourceWithId]): Future[Unit] =
-    client.storeBulkTeam(sources)
-
-  override def storeBulkStudy(sources: List[StudySourceWithId]): Future[Unit] =
-    client.storeBulkStudy(sources)
-
-  override def storeBulkGame(sources: List[GameSourceWithId]): Future[Unit] =
-    client.storeBulkGame(sources)
-
-  override def storeBulkForum(sources: List[ForumSourceWithId]): Future[Unit] =
-    client.storeBulkForum(sources)
-
-  override def store(id: String, source: Source): Future[Unit] =
-    writeable.so:
-      monitor("store", source.index):
-        client.store(id, source)
-
-  override def refresh(index: Index): Future[Unit] =
-    client.refresh(index)
-
-  override def mapping(index: Index): Future[Unit] =
-    client.mapping(index)
-
-  override def deleteById(index: Index, id: String): Future[Unit] =
-    writeable.so(client.deleteById(index, id))
-
-  override def deleteByIds(index: Index, ids: List[Id]): Future[Unit] =
-    writeable.so(client.deleteByIds(index, ids))
-
   override def count(query: Query): Future[CountOutput] =
     monitor("count", query.index):
       client
@@ -61,10 +32,3 @@ class LilaSearchClient(client: SearchClient, writeable: Boolean)(using Executor)
       case q: Query.Game  => "game"
       case q: Query.Study => "study"
       case q: Query.Team  => "team"
-
-  extension (source: Source)
-    def index = source match
-      case s: Source.ForumCase => "forum"
-      case s: Source.GameCase  => "game"
-      case s: Source.StudyCase => "study"
-      case s: Source.TeamCase  => "team"
