937a618e8d469cc9218e5394501a59ecc9f98149
Jonathan Gamble <101470903+schlawg@users.noreply.github.com>
2024-11-09 18:36:36 -0600

    remove unintentional code

--- ui/site/src/sound.ts
+++ ui/site/src/sound.ts
@@ -2,7 +2,7 @@ import { storage } from 'common/storage';
 import { isIOS } from 'common/device';
 import { throttle } from 'common/timing';
 import { defined } from 'common';
-import { charRole } from 'common/chessDefs';
+import { charRole } from 'chess';
 
 type Name = string;
 type Path = string;
@@ -139,9 +139,7 @@ export default new (class implements SoundI {
   say = (text: string, cut = false, force = false, translated = false) => {
     if (typeof window.speechSynthesis === 'undefined') return false;
     try {
-      if (cut || !speechSynthesis.speaking || !speechSynthesis.pending) {
-        speechSynthesis.cancel();
-      }
+      if (cut) speechSynthesis.cancel();
       if (!this.speech() && !force) return false;
       const msg = new SpeechSynthesisUtterance(text);
       msg.volume = this.getVolume();
@@ -168,7 +166,7 @@ export default new (class implements SoundI {
 
   set = () => this.theme;
 
-  saySan(san?: San, cut?: boolean, force?: boolean) {
+  saySan(san?: San, cut?: boolean) {
     const text = !san
       ? 'Game start'
       : san.includes('O-O-O#')
@@ -202,7 +200,7 @@ export default new (class implements SoundI {
                       .replace(/C /, 'c ') // Capital C is pronounced as "degrees celsius" when it comes after a number (e.g. R8c3)
                       .replace(/F /, 'f ') // Capital F is pronounced as "degrees fahrenheit" when it comes after a number (e.g. R8f3)
                       .replace(/(\d) H (\d)/, '$1H$2'); // "H" is pronounced as "hour" when it comes after a number with a space (e.g. Rook 5 H 3)
-    this.say(text, cut, force);
+    this.say(text, cut);
   }
 
   preloadBoardSounds() {
