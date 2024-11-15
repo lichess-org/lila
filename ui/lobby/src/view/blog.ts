34269581a2aff9498b5c4ba037a1e6943529367f
johndoknjas <jdoknjas@sfu.ca>
2024-11-15 03:06:42 -0800

    Some cleanup.

--- ui/lobby/src/view/blog.ts
+++ ui/lobby/src/view/blog.ts
@@ -4,33 +4,31 @@ export function rotateBlogs() {
   document.querySelectorAll<HTMLElement>('.lobby__blog').forEach(el => {
     const style = window.getComputedStyle(el);
     const gridGap = parseFloat(style.columnGap) * (style.columnGap.endsWith('%') ? el.clientWidth / 100 : 1);
-    const kids = Array.from(el.children) as HTMLElement[];
+    const kids = el.children as HTMLCollectionOf<HTMLElement>;
     const visible = Math.floor((el.clientWidth + gridGap) / (192 + gridGap));
     const colW = (el.clientWidth - gridGap * (visible - 1)) / visible;
     let startX = 0;
     let isDragging = false;
-    let isMouseDown = false;
     let currentCard: HTMLElement | null = null;
-
     el.style.gridTemplateColumns = `repeat(7, ${colW}px)`;
 
-    const rotateBlogInner = () => {
-      kids.forEach(kid => {
-        kid.style.transition = 'transform 0.6s ease';
-        kid.style.transform = `translateX(-${Math.floor(colW + gridGap)}px)`;
-      });
-
-      setTimeout(() => {
-        el.append(el.firstChild!);
-        fix();
-      }, 500); // Ensure this matches transition duration
+    const translate_cards = (translate_amt: number, transition: string = '') => {
+      for (let i = 0; i < kids.length; i++) {
+        kids[i].style.transition = transition;
+        kids[i].style.transform = `translateX(${translate_amt}px)`;
+      }
     };
 
     const fix = () => {
-      kids.forEach(kid => {
-        kid.style.transition = '';
-        kid.style.transform = 'translateX(0)';
-      });
+      translate_cards(0);
+    };
+
+    const rotateBlogInner = () => {
+      translate_cards(-Math.floor(colW + gridGap), 'transform 0.6s ease');
+      setTimeout(() => {
+        el.append(el.firstChild!);
+        fix();
+      }, 500);
     };
 
     fix();
@@ -47,28 +45,20 @@ export function rotateBlogs() {
     };
 
     const stopDragging = (xPosition: number) => {
-      if (currentCard) {
-        // Smoothly snap back all cards
-        kids.forEach(kid => {
-          kid.style.transition = 'transform 0.3s ease-out'; // Smooth transition back
-          kid.style.transform = 'translateX(0)';
-        });
-      }
       if (isDragging && currentCard) {
+        fix();
         const factor = (startX - xPosition) / (colW + gridGap);
         shiftCards(Math.max(0, Math.min(3, Math.ceil(factor - 0.7))));
       }
       isDragging = false;
       currentCard = null;
-      isMouseDown = false;
       clearInterval(blogRotateTimer);
       blogRotateTimer = setInterval(rotateBlogInner, 10000); // Resume auto-rotation
     };
 
     el.addEventListener('mousedown', event => {
-      event.preventDefault(); // Prevent default behavior to avoid red slash cursor
+      event.preventDefault();
       startX = event.clientX;
-      isMouseDown = true;
       currentCard = (event.target as HTMLElement).closest('.lobby__blog > *') as HTMLElement;
       if (currentCard) {
         clearInterval(blogRotateTimer); // Pause auto-rotation while dragging
@@ -76,12 +66,9 @@ export function rotateBlogs() {
     });
 
     el.addEventListener('mousemove', event => {
-      if (isMouseDown && currentCard) {
+      if (currentCard) {
         isDragging = true;
-        kids.forEach(kid => {
-          kid.style.transition = 'transform 0.1s ease';
-          kid.style.transform = `translateX(${event.clientX - startX}px)`;
-        });
+        translate_cards(Math.min(0, event.clientX - startX));
       }
     });
 
