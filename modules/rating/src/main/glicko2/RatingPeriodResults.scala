f9e34be195bcaa46b2aecd97a6bad0460f49ad1c
Daniel Dugovic <ddugovic@users.noreply.github.com>
2024-11-18 05:20:26 -0600

    Attempt to fix type errors

--- modules/rating/src/main/glicko2/RatingPeriodResults.scala
+++ modules/rating/src/main/glicko2/RatingPeriodResults.scala
@@ -2,3 +2,6 @@ package lila.rating.glicko2
 
 // rewrite from java https://github.com/goochjs/glicko2
 type RatingPeriodResults[R <: Result] = Map[Rating, List[R]]
+
+object RatingPeriodResults:
+  def apply[R <: Result]() = Map[Rating, List[R]]()
