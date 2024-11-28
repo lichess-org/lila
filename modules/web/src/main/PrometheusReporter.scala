5db493e5d2378c63f69493a4b51c3f85cd088f46
Thibault Duplessis <t@lichess.org>
2024-11-28 22:03:32 +0100

    remove unused prometheus embedded server

--- modules/web/src/main/PrometheusReporter.scala
+++ modules/web/src/main/PrometheusReporter.scala
@@ -4,24 +4,23 @@ import java.time.Duration
 
 import com.typesafe.config.Config
 import kamon.metric.*
-import kamon.module.{ MetricReporter, Module, ModuleFactory }
+import kamon.module.{ MetricReporter, Module }
 import kamon.prometheus.PrometheusReporter.DefaultConfigPath
-import kamon.prometheus.embeddedhttp.EmbeddedHttpServer
 import kamon.prometheus.*
 import kamon.Kamon
 
 import org.slf4j.LoggerFactory
 
 // copy paste with some minor changes/scalafmtAll from https://github.com/kamon-io/Kamon/blob/master/reporters/kamon-prometheus/src/main/scala/kamon/prometheus/PrometheusReporter.scala
+// unused http server has been removed
 class PrometheusReporter(configPath: String = DefaultConfigPath, initialConfig: Config = Kamon.config())
     extends MetricReporter
     with ScrapeSource:
 
-  import PrometheusReporter.Settings.readSettings
+  import PrometheusReporter.readSettings
   import kamon.prometheus.PrometheusSettings.environmentTags
 
   private val _logger = LoggerFactory.getLogger(classOf[PrometheusReporter])
-  private var _embeddedHttpServer: Option[EmbeddedHttpServer] = None
 
   private val stalePeriod = Duration.ofSeconds(2 * 24 * 60 * 60 + 1) // 2 days + 1 second
   private val _snapshotAccumulator =
@@ -34,8 +33,6 @@ class PrometheusReporter(configPath: String = DefaultConfigPath, initialConfig: 
   @volatile private var _reporterSettings = readSettings(initialConfig.getConfig(configPath))
 
   override def stop(): Unit =
-    stopEmbeddedServerIfStarted()
-
     // Removes a reference to the last reporter to avoid leaking instances.
     //
     // It might not be safe to assume that **this** object is the last created instance, but in practice
@@ -46,8 +43,6 @@ class PrometheusReporter(configPath: String = DefaultConfigPath, initialConfig: 
   override def reconfigure(newConfig: Config): Unit =
     _reporterSettings = readSettings(newConfig.getConfig(configPath))
     _config = newConfig
-    stopEmbeddedServerIfStarted()
-    startEmbeddedServerIfEnabled()
 
   override def reportPeriodSnapshot(snapshot: PeriodSnapshot): Unit =
     _snapshotAccumulator.add(snapshot)
@@ -68,34 +63,6 @@ class PrometheusReporter(configPath: String = DefaultConfigPath, initialConfig: 
   def scrapeData(): String =
     _preparedScrapeData
 
-  private def startEmbeddedServerIfEnabled(): Unit =
-    if _reporterSettings.startEmbeddedServer then
-      val server = _reporterSettings
-        .createEmbeddedHttpServerClass()
-        .getConstructor(
-          Array[Class[?]](
-            classOf[String],
-            classOf[Int],
-            classOf[String],
-            classOf[ScrapeSource],
-            classOf[Config]
-          )*
-        )
-        .newInstance(
-          _reporterSettings.embeddedServerHostname,
-          _reporterSettings.embeddedServerPort: Integer,
-          _reporterSettings.embeddedServerPath,
-          this,
-          _config
-        )
-      _logger.info(
-        s"Started the embedded HTTP server on http://${_reporterSettings.embeddedServerHostname}:${_reporterSettings.embeddedServerPort}${_reporterSettings.embeddedServerPath}"
-      )
-      _embeddedHttpServer = Some(server)
-
-  private def stopEmbeddedServerIfStarted(): Unit =
-    _embeddedHttpServer.foreach(_.stop())
-
 object PrometheusReporter:
 
   final val DefaultConfigPath = "kamon.prometheus"
@@ -113,37 +80,12 @@ object PrometheusReporter:
   def latestScrapeData(): Option[String] =
     _lastCreatedInstance.map(_.scrapeData())
 
-  class Factory extends ModuleFactory:
-    override def create(settings: ModuleFactory.Settings): Module =
-      val reporter = new PrometheusReporter(DefaultConfigPath, settings.config)
-      _lastCreatedInstance = Some(reporter)
-      reporter
-
   def create(): PrometheusReporter =
     new PrometheusReporter()
 
-  case class Settings(
-      startEmbeddedServer: Boolean,
-      embeddedServerHostname: String,
-      embeddedServerPort: Int,
-      embeddedServerPath: String,
-      embeddedServerImpl: String,
-      generic: PrometheusSettings.Generic
-  ):
-    def createEmbeddedHttpServerClass(): Class[? <: EmbeddedHttpServer] =
-      val clz = embeddedServerImpl match
-        case "sun" => "kamon.prometheus.embeddedhttp.SunEmbeddedHttpServer"
-        case fqcn  => fqcn
-      Class.forName(clz).asInstanceOf[Class[? <: EmbeddedHttpServer]]
+  case class Settings(generic: PrometheusSettings.Generic)
 
-  object Settings:
-
-    def readSettings(prometheusConfig: Config): PrometheusReporter.Settings =
-      PrometheusReporter.Settings(
-        startEmbeddedServer = prometheusConfig.getBoolean("start-embedded-http-server"),
-        embeddedServerHostname = prometheusConfig.getString("embedded-server.hostname"),
-        embeddedServerPort = prometheusConfig.getInt("embedded-server.port"),
-        embeddedServerPath = prometheusConfig.getString("embedded-server.metrics-path"),
-        embeddedServerImpl = prometheusConfig.getString("embedded-server.impl"),
-        generic = PrometheusSettings.readSettings(prometheusConfig)
-      )
+  def readSettings(prometheusConfig: Config): PrometheusReporter.Settings =
+    PrometheusReporter.Settings(
+      generic = PrometheusSettings.readSettings(prometheusConfig)
+    )
