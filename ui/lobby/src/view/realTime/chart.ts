2fd9264897eff063207f3dcf0fc60ad719052233
John Doknjas <jdoknjas@sfu.ca>
2024-12-06 08:36:42 +0000

    less than or equal

--- ui/lobby/src/view/realTime/chart.ts
+++ ui/lobby/src/view/realTime/chart.ts
@@ -31,7 +31,7 @@ const clockX = (dur: number) => {
 };
 
 function renderPlot(ctrl: LobbyController, hook: Hook) {
-  const isMobilePhone = window.screen.width < 480;
+  const isMobilePhone = window.screen.width <= 480;
   const bottom = Math.max(0, ratingY(hook.rating) - (isMobilePhone ? 3 : 2)),
     left = Math.max(0, clockX(hook.t) - (isMobilePhone ? 2.75 : 2)),
     klass = [
