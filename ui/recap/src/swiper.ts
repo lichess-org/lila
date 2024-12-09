22fc3e7d28293d9aa23fa22932f17c36761088f7
Thibault Duplessis <t@lichess.org>
2024-12-09 11:53:15 +0100

    recap UI

--- ui/recap/src/swiper.ts
+++ ui/recap/src/swiper.ts
@@ -1,38 +1,54 @@
 import Swiper from 'swiper';
 import { SwiperOptions } from 'swiper/types';
-import { Pagination } from 'swiper/modules';
+import * as mod from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/pagination';
+import 'swiper/css/bundle';
 import { Recap } from './interfaces';
 import { animateNumber } from './ui';
 
 export const makeSwiper =
   (_recap: Recap) =>
   (element: HTMLElement): void => {
+    const progressCircle = element.querySelector('.autoplay-progress svg') as SVGElement;
+    const progressContent = element.querySelector('.autoplay-progress span') as HTMLSpanElement;
     const options: SwiperOptions = {
-      modules: [Pagination],
+      modules: [mod.Pagination, mod.Navigation, mod.Keyboard, mod.Mousewheel, mod.Autoplay],
       initialSlide: 0,
-      direction: 'vertical',
+      direction: 'horizontal',
       loop: false,
-      cssMode: true,
+      cssMode: false,
+      grabCursor: true,
       keyboard: { enabled: true },
       mousewheel: true,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
       },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: true,
+      },
+      on: {
+        autoplayTimeLeft(_s, time, progress) {
+          progressCircle.style.setProperty('--progress', (1 - progress).toString());
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        },
+        slideChange() {
+          setTimeout(
+            () =>
+              element
+                .querySelectorAll('.swiper-slide-active .animated-number')
+                .forEach((counter: HTMLElement) => {
+                  animateNumber(counter);
+                }),
+            200,
+          );
+        },
+      },
     };
-    const swiper = new Swiper(element, options);
-    element.focus();
-    swiper.on('slideChange', function () {
-      setTimeout(
-        () =>
-          element
-            .querySelectorAll('.swiper-slide-active .animated-number')
-            .forEach((counter: HTMLElement) => {
-              animateNumber(counter);
-            }),
-        200,
-      );
-    });
+    new Swiper(element, options);
   };
