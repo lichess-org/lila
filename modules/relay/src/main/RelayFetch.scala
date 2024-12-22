2ad5999772ae7aeb6b3b9a4f05dacd8faaf12889
Thibault Duplessis <t@lichess.org>
2024-12-22 08:46:59 +0100

    reduce broadcast base period after adding etag support

--- modules/relay/src/main/RelayFetch.scala
+++ modules/relay/src/main/RelayFetch.scala
@@ -176,7 +176,7 @@ final private class RelayFetch(
 
   private def dynamicPeriod(tour: RelayTour, round: RelayRound, upstream: Sync.Upstream) = Seconds:
     val base =
-      if upstream.hasLcc then 6
+      if upstream.hasLcc then 5
       else if upstream.isRound then 10 // uses push so no need to pull often
       else 2
     base * {
