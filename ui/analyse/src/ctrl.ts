7583da675634bb5e8182db519246a04ba7e813f4
YaFred <yafred@users.noreply.github.com>
2024-12-29 09:56:11 +0100

    redraw at the end of deleteNode()

--- ui/analyse/src/ctrl.ts
+++ ui/analyse/src/ctrl.ts
@@ -620,10 +620,10 @@ export default class AnalyseCtrl {
     )
       return;
     this.tree.deleteNodeAt(path);
-    this.redraw();
     if (treePath.contains(this.path, path)) this.userJump(treePath.init(path));
     else this.jump(this.path);
     if (this.study) this.study.deleteNode(path);
+    this.redraw();
   }
 
   promote(path: Tree.Path, toMainline: boolean): void {
