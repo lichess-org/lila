4650d5a5046566c3feb3272b8df1776ce56cef22
Scala Steward <scala_steward@virtuslab.com>
2025-01-07 13:16:06 +0000

    Update munit to 1.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   val nettyTransport =
     ("io.netty" % s"netty-transport-native-$notifier" % "4.1.116.Final").classifier(s"$os-$arch")
   val lilaSearch  = "org.lichess.search"         %% "client"        % "3.1.0"
-  val munit       = "org.scalameta"              %% "munit"         % "1.0.3" % Test
+  val munit       = "org.scalameta"              %% "munit"         % "1.0.4" % Test
   val uaparser    = "org.uaparser"               %% "uap-scala"     % "0.18.0"
   val apacheText  = "org.apache.commons"          % "commons-text"  % "1.13.0"
   val apacheMath  = "org.apache.commons"          % "commons-math3" % "3.6.1"
