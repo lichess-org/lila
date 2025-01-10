ef9bc3b3aa36066807c9fd7d364c7fe4ac586499
IcyNation <rchil3818@gmail.com>
2025-01-10 18:53:42 +0530

    Update BoardEditorUi.scala
    Making it's a littler simpler

--- modules/web/src/main/ui/BoardEditorUi.scala
+++ modules/web/src/main/ui/BoardEditorUi.scala
@@ -2,48 +2,55 @@ package lila.web
 package ui
 
 import chess.format.Fen
-import play.api.libs.json.*
+import play.api.libs.json._
 
 import lila.common.Json.given
-import lila.ui.*
+import lila.ui._
 
-import ScalatagsTemplate.*
+import ScalatagsTemplate._
+
+final case class BoardEditorConfig(
+  fen: Option[Fen.Full] = None,
+  positionsJson: JsArray,
+  endgamePositionsJson: JsArray
+)
 
 final class BoardEditorUi(helpers: Helpers):
   import helpers.{ *, given }
 
-  def apply(
-      fen: Option[Fen.Full],
-      positionsJson: JsArray,
-      endgamePositionsJson: JsArray
-  )(using Context) =
+  def apply(config: BoardEditorConfig)(using Context) =
     Page(trans.site.boardEditor.txt())
       .js(
         PageModule(
           "editor",
-          jsData(fen) ++ Json.obj("positions" -> positionsJson, "endgamePositions" -> endgamePositionsJson)
+          constructJsData(config.fen) ++ Json.obj(
+            "positions" -> config.positionsJson,
+            "endgamePositions" -> config.endgamePositionsJson
+          )
         )
       )
       .css("editor")
       .zoom
       .graph(
-        title = "Chess board editor",
+        title = "Chess Board Editor",
         url = s"$netBaseUrl${routes.Editor.index.url}",
         description = "Load opening positions or create your own chess position on a chess board editor"
-      ):
-        main(id := "board-editor")(
-          div(cls := "board-editor")(
-            div(cls := "spare"),
-            div(cls := "main-board")(chessgroundBoard),
-            div(cls := "spare")
-          )
-        )
+      ) {
+        renderBoardEditor()
+      }
 
-  def jsData(fen: Option[Fen.Full] = None)(using ctx: Context) =
-    Json
-      .obj(
-        "baseUrl"   -> s"$netBaseUrl${routes.Editor.index}",
-        "animation" -> Json.obj("duration" -> ctx.pref.animationMillis),
-        "is3d"      -> ctx.pref.is3d
+  private def constructJsData(fen: Option[Fen.Full])(using ctx: Context) =
+    Json.obj(
+      "baseUrl"   -> s"$netBaseUrl${routes.Editor.index}",
+      "animation" -> Json.obj("duration" -> ctx.pref.animationMillis),
+      "is3d"      -> ctx.pref.is3d
+    ) ++ fen.map("fen" -> _).toList
+
+  private def renderBoardEditor() =
+    main(id := "board-editor")(
+      div(cls := "board-editor")(
+        div(cls := "spare"),
+        div(cls := "main-board")(chessgroundBoard),
+        div(cls := "spare")
       )
-      .add("fen" -> fen)
+    )
