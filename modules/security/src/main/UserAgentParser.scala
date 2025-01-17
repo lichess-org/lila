ec6f47ba6d21a8c016d3f495a30ecb1e4e2beed7
Thibault Duplessis <t@lichess.org>
2025-01-17 13:16:56 +0100

    detect weird UAs
    that probably come from randomizers

--- modules/security/src/main/UserAgentParser.scala
+++ modules/security/src/main/UserAgentParser.scala
@@ -3,6 +3,8 @@ package lila.security
 import org.uaparser.scala.*
 
 import lila.core.net.UserAgent as UA
+import play.api.mvc.RequestHeader
+import lila.common.HTTPRequest
 
 object UserAgentParser:
 
@@ -31,3 +33,27 @@ object UserAgentParser:
           OS(m.osName, m.osVersion.some),
           Device(m.device)
         )
+
+  object trust:
+
+    def isSuspicious(req: RequestHeader): Boolean = HTTPRequest.userAgent(req).forall(isSuspicious)
+
+    def isSuspicious(ua: UA): Boolean =
+      ua.value.lengthIs < 30 || !looksNormal(ua)
+
+    private def looksNormal(ua: UA) =
+      val sections = ua.value.toLowerCase.split(' ')
+      sections.exists: s =>
+        isRecentChrome(s) || isRecentFirefox(s) || isRecentSafari(s)
+
+    // based on https://caniuse.com/usage-table
+    private val isRecentChrome  = isRecentBrowser("chrome", 109) // also covers Edge and Opera
+    private val isRecentFirefox = isRecentBrowser("firefox", 128)
+    private val isRecentSafari  = isRecentBrowser("safari", 605) // most safaris also have a chrome/ section
+
+    private def isRecentBrowser(name: String, minVersion: Int): String => Boolean =
+      val slashed      = name + "/"
+      val prefixLength = slashed.length
+      (s: String) =>
+        s.startsWith(slashed) &&
+          s.drop(prefixLength).takeWhile(_ != '.').toIntOption.exists(_ >= minVersion)
---
+++ modules/security/src/test/UserAgentTrustTest.scala
@@ -0,0 +1,49 @@
+package lila.security
+
+import lila.core.net.UserAgent
+
+class UserAgentTrustTest extends munit.FunSuite:
+
+  def susp(ua: String) = UserAgentParser.trust.isSuspicious(UserAgent(ua))
+
+  test("normal"):
+    assert:
+      !susp:
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+    assert:
+      !susp:
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+    assert:
+      !susp:
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0"
+    assert:
+      !susp:
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0"
+    assert:
+      !susp:
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+    assert:
+      !susp:
+        "Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0"
+    assert:
+      !susp:
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.2 Safari/605.1.15"
+
+  test("susp"):
+    assert:
+      susp("")
+    assert:
+      susp("too short")
+    assert:
+      susp("Mozilla/5.0 (X11; U; FreeBSD i386; zh-tw; rv:31.0) Gecko/20100101 Opera/13.0")
+    assert:
+      susp:
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36 OPR/48.0.2685.52"
+    assert:
+      susp:
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 Vivaldi/1.2.490.43"
+    assert:
+      susp("Mozilla/5.0 (Android 6.0.1; Mobile; rv:43.0) Gecko/43.0 Firefox/43.0")
+    assert:
+      susp:
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
