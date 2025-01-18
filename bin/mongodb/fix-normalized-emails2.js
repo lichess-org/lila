62d4ffcafa8c6f8b3a5030cd764904ade0944ed9
Thomas Daniels <daniels.thomas@pm.me>
2025-01-18 14:33:55 +0100

    Add migration script for email +-normalization

---
+++ bin/mongodb/fix-normalized-emails2.js
@@ -0,0 +1,27 @@
+// https://github.com/lichess-org/lila/pull/16764
+
+function plusNormalize(email) {
+  let [name, domain] = email.toLowerCase().split('@');
+  [name] = name.split('+');
+  return name + '@' + domain;
+}
+
+db.user4
+  .find({ email: /^[^+]+\+.*@.+$/i })
+  .forEach(user => {
+    const normalized = plusNormalize(user.email);
+    const verbatim = user.verbatimEmail || user.email;
+    print(user.username, ': ', verbatim, '->', normalized);
+
+    db.user4.update(
+      {
+        _id: user._id,
+      },
+      {
+        $set: {
+          email: normalized,
+          verbatimEmail: verbatim,
+        },
+      },
+    );
+  });
