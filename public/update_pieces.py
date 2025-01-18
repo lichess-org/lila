7c8dbd2183bfde96266d0c26f3fc3c88db8b1253
johndoknjas <jdoknjas@sfu.ca>
2025-01-17 18:06:38 -0800

    Make some of the script code clearer.

--- public/update_pieces.py
+++ public/update_pieces.py
@@ -6,27 +6,30 @@ pieces = {'wP': '.pawn.white', 'wN': '.knight.white', 'wB': '.bishop.white',
           'bP': '.pawn.black', 'bN': '.knight.black', 'bB': '.bishop.black',
           'bR': '.rook.black', 'bQ': '.queen.black', 'bK': '.king.black'}
 
-piece_style = sys.argv[1]
-vertical_adjust = float(sys.argv[2]) if '.' in sys.argv[2] else int(sys.argv[2])
+def str_to_num(val: str) -> int | float:
+    return (float if '.' in val else int)(val)
 
-def make_svg_filename(piece_abbrv: str) -> str:
+piece_style = sys.argv[1]
+vertical_shift = str_to_num(sys.argv[2])
+
+def deduce_svg_filename(piece_abbrv: str) -> str:
     return f"piece/{piece_style}/{piece_abbrv}.svg"
 
 def get_svg_filename(css_piece_name: str) -> str:
-    return make_svg_filename(next(k for k in pieces if pieces[k] == css_piece_name))
+    return deduce_svg_filename(next(k for k in pieces if pieces[k] == css_piece_name))
 
 def get_css_piece_name(svg_filename: str) -> str:
     return pieces[svg_filename.split('/')[-1].split('.svg')[0]]
 
 def update_svg(piece_abbrv: str) -> None:
-    svg_filename = make_svg_filename(piece_abbrv)
+    svg_filename = deduce_svg_filename(piece_abbrv)
     with open(svg_filename, 'r') as f: contents = f.read()
-    curr_viewbox_str = contents.split('viewBox="')[-1].split('"')[0]
+    curr_viewbox_str = contents.split('viewBox="', 1)[1].split('"', 1)[0]
     # Should be in a form like: 0 0 800 800
-    viewbox_vals = curr_viewbox_str.split()
-    viewbox_vals[1] = str(vertical_adjust)
-    updated_viewbox_str = ' '.join(viewbox_vals)
-    contents = contents.replace(f'viewBox="{curr_viewbox_str}"', f'viewBox="{updated_viewbox_str}"', 1)
+    new_viewbox_vals = curr_viewbox_str.split()
+    new_viewbox_vals[1] = str(str_to_num(new_viewbox_vals[1]) + vertical_shift)
+    new_viewbox_str = ' '.join(new_viewbox_vals)
+    contents = contents.replace(f'viewBox="{curr_viewbox_str}"', f'viewBox="{new_viewbox_str}"', 1)
     with open(svg_filename, 'w') as f: f.write(contents)
     # Todo - add stuff for then updating the css lines.
 
