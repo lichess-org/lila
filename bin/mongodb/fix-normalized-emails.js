f5538a49f996cc36d96e135fa9906dccc38bc9cd
Thomas Daniels <daniels.thomas@pm.me>
2025-01-19 08:54:43 +0100

    Fix email normalization

--- bin/mongodb/fix-normalized-emails.js
+++ bin/mongodb/fix-normalized-emails.js
@@ -1,13 +1,22 @@
-function gmailNormalize(email) {
+function normalize(email) {
   let [name, domain] = email.toLowerCase().split('@');
   [name] = name.split('+');
-  return name.replace(/\./g, '') + '@' + domain;
+
+  const gmailOrProton = ['protonmail.com', 'protonmail.ch', 'pm.me', 'gmail.com', 'googlemail.com'];
+  if (gmailOrProton.includes(domain)) {
+    return name.replace(/\./g, '') + '@' + domain;
+  } else {
+    return name + '@' + domain;
+  }
 }
 
 db.user4
-  .find({ email: /[^+.]+[+.].*@(protonmail\.com|protonmail\.ch|pm\.me|gmail\.com|googlemail\.com)$/i })
+  .find({
+    email:
+      /([^+.]+[+.].*@(protonmail\.com|protonmail\.ch|pm\.me|gmail\.com|googlemail\.com)|^[^+]+\+.*@.+)$/i,
+  })
   .forEach(user => {
-    const normalized = gmailNormalize(user.email);
+    const normalized = normalize(user.email);
     const verbatim = user.verbatimEmail || user.email;
     print(user.username, ': ', verbatim, '->', normalized);
 
