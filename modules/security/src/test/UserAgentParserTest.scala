adc8efda32f42a250db3b67987f14f1d1c02e74d
Thibault Duplessis <t@lichess.org>
2025-01-20 08:30:09 +0100

    improve user-agent parser

--- modules/security/src/test/UserAgentParserTest.scala
+++ modules/security/src/test/UserAgentParserTest.scala
@@ -31,6 +31,9 @@ class UserAgentTrustTest extends munit.FunSuite:
     assert:
       !susp:
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+    assert:
+      !susp:
+        "LM/0.13.13 Android/14 SM-S916N"
 
   test("susp"):
     assert:
