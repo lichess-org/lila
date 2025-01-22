b9b852a316b0c9a7c5f7297e8733eed31b9c7960
johndoknjas <jdoknjas@sfu.ca>
2025-01-22 00:22:08 -0800

    Autogenerated strings for the vukovic mate.

--- ui/@types/lichess/i18n.d.ts
+++ ui/@types/lichess/i18n.d.ts
@@ -2609,6 +2609,10 @@ interface I18n {
     veryLong: string;
     /** Four moves or more to win. */
     veryLongDescription: string;
+    /** Vukovic mate */
+    vukovicMate: string;
+    /** A rook and knight team up to mate the king. The rook delivers mate while supported by a third piece, and the knight is used to block the king's escape squares. */
+    vukovicMateDescription: string;
     /** X-Ray attack */
     xRayAttack: string;
     /** A piece attacks or defends a square, through an enemy piece. */
