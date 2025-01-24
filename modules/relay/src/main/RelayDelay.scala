b23645900ef06776d28149838352b75a8fb55b30
Thibault Duplessis <t@lichess.org>
2025-01-24 11:30:24 +0100

    make broadcast delay work with all source types

--- modules/relay/src/main/RelayDelay.scala
+++ modules/relay/src/main/RelayDelay.scala
@@ -8,28 +8,30 @@ import lila.db.dsl.{ *, given }
 import lila.memo.CacheApi
 import lila.study.MultiPgn
 
-private sealed abstract class RelaySource[Source, CacheKey](val source: Source, val cacheKey: CacheKey)
-private final class UrlSource(url: URL) extends RelaySource(url, url)
-private final class GamesSource(games: List[GameId], roundId: RelayRoundId)
-    extends RelaySource(games, roundId)
-
 final private class RelayDelay(colls: RelayColls)(using Executor):
 
   import RelayDelay.*
 
-  def fromSource[Source, CacheKey](
-      source: RelaySource[Source, CacheKey],
-      round: RelayRound,
-      doFetch: Source => Fu[RelayGames]
-  ): Fu[RelayGames] =
-    dupCache(source, round, () => doFetch(source.source))
-      .flatMap: latest =>
-        round.sync.delayMinusLag match
-          case Some(delay) if delay > 0 => store.get(source, delay).map(_ | latest.map(_.resetToSetup))
-          case _                        => fuccess(latest)
+  def urlSource(url: URL, round: RelayRound, doFetch: URL => Fu[RelayGames]): Fu[RelayGames] =
+    fromSource(UrlSource(url), round, () => doFetch(url))
 
-  def fromGames(round: RelayRound, doFetchGames: => Fu[RelayGames]): Fu[RelayGames] =
-    doFetchGames
+  def internalSource(round: RelayRound, doFetch: => Fu[RelayGames]): Fu[RelayGames] =
+    fromSource(InternalSource(round.id), round, () => doFetch)
+
+  private def fromSource(
+      source: RelaySource,
+      round: RelayRound,
+      doFetch: () => Fu[RelayGames]
+  ): Fu[RelayGames] = for
+    latest <- source match
+      case UrlSource(url) => dedupCache(url, round, doFetch)
+      case _              => doFetch()
+    delayed <- round.sync.delayMinusLag match
+      case None => fuccess(latest)
+      case Some(delay) =>
+        store.putIfNew(source.cacheKey, latest)
+        store.get(source.cacheKey, delay).map(_ | latest.map(_.resetToSetup))
+  yield delayed
 
   // makes sure that an upstream used by several broadcasts
   // is only pulled from as many times as necessary, and not more.
@@ -38,52 +40,50 @@ final private class RelayDelay(colls: RelayColls)(using Executor):
     private val cache = CacheApi.scaffeineNoScheduler
       .initialCapacity(8)
       .maximumSize(256)
-      .build[RelaySource, GamesSeenBy]()
+      .build[URL, GamesSeenBy]()
       .underlying
 
-    def apply(source: RelaySource, round: RelayRound, doFetch: () => Fu[RelayGames]) =
+    def apply(url: URL, round: RelayRound, doFetch: () => Fu[RelayGames]) =
       cache.asMap
         .compute(
-          source,
+          url,
           (_, v) =>
             Option(v) match
               case Some(GamesSeenBy(games, seenBy)) if !seenBy(round.id) =>
                 lila.mon.relay.dedup.increment()
-                logger.debug(s"Relay dedup cache hit ${round.id} ${round.name} $source")
+                logger.debug(s"Relay dedup cache hit ${round.id} ${round.name} $url")
                 GamesSeenBy(games, seenBy + round.id)
               case _ =>
                 GamesSeenBy(doFetch(), Set(round.id))
         )
         .games
-        .addEffect: games =>
-          if round.sync.delayMinusLag.isDefined then store.putIfNew(source, games)
 
   private object store:
 
-    private def idOf(url: URL, at: Instant) = s"$url ${at.toSeconds}"
-    private val longPast                    = java.time.Instant.ofEpochMilli(0)
+    private def idOf(key: CacheKey, at: Instant) = s"$key ${at.toSeconds}"
+    private val longPast                         = java.time.Instant.ofEpochMilli(0)
 
-    def putIfNew(url: URL, games: RelayGames): Funit =
+    def putIfNew(key: CacheKey, games: RelayGames): Funit =
       val newPgn = RelayGame.iso.from(games).toPgnStr
-      getLatestPgn(url).flatMap:
+      getLatestPgn(key).flatMap:
         case Some(latestPgn) if latestPgn == newPgn => funit
         case _ =>
           val now = nowInstant
-          val doc = $doc("_id" -> idOf(url, now), "at" -> now, "pgn" -> newPgn)
+          val doc = $doc("_id" -> idOf(key, now), "at" -> now, "pgn" -> newPgn)
           colls.delay:
             _.insert.one(doc).void
 
-    def get(url: URL, delay: Seconds): Fu[Option[RelayGames]] =
-      getPgn(url, delay).map2: pgn =>
+    def get(key: CacheKey, delay: Seconds): Fu[Option[RelayGames]] =
+      getPgn(key, delay).map2: pgn =>
         RelayGame.iso.to(MultiPgn.split(pgn, Max(999)))
 
-    private def getLatestPgn(url: URL): Fu[Option[PgnStr]] =
-      getPgn(url, Seconds(0))
+    private def getLatestPgn(key: CacheKey): Fu[Option[PgnStr]] =
+      getPgn(key, Seconds(0))
 
-    private def getPgn(url: URL, delay: Seconds): Fu[Option[PgnStr]] =
+    private def getPgn(key: CacheKey, delay: Seconds): Fu[Option[PgnStr]] =
       colls.delay:
         _.find(
-          $doc("_id".$gt(idOf(url, longPast)).$lte(idOf(url, nowInstant.minusSeconds(delay.value)))),
+          $doc("_id".$gt(idOf(key, longPast)).$lte(idOf(key, nowInstant.minusSeconds(delay.value)))),
           $doc("pgn" -> true).some
         ).sort($sort.desc("_id"))
           .one[Bdoc]
@@ -91,6 +91,18 @@ final private class RelayDelay(colls: RelayColls)(using Executor):
             _.flatMap(_.getAsOpt[PgnStr]("pgn"))
 
 private object RelayDelay:
+
   val maxSeconds = Seconds(60 * 60)
 
   private case class GamesSeenBy(games: Fu[RelayGames], seenBy: Set[RelayRoundId])
+
+  opaque type CacheKey = String
+
+  private trait RelaySource:
+    def cacheKey: CacheKey
+
+  private case class UrlSource(url: URL) extends RelaySource:
+    def cacheKey = url.toString
+
+  private case class InternalSource(roundId: RelayRoundId) extends RelaySource:
+    def cacheKey = roundId.value
