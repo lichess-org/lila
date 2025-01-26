a525c46790801289b61f9031680b61a7f0118f74
Thibault Duplessis <t@lichess.org>
2025-01-26 10:43:16 +0100

    email normalization: simplify regex, refactor, add dry mode

--- bin/mongodb/fix-normalized-emails.js
+++ bin/mongodb/fix-normalized-emails.js
@@ -1,30 +1,24 @@
-const gmailOrProton = ['protonmail.com', 'protonmail.ch', 'pm.me', 'gmail.com', 'googlemail.com'];
+const dry = true;
+const gmailOrProton = ['protonmail.com', 'protonmail.ch', 'pm.me', 'proton.me', 'gmail.com', 'googlemail.com'];
 
 function normalize(email) {
   let [name, domain] = email.toLowerCase().split('@');
   [name] = name.split('+');
 
-  if (gmailOrProton.includes(domain)) {
-    return name.replace(/\./g, '') + '@' + domain;
-  } else {
-    return name + '@' + domain;
-  }
+  if (gmailOrProton.includes(domain)) name = name.replace(/\./g, '');
+
+  return name + '@' + domain;
 }
 
 db.user4
-  .find({
-    email:
-      /([^+.]+[+.].*@(protonmail\.com|protonmail\.ch|pm\.me|gmail\.com|googlemail\.com)|^[^+]+\+.*@.+)$/i,
-  })
+  .find({ email: /^[^+]+\+.*@.+$/ }, { email: 1, verbatimEmail: 1, username: 1 })
   .forEach(user => {
     const normalized = normalize(user.email);
     const verbatim = user.verbatimEmail || user.email;
     print(user.username, ': ', verbatim, '->', normalized);
 
-    db.user4.update(
-      {
-        _id: user._id,
-      },
+    if (!dry && user.email != normalized) db.user4.update(
+      { _id: user._id },
       {
         $set: {
           email: normalized,
