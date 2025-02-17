fd33e29870ef1e75c261a040f66e68a14662dddb
Benedikt Werner <1benediktwerner@gmail.com>
2025-02-17 22:38:17 +0100

    Return bots and accountAge conditions in tournament API

--- modules/tournament/src/main/JsonView.scala
+++ modules/tournament/src/main/JsonView.scala
@@ -140,6 +140,8 @@ final class JsonView(
           .add[Condition.RatingCondition]("minRating", tour.conditions.minRating)
           .add[Condition.RatingCondition]("maxRating", tour.conditions.maxRating)
           .add("minRatedGames", tour.conditions.nbRatedGame)
+          .add("botsAllowed", tour.conditions.bots.exists(_.allowed))
+          .add("minAccountAgeInDays", tour.conditions.accountAge.map(_.days))
           .add("onlyTitled", tour.conditions.titled.isDefined)
           .add("teamMember", tour.conditions.teamMember.map(_.teamId))
           .add("allowList", withAllowList.so(tour.conditions.allowList).map(_.userIds))
