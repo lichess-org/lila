cd38748768d95d665fdeca7ad67ed6913a7df607
Thibault Duplessis <t@lichess.org>
2025-02-17 23:55:19 +0100

    join/withdraw from tournaments with bot:play oauth token

--- app/controllers/Tournament.scala
+++ app/controllers/Tournament.scala
@@ -161,7 +161,7 @@ final class Tournament(env: Env, apiC: => Api)(using akka.stream.Materializer) e
           }
   }
 
-  def apiJoin(id: TourId) = ScopedBody(_.Tournament.Write) { ctx ?=> me ?=>
+  def apiJoin(id: TourId) = ScopedBody(_.Tournament.Write, _.Bot.Play) { ctx ?=> me ?=>
     NoLame:
       NoPlayban:
         limit.tourJoin(me, rateLimited):
@@ -186,7 +186,7 @@ final class Tournament(env: Env, apiC: => Api)(using akka.stream.Materializer) e
       else Redirect(routes.Tournament.show(tour.id))
   }
 
-  def apiWithdraw(id: TourId) = ScopedBody(_.Tournament.Write) { _ ?=> me ?=>
+  def apiWithdraw(id: TourId) = ScopedBody(_.Tournament.Write, _.Bot.Play) { _ ?=> me ?=>
     Found(cachedTour(id)): tour =>
       api.selfPause(tour.id, me).inject(jsonOkResult)
   }
