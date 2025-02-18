5ecebd816a470b5d6f3ea3cab41453708cd1e725
Thibault Duplessis <t@lichess.org>
2025-02-18 12:20:54 +0100

    simplify query when counting own studies

--- modules/study/src/main/StudyRepo.scala
+++ modules/study/src/main/StudyRepo.scala
@@ -97,8 +97,8 @@ final class StudyRepo(private[study] val coll: AsyncColl)(using
   private def hasMemberOrIsPublic(using as: Option[MyId]) = as.fold(selectPublic): me =>
     $or($doc(s"members.$me".$exists(true)), selectPublic)
 
-  def countByOwner(ownerId: UserId)(using Option[MyId]) = coll:
-    _.countSel(selectOwnerId(ownerId) ++ hasMemberOrIsPublic)
+  def countByOwner(ownerId: UserId)(using as: Option[MyId]) = coll:
+    _.countSel(selectOwnerId(ownerId) ++ as.forall(_.isnt(ownerId)).so(hasMemberOrIsPublic))
 
   def sourceByOwner(ownerId: UserId, isMe: Boolean): Source[Study, ?] =
     Source.futureSource:
