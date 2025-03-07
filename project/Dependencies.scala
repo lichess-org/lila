bd742a5f6bdd44e1b4a3623450bc22487b00219c
Thibault Duplessis <t@lichess.org>
2025-03-07 10:30:51 +0100

    not sure that's needed but lila-ws had it

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -78,7 +78,8 @@ object Dependencies {
   }
 
   object reactivemongo {
-    val driver = "org.reactivemongo" %% "reactivemongo"                              % "1.1.0-RC13"
+    val driver = ("org.reactivemongo" %% "reactivemongo" % "1.1.0-RC13")
+      .exclude("org.scala-lang.modules", "scala-java8-compat_2.13")
     val stream = "org.reactivemongo" %% "reactivemongo-akkastream"                   % "1.1.0-RC15"
     val shaded = "org.reactivemongo"  % s"reactivemongo-shaded-native-$os-$dashArch" % "1.1.0-RC15"
     // val kamon  = "org.reactivemongo" %% "reactivemongo-kamon"         % "1.0.8"
