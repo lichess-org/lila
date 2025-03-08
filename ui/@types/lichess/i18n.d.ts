03e696af0b65ef9f2a5a72bb1d9ab1848a44ae69
johndoknjas <jdoknjas@sfu.ca>
2025-03-08 14:05:12 -0800

    Reuse insufficient material string.

--- ui/@types/lichess/i18n.d.ts
+++ ui/@types/lichess/i18n.d.ts
@@ -3083,8 +3083,8 @@ interface I18n {
     drawByFiftyMoves: string;
     /** Draw by mutual agreement */
     drawByMutualAgreement: string;
-    /** Draw claimed by insufficient mating material */
-    drawClaimedByInsufficientMaterial: string;
+    /** Draw claimed */
+    drawClaimed: string;
     /** Drawn */
     drawn: string;
     /** Draw offer accepted */
