391a55cfbfbbc7fdf58aa72ce4c119508e302b0e
Thibault Duplessis <t@lichess.org>
2025-03-08 12:34:35 +0100

    bot list UI wip

--- ui/botPlay/src/botPlay.ts
+++ ui/botPlay/src/botPlay.ts
@@ -2,17 +2,12 @@ import { init, classModule, attributesModule } from 'snabbdom';
 
 import { BotOpts } from './interfaces';
 import { BotPlayCtrl } from './ctrl';
-import { Assets } from 'local/assets';
-import { BotCtrl } from 'local/botCtrl';
 
 export async function initModule(opts: BotOpts) {
   const element = document.querySelector('.bot-play-app') as HTMLElement,
     patch = init([classModule, attributesModule]);
 
-  const assets = new Assets();
-  const bot = new BotCtrl();
-  await Promise.all([bot.init(opts.bots), assets.init()]);
-  const ctrl = new BotPlayCtrl(opts, assets, redraw);
+  const ctrl = new BotPlayCtrl(opts, redraw);
 
   const blueprint = ctrl.view();
   element.innerHTML = '';
--- ui/botPlay/src/ctrl.ts
+++ ui/botPlay/src/ctrl.ts
@@ -1,4 +1,4 @@
-import { Assets } from 'local/assets';
+import { botAssetUrl } from 'local/assets';
 import { BotOpts } from './interfaces';
 
 import { looseH as h } from 'common/snabbdom';
@@ -7,16 +7,21 @@ import { BotInfo } from 'local';
 export class BotPlayCtrl {
   constructor(
     readonly opts: BotOpts,
-    readonly assets: Assets,
     readonly redraw: () => void,
   ) {
     console.log('BotPlayCtrl', opts);
   }
 
-  view = () => h('main.bot-play-app', ['bot play app', this.viewBotList()]);
+  view = () => h('main.bot-play-app', [this.viewBotList()]);
 
   private viewBotList = () => h('div.bpa__setup__bots', this.opts.bots.map(this.viewBotCard));
 
   private viewBotCard = (bot: BotInfo) =>
-    h('div.bpa__bot-card', [bot.name, bot?.image && this.assets.getImageUrl(bot.image)]);
+    h('div.bpa__bot-card', [
+      h('img.bpa__bot-card__image', { attrs: { src: bot?.image && botAssetUrl('image', bot.image) } }),
+      h('div.bpa__bot-card__content', [
+        h('h2.bpa__bot-card__name', bot.name),
+        h('p.bpa__bot-card__description', bot.description),
+      ]),
+    ]);
 }
