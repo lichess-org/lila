af77fa08cf3ac125fd6737fdb973d6dad231b96c
Jonathan Gamble <101470903+schlawg@users.noreply.github.com>
2025-03-13 19:20:43 -0500

    trailing comma

--- ui/common/src/dialogs.ts
+++ ui/common/src/dialogs.ts
@@ -18,7 +18,7 @@ export async function alerts(msgs: string[]): Promise<void> {
 export async function info(msg: string, autoDismiss?: Millis): Promise<Dialog> {
   const dlg = await domDialog({
     htmlText: escapeHtmlAddBreaks(msg),
-    noCloseButton: true
+    noCloseButton: true,
   });
   if (autoDismiss) setTimeout(() => dlg.close(), autoDismiss);
   return dlg.show();
