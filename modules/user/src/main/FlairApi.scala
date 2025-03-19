lichess-org/lila

commit 84c764fecb68141f1dce5db3dc020c0d2a0b5488

Fix flair/list.txt pathname

modules/user/src/main/FlairApi.scala
@@ -1,5 +1,6 @@
 package lila.user
 
+import play.api.Environment
 import lila.core.user.{ FlairGet, FlairGetMap }
 
 object FlairApi:
@@ -24,8 +25,9 @@ object FlairApi:
 
   val adminFlairs: Set[Flair] = Set(Flair("activity.lichess"))
 
-final class FlairApi(lightUserApi: LightUserApi)(using Executor)(using scheduler: Scheduler)
-    extends lila.core.user.FlairApi:
+final class FlairApi(environment: Environment, lightUserApi: LightUserApi)(using Executor)(using
+    scheduler: Scheduler
+) extends lila.core.user.FlairApi:
 
   import FlairApi.*
   export FlairApi.{ find, formField, adminFlairs }
@@ -44,7 +46,8 @@ final class FlairApi(lightUserApi: LightUserApi)(using Executor)(using scheduler
         pairs.toMap
 
   private def refresh(): Unit =
-    val source = scala.io.Source.fromFile("public/flair/list.txt", "UTF-8")
+    val pathname = environment.getFile("public/flair/list.txt").toPath.toString
+    val source   = scala.io.Source.fromFile(pathname, "UTF-8")
     try
       db = Flair.from(source.getLines.toSet)
       logger.info(s"Updated flair db with ${db.size} flairs")

modules/user/src/main/Env.scala
@@ -9,6 +9,7 @@ import lila.core.userId
 @Module
 final class Env(
     db: lila.db.Db,
+    environment: play.api.Environment,
     yoloDb: lila.db.AsyncDb @@ lila.db.YoloDb,
     mongoCache: lila.memo.MongoCache.Api,
     cacheApi: lila.memo.CacheApi,

commit 28873514f9f85df51ea747e9c1abd04b34697ad4

fix asset refreshing in prod and stage
was broken by 84c764fecb68141f1dce5db3dc020c0d2a0b5488

modules/user/src/main/FlairApi.scala
@@ -46,13 +46,17 @@ final class FlairApi(lightUserApi: LightUserApi, getFile: lila.common.config.Get
         pairs.toMap
 
   private def refresh(): Unit =
-    // val pathname = getFile.exec("public/flair/list.txt").toPath.toString
-    // val source   = scala.io.Source.fromFile(pathname, "UTF-8")
-    val source = scala.io.Source.fromFile("public/flair/list.txt", "UTF-8")
+    try refreshFrom(scala.io.Source.fromFile("public/flair/list.txt", "UTF-8"))
+    catch
+      case e: Exception =>
+        logger.error("Cannot read flairs, trying alternative path", e)
+        val pathname = getFile.exec("public/flair/list.txt").toPath.toString
+        refreshFrom(scala.io.Source.fromFile(pathname, "UTF-8"))
+
+  private def refreshFrom(source: scala.io.Source): Unit =
     try
       db = Flair.from(source.getLines.toSet)
       logger.info(s"Updated flair db with ${db.size} flairs")
-    catch case e: Exception => logger.error(s"Cannot read flairs", e)
     finally source.close()
 
   scheduler.scheduleOnce(11.seconds)(refresh())