c5b2031d9626c7fe18f3a75098915effd365a395
Thibault Duplessis <t@lichess.org>
2025-03-21 13:59:28 +0100

    fix incomplete refactoring

--- modules/ublog/src/main/UblogBestOf.scala
+++ modules/ublog/src/main/UblogBestOf.scala
@@ -68,7 +68,7 @@ final class UblogBestOf(colls: UblogColls, ublogApi: UblogApi, cacheApi: CacheAp
                 Match($doc("live" -> true) ++ UblogBestOf.selector(month))
               ) ++ UblogRank.Sorting.ByTimelessRank.sortingQuery(colls.post, framework) ++ List(
                 Limit(4)
-              ) ++ ublogApi.removeUnlistedOrClosed(colls.post, framework))
+              ) ++ ublogApi.removeUnlistedOrClosedAndProjectForPreview(colls.post, framework))
         ) -> List(
           Project($doc("all" -> $doc("$objectToArray" -> "$$ROOT"))),
           UnwindField("all"),
