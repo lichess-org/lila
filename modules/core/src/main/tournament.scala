700d2498b9ed1bfdc99d24c999949a5205d921ac
Thibault Duplessis <t@lichess.org>
2025-03-26 10:24:34 +0100

    also accept arena status ids

--- modules/core/src/main/tournament.scala
+++ modules/core/src/main/tournament.scala
@@ -19,7 +19,11 @@ enum Status(val id: Int):
 
 object Status:
   val byId: Map[Int, Status] = values.mapBy(_.id)
-  def byName(str: String)    = scala.util.Try(Status.valueOf(str)).toOption
+  def byName(str: String) = scala.util
+    .Try(Status.valueOf(str))
+    .toOption
+    .orElse:
+      str.toIntOption.flatMap(byId.get)
 
 trait GetTourName:
   def sync(id: TourId)(using Lang): Option[String]
