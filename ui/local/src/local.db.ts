cec92afe0d45f4423183c1379b77c99348fdaac6
Jonathan Gamble <gamblej@gmail.com>
2025-03-30 16:37:59 -0500

    remove unused sources

--- ui/local/src/local.db.ts
+++
@@ -1,8 +0,0 @@
-import { LocalDb } from './localDb';
-import { BotCtrl } from './botCtrl';
-import { type LocalEnv, makeEnv } from './localEnv';
-
-export default async function initModule(): Promise<LocalEnv> {
-  const [db, bot] = await Promise.all([new LocalDb().init(), new BotCtrl(false).init()]);
-  return makeEnv({ db, bot });
-}
