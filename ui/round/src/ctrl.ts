d8211f14ca9bab07bc8dc16db6327bad2b75b82e
Jonathan Gamble <101470903+schlawg@users.noreply.github.com>
2025-04-03 10:04:28 -0500

    only show result popup when game is aborted or opponent resigns

--- ui/round/src/ctrl.ts
+++ ui/round/src/ctrl.ts
@@ -31,7 +31,7 @@ import { opposite, uciToMove } from 'chessground/util';
 import { Replay } from 'lib/prefs';
 import { endGameView } from './view/main';
 import { info as infoDialog } from 'lib/dialogs';
-import { isCol1 } from 'lib/device';
+import { displayColumns } from 'lib/device';
 
 import type {
   Step,
@@ -583,8 +583,13 @@ export default class RoundController implements MoveRootCtrl {
         this.opts.chat?.instance?.post('Good game, well played');
     }
     endGameView();
-    if (isCol1()) {
-      infoDialog(viewStatus(this.data), 3000);
+    if (displayColumns() === 1) {
+      if (
+        (d.game.status.name === 'resign' && d.game.winner === d.player.color) ||
+        d.game.status.name === 'aborted'
+      ) {
+        infoDialog(viewStatus(this.data), 3000);
+      }
     }
     if (d.crazyhouse) crazyEndHook();
     this.clearJust();
