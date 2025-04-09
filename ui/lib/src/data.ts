7b17f77b65c5fea4ec9e4a93bfbe965c457b88fa
Thibault Duplessis <t@lichess.org>
2025-04-09 10:29:13 +0200

    add comment to lib/data

--- ui/lib/src/data.ts
+++ ui/lib/src/data.ts
@@ -2,6 +2,8 @@
 
 const makeKey = (key: string) => `lichess-${key}`;
 
+// should be
+// export const get = <A>(owner: Element, key: string): A | undefined => (owner as any)[makeKey(key)];
 export const get = (owner: Element, key: string): any => (owner as any)[makeKey(key)];
 
 export const set = (owner: Element, key: string, value: any): void => ((owner as any)[makeKey(key)] = value);
