af0e81373874ad853fc222e8c3d50b3fe6b81b21
Thibault Duplessis <t@lichess.org>
2025-04-09 09:39:13 +0200

    refactor proxyscrape fetch and disable by default

--- modules/security/src/main/SecurityConfig.scala
+++ modules/security/src/main/SecurityConfig.scala
@@ -25,6 +25,7 @@ final private class SecurityConfig(
     @ConfigName("ip2proxy") val ip2Proxy: Ip2Proxy,
     @ConfigName("lame_name_check") val lameNameCheck: LameNameCheck,
     @ConfigName("pwned.range_url") val pwnedRangeUrl: String,
+    @ConfigName("proxyscrape.url") val proxyscrapeUrl: String,
     @ConfigName("password.bpass.secret") val passwordBPassSecret: Secret
 )
 
