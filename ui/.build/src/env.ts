fbd214f6bfe4900d344e2b3ce6282e3cead6ae99
Jonathan Gamble <gamblej@gmail.com>
2025-04-27 12:54:16 -0500

    remove overly specific text processing functions from lib

--- ui/.build/src/env.ts
+++ ui/.build/src/env.ts
@@ -2,7 +2,7 @@ import type { Package } from './parse.ts';
 import fs from 'node:fs';
 import ps from 'node:process';
 import { join, resolve, dirname } from 'node:path';
-import { definedUnique, isEquivalent, trimLines } from './algo.ts';
+import { definedUnique, isEquivalent } from './algo.ts';
 import { updateManifest } from './manifest.ts';
 import { taskOk } from './task.ts';
 
@@ -143,6 +143,10 @@ export const env = new (class {
     return true;
   }
 })();
+
+export function trimLines(s: string): string[] {
+  return s.split(/[\n\r\f]+/).filter(x => x.trim());
+}
 
 export type Context = 'sass' | 'tsc' | 'esbuild' | 'sync' | 'hash' | 'i18n' | 'web';
 
