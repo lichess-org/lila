ad544f8c743d392204bb0f95bfcd672672101f32
kraktus <kraktus@users.noreply.github.com>
2025-04-28 21:12:01 +0200

    mod `subscribeFun` -> `sub`

--- modules/fishnet/src/main/Env.scala
+++ modules/fishnet/src/main/Env.scala
@@ -111,15 +111,18 @@ final class Env(
         repo.toKey(key).flatMap { repo.enableClient(_, v = true) }.inject("done!")
       case "fishnet" :: "client" :: "disable" :: key :: Nil => disable(key).inject("done!")
 
-  Bus.subscribeFun("adjustCheater", "adjustBooster", "shadowban"):
+  Bus.sub[lila.core.mod.MarkCheater]:
     case lila.core.mod.MarkCheater(userId, true) => disable(userId.value)
-    case lila.core.mod.MarkBooster(userId)       => disable(userId.value)
-    case lila.core.mod.Shadowban(userId, true)   => disable(userId.value)
+  Bus.sub[lila.core.mod.MarkBooster]:
+    case lila.core.mod.MarkBooster(userId) => disable(userId.value)
+  Bus.sub[lila.core.mod.Shadowban]:
+    case lila.core.mod.Shadowban(userId, true) => disable(userId.value)
 
   Bus.sub[lila.core.fishnet.Bus]:
     case lila.core.fishnet.Bus.GameRequest(id) =>
       analyser(id, Work.Sender(userId = UserId.lichess, ip = none, mod = false, system = true))
     case req: lila.core.fishnet.Bus.StudyChapterRequest => analyser.study(req)
 
+  // TODO FIXME introduce `fishnetPlay` wrapper
   Bus.subscribeFun("fishnetPlay"):
     case game: Game => player(game)
