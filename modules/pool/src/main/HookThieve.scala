90bc00db1e4dc5258a594f071537576e00270d44
kraktus <kraktus@users.noreply.github.com>
2025-05-08 22:16:10 +0200

    fix safeAsk type being too specific, referring to a HookBus member, instead of the HookBus type

--- modules/pool/src/main/HookThieve.scala
+++ modules/pool/src/main/HookThieve.scala
@@ -8,7 +8,7 @@ final private class HookThieve()(using Executor, Scheduler):
 
   def candidates(clock: chess.Clock.Config): Fu[PoolHooks] =
     Bus
-      .safeAsk[PoolHooks, HookBus.GetCandidates](HookBus.GetCandidates(clock, _))
+      .safeAsk[PoolHooks, HookBus](HookBus.GetCandidates(clock, _))
       .logFailure(logger)
       .recoverDefault(PoolHooks(Vector.empty))
 
