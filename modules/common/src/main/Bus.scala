449ac3dbc1b1bdaab46d2c1bac30179439cc5909
kraktus <kraktus@users.noreply.github.com>
2025-05-08 16:21:39 +0200

    Also check for NotBuseable for ActorRef, although strictly unnecessary

--- modules/common/src/main/Bus.scala
+++ modules/common/src/main/Bus.scala
@@ -1,7 +1,9 @@
 package lila.common
 
+import scala.util.NotGiven
+
 import akka.actor.{ ActorRef, Scheduler }
-import scalalib.bus.{ Channel, Tellable }
+import scalalib.bus.{ Channel, Tellable, NotBuseable }
 
 val Bus = scalalib.bus.Bus(initialCapacity = 4096)
 
@@ -13,11 +15,15 @@ object actorBus:
   extension (bus: scalalib.bus.Bus)
 
     // LOGIC : It is up to the caller to make sure `T`'s channel is relevant to the `tellable`
-    inline def subscribeActorRef[T <: scalalib.bus.Bus.Payload](ref: ActorRef) =
+    inline def subscribeActorRef[T <: scalalib.bus.Bus.Payload](ref: ActorRef)(using
+        NotGiven[T <:< NotBuseable]
+    ) =
       Bus.subTellable[T](ActorTellable(ref))
 
     // LOGIC : It is up to the caller to make sure `T`'s channel is relevant to the `tellable`
-    inline def unsubscribeActorRef[T <: scalalib.bus.Bus.Payload](ref: ActorRef) =
+    inline def unsubscribeActorRef[T <: scalalib.bus.Bus.Payload](ref: ActorRef)(using
+        NotGiven[T <:< NotBuseable]
+    ) =
       Bus.unsubUnchecked[T](ActorTellable(ref))
 
     // it's good to have subscribe and unsubscribe not taking channels the same way
