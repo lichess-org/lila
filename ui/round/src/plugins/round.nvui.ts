24e3124eacb759c39c763ed2847ef0bf94439a28
Thibault Duplessis <t@lichess.org>
2025-05-13 09:14:28 +0200

    round.nvui code golf

--- ui/round/src/plugins/round.nvui.ts
+++ ui/round/src/plugins/round.nvui.ts
@@ -178,8 +178,7 @@ export function initModule(): NvuiPlugin {
                   )(e);
                 else if (e.key === 'i') {
                   e.preventDefault();
-                  const $moveBox = $('input.move');
-                  $moveBox.get(0)?.focus();
+                  $('input.move').get(0)?.focus();
                 }
               });
             }),
@@ -299,12 +298,10 @@ const inputCommands: InputCommand[] = [
     help: 'Focus on board. Default square is e4. You can specify a square: board a1 or b a1 will take you to square a1.',
     cb: (_notify, _ctrl, _style, input) => {
       const words = input.split(' ');
-      const inputFile = words[1]?.charAt(0);
-      const inputRank = words[1]?.charAt(1);
-      const file = inputFile ? inputFile : 'e';
-      const rank = inputRank ? inputRank : '4';
+      const file = words[1]?.charAt(0) || 'e';
+      const rank = words[1]?.charAt(1) || '4';
       const button = $('button[file="' + file + '"][rank="' + rank + '"]').get(0);
-      if (button) button.focus();
+      button?.focus();
     },
     alt: 'b',
   },
