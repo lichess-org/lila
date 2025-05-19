fdebb12aa2d5c228799bbe351cab54052e41f90d
johndoknjas <jdoknjas@sfu.ca>
2025-05-18 22:41:35 -0700

    Weaken the condition for having `pinToBottom` set to true.

--- ui/lib/src/chat/discussion.ts
+++ ui/lib/src/chat/discussion.ts
@@ -53,7 +53,7 @@ export default function (ctrl: ChatCtrl): Array<VNode | undefined> {
           postpatch: (_, vnode) => {
             const el = vnode.elm as HTMLElement;
 
-            if (el.scrollTop + el.clientHeight > el.scrollHeight - 10) scrollState.pinToBottom = true;
+            if (el.scrollTop + el.clientHeight > el.scrollHeight - 32) scrollState.pinToBottom = true;
             else if (!scrollState.pinToBottom) return;
 
             if (document.visibilityState === 'hidden') el.scrollTop = el.scrollHeight;
