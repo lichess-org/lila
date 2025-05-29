9d58535857adf3502bd5413f7d1a167b9723fc56
Thibault Duplessis <t@lichess.org>
2025-05-29 12:25:12 +0200

    format

--- modules/storm/src/main/StormSelector.scala
+++ modules/storm/src/main/StormSelector.scala
@@ -30,20 +30,19 @@ final class StormSelector(colls: PuzzleColls, cacheApi: CacheApi)(using Executor
    * 800,  900,  1000, 1100, 1200, 1270, 1340, 1410, 1480, 1550, 1620,
    * 1690, 1760, 1830, 1900, 2000, 2100, 2200, 2350, 2500, 2650, 2800
    */
-  private val ratingBuckets =
-    List(
-      1050 -> 7,
-      1150 -> 7,
-      1300 -> 8,
-      1450 -> 9,
-      1600 -> 10,
-      1750 -> 11,
-      1900 -> 13,
-      2050 -> 15,
-      2199 -> 17,
-      2349 -> 19,
-      2499 -> 21
-    )
+  private val ratingBuckets = List(
+    1050 -> 7,
+    1150 -> 7,
+    1300 -> 8,
+    1450 -> 9,
+    1600 -> 10,
+    1750 -> 11,
+    1900 -> 13,
+    2050 -> 15,
+    2199 -> 17,
+    2349 -> 19,
+    2499 -> 21
+  )
   private val setSize = ratingBuckets.map(_._2).sum
 
   private val batchProvider =
