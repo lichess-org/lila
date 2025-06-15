ef8d1e9ef56b25b9f9bb96b1f6355b311600cec8
Jonathan Gamble <gamblej@gmail.com>
2025-06-15 16:42:51 -0500

    lockfile and scalafmt

--- modules/ublog/src/main/UblogBsonHandlers.scala
+++ modules/ublog/src/main/UblogBsonHandlers.scala
@@ -15,36 +15,7 @@ private object UblogBsonHandlers:
     { case BSONString(v) => UblogBlog.Id(v).toTry(s"Invalid blog id $v") },
     id => BSONString(id.full)
   )
-  given BSONDocumentHandler[Result] = new BSONDocumentHandler[Result]:
-    def writeTry(result: Result) = Success:
-      val quality = result.classification match
-        case "spam"  => 0
-        case "weak"  => 1
-        case "great" => 3
-        case _       => 2
-      BSONDocument(
-        "quality"    -> quality,
-        "flagged"    -> result.flagged,
-        "commercial" -> result.commercial,
-        "evergreen"  -> result.evergreen,
-        "hash"       -> result.hash
-      )
-    def readDocument(doc: BSONDocument) = Try[Result]:
-      val fromQuality = doc.getAsOpt[Int]("quality") match
-        case Some(0) => "spam"
-        case Some(1) => "weak"
-        case Some(3) => "great"
-        case _       => "good"
-      Result(
-        classification = doc.getAsOpt[String]("classification").getOrElse(fromQuality),
-        flagged = doc.getAsOpt[String]("flagged"),
-        commercial = doc.getAsOpt[String]("commercial"),
-        offtopic = doc.getAsOpt[String]("offtopic"),
-        evergreen = doc.getAsOpt[Boolean]("evergreen"),
-        hash = doc.getAsOpt[String]("hash")
-      )
-  given BSONDocumentHandler[UblogBlog] = Macros.handler
-
+  given BSONDocumentHandler[UblogBlog]    = Macros.handler
   given BSONHandler[Lang]                 = langByCodeHandler
   given BSONDocumentHandler[Recorded]     = Macros.handler
   given BSONDocumentHandler[Featured]     = Macros.handler
