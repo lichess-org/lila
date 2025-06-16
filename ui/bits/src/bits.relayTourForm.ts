0acbd829a363e4d68f0971aae33799b4062eadd1
Sérgio Glórias <sergioglorias@proton.me>
2025-06-16 23:41:05 +0000

    format

--- ui/bits/src/bits.relayTourForm.ts
+++ ui/bits/src/bits.relayTourForm.ts
@@ -1,6 +1,5 @@
 site.load.then(() => {
   $('.form3[action="/broadcast/new"] #form3-info_timeZone').each(function (this: HTMLSelectElement) {
-    if (!$('.is-invalid').length)
-      this.value = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    if (!$('.is-invalid').length) this.value = Intl.DateTimeFormat().resolvedOptions().timeZone;
   });
 });
