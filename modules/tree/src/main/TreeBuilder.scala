263060c90d81b5a59e252f9a18973bb8b2f2d8b2
Thanh Le <lenguyenthanh@hotmail.com>
2025-06-28 13:38:04 +0200

    Now main TreeBuilder can also use foldRight

--- modules/tree/src/main/TreeBuilder.scala
+++ modules/tree/src/main/TreeBuilder.scala
@@ -43,62 +43,56 @@ object TreeBuilder:
       eval = infos.lift(0).map(makeEval)
     )
 
-    init.position
-      .play(game.sans, init.ply)(step => chess.MoveOrDrop.WithPly(step.move, step.ply))
-      .fold(
-        error =>
-          logChessError(formatError(game.id, error))
-          root
-        ,
-        games =>
-          def makeBranch(move: chess.MoveOrDrop.WithPly): Branch =
-            val fen    = Fen.write(move.after, move.ply.fullMoveNumber)
-            val info   = infos.lift((move.ply - init.ply - 1).value)
-            val advice = advices.get(move.ply)
+    def makeBranch(move: chess.MoveOrDrop.WithPly): Branch =
+      val fen    = Fen.write(move.after, move.ply.fullMoveNumber)
+      val info   = infos.lift((move.ply - init.ply - 1).value)
+      val advice = advices.get(move.ply)
 
-            val branch = Branch(
-              id = UciCharPair(move.toUci),
-              ply = move.ply,
-              move = Uci.WithSan(move.toUci, move.toSanStr),
-              fen = fen,
-              check = move.after.check,
-              opening = openingOf(fen),
-              clock = withClocks.flatMap(_.lift((move.ply - init.ply - 1).value)).map(Clock(_)),
-              crazyData = move.after.crazyData,
-              eval = info.map(makeEval),
-              glyphs = Glyphs.fromList(advice.map(_.judgment.glyph).toList),
-              comments = Node.Comments(
-                drawOfferPlies(move.ply)
-                  .option(makeLichessComment(Comment(s"${!move.ply.turn} offers draw")))
-                  .toList :::
-                  advice
-                    .map(_.makeComment(withEval = false, withBestMove = true))
-                    .toList
-                    .map(makeLichessComment)
-              )
-            )
-
-            advices
-              .get(move.ply + 1)
-              .map { adv =>
-                withAnalysisChild(
-                  game.id,
-                  branch,
-                  move.after,
-                  move.ply,
-                  openingOf,
-                  logChessError
-                )(adv.info)
-              }
-              .getOrElse(branch)
-
-          games.reverse match
-            case Nil       => root
-            case g :: rest =>
-              root.prependChild(rest.foldLeft(makeBranch(g)) { case (node, g) =>
-                makeBranch(g).prependChild(node)
-              })
+      val branch = Branch(
+        id = UciCharPair(move.toUci),
+        ply = move.ply,
+        move = Uci.WithSan(move.toUci, move.toSanStr),
+        fen = fen,
+        check = move.after.check,
+        opening = openingOf(fen),
+        clock = withClocks.flatMap(_.lift((move.ply - init.ply - 1).value)).map(Clock(_)),
+        crazyData = move.after.crazyData,
+        eval = info.map(makeEval),
+        glyphs = Glyphs.fromList(advice.map(_.judgment.glyph).toList),
+        comments = Node.Comments(
+          drawOfferPlies(move.ply)
+            .option(makeLichessComment(Comment(s"${!move.ply.turn} offers draw")))
+            .toList :::
+            advice
+              .map(_.makeComment(withEval = false, withBestMove = true))
+              .toList
+              .map(makeLichessComment)
+        )
       )
+
+      advices
+        .get(move.ply + 1)
+        .map { adv =>
+          withAnalysisChild(
+            game.id,
+            branch,
+            move.after,
+            move.ply,
+            openingOf,
+            logChessError
+          )(adv.info)
+        }
+        .getOrElse(branch)
+
+    val (result, error) = init.position.foldRight(game.sans, init.ply)(
+      none[Branch],
+      (step, acc) =>
+        inline def branch = makeBranch(chess.MoveOrDrop.WithPly(step.move, step.ply))
+        acc.fold(branch)(acc => branch.prependChild(acc)).some
+    )
+
+    error.foreach(err => logChessError(formatError(game.id, err)))
+    result.fold(root)(b => root.prependChild(b))
 
   private[tree] def makeLichessComment(c: Comment) =
     Node.Comment(
