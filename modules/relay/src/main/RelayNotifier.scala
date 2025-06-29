2c12ac011aa087134c196e2eaecd671f90c10045
Julien <120588494+julien4215@users.noreply.github.com>
2025-06-29 18:19:28 +0200

    Improve follow FIDE player notification: - always send a notification even if opponent name is missing - if the notification can't be send, log it
    Also change type Funit to Unit because we don't need to wait for the
    futures.

--- modules/relay/src/main/RelayNotifier.scala
+++ modules/relay/src/main/RelayNotifier.scala
@@ -15,29 +15,38 @@ final private class RelayNotifier(
 
     private val dedupNotif = OnceEvery[StudyChapterId](1.day)
 
-    def apply(rt: RelayRound.WithTour, chapter: Chapter, game: RelayGame): Funit =
+    def apply(rt: RelayRound.WithTour, chapter: Chapter, game: RelayGame): Unit =
       dedupNotif(chapter.id).so:
-        val futureByColor = game.fideIds.mapWithColor: (color, fid) =>
-          for
-            followers <- fid.so(getPlayerFollowers)
-            notify    <- followers.nonEmpty.so:
-              chapter.tags.names.sequence.so: names =>
-                notifyApi.notifyMany(
-                  followers,
-                  NotificationContent.BroadcastRound(
-                    url = rt.path(chapter.id),
-                    title = rt.tour.name.value,
-                    text = s"${names(color)} is playing against ${names(!color)} in ${rt.round.name}"
-                  )
-                )
-          yield notify
-        Future.sequence(futureByColor.all).void
+        game.fideIds.foreach: (color, fid) =>
+          fid.so: fid =>
+            for
+              followers <- getPlayerFollowers(fid)
+              _         <- followers.nonEmpty.so(notify(followers, color, fid))
+            yield ()
+
+      def notify(followers: List[UserId], color: Color, fid: chess.FideId) =
+        val names = chapter.tags.names
+        names(color) match
+          case Some(playerName) =>
+            val opponent = names(!color).map(name => s" against ${name} ").getOrElse(" ")
+            notifyApi.notifyMany(
+              followers,
+              NotificationContent.BroadcastRound(
+                url = rt.path(chapter.id),
+                title = rt.tour.name.value,
+                text = s"${playerName} is playing${opponent}in ${rt.round.name}"
+              )
+            )
+          case None =>
+            fuccess(
+              lila.log("relay").warn(s"Missing player name for FIDE id ${fid} in game ${chapter.id}")
+            )
 
   private object notifyTournamentSubscribers:
 
     private val dedupDbReq = OnceEvery[RelayRoundId](5.minutes)
 
-    def apply(rt: RelayRound.WithTour): Funit =
+    def apply(rt: RelayRound.WithTour): Unit =
       dedupDbReq(rt.round.id).so:
         tourRepo
           .hasNotified(rt)
