1158da41b4a823458ce0125bdc71f6f8761539ca
Thibault Duplessis <t@lichess.org>
2025-07-01 10:57:21 +0200

    RelayGame.fideIdsPair even if only one ID is known

--- modules/relay/src/main/RelayGame.scala
+++ modules/relay/src/main/RelayGame.scala
@@ -31,7 +31,7 @@ case class RelayGame(
   )
 
   def fideIdsPair: Option[PairOf[Option[chess.FideId]]] =
-    tags.fideIds.map(_.filter(_.value > 0)).some.filter(_.forall(_.isDefined)).map(_.toPair)
+    tags.fideIds.some.filter(_.exists(_.exists(_.value > 0))).map(_.toPair)
 
   def hasUnknownPlayer: Boolean =
     List(RelayGame.whiteTags, RelayGame.blackTags).exists:
