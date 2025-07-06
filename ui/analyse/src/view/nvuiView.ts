da4e40ecf96b0be04a6e095e5e18f1da92c1a701
yafred <12477220+yafred@users.noreply.github.com>
2025-07-06 10:25:28 +0200

    announce computer eval from nvui chessboard

--- ui/analyse/src/view/nvuiView.ts
+++ ui/analyse/src/view/nvuiView.ts
@@ -232,7 +232,10 @@ export function clickHook(main?: (el: HTMLElement) => void, post?: () => void) {
   };
 }
 
-function boardEventsHook({ ctrl, pieceStyle, prefixStyle }: AnalyseNvuiContext, el: HTMLElement): void {
+function boardEventsHook(
+  { ctrl, pieceStyle, prefixStyle, notify }: AnalyseNvuiContext,
+  el: HTMLElement,
+): void {
   const $board = $(el);
   const $buttons = $board.find('button');
   const steps = () => ctrl.tree.getNodeList(ctrl.path);
@@ -253,6 +256,7 @@ function boardEventsHook({ ctrl, pieceStyle, prefixStyle }: AnalyseNvuiContext, 
     else if (e.key.match(/^[kqrbnp]$/i)) pieceJumpingHandler(selectSound, errorSound)(e);
     else if (e.key.toLowerCase() === 'm')
       possibleMovesHandler(ctrl.turnColor(), ctrl.chessground, ctrl.data.game.variant.key, ctrl.nodeList)(e);
+    else if (e.key.toLowerCase() === 'v') notify.set(renderEvalAndDepth(ctrl));
   });
 }
 
