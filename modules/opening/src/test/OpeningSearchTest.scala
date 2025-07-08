dbca2e24195c098482c80f06be471808ef262395
johndoknjas <jdoknjas@sfu.ca>
2025-07-07 20:48:57 -0700

    Add tests.

--- modules/opening/src/test/OpeningSearchTest.scala
+++ modules/opening/src/test/OpeningSearchTest.scala
@@ -2,6 +2,7 @@ package lila.opening
 
 import chess.opening.OpeningName
 import OpeningSearch.*
+import chess.format.pgn.PgnMovesStr
 
 class OpeningSearchTest extends munit.FunSuite:
 
@@ -27,4 +28,18 @@ class OpeningSearchTest extends munit.FunSuite:
     assertEquals(makeQuery("1. e4 e5 2. d4").numberedPgn, "1. e4 e5 2. d4")
 
   test("makeQuery works without spaces"):
-    assertEquals(makeQuery("1.e4 e5 2.d4").numberedPgn, makeQuery("1. e4 e5 2. d4").numberedPgn)
+    val querySpaces = makeQuery("1. e4 e5 2. d4")
+    val queryNoSpaces = makeQuery("1.e4 e5 2.d4")
+    assertEquals(querySpaces.numberedPgn, queryNoSpaces.numberedPgn)
+    assertEquals(querySpaces.tokens, queryNoSpaces.tokens)
+    // As 1. h3 a6 isn't an opening, the following assertion only passes if "h3" is a query token, not "1.h3".
+    assert(search("1.h3 a6").headOption.map(_.pgn.value).exists(_.startsWith("1. h3")))
+
+  test("long opening names penalized if matched using name"):
+    assertEquals(search("Advance").headOption.map(_.name), OpeningName("Réti Opening: Advance Variation").some)
+
+  test("long opening name not penalized if matched using notation"):
+    assertEquals(search("1. d4").headOption.map(_.name), OpeningName("Queen's Pawn Game").some)
+
+  test("piece letter in notation"):
+    assertEquals(search("1. d4 Nf6").headOption.map(_.pgn), PgnMovesStr("1. d4 Nf6").some)
