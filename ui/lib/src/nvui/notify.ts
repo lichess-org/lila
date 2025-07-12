5c4e156c75bea186dde403f5a3bda1b530ce8480
Jonathan Gamble <gamblej@gmail.com>
2025-07-12 01:09:54 -0500

    fix voiceover, again

--- ui/lib/src/nvui/notify.ts
+++ ui/lib/src/nvui/notify.ts
@@ -2,36 +2,26 @@ import { h, type VNode, type VNodeData } from 'snabbdom';
 import { requestIdleCallback } from '../common';
 import { isMac } from '../device';
 
-type Notification = {
-  text: string;
-  date: Date;
-};
-
 export class Notify {
-  notification: Notification | undefined;
-  redraw?: () => void;
+  text = '';
 
-  constructor() {}
+  constructor(public redraw: Redraw | undefined = undefined) {}
 
   set = (msg: string): void => {
-    // make sure it's different from previous, so it gets read again
-    if (this.notification && this.notification.text === msg) msg += ' ';
-    this.notification = { text: msg, date: new Date() };
-    requestIdleCallback(() => this.redraw && this.redraw(), 500);
+    this.text = msg;
+    requestIdleCallback(() => {
+      this.redraw?.();
+      this.text = '';
+    }, 500);
   };
 
-  currentText = (): string =>
-    this.notification && this.notification.date.getTime() > Date.now() - 3000 ? this.notification.text : '';
-
-  render = (): VNode => liveText(this.currentText(), 'assertive', 'div.notify');
+  render = (): VNode => liveText(this.text, 'assertive', 'div.notify');
 }
 
 export function liveText(text: string, live: 'assertive' | 'polite' = 'polite', sel: string = 'p'): VNode {
+  const key = new Date().getTime().toString();
   const data: VNodeData = isMac()
-    ? {
-        attrs: { role: 'alert' },
-        hook: { insert: (vnode: VNode) => setTimeout(() => (vnode.elm!.textContent = text), 50) },
-      }
-    : { attrs: { 'aria-live': live, 'aria-atomic': 'true' } };
-  return h(sel, data, isMac() ? undefined : text);
+    ? { key, attrs: { role: 'alert' } }
+    : { key, attrs: { 'aria-live': live, 'aria-atomic': 'true' } };
+  return h(sel, data, text);
 }
