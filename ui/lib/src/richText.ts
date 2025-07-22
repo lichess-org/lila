37b5be3eb6ecd0c1a9b6bc25fb98408cc8e71fe4
Allan Joseph <allanjoseph98@gmail.com>
2025-07-22 07:47:05 +0000

    fix extra space

--- ui/lib/src/richText.ts
+++ ui/lib/src/richText.ts
@@ -13,7 +13,7 @@ export const userPattern: RegExp = /(^|[^\w@#/])@([a-z0-9_-]{2,30})/gi;
 export const isMoreThanText = (str: string): boolean => /(\n|(@|#|\.)\w{2,}|(board|game) \d)/i.test(str);
 
 const linkHtml = (href: string, content: string, expandable: boolean = true): string =>
-  `<a ${expandable ? '' : 'class="text"'} target="_blank" rel="nofollow noreferrer" href="${href}">${content}</a>`;
+  `<a${expandable ? '' : ' class="text"'} target="_blank" rel="nofollow noreferrer" href="${href}">${content}</a>`;
 
 export function toLink(url: string): string {
   if (!url.match(/^[A-Za-z]+:\/\//)) url = 'https://' + url;
