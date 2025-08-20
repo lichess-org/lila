5d8dd5a49c777ee484d7fcdc06e2a2b8ea8ec677
Thibault Duplessis <t@lichess.org>
2025-08-20 18:46:29 +0200

    Pref.sayGG - server side courtesy chat message setting WIP

--- modules/pref/src/main/JsonView.scala
+++ modules/pref/src/main/JsonView.scala
@@ -2,47 +2,46 @@ package lila.pref
 
 import play.api.libs.json.*
 
-object JsonView:
-
-  def write(p: Pref, lichobileCompat: Boolean) =
-    Json.obj(
-      "dark" -> (p.bg != Pref.Bg.LIGHT),
-      "transp" -> (p.bg == Pref.Bg.TRANSPARENT),
-      "bgImg" -> p.bgImgOrDefault,
-      "is3d" -> p.is3d,
-      "theme" -> p.theme,
-      "pieceSet" -> p.pieceSet,
-      "theme3d" -> p.theme3d,
-      "pieceSet3d" -> p.pieceSet3d,
-      "soundSet" -> p.soundSet,
-      "autoQueen" -> p.autoQueen,
-      "autoThreefold" -> p.autoThreefold,
-      "takeback" -> p.takeback,
-      "moretime" -> p.moretime,
-      "clockTenths" -> p.clockTenths,
-      "clockBar" -> p.clockBar,
-      "clockSound" -> p.clockSound,
-      "premove" -> p.premove,
-      "animation" -> p.animation,
-      "pieceNotation" -> p.pieceNotation,
-      "captured" -> p.captured,
-      "follow" -> p.follow,
-      "highlight" -> p.highlight,
-      "destination" -> p.destination,
-      "coords" -> p.coords,
-      "replay" -> p.replay,
-      "challenge" -> p.challenge,
-      "message" -> p.message,
-      "submitMove" -> {
-        if lichobileCompat then Pref.SubmitMove.lichobile.serverToApp(p.submitMove)
-        else p.submitMove
-      },
-      "confirmResign" -> p.confirmResign,
-      "insightShare" -> p.insightShare,
-      "keyboardMove" -> p.keyboardMove,
-      "voiceMove" -> p.hasVoice,
-      "zen" -> p.zen,
-      "ratings" -> p.ratings,
-      "moveEvent" -> p.moveEvent,
-      "rookCastle" -> p.rookCastle
-    )
+def toJson(p: Pref, lichobileCompat: Boolean) = Json.obj(
+  "dark" -> (p.bg != Pref.Bg.LIGHT),
+  "transp" -> (p.bg == Pref.Bg.TRANSPARENT),
+  "bgImg" -> p.bgImgOrDefault,
+  "is3d" -> p.is3d,
+  "theme" -> p.theme,
+  "pieceSet" -> p.pieceSet,
+  "theme3d" -> p.theme3d,
+  "pieceSet3d" -> p.pieceSet3d,
+  "soundSet" -> p.soundSet,
+  "autoQueen" -> p.autoQueen,
+  "autoThreefold" -> p.autoThreefold,
+  "takeback" -> p.takeback,
+  "moretime" -> p.moretime,
+  "clockTenths" -> p.clockTenths,
+  "clockBar" -> p.clockBar,
+  "clockSound" -> p.clockSound,
+  "premove" -> p.premove,
+  "animation" -> p.animation,
+  "pieceNotation" -> p.pieceNotation,
+  "captured" -> p.captured,
+  "follow" -> p.follow,
+  "highlight" -> p.highlight,
+  "destination" -> p.destination,
+  "coords" -> p.coords,
+  "replay" -> p.replay,
+  "challenge" -> p.challenge,
+  "message" -> p.message,
+  "submitMove" -> {
+    if lichobileCompat then Pref.SubmitMove.lichobile.serverToApp(p.submitMove)
+    else p.submitMove
+  },
+  "confirmResign" -> p.confirmResign,
+  "insightShare" -> p.insightShare,
+  "keyboardMove" -> p.keyboardMove,
+  "voiceMove" -> p.hasVoice,
+  "zen" -> p.zen,
+  "ratings" -> p.ratings,
+  "moveEvent" -> p.moveEvent,
+  "rookCastle" -> p.rookCastle,
+  "flairs" -> p.flairs,
+  "sayGG" -> p.sayGG
+)
