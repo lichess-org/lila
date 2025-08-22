c4e3baed5b37510cfbd012992ee4dc60b5af23c3
Scala Steward <scala_steward@virtuslab.com>
2025-08-22 16:32:46 +0000

    Update reactivemongo-akkastream to 1.1.0-RC17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -83,7 +83,7 @@ object Dependencies {
   object reactivemongo {
     val driver = ("org.reactivemongo" %% "reactivemongo" % "1.1.0-RC13")
       .exclude("org.scala-lang.modules", "scala-java8-compat_2.13")
-    val stream = "org.reactivemongo" %% "reactivemongo-akkastream" % "1.1.0-RC16"
+    val stream = "org.reactivemongo" %% "reactivemongo-akkastream" % "1.1.0-RC17"
     val shaded = "org.reactivemongo" % s"reactivemongo-shaded-native-$os-$dashArch" % "1.1.0-RC15"
     // val kamon  = "org.reactivemongo" %% "reactivemongo-kamon"         % "1.0.8"
     def bundle = Seq(driver, stream)
