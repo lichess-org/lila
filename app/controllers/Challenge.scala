99d787d98885168ce4f4c1810876e2b0b166c817
Thibault Duplessis <t@lichess.org>
2025-08-27 21:09:37 +0200

    open /api/challenge/:id/show to anon

--- app/controllers/Challenge.scala
+++ app/controllers/Challenge.scala
@@ -27,7 +27,7 @@ final class Challenge(env: Env) extends LilaController(env):
   def show(id: ChallengeId, @annotation.nowarn color: Option[Color]) = Open:
     showId(id)
 
-  def apiShow(id: ChallengeId) = Scoped(_.Challenge.Read, _.Web.Mobile) { ctx ?=> _ ?=>
+  def apiShow(id: ChallengeId) = AnonOrScoped(_.Challenge.Read, _.Web.Mobile) { ctx ?=>
     Found(api.byId(id)): c =>
       val direction: Option[Direction] =
         if isMine(c) then Direction.Out.some
