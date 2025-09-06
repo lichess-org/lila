d61997f62ffe1fab556e7755d514dc224aecbb50
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2025-09-05 21:04:26 -0400

    formatting

--- ui/analyse/src/study/relay/relayManagerView.ts
+++ ui/analyse/src/study/relay/relayManagerView.ts
@@ -94,7 +94,11 @@ const statePush = (ctrl: RelayCtrl) =>
       hl('a', { attrs: { href: '/broadcast/app' } }, 'Broadcaster App'),
       hl('br'),
       hl('small', [
-        hl('a', { attrs: { href: broadcasterDeepLink(ctrl.currentRound().url) } }, 'Open this round in the app'),
+        hl(
+          'a',
+          { attrs: { href: broadcasterDeepLink(ctrl.currentRound().url) } },
+          'Open this round in the app',
+        ),
       ]),
     ]),
   ]);
