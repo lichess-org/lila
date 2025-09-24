8cd9d30ed8d7f637ac07042fc93ab4ac972ca0ae
Thibault Duplessis <t@lichess.org>
2025-09-24 20:57:32 +0200

    better count bot time spent playing with humans

--- bin/mongodb/bot-count-human.js
+++ bin/mongodb/bot-count-human.js
@@ -1,17 +1,24 @@
 const botIds = new Set(db.user4.distinct('_id', { title: 'BOT', 'count.game': { $gte: 10 } }))
 
-const countVsHuman = (uid) => {
+print(botIds.size + ' bot ids');
+
+const timeVsHuman = (uid) => {
   let nb = 0;
-  db.game5.find({ us: uid }, { us: 1, t: 1 }).forEach(g => {
+  let seconds = 0;
+  db.game5.find({ us: uid, c: { $exists: 1 } }, { us: 1, t: 1, ca: 1, ua: 1 }).forEach(g => {
     const oppId = g.us.find(id => id && id !== uid);
-    if (oppId && !botIds.has(oppId)) nb += g.t
+    if (oppId && !botIds.has(oppId)) {
+      const s = Math.round(Math.min(g.t * 5, (g.ua - (g.ca | 0)) / 1000));
+      seconds += s;
+      nb++;
+    }
   })
-  return nb;
+  return [nb, seconds];
 }
 
 db.user4.find({ title: 'BOT' }, { username: 1 }).forEach(function(user) {
-  const count = countVsHuman(user._id);
-  if (count > 0) print(user.username + ' ' + count);
-  db.user4.updateOne({ _id: user._id }, { $set: { 'time.human': count } });
+  const [nb, seconds] = timeVsHuman(user._id);
+  if (seconds > 0) print(user.username + ' ' + seconds + 's in ' + nb + ' games, ', Math.round(seconds / nb), 's/game');
+  db.user4.updateOne({ _id: user._id }, { $set: { 'time.human': seconds } });
 });
 
