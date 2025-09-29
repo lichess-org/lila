2b8777f8cb0ef7389ef3c82ce7706ea1dcea702b
Jonathan Gamble <gamblej@gmail.com>
2025-09-28 22:07:07 -0500

    fix lint

--- ui/site/src/asset.ts
+++ ui/site/src/asset.ts
@@ -84,7 +84,7 @@ export const loadPieces = new Promise<void>((resolve, reject) => {
   if (document.getElementById('main-wrap')?.classList.contains('is3d')) return resolve();
   const urls = ['white', 'black']
     .flatMap(c => ['pawn', 'knight', 'bishop', 'rook', 'queen', 'king'].map(r => `---${c}-${r}`))
-    .map(u => getComputedStyle(document.documentElement).getPropertyValue(u).slice(4, -1))
+    .map(u => window.getComputedStyle(document.documentElement).getPropertyValue(u).slice(4, -1))
     .filter(x => x);
   let assetsToDecode = urls.length;
   if (assetsToDecode === 0) return resolve();
