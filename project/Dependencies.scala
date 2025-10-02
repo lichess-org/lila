31def5a708b89aa4ba7360f15fb5bf77d22e14b5
Thanh Le <lenguyenthanh@hotmail.com>
2025-10-02 18:22:28 +0200

    Update cats-mtl 1.6.0
    oops it's was published for a while

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
 
   val cats = "org.typelevel" %% "cats-core" % "2.13.0"
   val alleycats = "org.typelevel" %% "alleycats-core" % "2.13.0"
-  val catsMtl = "org.typelevel" %% "cats-mtl" % "1.6-6ad7882-SNAPSHOT"
+  val catsMtl = "org.typelevel" %% "cats-mtl" % "1.6.0"
   val hasher = "com.roundeights" %% "hasher" % "1.3.1"
   val compression = "com.github.lichess-org" % "compression" % "3.1.1"
   val maxmind = "com.maxmind.geoip2" % "geoip2" % "4.0.1"
