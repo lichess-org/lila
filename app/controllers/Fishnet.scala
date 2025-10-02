47f01e9c8e7eda7b0a707c8b4cc6a8e0a82184d7
Thibault Duplessis <t@lichess.org>
2025-10-02 10:13:06 +0200

    code golf with cats.mtl.Handle

--- app/controllers/Fishnet.scala
+++ app/controllers/Fishnet.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import monocle.syntax.all.*
+import cats.mtl.Handle.*
 import play.api.libs.json.*
 import play.api.mvc.*
 
@@ -22,15 +23,14 @@ final class Fishnet(env: Env) extends LilaController(env):
         .acquire(client, slow)
         .addEffect: jobOpt =>
           lila.mon.fishnet.http.request(jobOpt.isDefined).increment()
-        .map(Right.apply)
     }
 
   def analysis(workId: String, slow: Boolean = false, stop: Boolean = false) =
     ClientAction[JsonApi.Request.PostAnalysis] { data => client =>
       import lila.fishnet.FishnetApi.*
       def onComplete =
-        if stop then fuccess(Left(NoContent))
-        else api.acquire(client, slow).map(Right.apply)
+        if stop then NoContent.raise
+        else api.acquire(client, slow)
       api
         .postAnalysis(Work.Id(workId), client, data)
         .flatMap:
@@ -38,32 +38,31 @@ final class Fishnet(env: Env) extends LilaController(env):
             env.round.proxyRepo.updateIfPresent(GameId(analysis.id.value)): g =>
               g.focus(_.metadata.analysed).replace(true)
             onComplete
-          case _: PostAnalysisResult.Partial => fuccess(Left(NoContent))
-          case PostAnalysisResult.UnusedPartial => fuccess(Left(NoContent))
+          case _: PostAnalysisResult.Partial => NoContent.raise
+          case PostAnalysisResult.UnusedPartial => NoContent.raise
         .recoverWith:
           case WorkNotFound => onComplete
           case GameNotFound => onComplete
           case NotAcquired => onComplete
-          case e => fuccess(Left(InternalServerError(e.getMessage)))
+          case e => InternalServerError(e.getMessage).raise
     }
 
   def abort(workId: String) =
     ClientAction[JsonApi.Request.Acquire] { _ => client =>
-      api.abort(Work.Id(workId), client).inject(Right(none))
+      api.abort(Work.Id(workId), client).inject(none)
     }
 
   def keyExists(key: String) = Anon:
     api
       .keyExists(lila.fishnet.Client.Key(key))
       .map:
-        if _ then Ok
-        else NotFound
+        if _ then Ok else NotFound
 
   val status = Anon:
     api.status.map { JsonStrOk(_) }
 
   private def ClientAction[A <: JsonApi.Request](
-      f: A => lila.fishnet.Client => Fu[Either[Result, Option[JsonApi.Work]]]
+      f: A => lila.fishnet.Client => FuRaise[Result, Option[JsonApi.Work]]
   )(using Reads[A]) =
     AnonBodyOf(parse.tolerantJson): body =>
       body
@@ -77,10 +76,9 @@ final class Fishnet(env: Env) extends LilaController(env):
             api.authenticateClient(data, req.ipAddress).flatMap {
               case Failure(msg) => Unauthorized(jsonError(msg.getMessage))
               case Success(client) =>
-                f(data)(client).map {
-                  case Right(Some(work)) => Accepted(Json.toJson(work))
-                  case Right(None) => NoContent
-                  case Left(result) => result
-                }
+                allow:
+                  f(data)(client).map:
+                    _.map(Json.toJson).fold(NoContent)(Accepted(_))
+                .rescue(identity)
             }
         )
