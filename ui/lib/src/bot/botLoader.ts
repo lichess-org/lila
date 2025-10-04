0d36a6fcdae993f4309674005fba21025d029e6b
Thibault Duplessis <t@lichess.org>
2025-10-04 18:39:26 +0200

    fix zerofish link, hash and loading

--- ui/lib/src/bot/botLoader.ts
+++ ui/lib/src/bot/botLoader.ts
@@ -7,6 +7,8 @@ import { definedMap } from '../algo';
 import { makeLichessBook } from './lichessBook';
 import { myUserId, myUsername } from '../common';
 
+export const zerofishPath = 'npm/@lichess-org/zerofish/dist';
+
 export class BotLoader {
   zerofish: Zerofish;
   readonly net: Map<string, Promise<{ key: string; data: Uint8Array }>> = new Map();
@@ -32,7 +34,8 @@ export class BotLoader {
       defBots ?? xhr.json('/bots').then(res => res.bots),
       this.zerofish ??
         makeZerofish({
-          locator: (file: string) => site.asset.url(`npm/${file}`, { documentOrigin: file.endsWith('js') }),
+          locator: (file: string) =>
+            site.asset.url(`${zerofishPath}/${file}`, { documentOrigin: file.endsWith('js') }),
         }).then(zf => (this.zerofish = zf)),
     ]);
     for (const b of [...bots].filter(Bot.isValid)) {
