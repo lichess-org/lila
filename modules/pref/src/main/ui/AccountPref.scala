e2379c42f2c359babc0dd001c4118bee0b5ddb75
Thibault Duplessis <t@lichess.org>
2025-10-05 12:21:44 +0200

    move new clock setting to the bottom
    so that existing settings stay where they were

--- modules/pref/src/main/ui/AccountPref.scala
+++ modules/pref/src/main/ui/AccountPref.scala
@@ -90,11 +90,6 @@ final class AccountPref(helpers: Helpers, helper: PrefHelper, bits: AccountUi):
           ),
           categFieldset(PrefCateg.ChessClock, categ)(
             setting(
-              "Swap clock and username positions in realtime games on portrait mode phones",
-              radios(form("clock.swapClock"), booleanChoices),
-              "swapClock"
-            ),
-            setting(
               trp.tenthsOfSeconds(),
               radios(form("clock.tenths"), translatedClockTenthsChoices),
               "tenthsOfSeconds"
@@ -113,6 +108,11 @@ final class AccountPref(helpers: Helpers, helper: PrefHelper, bits: AccountUi):
               trp.giveMoreTime(),
               radios(form("clock.moretime"), translatedMoretimeChoices),
               "giveMoreTime"
+            ),
+            setting(
+              "Swap clock and username positions in realtime games on portrait mode phones",
+              radios(form("clock.swapClock"), booleanChoices),
+              "swapClock"
             )
           ),
           categFieldset(PrefCateg.GameBehavior, categ)(
