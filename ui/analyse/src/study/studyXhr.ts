eb4d5e769b1d892b093d518d3ee79a124fa8d007
Thibault Duplessis <t@lichess.org>
2025-10-11 15:17:20 +0200

    rewrite study xhr functions as async

--- ui/analyse/src/study/studyXhr.ts
+++ ui/analyse/src/study/studyXhr.ts
@@ -26,19 +26,17 @@ export const practiceComplete = (chapterId: string, nbMoves: number) =>
     method: 'POST',
   });
 
-export const importPgn = (studyId: string, data: any) =>
-  xhrRaw(`/study/${studyId}/import-pgn?sri=${site.sri}`, {
+export const importPgn = async (studyId: string, data: any) => {
+  const res = await xhrRaw(`/study/${studyId}/import-pgn?sri=${site.sri}`, {
     method: 'POST',
     body: xhrForm(data),
-  }).then(res => ensureOk(res));
-
-export const ensureOk = (res: Response): Promise<string> => {
+  });
   if (res.ok) return res.text();
   if (res.status === 429) throw new Error('Too many requests');
   if (res.status === 413) throw new Error('The uploaded file is too large');
-  if (res.status === 400)
-    return res.text().then(text => {
-      throw new Error(text);
-    });
+  if (res.status === 400) {
+    const text = await res.text();
+    throw new Error(text);
+  }
   throw new Error(`Error ${res.status}`);
 };
