74d9a60c165c3386646b94dd8ea20f030361ca22
Thibault Duplessis <t@lichess.org>
2025-10-17 10:06:33 +0200

    remove unused dependency

--- modules/mod/src/main/Env.scala
+++ modules/mod/src/main/Env.scala
@@ -34,8 +34,7 @@ final class Env(
     noteApi: lila.user.NoteApi,
     cacheApi: lila.memo.CacheApi,
     ircApi: lila.core.irc.IrcApi,
-    msgApi: lila.core.msg.MsgApi,
-    picfitApi: lila.memo.PicfitApi
+    msgApi: lila.core.msg.MsgApi
 )(using Executor, Scheduler, lila.core.i18n.Translator, akka.stream.Materializer):
 
   private lazy val logRepo = ModlogRepo(db(CollName("modlog")))
