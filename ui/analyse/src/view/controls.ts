5feec0febd204532569c790f17fa6047e7b51cdd
Jonathan Gamble <gamblej@gmail.com>
2025-10-19 16:11:51 -0500

    filter displayed eval by current toggle values

--- ui/analyse/src/view/controls.ts
+++ ui/analyse/src/view/controls.ts
@@ -99,7 +99,7 @@ function renderPracticeTab(ctrl: AnalyseCtrl): LooseVNode {
 
 function renderMobileCevalTab(ctrl: AnalyseCtrl): LooseVNode {
   const engineMode = ctrl.activeControlMode() || 'ceval',
-    ev = ctrl.node.ceval ?? (ctrl.showFishnetAnalysis() ? ctrl.node.eval : undefined),
+    ev = ctrl.allowedEval() || undefined,
     evalstr = ev?.cp !== undefined ? renderEval(ev.cp) : ev?.mate ? '#' + ev.mate : '',
     active = ctrl.activeControlMode() && !ctrl.activeControlBarTool(),
     latent = ctrl.activeControlMode() && !!ctrl.activeControlBarTool();
