90e5092551beb7d50277f7ea59fd836b4cd33b0a
Scala Steward <scala_steward@virtuslab.com>
2025-10-27 15:25:24 +0000

    Update reactivemongo-shaded-native-linux-x86-64 to 1.1.0-RC19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -87,7 +87,7 @@ object Dependencies {
   object reactivemongo {
     val driver = ("org.reactivemongo" %% "reactivemongo" % "1.1.0-RC16")
     val stream = "org.reactivemongo" %% "reactivemongo-akkastream" % "1.1.0-RC18"
-    val shaded = "org.reactivemongo" % s"reactivemongo-shaded-native-$os-$dashArch" % "1.1.0-RC15"
+    val shaded = "org.reactivemongo" % s"reactivemongo-shaded-native-$os-$dashArch" % "1.1.0-RC19"
     // val kamon  = "org.reactivemongo" %% "reactivemongo-kamon"         % "1.0.8"
     def bundle = Seq(driver, stream)
   }
