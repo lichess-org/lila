c83949f45ea8c4bfc16c38491abcde175e4ec892
johndoknjas <jdoknjas@sfu.ca>
2025-11-01 03:08:05 -0700

    When going from the members tab to the chapters tab, wait until the next animation before doing scroll stuff. Fixes an issue where an obscured chapter would not be automatically scrolled to.

--- ui/analyse/src/study/studyChapters.ts
+++ ui/analyse/src/study/studyChapters.ts
@@ -172,17 +172,13 @@ export function view(ctrl: StudyCtrl): VNode {
         l = listOfChapters.getBoundingClientRect();
       return c.top >= l.top && c.bottom <= l.bottom;
     };
-    const newCount = ctrl.chapters.list.size(),
-      vData = vnode.data!.li!,
+    const vData = vnode.data!.li!,
       el = vnode.elm as HTMLElement;
-    if (vData.count !== newCount) {
-      if (current.id !== ctrl.chapters.list.first().id) scrollToInnerSelector(el, '.active');
-    } else if (vData.currentId !== ctrl.data.chapter.id) {
-      vData.currentId = ctrl.data.chapter.id;
-      if (!isChapterFullyVisible(el, el.querySelector('.active')!)) scrollToInnerSelector(el, '.active');
-    }
-    vData.count = newCount;
-    if (canContribute && newCount > 1 && !vData.sortable) {
+    requestAnimationFrame(() => {
+      const active = el.querySelector('.active') as HTMLElement | null;
+      if (active && !isChapterFullyVisible(el, active)) scrollToInnerSelector(el, '.active');
+    });
+    if (canContribute && ctrl.chapters.list.size() > 1 && !vData.sortable) {
       site.asset.loadEsm<typeof Sortable>('sortable.esm', { npm: true }).then(s => {
         vData.sortable = s.create(el, {
           draggable: '.draggable',
