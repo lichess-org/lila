e19d9b8daf31f5815799b5f7033f7346936e891d
Thibault Duplessis <t@lichess.org>
2025-11-24 08:36:36 +0100

    remove unused function isBrowserSupported

--- ui/lib/src/device.ts
+++ ui/lib/src/device.ts
@@ -2,9 +2,6 @@ import { memoize } from './index';
 import { bind, type Hooks } from './view';
 import * as licon from './licon';
 
-// when feature detection is not enough
-export const isBrowserSupported = (): boolean => !isSafari({ below: '15.4' }); // TODO add unsupported browsers
-
 export const hookMobileMousedown = (f: (e: Event) => any): Hooks =>
   bind('ontouchstart' in window ? 'click' : 'mousedown', f);
 
