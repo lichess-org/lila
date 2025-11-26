adf96911541ee3b55f8f37622354444905e1a10d
Eleos5 <44039115+Eleos5@users.noreply.github.com>
2025-11-25 20:20:04 -0600

    remove whitespace

--- modules/round/src/main/JsonView.scala
+++ modules/round/src/main/JsonView.scala
@@ -15,7 +15,7 @@ import lila.core.perf.KeyedPerf
 import lila.core.user.{ GameUser, GameUsers, WithPerf }
 import lila.game.GameExt.moveTimes
 import lila.game.JsonView.given
-import lila.pref.Pref 
+import lila.pref.Pref
 import lila.round.RoundGame.*
 
 final class JsonView(
