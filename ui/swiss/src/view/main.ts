0cb3c0f5c23351dd9fbf61c8290498a2bfc3f652
Thibault Duplessis <t@lichess.org>
2025-12-04 23:40:08 +0100

    update API doc urls

--- ui/swiss/src/view/main.ts
+++ ui/swiss/src/view/main.ts
@@ -249,7 +249,9 @@ function stats(ctrl: SwissCtrl) {
       hl('br'),
       hl(
         'a.text',
-        { attrs: { 'data-icon': licon.InfoCircle, href: 'https://lichess.org/api#tag/swiss-tournaments' } },
+        {
+          attrs: { 'data-icon': licon.InfoCircle, href: '/api#tag/swiss-tournaments' },
+        },
         'Swiss API documentation',
       ),
     ]),
