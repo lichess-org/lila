b2d0e317361e4fb3c282efbe7467dbd539063ab1
johndoknjas <jdoknjas@sfu.ca>
2025-12-07 21:42:36 -0800

    Make a number of loose (in)equality checks strict.

--- ui/swiss/src/view/main.ts
+++ ui/swiss/src/view/main.ts
@@ -151,7 +151,7 @@ function joinButton(ctrl: SwissCtrl): VNode | undefined {
           i18n.site.join,
         );
 
-  if (d.me && d.status != 'finished')
+  if (d.me && d.status !== 'finished')
     return d.me.absent
       ? ctrl.joinSpinner
         ? spinnerVdom()
