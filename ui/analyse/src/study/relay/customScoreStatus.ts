163b08608744065c8a6c7a4ce6df4d3b526be814
Thibault Duplessis <t@lichess.org>
2025-12-17 09:36:01 +0100

    simplify and maybe fix customScoreStatus

--- ui/analyse/src/study/relay/customScoreStatus.ts
+++ ui/analyse/src/study/relay/customScoreStatus.ts
@@ -1,15 +1,12 @@
 import { hl, type LooseVNodes, type VNodeChildElement } from 'lib/view';
-import type { StatusStr } from '../interfaces';
-import type { RelayRound } from './interfaces';
+import type { GamePointsStr } from '../interfaces';
+import type { CustomScoring, RelayRound } from './interfaces';
+import { opposite } from 'chessops/util';
 
 const points = (point: string) => parseFloat(point.replace('½', '.5'));
 const colorClass = (point: string) => (points(point) === 1 ? 'good' : points(point) === 0 ? 'bad' : 'status');
 
-const withCustomScore = (
-  point: string,
-  color: Color,
-  customScoring?: RelayRound['customScoring'],
-): VNodeChildElement => {
+const withCustomScore = (point: string, color: Color, customScoring?: CustomScoring): VNodeChildElement => {
   if (!customScoring) return point;
   const base = points(point);
   return base === 1
@@ -21,25 +18,19 @@ const withCustomScore = (
       : 0;
 };
 
-export const coloredStatusStr = (
-  status: Exclude<StatusStr, '*'>,
-  pov: Color,
-  round?: RelayRound,
-): LooseVNodes => {
+export const coloredStatusStr = (gamePoints: GamePointsStr, pov: Color, round?: RelayRound): LooseVNodes => {
   const customScoring = round?.customScoring;
-  const povStatus = pov === 'white' ? status : (status.split('').reverse().join('') as StatusStr);
+  const points = gamePoints.split('-');
+  if (pov === 'black') points.reverse();
   return [
-    hl(`${colorClass(povStatus[0])}.result`, withCustomScore(povStatus[0], 'white', customScoring)),
+    hl(`${colorClass(points[0])}.result`, withCustomScore(points[0], pov, customScoring)),
     '-',
-    hl(
-      `${colorClass(povStatus.split('').reverse()[0])}.result`,
-      withCustomScore(povStatus.split('').reverse()[0], 'black', customScoring),
-    ),
+    hl(`${colorClass(points[1])}.result`, withCustomScore(points[1], opposite(pov), customScoring)),
   ];
 };
 
 export const playerColoredResult = (
-  status: Exclude<StatusStr, '*'>,
+  status: GamePointsStr,
   color: Color,
   round?: RelayRound,
 ): { tag: 'good' | 'bad' | 'status'; points: VNodeChildElement } => {
