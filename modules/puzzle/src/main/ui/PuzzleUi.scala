81364004df6f135db4501c1d86d5189542f76389
Thibault Duplessis <t@lichess.org>
2025-12-18 09:26:07 +0100

    load nvui translations when speech synthesis is enabled
    on puzzles, games and studies
    
    for #18916
    closes #18923

--- modules/puzzle/src/main/ui/PuzzleUi.scala
+++ modules/puzzle/src/main/ui/PuzzleUi.scala
@@ -31,7 +31,8 @@ final class PuzzleUi(helpers: Helpers, val bits: PuzzleBits)(
       .css(ctx.pref.hasVoice.option("voice"))
       .css(ctx.blind.option("round.nvui"))
       .i18n(_.puzzle, _.puzzleTheme, _.storm)
-      .i18nOpt(ctx.blind, _.keyboardMove, _.nvui)
+      .i18nOpt(ctx.speechSynthesis, _.nvui)
+      .i18nOpt(ctx.blind, _.keyboardMove)
       .js(ctx.blind.option(Esm("puzzle.nvui")))
       .js(
         PageModule(
