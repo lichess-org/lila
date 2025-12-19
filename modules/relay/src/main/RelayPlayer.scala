72f87e8f78c387df101a84fd54d4494b3a1f76a0
Thibault Duplessis <t@lichess.org>
2025-12-19 18:31:17 +0100

    Revert "concatenate players of multiple tournaments of a group"
    This reverts commit 2b68adacb3428d63890b1ee2f1211d5c64ea8f51.

--- modules/relay/src/main/RelayPlayer.scala
+++ modules/relay/src/main/RelayPlayer.scala
@@ -170,7 +170,6 @@ object RelayPlayer:
 private final class RelayPlayerApi(
     tourRepo: RelayTourRepo,
     roundRepo: RelayRoundRepo,
-    groupRepo: RelayGroupRepo,
     chapterRepo: lila.study.ChapterRepo,
     chapterPreviewApi: ChapterPreviewApi,
     cacheApi: CacheApi,
@@ -181,17 +180,19 @@ private final class RelayPlayerApi(
 
   type RelayPlayers = SeqMap[StudyPlayer.Id, RelayPlayer]
 
-  private val singleTourCache = cacheApi[RelayTourId, RelayPlayers](128, "relay.players.data"):
-    _.expireAfterWrite(3.minute).buildAsyncFuture(computeTour)
+  private val cache = cacheApi[RelayTourId, RelayPlayers](128, "relay.players.data"):
+    _.expireAfterWrite(1.minute).buildAsyncFuture(compute)
 
-  private val groupJsonCache = cacheApi[RelayTourId, JsonStr](32, "relay.players.json"):
+  private val jsonCache = cacheApi[RelayTourId, JsonStr](32, "relay.players.json"):
     _.expireAfterWrite(1.minute).buildAsyncFuture: tourId =>
       import RelayPlayer.json.given
-      for
-        tourIds <- groupRepo.allTourIdsOfGroup(tourId)
-        tourPlayers <- tourIds.traverse(singleTourCache.get)
-        allPlayers = tourPlayers.foldLeft(SeqMap.empty: RelayPlayers)(_ ++ _)
-      yield JsonStr(Json.stringify(Json.toJson(allPlayers.values.toList)))
+      cache
+        .get(tourId)
+        .map: players =>
+          JsonStr(Json.stringify(Json.toJson(players.values.toList)))
+
+  export cache.get
+  export jsonCache.get as jsonList
 
   private val photosJsonCache = cacheApi[RelayTourId, PhotosJson](32, "relay.players.photos.json"):
     _.expireAfterWrite(10.seconds).buildAsyncFuture: tourId =>
@@ -201,14 +202,12 @@ private final class RelayPlayerApi(
         photos <- photosJson(fideIds)
       yield photos
 
-  def jsonList(tourId: RelayTourId): Fu[JsonStr] = groupJsonCache.get(tourId)
-
   def photosJson(tourId: RelayTourId): Fu[PhotosJson] = photosJsonCache.get(tourId)
 
   def player(tour: RelayTour, str: String): Fu[Option[RelayPlayer]] =
     val id = FideId.from(str.toIntOption) | PlayerName(str)
     for
-      players <- singleTourCache.get(tour.id)
+      players <- cache.get(tour.id)
       player = players.get(id)
     yield player
 
@@ -216,10 +215,10 @@ private final class RelayPlayerApi(
 
   private val invalidateDebouncer = Debouncer[RelayTourId](scheduler.scheduleOnce(3.seconds, _), 32): id =>
     import lila.memo.CacheApi.invalidate
-    singleTourCache.invalidate(id)
-    groupJsonCache.invalidate(id)
+    cache.invalidate(id)
+    jsonCache.invalidate(id)
 
-  private def computeTour(tourId: RelayTourId): Fu[RelayPlayers] =
+  private def compute(tourId: RelayTourId): Fu[RelayPlayers] =
     tourRepo
       .byId(tourId)
       .flatMapz: tour =>
