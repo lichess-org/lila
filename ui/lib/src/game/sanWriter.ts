5486db42f2cea133741bb883a78a75317d030565
Thibault Duplessis <t@lichess.org>
2025-12-29 09:39:54 +0100

    use chessops.makeSquare
    also type Uci

--- ui/lib/src/game/sanWriter.ts
+++ ui/lib/src/game/sanWriter.ts
@@ -1,6 +1,6 @@
 /* eslint no-restricted-syntax:"error" */ // no side effects allowed due to re-export by index.ts
 
-import { charToRole, type Square } from 'chessops';
+import { charToRole, makeSquare, type Square } from 'chessops';
 import { fixCrazySan } from './chess';
 
 type AlmostSan = string;
@@ -89,77 +89,10 @@ function slidingMovesTo(s: number, deltas: number[], board: Board): number[] {
  * but lacks the check/checkmate flag,
  * and probably has incomplete disambiguation.
  * But it's quick. */
-const SQUARE_TO_UCI = [
-  'a1',
-  'b1',
-  'c1',
-  'd1',
-  'e1',
-  'f1',
-  'g1',
-  'h1',
-  'a2',
-  'b2',
-  'c2',
-  'd2',
-  'e2',
-  'f2',
-  'g2',
-  'h2',
-  'a3',
-  'b3',
-  'c3',
-  'd3',
-  'e3',
-  'f3',
-  'g3',
-  'h3',
-  'a4',
-  'b4',
-  'c4',
-  'd4',
-  'e4',
-  'f4',
-  'g4',
-  'h4',
-  'a5',
-  'b5',
-  'c5',
-  'd5',
-  'e5',
-  'f5',
-  'g5',
-  'h5',
-  'a6',
-  'b6',
-  'c6',
-  'd6',
-  'e6',
-  'f6',
-  'g6',
-  'h6',
-  'a7',
-  'b7',
-  'c7',
-  'd7',
-  'e7',
-  'f7',
-  'g7',
-  'h7',
-  'a8',
-  'b8',
-  'c8',
-  'd8',
-  'e8',
-  'f8',
-  'g8',
-  'h8',
-];
-
 export function almostSanOf(
   board: Board,
   uci: string,
-  legalUcis: Set<string> | undefined = undefined,
+  legalUcis: Set<Uci> | undefined = undefined,
 ): AlmostSan {
   if (uci.includes('@')) return fixCrazySan(uci);
 
@@ -199,7 +132,7 @@ export function almostSanOf(
     file = false;
   for (let i = 0; i < candidates.length; i++) {
     if (candidates[i] === from || board.pieces[candidates[i]] !== p) continue;
-    if (legalUcis && !legalUcis.has(SQUARE_TO_UCI[candidates[i]] + move[1])) continue;
+    if (legalUcis && !legalUcis.has(makeSquare(candidates[i]) + move[1])) continue;
     if (from >> 3 === candidates[i] >> 3) file = true;
     if ((from & 7) === (candidates[i] & 7)) rank = true;
     else file = true;
@@ -213,7 +146,7 @@ export function almostSanOf(
   return san;
 }
 
-export function sanWriter(fen: string, ucis: string[]): SanToUci {
+export function sanWriter(fen: string, ucis: Uci[]): SanToUci {
   const board = readFen(fen);
   const sans: SanToUci = {};
   const legalUcis = new Set(ucis);
