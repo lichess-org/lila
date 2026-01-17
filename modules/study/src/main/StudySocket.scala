654ddee9bef66582262b434e6680b610a5423ecc
Thibault Duplessis <t@lichess.org>
2026-01-17 13:44:56 +0100

    the mobile app still uses node.id when merging server analysis

--- modules/study/src/main/StudySocket.scala
+++ modules/study/src/main/StudySocket.scala
@@ -12,9 +12,9 @@ import lila.room.RoomSocket.{ Protocol as RP, * }
 import lila.core.socket.{ protocol as P, * }
 import lila.tree.Branch
 import lila.tree.Node.{ Comment, Gamebook, Shape, Shapes }
-import lila.tree.Node.defaultNodeJsonWriter
 import lila.core.study.Visibility
 import cats.mtl.Handle.*
+import lila.tree.Node.lichobileNodeJsonWriter
 
 final private class StudySocket(
     api: StudyApi,
@@ -46,7 +46,7 @@ final private class StudySocket(
           Json.obj(
             "analysis" -> analysis,
             "ch" -> chapterId,
-            "tree" -> defaultNodeJsonWriter.writes(tree),
+            "tree" -> lichobileNodeJsonWriter.writes(tree),
             "division" -> division
           )
         )
