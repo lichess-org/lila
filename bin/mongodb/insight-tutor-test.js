95c2aa3d3ec806fdf71a26426d77a323702e65ca
kraktus <kraktus@users.noreply.github.com>
2026-01-21 12:36:54 +0100

    fix formatting of `bin/mongodb/insight-tutor-test.js`

--- bin/mongodb/insight-tutor-test.js
+++ bin/mongodb/insight-tutor-test.js
@@ -1,41 +1,43 @@
-const pipeline = [{
-  "$match": {
-    "a": true,
-    "m": {
-      "$elemMatch": {
-        "w": { "$lt": 33300 },
-        "i": { "$lt": -1 }
-      }
+const pipeline = [
+  {
+    $match: {
+      a: true,
+      m: {
+        $elemMatch: {
+          w: { $lt: 33300 },
+          i: { $lt: -1 },
+        },
+      },
+      p: 3,
+      mr: {
+        $gte: 1605,
+        $lte: 1627,
+      },
     },
-    "p": 3,
-    "mr": {
-      "$gte": 1605, "$lte": 1627
-    }
-  }
-}, {
-  "$limit": 3333
-}, {
-  "$group": {
-    "_id": "$p",
-    "loss": {
-      "$sum": {
-        "$cond": [
-          { "$eq": ["$r", 3] },
-          1,
-          0
-        ]
-      }
+  },
+  {
+    $limit: 3333,
+  },
+  {
+    $group: {
+      _id: '$p',
+      loss: {
+        $sum: {
+          $cond: [{ $eq: ['$r', 3] }, 1, 0],
+        },
+      },
+      nb: { $sum: 1 },
     },
-    "nb": { "$sum": 1 }
-  }
-}];
+  },
+];
 
 const q = {
-  "a": true,
-  "p": 3,
-  "mr": {
-    "$gte": 1605, "$lte": 1627
-  }
+  a: true,
+  p: 3,
+  mr: {
+    $gte: 1605,
+    $lte: 1627,
+  },
 };
 
 // db.insight.aggregate(pipeline);
