ab6df004bb6ac86db8bc5fdc3b8c6ab6a2541ff6
Thibault Duplessis <t@lichess.org>
2026-01-22 10:09:55 +0100

    add root-level oxfmt config
    to format stuff outside of ui/, such as .github/

--- ui/lint-staged.config.mjs
+++ ui/lint-staged.config.mjs
@@ -2,7 +2,7 @@ import { lstatSync } from 'fs';
 
 export default {
   // NOTE: these patterns must stay in sync with bin/git-hooks/pre-commit!
-  '*.{json,scss,ts}': files => {
+  'ui/*.{json,scss,ts}': files => {
     const regularFiles = files.filter(f => !lstatSync(f).isSymbolicLink());
     return regularFiles.length ? `oxfmt --config=ui/.oxfmtrc.json --write ${regularFiles.join(' ')}` : 'true';
   },
