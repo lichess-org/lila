2a379418dcbedf484f2d05e5e3a7a12ebd3445b4
Thibault Duplessis <t@lichess.org>
2026-01-22 09:38:02 +0100

    restore and fix git hook pre-commit after #19179

---
+++ ui/lint-staged.config.mjs
@@ -0,0 +1,11 @@
+import { lstatSync } from 'fs';
+
+export default {
+  // NOTE: these patterns must stay in sync with bin/git-hooks/pre-commit!
+  '*.{json,scss,ts}': files => {
+    const regularFiles = files.filter(f => !lstatSync(f).isSymbolicLink());
+    return regularFiles.length
+      ? `oxfmt --config=ui/.oxfmtrc.json --write ${regularFiles.join(' ')}`
+      : 'true';
+  },
+};
