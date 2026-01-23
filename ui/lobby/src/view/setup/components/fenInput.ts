4624de595191e55e286c0768288d34f79439b433
Thibault Duplessis <t@lichess.org>
2026-01-23 10:31:05 +0100

    fix lobby fenInput component argument

--- ui/lobby/src/view/setup/components/fenInput.ts
+++ ui/lobby/src/view/setup/components/fenInput.ts
@@ -1,40 +1,39 @@
 import { h } from 'snabbdom';
 import * as licon from 'lib/licon';
-import type LobbyController from '@/ctrl';
 import { initMiniBoard } from 'lib/view';
+import type SetupController from '@/setupCtrl';
 
-export const fenInput = (ctrl: LobbyController) => {
-  const { setupCtrl } = ctrl;
-  if (setupCtrl.variant() !== 'fromPosition') return null;
-  const fen = setupCtrl.fen();
+export const fenInput = (ctrl: SetupController) => {
+  if (ctrl.variant() !== 'fromPosition') return null;
+  const fen = ctrl.fen();
   return h('div.config-group', [
     h('div.fen__form', [
       h('input#fen-input', {
         attrs: { placeholder: i18n.site.pasteTheFenStringHere, value: fen },
         on: {
           input: (e: InputEvent) => {
-            setupCtrl.fen((e.target as HTMLInputElement).value.replace(/_/g, ' ').trim());
-            setupCtrl.validateFen();
+            ctrl.fen((e.target as HTMLInputElement).value.replace(/_/g, ' ').trim());
+            ctrl.validateFen();
           },
         },
-        hook: { insert: setupCtrl.validateFen },
-        class: { failure: setupCtrl.fenError },
+        hook: { insert: ctrl.validateFen },
+        class: { failure: ctrl.fenError },
       }),
       h('a.button.button-empty', {
         attrs: {
           'data-icon': licon.Pencil,
           title: i18n.site.boardEditor,
-          href: '/editor' + (fen && !setupCtrl.fenError ? `/${fen.replace(/ /g, '_')}` : ''),
+          href: '/editor' + (fen && !ctrl.fenError ? `/${fen.replace(/ /g, '_')}` : ''),
         },
       }),
     ]),
     h(
       'a.fen__board',
-      { attrs: { href: `/editor/${setupCtrl.lastValidFen.replace(/ /g, '_')}` } },
-      !setupCtrl.lastValidFen || !setupCtrl.validFen()
+      { attrs: { href: `/editor/${ctrl.lastValidFen.replace(/ /g, '_')}` } },
+      !ctrl.lastValidFen || !ctrl.validFen()
         ? null
         : h('div.position.mini-board.cg-wrap.is2d', {
-            attrs: { 'data-state': `${setupCtrl.lastValidFen},white` },
+            attrs: { 'data-state': `${ctrl.lastValidFen},white` },
             hook: {
               insert: vnode => initMiniBoard(vnode.elm as HTMLElement),
               update: vnode => initMiniBoard(vnode.elm as HTMLElement),
