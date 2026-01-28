d6636af901e018e68c593adbece5e896cc25e806
Thibault Duplessis <t@lichess.org>
2026-01-28 08:37:30 +0100

    insights cleanup script

---
+++ bin/mongodb/insight-cleanup.js
@@ -0,0 +1,20 @@
+const insight = connect('mongodb://localhost:27318/insight');
+const sec = connect('mongodb://localhost:27117/lichess');
+
+console.log('Counting marked users...');
+
+const markedSelector = { marks: { $in: ['engine', 'boost'] } };
+const nbMarked = sec.user4.countDocuments(markedSelector);
+
+console.log(`Found ${nbMarked} marked users. Starting cleanup...`);
+
+let inspected = 0;
+let deleted = 0;
+
+sec.user4.find(markedSelector, { _id: 1 }).forEach((u) => {
+  deleted += insight.insight.deleteMany({ u: u._id }).deletedCount;
+  inspected += 1;
+  if (inspected % 1000 === 0) {
+    console.log(`Processing user ${inspected}/${nbMarked} / Deleted ${deleted} insight entries`);
+  }
+});
