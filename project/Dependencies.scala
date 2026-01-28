b2c55b1fb7019ec442d10c5382bd62d0e2bc534a
Scala Steward <scala_steward@virtuslab.com>
2026-01-28 02:01:13 +0000

    Update compression to 3.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val alleycats = "org.typelevel" %% "alleycats-core" % "2.13.0"
   val catsMtl = "org.typelevel" %% "cats-mtl" % "1.6.0"
   val hasher = "com.roundeights" %% "hasher" % "1.3.1"
-  val compression = "com.github.lichess-org" % "compression" % "3.1.3"
+  val compression = "com.github.lichess-org" % "compression" % "3.2.0"
   val maxmind = "com.maxmind.geoip2" % "geoip2" % "4.0.1"
   val caffeine = "com.github.ben-manes.caffeine" % "caffeine" % "3.2.3" % "compile"
   val scaffeine = "com.github.blemale" %% "scaffeine" % "5.3.0" % "compile"
