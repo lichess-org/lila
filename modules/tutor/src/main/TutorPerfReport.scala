e33d33a50bde0bb44480c3c246ee9f2666a75e27
Thibault Duplessis <t@lichess.org>
2026-01-29 12:37:51 +0100

    flagging skills is not relevant in classical

--- modules/tutor/src/main/TutorPerfReport.scala
+++ modules/tutor/src/main/TutorPerfReport.scala
@@ -127,7 +127,6 @@ private object TutorPerfReport:
     InsightMetric.ClockPercent,
     List(Filter(InsightDimension.Phase, List(Phase.Middle, Phase.End)))
   )
-  private def hasClock(p: PerfType) = p != PerfType.Correspondence
 
   def compute(users: NonEmptyList[TutorPlayer])(using InsightApi, Executor): Fu[List[TutorPerfReport]] =
     for
@@ -142,7 +141,7 @@ private object TutorPerfReport:
         for
           openings <- TutorOpening.compute(user)
           phases <- TutorPhases.compute(user)
-          flagging <- hasClock(user.perfType).so(TutorFlagging.compute(user))
+          flagging <- TutorFlagging.computeIfRelevant(user)
         yield TutorPerfReport(
           user.perfType,
           user.perfStats,
