9c331fd41f08336ace23d4afdd99d8fdc09b3aea
Thibault Duplessis <t@lichess.org>
2026-01-31 10:17:18 +0100

    RelayI18n scala tweaks

--- modules/relay/src/main/RelayI18n.scala
+++ modules/relay/src/main/RelayI18n.scala
@@ -1,8 +1,11 @@
 package lila.relay
 
-object RelayI18n:
-  import lila.core.i18n.{ I18nKey, Translate }
-  private val roundRegex = """(?i)^Round (\d+)$""".r
+import lila.core.i18n.Translate
+import lila.core.i18n.I18nKey.*
+
+private object RelayI18n:
+
+  private val roundRegex = """(?i)^round (\d+)$""".r
   private val blitzRegex = """(?i)^blitz$""".r
   private val rapidRegex = """(?i)^rapid$""".r
   private val classicalRegex = """(?i)^classical$""".r
@@ -17,24 +20,24 @@ object RelayI18n:
   private val finalsRegex = """(?i)^finals$""".r
   private val tiebreaksRegex = """(?i)^tiebreaks$""".r
   private val sep = """\s+\|\s+""".r
+
   def apply(name: String)(using Translate): String =
     sep
       .split(name)
-      .map: part =>
-        part match
-          case roundRegex(number) => I18nKey.broadcast.roundX.txt(number)
-          case blitzRegex() => I18nKey.site.blitz.txt()
-          case rapidRegex() => I18nKey.site.rapid.txt()
-          case classicalRegex() => I18nKey.site.classical.txt()
-          case openRegex() => I18nKey.broadcast.openTournament.txt()
-          case womenRegex() => I18nKey.broadcast.womenTournament.txt()
-          case boysRegex() => I18nKey.broadcast.boysTournament.txt()
-          case girlsRegex() => I18nKey.broadcast.girlsTournament.txt()
-          case boysUnderXRegex(age) => I18nKey.broadcast.boysUnderXTournament.txt(age)
-          case girlsUnderXRegex(age) => I18nKey.broadcast.girlsUnderXTournament.txt(age)
-          case quarterfinalsRegex() => I18nKey.broadcast.quarterfinals.txt()
-          case semifinalsRegex() => I18nKey.broadcast.semifinals.txt()
-          case finalsRegex() => I18nKey.broadcast.finals.txt()
-          case tiebreaksRegex() => I18nKey.broadcast.tiebreaks.txt()
-          case _ => name
+      .map:
+        case roundRegex(number) => broadcast.roundX.txt(number)
+        case blitzRegex() => site.blitz.txt()
+        case rapidRegex() => site.rapid.txt()
+        case classicalRegex() => site.classical.txt()
+        case openRegex() => broadcast.openTournament.txt()
+        case womenRegex() => broadcast.womenTournament.txt()
+        case boysRegex() => broadcast.boysTournament.txt()
+        case girlsRegex() => broadcast.girlsTournament.txt()
+        case boysUnderXRegex(age) => broadcast.boysUnderXTournament.txt(age)
+        case girlsUnderXRegex(age) => broadcast.girlsUnderXTournament.txt(age)
+        case quarterfinalsRegex() => broadcast.quarterfinals.txt()
+        case semifinalsRegex() => broadcast.semifinals.txt()
+        case finalsRegex() => broadcast.finals.txt()
+        case tiebreaksRegex() => broadcast.tiebreaks.txt()
+        case _ => name
       .mkString(" | ")
