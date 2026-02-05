27351e6eead105a0f0cb5cca761925c81df4eca6
Thibault Duplessis <t@lichess.org>
2026-02-05 19:36:00 +0100

    send fewer user_perf queries to the PRI

--- modules/core/src/main/user.scala
+++ modules/core/src/main/user.scala
@@ -215,7 +215,11 @@ object user:
     def withPerfs(u: User): Fu[UserWithPerfs]
     def withPerfs[U: UserIdOf](id: U): Fu[Option[UserWithPerfs]]
     def byIdWithPerf[U: UserIdOf](id: U, pk: PerfKey): Fu[Option[WithPerf]]
-    def listWithPerfs[U: UserIdOf](us: List[U], includeClosed: Boolean): Fu[List[UserWithPerfs]]
+    def listWithPerfs[U: UserIdOf](
+        us: List[U],
+        includeClosed: Boolean,
+        fromPri: Boolean = false
+    ): Fu[List[UserWithPerfs]]
     def perfOf[U: UserIdOf](u: U, perfKey: PerfKey): Fu[Perf]
     def perfOf(ids: Iterable[UserId], perfKey: PerfKey): Fu[Map[UserId, Perf]]
     def perfOptionOf[U: UserIdOf](u: U, perfKey: PerfKey): Fu[Option[Perf]]
