01a5a61174dd017e4442e6f7d3673b0e5ef12f58
Thibault Duplessis <t@lichess.org>
2026-02-07 12:01:15 +0100

    cleanup simuls after 48h

--- cron/mongodb-simul-cleanup.js
+++ cron/mongodb-simul-cleanup.js
@@ -1,4 +1,4 @@
-const since = new Date(Date.now() - 1000 * 3600 * 24 * 7);
+const since = new Date(Date.now() - 1000 * 3600 * 24 * 2);
 db.simul.deleteMany({
   status: 10,
   hostSeenAt: { $lt: since },
