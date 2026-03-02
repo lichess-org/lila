b522246663ce380ec9b1d00f434c096607a329a7
Simek <gosimek@gmail.com>
2026-03-02 11:17:30 +0100

    workspace: fix lint-staged configs, lint styles, add scalafmt command

--- ui/lint-staged.config.mjs
+++ ui/lint-staged.config.mjs
@@ -1,9 +1,19 @@
-import { lstatSync } from 'fs';
+import { lstatSync } from 'node:fs';
+
+function filterSymbolicLinks(files) {
+  return files.filter(f => !lstatSync(f).isSymbolicLink());
+}
 
 export default {
   // NOTE: these patterns must stay in sync with bin/git-hooks/pre-commit!
-  'ui/*.{json,scss,ts}': files => {
-    const regularFiles = files.filter(f => !lstatSync(f).isSymbolicLink());
-    return regularFiles.length ? `oxfmt --config=ui/.oxfmtrc.json --write ${regularFiles.join(' ')}` : 'true';
+  'ui/*.{json,ts,mts,mjs}': files => {
+    const regularFiles = filterSymbolicLinks(files);
+    return regularFiles.length
+      ? `oxlint --type-aware --config=.oxlintrc.json --tsconfig=./ui/tsconfig.base.json --fix ${regularFiles.join(' ')} && oxfmt`
+      : 'true';
+  },
+  'ui/*.scss': files => {
+    const regularFiles = filterSymbolicLinks(files);
+    return regularFiles.length ? `stylelint ${regularFiles.join(' ')} --fix && oxfmt` : 'true';
   },
 };
