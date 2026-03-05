f5122965709336b675129118e259c0fef9b4233c
Thibault Duplessis <t@lichess.org>
2026-03-05 11:02:00 +0100

    add formatters/linters to flake.nix

--- flake.nix
+++ flake.nix
@@ -59,6 +59,9 @@
               nodejs_24
               nodePackages.pnpm
               esbuild
+              oxlint
+              oxfmt
+              stylelint
 
               java
               scala
