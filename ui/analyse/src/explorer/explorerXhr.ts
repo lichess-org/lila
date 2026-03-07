3a7af3c7fef018623f17e0ae9637dd2fe385704f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2026-03-07 16:30:28 +0100

    use normal explorer endpoint without /li again

--- ui/analyse/src/explorer/explorerXhr.ts
+++ ui/analyse/src/explorer/explorerXhr.ts
@@ -22,7 +22,7 @@ export async function opening(
 ): Promise<void> {
   const conf = opts.config;
   const confByDb = conf.byDb();
-  const url = new URL(`/li/${opts.db}`, opts.endpoint);
+  const url = new URL(`/${opts.db}`, opts.endpoint);
   const params = url.searchParams;
   params.set('variant', opts.variant || 'standard');
   params.set('fen', opts.rootFen);
