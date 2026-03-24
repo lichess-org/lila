c322bc45cafc2c11debafa655a549cbfb4458a9d
ComradeRamen <150104144+ComradeRamen@users.noreply.github.com>
2026-03-24 14:52:12 -0700

    Increase onlineBotVisible limit from 200 to 500

--- modules/user/src/main/package.scala
+++ modules/user/src/main/package.scala
@@ -6,6 +6,6 @@ export lila.rating.UserWithPerfs
 
 private val logger = lila.log("user")
 
-val onlineBotVisible = Max(200)
+val onlineBotVisible = Max(500)
 
 case class LightCount(user: lila.core.LightUser, count: Int)
