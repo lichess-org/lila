f3c523215e6be7683aee7dee6d23e95dfe03fcd3
johndoknjas <jdoknjas@sfu.ca>
2026-05-30 16:09:16 -0700

    If the best move was played in the parent node, never annotate it as a bad move.

--- ui/analyse/src/liveAnnotate.ts
+++ ui/analyse/src/liveAnnotate.ts
@@ -19,13 +19,15 @@ export default class LiveAnnotate {
     node.children.forEach(child => this.update(path + child.id, child, node));
   };
 
-  private readonly liveGlyph = (
-    parentEval: EvalScore,
-    currentEval: EvalScore,
-    ply: Ply,
-  ): Glyph | undefined => {
-    const color: Color = ply % 2 === 1 ? 'white' : 'black';
-    const loss = povChances(color, parentEval) - povChances(color, currentEval);
+  private readonly liveGlyph = (node: TreeNode, parent: TreeNode): Glyph | undefined => {
+    if (
+      !parent.ceval ||
+      !node.ceval ||
+      (node.uci && node.uci === (parent.ceval.pvs[0]?.moves[0] ?? parent.ceval.bestmove))
+    )
+      return undefined;
+    const color: Color = node.ply % 2 === 1 ? 'white' : 'black';
+    const loss = povChances(color, parent.ceval) - povChances(color, node.ceval);
     if (loss > 0.3) return glyphs.blunder;
     if (loss > 0.2) return glyphs.mistake;
     if (loss > 0.1) return glyphs.inaccuracy;
@@ -34,7 +36,7 @@ export default class LiveAnnotate {
 
   private readonly update = (path: TreePath, node: TreeNode, parent: TreeNode): void => {
     if (!path.length) return;
-    const glyph = parent.ceval && node.ceval && this.liveGlyph(parent.ceval, node.ceval, node.ply);
+    const glyph = this.liveGlyph(node, parent);
     if (glyph) this.glyphs.set(path, glyph);
     else this.glyphs.delete(path);
   };
