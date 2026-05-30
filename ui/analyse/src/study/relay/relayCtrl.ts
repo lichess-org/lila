421607167e1e2847ce82a93493759ad04d221566
Nicolas Vaagen <nicolasvaagen@gmail.com>
2026-05-30 13:08:19 -0600

    Add check for broadcasts to allow optional off for chat

--- ui/analyse/src/study/relay/relayCtrl.ts
+++ ui/analyse/src/study/relay/relayCtrl.ts
@@ -46,7 +46,9 @@ export default class RelayCtrl {
     this.tourSelectShow = toggle(false, this.study.ctrl.redraw);
     this.roundSelectShow = toggle(false, this.study.ctrl.redraw);
     if (study.ctrl.opts.chat) {
-      const liveboardDisabled = () => this.tourShow() || !study.multiBoard.showResults();
+      const liveboardDisabled = () => {
+        study.chapters.list.size() === 1 || this.tourShow() || !study.multiBoard.showResults();
+      }
       this.liveboardPlugin = new LiveboardPlugin(
         study,
         this.round,
