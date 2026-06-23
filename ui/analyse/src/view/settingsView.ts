416f23f428fcfeacceadc7e38e7006b8395d4076
Jonathan Gamble <gamblej@gmail.com>
2026-06-23 15:47:59 -0500

    fix analysis settings on hover mobiles with coarse pointer (samsung)

--- ui/analyse/src/view/settingsView.ts
+++ ui/analyse/src/view/settingsView.ts
@@ -205,10 +205,12 @@ function setupHoverHelp(view: HTMLElement) {
 }
 
 function defaultToggleHtml(ctrl: SettingsCtrl, key: SettingKey) {
-  const label =
-    settings[key].helpHtml && isTouchDevice()
-      ? `<button class="help-button" data-key="${key}" data-icon="${licon.InfoCircle}">${settings[key].label}</button>`
-      : `<span class="hover-help" data-key="${key}">${settings[key].label}</span>`;
+  const setting = settings[key];
+  const label = setting.helpHtml
+    ? isTouchDevice()
+      ? `<button class="help-button" data-key="${key}" data-icon="${licon.InfoCircle}">${setting.label}</button>`
+      : `<span class="hover-help" data-key="${key}">${setting.label}</span>`
+    : setting.label;
   return $html`
     <span class="setting">
       ${label}
