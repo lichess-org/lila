f3aff5c53d0e080111f8bda02532a2e248482499
Jonathan Gamble <gamblej@gmail.com>
2026-07-07 18:57:18 -0500

    Dont auto-quote all of a forum post

--- ui/bits/src/bits.forum.ts
+++ ui/bits/src/bits.forum.ts
@@ -109,19 +109,14 @@ site.load.then(() => {
       setMode(textarea, 'write');
     });
 
-  const quoted = new Set<string>();
-
   $('.quote.button').on('click', function (this: HTMLButtonElement) {
     const post = this.closest<HTMLElement>('.forum-post')!,
       authorUsername = $(post).find('.author').attr('href')?.substring(3),
       author = authorUsername ? '@' + authorUsername : $(post).find('.author').text(),
       reply = document.querySelector<HTMLTextAreaElement>('.reply .post-text-area')!;
 
-    const lines = (
-      quotedMarkdown(this.closest('article')) ??
-      post.querySelector('.forum-post__message-source')!.textContent
-    )
-      .replace(/!\[([^\]]*)]\(([^)]+)\)/g, '$1 ($2)')
+    const lines = quotedMarkdown(this.closest('article'))
+      .replace(/!\[([^\]]*)]\(([^)]+)\)/g, '$1 ($2)') // unlink images
       .split('\n');
     if (lines[0].match(/^(?:> )*@.+ said (?:in #\d+:$|\[\^\]\()/)) lines.shift();
 
@@ -135,8 +130,6 @@ site.load.then(() => {
         .trim() +
       '\n\n';
 
-    if (quoted.has(quote)) return;
-    quoted.add(quote);
     setMode(reply, 'write');
     reply.value = reply.value.slice(0, reply.selectionStart) + quote + reply.value.slice(reply.selectionEnd);
     const caretOffset = reply.selectionStart + quote.length;
@@ -215,12 +208,16 @@ site.load.then(() => {
   if (replyEl?.value) replyEl.scrollIntoView(); // scrollto if pre-populated
 });
 
-function quotedMarkdown(postEl: HTMLElement | null): string | undefined {
+function quotedMarkdown(postEl: HTMLElement | null): string {
+  const source = postEl?.querySelector('.forum-post__message-source')?.textContent;
+  if (!source) return '';
+
+  const trimmed = source.slice(0, 400) + (source.length > 400 ? '...' : '');
   const selection = window.getSelection();
-  if (!postEl || !selection || selection.rangeCount === 0) return undefined;
+  if (!selection || selection.rangeCount === 0) return trimmed;
 
   const r = selection.getRangeAt(0);
-  if (!postEl?.contains(r.startContainer) || !postEl.contains(r.endContainer)) return undefined;
+  if (!postEl?.contains(r.startContainer) || !postEl.contains(r.endContainer)) return trimmed;
 
   const startEl =
     r.startContainer.nodeType === 3 ? r.startContainer.parentElement : (r.startContainer as Element);
@@ -228,9 +225,7 @@ function quotedMarkdown(postEl: HTMLElement | null): string | undefined {
 
   const startCap = Number(startEl?.closest<HTMLElement>('[data-ms]')?.dataset.ms);
   const endCap = Number(endEl?.closest<HTMLElement>('[data-me]')?.dataset.me);
-  const source = postEl.querySelector('.forum-post__message-source')?.textContent;
-
-  if (isNaN(startCap) || isNaN(endCap) || !source) return undefined;
+  if (isNaN(startCap) || isNaN(endCap) || !source) return trimmed;
 
   const sourceLines = selection.toString().trim().split('\n');
   const lastLine = sourceLines[sourceLines.length - 1].trim();
