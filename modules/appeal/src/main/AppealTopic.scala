20c34711acbe6becc74e8f2065916051476b131f
Thibault Duplessis <t@lichess.org>
2026-07-07 13:36:52 +0200

    select topic for mark

--- modules/appeal/src/main/AppealTopic.scala
+++ modules/appeal/src/main/AppealTopic.scala
@@ -1,4 +1,17 @@
 package lila
 package appeal
 
-object AppealTopicApi
+object AppealTopicApi:
+
+  def select(u: User): Option[AppealTopic] =
+    import AppealTopic.*
+    if u.enabled.no then close.some
+    else if u.marks.engine then cheat.some
+    else if u.marks.boost then boost.some
+    else if u.marks.troll then comm.some
+    else if u.marks.rankban then rank.some
+    else if u.marks.arenaBan then arena.some
+    else if u.marks.prizeban then prize.some
+    else none
+
+  // def userMessage(topic
