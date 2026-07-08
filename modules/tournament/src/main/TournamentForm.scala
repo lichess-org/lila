c50f5f41b94de172d2c186719283f6a17186c676
Thibault Duplessis <t@lichess.org>
2026-07-08 08:25:11 +0200

    tournament managers can change team battle dates

--- modules/tournament/src/main/TournamentForm.scala
+++ modules/tournament/src/main/TournamentForm.scala
@@ -85,7 +85,10 @@ final class TournamentForm:
           )
           .verifying(
             "Can't change start date of a team battle after 10 players have joined",
-            d => d.startDate.contains(tour.startsAt) || tour.nbPlayers < 10 || tour.teamBattle.isEmpty
+            _.startDate.contains(tour.startsAt) ||
+              tour.nbPlayers < 10 ||
+              tour.teamBattle.isEmpty ||
+              Granter(_.ManageTournament)
           )
 
   private def makeMapping(leaderTeams: List[LightTeam], prev: Option[Tournament])(using me: Me) =
