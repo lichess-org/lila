54c6bd52d6804c1dbc0360c36aaa72baa7d88cf0
Thibault Duplessis <t@lichess.org>
2026-07-09 10:40:13 +0200

    document export script

--- bin/mongodb/export-draws.js
+++ bin/mongodb/export-draws.js
@@ -1,3 +1,20 @@
+/*
+Exports draw offers to a CSV file.
+
+Output format:
+GameID,Draws,Variant
+
+Draws are plies separated by white space. Plus for white, minus for black.
+The variant is omitted for standard chess.
+
+Output sample:
+
+STsO5Bvd,+47,
+0M3VDciI,+58 +80 +102 -119,
+0B5JCSyq,-7 -28,crazy
+tEby487R,-34,atomic
+*/
+
 const date = { $gt: new Date('2022-01-01'), $lt: new Date('2026-01-01') };
 
 const variants = [
