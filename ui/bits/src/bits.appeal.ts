6c2db91206eb0263a76a703903e3f231f95ed923
Thibault Duplessis <t@lichess.org>
2026-07-12 10:00:43 +0200

    remove now superfluous appeal text date enhancements

--- ui/bits/src/bits.appeal.ts
+++ ui/bits/src/bits.appeal.ts
@@ -20,48 +20,4 @@ export function initModule(): void {
   $('form select[name="months"]').on('click', function (this: HTMLSelectElement) {
     if (this.value) (this.parentElement as HTMLFormElement).submit();
   });
-
-  enhanceDates();
-}
-
-function enhanceDates() {
-  const isoDateRegex = /\b(\d{4}-\d{2}-\d{2})\b/g;
-  const now = new Date();
-  now.setHours(0, 0, 0, 0);
-
-  $('.appeal__msg--mod .appeal__msg__text').each(function (this: HTMLElement) {
-    const walker = document.createTreeWalker(this, NodeFilter.SHOW_TEXT);
-
-    let node: Text | null;
-    while ((node = walker.nextNode() as Text | null)) {
-      if (!node?.nodeValue) continue;
-      const text = node.nodeValue;
-      const matches = [...text.matchAll(isoDateRegex)];
-      if (matches.length === 0) continue;
-
-      const frag = document.createDocumentFragment();
-      let lastIndex = 0;
-
-      matches.forEach(match => {
-        const date = new Date(match[0]);
-        const color = date > now ? 'var(--c-bad)' : 'var(--c-good)';
-
-        frag.append(text.slice(lastIndex, match.index));
-
-        const span = document.createElement('span');
-        span.style.color = color;
-        span.textContent = date.toLocaleDateString(undefined, {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        });
-
-        frag.append(span);
-        lastIndex = match.index + match[0].length;
-      });
-
-      frag.append(text.slice(lastIndex));
-      node.replaceWith(frag);
-    }
-  });
 }
