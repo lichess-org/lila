576cb9f66a2dc1230b45627a4d830a2e4894a8cd
Thibault Duplessis <t@lichess.org>
2026-07-16 16:27:40 +0200

    prevent posting in the wrong appeal topic
    when they open an appeal with an account, but post with another one

--- app/controllers/Appeal.scala
+++ app/controllers/Appeal.scala
@@ -50,10 +50,17 @@ final class Appeal(env: Env, reportC: => report.Report, userC: => User) extends 
   yield lila.appeal.UserStatus(user, playban, blogHidden, warning)
 
   def post(topic: AppealTopic) = AuthBody { ctx ?=> me ?=>
-    bindForm(userForm)(
-      err => BadRequest.async(renderAppealOrTree(err.some)),
-      text => env.appeal.api.post(topic, text).inject(Redirect(routes.Appeal.home).flashSuccess)
-    )
+    for
+      appeals <- env.appeal.api.byTopic(me)
+      status <- makeStatus(me)
+      res <-
+        if AppealTopicApi.select(status, appeals).exists(_ == topic) then
+          bindForm(userForm)(
+            err => BadRequest.async(renderAppealOrTree(err.some)),
+            text => env.appeal.api.post(topic, text).inject(Redirect(routes.Appeal.home).flashSuccess)
+          )
+        else fuccess(Redirect(routes.Appeal.home).flashFailure("You cannot post an appeal for this topic"))
+    yield res
   }
 
   def modQueue = Secure(_.Appeals) { ctx ?=> me ?=>
