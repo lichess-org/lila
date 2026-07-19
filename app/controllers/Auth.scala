f156735c49e43885eb1a1983c7392e949795dcbe
Thibault Duplessis <t@lichess.org>
2026-07-19 12:45:00 +0200

    no point in returning user tier in auth nginx subrequest

--- app/controllers/Auth.scala
+++ app/controllers/Auth.scala
@@ -685,15 +685,7 @@ final class Auth(env: Env, accountC: => Account) extends LilaController(env):
 
   def check = OpenOrScoped() { ctx ?=>
     ctx.me match
-      case Some(me) =>
-        val tier =
-          if me.is(UserId.lichess) then 4
-          else if me.isVerified then 2
-          else 1
-        NoContent.withHeaders(
-          "X-User" -> me.userId.value,
-          "X-Tier" -> tier.toString
-        )
+      case Some(me) => NoContent.withHeaders("X-User" -> me.userId.value)
       case None => Unauthorized
   }
 
