67a6aed3c5f8316234234392264c3998274b4c53
Thibault Duplessis <t@lichess.org>
2026-07-26 18:30:53 +0200

    fix use of `onInsert` in study multiboard
    fixess broadcast embeds

--- ui/analyse/src/study/relay/relayTeams.ts
+++ ui/analyse/src/study/relay/relayTeams.ts
@@ -84,12 +84,10 @@ export const teamsView = (ctrl: RelayTeams, chapters: StudyChapters, players: Re
     'div.relay-tour__team-table',
     {
       class: { loading: ctrl.loading, nodata: !ctrl.teams },
-      hook: {
-        insert: vnode => {
-          gameLinksListener(ctrl.chapterSelect)(vnode);
-          ctrl.loadFromXhr(true);
-        },
-      },
+      hook: onInsert(elm => {
+        gameLinksListener(ctrl.chapterSelect)(elm);
+        ctrl.loadFromXhr(true);
+      }),
     },
     ctrl.teams
       ? renderTeams(
