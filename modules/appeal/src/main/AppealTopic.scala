fc40b31eb8e90c63609baadef919bf7eff54bcf1
Thibault Duplessis <t@lichess.org>
2026-08-01 23:28:46 +0200

    ignore legacy appeals - they're all closed

--- modules/appeal/src/main/AppealTopic.scala
+++ modules/appeal/src/main/AppealTopic.scala
@@ -7,7 +7,7 @@ import lila.core.i18n.I18nKey.appeal as trans
 
 object AppealTopicApi:
 
-  val irrelevant = Set(AppealTopic.play, AppealTopic.chat)
+  val irrelevant = Set(AppealTopic.play, AppealTopic.chat, AppealTopic.legacy)
   val relevant = AppealTopic.values.filterNot(irrelevant).toList
 
   private[appeal] def candidatesFor(u: UserStatus): List[AppealTopic] =
@@ -30,8 +30,7 @@ object AppealTopicApi:
       .find: topic =>
         appeals.get(topic).forall(_.isOpen)
       .orElse:
-        List(AppealTopic.warning, AppealTopic.legacy)
-          .find(t => appeals.get(t).exists(_.isOpen))
+        List(AppealTopic.warning).find(t => appeals.get(t).exists(_.isOpen))
 
   def unmark(user: UserStatus, topic: AppealTopic): Option[(String, Call)] =
     candidatesFor(user)
