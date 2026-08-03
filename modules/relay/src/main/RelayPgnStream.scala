af3dda509323a56fa5fcb8da88f5303c83854c97
Thibault Duplessis <t@lichess.org>
2026-08-03 14:54:25 +0200

    add broadcast tournament streaming endpoint, refactor streams
    TODO: test

--- modules/relay/src/main/RelayPgnStream.scala
+++ modules/relay/src/main/RelayPgnStream.scala
@@ -10,6 +10,7 @@ import lila.common.Bus
 import lila.db.dsl.*
 import lila.study.{ ChapterRepo, PgnDump, StudyRepo, Study, Chapter }
 import lila.relay.BSONHandlers.given
+import lila.relay.RelayRoundRepo.selectors.notLongFinished
 
 final class RelayPgnStream(
     roundRepo: RelayRoundRepo,
@@ -146,56 +147,61 @@ final class RelayPgnStream(
         .orFail(s"Missing tour for round ${rs.relay.id}")
         .map(rs.withTour)
         .map: rt =>
-          initialSource(rt, flags).concat:
-            Source
-              .queue[Set[StudyChapterId]](8, org.apache.pekko.stream.OverflowStrategy.dropHead)
-              .mapMaterializedValue:
-                setupQueue(_, SyncResult.roundBusChannel(rt.relay.id))
-              .flatMapConcat(studyChapterRepo.byIdsSource)
-              .throttle(16, 1.second)
-              .mapAsync(1)(ofGame(rt, _, flags))
+          initialSource(rt, flags).concat(pgnSource(flags, SyncResult.roundBusChannel(rt.relay.id)))
+
+  def streamTourGames(tour: RelayTour)(using RequestHeader): Source[PgnStr, ?] =
+    val flags = requestPgnFlags
+    Source.futureSource:
+      for
+        rounds <- roundRepo.byTourOrdered(tour.id, notLongFinished)
+        withStudies <- withStudies(rounds.map(_.withTour(tour)))
+      yield Source(withStudies)
+        .flatMapConcat(initialSource(_, flags))
+        .concat(pgnSource(flags, SyncResult.tourBusChannel(tour.id)))
 
   def streamGroupGames(group: RelayGroup)(using RequestHeader): Source[PgnStr, ?] =
     val flags = requestPgnFlags
     Source.futureSource:
       for
         tours <- tourRepo.byIds(group.tours.toList)
-        allRounds <- roundRepo.byToursOrdered(tours.map(_.id))
-        rounds = allRounds.filter:
-          _.finishedAt.forall(_.isAfter(nowInstant.minusHours(1)))
+        rounds <- roundRepo.byToursOrdered(tours.map(_.id), notLongFinished)
         tourMap = tours.mapBy(_.id)
         withTours =
           for
             round <- rounds
             tour <- tourMap.get(round.tourId)
           yield round.withTour(tour)
-        studies <- studyRepo.publicByIds(rounds.map(_.studyId))
-        studyMap = studies.mapBy(_.id)
-        withStudies = withTours.flatMap(rt => studyMap.get(rt.round.studyId).map(rt.withStudy))
-        byStudyIdMap = withStudies.mapBy(_.study.id)
+        withStudies <- withStudies(withTours)
       yield Source(withStudies)
         .flatMapConcat(initialSource(_, flags))
-        .concat:
-          Source
-            .queue[Set[StudyChapterId]](8, org.apache.pekko.stream.OverflowStrategy.dropHead)
-            .mapMaterializedValue:
-              setupQueue(_, SyncResult.groupBusChannel(group.id))
-            .flatMapConcat(studyChapterRepo.byIdsSource)
-            .throttle(16, 1.second)
-            .mapAsync(1): chapter =>
-              byStudyIdMap.get(chapter.studyId).traverse(ofGame(_, chapter, flags))
-            .mapConcat(_.toList)
+        .concat(pgnSource(flags, SyncResult.groupBusChannel(group.id)))
+
+  private def withStudies(withTours: List[RelayRound.WithTour]) = for
+    studies <- studyRepo.publicByIds(withTours.map(_.round.studyId))
+    studyMap = studies.mapBy(_.id)
+  yield withTours.flatMap(rt => studyMap.get(rt.round.studyId).map(rt.withStudy))
+
+  private def pgnSource(flags: PgnDump.WithFlags, busChannel: String) =
+    Source
+      .queue[SyncEvent](8, org.apache.pekko.stream.OverflowStrategy.dropHead)
+      .mapMaterializedValue: queue =>
+        val sub = Bus.subscribeFunDyn(busChannel) { case SyncResult.Ok(chapters, _, in) =>
+          val chapterIds = chapters.view.filter(c => c.tagUpdate || c.newMoves > 0).map(_.id).toSet
+          queue.offer(SyncEvent(chapterIds, in))
+        }
+        queue
+          .watchCompletion()
+          .addEffectAnyway:
+            Bus.unsubscribeDyn(sub, List(busChannel))
+      .flatMapConcat: event =>
+        studyChapterRepo.byIdsSource(event.chapterIds).map(_ -> event.in)
+      .throttle(16, 1.second)
+      .mapAsync(1): (chapter, in) =>
+        ofGame(in, chapter, flags)
+
+  private case class SyncEvent(chapterIds: Set[StudyChapterId], in: RelayRound.WithTourAndStudy)
 
   private def initialSource(rt: RelayRound.WithTourAndStudy, flags: PgnDump.WithFlags) =
     if rt.relay.hasStarted
     then ofGames(rt, flags).throttle(32, 1.second)
     else Source.empty[PgnStr]
-
-  private def setupQueue(queue: SourceQueue[Set[StudyChapterId]], chan: String) =
-    val sub = Bus.subscribeFunDyn(chan) { case SyncResult.Ok(chapters, _) =>
-      queue.offer(chapters.view.filter(c => c.tagUpdate || c.newMoves > 0).map(_.id).toSet)
-    }
-    queue
-      .watchCompletion()
-      .addEffectAnyway:
-        Bus.unsubscribeDyn(sub, List(chan))
