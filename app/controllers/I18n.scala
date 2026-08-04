lichess-org/lila

commit df1fcf908806d86754a7a5eca6cf180565cc17a3

query string code golf

app/controllers/I18n.scala
@@ -21,10 +21,7 @@ final class I18n(env: Env) extends LilaController(env):
                 .fold(routes.Lobby.home.url): str =>
                   try
                     val pageUrl = new java.net.URI(str).parseServerAuthority().toURL()
-                    val path = pageUrl.getPath
-                    val query = pageUrl.getQuery
-                    if query == null then path
-                    else path + "?" + query
+                    pageUrl.getPath + pageUrl.getQuery.nonEmptyOption.so("?" + _)
                   catch case _: Exception => routes.Lobby.home.url
             if ctx.isAnon
             then redir.withCookies(env.security.lilaCookie.session("lang", lang.code))

modules/web/src/main/ResponseBuilder.scala
@@ -54,9 +54,7 @@ trait ResponseBuilder(using Executor)
 
   def redirectWithQueryString(path: String)(using req: RequestHeader) =
     Redirect:
-      if req.target.uriString.contains("?")
-      then s"$path?${req.target.queryString}"
-      else path
+      path + req.rawQueryString.nonEmptyOption.so("?" + _)
 
   private val movedMap: Map[String, String] = Map(
     "yt" -> "https://youtube.com/@LichessDotOrg",

commit 5bf2f63b5c6604704b7661cb8bdbe4141ad6c2b5

fix redirect after language change
was broken by df1fcf908806d86754a7a5eca6cf180565cc17a3

`getQuery` is a java function and can return null

app/controllers/I18n.scala
@@ -16,13 +16,7 @@ final class I18n(env: Env) extends LilaController(env):
         } >> negotiate(
           html =
             val redir = Redirect:
-              lila.common.HTTPRequest
-                .referer(ctx.req)
-                .fold(routes.Lobby.home.url): str =>
-                  try
-                    val pageUrl = new java.net.URI(str).parseServerAuthority().toURL()
-                    pageUrl.getPath + pageUrl.getQuery.nonEmptyOption.so("?" + _)
-                  catch case _: Exception => routes.Lobby.home.url
+              lila.common.HTTPRequest.referer(ctx.req) | routes.Lobby.home.url
             if ctx.isAnon
             then redir.withCookies(env.security.lilaCookie.session("lang", lang.code))
             else redir