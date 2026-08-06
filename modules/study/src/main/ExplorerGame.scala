f75da8696c6d2ca3913822907c94896efea9c48f
Alexander Hu <alexanderhu77@ucla.edu>
2026-08-06 08:36:55 -0700

    replay explorer games to fix ply renumbering on insert

--- modules/study/src/main/ExplorerGame.scala
+++ modules/study/src/main/ExplorerGame.scala
@@ -1,10 +1,11 @@
 package lila.study
 
-import chess.format.{ Fen, UciPath }
+import chess.format.{ Fen, Uci, UciPath }
 import chess.format.pgn.{ Parser, Tags, Comment as CommentStr }
+import chess.variant.Variant
 
 import lila.tree.Node.Comment
-import lila.tree.{ Branch, Node, Root }
+import lila.tree.{ Branch, Branches, Node, Root }
 
 final private class ExplorerGameApi(
     explorer: lila.core.game.Explorer,
@@ -30,7 +31,8 @@ final private class ExplorerGameApi(
                 path = UciPath.fromIds(root.mainline.map(_.id))
               )
             .so: gameRoot =>
-              merge(fromNode, position.path, gameRoot).flatMap { (newNode, path) =>
+              val variant = position.chapter.setup.variant
+              merge(fromNode, position.path, gameRoot, variant, gameId).flatMap { (newNode, path) =>
                 position.chapter.addNode(newNode, path).map(_ -> path)
               }
 
@@ -43,23 +45,55 @@ final private class ExplorerGameApi(
       val nodes = game.mainline.dropWhile(n => !compareFens(n.fen, fromNode.fen, firstTry))
       if nodes.nonEmpty || !firstTry then nodes.drop(1) else gameNodes(fromNode, game, false)
 
-  private def merge(fromNode: Node, fromPath: UciPath, game: Root): Option[(Branch, UciPath)] =
-    val (path, foundGameNode) = gameNodes(fromNode, game, true).foldLeft((UciPath.root, none[Branch])):
-      case ((path, None), gameNode) =>
-        val nextPath = path + gameNode.id
-        if fromNode.children.nodeAt(nextPath).isDefined then (nextPath, none)
-        else (path, gameNode.some)
-      case (found, _) => found
-    foundGameNode.map { _ -> fromPath.+(path) }
+  private def merge(
+      fromNode: Node,
+      fromPath: UciPath,
+      game: Root,
+      variant: Variant,
+      gameId: GameId
+  ): Option[(Branch, UciPath)] =
+    @annotation.tailrec
+    def dropKnown(anchor: Node, path: UciPath, nodes: List[Branch]): (Node, UciPath, List[Branch]) =
+      nodes match
+        case gameNode :: rest =>
+          anchor.children.get(gameNode.id) match
+            case Some(child) => dropKnown(child, path + gameNode.id, rest)
+            case None => (anchor, path, nodes)
+        case Nil => (anchor, path, nodes)
+    val (anchor, path, newNodes) = dropKnown(fromNode, UciPath.root, gameNodes(fromNode, game, true))
+    replay(anchor, newNodes, variant, gameId).map { _ -> fromPath.+(path) }
+
+  private def replay(anchor: Node, nodes: List[Branch], variant: Variant, gameId: GameId): Option[Branch] =
+    val setup = chess.Position.AndFullMoveNumber(variant, anchor.fen)
+    val sources = nodes.toVector
+    val (result, error) = setup.position.foldRight(nodes.map(_.move.uci), anchor.ply)(
+      none[Branch],
+      (step, acc) =>
+        val source = sources((step.ply - anchor.ply - 1).value)
+        inline def branch = makeBranch(source, step.move, step.ply)
+        acc.fold(branch)(branch.prependChildUnchecked).some
+    )
+    error.foreach: err =>
+      logger.warn(s"ExplorerGame replay ${gameUrl(gameId)} ${err.value}")
+    result
+
+  private def makeBranch(source: Branch, move: chess.MoveOrDrop, ply: chess.Ply): Branch =
+    source.copy(
+      ply = ply,
+      move = Uci.WithSan(move.toUci, move.toSanStr),
+      fen = Fen.write(move.after, ply.fullMoveNumber),
+      crazyData = move.after.crazyData,
+      children = Branches.empty
+    )
 
   private def gameComment(game: Game) =
     Comment(
       id = Comment.Id.make,
-      text = CommentStr(s"${gameTitle(game)}, ${gameUrl(game)}"),
+      text = CommentStr(s"${gameTitle(game)}, ${gameUrl(game.id)}"),
       by = Comment.Author.Lichess
     )
 
-  private def gameUrl(game: Game) = s"${net.baseUrl}/${game.id}"
+  private def gameUrl(gameId: GameId) = s"${net.baseUrl}/$gameId"
 
   private def gameTitle(g: Game): String =
     val tags = g.pgnImport.flatMap(pgni => Parser.tags(pgni.pgn).toOption).getOrElse(Tags.empty)
