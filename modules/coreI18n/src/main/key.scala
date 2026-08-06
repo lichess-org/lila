8b389bdafb45e67d79ef4228cf012014732d8c85
Thibault Duplessis <t@lichess.org>
2026-08-06 08:27:46 +0200

    move to i18n.msg.modActionFeedback

--- modules/coreI18n/src/main/key.scala
+++ modules/coreI18n/src/main/key.scala
@@ -965,6 +965,7 @@ object I18nKey:
   object msg:
     val `lichobileNewAppAvailable`: I18nKey = "msg:lichobileNewAppAvailable"
     val `lichobileNewAppDownload`: I18nKey = "msg:lichobileNewAppDownload"
+    val `modActionFeedback`: I18nKey = "msg:modActionFeedback"
 
   object nvui:
     val `featuredEvents`: I18nKey = "nvui:featuredEvents"
@@ -2032,7 +2033,6 @@ object I18nKey:
     val `biography`: I18nKey = "biography"
     val `countryRegion`: I18nKey = "countryRegion"
     val `thankYou`: I18nKey = "thankYou"
-    val `modActionFeedback`: I18nKey = "modActionFeedback"
     val `socialMediaLinks`: I18nKey = "socialMediaLinks"
     val `oneUrlPerLine`: I18nKey = "oneUrlPerLine"
     val `inlineNotation`: I18nKey = "inlineNotation"
