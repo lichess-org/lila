8b820929b2a7cbbfbf1ac67556acd9472a518fb6
Thibault Duplessis <t@lichess.org>
2026-08-09 09:48:58 +0200

    use .text class instead of custom margin

--- modules/ui/src/main/ui/navTree.scala
+++ modules/ui/src/main/ui/navTree.scala
@@ -47,7 +47,7 @@ object navTree:
   private def goBack(parent: Node, forceLtr: Boolean): Frag =
     a(
       makeLink(parent.id),
-      cls := List("back" -> true, "no-mirror" -> forceLtr),
+      cls := List("back text" -> true, "no-mirror" -> forceLtr),
       dataIcon := Icon.LessThan,
       title := "Go back"
     )
