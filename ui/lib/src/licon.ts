510f34314820497b11055667d0b833d5eb35b947
Jonathan Gamble <gamblej@gmail.com>
2026-08-12 14:49:48 -0500

    apply handset removal, leave scissors

--- ui/lib/src/licon.ts
+++ ui/lib/src/licon.ts
@@ -121,7 +121,6 @@ export const licon = {
   Disc: '' as const, // e06b
   Wings: '' as const, // e06c
   DiscOutline: '' as const, // e06d
-  Handset: '' as const, // e06e
   ArrowThruApple: '' as const, // e06f
   Clipboard: '' as const, // e070
   Move: '' as const, // e071
