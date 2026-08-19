73c594b9613e0d07aab34692fb027a8b9fc54f16
Thibault Duplessis <t@lichess.org>
2026-08-19 11:13:03 +0200

    delete superfluous scala pref json code
    pretty sure nothing of value was lost

--- app/controllers/Pref.scala
+++ app/controllers/Pref.scala
@@ -97,20 +97,12 @@ final class Pref(env: Env) extends LilaController(env):
   }
 
   def getJson(name: String) = Auth { ctx ?=> _ ?=>
-    lila.pref.PrefJsonChange.changes
-      .get(name)
-      .fold(notFound): change =>
-        change.get(ctx.pref).fold(NoContent)(JsonOk(_))
+    lila.pref.PrefJsonChange(name).so(_.read(ctx.pref).fold(NoContent)(JsonOk(_)))
   }
 
   def setJson(name: String) = AuthBody(parse.json) { ctx ?=> me ?=>
-    lila.pref.PrefJsonChange.changes
-      .get(name)
-      .fold(notFound): change =>
-        change
-          .set(ctx.pref, ctx.body.body)
-          .fold(
-            err => BadRequest(JsError.toJson(err)).toFuccess,
-            pref => api.setPref(me, pref).inject(NoContent)
-          )
+    lila.pref
+      .PrefJsonChange(name)
+      .so: change =>
+        api.setPref(me, change.write(ctx.pref, ctx.body.body)).inject(NoContent)
   }
--- modules/pref/src/main/PrefJsonChange.scala
+++ modules/pref/src/main/PrefJsonChange.scala
@@ -4,29 +4,12 @@ import play.api.libs.json.*
 
 object PrefJsonChange:
 
-  sealed trait Change:
-    def name: String
-    def get(pref: Pref): Option[JsValue]
-    def set(pref: Pref, value: JsValue): JsResult[Pref]
+  case class Change(name: String, read: Pref => Option[JsValue], write: (Pref, JsValue) => Pref)
 
-  private final case class Field[A: Reads: Writes](
-      name: String,
-      read: Pref => Option[A],
-      write: (Pref, Option[A]) => Pref
-  ) extends Change:
-    def get(pref: Pref): Option[JsValue] = read(pref).map(Json.toJson(_))
-    def set(pref: Pref, value: JsValue): JsResult[Pref] = value match
-      case JsNull => JsSuccess(write(pref, none))
-      case json => json.validate[A].map(v => write(pref, v.some))
+  private def notNull(js: JsValue) = Option.unless(js == JsNull)(js)
 
-  private def optional[A: Reads: Writes](name: String)(read: Pref => Option[A])(
-      write: (Pref, Option[A]) => Pref
-  ): Change =
-    Field(name, read, write)
+  val changes = List(
+    Change("lobbyShortcuts", _.lobbyShortcuts, (pref, js) => pref.copy(lobbyShortcuts = notNull(js)))
+  )
 
-  val changes: Map[String, Change] = List(
-    optional[JsValue]("lobbyShortcuts")(_.lobbyShortcuts): (pref, shortcuts) =>
-      pref.copy(lobbyShortcuts = shortcuts)
-  ).map: change =>
-    change.name -> change
-  .toMap
+  def apply(name: String) = changes.find(_.name == name)
