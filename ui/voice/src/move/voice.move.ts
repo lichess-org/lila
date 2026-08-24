5664dcf64d844d301c52e8cdadafdf6fb90a5666
Yet Another Fred <yafred@users.noreply.github.com>
2026-08-24 23:17:10 +0200

    Update ui/voice/src/move/voice.move.ts
    Tested
    
    Co-authored-by: Jonathan Gamble <gamblej@gmail.com>

--- ui/voice/src/move/voice.move.ts
+++ ui/voice/src/move/voice.move.ts
@@ -432,7 +432,7 @@ export function initModule({
         addToks(udest, uci); // includes en passant
         if (uci.startsWith(uci[2])) {
           addToks(`P${udest}`);
-        } else if (dp || !uci.startsWith(uci[2])) {
+        } else {
           addToks(`${usrc}x${udest}`);
           addToks(`Px${udest}`);
           addToks(`${uci[0]}x${udest}`, uci);
