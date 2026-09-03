773549ceaba6b9aa36688601fe0cd7953611ef5d
Alexander Hu <alexanderhu77@ucla.edu>
2026-09-02 23:23:47 -0700

    remove broken slist css key from shield category page

--- modules/tournament/src/main/ui/TournamentList.scala
+++ modules/tournament/src/main/ui/TournamentList.scala
@@ -321,7 +321,7 @@ final class TournamentList(helpers: Helpers, ui: TournamentUi)(
 
     def byCateg(categ: TournamentShield.Category, awards: List[TournamentShield.Award])(using Context) =
       Page("Tournament shields")
-        .css("tournament.leaderboard", "slist"):
+        .css("tournament.leaderboard"):
           main(cls := "page-menu page-small tournament-categ-shields")(
             shieldMenu,
             div(cls := "page-menu__content box")(
