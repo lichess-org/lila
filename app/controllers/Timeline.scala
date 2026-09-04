7c5f2468c4b4c79c00f9f311d37ff1ca01cddd7d
Thibault Duplessis <t@lichess.org>
2026-09-04 10:01:15 +0200

    remove oauth access to /api/timeline
    it's unused anyway

--- app/controllers/Timeline.scala
+++ app/controllers/Timeline.scala
@@ -32,8 +32,8 @@ final class Timeline(env: Env) extends LilaController(env):
     )
   }
 
-  def api = AuthOrScoped():
-    apiOutput(getIntAs[Max]("nb").fold(Max(15))(_.atMost(Max(30))))
+  def api = Auth: _ ?=>
+    _ ?=> apiOutput(getIntAs[Max]("nb").fold(Max(15))(_.atMost(Max(30))))
 
   private def apiOutput(max: Max)(using ctx: Context, me: Me) = for
     entries <- env.timeline.entryApi.moreUserEntries(me, max, since = getTimestamp("since"))
