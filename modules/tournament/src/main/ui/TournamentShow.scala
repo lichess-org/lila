2b5aa06ea075f865f0526223868e23cb242cb4b3
Sérgio Glórias <sergio@serginho.dev>
2026-09-04 23:29:59 +0100

    i18n: update tournament variant names

--- modules/tournament/src/main/ui/TournamentShow.scala
+++ modules/tournament/src/main/ui/TournamentShow.scala
@@ -280,15 +280,15 @@ final class TournamentShow(helpers: Helpers, gathering: GatheringUi)(
           ),
           tbody(
             tr(
-              td(trans.site.standard(), ", Chess960, Horde"),
+              td(s"${trans.variant.standard.txt()}, ${trans.variant.chess960.txt()}, ${trans.variant.horde.txt()}"),
               td(30)
             ),
             tr(
-              td("Antichess, Crazyhouse, King of the Hill"),
+              td(s"${trans.variant.antichess.txt()}, ${trans.variant.crazyhouse.txt()}, ${trans.variant.kingOfTheHill.txt()}"),
               td(20)
             ),
             tr(
-              td("Three check, Atomic, Racing Kings"),
+              td(s"${trans.variant.threeCheck.txt()}, ${trans.variant.atomic.txt()}, ${trans.variant.racingKings.txt()}"),
               td(10)
             )
           )
