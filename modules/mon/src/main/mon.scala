d7afe09fa73c878432bee610549a380a79fabf19
Thibault Duplessis <t@lichess.org>
2026-09-08 10:18:50 +0200

    monitor prometheus lines per metric

--- modules/mon/src/main/mon.scala
+++ modules/mon/src/main/mon.scala
@@ -773,7 +773,7 @@ object jvm:
     yield perState.withTags(tags("name" -> group.name, "state" -> state.toString)).update(count)
 
 object prometheus:
-  val lines = gauge("prometheus.lines").withoutTags()
+  def lines(metric: String) = gauge("prometheus.lines").withTag("metric", metric)
 
 def chronoSync[A] = Chronometer.syncMon[A]
 
--- modules/web/src/main/PrometheusReporter.scala
+++ modules/web/src/main/PrometheusReporter.scala
@@ -70,8 +70,22 @@ object PrometheusReporter:
     * to keep references to those instances programmatically and calling `.scrapeData()` directly on them
     * instead of using this function.
     */
-  def latestScrapeData(): Option[String] =
-    _lastCreatedInstance.map(_.scrapeData())
+  def latestScrapeData(): String =
+    _lastCreatedInstance.map(_.scrapeData()).orZero
+
+  def linesPerMetric(): Map[String, Int] =
+    latestScrapeData().linesIterator
+      .filter(_.nonEmpty)
+      .filterNot(_.startsWith("#"))
+      .foldLeft(Map.empty[String, Int]): (acc, line) =>
+        val metricName = line.takeWhile(c => c != ' ' && c != '{')
+        acc.updated(metricName, acc.getOrElse(metricName, 0) + 1)
+
+  private[web] def setupPeriodicMonitor()(using scheduler: Scheduler)(using Executor): Unit =
+    import scala.concurrent.duration.*
+    scheduler.scheduleAtFixedRate(1.minute, 1.minute): () =>
+      linesPerMetric().foreach: (name, lines) =>
+        lila.mon.prometheus.lines(name).update(lines)
 
   class Factory extends ModuleFactory:
     override def create(settings: ModuleFactory.Settings): Module =
