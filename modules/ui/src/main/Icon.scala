72ce487dcc08d5fc77a28811f1bcb91080f79a0e
Jonathan Gamble <gamblej@gmail.com>
2026-09-08 12:08:40 -0500

    Readd pruning shears icon

--- modules/ui/src/main/Icon.scala
+++ modules/ui/src/main/Icon.scala
@@ -129,7 +129,7 @@ object Icon:
   val Disc: Icon = "" // e06b
   val Wings: Icon = "" // e06c
   val DiscOutline: Icon = "" // e06d
-  val Language: Icon = "" // e06e
+  val Prune: Icon = "" // e06e
   val ArrowThruApple: Icon = "" // e06f
   val Clipboard: Icon = "" // e070
   val Move: Icon = "" // e071
@@ -143,3 +143,4 @@ object Icon:
   val Logo: Icon = "" // e07a
   val Switch: Icon = "" // e07b
   val Cpu: Icon = "" // e07e
+  val Language: Icon = "" // e07f
