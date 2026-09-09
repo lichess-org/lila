3e303a98bd1acb0a1a6cfa3f9721ec4a60c0f9bd
Sérgio Glórias <sergio@serginho.dev>
2026-09-09 23:55:06 +0100

    eval gauge: use linear-gradient

--- ui/lib/src/ceval/view/main.ts
+++ ui/lib/src/ceval/view/main.ts
@@ -137,8 +137,8 @@ export function renderGauge(ctrl: CevalHandler): VNode | undefined {
   } else ev = gaugeLast;
   return hl(
     'div.eval-gauge',
-    { class: { empty: !defined(bestEv), reverse: ctrl.getOrientation() === 'black' } },
-    [hl('div.black', { attrs: { style: `height: ${100 - (ev + 1) * 50}%` } }), gaugeTicks],
+    { class: { empty: !defined(bestEv), reverse: ctrl.getOrientation() === 'black' }, attrs: { style: `--evalPercent: ${100 - (ev + 1) * 50}%` } },
+    [gaugeTicks],
   );
 }
 
