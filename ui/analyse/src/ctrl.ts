3bff9fa12124e596f1b9b400d5ab256bea9a5c1a
mrobaczyk <mrobaczyk@man.poznan.pl>
2026-09-09 12:31:50 +0200

    fix(ui): preserve glyphs when navigating analysis

--- ui/analyse/src/ctrl.ts
+++ ui/analyse/src/ctrl.ts
@@ -26,6 +26,7 @@ import { ChatCtrl } from 'lib/chat/chatCtrl';
 import { displayColumns } from 'lib/device';
 import { playable, playedTurns, fenToEpd, validUci } from 'lib/game';
 import { plyColor } from 'lib/game/chess';
+import { endgameHighlights } from 'lib/game/endgame';
 import { PromotionCtrl } from 'lib/game/promotion';
 import { pubsub } from 'lib/pubsub';
 import { storedBooleanProp } from 'lib/storage';
@@ -377,6 +378,16 @@ export default class AnalyseCtrl implements CevalHandler {
         },
         check: node.check(),
         lastMove: uciToMove(node.uci),
+        highlight: {
+          custom:
+            node.outcome() || this.data.game.status.id >= 30
+              ? endgameHighlights(
+                  node.fen,
+                  node.outcome()?.winner || this.data.game.winner,
+                  node.check() ? 'mate' : node.outcome() ? 'stalemate' : this.data.game.status.name,
+                )
+              : new Map<Key, string>(),
+        },
       };
     config.premovable = {
       enabled: config.movable!.color && config.turnColor !== config.movable!.color,
