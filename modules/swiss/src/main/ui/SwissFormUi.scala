c7b64e763cc32eb9104c872d9c0065edbd8d6414
Simek <gosimek@gmail.com>
2026-09-12 22:47:14 +0200

    ui(swiss): edit form arrangement tweaks

--- modules/swiss/src/main/ui/SwissFormUi.scala
+++ modules/swiss/src/main/ui/SwissFormUi.scala
@@ -84,8 +84,9 @@ final class SwissFormUi(helpers: Helpers)(
 
     def tournamentFields =
       form3.fieldset("Tournament", toggle = true.some)(
-        form3.split(name, nbRounds),
-        form3.split(startsAt, description),
+        name,
+        form3.split(nbRounds, startsAt),
+        description,
         gatheringFormUi.payouts(form("payouts"))
       )
 
@@ -139,7 +140,6 @@ final class SwissFormUi(helpers: Helpers)(
         form("description"),
         trans.site.tournDescription(),
         help = trans.site.tournDescriptionHelp().some,
-        half = true
       )(form3.textarea(_)(rows := 4))
     def position =
       form3.group(
