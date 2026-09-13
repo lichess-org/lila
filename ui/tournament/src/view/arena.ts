d640c83f89f0d5ccdbdfdb855beb02bca4c446bb
Sérgio Glórias <sergio@serginho.dev>
2026-09-13 21:09:09 +0100

    refactor: replace h with div for podium rendering

--- ui/tournament/src/view/arena.ts
+++ ui/tournament/src/view/arena.ts
@@ -1,7 +1,5 @@
-import { h, type VNode } from 'snabbdom';
-
 import { licon } from 'lib/licon';
-import { bind, dataIcon, icon, type MaybeVNodes } from 'lib/view';
+import { bind, dataIcon, div, icon, type MaybeVNodes, type VNode, snabH as h } from 'lib/view';
 import { renderPager, searchButton, searchInput } from 'lib/view/pagination';
 import { userPatron, userLinkData } from 'lib/view/userLink';
 import { numberRow } from 'lib/view/util';
@@ -85,13 +83,13 @@ export function podium(ctrl: TournamentController) {
   const p = ctrl.data.podium || [];
   const podiumPosition = (p: PodiumPlayer, pos: string): VNode | undefined =>
     p
-      ? h('div.' + pos, [
-          h('div.trophy'),
+      ? div(`.${pos}`, [
+          div('.trophy'),
           h('a', userLinkData(p), [p.patronColor && userPatron(p), ...fullName(p)]),
           podiumStats(p, ctrl.data.berserkable, ctrl),
         ])
       : undefined;
-  return h('div.podium', [
+  return div('.podium', [
     podiumPosition(p[1], 'second'),
     podiumPosition(p[0], 'first'),
     podiumPosition(p[2], 'third'),
