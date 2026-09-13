9ccd3cf1c117afb9e0dd1a1bee4d8bdadef8a626
Thibault Duplessis <t@lichess.org>
2026-09-13 09:22:24 +0200

    narrow arena tournament textarea

--- modules/tournament/src/main/ui/TournamentForm.scala
+++ modules/tournament/src/main/ui/TournamentForm.scala
@@ -92,8 +92,7 @@ final class TournamentForm(val helpers: Helpers, showUi: TournamentShow)(
       form3.globalError(form),
       form3.fieldset("Tournament", toggle = true.some, disabled = fields.frozen)(
         form3.split(fields.name, fields.minutes),
-        form3.split(fields.description),
-        gatheringFormUi.payouts(form.prefix("payouts"))
+        form3.split(fields.description, gatheringFormUi.payouts(form.prefix("payouts")))
       ),
       form3.fieldset("Games", toggle = true.some, disabled = fields.frozen)(
         fields.clock,
@@ -412,7 +411,8 @@ final class TourFields(tourForm: TournamentForm)(form: Form[?], tour: Option[Tou
     form3.group(
       form.prefix("description"),
       trans.site.tournDescription(),
-      help = trans.site.tournDescriptionHelp().some
+      help = trans.site.tournDescriptionHelp().some,
+      half = true
     )(form3.textarea(_)(rows := 4))
   def entryCode =
     form3.group(
