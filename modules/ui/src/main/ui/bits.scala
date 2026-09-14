944ea90bbac1bc5413c76878603e7e261ea10c2a
Prajna Zhuge <322635747+zhugeprajna@users.noreply.github.com>
2026-09-14 10:51:33 +0800

    maybe ok

--- modules/ui/src/main/ui/bits.scala
+++ modules/ui/src/main/ui/bits.scala
@@ -134,8 +134,8 @@ object bits:
       uploadUrl.map(url => attr("data-image-upload-url") := url)
     )(
       div(cls := "header")(
-        button(cls := "header-tab write-tab active", tpe := "button")(trans.ublog.write()),
-        button(cls := "header-tab preview-tab", tpe := "button")(trans.ublog.preview()),
+        button(cls := "header-tab write-tab active", tpe := "button")(lila.core.i18n.I18nKey.ublog.write()),
+        button(cls := "header-tab preview-tab", tpe := "button")(lila.core.i18n.I18nKey.ublog.preview()),
         imageUploadButton
       ),
       div(cls := "content")(
