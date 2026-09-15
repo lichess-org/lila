6383970a7add48895e38aa40e0422622c890a18e
Sérgio Glórias <sergio@serginho.dev>
2026-09-15 11:30:04 +0100

    format

--- modules/mod/src/main/ui/ModUi.scala
+++ modules/mod/src/main/ui/ModUi.scala
@@ -16,7 +16,11 @@ final class ModUi(helpers: Helpers):
 
   def impersonate(user: User)(using Translate) =
     div(id := "impersonate")(
-      div(cls := "meat")("You are impersonating ", userLink(user, withOnline = false, withPatron = false), "."),
+      div(cls := "meat")(
+        "You are impersonating ",
+        userLink(user, withOnline = false, withPatron = false),
+        "."
+      ),
       div(cls := "actions"):
         postForm(action := routes.Mod.impersonate("-")):
           submitButton(cls := "button button-empty")("Quit")
