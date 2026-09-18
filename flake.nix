2b8eda7506e50559f3bfba39e978628abcbc5296
Simek <gosimek@gmail.com>
2026-09-18 10:03:10 +0200

    use repo-provided flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,13 @@
 {
   description = "lila development environment for Nix & flakes";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    sasso = {
+      url = "github:momiji-rs/sasso/v0.16.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
     { self, ... }@inputs:
@@ -35,7 +41,8 @@
         let
           jdk = prev."jdk${toString javaVersion}";
         in
-        rec {
+        (inputs.sasso.overlays.default final prev)
+        // rec {
           java = jdk;
           sbt = prev.sbt.override { jre = jdk; };
           scala = prev.scala_3.override { jre = jdk; };
@@ -53,41 +60,16 @@
             };
           });
 
-          sasso =
-            let
-              release = {
-                "aarch64-darwin" = {
-                  asset = "sasso-aarch64-apple-darwin.tar.xz";
-                  hash = "sha256-YCxkctYazgQt4+OHmMXvtyqu4eB27gmWsQrJzP/T9qw=";
-                };
-                "x86_64-darwin" = {
-                  asset = "sasso-x86_64-apple-darwin.tar.xz";
-                  hash = "sha256-23GCIPynJ0hqMI96g33Bg+vcKl/HFDkGYlRifKUSzng=";
-                };
-                "aarch64-linux" = {
-                  asset = "sasso-aarch64-unknown-linux-gnu.tar.xz";
-                  hash = "sha256-6FSQ2V/ODozpuTdvovh+4f/vbAooDysVTnuToqmuWhM=";
-                };
-                "x86_64-linux" = {
-                  asset = "sasso-x86_64-unknown-linux-gnu.tar.xz";
-                  hash = "sha256-nbPdqUzjAA9t3wuHs5+tcKvQEaEaKzpNK/9r1ytsK70=";
-                };
-              }."${final.stdenv.hostPlatform.system}";
-            in
-            final.stdenv.mkDerivation {
-              pname = "sasso";
-              version = "0.16.0";
-              src = final.fetchurl {
-                url = "https://github.com/momiji-rs/sasso/releases/download/v0.16.0/${release.asset}";
-                inherit (release) hash;
-              };
-              sourceRoot = ".";
-              installPhase = ''
-                mkdir -p $out/bin
-                tar -xJf $src --strip-components=1 -C $out/bin
-              '';
-            };
         };
+
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        {
+          sasso = inputs.sasso.packages.${pkgs.stdenv.system}.sasso;
+          sasso-ffi = inputs.sasso.packages.${pkgs.stdenv.system}.sasso-ffi;
+          default = inputs.sasso.packages.${pkgs.stdenv.system}.default;
+        }
+      );
 
       devShells = forEachSupportedSystem (
         { pkgs }:
