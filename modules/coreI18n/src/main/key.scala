bafbbabd8bf0b55fa9aa5b2ffb4da142d23caa9b
Prajna Zhuge <322635747+zhugeprajna@users.noreply.github.com>
2026-09-20 07:46:04 +0800

    add AccountPages i18n

--- modules/coreI18n/src/main/key.scala
+++ modules/coreI18n/src/main/key.scala
@@ -1611,6 +1611,12 @@ object I18nKey:
     val `cancelKeepAccount`: I18nKey = "settings:cancelKeepAccount"
     val `closeAccountAreYouSure`: I18nKey = "settings:closeAccountAreYouSure"
     val `thisAccountIsClosed`: I18nKey = "settings:thisAccountIsClosed"
+    val `wereSorryToSeeYouGo`: I18nKey = "settings:wereSorryToSeeYouGo"
+    val `closeAccountForeverLabel`: I18nKey = "settings:closeAccountForeverLabel"
+    val `closeAccountForeverWarning`: I18nKey = "settings:closeAccountForeverWarning"
+    val `deleteYourAccount`: I18nKey = "settings:deleteYourAccount"
+    val `deleteAccountWarning`: I18nKey = "settings:deleteAccountWarning"
+    val `deleteAccountConfirmText`: I18nKey = "settings:deleteAccountConfirmText"
 
   object site:
     val `gameSetup`: I18nKey = "gameSetup"
--- ui/@types/lichess/i18n.d.ts
+++ ui/@types/lichess/i18n.d.ts
@@ -3125,12 +3125,24 @@ interface I18n {
     closeAccount: string;
     /** Are you sure you want to close your account? */
     closeAccountAreYouSure: string;
+    /** Close forever: make it impossible to reopen */
+    closeAccountForeverLabel: string;
+    /** Prevent reopening the account later. If you tick this box, even administrators will be unable to reopen your account at your request. */
+    closeAccountForeverWarning: string;
+    /** I understand that deleted accounts aren't recoverable */
+    deleteAccountConfirmText: string;
+    /** Once you delete your account, it's removed from Lichess and our administrators won't be able to bring it back for you. */
+    deleteAccountWarning: string;
+    /** Delete your account */
+    deleteYourAccount: string;
     /** Your account is managed, and cannot be closed. */
     managedAccountCannotBeClosed: string;
     /** Settings */
     settings: string;
     /** This account is closed. */
     thisAccountIsClosed: string;
+    /** We're sorry to see you go. */
+    wereSorryToSeeYouGo: string;
   };
   site: {
     /** Abort game */
