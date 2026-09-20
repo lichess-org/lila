4d80ad7e8fd7d2f906b4af5fb9b2180722c90f99
YaFred <yafred@users.noreply.github.com>
2026-09-20 02:43:53 +0200

    add disconnect notification

--- ui/lib/src/nvui/notify.ts
+++ ui/lib/src/nvui/notify.ts
@@ -7,7 +7,9 @@ export class Notify {
   text = '';
   date?: Date;
 
-  constructor(public redraw: Redraw | undefined) {}
+  constructor(public redraw: Redraw | undefined) {
+    startOfflineObserver(this);
+  }
 
   set = (msg: string): void => {
     this.text = msg + (this.text === msg ? '\u00A0' : '');
@@ -29,4 +31,61 @@ export function liveText(
     ? { key: forceKey?.getTime().toString() || text, attrs: { role: 'alert' } }
     : { attrs: { 'aria-live': live, 'aria-atomic': 'true' } };
   return h(sel, data, text);
+}
+
+function startOfflineObserver(notify: Notify) {
+  let isConnected = document.body.classList.contains('online');
+  let offlineInterval: ReturnType<typeof setInterval> | null = null;
+  let reconnectAttempt = 0;
+
+  function startOfflineNotifications() {
+    if (offlineInterval !== null) {
+      return;
+    }
+
+    notify.set('You are disconnected');
+    offlineInterval = setInterval(() => {
+      if (!document.body.classList.contains('online')) {
+        reconnectAttempt++;
+        notify.set('You are disconnected. Reconnection attempt ' + reconnectAttempt);
+      } else {
+        stopOfflineNotifications();
+      }
+    }, 10000);
+  }
+
+  function stopOfflineNotifications() {
+    if (offlineInterval !== null) {
+      notify.set('You are online');
+      clearInterval(offlineInterval);
+      offlineInterval = null;
+    }
+  }
+
+  const observer = new MutationObserver(mutations => {
+    for (const mutation of mutations) {
+      if (mutation.type !== 'attributes' || mutation.attributeName !== 'class') {
+        continue;
+      }
+
+      const nowConnected = document.body.classList.contains('online');
+
+      if (!nowConnected && isConnected) {
+        isConnected = false;
+        startOfflineNotifications();
+      }
+
+      if (nowConnected && !isConnected) {
+        reconnectAttempt = 0;
+        isConnected = true;
+        stopOfflineNotifications();
+      }
+    }
+  });
+
+  observer.observe(document.body, {
+    attributes: true,
+    attributeFilter: ['class'],
+    attributeOldValue: true,
+  });
 }
