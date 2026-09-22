6c38b53b615fceb1cda3a4c2b9c12668d5d2904f
YaFred <yafred@users.noreply.github.com>
2026-09-22 18:56:03 +0200

    use chat.player.enabled only for anonymous user

--- ui/lib/src/chat/chatCtrl.ts
+++ ui/lib/src/chat/chatCtrl.ts
@@ -41,7 +41,8 @@ export class ChatCtrl {
   ) {
     this.data = opts.data;
     this.chatEnabled = this.data // tmp BC, remove check
-      ? storedBooleanProp(`chat.${this.data.resourceType}.enabled`, true)
+      ? (this.data.resourceType == 'player' && this.data.userId ? prop(true) : undefined) ||
+        storedBooleanProp(`chat.${this.data.resourceType}.enabled`, true)
       : prop(false);
     this.storedTabKey = storedStringProp(`chat.${opts.plugin ? opts.plugin.key + '.' : ''}tab`, 'discussion');
     if (!opts.kidMode) this.allTabs.push({ key: 'discussion' });
